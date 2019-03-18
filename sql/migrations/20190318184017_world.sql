984c411ea9d5663a2695b51445ca599663b868b3
GrenderG <grenderg@gmail.com>
2019-03-18 20:34:20 +0100

    Removed faction requirement for legacy mounts.

--- sql/migrations/20190318184017_world.sql
+++ sql/migrations/20190318184017_world.sql
@@ -18,6 +18,8 @@ update item_template set Quality = 3, display_id = 29448, Required_Skill = 762, 
 update item_template set name = 'Horn of the \"Skeletal\" Horse', description = 'Okay, not exactly skeletal, but you dont\'t want to mess with him.' where entry = 8583;
 update item_template set quality = 3, required_level = 0, bonding = 1, max_count = 1, stackable = 1 where entry = 23720;
 
+update item_template set allowable_race = -1 where entry in (12354, 12353, 12302, 13328, 13329, 13327, 13326, 12351, 12330, 15292, 15293, 13317, 8586);
+
 -- End of migration.
 END IF;
 END??
