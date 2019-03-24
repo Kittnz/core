af1faba62d86ce5a214e88808c8b159ba9455cd5
GrenderG <grenderg@gmail.com>
2019-03-25 00:32:52 +0100

    Added Diplomacy NPCs and quest and some minor db changes.

---
+++ sql/migrations/20190325001859_world.sql
@@ -0,0 +1,31 @@
+DROP PROCEDURE IF EXISTS add_migration;
+delimiter ??
+CREATE PROCEDURE `add_migration`()
+BEGIN
+DECLARE v INT DEFAULT 1;
+SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20190325001859');
+IF v=0 THEN
+INSERT INTO `migrations` VALUES ('20190325001859');
+-- Add your query below.
+
+update quest_template set requestitemstext = replace(requestitemstext, 'Ferlis', '$N') where entry = 3118;
+update creature_template set subname = 'The Impostor' where entry = 12657;
+
+insert into quest_template (entry, Method, MinLevel, QuestLevel, RequiredRaces, RewMoneyMaxLevel, Type, SpecialFlags, Title, OfferRewardText, RewItemId1, RewItemCount1) values
+(50011, 0, 0, 0, 178, 0, 82, 1, 'Horde diplomat', 'I see that you have finally understood that we should all unite in order to fight the real dangers that are upon us.', 50012, 1),
+(50012, 0, 0, 0, 77, 0, 82, 1, 'Alliance diplomat', 'I see that you have finally understood that we should all unite in order to fight the real dangers that are upon us.', 50012, 1);
+
+insert into creature_involvedrelation (id, quest) values
+(50027, 50011),
+(50028, 50012);
+
+insert into creature_questrelation (id, quest) values
+(50027, 50011),
+(50028, 50012);
+
+-- End of migration.
+END IF;
+END??
+delimiter ; 
+CALL add_migration();
+DROP PROCEDURE IF EXISTS add_migration;
