688183024605540418cf5b0f36e4a636b8966e6a
GrenderG <grenderg@gmail.com>
2019-03-28 00:58:29 +0100

    Typo.

--- sql/migrations/20190327233117_world.sql
+++ sql/migrations/20190327233117_world.sql
@@ -9,7 +9,7 @@ INSERT INTO `migrations` VALUES ('20190327233117');
 -- Add your query below.
 
 update item_template set display_id = 8119, name = 'Reindeer Reins', required_level = 40, required_skill = 762, required_skill_rank = 75, max_count = 1 where entry = 21044;
-update item_template set required_races = -1 where entry in (13322, 13323, 13324, 13325);
+update item_template set allowable_race = -1 where entry in (13322, 13323, 13324, 13325);
 update item_template set name = 'Hardcore Master\'s Tabard' where entry = 19160;
 
 -- End of migration.
