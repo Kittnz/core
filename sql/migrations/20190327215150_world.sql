ed1d6f70208e5af8ca363894a5719d49d839ac56
GrenderG <grenderg@gmail.com>
2019-03-28 01:00:52 +0100

    More typos.

--- sql/migrations/20190327215150_world.sql
+++ sql/migrations/20190327215150_world.sql
@@ -9,10 +9,10 @@ INSERT INTO `migrations` VALUES ('20190327215150');
 -- Add your query below.
 
 replace into creature_template (entry, display_id1, name, subname, level_min, level_max, health_min, health_max, faction, script_name, scale) values
-('50050', '15381', 'Darkmoon Steam Tonk', '', '1', '1', '64', '64', '35', '', 1),
+('50050', '15381', 'Darkmoon Steam Tonk', '', '1', '1', '64', '64', '35', '', 1);
 
 replace into custom_pet_entry_relation (item_entry, creature_entry) values
-('50200', '50050'), -- Darkmoon Steam Tonk
+('50200', '50050'); -- Darkmoon Steam Tonk
 
 -- End of migration.
 END IF;
