2ad68de1ff5cb45e595d3e9a72fab4896a462cc1
GrenderG <grenderg@gmail.com>
2019-03-30 03:32:48 +0100

    More fixes for Strange Bottle and Magic Muffin.

--- sql/migrations/20190330032141_world.sql
+++ sql/migrations/20190330032141_world.sql
@@ -8,7 +8,7 @@ IF v=0 THEN
 INSERT INTO `migrations` VALUES ('20190330032141');
 -- Add your query below.
 
-update item_template set bonding = 0, stackable = 20, buy_price = 1250, sell_price = 250 where entry in (50020, 50021);
+update item_template set bonding = 0, stackable = 20, buy_price = 500, sell_price = 250, buy_count = 1, spellcharges_1 = -1, spellcooldown_1 = 1000, delay = 0, subclass = 0 where entry in (50020, 50021);
 
 -- End of migration.
 END IF;
