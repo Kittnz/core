745d4b8b2a16113ab26fb0d8cbe9b036692d6ad2
GrenderG <grenderg@gmail.com>
2019-04-15 16:48:07 +0200

    Turtlhu should be more fun now. :)

---
+++ sql/migrations/20190415164605_world.sql
@@ -0,0 +1,18 @@
+DROP PROCEDURE IF EXISTS add_migration;
+delimiter ??
+CREATE PROCEDURE `add_migration`()
+BEGIN
+DECLARE v INT DEFAULT 1;
+SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20190415164605');
+IF v=0 THEN
+INSERT INTO `migrations` VALUES ('20190415164605');
+-- Add your query below.
+
+update creature_template set health_min = 398562, health_max = 398562 where entry = 50056;
+
+-- End of migration.
+END IF;
+END??
+delimiter ; 
+CALL add_migration();
+DROP PROCEDURE IF EXISTS add_migration;
--- src/scripts/custom/boss_turtlhu.cpp
+++ src/scripts/custom/boss_turtlhu.cpp
@@ -7,7 +7,13 @@ enum
     SPELL_MANA_BURN             = 26049,
     SPELL_SHADOWFORM            = 16592,
     SPELL_SHADOW_BOLT           = 17483,
-    SPELL_SHADOW_SHOCK          = 30138
+    SPELL_SHADOW_SHOCK          = 30138,
+    SPELL_PERIODIC_SHADOW_STORM = 2148,
+    SPELL_PIERCING_SHADOW       = 16429,
+    SPELL_VEIL_OF_SHADOW        = 22687,
+    SPELL_BLACKOUT              = 15269,
+    TWIN_TELEPORT_VISUAL        = 26638
+
 };
 
 struct boss_turtlhuAI : public ScriptedAI
@@ -21,6 +27,9 @@ struct boss_turtlhuAI : public ScriptedAI
     uint32 ManaBurn_Timer;
     uint32 ShadowBolt_Timer;
     uint32 ShadowShock_Timer;
+    uint32 PiercingShadow_Timer;
+    uint32 Veil_of_Shadow_Timer;
+    uint32 Blackout_Timer;
 
     bool mana_burn_warning_said;
     bool in_shadow_form;
@@ -28,10 +37,13 @@ struct boss_turtlhuAI : public ScriptedAI
     void SetDefaults() {
         m_creature->SetObjectScale(4.2f);
 
-        VoidBolt_Timer = 16000;
-        ManaBurn_Timer = 45000;
+        VoidBolt_Timer = 12000;
+        ManaBurn_Timer = 30000;
         ShadowBolt_Timer = urand(4000, 6000);
-        ShadowShock_Timer = urand(8000, 12000);
+        ShadowShock_Timer = urand(6000, 10000);
+        PiercingShadow_Timer = 12000;
+        Veil_of_Shadow_Timer = 16000;
+        Blackout_Timer = urand(6000, 16000);
 
         mana_burn_warning_said = false;
         in_shadow_form = false;
@@ -114,6 +126,7 @@ struct boss_turtlhuAI : public ScriptedAI
             m_creature->PMonsterYell("The shadows! I'll let them consume me to erase you!");
             m_creature->InterruptNonMeleeSpells(false);
             DoCast(m_creature, SPELL_SHADOWFORM);
+            DoCast(m_creature, SPELL_PERIODIC_SHADOW_STORM);
             in_shadow_form = true;
         }
 
@@ -122,10 +135,34 @@ struct boss_turtlhuAI : public ScriptedAI
             if (ShadowShock_Timer < diff)
             {
                 DoCast(m_creature, SPELL_SHADOW_SHOCK);
-                ShadowShock_Timer = urand(12000, 18000);
+                ShadowShock_Timer = urand(6000, 10000);
             } else {
                 ShadowShock_Timer -= diff;
             }
+        }
+
+        if (in_shadow_form) {
+            // Veil_of_Shadow_Timer
+            if (Veil_of_Shadow_Timer < diff)
+            {
+                if (DoCastSpellIfCan(m_creature->getVictim(), SPELL_VEIL_OF_SHADOW) == CAST_OK)
+                    Veil_of_Shadow_Timer = 20000;
+            } else {
+                Veil_of_Shadow_Timer -= diff;
+            }
+        }
+
+        // Blackout_Timer
+        if (Blackout_Timer < diff)
+        {
+            if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0)) {
+                DoCast(pTarget, SPELL_BLACKOUT);
+                Blackout_Timer = urand(6000, 16000);
+            } else {
+                Blackout_Timer = 1000;
+            }
+        } else {
+            Blackout_Timer -= diff;
         }
 
         // ShadowBolt_Timer
@@ -133,7 +170,10 @@ struct boss_turtlhuAI : public ScriptedAI
         {
             if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0)) {
                 DoCast(pTarget, SPELL_SHADOW_BOLT);
-                ShadowBolt_Timer = urand(3000, 6000);
+                if (!in_shadow_form)
+                    ShadowBolt_Timer = urand(3000, 6000);
+                else
+                    ShadowBolt_Timer = urand(1500, 3000);
             } else {
                 ShadowBolt_Timer = 1000;
             }
@@ -141,8 +181,32 @@ struct boss_turtlhuAI : public ScriptedAI
             ShadowBolt_Timer -= diff;
         }
 
+        // PiercingShadow_Timer
+        if (PiercingShadow_Timer < diff)
+        {
+            if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0)) {
+                DoCast(pTarget, SPELL_PIERCING_SHADOW);
+                PiercingShadow_Timer = 12000;
+            } else {
+                PiercingShadow_Timer = 1000;
+            }
+        } else {
+            PiercingShadow_Timer -= diff;
+        }
+
         //ManaBurn_Timer
         if ((ManaBurn_Timer - 10000) <= diff && !mana_burn_warning_said) {
+            std::list<Player*> players;
+            GetPlayersWithinRange(players, 75);
+            m_creature->InterruptNonMeleeSpells(false);
+            DoCast(m_creature, TWIN_TELEPORT_VISUAL);
+
+            for (auto &player : players) {
+                if (player && player->isAlive() && player != m_creature->getVictim())
+                    player->TeleportTo(m_creature->GetMapId(), m_creature->GetPositionX(), m_creature->GetPositionY(), m_creature->GetPositionZ(),
+                                       m_creature->GetOrientation(), TELE_TO_NOT_LEAVE_COMBAT);
+            }
+
             m_creature->PMonsterYell("Soon... You will run! Cowards!");
             mana_burn_warning_said = true;
         }
@@ -151,7 +215,7 @@ struct boss_turtlhuAI : public ScriptedAI
             m_creature->PMonsterYell("BURN! ALL OF YOUR ENERGY, BURN!");
             m_creature->InterruptNonMeleeSpells(false);
             DoCast(m_creature, SPELL_MANA_BURN);
-            ManaBurn_Timer = 45000;
+            ManaBurn_Timer = 30000;
             mana_burn_warning_said = false;
         }
         else
@@ -161,7 +225,7 @@ struct boss_turtlhuAI : public ScriptedAI
         if (VoidBolt_Timer < diff && !in_shadow_form)
         {
             if (DoCastSpellIfCan(m_creature->getVictim(), SPELL_VOIDBOLT) == CAST_OK)
-                VoidBolt_Timer = urand(9000, 16000);
+                VoidBolt_Timer = urand(9000, 12000);
         }
         else
             VoidBolt_Timer -= diff;
