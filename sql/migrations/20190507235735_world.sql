4aa5f5498cb5564632669a41056c53dfb186cba2
GrenderG <grenderg@gmail.com>
2019-05-08 00:02:53 +0200

    BOP for Turtle Eggs.

--- sql/migrations/20190507235735_world.sql
+++ sql/migrations/20190507235735_world.sql
@@ -9,6 +9,7 @@ INSERT INTO `migrations` VALUES ('20190507235735');
 -- Add your query below.
 
 update creature_template set inhabit_type = 2 where entry = 3581;
+update item_template set bonding = 1 where entry in (18963, 18964, 18965, 18966, 18967);
 
 -- End of migration.
 END IF;
