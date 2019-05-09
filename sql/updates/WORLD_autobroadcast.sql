8e7761dd63b82278eea2f5ebbe5fdbd3107fae3f
Torta <slowtorta@gmail.com>
2019-05-09 03:22:09 +0200

    Turtle TV & Chill.

--- sql/updates/WORLD_autobroadcast.sql
+++ sql/updates/WORLD_autobroadcast.sql
@@ -3,13 +3,15 @@ replace into `mangos_string` (`entry`, `content_default`) values ('50000', 'Turt
 replace into `mangos_string` (`entry`, `content_default`) values ('50001', 'Turtle WoW: Have a suggestion for our staff? Head over to our forums and submit your ideas, get support, or post something for the community to get involved in!');
 replace into `mangos_string` (`entry`, `content_default`) values ('50002', 'Turtle WoW: Take a peak at our Facebook page every now and then because we tend to it every day. You might just take it to your liking! https://www.facebook.com/turtlewowproject');
 replace into `mangos_string` (`entry`, `content_default`) values ('50003', 'Turtle WoW: Dear players, our shop is where you can find exotic wardrobes, mounts, and anything you need to spice up your roleplay! If you want to help support the server, check it out now: turtle-wow.org/donate');
-replace into `mangos_string` (`entry`, `content_default`) values ('50004', 'Turtle WoW: Greetings Tortollans! Want to find players to start a RP adventure? Looking for a fun guild to join? Keep up to date with the Turtle WoW community and join our Discord channel: https://discord.gg/mBGxmHy');
+replace into `mangos_string` (`entry`, `content_default`) values ('50004', 'Turtle WoW: Greetings! Want to find players to start a RP adventure? Looking for a fun guild to join? Keep up to date with the Turtle WoW community and join our Discord channel: https://discord.gg/mBGxmHy');
 replace into `mangos_string` (`entry`, `content_default`) values ('50005', 'Turtle WoW: The world is awaiting you! Type /join World now to connect with the community around you, and find friends for groups, dungeons, and trades!');
 replace into `mangos_string` (`entry`, `content_default`) values ('50006', 'Turtle WoW: Please remember, a GM will never ask for your password, so please do NOT share your password with anyone. Use a secure password, and change it often. Protect your account!');
 replace into `mangos_string` (`entry`, `content_default`) values ('50007', 'Turtle WoW: Donations are a way to help us improve your experience here at Turtle WoW, as well as help pay for general server costs, advertisement campaigns, developers, and overall production. You can find changelogs and videos on our website and social media platforms. Thanks for your continued support!');
 replace into `mangos_string` (`entry`, `content_default`) values ('50009', 'Turtle WoW: Please support Turtle WoW by voting! It will help us to maintain a healthy and stable population: turtle-wow.org/vote');
 replace into `mangos_string` (`entry`, `content_default`) values ('50010', 'Turtle WoW: Speak with Karn Deepeye in Orgrimmar or Tarun Swifteagle in Stormwind in order to become diplomats! You will be able to group, trade and speak with members of the other faction!');
 replace into `mangos_string` (`entry`, `content_default`) values ('50011', 'Turtle WoW: Come join the arena! You\'ll find Arena Ambassadors in any major city. Join the fray, get cool rewards for murdering friends and enemies in the Battle Ring, where you will experience a bonus x5 honor gaining!');
+replace into `mangos_string` (`entry`, `content_default`) values ('50012', 'Turtle TV & Chill! Please support our favorite streamer on Youtube: https://www.youtube.com/user/serenitytyaena/live');
+replace into `mangos_string` (`entry`, `content_default`) values ('50013', 'Turtle WoW: Do your new fancy shoulderpads look awful with your hair? Would you rather not cover your amazing face with an ugly helmet? Don\'t worry! Find your local barber in Stormwind or Crossroads and buy some hair dyes.');
 
 replace into `autobroadcast` (`string_Id`) values ('50000');
 replace into `autobroadcast` (`string_Id`) values ('50001');
@@ -22,3 +24,5 @@ replace into `autobroadcast` (`string_Id`) values ('50007');
 replace into `autobroadcast` (`string_Id`) values ('50009');
 replace into `autobroadcast` (`string_Id`) values ('50010');
 replace into `autobroadcast` (`string_Id`) values ('50011');
+replace into `autobroadcast` (`string_Id`) values ('50012');
+replace into `autobroadcast` (`string_Id`) values ('50013');
