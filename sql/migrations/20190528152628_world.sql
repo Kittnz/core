8e7b730e77a65ad9c21d792f933b7cdca3ca650e
GrenderG <grenderg@gmail.com>
2019-05-28 16:58:43 +0200

    Added tabards to sell to custom battlemasters.

--- sql/migrations/20190528152628_world.sql
+++ sql/migrations/20190528152628_world.sql
@@ -10,6 +10,16 @@ INSERT INTO `migrations` VALUES ('20190528152628');
 
 update item_template set buy_price = 10000, sell_price = 2500, required_reputation_faction = 0, required_reputation_rank = 0, script_name = 'wsg_tabard', spellid_1 = 26489, description = 'Fight for the Warsong Gulch! Use it to join the battle.' where entry in (19505, 19506);
 
+replace into npc_vendor (entry, item) values
+(50000, 19505);
+
+replace into npc_vendor (entry, item) values
+(50001, 19506);
+
+update creature_template set npc_flags = 5 where entry in (50000, 50001);
+update creature_template set faction = 85, vendor_id = 50000 where entry = 50000;
+update creature_template set faction = 11, vendor_id = 50001 where entry = 50001;
+
 -- End of migration.
 END IF;
 END??
--- src/scripts/custom/battlemaster.cpp
+++ src/scripts/custom/battlemaster.cpp
@@ -14,7 +14,8 @@ bool GossipHello_npc_battlemaster(Player* p_Player, Creature* p_Creature)
         p_Player->ADD_GOSSIP_ITEM(GOSSIP_ICON_BATTLE, MENU_AB, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
     if (p_Player->getLevel() > 50)
         p_Player->ADD_GOSSIP_ITEM(GOSSIP_ICON_BATTLE, MENU_AV, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3);
-    
+
+    p_Player->ADD_GOSSIP_ITEM(GOSSIP_ICON_VENDOR, GOSSIP_TEXT_BROWSE_GOODS, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 4);
 	p_Player->SEND_GOSSIP_MENU(GREETINGS, p_Creature->GetGUID());
 	p_Creature->HandleEmote(1);
 	return true;
@@ -33,9 +34,13 @@ bool GossipSelect_npc_battlemaster(Player* p_Player, Creature* p_Creature, uint3
             p_Player->GetSession()->SendBattlegGroundList(p_Player->GetObjectGuid(), BATTLEGROUND_AB);
         else if (uiAction == GOSSIP_ACTION_INFO_DEF + 3)
             p_Player->GetSession()->SendBattlegGroundList(p_Player->GetObjectGuid(), BATTLEGROUND_AV);
+        else if (uiAction == GOSSIP_ACTION_INFO_DEF + 4)
+            p_Player->SEND_VENDORLIST(p_Creature->GetGUID());
+
+        if (uiAction < (GOSSIP_ACTION_INFO_DEF + 4))
+            p_Creature->MonsterSay(IN_QUEUE, 0U, p_Player);
     }
 
-    p_Creature->MonsterSay(IN_QUEUE, 0U, p_Player);
 	p_Player->CLOSE_GOSSIP_MENU();
 	return true;
 }
