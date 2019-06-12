533626f0188954f858f0c04a5758031758385a64
GrenderG <grenderg@gmail.com>
2019-06-12 13:11:13 +0200

    Greysteel NPC should be friendly to everyone (xfaction guild).

---
+++ sql/migrations/20190612131030_world.sql
@@ -0,0 +1,18 @@
+DROP PROCEDURE IF EXISTS add_migration;
+delimiter ??
+CREATE PROCEDURE `add_migration`()
+BEGIN
+DECLARE v INT DEFAULT 1;
+SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20190612131030');
+IF v=0 THEN
+INSERT INTO `migrations` VALUES ('20190612131030');
+-- Add your query below.
+
+update creature_template set faction = 35 where entry in (50078, 50079);
+
+-- End of migration.
+END IF;
+END??
+delimiter ; 
+CALL add_migration();
+DROP PROCEDURE IF EXISTS add_migration;
