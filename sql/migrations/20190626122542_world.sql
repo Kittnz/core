fdb1c5d07dccd929b3d8dbbabb5d3c6ec46bdf32
Torta <slowtorta@gmail.com>
2019-06-26 14:47:30 +0200

    New GO: Duskwood Bed.

--- sql/migrations/20190626122542_world.sql
+++ sql/migrations/20190626122542_world.sql
@@ -16,6 +16,10 @@ REPLACE INTO `gameobject_template` (`entry`, `patch`, `type`, `displayId`, `name
 REPLACE INTO `gameobject` (`id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecsmin`, `spawntimesecsmax`, `animprogress`, `state`, `spawnFlags`, `visibilitymod`, `patch_min`, `patch_max`) VALUES (1000006, 0, -10729.4, 316.83, 40.75, 3.1507, 0, 0, 0.99999, -0.0045512, 0, 0, 100, 1, 0, 0, 0, 10);
 
 
+-- Custom GO:
+-- Bed:
+REPLACE INTO `gameobject_template` (`entry`, `patch`, `type`, `displayId`, `name`, `faction`, `flags`, `size`, `data0`, `data1`, `data2`, `data3`, `data4`, `data5`, `data6`, `data7`, `data8`, `data9`, `data10`, `data11`, `data12`, `data13`, `data14`, `data15`, `data16`, `data17`, `data18`, `data19`, `data20`, `data21`, `data22`, `data23`, `mingold`, `maxgold`, `script_name`) VALUES (1000007, 0, 5, 3471, 'Duskwood Bed', 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');
+
 -- End of migration.
 END IF;
 END??
