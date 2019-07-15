593b239c64caf48715315e498cd0554383b05c55
GrenderG <grenderg@gmail.com>
2019-07-15 18:03:57 +0200

    Removed unused sql.

--- sql/updates/raid_adjustments/MC.sql
+++
@@ -1,5 +0,0 @@
-update creature_template join creature on creature_template.entry=creature.id set health_min = (health_min * 0.4), health_max = (health_max * 0.4), dmg_min = (dmg_min * 0.7), dmg_max = (dmg_max * 0.8) where map = 409;
-update creature_template set health_min = (health_min * 0.4), health_max = (health_max * 0.4), dmg_min = (dmg_min * 0.6), dmg_max = (dmg_max * 0.7) where entry in (11663, 11664, 11502);
-
-update creature_template set health_min = (health_min * 0.7), health_max = (health_max * 0.7), dmg_min = (dmg_min * 0.7), dmg_max = (dmg_max * 0.7) where entry = 12099;
-update creature_template set dmg_min = (dmg_min * 0.75), dmg_max = (dmg_max * 0.75) where entry = 11672;
