cb5dfcd358d4a78780046bb1fa456ff345caf5a0
GrenderG <grenderg@gmail.com>
2019-07-28 04:21:45 +0200

    Removed unused Cho'rush script.

--- sql/migrations/20190727180648_world.sql
+++ sql/migrations/20190727180648_world.sql
@@ -16,6 +16,9 @@ INSERT INTO `creature_template` (`entry`, `patch`, `display_id1`, `display_id2`,
 DELETE FROM `spell_mod` WHERE `ID`=897;
 DELETE FROM `spell_effect_mod` WHERE `ID`=897;
 
+-- Removing unused db script for Cho'Rush the Observer.
+DELETE FROM `creature_ai_events` WHERE `creature_id`=14324;
+DELETE FROM `creature_ai_scripts` WHERE `id` IN (1432401, 1432402, 1432403, 1432404, 1432405, 1432406, 1432407, 1432408, 1432409, 1432410, 1432411, 1432412, 1432413, 1432414, 1432415, 1432416);
 
 -- End of migration.
 END IF;
