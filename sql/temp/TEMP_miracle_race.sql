22e84f2ae9b5cc50ed4e5d3ad8cc7f0d01984fd6
Torta <slowtorta@gmail.com>
2019-08-16 21:29:43 +0300

    SQL typo.

--- sql/temp/TEMP_miracle_race.sql
+++ sql/temp/TEMP_miracle_race.sql
@@ -1,6 +1,6 @@
 
 -- Daisy <Race Starter Girl>:
-update creature_template set script_name=npc_daisy where entry=4507;
+update creature_template set script_name='npc_daisy' where entry=4507;
 update creature_template set npc_flags=1 where entry=4507;		
 
 delete from creature where id = 4507;
@@ -13,5 +13,5 @@ replace into gameobject_template (entry, patch, type, displayid, name, faction, 
 (1000030, 0, 5, 3671, 'Salt Flats Racer Normal (Invisible)', 0, 0, 0.3, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'go_speed_normal');
 
 replace into creature_template (entry, patch, display_id1, display_id2, display_id3, display_id4, name, subname, gossip_menu_id, level_min, level_max, health_min, health_max, mana_min, mana_max, armor, faction, npc_flags, speed_walk, speed_run, scale, detection_range, call_for_help_range, leash_range, rank, xp_multiplier, dmg_min, dmg_max, dmg_school, attack_power, dmg_multiplier, base_attack_time, ranged_attack_time, unit_class, unit_flags, dynamic_flags, beast_family, trainer_type, trainer_spell, trainer_class, trainer_race, ranged_dmg_min, ranged_dmg_max, ranged_attack_power, type, type_flags, loot_id, pickpocket_loot_id, skinning_loot_id, holy_res, fire_res, nature_res, frost_res, shadow_res, arcane_res, spell_id1, spell_id2, spell_id3, spell_id4, spell_list_id, pet_spell_list_id, gold_min, gold_max, ai_name, movement_type, inhabit_type, civilian, racial_leader, regeneration, equipment_id, trainer_id, vendor_id, mechanic_immune_mask, school_immune_mask, flags_extra, script_name) values 
-(50513, 0, 3886, 0, 0, 0, 'Sheep', 'Definitely Non-Explosive', 90200, 30, 30, 1605, 1605, 0, 0, 1200, 11, 135, 1.08, 1.14286, 0, 20, 5, 0, 0, 1, 42, 53, 0, 122, 1, 1000, 2000, 1, 4608, 0, 0, 0, 0, 0, 0, 45.144, 62.073, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, , 0, 3, 0, 0, 3, 0, 0, 100, 0, 0, 10, 'npc_raceaway_sheep');
+(50513, 0, 3886, 0, 0, 0, 'Sheep', 'Definitely Non-Explosive', 90200, 30, 30, 1605, 1605, 0, 0, 1200, 11, 135, 1.08, 1.14286, 0, 20, 5, 0, 0, 1, 42, 53, 0, 122, 1, 1000, 2000, 1, 4608, 0, 0, 0, 0, 0, 0, 45.144, 62.073, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 0, 0, 100, 0, 0, 10, 'npc_raceaway_sheep');
 
