ce4b92840407e67f887e05c36309799bda603acc
Torta <slowtorta@gmail.com>
2019-09-29 14:29:43 +0200

    More Mirage Raceway quest fixes.

--- sql/migrations/20190929120308_world.sql
+++ sql/migrations/20190929120308_world.sql
@@ -2,8 +2,15 @@ replace into migrations values (20190929120308);
 
 -- Mirage Raceway quest fixes:
 update quest_template set type = 82 where entry in (50310, 50311, 50312, 50313, 50315);
-update quest_template set specialflags = 1 where entry in (50311, 50313);
 update quest_template set nextquestinchain = 50311 where entry = 50310;
 update quest_template set nextquestinchain = 50313 where entry = 50312;
+update quest_template set questflags = 1 where entry = 50310;
+update quest_template set specialflags = 2 where entry = 50310;
+update quest_template set questflags = 1 where entry = 50312;
+update quest_template set specialflags = 2 where entry = 50312;
+update quest_template set questflags = 1 where entry = 50311;
+update quest_template set specialflags = 3 where entry = 50311;
+update quest_template set questflags = 1 where entry = 50313;
+update quest_template set specialflags = 3 where entry = 50313;
 
 
