189c7f1b230d7267728211488cf36d32b92e4791
Torta <slowtorta@gmail.com>
2019-10-22 17:04:53 +0200

    New Expert training dummy.

--- sql/migrations/20191010150616_world.sql
+++ sql/migrations/20191010150616_world.sql
@@ -132,5 +132,14 @@ UPDATE `creature_template` SET `display_id2`='15173' WHERE (`entry`='50533') AND
 UPDATE `creature_template` SET `display_id3`='15173' WHERE (`entry`='50533') AND (`patch`='0');
 UPDATE `creature_template` SET `display_id4`='15173' WHERE (`entry`='50533') AND (`patch`='0');
 
+UPDATE `creature_model_info` SET `modelid_other_gender`='0' WHERE (`modelid`='15173') AND (`build`='0');
+UPDATE `creature_template` SET `display_id1`='15173' WHERE (`entry`='15063') AND (`patch`='5');
 
+-- Dummy fix:
+
+replace into `creature_template` (`entry`, `display_id1`, `display_id2`, `display_id3`, `display_id4`, `name`, `subname`, `gossip_menu_id`, `level_min`, `level_max`, `health_min`, `health_max`, `mana_min`, `mana_max`, `armor`, `faction`, `faction`, `npc_flags`, `speed_walk`, `speed_run`, `scale`, `rank`, `dmg_min`, `dmg_max`, `dmg_school`, `attack_power`, `dmg_multiplier`, `base_attack_time`, `ranged_attack_time`, `unit_class`, `unit_flags`, `dynamic_flags`, `beast_family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `ranged_dmg_min`, `ranged_dmg_max`, `ranged_attack_power`, `type`, `type_flags`, `loot_id`, `pickpocket_loot_id`, `skinning_loot_id`, `resistance1`, `resistance2`, `resistance3`, `resistance4`, `resistance5`, `resistance6`, `spell1`, `spell2`, `spell3`, `spell4`, `PetSpellDataId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `InhabitType`, `Civilian`, `RacialLeader`, `RegenHealth`, `equipment_id`, `trainer_id`, `vendor_id`, `MechanicImmuneMask`, `SchoolImmuneMask`, `flags_extra`, `ScriptName`) 
+values
+(50516,3019, 0, 0, 0, 'Expert Training Dummy', '', 0, 60, 60, 1000000, 1000000, 0, 0, 3000, 914, 914, 0, 0.91, 1.14286, 2, 0, 1, 2, 0, 0, 1, 2000, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 100, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 1, 0, 0, 0, 17, 0, 64, 'custom_npc_training_dummy');
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '2793635679' WHERE entry in (50516, 50514, 50515);
 
