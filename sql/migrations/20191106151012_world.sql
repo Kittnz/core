d8f90d9346eeff8fcfa9c082cdb2af68594fa6a5
Torta <slowtorta@gmail.com>
2019-11-12 18:38:12 +0100

    Another fix for Aurrius shield.

--- sql/migrations/20191106151012_world.sql
+++ sql/migrations/20191106151012_world.sql
@@ -3,3 +3,12 @@ replace into migrations values ('20191106151012');
 update item_template set spell_id1 = 5 where entry = 50243;
 delete from item_template where entry in (51203, 51204);
 update item_template set class = 15, subclass = 0, inventory_type = 0, spellcooldown_1 = 5000 where entry in (51200, 51201, 51202, 50017, 50408, 51205, 51206, 51207, 51208);
+
+
+delete from spell_effect_mod where id = 18392 and effectindex = 1;
+
+-- Fix Aurrius items:
+
+
+UPDATE `item_template` SET `class`='4', `subclass`='6' WHERE (`entry`='51042') AND (`patch`='0');
+UPDATE `item_template` SET `sheath`='4' WHERE (`entry`='51042') AND (`patch`='0');
--- sql/migrations/20191109164746_world.sql
+++
@@ -1,5 +0,0 @@
-replace into migrations values (20191109164746);
-
-delete from spell_effect_mod where id = 18392 and effectindex = 1;
-
-
