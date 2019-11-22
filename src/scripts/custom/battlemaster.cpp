d552d3a80c8cb558a1282fa7117703c71e5ff615
Torta <slowtorta@gmail.com>
2019-11-22 06:17:32 +0100

    New neutral WSG battlemaster.

--- src/scripts/custom/battlemaster.cpp
+++ src/scripts/custom/battlemaster.cpp
@@ -45,6 +45,34 @@ bool GossipSelect_npc_battlemaster(Player* p_Player, Creature* p_Creature, uint3
 	return true;
 }
 
+#define YOU_HAVE_NO_HONOR 90315
+
+bool GossipHello_wsg_arena(Player* p_Player, Creature* p_Creature)
+{
+    p_Player->ADD_GOSSIP_ITEM(GOSSIP_ICON_BATTLE, MENU_WSG, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+    p_Player->SEND_GOSSIP_MENU(YOU_HAVE_NO_HONOR, p_Creature->GetGUID());
+    p_Creature->HandleEmote(1);
+    return true;
+}
+
+bool GossipSelect_wsg_arena(Player* p_Player, Creature* p_Creature, uint32 uiSender, uint32 uiAction)
+{
+    if (!p_Player)
+        return false;
+
+    if (p_Player->isInCombat() || p_Player->InBattleGround() || p_Player->IsBeingTeleported() || p_Player->HasSpellCooldown(20939) || p_Player->HasSpellCooldown(26013) || (p_Player->getDeathState() == CORPSE))
+        ChatHandler(p_Player).PSendSysMessage("You are not meeting the conditions for joining!");
+    else
+    {
+        p_Player->SetBattleGroundEntryPoint();
+        if (uiAction == GOSSIP_ACTION_INFO_DEF + 1)
+            p_Player->GetSession()->SendBattlegGroundList(p_Player->GetObjectGuid(), BATTLEGROUND_WS);
+    }
+
+    p_Player->CLOSE_GOSSIP_MENU();
+    return true;
+}
+
 void AddSC_battlemaster()
 {
     Script *newscript;
@@ -53,5 +81,11 @@ void AddSC_battlemaster()
     newscript->Name = "npc_battlemaster";
     newscript->pGossipHello = &GossipHello_npc_battlemaster;
     newscript->pGossipSelect = &GossipSelect_npc_battlemaster;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name = "wsg_arena";
+    newscript->pGossipHello = &GossipHello_wsg_arena;
+    newscript->pGossipSelect = &GossipSelect_wsg_arena;
     newscript->RegisterSelf();
 }
