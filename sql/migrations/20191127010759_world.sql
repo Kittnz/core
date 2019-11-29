1a183210e300e49d11a687f8ba14a47850b510a4
Torta <slowtorta@gmail.com>
2019-11-29 02:42:11 +0100

    Winter Veil Vale NPCs and vendors.

--- sql/migrations/20191127010759_world.sql
+++ sql/migrations/20191127010759_world.sql
@@ -10,11 +10,13 @@ replace into creature_template_addon (entry, auras) values (50112, 26000);
 
 REPLACE INTO `creature_template` (`entry`, `patch`, `display_id1`, `display_id2`, `display_id3`, `display_id4`, `name`, `subname`, `gossip_menu_id`, `level_min`, `level_max`, `health_min`, `health_max`, `mana_min`, `mana_max`, `armor`, `faction`, `npc_flags`, `speed_walk`, `speed_run`, `scale`, `detection_range`, `call_for_help_range`, `leash_range`, `rank`, `xp_multiplier`, `dmg_min`, `dmg_max`, `dmg_school`, `attack_power`, `dmg_multiplier`, `base_attack_time`, `ranged_attack_time`, `unit_class`, `unit_flags`, `dynamic_flags`, `beast_family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `ranged_dmg_min`, `ranged_dmg_max`, `ranged_attack_power`, `type`, `type_flags`, `loot_id`, `pickpocket_loot_id`, `skinning_loot_id`, `holy_res`, `fire_res`, `nature_res`, `frost_res`, `shadow_res`, `arcane_res`, `spell_id1`, `spell_id2`, `spell_id3`, `spell_id4`, `spell_list_id`, `pet_spell_list_id`, `gold_min`, `gold_max`, `ai_name`, `movement_type`, `inhabit_type`, `civilian`, `racial_leader`, `regeneration`, `equipment_id`, `trainer_id`, `vendor_id`, `mechanic_immune_mask`, `school_immune_mask`, `flags_extra`, `script_name`) VALUES (50637, 0, 15374, 15375, 15374, 15375, 'Shivering Moonkin Hatchling', '', 6460, 60, 60, 10626, 10626, 8019, 8019, 4091, 1514, 2049, 1, 1.14286, 0, 20, 5, 0, 1, 1, 361, 467, 0, 278, 1, 2000, 2000, 1, 4672, 0, 0, 0, 0, 0, 0, 172.1, 240.07, 100, 7, 0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 14981, 0, 0, 0, 0, 524296, 'npc_shivering_moonkin');
 
--- replace into creature_template_addon (entry, emote) values ('50637', '13'); MAKE THEM SIT OR SLEEP (ADD SNOWY AURA)
+replace into creature_template_addon (entry, emote) values (50637, 16);
+replace into creature_template_addon (entry, auras) values (50637, 26000);
 
 UPDATE creature_template SET faction = 35 WHERE entry = 50637;
 UPDATE creature_template SET npc_flags = 1 WHERE entry = 50637;
 UPDATE creature_template SET scale = 0.4 WHERE entry = 50637;
+UPDATE creature_template SET movement_type = 1 WHERE entry = 50637;
 
 replace into broadcast_text (ID, MaleText) values ('90318', '<The feathers are covered with snow and the hatchling is shivering with cold we chomped.>');
 replace into npc_text (ID, BroadcastTextID0) values ('90318', '90318');
@@ -124,4 +126,132 @@ values
  '0', '1', 'item_engie');
  
  -- TODO: rename script.
+
+-- Winter Veil Vale citizens: 50638
+
+REPLACE INTO `creature_template` (`entry`, `patch`, `display_id1`, `display_id2`, `display_id3`, `display_id4`, `name`, `subname`, `gossip_menu_id`, `level_min`, `level_max`, `health_min`, `health_max`, `mana_min`, `mana_max`, `armor`, `faction`, `npc_flags`, `speed_walk`, `speed_run`, `scale`, `detection_range`, `call_for_help_range`, `leash_range`, `rank`, `xp_multiplier`, `dmg_min`, `dmg_max`, `dmg_school`, `attack_power`, `dmg_multiplier`, `base_attack_time`, `ranged_attack_time`, `unit_class`, `unit_flags`, `dynamic_flags`, `beast_family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `ranged_dmg_min`, `ranged_dmg_max`, `ranged_attack_power`, `type`, `type_flags`, `loot_id`, `pickpocket_loot_id`, `skinning_loot_id`, `holy_res`, `fire_res`, `nature_res`, `frost_res`, `shadow_res`, `arcane_res`, `spell_id1`, `spell_id2`, `spell_id3`, `spell_id4`, `spell_list_id`, `pet_spell_list_id`, `gold_min`, `gold_max`, `ai_name`, `movement_type`, `inhabit_type`, `civilian`, `racial_leader`, `regeneration`, `equipment_id`, `trainer_id`, `vendor_id`, `mechanic_immune_mask`, `school_immune_mask`, `flags_extra`, `script_name`) VALUES 
+
+(50638, 6, 15745, 0, 0, 0, 'Rosemary Riley', 'Winter Veil Vendor', 0, 1, 1, 5000, 5000, 0, 0, 7, 35, 0, 1.2, 1.14286, 1, 20, 5, 0, 0, 1, 2, 2, 0, 44, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 524288, ''),
+(50639, 6, 15747, 0, 0, 0, 'Meizu Dirgeheart', 'Winter Veil Reveler', 0, 1, 1, 5000, 5000, 0, 0, 7, 35, 0, 1.2, 1.14286, 1, 20, 5, 0, 0, 1, 2, 2, 0, 44, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 524288, ''),
+(50640, 6, 15748, 0, 0, 0, 'Vesstra Nightbreath', 'Winter Veil Sweet Treats>', 0, 1, 1, 5000, 5000, 0, 0, 7, 35, 0, 1.2, 1.14286, 1, 20, 5, 0, 0, 1, 2, 2, 0, 44, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 524288, ''),
+(50641, 6, 15749, 0, 0, 0, 'Mu\'ju', 'Winter Veil Reveler', 0, 1, 1, 5000, 5000, 0, 0, 7, 35, 0, 1.2, 1.14286, 1, 20, 5, 0, 0, 1, 2, 2, 0, 44, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 524288, ''),
+(50642, 6, 15751, 0, 0, 0, 'Endoga Wolfchain', 'Winter Veil Reveler', 0, 1, 1, 5000, 5000, 0, 0, 7, 35, 0, 1.2, 1.14286, 1, 20, 5, 0, 0, 1, 2, 2, 0, 44, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 524288, ''),
+(50643, 6, 15752, 0, 0, 0, 'Rasi Falseblaster', 'Winter Veil Reveler', 0, 1, 1, 5000, 5000, 0, 0, 7, 35, 0, 1.2, 1.14286, 1, 20, 5, 0, 0, 1, 2, 2, 0, 44, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 524288, ''),
+(50644, 6, 15753, 0, 0, 0, 'Marigold Hale', 'Winter Veil Reveler', 0, 1, 1, 5000, 5000, 0, 0, 7, 35, 0, 1.2, 1.14286, 1, 20, 5, 0, 0, 1, 2, 2, 0, 44, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 524288, ''),
+(50645, 6, 15754, 0, 0, 0, 'Chickoa Tallhair', 'Winter Veil Reveler', 0, 1, 1, 5000, 5000, 0, 0, 7, 35, 0, 1.2, 1.14286, 1, 20, 5, 0, 0, 1, 2, 2, 0, 44, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 524288, ''),
+
+(50646, 6, 15744, 0, 0, 0, 'Donavan Hale', 'Winter Veil Reveler', 0, 1, 1, 5000, 5000, 0, 0, 7, 35, 0, 1.2, 1.14286, 1, 20, 5, 0, 0, 1, 2, 2, 0, 44, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 524288, ''),
+(50647, 6, 15746, 0, 0, 0, 'Bomarn Fireaxe', 'Winter Veil Beverages', 0, 1, 1, 5000, 5000, 0, 0, 7, 35, 0, 1.2, 1.14286, 1, 20, 5, 0, 0, 1, 2, 2, 0, 44, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 524288, ''),
+(50648, 6, 15755, 0, 0, 0, 'Florianus Hartford', 'Winter Veil Reveler', 0, 1, 1, 5000, 5000, 0, 0, 7, 35, 0, 1.2, 1.14286, 1, 20, 5, 0, 0, 1, 2, 2, 0, 44, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 524288, ''),
+(50649, 6, 15756, 0, 0, 0, 'Gremm Tuskteeth', 'Winter Veil Reveler', 0, 1, 1, 5000, 5000, 0, 0, 7, 35, 0, 1.2, 1.14286, 1, 20, 5, 0, 0, 1, 2, 2, 0, 44, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 524288, ''),
+(50650, 6, 15757, 0, 0, 0, 'Min\'tu', 'Winter Veil Fireworks', 0, 1, 1, 5000, 5000, 0, 0, 7, 35, 0, 1.2, 1.14286, 1, 20, 5, 0, 0, 1, 2, 2, 0, 44, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 524288, ''),
+(50651, 6, 15759, 0, 0, 0, 'Hototo Fogrunner', 'Winter Veil Reveler', 0, 1, 1, 5000, 5000, 0, 0, 7, 35, 0, 1.2, 1.14286, 1, 20, 5, 0, 0, 1, 2, 2, 0, 44, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 15793, 0, 0, 0, 0, 524288, ''),
+(50652, 6, 15758, 0, 0, 0, 'Carus Silversnow', 'Winter Veil Reveler', 0, 1, 1, 5000, 5000, 0, 0, 7, 35, 0, 1.2, 1.14286, 1, 20, 5, 0, 0, 1, 2, 2, 0, 44, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 15794, 0, 0, 0, 0, 524288, ''),
+(50653, 6, 15750, 0, 0, 0, 'Quark Goldcoil', 'Winter Veil Snow Trader', 0, 1, 1, 5000, 5000, 0, 0, 7, 35, 0, 1.2, 1.14286, 1, 20, 5, 0, 0, 1, 2, 2, 0, 44, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 524288, '');
+
+-- Food Vendor:
+
+update creature_template set vendor_id = 50638 where entry = 50638;
+replace into npc_vendor_template (entry, item, maxcount, incrtime) values
+(50638, 17196, 0, 0),
+(50638, 17194, 0, 0),
+(50638, 21236, 0, 0),
+(50638, 21254, 0, 0),
+(50638, 21240, 0, 0),
+(50638, 21235, 0, 0),
+(50638, 22324, 0, 0),
+(50638, 21721, 0, 0),
+(50638, 17200, 0, 0),
+(50638, 17201, 0, 0);
+
+-- Snow Vendor
+
+update creature_template set vendor_id = 50653 where entry = 50653;
+replace into npc_vendor_template (entry, item, maxcount, incrtime) values
+(50653, 17202, 0, 0);
+update item_template set buy_price = 100 where entry = 17202;
+
+
+-- Booze Vendor
+
+update creature_template set vendor_id = 50647 where entry = 50647;
+replace into npc_vendor_template (entry, item, maxcount, incrtime) values
+(50647, 17402, 0, 0),
+(50647, 2888, 0, 0),
+(50647, 17222, 0, 0),
+(50647, 19222, 0, 0);
+
+-- Firework Vendor
+
+update creature_template set vendor_id = 50643 where entry = 50643;
+
+delete from npc_vendor where entry = 50643;		
+replace into npc_vendor (entry, item) values 
+(50643, 11023),
+(50643, 9312),
+(50643, 9313),
+(50643, 9318),
+(50643, 9314),
+(50643, 21738),
+(50643, 21593),
+(50643, 21595),
+(50643, 21592),
+(50643, 21719),
+(50643, 21720),
+(50643, 21717),
+(50643, 21590),
+(50643, 21718),
+(50643, 21714),
+(50643, 21716),
+(50643, 21589);
+
+-- Sweets Vedor
+
+update creature_template set vendor_id = 50640 where entry = 50640;
+replace into npc_vendor_template (entry, item, maxcount, incrtime) values
+(50640, 21215, 0, 0),
+(50640, 17407, 0, 0),
+(50640, 8950, 0, 0),
+(50640, 21030, 0, 0),
+(50640, 15723, 0, 0),
+(50640, 17405, 0, 0);
+
+update item_template set buy_price = 4000 where entry = 21215;
+update item_template set buy_price = 4000 where entry = 17407;
+
+update item_template set buy_price = 1000 where entry = 21254;
+update item_template set buy_price = 1000 where entry = 21236;
+update item_template set buy_price = 1000 where entry = 21240;
+update item_template set buy_price = 1000 where entry =  21235;
+
+
+-- Party Grenade Vendor
+
+update creature_template set vendor_id = 50650 where entry = 50650;
+replace into npc_vendor_template (entry, item, maxcount, incrtime) values
+(50650, 5859, 0, 0);
+
+-- Flags
+
+update creature_template set npc_flags = 4 where entry in (50640, 50643, 50647, 50653, 50650);
+
+-- Some NPCs dance:
+
+replace into creature_template_addon (entry, emote) values (50644, 10);
+replace into creature_template_addon (entry, emote) values (50648, 10);
+replace into creature_template_addon (entry, emote) values (50645, 10);
+replace into creature_template_addon (entry, emote) values (50651, 10);
+replace into creature_template_addon (entry, emote) values (50639, 10);
+
+
+-- Snowman
+
+REPLACE INTO `creature_template` (`entry`, `patch`, `display_id1`, `display_id2`, `display_id3`, `display_id4`, `name`, `subname`, `gossip_menu_id`, `level_min`, `level_max`, `health_min`, `health_max`, `mana_min`, `mana_max`, `armor`, `faction`, `npc_flags`, `speed_walk`, `speed_run`, `scale`, `detection_range`, `call_for_help_range`, `leash_range`, `rank`, `xp_multiplier`, `dmg_min`, `dmg_max`, `dmg_school`, `attack_power`, `dmg_multiplier`, `base_attack_time`, `ranged_attack_time`, `unit_class`, `unit_flags`, `dynamic_flags`, `beast_family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `ranged_dmg_min`, `ranged_dmg_max`, `ranged_attack_power`, `type`, `type_flags`, `loot_id`, `pickpocket_loot_id`, `skinning_loot_id`, `holy_res`, `fire_res`, `nature_res`, `frost_res`, `shadow_res`, `arcane_res`, `spell_id1`, `spell_id2`, `spell_id3`, `spell_id4`, `spell_list_id`, `pet_spell_list_id`, `gold_min`, `gold_max`, `ai_name`, `movement_type`, `inhabit_type`, `civilian`, `racial_leader`, `regeneration`, `equipment_id`, `trainer_id`, `vendor_id`, `mechanic_immune_mask`, `school_immune_mask`, `flags_extra`, `script_name`) VALUES (50654, 0, 13730, 0, 0, 0, 'Mr. Snowman', 'Winter Veil Ambassador', 0, 35, 35, 2016, 2016, 0, 0, 1357, 35, 2, 1.11, 1.14286, 1.8, 20, 5, 0, 0, 1, 51, 65, 0, 138, 1, 2000, 2000, 1, 768, 0, 0, 0, 0, 0, 0, 51.128, 70.301, 100, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 2, '');
+
+-- Yellow Snowball
+
+REPLACE INTO `item_template` (`entry`, `patch`, `class`, `subclass`, `name`, `description`, `display_id`, `quality`, `flags`, `buy_count`, `buy_price`, `sell_price`, `inventory_type`, `allowable_class`, `allowable_race`, `item_level`, `required_level`, `required_skill`, `required_skill_rank`, `required_spell`, `required_honor_rank`, `required_city_rank`, `required_reputation_faction`, `required_reputation_rank`, `max_count`, `stackable`, `container_slots`, `stat_type1`, `stat_value1`, `stat_type2`, `stat_value2`, `stat_type3`, `stat_value3`, `stat_type4`, `stat_value4`, `stat_type5`, `stat_value5`, `stat_type6`, `stat_value6`, `stat_type7`, `stat_value7`, `stat_type8`, `stat_value8`, `stat_type9`, `stat_value9`, `stat_type10`, `stat_value10`, `delay`, `range_mod`, `ammo_type`, `dmg_min1`, `dmg_max1`, `dmg_type1`, `dmg_min2`, `dmg_max2`, `dmg_type2`, `dmg_min3`, `dmg_max3`, `dmg_type3`, `dmg_min4`, `dmg_max4`, `dmg_type4`, `dmg_min5`, `dmg_max5`, `dmg_type5`, `block`, `armor`, `holy_res`, `fire_res`, `nature_res`, `frost_res`, `shadow_res`, `arcane_res`, `spellid_1`, `spelltrigger_1`, `spellcharges_1`, `spellppmrate_1`, `spellcooldown_1`, `spellcategory_1`, `spellcategorycooldown_1`, `spellid_2`, `spelltrigger_2`, `spellcharges_2`, `spellppmrate_2`, `spellcooldown_2`, `spellcategory_2`, `spellcategorycooldown_2`, `spellid_3`, `spelltrigger_3`, `spellcharges_3`, `spellppmrate_3`, `spellcooldown_3`, `spellcategory_3`, `spellcategorycooldown_3`, `spellid_4`, `spelltrigger_4`, `spellcharges_4`, `spellppmrate_4`, `spellcooldown_4`, `spellcategory_4`, `spellcategorycooldown_4`, `spellid_5`, `spelltrigger_5`, `spellcharges_5`, `spellppmrate_5`, `spellcooldown_5`, `spellcategory_5`, `spellcategorycooldown_5`, `bonding`, `page_text`, `page_language`, `page_material`, `start_quest`, `lock_id`, `material`, `sheath`, `random_property`, `set_id`, `max_durability`, `area_bound`, `map_bound`, `duration`, `bag_family`, `disenchant_id`, `food_type`, `min_money_loot`, `max_money_loot`, `extra_flags`, `other_team_entry`, `script_name`) VALUES (51058, 0, 0, 0, 'Weird Snowball', 'For some reason it\'s yellow.', 29169, 1, 64, 5, 100, 0, 0, 32767, -1, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 21343, 0, -1, 0, 1000, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, NULL);
+
+update item_template set buy_price = 50 where entry = 51058;
+
 
