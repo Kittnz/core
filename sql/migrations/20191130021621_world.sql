21c9b3a3cac68c4c5340c647f2cb5cbd7ea1d48c
Torta <slowtorta@gmail.com>
2019-12-04 01:54:53 +0100

    Snowball quest and the reward.

--- sql/migrations/20191130021621_world.sql
+++ sql/migrations/20191130021621_world.sql
@@ -539,7 +539,7 @@ StartScript, CompleteScript)
  ('50328', '0', '2', '0', '1', '60', '60', '0', '0', '0', '0',
 '0', '0', '0', '0', '0','0',
 '0', '0', '0', '0', '0', '0', '0', '0', '0',
-'0', '0', '0', 'Jolly Holly Dances Prolly', 'Hey there, pretty sweet cheeks! Wouldn\'t you like to have some real fun with jolly Holly right here, my dear? <laughs>\n\nOf course you would, I can see that in your eyes right there. But … you do know that there\'s an ultimate intimate fun is about to follow, don\'t you? Good sweet brews and drinks!\n\nBest way to have some jolly fun with Holly!\n\nNow, drink up and do me … a good long sweety dance, my li\'l marmelade! Haha!', 'Get smashed with Holly and after that /dance with her.', 'Yaaa-ho!', 'Marry Winter Veil!', '', 'Get drunk to boot', 'Dance with Holly Tallhair',
+'0', '0', '0', 'Jolly Holly Dances Prolly', 'Hey there, pretty sweet cheeks! Wouldn\'t you like to have some real fun with jolly Holly right here, my dear? <laughs>\n\nOf course you would, I can see that in your eyes right there. But … you do know that there\'s an ultimate intimate fun is about to follow, don\'t you? Good sweet brews and drinks!\n\nBest way to have some jolly fun with Holly!\n\nNow, drink up and do me … a good long sweety dance, my li\'l marmelade! Haha!', 'Get smashed with Holly and after that /dance with her.', 'Yaaa-ho!', 'Marry Winter Veil!', '', 'Get drunk to boot', 'While drunk /dance with Holly Tallhair',
 '', '', '0', '0', '0', '0', '0', '0', '0',
 '0', '0', '0', '0', '0', '0', '0', '0',
 '0', '0', '0', '0', '0', '0',
@@ -581,3 +581,78 @@ replace into creature_questrelation (id, quest, patch_min, patch_max)
 	 replace into creature_equip_template (entry, equipentry1) values (50649, 1906);
 	 
 	 update creature_template set equipment_id = 50649 where entry = 50649;
+	 
+	 
+	 replace into item_template (
+entry, patch, class, subclass, name,description, display_id, quality, flags, buy_count,
+buy_price, sell_price, inventory_type, allowable_class, allowable_race, item_level,
+required_level, required_skill, required_skill_rank, required_spell, required_honor_rank,
+required_city_rank, required_reputation_faction, required_reputation_rank, max_count, stackable,
+container_slots, stat_type1, stat_value1, stat_type2, stat_value2, stat_type3, stat_value3,
+stat_type4, stat_value4, stat_type5, stat_value5, stat_type6, stat_value6, stat_type7,
+stat_value7, stat_type8, stat_value8, stat_type9, stat_value9, stat_type10, stat_value10,
+delay, range_mod, ammo_type, dmg_min1, dmg_max1, dmg_type1, dmg_min2, dmg_max2, dmg_type2,
+dmg_min3, dmg_max3, dmg_type3, dmg_min4, dmg_max4, dmg_type4, dmg_min5, dmg_max5, dmg_type5, block,
+armor, holy_res, fire_res, nature_res, frost_res, shadow_res, arcane_res, spellid_1, spelltrigger_1,
+spellcharges_1, spellppmrate_1, spellcooldown_1, spellcategory_1, spellcategorycooldown_1, spellid_2,
+spelltrigger_2, spellcharges_2, spellppmrate_2, spellcooldown_2, spellcategory_2, spellcategorycooldown_2,
+spellid_3, spelltrigger_3, spellcharges_3, spellppmrate_3, spellcooldown_3, spellcategory_3,
+spellcategorycooldown_3, spellid_4, spelltrigger_4, spellcharges_4, spellppmrate_4, spellcooldown_4,
+spellcategory_4, spellcategorycooldown_4, spellid_5, spelltrigger_5, spellcharges_5, spellppmrate_5,
+spellcooldown_5, spellcategory_5, spellcategorycooldown_5, bonding, page_text, page_language, page_material,
+start_quest, lock_id, material, sheath, random_property, set_id, max_durability, area_bound, map_bound,
+duration, bag_family, disenchant_id, food_type, min_money_loot, max_money_loot, extra_flags, other_team_entry,
+script_name)
+values
+ ('51254', '0', '1', '0', 'Winter Veil Sack of Presents', '', '34798', '2', '0', '1', '0', '0', '18', '-1', '-1', '60',
+ '0', '0', '0', '0', '0', '0', '0', '0', '1', '1', '18', '0', '0', '0', '0',
+ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '0', '0', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0',
+ '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0',
+ '-1', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '1', NULL);
+ 
+ 
+ replace into quest_template (
+entry, patch, Method, ZoneOrSort, MinLevel, QuestLevel, MaxLevel, Type, RequiredClasses, RequiredRaces, RequiredSkill,
+RequiredSkillValue, RepObjectiveFaction, RepObjectiveValue, RequiredMinRepFaction, RequiredMinRepValue,RequiredMaxRepFaction,
+RequiredMaxRepValue, SuggestedPlayers, LimitTime, QuestFlags, SpecialFlags, PrevQuestId, NextQuestId, ExclusiveGroup, NextQuestInChain,
+SrcItemId, SrcItemCount, SrcSpell, Title, Details, Objectives, OfferRewardText, RequestItemsText, EndText, ObjectiveText1, ObjectiveText2,
+ObjectiveText3, ObjectiveText4, ReqItemId1, ReqItemId2, ReqItemId3, ReqItemId4, ReqItemCount1, ReqItemCount2, ReqItemCount3,
+ReqItemCount4, ReqSourceId1, ReqSourceId2, ReqSourceId3, ReqSourceId4, ReqSourceCount1, ReqSourceCount2, ReqSourceCount3,
+ReqSourceCount4, ReqCreatureOrGOId1, ReqCreatureOrGOId2, ReqCreatureOrGOId3, ReqCreatureOrGOId4, ReqCreatureOrGOCount1,
+ReqCreatureOrGOCount2, ReqCreatureOrGOCount3, ReqCreatureOrGOCount4, ReqSpellCast1, ReqSpellCast2, ReqSpellCast3,
+ReqSpellCast4, RewChoiceItemId1, RewChoiceItemId2, RewChoiceItemId3, RewChoiceItemId4, RewChoiceItemId5, RewChoiceItemId6,
+RewChoiceItemCount1, RewChoiceItemCount2, RewChoiceItemCount3, RewChoiceItemCount4, RewChoiceItemCount5, RewChoiceItemCount6,
+RewItemId1, RewItemId2, RewItemId3, RewItemId4, RewItemCount1, RewItemCount2, RewItemCount3, RewItemCount4, RewRepFaction1,
+RewRepFaction2, RewRepFaction3, RewRepFaction4, RewRepFaction5, RewRepValue1, RewRepValue2, RewRepValue3, RewRepValue4,
+RewRepValue5, RewOrReqMoney, RewMoneyMaxLevel, RewSpell, RewSpellCast, RewMailTemplateId, RewMailMoney, RewMailDelaySecs,
+PointMapId, PointX, PointY, PointOpt, DetailsEmote1, DetailsEmote2, DetailsEmote3, DetailsEmote4, DetailsEmoteDelay1,
+DetailsEmoteDelay2, DetailsEmoteDelay3, DetailsEmoteDelay4, IncompleteEmote, CompleteEmote, OfferRewardEmote1, OfferRewardEmote2,
+OfferRewardEmote3, OfferRewardEmote4, OfferRewardEmoteDelay1, OfferRewardEmoteDelay2, OfferRewardEmoteDelay3,OfferRewardEmoteDelay4,
+StartScript, CompleteScript)
+         values
+ ('50330', '0', '2', '1', '40', '60', '60', '62', '0', '0', '0',
+'0', '0', '0', '0', '0','0',
+'0', '0', '0', '0', '0', '0', '0', '0', '0',
+'0', '0', '0', 'Peace Comes at a Price', 'Greem is a warrior. Me task is protect. Winter Veil Vale is in danger.\n\nBig bad wolf is danger to the village. He attacks every few days. <Greem shows you three fingers.>\n\nYou look like <player class> and look gud. You can help! Find more gud warriors! Attack Snowball while he sleep! He\'s down there.', 'Defeat the Great Direwolf Snowball to the south from Winter Veil Vale.', 'You have done it! Well done! Me respect and thank you!', 'Attack Snowball while he sleep! He\'s down there.', '', 'Defeat Snowball', '',
+'', '', '0', '0', '0', '0', '0', '0', '0',
+'0', '0', '0', '0', '0', '0', '0', '0',
+'0', '50112', '0', '0', '0', '1',
+'0', '0', '0', '0', '0', '0',
+'0', '0', '0', '0', '0', '0', '0',
+'0', '0', '0', '0', '0', '0',
+'51254', '0', '0', '0', '1', '0', '0', '0', '0',
+'0', '0', '0', '0', '0', '0', '0', '0',
+'0', '0', '10000', '0', '0', '0', '0', '0',
+'0', '0', '0', '0', '1', '0', '0', '0', '0',
+'0', '0', '0', '0', '4', '0', '0',
+'0', '0', '0', '0', '0','0',
+0, 0);
+replace into creature_questrelation (id, quest, patch_min, patch_max)
+      values
+      ('50649','50330','10','10');
+	  replace into creature_involvedrelation (id, quest, patch_min, patch_max)
+      values
+      ('50649','50330','10','10');
