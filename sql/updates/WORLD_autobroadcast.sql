1424df71270f118a82833f5f97c8ae911a7a8f78
Torta <slowtorta@gmail.com>
2019-12-07 07:59:33 +0100

    Update WORLD_autobroadcast.sql

--- sql/updates/WORLD_autobroadcast.sql
+++ sql/updates/WORLD_autobroadcast.sql
@@ -14,10 +14,9 @@ replace into `mangos_string` (`entry`, `content_default`) values
 ('50011', 'Grab your dusty sword and join your faction\'s defending forces! Get your brand new equipment sets from the Guard Recruiters in the main cities!'),
 ('50012', 'Check our shop for donation rewards if you are interested to contribute to this community: [turtle-wow.org/donate]. Thank you all for supporting Turtle WoW since launch. We couldn’t be here without your help.'),
 ('50013', 'Speak to Karn Deepeye in Orgrimmar or Tarun Swifteagle in Stormwind to become a diplomat! You will be able to group and trade with the players of an opposite faction.'),
-('50014', 'Please support Turtle WoW by using our official launcher. It\'s one way to help us to advertise our project! Learn more: turtle-wow.org/launcher'),
-('50015', 'We\'ve implemented a list of features that Blizzard never published, you can find information about them on our forums under [Sands of Time] section.'),
-('50016', 'If you really like questing and leveling, play in hardcore mode, your creature kill rates will be set to 0.5 raters. Enjoy your journey and receive rewards every 10 levels!'),
-('50017', 'We\'re constantly working on improving the leveling experience on Turtle WoW: creating new quests for starting locations or extending the existing quest chains. If you have an idea for a new quest, please visit this page: [dev.turtle-wow.org].');
+('50014', 'We\'ve implemented a list of features that Blizzard never published, you can find information about them on our forums under [Sands of Time] section.'),
+('50015', 'If you really like questing and leveling, play in hardcore mode, your creature kill rates will be set to 0.5 raters. Enjoy your journey and receive rewards every 10 levels!'),
+('50016', 'We\'re constantly working on improving the leveling experience on Turtle WoW: creating new quests for starting locations or extending the existing quest chains. If you have an idea for a new quest, please visit this page: [dev.turtle-wow.org].');
 replace into `autobroadcast` (`string_Id`) values
 ('50000'),
 ('50001'),
@@ -35,5 +34,4 @@ replace into `autobroadcast` (`string_Id`) values
 ('50013'),
 ('50014'),
 ('50015'),
-('50016'),
-('50017');
+('50016');
