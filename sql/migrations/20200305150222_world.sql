6c4b0a656cea10030ae055c8be56a4a4827a6e29
Torta <slowtorta@gmail.com>
2020-03-06 00:07:02 +0300

    Fitting modelsIDs.

--- sql/migrations/20200305150222_world.sql
+++ sql/migrations/20200305150222_world.sql
@@ -8,7 +8,7 @@ update item_template set description = 'Fashion community currency.' where entry
 update item_template set name = 'Fashion Coin' where entry = 51217;
 update item_template set quality = 3 where entry = 51217;
 
-replace into creature_template values (51290, 0, 10573, 0, 0, 0, 'Felicia Starweaver', 'Stormwind Fashionista', 0, 35, 35, 2016, 2016, 0, 0, 1357, 35, 1, 1.11, 1.14286, 1, 20, 5, 0, 0, 1, 51, 65, 0, 138, 1, 2000, 2000, 1, 768, 0, 0, 0, 0, 0, 0, 51.128, 70.301, 100, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 2, 'npc_transmog');
+replace into creature_template values (51290, 0, 1442, 0, 0, 0, 'Felicia', 'Stormwind Fashionista', 0, 35, 35, 2016, 2016, 0, 0, 1357, 35, 1, 1.11, 1.14286, 1, 20, 5, 0, 0, 1, 51, 65, 0, 138, 1, 2000, 2000, 1, 768, 0, 0, 0, 0, 0, 0, 51.128, 70.301, 100, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 2, 'npc_transmog');
 
 update creature_template set npc_flags='1' where entry='51290';	
 update creature_template set faction='12' where entry='51290';
