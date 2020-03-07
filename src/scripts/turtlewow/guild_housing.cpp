740510f1ff79b80dfbfd580a228fb853a7ee5eb9
Torta <slowtorta@gmail.com>
2020-03-07 16:53:32 +0300

    Delete outdated RP scripts.

--- src/scripts/turtlewow/guild_housing.cpp
+++ src/scripts/turtlewow/guild_housing.cpp
@@ -1,12 +1,9 @@
 
 #include "scriptPCH.h"
 
-// <The Greysteel Company> NPCs
-
 bool GossipHello_npc_dirge_the_bouncer(Player* p_Player, Creature* p_Creature)
 {
     p_Player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "What is this place?", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
-    p_Player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Who are the Greysteels?", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
     p_Player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Why do they call you Dirge?", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3);
     p_Player->SEND_GOSSIP_MENU(90200, p_Creature->GetGUID());
     return true;
@@ -16,15 +13,11 @@ bool GossipSelect_npc_dirge_the_bouncer(Player* p_Player, Creature* p_Creature, 
 {
     if (uiAction == GOSSIP_ACTION_INFO_DEF + 1)
         p_Creature->MonsterSay("This be tha Grey Raven lad. Ye better have coin ta pay for yer drinks. There's no handouts here.", 7, 0);
-    if (uiAction == GOSSIP_ACTION_INFO_DEF + 2)
-        p_Creature->MonsterSay("Da Greysteel Company. Mercen... I mean, Adventurer Company. Check in with the guys inside if ye want work.", 7, 0);
     if (uiAction == GOSSIP_ACTION_INFO_DEF + 3)
         p_Creature->MonsterSay("They call me Dirge, 'cause I'm tha last thing ye hear before they put ye in tha ground.", 7, 0);
     p_Player->CLOSE_GOSSIP_MENU();
     return true;
 }
-
-// <Ghosts of Lordaeron> NPCs
 
 bool GossipHello_npc_lordaeron_alice(Player* p_Player, Creature* p_Creature)
 {
@@ -56,16 +49,6 @@ bool GossipHello_npc_lordaeron_ike(Player* p_Player, Creature* p_Creature)
     return true;
 }
 
-// Raven Trade Co. NPCs:
-
-bool GossipHello_npc_raventrade_guard(Player* p_Player, Creature* p_Creature)
-{
-
-    p_Player->PrepareGossipMenu(p_Creature, p_Creature->GetCreatureInfo()->gossip_menu_id);
-    p_Player->SEND_GOSSIP_MENU(90203, p_Creature->GetGUID());
-    return true;
-}
-
 
 void AddSC_guild_housing()
 {
@@ -88,9 +71,6 @@ void AddSC_guild_housing()
     newscript->pGossipHello = &GossipHello_npc_lordaeron_ike;
     newscript->RegisterSelf();
     
-    newscript = new Script;
-    newscript->Name = "npc_raventrade_guard";
-    newscript->pGossipHello = &GossipHello_npc_raventrade_guard;
-    newscript->RegisterSelf();
+
 
 }
