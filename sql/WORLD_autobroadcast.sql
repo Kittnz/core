791b72e611b7b1db2c1a58f3214cab2ec4118650
Torta <slowtorta@gmail.com>
2020-03-16 14:25:07 +0100

    Update WORLD_autobroadcast.sql

--- sql/WORLD_autobroadcast.sql
+++ sql/WORLD_autobroadcast.sql
@@ -1,22 +1,21 @@
 
-replace into `mangos_string` (`entry`, `content_default`) values 
-('50000', 'Delete your WDB folder regularly. This is your game cache, and can often fix minor game/display glitches. Always try this before submitting a ticket, thanks!'),
-('50001', 'Have a suggestion for our staff? Head over to our forums and submit your ideas, get support, or post something for the community to get involved in!'),
+replace into mangos_string (entry, content_default) values 
+('50000', 'Delete your WDB folder regularly. This is your game cache, and can often fix minor game glitches. Always try this before submitting a ticket, thanks!'),
 ('50002', 'Visit Stranglethorn Vale and speak to S. J. Erlgadin of Nesingwary\'s Expedition to get your intensive training in Survival.'),
-('50003', 'Want to find players to start a RP adventure? Looking for a fun guild to join? Keep up to date with the Turtle WoW community and join our Discord channel: [discord.turtle-wow.org]'),
-('50004', 'The world is united! Type /join World now to connect with the community around you, and find friends for groups, dungeons, and trades!  Please /join Horde or /join Alliance for faction-only talks.'),
+('50003', 'Keep up to date with the Turtle WoW community and join our Discord channel: |cff0ccaecdiscord.turtle-wow.org|r'),
+('50004', 'Type |cff0ccaec/join world|r now to connect with the community around you, and find friends for groups, dungeons, and trades! Please |cff0ccaec/join horde|r or |cff0ccaec/join alliance|r for faction-only talks.'),
 ('50005', 'Please remember, a GM will never ask for your password, so please do not share your password with anyone. Use a secure password, and change it often.'),
-('50006', 'Donations are a way to help us improve your experience here at Turtle WoW, as well as help pay for general server costs, advertisement campaigns and overall production. You can find changelogs and videos on our website and social media platforms. Thanks for your continued support!'),
-('50007', 'Speak to Fara Boltbreaker in Stormwind or Orgrimmar for a fast transfer to Shimmering Flats and participate to the real Player Controlled Mirage Raceway!'),
-('50009', 'Remember that the level 1—9 PvP bracket is open for Warsong Gulch in Turtle WoW.'),
-('50010', 'Do your new fancy shoulderpads look awful with your hair? Would you rather not cover your amazing face with an ugly helmet? Don\'t worry! Find your local barber in Stormwind or Crossroads and buy some hair dyes.'),
-('50011', 'Grab your dusty sword and join your faction\'s defending forces! Get your brand new equipment sets from the Guard Recruiters in the main cities!'),
-('50012', 'Check our shop for donation rewards if you are interested to contribute to this community: [turtle-wow.org/donate]. Thank you all for supporting Turtle WoW since launch. We couldn’t be here without your help.'),
+('50006', 'Donations are a way to help us improve your experience here at Turtle WoW, as well as help pay for general server costs. Check our shop for donation rewards if you are interested to contribute to this community: |cff0ccaecturtle-wow.org/donate|r'),
+('50007', 'Speak to Fara Boltbreaker in Stormwind or Orgrimmar for a fast transfer to Shimmering Flats and participate to the real Player Controlled Mirage Raceway.'),
+('50008', 'Few of us know where and how to preserve style and fashion while protecting your kingdoms and helping your allies. We are eagerly invite you and your friends to come and visit our fashionistas and barbers in all major cities!'),
+('50009', 'Remember that the level 1, 2—9 PvP brackets are open for Warsong Gulch in Turtle WoW.'),
 ('50013', 'Speak to Karn Deepeye in Orgrimmar or Tarun Swifteagle in Stormwind to become a diplomat! You will be able to group and trade with the players of an opposite faction.'),
-('50014', 'We\'ve implemented a list of features that Blizzard never published, you can find information about them on our forums under [Sands of Time] section.'),
+('50014', 'We\'ve implemented a list of features that Blizzard never published, you can find information about them on our forums under |cff0ccaecSands of Time|r section.'),
 ('50015', 'If you really like questing and leveling, play in hardcore mode, your creature kill rates will be set to 0.5 raters. Enjoy your journey and receive rewards every 10 levels!'),
-('50016', 'We\'re constantly working on improving the leveling experience on Turtle WoW: creating new quests for starting locations or extending the existing quest chains. If you have an idea for a new quest, please visit this page: [dev.turtle-wow.org].');
-replace into `autobroadcast` (`string_Id`) values
+('50016', 'We\'re constantly working on improving the leveling experience on Turtle WoW: creating new quests for starting locations or extending the existing quest chains. If you have an idea for a new quest, please visit this page: |cff0ccaecdev.turtle-wow.org|r');
+
+delete from autobroadcast where string_id between 50000 and 50020;
+replace into autobroadcast (string_Id) values
 ('50000'),
 ('50001'),
 ('50002'),
