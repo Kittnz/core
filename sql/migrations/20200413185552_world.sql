5407b6941c3b8c30f3ec624e352aa63305234b62
Torta <slowtorta@gmail.com>
2020-04-14 14:57:31 +0200

    Fix relations for undead priest quests.

--- sql/migrations/20200413185552_world.sql
+++ sql/migrations/20200413185552_world.sql
@@ -1933,6 +1933,21 @@ INSERT INTO `creature_questrelation` (`id`, `quest`, `patch_min`, `patch_max`) V
 INSERT INTO `creature_questrelation` (`id`, `quest`, `patch_min`, `patch_max`) VALUES (6018, 8254, 5, 10);
 INSERT INTO `creature_questrelation` (`id`, `quest`, `patch_min`, `patch_max`) VALUES (11406, 8254, 5, 10);
 
+-- Quest 5663 should be started by Miles Welsh.
+UPDATE `creature_questrelation` SET `id`=3044 WHERE `id`=2129 && `quest`=5663;
+
+-- Quest 5661 should not by started by anyone. It's unobtainable.
+DELETE FROM `creature_questrelation` WHERE `id`=3044 && `quest`=5661;
+
+-- <NYI> Touch of Weakness should not be a prerequisite of 5658.
+UPDATE `quest_template` SET `NextQuestInChain`=0, `RewMoneyMaxLevel`=120, `RewSpell`=19318 WHERE `entry`=5659;
+
+-- Quest 5644 should be started by Miles Welsh.
+UPDATE `creature_questrelation` SET `id`=3044 WHERE `id`=5994 && `quest`=5644;
+
+-- Quest 5646 should be started by Ur'kyo.
+UPDATE `creature_questrelation` SET `id`=6018 WHERE `id`=3046 && `quest`=5646;
+
 
 -- End of migration.
 END IF;
