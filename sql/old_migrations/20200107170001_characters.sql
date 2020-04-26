038e1ad429c9a3514bd7b697d2959ae3228f51e8
Torta <slowtorta@gmail.com>
2020-04-26 17:46:20 +0200

    Removed migrations.

--- sql/old_migrations/20200107170001_characters.sql
+++ sql/old_migrations/20200107170001_characters.sql
@@ -7,6 +7,14 @@ SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20200107170001');
 IF v=0 THEN
 INSERT INTO `migrations` VALUES ('20200107170001');
 
+DROP TABLE IF EXISTS `bounty_quest_targets`;
+CREATE TABLE `bounty_quest_targets` (
+  `id` int(5) unsigned NOT NULL DEFAULT '1',
+  `horde_player` int(15) unsigned NOT NULL DEFAULT '0',
+  `alliance_player` int(15) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 DROP TABLE IF EXISTS `character_transmog`;
 CREATE TABLE `character_transmog` (
   `itemguid` int(18) unsigned NOT NULL,
@@ -16,7 +24,6 @@ CREATE TABLE `character_transmog` (
   `newentry` int(11) NOT NULL,
   PRIMARY KEY (`itemguid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 
 END IF;
 END??
