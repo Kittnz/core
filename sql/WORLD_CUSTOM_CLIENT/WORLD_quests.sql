7826c27f1e3c399c1d7866d04f10e8a2fb3768c7
Torta <slowtorta@gmail.com>
2020-05-02 00:01:23 +0200

    Added more variables for quest template query.

--- sql/WORLD_CUSTOM_CLIENT/WORLD_quests.sql
+++ sql/WORLD_CUSTOM_CLIENT/WORLD_quests.sql
@@ -1,11 +1,44 @@
+
+-- To Alah\'thalas!
+
+SET @quest_entry = 80000;
+SET @quest_zone = 1519;
 SET @faction_id = 269;
 SET @faction_count = 250;
 SET @xp_or_money = 650;
 SET @quest_level = 60;
 SET @min_level = 10;
+SET @questgiver_id = 80000;
+SET @quest_finisher = 81752;
+SET @nextquest = 80001;
+SET @nextquestinchain = 80001;
+SET @prevquest = 0;
+SET @reward_item_1 = 0;
+SET @reward_item_2 = 0;
+SET @reward_item_3 = 0;
+SET @reward_item_4 = 0;
+SET @reward_item_1_count = 0;
+SET @reward_item_2_count = 0;
+SET @reward_item_3_count = 0;
+SET @reward_item_4_count = 0;
+SET @creature_to_kill_1 = 0;
+SET @creature_to_kill_2 = 0;
+SET @creature_to_kill_3 = 0;
+SET @creature_to_kill_4 = 0;
+SET @creature_to_kill_1_count = 0;
+SET @creature_to_kill_2_count = 0;
+SET @creature_to_kill_3_count = 0;
+SET @creature_to_kill_4_count = 0;
+SET @required_item_1 = 0;
+SET @required_item_2 = 0;
+SET @required_item_3 = 0;
+SET @required_item_4 = 0;
+SET @required_item_1_count = 0;
+SET @required_item_2_count = 0;
+SET @required_item_3_count = 0;
+SET @required_item_4_count = 0;
 
-replace into quest_template (
-entry, patch, Method, ZoneOrSort, MinLevel, QuestLevel, MaxLevel, Type, RequiredClasses, RequiredRaces, RequiredSkill,
+replace into quest_template ( entry, patch, Method, ZoneOrSort, MinLevel, QuestLevel, MaxLevel, Type, RequiredClasses, RequiredRaces, RequiredSkill,
 RequiredSkillValue, RepObjectiveFaction, RepObjectiveValue, RequiredMinRepFaction, RequiredMinRepValue,RequiredMaxRepFaction,
 RequiredMaxRepValue, SuggestedPlayers, LimitTime, QuestFlags, SpecialFlags, PrevQuestId, NextQuestId, ExclusiveGroup, NextQuestInChain,
 SrcItemId, SrcItemCount, SrcSpell, Title, Details, Objectives, OfferRewardText, RequestItemsText, EndText, ObjectiveText1, ObjectiveText2,
@@ -22,23 +55,40 @@ PointMapId, PointX, PointY, PointOpt, DetailsEmote1, DetailsEmote2, DetailsEmote
 DetailsEmoteDelay2, DetailsEmoteDelay3, DetailsEmoteDelay4, IncompleteEmote, CompleteEmote, OfferRewardEmote1, OfferRewardEmote2,
 OfferRewardEmote3, OfferRewardEmote4, OfferRewardEmoteDelay1, OfferRewardEmoteDelay2, OfferRewardEmoteDelay3,OfferRewardEmoteDelay4,
 StartScript, CompleteScript) values
-('80000', '0', '2', '1519', @min_level, @quest_level, '0', '0', '0', '69', '0',
-'0', '0', '0', '0', '0','0',
-'0', '0', '0', '0', '0', '0', '0', '0', '0',
-'0', '0', '0', 'To Alah\'thalas!', 'Bal\'a dash, malanore $R! I represent a group of my people who seek to restore our people\'s glory.$B$BWe seek to settle a new home for our people and your assistance would be quite welcome.$B$BWhen you have the time, journey to Stormwind and speak to Caledra Dawnbreeze in the Stormwind Keep.$B$BTrust that you will be well compensated for your aid!', 'Speak to Caledra Dawnbreeze in Stormwind Keep.', 'Ah so you seek to assist us in our efforts?$B$BWonderful...Our people have setup a new settlement called Alah\'thalas on the coast north of Stratholme, bordering Quel\'thalas.$B$BHowever, setting up a new home will be difficult and we have run into our share of issues. Your help will definitely be appreciated!', 'Yes? What can I do for you stranger?', '', '', '',
-'', '', '0', '0', '0', '0', '0', '0', '0',
-'0', '0', '0', '0', '0', '0', '0', '0',
-'0', '0', '0', '0', '0', '0',
-'0', '0', '0', '0', '0', '0',
-'0', '0', '0', '0', '0', '0', '0',
-'0', '0', '0', '0', '0', '0',
-'0', '0', '0', '0', '0', '0', '0', '0', @faction_id,
-'0', '0', '0', '0', @faction_count, '0', '0', '0',
-'0', '0', @xp_or_money, '0', '0', '0', '0', '0',
-'0', '0', '0', '0', '0', '0', '0', '0', '0',
-'0', '0', '0', '0', '0', '0', '0',
-'0', '0', '0', '0', '0','0', 0, 0);
+
+(@quest_entry, '0', '2', @quest_zone, @min_level, @quest_level, '0', '0', '0', '69', '0', '0', '0', '0', '0', '0','0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 
+
+'To Alah\'thalas!', 
+
+'Bal\'a dash, malanore $R! I represent a group of my people who seek to restore our people\'s glory.$B$BWe seek to settle a new home for our people and your assistance would be quite welcome.$B$BWhen you have the time, journey to Stormwind and speak to Caledra Dawnbreeze in the Stormwind Keep.$B$BTrust that you will be well compensated for your aid!', 
+
+'Speak to Caledra Dawnbreeze in Stormwind Keep.', 
+
+'Ah so you seek to assist us in our efforts?$B$BWonderful...Our people have setup a new settlement called Alah\'thalas on the coast north of Stratholme, bordering Quel\'thalas.$B$BHowever, setting up a new home will be difficult and we have run into our share of issues. Your help will definitely be appreciated!', 
+
+'Yes? What can I do for you stranger?', 
+
+'', '', '', '', '', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', @faction_id, '0', '0', '0', '0', @faction_count, '0', '0', '0', '0', '0', @xp_or_money, '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0','0', 0, 0);
       
-replace into creature_questrelation (id, quest, patch_min, patch_max) values ('80000','80000','10','10'); -- NPC 80000 doesn't exist yet. 15004 DID High Elf Ranger
-replace into creature_involvedrelation (id, quest, patch_min, patch_max) values ('1752','80000','10','10');
+update quest_template set 
+rewitemid1 = @reward_item_1, rewitemcount1 = @reward_item_1_count,
+rewitemid2 = @reward_item_2, rewitemcount2 = @reward_item_2_count,
+rewitemid3 = @reward_item_3, rewitemcount3 = @reward_item_3_count,
+rewitemid4 = @reward_item_4, rewitemcount4 = @reward_item_4_count,
+ReqCreatureOrGOId2 = @creature_to_kill_2, ReqCreatureOrGOCount1 = @creature_to_kill_1_count,
+ReqCreatureOrGOId2 = @creature_to_kill_2, ReqCreatureOrGOCount2 = @creature_to_kill_2_count,
+ReqCreatureOrGOId3 = @creature_to_kill_3, ReqCreatureOrGOCount3 = @creature_to_kill_3_count,
+ReqCreatureOrGOId4 = @creature_to_kill_4, ReqCreatureOrGOCount4 = @creature_to_kill_4_count,
+reqitemid1 = @required_item_1, rewitemcount1 = @required_item_1_count,
+reqitemid2 = @required_item_2, rewitemcount2 = @required_item_2_count,
+reqitemid3 = @required_item_3, rewitemcount3 = @required_item_3_count,
+reqitemid4 = @required_item_4, rewitemcount4 = @required_item_4_count
+where entry = @quest_entry;
+
+update quest_template set nextquestid = @nextquest where entry = @quest_entry;
+update quest_template set nextquestinchain = @nextquestinchain where entry = @quest_entry;
+update quest_template set prevquestid = @nextquest where entry = @quest_entry;
+	  
+replace into creature_questrelation (id, quest, patch_min, patch_max) values (@questgiver_id, @quest_entry,'10','10'); -- NPC 80000 doesn't exist yet. 15004 DID High Elf Ranger
+replace into creature_involvedrelation (id, quest, patch_min, patch_max) values (@quest_finisher, @quest_entry,'10','10');
       
