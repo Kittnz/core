8512df277a676d5b19c744079ed367fefc473875
Giperion <dereline@hotmail.com>
2020-05-03 23:28:00 +0300

    Implement weather copying.

--- src/game/Weather.h
+++ src/game/Weather.h
@@ -94,6 +94,7 @@ class Weather
         void LogWeatherState(WeatherState state) const;
 
         uint32 m_zone;
+		uint32 m_CopyFromAnotherZone = 0;
         WeatherType m_type;
         float m_grade;
         ShortIntervalTimer m_timer;
@@ -136,6 +137,7 @@ struct WeatherSeasonChances
 
 struct WeatherZoneChances
 {
+	uint32 CopyWeatherFromAnotherZone;
     WeatherSeasonChances data[WEATHER_SEASONS];
 };
 
