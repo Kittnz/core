411f8dcdff757562d5e1c6b109ac592bf1ddb721
Torta <slowtorta@gmail.com>
2020-05-05 01:07:28 +0200

    insert into > replace into.

--- sql/_UPD_2020_WORLD_may.sql
+++ sql/_UPD_2020_WORLD_may.sql
@@ -80,20 +80,21 @@ UPDATE `creature_template` SET `speed_run`=2.14286 WHERE `entry`=14517;
 UPDATE `creature_template` SET `speed_run`=2.14286 WHERE `entry`=14750;
 UPDATE `creature_template` SET `speed_run`=2.14286 WHERE `entry`=14834;
 
--- Add missing quest greetings from classic sniffs.
-INSERT INTO `quest_greeting` (`entry`, `type`, `content_default`, `Emote`, `EmoteDelay`) VALUES (1952, 0, 'Hello, $C.  If you\'re here, then you must know that Silverpine is saturated with our enemies.  To survive, the Forsaken must drive them back!', 0, 0);
-INSERT INTO `quest_greeting` (`entry`, `type`, `content_default`, `Emote`, `EmoteDelay`) VALUES (10321, 0, '<Emberstrife acknowledges your presence.>', 0, 0);
-INSERT INTO `quest_greeting` (`entry`, `type`, `content_default`, `Emote`, `EmoteDelay`) VALUES (11596, 0, 'My business plan never accounted for housing giant kodos... maybe I should have gone into the underwater basket weaving business instead.', 1, 0);
--- Add missing quest greetings from cmangos db.
-INSERT INTO `quest_greeting` (`entry`, `type`, `content_default`, `Emote`, `EmoteDelay`) VALUES (7777, 0, 'The Gordunni Ogres encroach upon our lands.  We send as many as are willing to face them in the desecrated ruins of Feralas.', 1, 0);
-INSERT INTO `quest_greeting` (`entry`, `type`, `content_default`, `Emote`, `EmoteDelay`) VALUES (16361, 0, 'The Lich King has brought war against us from the frozen north, and only we of the Argent Dawn stand in his way.', 1, 0);
-INSERT INTO `quest_greeting` (`entry`, `type`, `content_default`, `Emote`, `EmoteDelay`) VALUES (16478, 0, 'Good day to you, citizen. Have you come to aid us against the Scourge?', 1, 0);
-INSERT INTO `quest_greeting` (`entry`, `type`, `content_default`, `Emote`, `EmoteDelay`) VALUES (16786, 0, 'Greetings, $n. If you bring me necrotic stones from the undead invaders, I can give you access to the stores of the Argent Dawn.', 66, 0);
-INSERT INTO `quest_greeting` (`entry`, `type`, `content_default`, `Emote`, `EmoteDelay`) VALUES (16788, 0, 'Greetings, $c. I am the Flamekeeper. During the Midsummer Fire Festival, it is my duty to keep this fire beside me burning brightly. It is an honor to be selected for such a task; I fill it gladly.$b$b How can I help you?', 1, 0);
 -- Change naming style emote fields.
-ALTER TABLE `quest_greeting`
+/* ALTER TABLE `quest_greeting`
     CHANGE COLUMN `Emote` `emote_id` SMALLINT(5) UNSIGNED NOT NULL DEFAULT '0' AFTER `content_loc8`,
-    CHANGE COLUMN `EmoteDelay` `emote_delay` INT(10) UNSIGNED NOT NULL DEFAULT '0' AFTER `emote_id`;
+    CHANGE COLUMN `EmoteDelay` `emote_delay` INT(10) UNSIGNED NOT NULL DEFAULT '0' AFTER `emote_id`; */
+
+-- Add missing quest greetings from classic sniffs.
+replace into `quest_greeting` (`entry`, `type`, `content_default`, `Emote_id`, `emote_delay`) VALUES (1952, 0, 'Hello, $C.  If you\'re here, then you must know that Silverpine is saturated with our enemies.  To survive, the Forsaken must drive them back!', 0, 0);
+replace into `quest_greeting` (`entry`, `type`, `content_default`, `Emote_id`, `emote_delay`) VALUES (10321, 0, '<Emberstrife acknowledges your presence.>', 0, 0);
+replace into `quest_greeting` (`entry`, `type`, `content_default`, `Emote_id`, `emote_delay`) VALUES (11596, 0, 'My business plan never accounted for housing giant kodos... maybe I should have gone into the underwater basket weaving business instead.', 1, 0);
+-- Add missing quest greetings from cmangos db.
+replace into `quest_greeting` (`entry`, `type`, `content_default`, `Emote_id`, `emote_delay`) VALUES (7777, 0, 'The Gordunni Ogres encroach upon our lands.  We send as many as are willing to face them in the desecrated ruins of Feralas.', 1, 0);
+replace into `quest_greeting` (`entry`, `type`, `content_default`, `Emote_id`, `emote_delay`) VALUES (16361, 0, 'The Lich King has brought war against us from the frozen north, and only we of the Argent Dawn stand in his way.', 1, 0);
+replace into `quest_greeting` (`entry`, `type`, `content_default`, `Emote_id`, `emote_delay`) VALUES (16478, 0, 'Good day to you, citizen. Have you come to aid us against the Scourge?', 1, 0);
+replace into `quest_greeting` (`entry`, `type`, `content_default`, `Emote_id`, `emote_delay`) VALUES (16786, 0, 'Greetings, $n. If you bring me necrotic stones from the undead invaders, I can give you access to the stores of the Argent Dawn.', 66, 0);
+replace into `quest_greeting` (`entry`, `type`, `content_default`, `Emote_id`, `emote_delay`) VALUES (16788, 0, 'Greetings, $c. I am the Flamekeeper. During the Midsummer Fire Festival, it is my duty to keep this fire beside me burning brightly. It is an honor to be selected for such a task; I fill it gladly.$b$b How can I help you?', 1, 0);
 
 -- Correct bounding radius for some creatures.
 UPDATE `creature_model_info` SET `bounding_radius`=0.5 WHERE `modelid`=4449;
