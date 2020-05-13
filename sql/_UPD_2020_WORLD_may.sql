1c0424b8d7efc9a1842bbf74f33aacf8d5b90541
Torta <slowtorta@gmail.com>
2020-05-13 16:52:07 +0200

    Nerubian loot: cloaks.

--- sql/_UPD_2020_WORLD_may.sql
+++ sql/_UPD_2020_WORLD_may.sql
@@ -1046,6 +1046,74 @@ UPDATE `item_template` SET `spellid_1`='0' WHERE (`entry`='50521') AND (`patch`=
 UPDATE `item_template` SET `spellcooldown_1`='0' WHERE (`entry`='50521') AND (`patch`='0');
 UPDATE `item_template` SET `spellcharges_1`='0' WHERE (`entry`='50521') AND (`patch`='0');
 
+-- Nerubian Loot
+
+replace into item_template (
+entry, patch, class, subclass, name,description, display_id, quality, flags, buy_count,
+buy_price, sell_price, inventory_type, allowable_class, allowable_race, item_level,
+required_level, required_skill, required_skill_rank, required_spell, required_honor_rank,
+required_city_rank, required_reputation_faction, required_reputation_rank, max_count, stackable,
+container_slots, stat_type1, stat_value1, stat_type2, stat_value2, stat_type3, stat_value3,
+stat_type4, stat_value4, stat_type5, stat_value5, stat_type6, stat_value6, stat_type7,
+stat_value7, stat_type8, stat_value8, stat_type9, stat_value9, stat_type10, stat_value10,
+delay, range_mod, ammo_type, dmg_min1, dmg_max1, dmg_type1, dmg_min2, dmg_max2, dmg_type2,
+dmg_min3, dmg_max3, dmg_type3, dmg_min4, dmg_max4, dmg_type4, dmg_min5, dmg_max5, dmg_type5, block,
+armor, holy_res, fire_res, nature_res, frost_res, shadow_res, arcane_res, spellid_1, spelltrigger_1,
+spellcharges_1, spellppmrate_1, spellcooldown_1, spellcategory_1, spellcategorycooldown_1, spellid_2,
+spelltrigger_2, spellcharges_2, spellppmrate_2, spellcooldown_2, spellcategory_2, spellcategorycooldown_2,
+spellid_3, spelltrigger_3, spellcharges_3, spellppmrate_3, spellcooldown_3, spellcategory_3,
+spellcategorycooldown_3, spellid_4, spelltrigger_4, spellcharges_4, spellppmrate_4, spellcooldown_4,
+spellcategory_4, spellcategorycooldown_4, spellid_5, spelltrigger_5, spellcharges_5, spellppmrate_5,
+spellcooldown_5, spellcategory_5, spellcategorycooldown_5, bonding, page_text, page_language, page_material,
+start_quest, lock_id, material, sheath, random_property, set_id, max_durability, area_bound, map_bound,
+duration, bag_family, disenchant_id, food_type, min_money_loot, max_money_loot, extra_flags, other_team_entry,
+script_name) values
+
+ ('51730', '0', '4', '1', 'Shawl of Nerubian Silk', '', '23042', '4', '0', '1', '54000', '54000', '16', '-1', '-1', '75',
+ '60', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '5', '12', '7', '11',
+ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '65', '0', '0', '0',
+ '0', '15', '0', '14127', '1', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0',
+ '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0',
+ '-1', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '1', NULL),
+ 
+ ('51731', '0', '4', '1', 'Shawl of Haunted Memories', '', '30831', '4', '0', '1', '64000', '64000', '16', '-1', '-1', '75',
+ '60', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '7', '13', '0', '0',
+ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '386', '0', '0', '0',
+ '0', '15', '0', '13671', '1', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0',
+ '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0',
+ '-1', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '1', NULL),
+ 
+ ('51732', '0', '4', '1', 'Silken Mantle of Dying Hope', '', '31351', '4', '0', '1', '64000', '64000', '16', '-1', '-1', '75',
+ '60', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '5', '10', '6', '10',
+ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '165', '0', '0', '0',
+ '0', '10', '0', '9317', '1', '0', '0', '-1', '0', '-1', '21626', '1', '0', '0', '-1', '0', '-1', '0', '0',
+ '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0',
+ '-1', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '1', NULL),
+ 
+ ('51733', '0', '4', '1', 'Shadow-Weaver\'s Cape', '', '34239', '4', '0', '1', '64000', '64000', '16', '-1', '-1', '75',
+ '60', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '7', '12', '3', '10',
+ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '53', '0', '0', '0',
+ '0', '10', '0', '14056', '1', '0', '0', '-1', '0', '-1', '0', '1', '0', '0', '-1', '0', '-1', '0', '0',
+ '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0',
+ '-1', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '1', NULL),
+
+ ('51734', '0', '4', '1', 'Shadowmoon Cape', '', '24013', '4', '0', '1', '64000', '64000', '16', '-1', '-1', '75',
+ '60', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '3', '10', '7', '12',
+ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '53', '0', '0', '0',
+ '0', '0', '0', '12438', '0', '0', '0', '-1', '0', '-1', '14049', '1', '0', '0', '-1', '0', '-1', '0', '0',
+ '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0',
+ '-1', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '1', NULL);
+ 
 -- Add missing transmog templates
 
 REPLACE INTO transmog_template VALUES (3173, 800, 7);
@@ -1155,3 +1223,4 @@ REPLACE INTO transmog_template VALUES  (34781, 910,1);
 REPLACE INTO transmog_template VALUES  (24314, 911,4);
 REPLACE INTO transmog_template VALUES  (16829, 914,20);
 REPLACE INTO transmog_template VALUES  (28704, 915,3);
+
