0f8b459cd318366e2b51b6330ccaae16e7d6f3fa
Torta <slowtorta@gmail.com>
2020-05-18 00:05:28 +0200

    Revert "Trying to fix Windows build."
    This reverts commit c33d2c2b346c822816f6efb854dfdb880ffb48b2.

--- cmake/FindMySQL.cmake
+++ cmake/FindMySQL.cmake
@@ -1,14 +1,3 @@
-# Copyright (C) 2011-2020 Project SkyFire <http://www.projectskyfire.org/
-# Copyright (C) 2008-2020 TrinityCore <http://www.trinitycore.org/>
-#
-# This file is free software; as a special exception the author gives
-# unlimited permission to copy and/or distribute it, with or without
-# modifications, as long as this notice is preserved.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY, to the extent permitted by law; without even the
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
 #
 # Find the MySQL client includes and library
 #
@@ -25,32 +14,32 @@ set( MYSQL_FOUND 0 )
 
 if( UNIX )
   set(MYSQL_CONFIG_PREFER_PATH "$ENV{MYSQL_HOME}/bin" CACHE FILEPATH
-          "preferred path to MySQL (mysql_config)"
-          )
+    "preferred path to MySQL (mysql_config)"
+  )
 
   find_program(MYSQL_CONFIG mysql_config
-          ${MYSQL_CONFIG_PREFER_PATH}
-          /usr/local/mysql/bin/
-          /usr/local/bin/
-          /usr/bin/
-          )
+    ${MYSQL_CONFIG_PREFER_PATH}
+    /usr/local/mysql/bin/
+    /usr/local/bin/
+    /usr/bin/
+  )
 
   if( MYSQL_CONFIG )
     message(STATUS "Using mysql-config: ${MYSQL_CONFIG}")
     # set INCLUDE_DIR
     exec_program(${MYSQL_CONFIG}
-            ARGS --include
-            OUTPUT_VARIABLE MY_TMP
-            )
+      ARGS --include
+      OUTPUT_VARIABLE MY_TMP
+    )
 
     string(REGEX REPLACE "-I([^ ]*)( .*)?" "\\1" MY_TMP "${MY_TMP}")
     set(MYSQL_ADD_INCLUDE_PATH ${MY_TMP} CACHE FILEPATH INTERNAL)
     #message("[DEBUG] MYSQL ADD_INCLUDE_PATH : ${MYSQL_ADD_INCLUDE_PATH}")
     # set LIBRARY_DIR
     exec_program(${MYSQL_CONFIG}
-            ARGS --libs_r
-            OUTPUT_VARIABLE MY_TMP
-            )
+      ARGS --libs_r
+      OUTPUT_VARIABLE MY_TMP
+    )
     set(MYSQL_ADD_LIBRARIES "")
     string(REGEX MATCHALL "-l[^ ]*" MYSQL_LIB_LIST "${MY_TMP}")
     foreach(LIB ${MYSQL_LIB_LIST})
@@ -74,63 +63,62 @@ if( UNIX )
 endif( UNIX )
 
 find_path(MYSQL_INCLUDE_DIR
-        NAMES
-        mysql.h
-        PATHS
-        ${MYSQL_ADD_INCLUDE_PATH}
-        /usr/include
-        /usr/include/mysql
-        /usr/local/include
-        /usr/local/include/mysql
-        /usr/local/mysql/include
-        "C:/Program Files/MySQL/MySQL Server 8.0/include"
-        "C:/Program Files/MySQL/include"
-        "C:/MySQL/include"
-        "[HKEY_LOCAL_MACHINE\\SOFTWARE\\MySQL AB\\MySQL Server 8.0;Location]/include"
-        "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\MySQL AB\\MySQL Server 8.0;Location]/include"
-        "$ENV{ProgramFiles}/MySQL/*/include"
-        "$ENV{SystemDrive}/MySQL/*/include"
-        "c:/msys/local/include"
-        DOC
-        "Specify the directory containing mysql.h."
-        )
+  NAMES
+    mysql.h
+  PATHS
+    ${MYSQL_ADD_INCLUDE_PATH}
+    /usr/include
+    /usr/include/mysql
+    /usr/local/include
+    /usr/local/include/mysql
+    /usr/local/mysql/include
+    "C:/Program Files/MySQL/include"
+    "C:/Program Files/MySQL/MySQL Server 5.0/include"
+    "C:/Program Files/MySQL/MySQL Server 5.1/include"
+    "C:/MySQL/include"
+    "[HKEY_LOCAL_MACHINE\\SOFTWARE\\MySQL AB\\MySQL Server 5.0;Location]/include"
+    "[HKEY_LOCAL_MACHINE\\SOFTWARE\\MySQL AB\\MySQL Server 5.1;Location]/include"
+    "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\MySQL AB\\MySQL Server 5.0;Location]/include"
+    "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\MySQL AB\\MySQL Server 5.1;Location]/include"
+    "c:/msys/local/include"
+  DOC
+    "Specify the directory containing mysql.h."
+)
 
 if( UNIX )
-  foreach(LIB ${MYSQL_ADD_LIBRARIES})
-    find_library( MYSQL_LIBRARY
-            NAMES
-            mysql libmysql ${LIB}
-            PATHS
-            ${MYSQL_ADD_LIBRARIES_PATH}
-            /usr/lib
-            /usr/lib/mysql
-            /usr/local/lib
-            /usr/local/lib/mysql
-            /usr/local/mysql/lib
-            DOC "Specify the location of the mysql library here."
-            )
-  endforeach(LIB ${MYSQL_ADD_LIBRARY})
+foreach(LIB ${MYSQL_ADD_LIBRARIES})
+  find_library( MYSQL_LIBRARY
+    NAMES
+      mysql libmysql ${LIB}
+    PATHS
+      ${MYSQL_ADD_LIBRARIES_PATH}
+      /usr/lib
+      /usr/lib/mysql
+      /usr/local/lib
+      /usr/local/lib/mysql
+      /usr/local/mysql/lib
+    DOC "Specify the location of the mysql library here."
+  )
+endforeach(LIB ${MYSQL_ADD_LIBRARY})
 endif( UNIX )
 
 if( WIN32 )
   find_library( MYSQL_LIBRARY
-          NAMES
-          libmysql
-          PATHS
-          ${MYSQL_ADD_LIBRARIES_PATH}
-          "C:/Program Files/MySQL/MySQL Server 8.0/lib/opt"
-          "C:/Program Files/MySQL/lib"
-          "C:/MySQL/lib/debug"
-          "C:/MySQL/lib"
-          "[HKEY_LOCAL_MACHINE\\SOFTWARE\\MySQL AB\\MySQL Server 8.0;Location]/lib"
-          "[HKEY_LOCAL_MACHINE\\SOFTWARE\\MySQL AB\\MySQL Server 8.0;Location]/lib/opt"
-          "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\MySQL AB\\MySQL Server 8.0;Location]/lib"
-          "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\MySQL AB\\MySQL Server 8.0;Location]/lib/opt"
-          "$ENV{ProgramFiles}/MySQL/*/lib/opt"
-          "$ENV{SystemDrive}/MySQL/*/lib/opt"
-          "c:/msys/local/include"
-          DOC "Specify the location of the mysql library here."
-          )
+    NAMES
+      mysql libmysql ${LIB}
+    PATHS
+      ${MYSQL_ADD_LIBRARIES_PATH}
+      "C:/Program Files/MySQL/lib"
+      "C:/Program Files/MySQL/MySQL Server 5.0/lib/opt"
+      "C:/Program Files/MySQL/MySQL Server 5.1/lib/opt"
+      "C:/MySQL/lib/debug"
+      "[HKEY_LOCAL_MACHINE\\SOFTWARE\\MySQL AB\\MySQL Server 5.0;Location]/lib/opt"
+      "[HKEY_LOCAL_MACHINE\\SOFTWARE\\MySQL AB\\MySQL Server 5.1;Location]/lib/opt"
+      "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\MySQL AB\\MySQL Server 5.0;Location]/lib/opt"
+      "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\MySQL AB\\MySQL Server 5.1;Location]/lib/opt"
+      "c:/msys/local/include"
+    DOC "Specify the location of the mysql library here."
+  )
 endif( WIN32 )
 
 # On Windows you typically don't need to include any extra libraries
@@ -138,14 +126,14 @@ endif( WIN32 )
 
 if( NOT WIN32 )
   find_library( MYSQL_EXTRA_LIBRARIES
-          NAMES
-          z zlib
-          PATHS
-          /usr/lib
-          /usr/local/lib
-          DOC
-          "if more libraries are necessary to link in a MySQL client (typically zlib), specify them here."
-          )
+    NAMES
+      z zlib
+    PATHS
+      /usr/lib
+      /usr/local/lib
+    DOC
+      "if more libraries are necessary to link in a MySQL client (typically zlib), specify them here."
+  )
 else( NOT WIN32 )
   set( MYSQL_EXTRA_LIBRARIES "" )
 endif( NOT WIN32 )
@@ -156,9 +144,9 @@ if( MYSQL_LIBRARY )
     message(STATUS "Found MySQL library: ${MYSQL_LIBRARY}")
     message(STATUS "Found MySQL headers: ${MYSQL_INCLUDE_DIR}")
   else( MYSQL_INCLUDE_DIR )
-    message(FATAL_ERROR "Could not find MySQL headers! Please install the development libraries and headers")
+      message(FATAL_ERROR "Could not find MySQL headers! Please install the development-libraries and headers.")
   endif( MYSQL_INCLUDE_DIR )
   mark_as_advanced( MYSQL_FOUND MYSQL_LIBRARY MYSQL_EXTRA_LIBRARIES MYSQL_INCLUDE_DIR )
 else( MYSQL_LIBRARY )
-  message(FATAL_ERROR "Could not find the MySQL libraries! Please install the development libraries and headers")
+  message(FATAL_ERROR "Could not find the MySQL libraries! Please install the development-libraries and headers.")
 endif( MYSQL_LIBRARY )
