d1a2a11b4d256c59831380923443a0a7f9557b2a
GrenderG <grenderg@gmail.com>
2020-07-07 16:34:27 +0200

    Fixes for Hillsbrad Vendors.

--- sql/_UPD_2020_WORLD_july.sql
+++ sql/_UPD_2020_WORLD_july.sql
@@ -137,3 +137,63 @@ UPDATE `creature` SET `position_x`=-1381.13, `position_y`=-87.0034, `position_z`
 -- Varimathras
 UPDATE `creature` SET `orientation`='0.820305' WHERE  `guid`=31901;
 UPDATE `creature_template` SET `scale`='1.2' WHERE  `entry`=2425;
+
+-- Pattern: Icy Cloak - Micha Yance
+UPDATE `npc_vendor` SET `maxcount` = 1, `incrtime` = 7200 WHERE `entry` = 2381 AND `item` = 4355;
+
+-- Formula: Enchant Bracer - Lesser Deflection - Micha Yance
+UPDATE `npc_vendor` SET `maxcount` = 1, `incrtime` = 43200 WHERE `entry` = 2381 AND `item` = 11163;
+
+-- Weighted Throwing Axe - Sarah Raycroft
+DELETE FROM `npc_vendor` WHERE `entry` = 3541 AND `item` = 3131;
+
+-- Balanced Throwing Dagger - Sarah Raycroft
+DELETE FROM `npc_vendor` WHERE `entry` = 3541 AND `item` = 2946;
+
+-- Striped Yellowtail - Hal McAllister
+UPDATE `npc_vendor` SET `maxcount` = 0, `incrtime` = 0 WHERE `entry` = 3540 AND `item` = 21552;
+
+-- Strong Fishing Pole - Lindea Rabonne
+UPDATE `npc_vendor` SET `maxcount` = 1, `incrtime` = 7200 WHERE `entry` = 2383 AND `item` = 6365;
+
+-- Flagon of Mead - Barkeep Kelly
+DELETE FROM `npc_vendor` WHERE `entry` = 2366 AND `item` = 2594;
+
+-- Southshore Stout - Barkeep Kelly
+DELETE FROM `npc_vendor` WHERE `entry` = 2366 AND `item` = 3703;
+
+-- Cherry Grog - Barkeep Kelly
+DELETE FROM `npc_vendor` WHERE `entry` = 2366 AND `item` = 4600;
+
+-- Cheap Beer - Barkeep Kelly
+DELETE FROM `npc_vendor` WHERE `entry` = 2366 AND `item` = 19222;
+
+-- Lesser Healing Potion - Nandar Branson
+DELETE FROM `npc_vendor` WHERE `entry` = 2380 AND `item` = 858;
+
+-- Lesser Mana Potion - Nandar Branson
+DELETE FROM `npc_vendor` WHERE `entry` = 2380 AND `item` = 3385;
+
+-- Fiery Cloak - Robert Aebischer
+UPDATE `npc_vendor` SET `maxcount` = 1, `incrtime` = 14400 WHERE `entry` = 3543 AND `item` = 4797;
+
+-- Heavy Runed Cloak - Robert Aebischer
+UPDATE `npc_vendor` SET `maxcount` = 1, `incrtime` = 14400 WHERE `entry` = 3543 AND `item` = 4798;
+
+-- Antiquated Cloak - Robert Aebischer
+UPDATE `npc_vendor` SET `maxcount` = 1, `incrtime` = 14400 WHERE `entry` = 3543 AND `item` = 4799;
+
+-- Mighty Chain Pants - Robert Aebischer
+UPDATE `npc_vendor` SET `maxcount` = 1, `incrtime` = 14400 WHERE `entry` = 3543 AND `item` = 4800;
+
+-- Legionnaire's Leggings - Robert Aebischer
+UPDATE `npc_vendor` SET `maxcount` = 1, `incrtime` = 14400 WHERE `entry` = 3543 AND `item` = 4816;
+
+-- Weighted Throwing Axe - Kayren Soothallow
+DELETE FROM `npc_vendor` WHERE `entry` = 2401 AND `item` = 3131;
+
+-- Balanced Throwing Dagger - Kayren Soothallow
+DELETE FROM `npc_vendor` WHERE `entry` = 2401 AND `item` = 2946;
+
+-- Executor Staff - Ott
+DELETE FROM `npc_vendor` WHERE `entry` = 3539 AND `item` = 3277;
