77e3bfae47b422bb25af3d1cd1fa7ad07677793d
Torta <slowtorta@gmail.com>
2020-08-25 02:24:46 +0200

    Added bunch of the other foxes.

--- sql/_expansion/hunter_pets.sql
+++ sql/_expansion/hunter_pets.sql
@@ -3,7 +3,39 @@ REPLACE INTO `creature_template` VALUES (80250, 0, 18027, 0, 0, 0, 'Crimson Lynx
 update creature_template set faction = 7 where entry = 80250;
 update creature_template set rank = 0 where entry = 80250;
 
-REPLACE INTO `creature_template` VALUES (80251, 0, 18244, 0, 0, 0, 'Spirit Fox', NULL, 0, 53, 53, 3418, 3662, 0, 0, 3163, 14, 0, 1, 1.42857, 0, 20, 5, 0, 4, 1, 452, 584, 0, 238, 1, 1200, 1329, 1, 0, 0, 1, 0, 0, 0, 0, 57.4128, 78.9426, 100, 1, 1, 10077, 0, 0, 0, 100, 100, 100, 100, 100, 0, 0, 0, 0, 100770, 0, 0, 0, '', 1, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, '');
+REPLACE INTO `creature_template` VALUES (80252, 0, 18244, 0, 0, 0, 'Spirit Fox', NULL, 0, 53, 53, 3418, 3662, 0, 0, 3163, 14, 0, 1, 1.42857, 0, 20, 5, 0, 4, 1, 452, 584, 0, 238, 1, 1200, 1329, 1, 0, 0, 1, 0, 0, 0, 0, 57.4128, 78.9426, 100, 1, 1, 10077, 0, 0, 0, 100, 100, 100, 100, 100, 0, 0, 0, 0, 100770, 0, 0, 0, '', 1, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, '');
 
 update creature_template set movement_type = 1 where entry = 80251;
 update creature_template set scale = 1.2 where entry = 80251;
+
+REPLACE INTO `creature_template` VALUES (80252, 0, 18005, 0, 0, 0, 'Young Arctic Fox', NULL, 0, 7, 8, 141, 161, 0, 0, 316, 38, 0, 1, 0.857143, 0, 20, 5, 0, 0, 1, 9, 13, 0, 58, 1, 2000, 2000, 1, 0, 0, 1, 0, 0, 0, 0, 13.4464, 18.4888, 100, 1, 1, 1131, 0, 1131, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5940, 0, 0, '', 1, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, '');
+
+update creature_template set faction = 7 where entry = 80252;
+
+REPLACE INTO `creature_template` VALUES (80253, 3, 18005, 0, 0, 0, 'Arctic Fox', NULL, 0, 50, 51, 2196, 2324, 0, 0, 2200, 1275, 0, 1, 2.57143, 0, 20, 5, 0, 0, 1, 161, 193, 0, 230, 1, 2000, 2000, 1, 0, 0, 1, 0, 0, 0, 0, 71.9664, 98.9538, 100, 1, 1, 10981, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 109810, 0, 0, 0, '', 1, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, 'npc_ram_wolf_tamed');
+
+update creature_template set faction = 7 where entry = 80253;
+
+REPLACE INTO `creature_template` VALUES (80254, 0, 18007, 0, 0, 0, 'Young Brown Fox', NULL, 0, 10, 11, 198, 221, 0, 0, 538, 38, 0, 1, 1.14286, 0, 20, 5, 0, 0, 1, 15, 21, 0, 64, 1, 2000, 2000, 1, 0, 0, 1, 0, 0, 0, 0, 16.632, 22.869, 100, 1, 1, 1765, 0, 1765, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 17650, 5945, 0, 0, '', 1, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, '');
+
+update creature_template set faction = 7 where entry = 80254;
+
+REPLACE INTO `creature_template` VALUES (80255, 0, 18007, 0, 0, 0, 'Brown Fox', NULL, 0, 41, 42, 1857, 2123, 0, 0, 2246, 38, 0, 1, 1.14286, 0, 20, 5, 0, 0, 1, 61, 78, 0, 172, 1, 2000, 2000, 1, 0, 0, 1, 0, 0, 0, 0, 57.0064, 78.3838, 100, 1, 1, 2923, 0, 2923, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 1, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, '');
+
+update creature_template set faction = 7 where entry = 80255;
+
+REPLACE INTO `creature_template` VALUES (80256, 0, 18006, 0, 0, 0, 'Silver Fox', NULL, 0, 27, 28, 843, 915, 0, 0, 1130, 38, 0, 1, 1.14286, 0, 20, 5, 0, 0, 1, 31, 38, 0, 116, 1, 1300, 2000, 1, 0, 0, 1, 0, 0, 0, 0, 42.6272, 58.6124, 100, 1, 1, 3825, 0, 3825, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5951, 0, 0, 'EventAI', 1, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, '');
+
+update creature_template set faction = 7 where entry = 80256;
+
+REPLACE INTO `creature_template` VALUES (80257, 0, 18008, 0, 0, 0, 'Red Fox', NULL, 0, 10, 11, 198, 221, 0, 0, 538, 38, 0, 1, 1.14286, 0, 20, 5, 0, 0, 1, 15, 21, 0, 64, 1, 2000, 2000, 1, 0, 0, 1, 0, 0, 0, 0, 16.632, 22.869, 100, 1, 1, 1765, 0, 1765, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 17650, 5945, 0, 0, '', 1, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, '');
+
+update creature_template set faction = 7 where entry = 80257;
+
+REPLACE INTO `creature_template` VALUES (80258, 0, 18004, 0, 0, 0, 'Young Mist Fox', NULL, 0, 10, 11, 198, 221, 0, 0, 538, 38, 0, 1, 1.14286, 0, 20, 5, 0, 0, 1, 15, 21, 0, 64, 1, 2000, 2000, 1, 0, 0, 1, 0, 0, 0, 0, 16.632, 22.869, 100, 1, 1, 1765, 0, 1765, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 17650, 5945, 0, 0, '', 1, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, '');
+
+update creature_template set faction = 7 where entry = 80258;
+
+REPLACE INTO `creature_template` VALUES (80259, 3, 18004, 0, 0, 0, 'Mist Fox', NULL, 0, 50, 51, 2196, 2324, 0, 0, 2200, 1275, 0, 1, 2.57143, 0, 20, 5, 0, 0, 1, 161, 193, 0, 230, 1, 2000, 2000, 1, 0, 0, 1, 0, 0, 0, 0, 71.9664, 98.9538, 100, 1, 1, 10981, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 109810, 0, 0, 0, '', 1, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, 'npc_ram_wolf_tamed');
+
+update creature_template set faction = 7 where entry = 80259;
