66c70fb3b6f2b0380e0cca5197d70c0ef01b67eb
Torta <slowtorta@gmail.com>
2020-08-30 22:00:34 +0200

    Update WORLD_autobroadcast.sql

--- sql/WORLD_autobroadcast.sql
+++ sql/WORLD_autobroadcast.sql
@@ -9,7 +9,7 @@ replace into mangos_string (entry, content_default) values
 ('50005', 'A few of us know where and how to preserve style and fashion while protecting your kingdom and helping your allies. We eagerly invite you and your friends to come and visit fashionistas and barbers located in all major cities.'),
 ('50006', 'If you want to help our project, check our shop for donation rewards: exotic garments, large bags, mysterious engineering devices and much more: turtle-wow.org/donate'),
 ('50007', 'Speak to Karn Deepeye in Orgrimmar or Tarun Swifteagle in Stormwind to become a diplomat! You will be able to group and trade with the players of an opposite faction.'),
-('50008', 'We\'ve implemented a list of features that Blizzard never published, you can find information about them on our forums under Sands of Time section.'),
+('50008', 'We\'ve implemented a list of features that Blizzard never published, you can find information about them on our website.'),
 ('50009', 'If you really like questing and leveling, play in hardcore mode, your creature kill rates will be set to 0.5. Enjoy your journey and receive rewards every 10 levels!'),
 ('50010', 'We\'re constantly working on improving the leveling experience on Turtle WoW: creating new quests for starting locations or extending the existing quest chains. If you have an idea for a new quest, please visit this page: dev.turtle-wow.org'),
 ('50011', 'Have you ever had a dream about setting your own garden? Grow your own food: plant the seed, water it, foster it, and enjoy the results of your hard work once it’s fully grown! Speak to Kern Mosshoof in Mulgore or find Mortimer Stansfield in Elwynn Forest.');
