0846ecc53d3a08b7e0c8abf3038f127e7d44e306
Torta <slowtorta@gmail.com>
2020-09-12 23:03:13 +0200

    New quest: Grease-stained letter (Horde).

---
+++ sql/_expansion/stormwind_vault/sw_vault_quests.sql
@@ -0,0 +1,93 @@
+
+-- Horde
+
+replace into`creature_template` values (80000, 0, 0, 0, 0, 0, 'Quest 80000 Custom Objective', '', 0, 5, 5, 319, 319, 0, 0, 852, 84, 2, 1, 1.14286, 0, 20, 5, 0, 0, 1, 24, 31, 0, 90, 1, 2000, 2000, 1, 512, 0, 0, 0, 0, 0, 0, 31.856, 43.802, 100, 7, 4096, 0, 0, 0, 0, 0, 0, 0, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 0, 0, 3, 4276, 0, 0, 0, 0, 2, '');
+
+replace into broadcast_text (ID, MaleText) values (80000, 'I am more than happy to have your aid and have you fight on our side for the time being, yet I do not need to remind you, you are a stranger in the house of your enemy, so tread carefully.');
+replace into npc_text (ID, BroadcastTextID0) values (80000, 80000);
+
+replace into gameobject_template values 
+(3000226, 0, 1, 6696, 'Portal to Stormwind Vaults', 35, 0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'go_portal_stormwind_vaults');
+
+set @quest_entry = 80000;
+set @quest_zone = 85;
+set @title = 'My friend is my enemy but he wasn\'t always...';
+set @description = 'I’ve received a letter from a long lost friend to say so... A great dark presence looms in the city of Stormwind, something very powerful and old, I have no idea how Stormwind even came across such a beast but nor do I wish to!\n\nThis place I am sending you to is very, very dangerous, yet at the same time could aid both you and me and of course, the Horde.\n\nIt is said that the mastermind behind the enchantments and seals to contain the monsters of that place was none other than Medivh, the Last Guardian, what manner of idiot would refuse a chance to see with his/her own eyes such a masterwork probably familiar to Karazhan?\n\nWhat I want you to do is jump in this here portal and find out if there is any information left about those seals, and of course aid the Alliance for the greater good if you are particularly keen to that.\n\nOh and if by any means you meet Elsharin on the way in or out tell her Gunther says hi and that he would very much enjoy meeting her again.';
+set @objective = 'Take Gunther’s portal to the Stormwind Vaults.';
+set @completetext = 'Do you want me to push you or something?';
+set @incompletetext = '$N, it is good to have someone as competent as yourself aid us with this matter of secrecy.';
+set @faction_id = 269;
+set @faction_count = 250;
+set @xp_or_money = 2500;
+set @reward_money = 0; 
+set @quest_level = 60;
+set @min_level = 58;
+set @questgiver_id = 1497;
+set @quest_finisher = 1497;
+set @nextquest = 0;
+set @nextquestinchain = 0;
+set @prevquest = 0;
+set @RewChoiceItemId1 = 0; 
+set @RewChoiceItemId2 = 0; 
+set @RewChoiceItemId3 = 0;
+set @RewChoiceItemId4 = 0; 
+set @RewChoiceItemCount1 = 0;
+set @RewChoiceItemCount2 = 0;
+set @RewChoiceItemCount3 = 0;
+set @RewChoiceItemCount4 = 0;
+set @reward_item_1 = 0;
+set @reward_item_2 = 0; 
+set @reward_item_3 = 0;
+set @reward_item_4 = 0;
+set @reward_item_1_count = 0;
+set @reward_item_2_count = 0;
+set @reward_item_3_count = 0;
+set @reward_item_4_count = 0;
+set @creature_to_kill_1 = 80000;
+set @creature_to_kill_2 = 0; 
+set @creature_to_kill_3 = 0;
+set @creature_to_kill_4 = 0;
+set @creature_to_kill_1_count = 1;
+set @creature_to_kill_2_count = 0;
+set @creature_to_kill_3_count = 0;
+set @creature_to_kill_4_count = 0;
+set @required_item_1 = 0; 
+set @required_item_2 = 0;
+set @required_item_3 = 0;
+set @required_item_4 = 0;
+set @required_item_1_count = 0;
+set @required_item_2_count = 0;
+set @required_item_3_count = 0;
+set @required_item_4_count = 0;
+
+replace into quest_template values 
+
+(@quest_entry, '0', '2', @quest_zone, @min_level,  '0', @quest_level, '0', '0', '0', '0', '0', '0', '0', '0', '0','0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', @title, @description, @objective, @completetext, @incompletetext, '', '', '', '', '', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', @faction_id, '0', '0', '0', '0', @faction_count, '0', '0', '0', '0', '0', @xp_or_money, '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0','0', 0, 0);
+     
+replace into creature_questrelation (id, quest, patch_min, patch_max) values (@questgiver_id, @quest_entry,'0','10'); 
+replace into creature_involvedrelation (id, quest, patch_min, patch_max) values (@quest_finisher, @quest_entry,'0','10');
+
+update creature_template set npc_flags = 3 where entry = @questgiver_id;  
+update creature_template set script_name = "npc_gunther" where entry = @questgiver_id;  
+	 
+update quest_template set 
+rewitemid1 = @reward_item_1, rewitemcount1 = @reward_item_1_count,
+rewitemid2 = @reward_item_2, rewitemcount2 = @reward_item_2_count,
+rewitemid3 = @reward_item_3, rewitemcount3 = @reward_item_3_count,
+rewitemid4 = @reward_item_4, rewitemcount4 = @reward_item_4_count,
+RewChoiceItemId1 = @RewChoiceItemId1, RewChoiceItemCount1 = @RewChoiceItemCount1,
+RewChoiceItemId2 = @RewChoiceItemId2, RewChoiceItemCount2 = @RewChoiceItemCount2,
+RewChoiceItemId3 = @RewChoiceItemId3, RewChoiceItemCount3 = @RewChoiceItemCount3,
+RewChoiceItemId4 = @RewChoiceItemId4, RewChoiceItemCount4 = @RewChoiceItemCount4	,
+ReqCreatureOrGOId1 = @creature_to_kill_1, ReqCreatureOrGOCount1 = @creature_to_kill_1_count,
+ReqCreatureOrGOId2 = @creature_to_kill_2, ReqCreatureOrGOCount2 = @creature_to_kill_2_count,
+ReqCreatureOrGOId3 = @creature_to_kill_3, ReqCreatureOrGOCount3 = @creature_to_kill_3_count,
+ReqCreatureOrGOId4 = @creature_to_kill_4, ReqCreatureOrGOCount4 = @creature_to_kill_4_count,
+reqitemid1 = @required_item_1, reqitemcount1 = @required_item_1_count,
+reqitemid2 = @required_item_2, reqitemcount2 = @required_item_2_count,
+reqitemid3 = @required_item_3, reqitemcount3 = @required_item_3_count,
+reqitemid4 = @required_item_4, reqitemcount4 = @required_item_4_count,
+nextquestid = @nextquest, RewOrReqMoney = @reward_money, 
+nextquestinchain = @nextquestinchain, prevquestid = @prevquest,
+objectivetext1='Ask Gunther to open a portal'  
+where entry = @quest_entry;	 
