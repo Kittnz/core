8a39a3b460a89ccb81c8bcdb04b36c46e14f3a0b
Torta <slowtorta@gmail.com>
2020-09-13 14:51:09 +0200

    New quest: Garbage Man (Horde).

--- sql/_expansion/goblin_starting_zone.sql
+++ sql/_expansion/goblin_starting_zone.sql
@@ -2038,6 +2038,128 @@ nextquestinchain = @nextquestinchain, prevquestid = @prevquest,
 srcItemId = 80154, srcitemcount = 1, RequiredClasses = 256
 where entry = @quest_entry;	
 
+-- Garbage Man 
+
+REPLACE INTO `creature_template` VALUES (80131, 0, 11353, 0, 0, 0, 'Kazznik', 'Venture Co. Worker', 0, 5, 5, 319, 319, 0, 0, 852, 35, 2, 1, 1.14286, 0, 20, 5, 0, 0, 1, 24, 31, 0, 90, 1, 2000, 2000, 1, 512, 0, 0, 0, 0, 0, 0, 31.856, 43.802, 100, 7, 4096, 0, 0, 0, 0, 0, 0, 0, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 0, 0, 3, 4276, 0, 0, 0, 0, 2, '');
+
+update creature_template set equipment_id = 51261 where entry = 80131;
+update creature_template set npc_flags = 2 where entry = 80131;
+
+replace into gameobject_template values 
+(3000227, 0, 3, 23550, 'Trash', 0, 0, 0.7, 57, 3000227, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');
+
+update gameobject_template set flags = 4 where entry = 3000227;
+update gameobject_template set data0 = 43 where entry = 3000227;
+update gameobject_template set data2 = 0 where entry = 3000227;
+update gameobject_template set data3 = 1 where entry = 3000227;
+
+replace into gameobject_template values 
+(3000228, 0, 3, 23556, 'Trash', 0, 0, 0.8, 57, 3000227, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');
+
+update gameobject_template set flags = 4 where entry = 3000228;
+update gameobject_template set data0 = 43 where entry = 3000228;
+update gameobject_template set data2 = 0 where entry = 3000228;
+update gameobject_template set data3 = 1 where entry = 3000228;
+
+replace into gameobject_template values 
+(3000229, 0, 3, 23542, 'Trash', 0, 0, 0.8, 57, 3000227, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');
+
+update gameobject_template set flags = 4 where entry = 3000229;
+update gameobject_template set data0 = 43 where entry = 3000229;
+update gameobject_template set data2 = 0 where entry = 3000229;
+update gameobject_template set data3 = 1 where entry = 3000229;
+
+replace into `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) values 
+(3000227, 80155, -100, 1, 1, 1, 0, 0, 10);
+
+replace into item_template values
+ ('80155', '0', '0', '0', 'Trash', '', '2053', '1', '0', '1', '0', '0', '0', '-1', '-1', '0',
+ '0', '0', '0', '0', '0', '0', '0', '0', '0', '8', '0', '0', '0', '0', '0',
+ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '0', '0', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0',
+ '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0',
+ '-1', '4', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '1', NULL);
+
+set @quest_entry = 80120;
+set @quest_zone = 406;
+set @title = 'Garbage Man';
+set @description = 'Hey, you! I\'m talking to ya, pal!\n\nI\'ve been picking up our crew\'s trash all day long and I could really use a break.\n\nSay, how about you clean up for a while?\n\nI\'ll be over here in the shade taking a quick snooze. When I wake up, I expect this place to be spotless.\n\nNow get going!';
+set @objective = 'Kazznik has asked you to collect trash littered throughout the worksite.';
+set @completetext = 'Good job!\n\nDon\'t tell the boss I was taking a break. ';
+set @incompletetext = 'Oh, you\'re back already?';
+set @faction_id = 0;
+set @faction_count = 0;
+set @xp_or_money = 178;
+set @reward_money = 50; 
+set @quest_level = 2;
+set @min_level = 2;
+set @questgiver_id = 80131;
+set @quest_finisher = 80131;
+set @nextquest = 0;
+set @nextquestinchain = 0;
+set @prevquest = 80100;
+set @RewChoiceItemId1 = 0; 
+set @RewChoiceItemId2 = 0; 
+set @RewChoiceItemId3 = 0;
+set @RewChoiceItemId4 = 0; 
+set @RewChoiceItemCount1 = 0;
+set @RewChoiceItemCount2 = 0;
+set @RewChoiceItemCount3 = 0;
+set @RewChoiceItemCount4 = 0;
+set @reward_item_1 = 4957;
+set @reward_item_2 = 0; 
+set @reward_item_3 = 0;
+set @reward_item_4 = 0;
+set @reward_item_1_count = 1;
+set @reward_item_2_count = 0;
+set @reward_item_3_count = 0;
+set @reward_item_4_count = 0;
+set @creature_to_kill_1 = 0;
+set @creature_to_kill_2 = 0; 
+set @creature_to_kill_3 = 0;
+set @creature_to_kill_4 = 0;
+set @creature_to_kill_1_count = 0;
+set @creature_to_kill_2_count = 0;
+set @creature_to_kill_3_count = 0;
+set @creature_to_kill_4_count = 0;
+set @required_item_1 = 80155; 
+set @required_item_2 = 0;
+set @required_item_3 = 0;
+set @required_item_4 = 0;
+set @required_item_1_count = 8;
+set @required_item_2_count = 0;
+set @required_item_3_count = 0;
+set @required_item_4_count = 0;
+
+replace into quest_template values 
+
+(@quest_entry, '0', '2', @quest_zone, @min_level,  '0', @quest_level, '0', '0', '0', '0', '0', '0', '0', '0', '0','0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', @title, @description, @objective, @completetext, @incompletetext, '', '', '', '', '', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', @faction_id, '0', '0', '0', '0', @faction_count, '0', '0', '0', '0', '0', @xp_or_money, '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0','0', 0, 0);
+     
+replace into creature_questrelation (id, quest, patch_min, patch_max) values (@questgiver_id, @quest_entry,'0','10'); 
+replace into creature_involvedrelation (id, quest, patch_min, patch_max) values (@quest_finisher, @quest_entry,'0','10');
+	 
+update quest_template set 
+rewitemid1 = @reward_item_1, rewitemcount1 = @reward_item_1_count,
+rewitemid2 = @reward_item_2, rewitemcount2 = @reward_item_2_count,
+rewitemid3 = @reward_item_3, rewitemcount3 = @reward_item_3_count,
+rewitemid4 = @reward_item_4, rewitemcount4 = @reward_item_4_count,
+RewChoiceItemId1 = @RewChoiceItemId1, RewChoiceItemCount1 = @RewChoiceItemCount1,
+RewChoiceItemId2 = @RewChoiceItemId2, RewChoiceItemCount2 = @RewChoiceItemCount2,
+RewChoiceItemId3 = @RewChoiceItemId3, RewChoiceItemCount3 = @RewChoiceItemCount3,
+RewChoiceItemId4 = @RewChoiceItemId4, RewChoiceItemCount4 = @RewChoiceItemCount4,
+ReqCreatureOrGOId1 = @creature_to_kill_1, ReqCreatureOrGOCount1 = @creature_to_kill_1_count,
+ReqCreatureOrGOId2 = @creature_to_kill_2, ReqCreatureOrGOCount2 = @creature_to_kill_2_count,
+ReqCreatureOrGOId3 = @creature_to_kill_3, ReqCreatureOrGOCount3 = @creature_to_kill_3_count,
+ReqCreatureOrGOId4 = @creature_to_kill_4, ReqCreatureOrGOCount4 = @creature_to_kill_4_count,
+reqitemid1 = @required_item_1, reqitemcount1 = @required_item_1_count,
+reqitemid2 = @required_item_2, reqitemcount2 = @required_item_2_count,
+reqitemid3 = @required_item_3, reqitemcount3 = @required_item_3_count,
+reqitemid4 = @required_item_4, reqitemcount4 = @required_item_4_count,
+nextquestid = @nextquest, RewOrReqMoney = @reward_money, 
+nextquestinchain = @nextquestinchain, prevquestid = @prevquest 
+where entry = @quest_entry;	 
 
 -- Populating the area:
 
