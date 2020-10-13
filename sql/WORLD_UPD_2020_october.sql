09dbd2a110f0e96aed8cb4456fd723660f8704e4
Torta <slowtorta@gmail.com>
2020-10-13 14:24:24 -0700

    Nythandral's grammar fixes.

--- sql/WORLD_UPD_2020_october.sql
+++ sql/WORLD_UPD_2020_october.sql
@@ -276,3 +276,59 @@ update quest_template set details = 'One of my esteemed colleagues set off to ga
 update quest_template set objectives = 'High Sorcerer Andromath wants you to travel to Shadowfang Keep in Silverpine Forest and find out what happened to Sorcerer Ashcrombe.' where title = 'The Missing Sorcerer';
 update quest_template set offerrewardtext = 'High Sorcerer Andromath sent you?\n\nMy, how the time flies when you’re watching insanity and horror unfold from every angle.\n\nI will have to return with my findings and reassure him that I have not fallen to the same madness as those who are trapped here.' where title = 'The Missing Sorcerer';
 update quest_template set requestitemstext = 'There is untold darkness that will forever linger inside of these walls.' where title = 'The Missing Sorcerer';
+
+
+update quest_template set details = 'The curse of undeath has caused my mind to rot so much that I forgot the basics about human anatomy! The human I’ve captured in the next room has been causing my brain to swell in my skull from its piercing screams. I can’t study it like that!\n\nIf I can examine its  body without that incessant howling drowning out my thoughts, then I will impart to you a skill most valuable: the ability to disguise yourself as a human! Get a nice, clean slice across its neck and bring me its head intact.\n\nI’ll take great joy in seeing those lifeless eyes.' where title = 'Human Anatomy';
+update quest_template set objectives = 'Kill the human peasant and bring his head to Professor Malkovich.' where title = 'Human Anatomy';
+update quest_template set offerrewardtext = 'Muhahaha!\n\nFantastic work my little prodigy. I have a gruesome lesson for you, so pay attention! And don’t forget to take detailed notes.' where title = 'Human Anatomy';
+update quest_template set requestitemstext = '<Professor Malkovich clicks the bony tips of his fingers together.>\n\nGet to it! Bring me the human\'s head.' where title = 'Human Anatomy';
+
+
+update quest_template set details = 'Between the webbing of a murloc’s fingers, you spot a scuffed ring. Something about it calls out to you to take as if it’s more valuable than it appears at first glance. You try to shine it on your armor, but it seems to be getting dirtier with each wipe.\n\nThis magical ring has more to it than meets the eye. You should find someone with more experience to examine it for you.
+' where title = 'That Dirty Old Ring';
+update quest_template set objectives = 'Find a magical expert in Goldshire to examine the Dirty Old Ring.' where title = 'That Dirty Old Ring';
+update quest_template set offerrewardtext = 'Interesting… someone has placed a curse upon this ring. If I had to guess, they probably didn’t want it stolen. Oh well.\n\n<Zaldimar utters an incantation while gesturing with his hand, and in a flash of light, the ring appears as a lustrous gold band.>\n\nHere you are.' where title = 'That Dirty Old Ring';
+update quest_template set requestitemstext = 'I don’t have time for such silly little trinkets! But wait… there’s something different about this one.' where title = 'That Dirty Old Ring';
+
+
+update quest_template set details = 'You have humbly accepted our burden to purge the Scourge, yet you lack the proper coat of arms to show for it. \n\nGo see Benjamin Bradley and he will assist with fitting you into a tabard that will signify your standing with the Scarlet Crusade.\n\nWear our colors proudly, recruit.' where title = 'Before the Storm';
+update quest_template set objectives = 'Speak to Benjamin Bradley at the Scarlet Outpost near the Scarlet Monastery.' where title = 'Before the Storm';
+update quest_template set offerrewardtext = 'Remember the significance of this banner always. When you feel a flame of passion and fury light up your heart, know that the entire Crusade is standing behind you.\n\nLead us to glory, young acolyte.' where title = 'Before the Storm';
+update quest_template set requestitemstext = 'You must be one of the initiates. I’ve had a tabard freshly stitched for you.' where title = 'Before the Storm';
+
+
+update quest_template set details = 'It is time to prove your loyalties to the Crusade, recruit.\n\nLook around you. This farmland was once home to bountiful fields of crops and grain. Now, it is a place of death and despair. How could it have devolved to such a point, you might wonder?\n\nThe Scourge. Their goal is to extinguish the Light from this world, and we are all that stands in their way. By being here, you have chosen your destiny to protect all that is righteous.\n\nTheir horrid presence taints the land around us. Exterminate them.' where entry = 50302;
+update quest_template set objectives = 'Kill 15 Undead Slayers at Solliden\'s Farmstead in Tirisfal Glades.' where entry = 50302;
+update quest_template set offerrewardtext = 'You have proven yourself worthy to bear our crest.\n\nI bestow upon you your signet to authenticate your allegiance to the Scarlet Crusade.\n\nThere is still more work to be done.' where entry = 50302;
+update quest_template set requestitemstext = 'Have you completed your first mission, recruit? Or have you forsaken the Light?' where entry = 50302;
+
+
+update quest_template set details = 'You have proven yourself capable enough in assisting us to deal with their leaders, Agal and Greenpaw, $C. Our scouts have pinpointed their locations. First is Agal, who orchestrates the ambushes from below the road in a cave. Second is Greenpaw, a powerful shaman who terrorizes Ban\'ethil Barrow Den from within.\n\nThey are surrounded by their respective underlings. I suggest you bring an ally or prepare yourself to fight your way through them alone. No matter your decision, may Elune protect you.' where title = 'A Decisive Blow';
+update quest_template set objectives = 'Moon Priestess Amara has asked you to kill Agal in the cave below the road to Darnassus and Greenpaw inside Ban\'ethil Barrow Den.' where title = 'A Decisive Blow';
+update quest_template set offerrewardtext = 'Elune be praised! Though I must admit, it is painful that it had to end this way.\n\nPlease know that your actions today will ensure safe passage to Darnassus for our people.\n\nThank you, $C.' where title = 'A Decisive Blow';
+update quest_template set requestitemstext = 'Please be careful, Agal and Greenpaw were once honorable and powerful allies.\n\nNow they have been corrupted beyond redemption.' where title = 'A Decisive Blow';
+
+
+update quest_template set details = 'Pssst… I’ve got a task for you, $C. Look behind me. I found this drunk orc spyin’ around Ironforge and captured him, see? But I want to get a little “creative” with my payback. Bring me his head in good condition and I’ll show you somethin\' very worth your while.' where title = 'The Orcish Anatomy';
+update quest_template set objectives = 'Kill the Stupefied Orcish Peon and bring his head to Professor Papucho.' where title = 'The Orcish Anatomy';
+update quest_template set offerrewardtext = 'Ma always said, “Waste not, want not!”\n\nI really took that to heart and started “repurposing” the creatures that I could get away with doppleganging as. Goblins, kobolds, troggs... things like that.\n\nWhat, you don’t think I can look like this orc? Maybe I can’t, but you definitely can.\n\nThere’s a bunch of other disguises out in the world if you’re crafty enough. You just have to go and find \'em.
+Take these notes with you, and good luck with your game of charades.' where title = 'The Orcish Anatomy';
+update quest_template set requestitemstext = 'What’re you waitin’ for? I ain’t got all day, bub. Bring me the drunk orc’s head.' where title = 'The Orcish Anatomy';
+
+
+update quest_template set details = 'You look like a brave hero, $N. Brother Crowley wants me to find the most devout ones who want to do the good work of the Light, he said.\n\nHe seemed pretty excited about it, too. You should go talk to him! He’s in the lower part of the Cathedral.' where entry = 50300;
+update quest_template set objectives = 'Talk to Brother Crowley at the Stormwind City Cathedral.' where entry = 50300;
+update quest_template set offerrewardtext = 'Thomas has a pure soul. He’ll make a fine commander in our ranks someday.\n\nOnly the holiest will be able to ascend to our cause. Are you one of them?' where entry = 50300;
+update quest_template set requestitemstext = 'Greetings, weary traveler. Light be with you.' where entry = 50300;
+
+
+update quest_template set details = 'Our righteous duty is not for the faint of heart, adventurer. Many critics come with the banner that we carry, but in the end, we know we are doing what is just.\n\nWhen Lordaeron was destroyed, the Light guided us into assembly to reclaim the land that once was ours and purge it of the vile Scourge threat. Our methods are decisive and more than anyone in the Alliance is willing to do. You must be prepared to make sacrifices if you are to join our ranks, $C.\n\nIf you’re ready and willing, then I will arrange a caravan that will take you to Tirisfal Glades where you can find Godrick Bell. He will initiate you, thereby pledging your life in service to the Light.\n\nI\'ve arranged a ride to go to Tirisfal for you and the other recruits. Are you ready to depart?' where entry = 50301;
+update quest_template set objectives = 'Speak with Godrick Bell at Solliden’s Farmstead in Tirisfal Glades.' where entry = 50301;
+update quest_template set offerrewardtext = 'Well, initiate, you came all this way, didn’t you? Don\'t disappoint me.' where entry = 50301;
+update quest_template set requestitemstext = 'Splendid, new recruits! Just in time.' where entry = 50301;
+
+
+-- update quest_template set details = '' where title = '';
+-- update quest_template set objectives = '' where title = '';
+-- update quest_template set offerrewardtext = '' where title = '';
+-- update quest_template set requestitemstext = '' where title = '';
