94eb7d246393a910d6beb86f25646fe841023ff3
Torta <slowtorta@gmail.com>
2020-10-19 23:04:53 -0700

    New NPCs.

--- sql/_expansion/general_fixes.sql
+++ sql/_expansion/general_fixes.sql
@@ -6,10 +6,6 @@ REPLACE INTO `creature_template` VALUES (80250, 0, 18027, 0, 0, 0, 'Crimson Lynx
 update creature_template set faction = 7 where entry = 80250;
 update creature_template set rank = 0 where entry = 80250;
 
-REPLACE INTO `creature_template` VALUES (80252, 0, 18244, 0, 0, 0, 'Spirit Fox', NULL, 0, 53, 53, 3418, 3662, 0, 0, 3163, 14, 0, 1, 1.42857, 0, 20, 5, 0, 4, 1, 452, 584, 0, 238, 1, 1200, 1329, 1, 0, 0, 1, 0, 0, 0, 0, 57.4128, 78.9426, 100, 1, 1, 10077, 0, 0, 0, 100, 100, 100, 100, 100, 0, 0, 0, 0, 100770, 0, 0, 0, '', 1, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, '');
-
-update creature_template set movement_type = 1 where entry = 80251;
-update creature_template set scale = 1.2 where entry = 80251;
 
 REPLACE INTO `creature_template` VALUES (80252, 0, 18005, 0, 0, 0, 'Young Arctic Fox', NULL, 0, 7, 8, 141, 161, 0, 0, 316, 38, 0, 1, 0.857143, 0, 20, 5, 0, 0, 1, 9, 13, 0, 58, 1, 2000, 2000, 1, 0, 0, 1, 0, 0, 0, 0, 13.4464, 18.4888, 100, 1, 1, 1131, 0, 1131, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5940, 0, 0, '', 1, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, '');
 
@@ -42,6 +38,11 @@ update creature_template set faction = 7 where entry = 80258;
 REPLACE INTO `creature_template` VALUES (80259, 3, 18004, 0, 0, 0, 'Mist Fox', NULL, 0, 50, 51, 2196, 2324, 0, 0, 2200, 1275, 0, 1, 2.57143, 0, 20, 5, 0, 0, 1, 161, 193, 0, 230, 1, 2000, 2000, 1, 0, 0, 1, 0, 0, 0, 0, 71.9664, 98.9538, 100, 1, 1, 10981, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 109810, 0, 0, 0, '', 1, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, 'npc_ram_wolf_tamed');
 
 update creature_template set faction = 7 where entry = 80259;
+
+REPLACE INTO `creature_template` VALUES (80260, 0, 18244, 0, 0, 0, 'Spirit Fox', NULL, 0, 53, 53, 3418, 3662, 0, 0, 3163, 14, 0, 1, 1.42857, 0, 20, 5, 0, 4, 1, 452, 584, 0, 238, 1, 1200, 1329, 1, 0, 0, 1, 0, 0, 0, 0, 57.4128, 78.9426, 100, 1, 1, 10077, 0, 0, 0, 100, 100, 100, 100, 100, 0, 0, 0, 0, 100770, 0, 0, 0, '', 1, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, '');
+
+update creature_template set movement_type = 1 where entry = 80260;
+update creature_template set scale = 1.2 where entry = 80260;
 
 -- Mount NPCs:
 
