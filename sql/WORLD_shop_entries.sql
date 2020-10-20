2deb9e875f053466af58bbb805e023edfc11df53
Torta <slowtorta@gmail.com>
2020-10-20 16:51:01 -0700

    Update WORLD_shop_entries.sql

--- sql/WORLD_shop_entries.sql
+++ sql/WORLD_shop_entries.sql
@@ -42,7 +42,6 @@ replace into shop_items values
 (110, 1, 50005, 'Field Repair Bot (Companion)', 160),
 (111, 1, 50007, 'Forworn Mule (Bank Companion)', 200),
 (112, 1, 51421, 'Caravan Kodo (Bank Companion)', 200),
-(113, 1, 51306, 'Fashion Coins x10 (Transmogrification)', 160),
 -- Pets ------------------------------------------------------------------------------------------------
 (201, 2, 13584, 'Diablo Stone', 60),
 (202, 2, 13583, 'Panda Collar', 60),
