34e39fa8e0cdaa1cf8529b770cb5050dbe565bfe
Torta <slowtorta@gmail.com>
2020-10-21 20:03:43 -0700

    Update WORLD_autobroadcast.sql

--- sql/WORLD_autobroadcast.sql
+++ sql/WORLD_autobroadcast.sql
@@ -7,13 +7,13 @@ replace into mangos_string (entry, content_default) values
 
 ('50002', 'If you want to help our project, check our shop for donation rewards: exotic mounts, large bags, mysterious engineering devices and much more: turtle-wow.org/donate'),
 
-('50003', '|cff0ccaec/join world|r to connect with the community around you! Keep up to date with the our news and join our Discord channel: discord.turtle-wow.org'),
+('50003', '/join world to connect with the community around you! Keep up to date with the our news and join our Discord channel: discord.turtle-wow.org'),
 
 ('50004', 'Speak to Fara Boltbreaker in Stormwind or Orgrimmar for a fast transfer to Shimmering Flats and participate to the real player controlled Mirage Raceway.'),
 
 ('50005', 'We\'ve implemented a list of features that Blizzard never published, you can find information about them on our website.'),
 
-('50006', '|cff0ccaec/join world|r to connect with the community around you! Keep up to date with the our news and join our Discord channel: discord.turtle-wow.org'),
+('50006', '/join world to connect with the community around you! Keep up to date with the our news and join our Discord channel: discord.turtle-wow.org'),
 
 ('50007', 'Speak to Karn Deepeye in Orgrimmar or Tarun Swifteagle in Stormwind to become a diplomat! You will be able to group and trade with the players of an opposite faction.'),
 
@@ -23,7 +23,7 @@ replace into mangos_string (entry, content_default) values
 
 ('50010', 'We\'re constantly working on improving the leveling experience on Turtle WoW: creating new quests for starting locations or extending the existing quest chains. If you have an idea for a new quest, please visit this page: dev.turtle-wow.org'),
 
-('50011', '|cff0ccaec/join world|r to connect with the community around you! Keep up to date with the Turtle WoW news and join our Discord channel: discord.turtle-wow.org'),
+('50011', 'join world to connect with the community around you! Keep up to date with the Turtle WoW news and join our Discord channel: discord.turtle-wow.org'),
 
 ('50012', 'Have you ever had a dream about setting your own garden? Grow your own food: plant the seed, water it, foster it, and enjoy the results of your hard work once it’s fully grown! Speak to Kern Mosshoof in Mulgore or find Mortimer Stansfield in Elwynn Forest.'),
 
