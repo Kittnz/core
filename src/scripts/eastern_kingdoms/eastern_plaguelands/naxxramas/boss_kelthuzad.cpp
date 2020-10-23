d86492e31dac21bb2e25b028fb7cfc5cfd476426
GrenderG <grenderg@gmail.com>
2020-10-23 16:34:43 +0200

    Fix Naxxramas text errors.

--- src/scripts/eastern_kingdoms/eastern_plaguelands/naxxramas/boss_kelthuzad.cpp
+++ src/scripts/eastern_kingdoms/eastern_plaguelands/naxxramas/boss_kelthuzad.cpp
@@ -581,7 +581,7 @@ struct boss_kelthuzadAI : public ScriptedAI
                     sLog.outBasic("(post)[%d] Spawn skele #%d, next in %dms", p1Timer, ++numSkeletons, nextBanshee);
                 }
 
-                DoScriptText(irand(SAY_AGGRO3, SAY_AGGRO1), m_creature);
+                DoScriptText(urand(SAY_AGGRO3, SAY_AGGRO1), m_creature);
                 m_creature->RemoveAurasDueToSpell(SPELL_VISUAL_CHANNEL);
                 DespawnAllIntroCreatures();
                 break;
