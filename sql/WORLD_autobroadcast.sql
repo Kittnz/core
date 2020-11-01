23e1255e86caa355851f6850749ff3acd043a59e
Torta <slowtorta@gmail.com>
2020-11-01 01:40:24 -0700

    Update WORLD_autobroadcast.sql

--- sql/WORLD_autobroadcast.sql
+++ sql/WORLD_autobroadcast.sql
@@ -7,13 +7,13 @@ replace into mangos_string (entry, content_default) values
 
 ('50002', 'If you want to help our project, check our shop for donation rewards: exotic mounts, large bags, mysterious engineering devices and much more: turtle-wow.org/donate'),
 
-('50003', '/join world to connect with the community around you! Keep up to date with the our news and join our Discord channel: discord.turtle-wow.org'),
+('50003', 'Join /world to connect with the community around you! Keep up to date with the our news and join our Discord channel: discord.turtle-wow.org'),
 
 ('50004', 'Speak to Fara Boltbreaker in Stormwind or Orgrimmar for a fast transfer to Shimmering Flats and participate to the real player controlled Mirage Raceway.'),
 
 ('50005', 'We\'ve implemented a list of features that Blizzard never published, you can find information about them on our website.'),
 
-('50006', '/join world to connect with the community around you! Keep up to date with the our news and join our Discord channel: discord.turtle-wow.org'),
+('50006', 'Join /world to connect with the community around you! Keep up to date with the our news and join our Discord channel: discord.turtle-wow.org'),
 
 ('50007', 'Speak to Karn Deepeye in Orgrimmar or Tarun Swifteagle in Stormwind to become a diplomat! You will be able to group and trade with the players of an opposite faction.'),
 
