10252bf6102618dfcc6e4f1280d16aa15eb7405c
Torta <slowtorta@gmail.com>
2020-11-01 01:38:43 -0700

    Revert "Update WORLD_autobroadcast.sql"
    This reverts commit 992597751454c8e7d008cc94931709ddd266b1da.

--- sql/WORLD_autobroadcast.sql
+++ sql/WORLD_autobroadcast.sql
@@ -7,13 +7,13 @@ replace into mangos_string (entry, content_default) values
 
 ('50002', 'All shop items are half the price on 23—31 October! Exotic mounts, large bags, mysterious engineering devices and more: turtle-wow.org/donate'),
 
-('50003', 'Join /world to connect with the community around you! Keep up to date with the our news and join our Discord channel: discord.turtle-wow.org'),
+('50003', '/join world to connect with the community around you! Keep up to date with the our news and join our Discord channel: discord.turtle-wow.org'),
 
 ('50004', 'Speak to Fara Boltbreaker in Stormwind or Orgrimmar for a fast transfer to Shimmering Flats and participate to the real player controlled Mirage Raceway.'),
 
 ('50005', 'All shop items are half the price on 23—31 October! Exotic mounts, large bags, mysterious engineering devices and more: turtle-wow.org/donate'),
 
-('50006', 'Join /world to connect with the community around you! Keep up to date with the our news and join our Discord channel: discord.turtle-wow.org'),
+('50006', '/join world to connect with the community around you! Keep up to date with the our news and join our Discord channel: discord.turtle-wow.org'),
 
 ('50007', 'Speak to Karn Deepeye in Orgrimmar or Tarun Swifteagle in Stormwind to become a diplomat! You will be able to group and trade with the players of an opposite faction.'),
 
@@ -23,7 +23,7 @@ replace into mangos_string (entry, content_default) values
 
 ('50010', 'All shop items are half the price on 23—31 October! Exotic mounts, large bags, mysterious engineering devices and more: turtle-wow.org/donate'),
 
-('50011', 'Join /world to connect with the community around you! Keep up to date with the Turtle WoW news and join our Discord channel: discord.turtle-wow.org'),
+('50011', '/join world to connect with the community around you! Keep up to date with the Turtle WoW news and join our Discord channel: discord.turtle-wow.org'),
 
 ('50012', 'Have you ever had a dream about setting your own garden? Grow your own food: plant the seed, water it, foster it, and enjoy the results of your hard work once it’s fully grown! Speak to Kern Mosshoof in Mulgore or find Mortimer Stansfield in Elwynn Forest.'),
 
