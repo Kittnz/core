c4e83714256bb1e6edad6eedeaad516fb593cc5f
GrenderG <grenderg@gmail.com>
2020-11-16 16:46:51 +0100

    Fix Chromie crash.

--- src/scripts/turtlewow/creatures/cot/black_morass/boss_chromie.cpp
+++ src/scripts/turtlewow/creatures/cot/black_morass/boss_chromie.cpp
@@ -26,6 +26,8 @@ struct boss_chromieAI : public ScriptedAI
         Reset();
     }
 
+    ObjectGuid m_uiChronormuGuid;
+
     uint32 m_uiTeleportTimer;
     uint32 m_uiAoESleepTimer;
     uint32 m_uiLightningCloudTimer;
@@ -33,7 +35,6 @@ struct boss_chromieAI : public ScriptedAI
     uint32 m_uiChronormuTimer;
     uint32 m_uiChronormuCombatStartTimer;
 
-    Creature* chronormu;
     bool chronormuSummoned = false;
     bool chronormuCombatStarted = false;
     bool stopCasting = false;
@@ -61,6 +62,8 @@ struct boss_chromieAI : public ScriptedAI
         m_uiSecondPhaseDialog1 = 8000;
         m_uiSecondPhaseDialog2 = 24000;
         m_uiSecondPhaseDialog3 = 40000;
+
+        m_uiChronormuGuid = 0;
     }
 
     void JustRespawned()
@@ -74,12 +77,6 @@ struct boss_chromieAI : public ScriptedAI
         chronormuCombatStarted = false;
         stopCasting = false;
         isFriendly = false;
-        if (chronormu)
-        {
-            chronormu->DespawnOrUnsummon();
-            chronormu->ForcedDespawn();
-            chronormu = nullptr;
-        }
     }
 
     void Aggro(Unit *who)
@@ -97,7 +94,8 @@ struct boss_chromieAI : public ScriptedAI
 
     }
 
-    void LeaveCombat() {
+    void LeaveCombat()
+    {
         m_creature->ClearTarget();
         m_creature->ClearInCombat();
         m_creature->CombatStop(true);
@@ -105,7 +103,26 @@ struct boss_chromieAI : public ScriptedAI
         m_creature->UpdateCombatWithZoneState(false);
     }
 
-    void UpdateAI(const uint32 diff) {
+    void JustSummoned(Creature* summoned)
+    {
+        if (summoned->GetEntry() == CREATURE_CHRONORMU)
+        {
+            m_uiChronormuGuid = summoned->GetObjectGuid();
+
+            summoned->setFaction(35);
+            summoned->ClearTarget();
+            summoned->AI()->SetCombatMovement(false);
+            summoned->CastSpell(summoned, SPELL_TWIN_TELEPORT_VISUAL, true);
+            m_creature->SetFacingTo(m_creature->GetAngle(old_x, old_y));
+
+            summoned->PMonsterYell("You have failed me, my puppet!");
+
+            chronormuSummoned = true;
+        }
+    }
+
+    void UpdateAI(const uint32 diff)
+    {
         if (m_creature->GetHealthPercent() <= 25 && !isFriendly)
         {
             isFriendly = true;
@@ -138,52 +155,60 @@ struct boss_chromieAI : public ScriptedAI
 
             if (m_uiChronormuTimer < diff && !chronormuSummoned)
             {
-                chronormuSummoned = true;
+                m_creature->SummonCreature(CREATURE_CHRONORMU, old_x, old_y, old_z, 0,
+                        TEMPSUMMON_CORPSE_TIMED_DESPAWN, 300000);
 
-                chronormu = m_creature->SummonCreature(CREATURE_CHRONORMU, old_x, old_y,
-                                                                 old_z, 0, TEMPSUMMON_CORPSE_TIMED_DESPAWN, 300000);
-
-                chronormu->setFaction(35);
-                chronormu->ClearTarget();
-                chronormu->AI()->SetCombatMovement(false);
-                chronormu->CastSpell(chronormu, SPELL_TWIN_TELEPORT_VISUAL, true);
-                m_creature->SetFacingTo(m_creature->GetAngle(old_x, old_y));
-
-                chronormu->PMonsterYell("You have failed me, my puppet!");
             } else m_uiChronormuTimer -= diff;
 
             if (chronormuSummoned)
             {
-                if (m_uiSecondPhaseDialog1 < diff && !saidDialog1) {
-                    saidDialog1 = true;
-                    chronormu->SetFacingToObject(m_creature);
-                    chronormu->PMonsterSay("Your time here has come to an end, mortals. The dragonflights won't tolerate living in a world inhabited by such evil, greedy and cynical beings. I ask you again, \"Chromie\", why don't you join me? Why don't you join your wiser self?");
+                if (m_uiSecondPhaseDialog1 < diff && !saidDialog1)
+                {
+                    if (Creature* chronormu = m_creature->GetMap()->GetCreature(m_uiChronormuGuid))
+                    {
+                        saidDialog1 = true;
+                        chronormu->SetFacingToObject(m_creature);
+                        chronormu->PMonsterSay(
+                                "Your time here has come to an end, mortals. The dragonflights won't tolerate living in a world inhabited by such evil, greedy and cynical beings. I ask you again, \"Chromie\", why don't you join me? Why don't you join your wiser self?");
+                    }
                 } else m_uiSecondPhaseDialog1 -= diff;
 
-                if (m_uiSecondPhaseDialog2 < diff && !saidDialog2) {
-                    saidDialog2 = true;
-                    m_creature->PMonsterSay("NEVER! You are set on a final road, one ending timeline, while my hearth still throbs with the hopes of tomorrow. The Sands of Time flows different, shifting day by day, with every choice, yet yours is an never ending sea of still sand. These mortals, with every choice and every action they do they give birth to another future, I rather part of theirs than wither away with you.");
+                if (m_uiSecondPhaseDialog2 < diff && !saidDialog2)
+                {
+                    if (Creature* chronormu = m_creature->GetMap()->GetCreature(m_uiChronormuGuid))
+                    {
+                        saidDialog2 = true;
+                        m_creature->PMonsterSay(
+                                "NEVER! You are set on a final road, one ending timeline, while my hearth still throbs with the hopes of tomorrow. The Sands of Time flows different, shifting day by day, with every choice, yet yours is an never ending sea of still sand. These mortals, with every choice and every action they do they give birth to another future, I rather part of theirs than wither away with you.");
+                    }
                 } else m_uiSecondPhaseDialog2 -= diff;
 
                 if (m_uiSecondPhaseDialog3 < diff && !saidDialog3)
                 {
-                    saidDialog3 = true;
-                    chronormu->PMonsterYell("How pitful... I truly believed you'd understand. Come then, mortals, witness your own demise. WITNESS THE CREATION OF A NEW ORCISH ARMY!");
+                    if (Creature* chronormu = m_creature->GetMap()->GetCreature(m_uiChronormuGuid))
+                    {
+                        saidDialog3 = true;
+                        chronormu->PMonsterYell(
+                                "How pitful... I truly believed you'd understand. Come then, mortals, witness your own demise. WITNESS THE CREATION OF A NEW ORCISH ARMY!");
+                    }
                 } else m_uiSecondPhaseDialog3 -= diff;
 
                 if (m_uiChronormuCombatStartTimer < diff && !chronormuCombatStarted)
                 {
-                    m_creature->PMonsterYell("Now, heroes! Attack now! I'll contain Chronormu's strength!");
+                    if (Creature* chronormu = m_creature->GetMap()->GetCreature(m_uiChronormuGuid))
+                    {
+                        m_creature->PMonsterYell("Now, heroes! Attack now! I'll contain Chronormu's strength!");
 
-                    chronormuCombatStarted = true;
-                    m_creature->RemoveAllAuras();
+                        chronormuCombatStarted = true;
+                        m_creature->RemoveAllAuras();
 
-                    DoCast(m_creature, SPELL_GREEN_CHANNELING, true);
-                    chronormu->SetObjectScale(0.75);
-                    chronormu->AddAura(SPELL_PARTICLES_GREEN);
+                        DoCast(m_creature, SPELL_GREEN_CHANNELING, true);
+                        chronormu->SetObjectScale(0.75);
+                        chronormu->AddAura(SPELL_PARTICLES_GREEN);
 
-                    chronormu->setFaction(38);
-                    chronormu->AI()->SetCombatMovement(true);
+                        chronormu->setFaction(38);
+                        chronormu->AI()->SetCombatMovement(true);
+                    }
                 } else m_uiChronormuCombatStartTimer -= diff;
             }
         }
