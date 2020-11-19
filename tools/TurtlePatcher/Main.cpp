feb3978855c160307bbe08249f85a6f1e200f316
Torta <slowtorta@gmail.com>
2020-11-18 22:36:49 -0800

    Kill patcher's confirmation dialog.

--- tools/TurtlePatcher/Main.cpp
+++ tools/TurtlePatcher/Main.cpp
@@ -223,14 +223,6 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLin
 {
 	gHInstance = hInstance;
 
-#ifndef _DEBUG
-	int UserResponse = MessageBox(NULL, "This program will patch your WoW 1.12.1 to Turtle version\n\nContinue?", "Warning", MB_YESNO | MB_ICONEXCLAMATION);
-	if (UserResponse != IDYES)
-	{
-		return 0;
-	}
-#endif
-
 	fs::path currentPath = fs::current_path();
 	fs::path PatchDir = currentPath / "wow-patch.mpq";
 
