3a56025d0c113f774275fbc508e161313fc72293
Torta <slowtorta@gmail.com>
2020-12-11 21:04:04 -0800

    Minor DB fixes.

--- sql/WORLD_UPD_expansion_03.sql
+++ sql/WORLD_UPD_expansion_03.sql
@@ -2,3 +2,5 @@
 
 delete from playercreateinfo_spell where race = 9 and class = 3;
 replace into playercreateinfo_spell values (9, 3, 266, 0, 5875, 'Guns');
+
+replace into broadcast_text (ID, MaleText) values (80601, 'Hey, if you aren\'t spending coins, you might as well get out. You\'re bothering my real customers.');
