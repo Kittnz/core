c2e312926b3e3848324b01fc1d88f232ce7b234b
Torta <slowtorta@gmail.com>
2020-12-18 20:49:35 -0800

    Right spell ID for Amani Eagle.

--- sql/WORLD_UPD_expansion_05.sql
+++ sql/WORLD_UPD_expansion_05.sql
@@ -2,6 +2,8 @@ update gameobject_template set displayid = 29624, size = 0.8 where entry = 30002
 delete from gameobject where guid = 4004347;
 update gameobject_template set displayid = 22748, size = 1 where entry = 3000204;
 
+update item_template set spellid_1 = 46033 where entry = 80878;
+
 -- Revantusk Trolls Armor Quartermaster
 
 REPLACE INTO `creature_template` VALUES (80915, 0, 14768, 0, 0, 0, 'Tan\'Pogo', 'Revantusk Trolls Armor Quartermaster', 0, 11, 11, 456, 456, 0, 0, 20, 1494, 16388, 1.1, 1.14286, 0, 20, 5, 0, 0, 1, 12, 14, 0, 64, 1, 2000, 2090, 1, 4864, 0, 0, 0, 0, 0, 0, 18.392, 25.289, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 524298, '');
