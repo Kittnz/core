24906878803b7fa2e34df91d91abb19a5021d090
Torta <slowtorta@gmail.com>
2021-01-02 00:24:34 -0800

    Penus!

--- tools/TurtlePatcher/Main.cpp
+++ tools/TurtlePatcher/Main.cpp
@@ -12,7 +12,7 @@
 #define fs std::filesystem
 
 //#TODO: Read from config blob at the end of the file
-#define NEW_BUILD 5940u
+#define NEW_BUILD 5950u
 #define NEW_VISUAL_BUILD "5950"
 #define NEW_VISUAL_VERSION "1.14.6"
 #define NEW_BUILD_DATE "Jan 1 2021"
