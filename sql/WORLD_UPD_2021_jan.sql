522d0ff7fffc3ab467782224a6a5ffbbbf0b377f
GrenderG <grenderg@gmail.com>
2021-01-06 23:18:11 +0100

    Remove Songflower in STV.

--- sql/WORLD_UPD_2021_jan.sql
+++ sql/WORLD_UPD_2021_jan.sql
@@ -1,3 +1,4 @@
 update creature_template set gold_min = 24055, gold_max = 31418 where entry = 15544;
 delete from spell_effect_mod where Id = 26093;
 update areatrigger_teleport set required_level = 60 where id in (4008, 4010);
+delete from gameobject where guid = 4007494;
