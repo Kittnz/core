c61fed622e5867a305c97c5e96a72d311182b6b2
GrenderG <grenderg@gmail.com>
2021-01-06 23:54:57 +0100

    Remove faction requirements from AD tabard.

--- sql/WORLD_UPD_2021_jan.sql
+++ sql/WORLD_UPD_2021_jan.sql
@@ -2,3 +2,4 @@ update creature_template set gold_min = 24055, gold_max = 31418 where entry = 15
 delete from spell_effect_mod where Id = 26093;
 update areatrigger_teleport set required_level = 60 where id in (4008, 4010);
 delete from gameobject where guid = 4007494;
+update item_template set required_reputation_faction = 0, required_reputation_rank = 0 where entry = 22999;
