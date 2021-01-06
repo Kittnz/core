7c0f6748ac9d784b8ef26cbb6159a82b8f220f48
Torta <slowtorta@gmail.com>
2021-01-06 15:04:23 -0800

    Spells + item fixes.

---
+++ sql/WORLD_UPD_expansion_hotfixes.sql
@@ -0,0 +1,3 @@
+update item_template set inventory_type = 14, class = 4, subclass = 6 where entry = 80543;
+update item_template set inventory_type = 13 where entry = 80635;
+update item_template set name = 'Codex: Divine Sanction' where entry = 81012;
