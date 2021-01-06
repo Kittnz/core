9431563815de977b5df414efadecad2fac96024e
GrenderG <grenderg@gmail.com>
2021-01-06 17:28:50 +0100

    Fix AQ portal.

--- sql/WORLD_UPD_2021_jan.sql
+++ sql/WORLD_UPD_2021_jan.sql
@@ -1,2 +1,3 @@
 update creature_template set gold_min = 24055, gold_max = 31418 where entry = 15544;
 delete from spell_effect_mod where Id = 26093;
+update areatrigger_teleport set required_level = 60 where id in (4008, 4010);
