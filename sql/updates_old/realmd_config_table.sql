6123d11791a8642e27cc2cf9e58372a6edcae81a
GrenderG <grenderg@gmail.com>
2021-01-07 21:10:24 +0100

    Add config table to realm struct.

--- sql/updates_old/realmd_config_table.sql
+++ sql/updates_old/realmd_config_table.sql
@@ -1,9 +1,7 @@
-CREATE TABLE `config` (
+CREATE TABLE IF NOT EXISTS `config` (
   `id` INT NOT NULL,
   `value` VARCHAR(45) NULL,
+  `comment` VARCHAR(255) NULL,
   PRIMARY KEY (`id`));
-
-ALTER TABLE `config` 
-ADD COLUMN `comment` VARCHAR(45) NULL AFTER `value`;
 
 INSERT INTO `config` (`id`, `value`, `comment`) VALUES ('1', '2048', 'Patch download speed limit. Number of Kbytes per client');
