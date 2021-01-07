e60256e4a89c1d50ebee985bd2dfc1c5c91049e7
Torta <slowtorta@gmail.com>
2021-01-06 19:00:47 -0800

    New DBCs + old world spirit healers.

--- sql/WORLD_UPD_expansion_hotfixes.sql
+++ sql/WORLD_UPD_expansion_hotfixes.sql
@@ -6,3 +6,5 @@ update item_template set allowable_race = -1 where entry = 19377;
 update item_template set inventory_type = 17, class = 2, subclass = 8 where entry = 80534;
 
 replace into item_template values (81098, 10, 4, 0, 'Revantusk Tabard', '', 50637, 1, 0, 1, 50000, 12500, 19, -1, -1, 1, 0, 0, 0, 0, 0, 0, 893, 7, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 1, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, null);
+
+update `item_template` set `spellid_1`='0', `spelltrigger_1`='0' where (`entry`='50440') and (`patch`='0');
