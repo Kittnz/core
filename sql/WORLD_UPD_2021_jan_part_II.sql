05760f61d8d227fafa461668a896556af1ee68e4
Torta <slowtorta@gmail.com>
2021-01-14 01:52:58 -0800

    Documented some questionable query examples.

--- sql/WORLD_UPD_2021_jan_part_II.sql
+++ sql/WORLD_UPD_2021_jan_part_II.sql
@@ -2,15 +2,15 @@ update item_template set allowable_race = -1 where entry in (5102, 4898, 4897);
 
 delete from playercreateinfo_spell where race = 9 and class = 3 and spell = 266;
 delete from playercreateinfo_spell where race = 10 and class = 3 and spell = 266;
-insert into playercreateinfo_spell values
-(9, 3, 266, 0, 5875, 'Guns'),
-(10, 3, 266, 0, 5875, 'Guns');
+-- insert into playercreateinfo_spell values
+-- (9, 3, 266, 0, 5875, 'Guns'),
+-- (10, 3, 266, 0, 5875, 'Guns');
 
 delete from playercreateinfo_spell where race = 9 and class = 3 and spell = 5011;
 delete from playercreateinfo_spell where race = 10 and class = 3 and spell = 5011;
-insert into playercreateinfo_spell values
-(9, 3, 5011, 0, 5875, 'Crossbows'),
-(10, 3, 5011, 0, 5875, 'Crossbows');
+-- insert into playercreateinfo_spell values
+--(9, 3, 5011, 0, 5875, 'Crossbows'),
+--(10, 3, 5011, 0, 5875, 'Crossbows');
 
 -- NOT NEEDED FOR HUNTERS! They use Auto Shop for everything.
 
@@ -25,6 +25,17 @@ insert into playercreateinfo_spell values
 -- insert into playercreateinfo_spell values
 -- (9, 3, 7919, 0, 5875, 'Shoot Crossbow'),
 -- (10, 3, 7919, 0, 5875, 'Shoot Crossbow');
+
+-- EXAMPLE QUERY:
+
+-- INSERT IGNORE INTO character_skills_copy (guid, skill, value, max)
+-- Insert data into table and ignore duplicates
+-- SELECT guid, 266, 1, 150
+-- Select ONLY the guid from characters and enter the other values manually
+-- FROM characters
+-- Table where we pull the GUID from
+-- WHERE race = 10 AND class = 3;
+-- We want only race 10 and class 3
 
 update quest_template set method = 1 where entry in (50300, 50301, 50302, 50303);
 
