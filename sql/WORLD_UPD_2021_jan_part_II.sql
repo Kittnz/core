a9bcff27c8751357ffa8513314ccc8e207c65c97
Torta <slowtorta@gmail.com>
2021-01-13 17:43:10 -0800

    More missing skills to a character creation.

--- sql/WORLD_UPD_2021_jan_part_II.sql
+++ sql/WORLD_UPD_2021_jan_part_II.sql
@@ -9,5 +9,17 @@ insert into playercreateinfo_spell values
 delete from playercreateinfo_spell where race = 9 and class = 3 and spell = 5011;
 delete from playercreateinfo_spell where race = 10 and class = 3 and spell = 5011;
 insert into playercreateinfo_spell values
-(9, 3, 5011, 0, 5875, 'Guns'),
-(10, 3, 5011, 0, 5875, 'Guns');
+(9, 3, 5011, 0, 5875, 'Crossbows'),
+(10, 3, 5011, 0, 5875, 'Crossbows');
+
+delete from playercreateinfo_spell where race = 9 and class = 3 and spell = 7918;
+delete from playercreateinfo_spell where race = 10 and class = 3 and spell = 7918;
+insert into playercreateinfo_spell values
+(9, 3, 7918, 0, 5875, 'Shoot Gun'),
+(10, 3, 7918, 0, 5875, 'Shoot Gun');
+
+delete from playercreateinfo_spell where race = 9 and class = 3 and spell = 7919;
+delete from playercreateinfo_spell where race = 10 and class = 3 and spell = 7919;
+insert into playercreateinfo_spell values
+(9, 3, 7919, 0, 5875, 'Shoot Crossbow'),
+(10, 3, 7919, 0, 5875, 'Shoot Crossbow');
