0f7c0dbf32223b66a9e638db65e12a51a5fca52d
Torta <slowtorta@gmail.com>
2021-01-13 16:44:51 -0800

    Teach HE/GO hunters bows and crossbows on character creation.

--- sql/WORLD_UPD_2021_jan_part_II.sql
+++ sql/WORLD_UPD_2021_jan_part_II.sql
@@ -1,1 +1,13 @@
 update item_template set allowable_race = -1 where entry in (5102, 4898, 4897);
+
+delete from playercreateinfo_spell where race = 9 and class = 3 and spell = 266;
+delete from playercreateinfo_spell where race = 10 and class = 3 and spell = 266;
+insert into playercreateinfo_spell values
+(9, 3, 266, 0, 5875, 'Guns'),
+(10, 3, 266, 0, 5875, 'Guns');
+
+delete from playercreateinfo_spell where race = 9 and class = 3 and spell = 5011;
+delete from playercreateinfo_spell where race = 10 and class = 3 and spell = 5011;
+insert into playercreateinfo_spell values
+(9, 3, 5011, 0, 5875, 'Guns'),
+(10, 3, 5011, 0, 5875, 'Guns');
