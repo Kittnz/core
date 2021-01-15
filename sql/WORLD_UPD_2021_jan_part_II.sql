91f42bb2722829d5f9382e80a265574351f6e186
GrenderG <grenderg@gmail.com>
2021-01-15 01:29:56 +0100

    Lake Toads will no longer be humanoids. :D

--- sql/WORLD_UPD_2021_jan_part_II.sql
+++ sql/WORLD_UPD_2021_jan_part_II.sql
@@ -30,4 +30,4 @@ update skill_line_ability set race_mask = 434 where id = 5991;
 update skill_line_ability set race_mask = 434 where id = 5993;
 update skill_line_ability set race_mask = 434 where id = 5994;
 
-update creature_template set faction = 7, equipment_id = 0, gold_min = 0, gold_max = 0 where entry = 80965;
+update creature_template set faction = 7, equipment_id = 0, gold_min = 0, gold_max = 0, type = 10 where entry = 80965;
