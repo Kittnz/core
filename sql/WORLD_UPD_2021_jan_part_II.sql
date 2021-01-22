52249e4ead63dd666f53cd5717fdb2138896dfdd
Torta <slowtorta@gmail.com>
2021-01-21 16:35:00 -0800

    Minor fix for Arikara summon.

--- sql/WORLD_UPD_2021_jan_part_II.sql
+++ sql/WORLD_UPD_2021_jan_part_II.sql
@@ -465,3 +465,11 @@ replace into npc_vendor (entry, item) values
 (50053, 80657),
 (50053, 80658),
 (50053, 80659);
+
+
+-- https://www.youtube.com/watch?v=dn3sVUahDAQ
+DELETE FROM `event_scripts` WHERE `id`=175944;
+INSERT INTO `event_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (175944, 5, 10, 10882, 30000, 1, 30, 0, 0, 0, 0, 8, 0, 6, 1, -5002.889, -2103.455, 83.81, 1.365, 0, 'Sacred Fire of Life - Summon Arikara');
+UPDATE `gameobject_template` SET `data2`=175944, `script_name`='' WHERE `entry`=175944;
+UPDATE `creature_template` SET `faction`=14 WHERE `entry`=10882;
+
