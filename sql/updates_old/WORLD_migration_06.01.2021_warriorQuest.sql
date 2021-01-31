1c3a9c688114724309040ddaed24fbcad959ce76
Torta <slowtorta@gmail.com>
2021-01-30 22:07:51 -0800

    New World DB release.

--- sql/updates_old/WORLD_migration_06.01.2021_warriorQuest.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO `creature_questrelation` (`id`, `quest`, `patch_min`, `patch_max`) VALUES ('5480', '1638', '0', '10');
--- sql/updates_old/realmd_config_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `config` (
-  `id` INT NOT NULL,
-  `value` VARCHAR(45) NULL,
-  `comment` VARCHAR(255) NULL,
-  PRIMARY KEY (`id`));
-
-INSERT INTO `config` (`id`, `value`, `comment`) VALUES ('1', '2048', 'Patch download speed limit. Number of Kbytes per client');
