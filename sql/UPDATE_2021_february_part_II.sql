804c8da0356aec4a2deefe2bba1432c8975e2bd1
Torta <slowtorta@gmail.com>
2021-02-10 22:54:37 -0800

    Added Steamwheedle Limited Edition Wormhole.

--- sql/UPDATE_2021_february_part_II.sql
+++ sql/UPDATE_2021_february_part_II.sql
@@ -1,1 +1,12 @@
--- 
+-- Steamwheedle Beach Experience teleportation stone, limited edition.
+replace into item_template values
+ ('81140', '0', '4', '0', 'Steamwheedle Limited Edition Wormhole', 'Takes you to the Steamwheedle Beach Experience. Notice: Upon use, the user consents that The Steamwheedle Cartel is not responsible for device malfunctions.', '31199', '1', '0', '1', '0', '0', '12', '-1', '-1', '1',
+ '0', '0', '0', '0', '0', '0', '0', '0', '1', '1', '0', '0', '0', '0', '0',
+ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '0', '0', '31726', '0', '0', '0', '-1', '10', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0',
+ '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0',
+ '-1', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '1', 'item_cenarion_hold_teleport');
+ 
+ UPDATE `item_template` SET `duration`='604800' WHERE (`entry`='81140') AND (`patch`='0');
