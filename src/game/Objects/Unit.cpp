68c02b2dab825aceb6a9ce5f5d5445e05b722c7a
Torta <slowtorta@gmail.com>
2021-02-15 11:28:22 -0800

    Clear power on death.
    https://github.com/vmangos/core/commit/47ec0fcf4f348c8ca7b244c040f4ed3266d827a1

--- src/game/Objects/Unit.cpp
+++ src/game/Objects/Unit.cpp
@@ -8619,6 +8619,9 @@ void Unit::SetDeathState(DeathState s)
         i_motionMaster.MoveIdle();
         StopMoving(true);
 
+        // Powers are cleared on death.
+        SetPower(getPowerType(), 0);
+
         ModifyAuraState(AURA_STATE_HEALTHLESS_20_PERCENT, false);
         ModifyAuraState(AURA_STATE_HEALTHLESS_15_PERCENT, false);
         ModifyAuraState(AURA_STATE_HEALTHLESS_10_PERCENT, false);
