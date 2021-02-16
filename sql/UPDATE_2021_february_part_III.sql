51746061963c6d3a8ad800b20333fede61923c48
Torta <slowtorta@gmail.com>
2021-02-15 17:05:56 -0800

    Updated loot table of Gurubashi Arena Chest.

---
+++ sql/UPDATE_2021_february_part_III.sql
@@ -0,0 +1,6 @@
+-- Updated loottable of Gurubashi Arena Chest:
+
+replace into gameobject_loot_template values (16718, 80845, 4.3, 0, 1, 1, 0, 0, 10);
+replace into gameobject_loot_template values (16718, 80846, 4.5, 0, 1, 1, 0, 0, 10);
+replace into gameobject_loot_template values (16718, 80847, 4.2, 0, 1, 1, 0, 0, 10);
+replace into gameobject_loot_template values (16718, 80848, 4.7, 0, 1, 1, 0, 0, 10);
