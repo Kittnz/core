af5b4729d24438d7e3f37f5764bddcdd9b77c288
Torta <slowtorta@gmail.com>
2021-03-02 00:00:17 -0800

    New quest: A gizmo a day… (Horde).

--- sql/UPDATE_2021_march.sql
+++ sql/UPDATE_2021_march.sql
@@ -43,8 +43,7 @@ update creature_template set display_id1 = 18315 where entry = 80306;
 update creature_template set display_id1 = 18316 where entry = 80307;
 update creature_template set display_id1 = 18317 where entry = 80308;
 update creature_template set display_id1 = 18318 where entry = 80305;
-
--- Gnome faction rewards (mechaspiders)(todo)
+update creature_template set display_id1 = 15236, npc_flags = 7 where entry = 80961;
 
 -- Cleanup useless items: 
 
@@ -60,3 +59,80 @@ replace into `item_template` values (11132, 0, 12, 0, 'Unused Scraping Vial', ''
 update item_template set quality = 4 where entry = 50094;
 update item_template set quality = 4 where entry = 50243;
 update item_template set quality = 4 where entry = 51838;
+
+-- Durotar Labor Union reputation repeatable quest:
+
+set @quest_entry = 80386;
+set @quest_zone = 15;
+set @title = 'A gizmo a day…';
+set @description = 'I never thought we’d have a fresh start, and not in my wildest dreams I’d think we’d do that under the Horde’s flag, but what do you know, here we are.\n\nWe currently are a small group of goblins that ran away with only scraps and dust in our pockets, but slowly we will make a name for ourselves, and if you want, you can be part of that.\n\nThe Labor Union grows day by day, goblins come our way to find a new meaning in life under the Horde’s gaze, but we lack both means and resources.\n\nIf you wish to be part of the Union’s future bring me a handful of Whirring Bronze Gizmos whenever you can. You will eventually get paid.';
+set @objective = 'Bring 20 Whirring Bronze Gizmos to Vanessa Clang in Mudsprocket.';
+set @completetext = 'Your help is appreciated, sugar, keep them coming!';
+set @incompletetext = 'Got any of them ready yet?';
+set @faction_id = 1001;
+set @faction_count = 100;
+set @xp_or_money = 465;
+set @reward_money = 0; 
+set @quest_level = 60;
+set @min_level = 10;
+set @questgiver_id = 80961;
+set @quest_finisher = 80961;
+set @nextquest = 0;
+set @nextquestinchain = 0;
+set @prevquest = 0;
+set @RewChoiceItemId1 = 0; 
+set @RewChoiceItemId2 = 0; 
+set @RewChoiceItemId3 = 0;
+set @RewChoiceItemId4 = 0; 
+set @RewChoiceItemCount1 = 0;
+set @RewChoiceItemCount2 = 0;
+set @RewChoiceItemCount3 = 0;
+set @RewChoiceItemCount4 = 0;
+set @reward_item_1 = 0;
+set @reward_item_2 = 0; 
+set @reward_item_3 = 0;
+set @reward_item_4 = 0;
+set @reward_item_1_count = 0;
+set @reward_item_2_count = 0;
+set @reward_item_3_count = 0;
+set @reward_item_4_count = 0;
+set @creature_to_kill_1 = 0;
+set @creature_to_kill_2 = 0; 
+set @creature_to_kill_3 = 0;
+set @creature_to_kill_4 = 0;
+set @creature_to_kill_1_count = 0;
+set @creature_to_kill_2_count = 0;
+set @creature_to_kill_3_count = 0;
+set @creature_to_kill_4_count = 0;
+set @required_item_1 = 4375; 
+set @required_item_2 = 0;
+set @required_item_3 = 0;
+set @required_item_4 = 0;
+set @required_item_1_count = 20;
+set @required_item_2_count = 0;
+set @required_item_3_count = 0;
+set @required_item_4_count = 0;
+replace into quest_template values 
+(@quest_entry, '0', '2', @quest_zone, @min_level,  '0', @quest_level, '0', '0', '0', '0', '0', '0', '0', '0', '0','0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', @title, @description, @objective, @completetext, @incompletetext, '', '', '', '', '', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', @faction_id, '0', '0', '0', '0', @faction_count, '0', '0', '0', '0', '0', @xp_or_money, '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0','0', 0, 0);
+replace into creature_questrelation (id, quest, patch_min, patch_max) values (@questgiver_id, @quest_entry,'0','10'); 
+replace into creature_involvedrelation (id, quest, patch_min, patch_max) values (@quest_finisher, @quest_entry,'0','10');
+update quest_template set 
+rewitemid1 = @reward_item_1, rewitemcount1 = @reward_item_1_count,
+rewitemid2 = @reward_item_2, rewitemcount2 = @reward_item_2_count,
+rewitemid3 = @reward_item_3, rewitemcount3 = @reward_item_3_count,
+rewitemid4 = @reward_item_4, rewitemcount4 = @reward_item_4_count,
+RewChoiceItemId1 = @RewChoiceItemId1, RewChoiceItemCount1 = @RewChoiceItemCount1,
+RewChoiceItemId2 = @RewChoiceItemId2, RewChoiceItemCount2 = @RewChoiceItemCount2,
+RewChoiceItemId3 = @RewChoiceItemId3, RewChoiceItemCount3 = @RewChoiceItemCount3,
+RewChoiceItemId4 = @RewChoiceItemId4, RewChoiceItemCount4 = @RewChoiceItemCount4,
+ReqCreatureOrGOId1 = @creature_to_kill_1, ReqCreatureOrGOCount1 = @creature_to_kill_1_count,
+ReqCreatureOrGOId2 = @creature_to_kill_2, ReqCreatureOrGOCount2 = @creature_to_kill_2_count,
+ReqCreatureOrGOId3 = @creature_to_kill_3, ReqCreatureOrGOCount3 = @creature_to_kill_3_count,
+ReqCreatureOrGOId4 = @creature_to_kill_4, ReqCreatureOrGOCount4 = @creature_to_kill_4_count,
+reqitemid1 = @required_item_1, reqitemcount1 = @required_item_1_count,
+reqitemid2 = @required_item_2, reqitemcount2 = @required_item_2_count,
+reqitemid3 = @required_item_3, reqitemcount3 = @required_item_3_count,
+reqitemid4 = @required_item_4, reqitemcount4 = @required_item_4_count,
+nextquestid = @nextquest, RewOrReqMoney = @reward_money, 
+nextquestinchain = @nextquestinchain, prevquestid = @prevquest,
+specialflags = 1, requiredraces = 434, incompleteemote = 1, completeemote = 1, offerrewardemote1 = 4, offerrewardemote2 = 1  where entry = @quest_entry;	
