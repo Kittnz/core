75a1ec3d436b2ba763583f4c68e810e58f1625dd
Torta <slowtorta@gmail.com>
2021-03-15 13:06:30 -0800

    Archieved updates.

--- sql/UPDATE_object_scaling_table.sql
+++
@@ -1,7 +0,0 @@
--- object scaling table
-
-CREATE TABLE `object_scaling` (
-	`fullGuid` BIGINT(20) UNSIGNED NOT NULL,
-	`scale` FLOAT(12) NOT NULL DEFAULT '0',
-	PRIMARY KEY (`fullGuid`) USING BTREE
-)
