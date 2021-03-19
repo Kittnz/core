856f3041028d25abe95ab58c7674a0e2863901c4
Torta <slowtorta@gmail.com>
2021-03-19 00:23:36 -0800

    Probably fix for 86 [AQ War Effort (post-event)].

--- sql/UPDATE_2021_march.sql
+++ sql/UPDATE_2021_march.sql
@@ -736,3 +736,90 @@ replace into item_template values
  '0', '1', NULL);
  
  REPLACE INTO `creature_template` VALUES (80967, 0, 18135, 0, 0, 0, 'Lord Darkhallow', 'Scourge Quartermaster', 0, 11, 11, 456, 456, 0, 0, 20, 21, 16388, 1.1, 1.14286, 0, 20, 5, 0, 0, 1, 12, 14, 0, 64, 1, 2000, 2090, 1, 4864, 0, 0, 0, 0, 0, 0, 18.392, 25.289, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 524298, '');
+
+
+-- ----------------------------
+-- Table structure for `variables`
+-- ----------------------------
+DROP TABLE IF EXISTS `variables`;
+CREATE TABLE `variables` (
+  `index` int(10) unsigned NOT NULL DEFAULT 0,
+  `value` int(10) unsigned NOT NULL DEFAULT 0,
+  PRIMARY KEY (`index`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of variables
+-- ----------------------------
+INSERT INTO `variables` VALUES ('12', '1595378403');
+INSERT INTO `variables` VALUES ('13', '1595378403');
+INSERT INTO `variables` VALUES ('10', '440');
+INSERT INTO `variables` VALUES ('11', '4');
+INSERT INTO `variables` VALUES ('14', '0');
+INSERT INTO `variables` VALUES ('15', '0');
+INSERT INTO `variables` VALUES ('16', '0');
+INSERT INTO `variables` VALUES ('17', '0');
+INSERT INTO `variables` VALUES ('18', '0');
+INSERT INTO `variables` VALUES ('19', '0');
+INSERT INTO `variables` VALUES ('20', '0');
+INSERT INTO `variables` VALUES ('21', '0');
+INSERT INTO `variables` VALUES ('22', '0');
+INSERT INTO `variables` VALUES ('23', '0');
+INSERT INTO `variables` VALUES ('30002', '0');
+INSERT INTO `variables` VALUES ('30054', '1603586531');
+INSERT INTO `variables` VALUES ('30050', '5');
+INSERT INTO `variables` VALUES ('30016', '1603585473');
+INSERT INTO `variables` VALUES ('30017', '3');
+INSERT INTO `variables` VALUES ('30012', '7');
+INSERT INTO `variables` VALUES ('30008', '5');
+INSERT INTO `variables` VALUES ('30020', '3');
+INSERT INTO `variables` VALUES ('30015', '18');
+INSERT INTO `variables` VALUES ('30011', '5');
+INSERT INTO `variables` VALUES ('30019', '3');
+INSERT INTO `variables` VALUES ('30014', '0');
+INSERT INTO `variables` VALUES ('30010', '4');
+INSERT INTO `variables` VALUES ('30018', '3');
+INSERT INTO `variables` VALUES ('30013', '2');
+INSERT INTO `variables` VALUES ('30009', '5');
+INSERT INTO `variables` VALUES ('2318', '30000');
+INSERT INTO `variables` VALUES ('30032', '100000');
+INSERT INTO `variables` VALUES ('1251', '100000');
+INSERT INTO `variables` VALUES ('6450', '100000');
+INSERT INTO `variables` VALUES ('3575', '14000');
+INSERT INTO `variables` VALUES ('2447', '12000');
+INSERT INTO `variables` VALUES ('30027', '4340');
+INSERT INTO `variables` VALUES ('13935', '2500');
+INSERT INTO `variables` VALUES ('2319', '27500');
+INSERT INTO `variables` VALUES ('30028', '13340');
+INSERT INTO `variables` VALUES ('12359', '12000');
+INSERT INTO `variables` VALUES ('8836', '10000');
+INSERT INTO `variables` VALUES ('30033', '100000');
+INSERT INTO `variables` VALUES ('3820', '5500');
+INSERT INTO `variables` VALUES ('30026', '4340');
+INSERT INTO `variables` VALUES ('30029', '13340');
+INSERT INTO `variables` VALUES ('8544', '41680');
+INSERT INTO `variables` VALUES ('30024', '15000');
+INSERT INTO `variables` VALUES ('3576', '5500');
+INSERT INTO `variables` VALUES ('3860', '9000');
+INSERT INTO `variables` VALUES ('8170', '15000');
+INSERT INTO `variables` VALUES ('3530', '41680');
+INSERT INTO `variables` VALUES ('5095', '2340');
+INSERT INTO `variables` VALUES ('12210', '5000');
+INSERT INTO `variables` VALUES ('30030', '5680');
+INSERT INTO `variables` VALUES ('1', '0');
+INSERT INTO `variables` VALUES ('0', '0');
+INSERT INTO `variables` VALUES ('30025', '15000');
+INSERT INTO `variables` VALUES ('4234', '15000');
+INSERT INTO `variables` VALUES ('4625', '4760');
+INSERT INTO `variables` VALUES ('30031', '5680');
+INSERT INTO `variables` VALUES ('12209', '1680');
+INSERT INTO `variables` VALUES ('30022', '0');
+INSERT INTO `variables` VALUES ('30023', '1');
+INSERT INTO `variables` VALUES ('30021', '1603633121');
+INSERT INTO `variables` VALUES ('2313', '26');
+INSERT INTO `variables` VALUES ('2314', '0');
+INSERT INTO `variables` VALUES ('2317', '200');
+INSERT INTO `variables` VALUES ('30051', '1603912309');
+INSERT INTO `variables` VALUES ('30053', '1');
+INSERT INTO `variables` VALUES ('30052', '1604007207');
+INSERT INTO `variables` VALUES ('30055', '5');
