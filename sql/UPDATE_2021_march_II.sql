29fbcbc608ba9d348bfc40b4f5dff530cec3c5ce
Torta <slowtorta@gmail.com>
2021-03-23 21:29:15 +0300

    New quest: Crawling for Meat and some new items.

--- sql/UPDATE_2021_march_II.sql
+++ sql/UPDATE_2021_march_II.sql
@@ -97,3 +97,115 @@ nextquestid = @nextquest, RewOrReqMoney = @reward_money,
 nextquestinchain = @nextquestinchain, prevquestid = @prevquest,
 srcitemid = 80690, srcitemcount = 1
 where entry = @quest_entry;	
+
+UPDATE `item_template` SET `required_reputation_faction`='0', `required_reputation_rank`='0' WHERE (`entry`='81079') AND (`patch`='0');
+
+replace into item_template values
+ ('51802', '0', '4', '8', 'Idol of Evergrowth', '', '37394', '3', '65536', '1', '12762', '12762', '28', '-1', '-1', '51',
+ '46', '0', '0', '0', '0', '0', '0', '0', '1', '1', '0', '0', '0', '0', '0',
+ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '0', '0', '21972', '1', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0',
+ '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0',
+ '-1', '1', '0', '0', '0', '0', '0', '2', '1', '0', '0', '0', '0', '0', '0', '0', '46', '0', '0', '0',
+ '0', '1', NULL);
+replace into creature_loot_template values (12258, 51802, 7.5, 2, 1, 1, 0, 0, 10);
+
+replace into item_template values
+ ('51803', '0', '4', '9', 'Totem of the Endless Flicker', '', '5686', '3', '65536', '1', '11876', '11876', '28', '-1', '-1', '47',
+ '42', '0', '0', '0', '0', '0', '0', '0', '1', '1', '0', '0', '0', '0', '0',
+ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '0', '0', '16085', '1', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0',
+ '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0',
+ '-1', '1', '0', '0', '0', '0', '0', '2', '1', '0', '0', '0', '0', '0', '0', '0', '45', '0', '0', '0',
+ '0', '1', NULL);
+replace into creature_loot_template values (7271, 51803, 7.5, 2, 1, 1, 0, 0, 10);
+replace into item_template values
+ ('51804', '0', '4', '7', 'Libram of the Resolute', '', '1134', '3', '65536', '1', '9845', '9845', '28', '-1', '-1', '44',
+ '39', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0',
+ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '0', '0', '20106', '1', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0',
+ '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0',
+ '-1', '1', '0', '0', '0', '0', '0', '8', '1', '0', '0', '0', '0', '0', '0', '0', '44', '0', '0', '0',
+ '0', '1', NULL);
+replace into creature_loot_template values (3976, 51804, 7.5, 2, 1, 1, 0, 0, 10);
+
+
+update creature_template set npc_flags = 2 where entry = 81046;
+
+set @quest_entry = 80391;
+set @quest_zone = 148;
+set @title = 'Crawling for Meat';
+set @description = 'Greetings! Your company is welcome. I am Inunquaq of Moa\'ki Harbor. I find myself in this strange land after weeks of sailing.\n\nMy tale began when a Winter Tauren friend of mine known as Buniq visited my settlement in her canoe, she invited me to accompany her to the village of Taunka\'le. But as I entered the canoe, a strong icy gust blew the rope out of Buniq\'s hands. Within moments a strong current had taken me out to the open sea!\n\nBuniq had the oars so all I could do was to desperately cling to the canoe and hope for the best.\n\nEventually the current took me here to this warm land that looks nothing like my home. Listen friend, the canoe had some provisions but I have finished them days ago. Would you please collect some meat from these shelled creatures nearby?\n\nWe have similar creatures in the far north, but I am without the tools and the knowledge I need to try and approach them, as far as I can guess they are edible. If you do so, I will be forever in your debt.';
+set @objective = 'Collect 8 bunches of Crawler Meat.';
+set @completetext = 'My hunger has finally been sated, and my belly feels overjoyed!\n\nYou can have these odd objects I found while scavenging for tools as well as my friendship and gratitude. Hopefully in time I can find my way back to my homeland.\n\nThank you so much friend. I have the strength and soon I will have the tools to hunt for myself now as I seek a way home, you need not worry for me anymore, may our paths unite again one day.';
+set @incompletetext = 'Did you get the meat? I feel my strength vane...';
+set @faction_id = 69;
+set @faction_count = 25;
+set @xp_or_money = 1865;
+set @reward_money = 0; 
+set @quest_level = 18;
+set @min_level = 17;
+set @questgiver_id = 81046;
+set @quest_finisher = 81046;
+set @nextquest = 0;
+set @nextquestinchain = 0;
+set @prevquest = 0;
+set @RewChoiceItemId1 = 0; 
+set @RewChoiceItemId2 = 0; 
+set @RewChoiceItemId3 = 0;
+set @RewChoiceItemId4 = 0; 
+set @RewChoiceItemCount1 = 0;
+set @RewChoiceItemCount2 = 0;
+set @RewChoiceItemCount3 = 0;
+set @RewChoiceItemCount4 = 0;
+set @reward_item_1 = 2683;
+set @reward_item_2 = 0; 
+set @reward_item_3 = 0;
+set @reward_item_4 = 0;
+set @reward_item_1_count = 5;
+set @reward_item_2_count = 0;
+set @reward_item_3_count = 0;
+set @reward_item_4_count = 0;
+set @creature_to_kill_1 = 0;
+set @creature_to_kill_2 = 0; 
+set @creature_to_kill_3 = 0;
+set @creature_to_kill_4 = 0;
+set @creature_to_kill_1_count = 0;
+set @creature_to_kill_2_count = 0;
+set @creature_to_kill_3_count = 0;
+set @creature_to_kill_4_count = 0;
+set @required_item_1 = 2674; 
+set @required_item_2 = 0;
+set @required_item_3 = 0;
+set @required_item_4 = 0;
+set @required_item_1_count = 8;
+set @required_item_2_count = 0;
+set @required_item_3_count = 0;
+set @required_item_4_count = 0;
+replace into quest_template values 
+(@quest_entry, '0', '2', @quest_zone, @min_level,  '0', @quest_level, '0', '0', '0', '0', '0', '0', '0', '0', '0','0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', @title, @description, @objective, @completetext, @incompletetext, '', '', '', '', '', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', @faction_id, '0', '0', '0', '0', @faction_count, '0', '0', '0', '0', '0', @xp_or_money, '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0','0', 0, 0);
+replace into creature_questrelation (id, quest, patch_min, patch_max) values (@questgiver_id, @quest_entry,'0','10'); 
+replace into creature_involvedrelation (id, quest, patch_min, patch_max) values (@quest_finisher, @quest_entry,'0','10');
+update quest_template set 
+rewitemid1 = @reward_item_1, rewitemcount1 = @reward_item_1_count,
+rewitemid2 = @reward_item_2, rewitemcount2 = @reward_item_2_count,
+rewitemid3 = @reward_item_3, rewitemcount3 = @reward_item_3_count,
+rewitemid4 = @reward_item_4, rewitemcount4 = @reward_item_4_count,
+RewChoiceItemId1 = @RewChoiceItemId1, RewChoiceItemCount1 = @RewChoiceItemCount1,
+RewChoiceItemId2 = @RewChoiceItemId2, RewChoiceItemCount2 = @RewChoiceItemCount2,
+RewChoiceItemId3 = @RewChoiceItemId3, RewChoiceItemCount3 = @RewChoiceItemCount3,
+RewChoiceItemId4 = @RewChoiceItemId4, RewChoiceItemCount4 = @RewChoiceItemCount4,
+ReqCreatureOrGOId1 = @creature_to_kill_1, ReqCreatureOrGOCount1 = @creature_to_kill_1_count,
+ReqCreatureOrGOId2 = @creature_to_kill_2, ReqCreatureOrGOCount2 = @creature_to_kill_2_count,
+ReqCreatureOrGOId3 = @creature_to_kill_3, ReqCreatureOrGOCount3 = @creature_to_kill_3_count,
+ReqCreatureOrGOId4 = @creature_to_kill_4, ReqCreatureOrGOCount4 = @creature_to_kill_4_count,
+reqitemid1 = @required_item_1, reqitemcount1 = @required_item_1_count,
+reqitemid2 = @required_item_2, reqitemcount2 = @required_item_2_count,
+reqitemid3 = @required_item_3, reqitemcount3 = @required_item_3_count,
+reqitemid4 = @required_item_4, reqitemcount4 = @required_item_4_count,
+nextquestid = @nextquest, RewOrReqMoney = @reward_money, 
+nextquestinchain = @nextquestinchain, prevquestid = @prevquest
+where entry = @quest_entry;	
