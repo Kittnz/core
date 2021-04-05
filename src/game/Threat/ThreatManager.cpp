d762f24b4364d5bda27c97b2b2d7afbca24ed822
Cosmin POP <mtx.rake@gmail.com>
2021-04-05 13:12:23 +0300

    add UnitDetailedThreatSituation

--- src/game/Threat/ThreatManager.cpp
+++ src/game/Threat/ThreatManager.cpp
@@ -423,38 +423,77 @@ void ThreatManager::addThreat(Unit* pVictim, float pThreat, bool crit, SpellScho
     float threat = ThreatCalcHelper::CalcThreat(pVictim, iOwner, pThreat, crit, schoolMask, pThreatSpell);
 
     addThreatDirectly(pVictim, threat);
-
-	sendThreatToVictim(pVictim, threat);
-	
 }
 
-void ThreatManager::sendThreatToVictim(Unit* pVictim, float threat)
+void ThreatManager::UnitDetailedThreatSituation(Creature* creature, Player* requester)
 {
-	if (!pVictim || pVictim == getOwner() || !pVictim->isAlive() || !pVictim->IsInMap(getOwner()) || 
-		threat <= 0.0f || pVictim->GetTypeId() != TYPEID_PLAYER || !iOwner->ToCreature()->IsElite() || 
-		!pVictim->ToPlayer()->hasThreatAddon() || !pVictim->ToPlayer()->GetGroup())
+
+	if (!creature || !creature->isInCombat() || !creature->IsElite())
 		return;
-	
 
-	bool inParty = pVictim->ToPlayer()->GetGroup() && !pVictim->ToPlayer()->GetGroup()->isRaidGroup();
+	if (!requester || !requester->isAlive() || requester->GetTypeId() != TYPEID_PLAYER  || !requester->GetGroup())
+		return;
 
-	std::string creatureName = iOwner->GetName();
-	std::string pThreat = std::to_string((int)getThreat(pVictim));
-	std::string pDistance = std::to_string((int)pVictim->ToPlayer()->GetDistance2d(iOwner->GetPositionX(), iOwner->GetPositionY()));
+	std::string tankName     = creature->getThreatManager().getHostileTarget()->GetName();
+	std::string creatureName = creature->GetName();
+
+	bool isMelee = true;
+	bool isTanking = false;
+
+	int tankThreat  = 0;
+	int threatPct   = 0;
+	int threatValue = 0;
+
+	ThreatList const& threatList = creature->getThreatManager().getThreatList();
+
+	for (ThreatList::const_iterator iter = threatList.begin(); iter != threatList.end(); ++iter)
+	{
+		if ((*iter)->getTarget()->GetName() == tankName)
+		{
+			tankThreat = (int)round((*iter)->getThreat());
+			break;
+		}
+	}
+
+	if (tankThreat == 0) // something went wrong, cant find tank
+		return;
 
 	WorldPacket data;
+	bool inParty = requester->GetGroup() && !requester->GetGroup()->isRaidGroup();
 
-	std::string msg = "TWT:" + creatureName + ":" + std::to_string(iOwner->GetGUIDLow()) +
-		":" + pThreat + ":" + pDistance;
+	for (ThreatList::const_iterator iter = threatList.begin(); iter != threatList.end(); ++iter)
+	{
+		threatValue = (int)round((*iter)->getThreat());
 
-	ChatHandler::BuildChatPacket(data, inParty ? CHAT_MSG_PARTY : CHAT_MSG_RAID,
-		msg.c_str(), Language(LANG_ADDON), pVictim->ToPlayer()->GetChatTag(),
-		pVictim->ToPlayer()->GetObjectGuid(), pVictim->ToPlayer()->GetName());
+		if (threatValue <= 0) // dont care for negative or 0 threat
+			continue;
 
-	pVictim->ToPlayer()->GetSession()->SendPacket(&data);
-		
+		isTanking = (*iter)->getTarget()->GetName() == tankName;
+
+		isMelee   = (*iter)->getSourceUnit()->CanReachWithMeleeAttack((*iter)->getTarget());
+
+		threatPct = isTanking ? 100 : (int)round(threatValue * 100 / (tankThreat * (isMelee ? 1.1 : 1.3)));
+
+		std::string separator = ":";
+		std::string msg;
+		msg = "TWTv2";                                                // threat api version
+		msg += separator + creatureName;                              // creature name
+		msg += separator + std::to_string(creature->GetGUIDLow());    // creature guid
+		msg += separator + (*iter)->getTarget()->GetName();           // player name
+		msg += separator + std::to_string((int)isTanking);            // 1 if player is tanking
+		msg += separator + std::to_string(threatValue);               // player's threat value, rounded
+		msg += separator + std::to_string(threatPct);                 // player's threat percent, rounded
+		msg += separator + std::to_string(isMelee);                   // 1 if creature can reach player with melee
+
+		ChatHandler::BuildChatPacket(data, inParty ? CHAT_MSG_PARTY : CHAT_MSG_RAID,
+			msg.c_str(), Language(LANG_ADDON), requester->GetChatTag(),
+			requester->GetObjectGuid(), requester->GetName());
+
+		requester->GetSession()->SendPacket(&data);
+
+	}
+
 }
-
 
 void ThreatManager::addThreatDirectly(Unit* pVictim, float threat)
 {
