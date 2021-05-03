5c171a622f9bc468833a153b54664bcdb43959ab
Torta <slowtorta@gmail.com>
2021-05-03 21:00:36 +0300

    Revert "Bring back Naxx scaling up down to 35."
    This reverts commit c1ef27fb922c0b0fbc2cf14c7b7740321cee5939.

--- src/game/Autoscaling/AutoScaler.cpp
+++ src/game/Autoscaling/AutoScaler.cpp
@@ -49,6 +49,10 @@ void AutoScaler::Scale(DungeonMap* map)
     //if (disabledScaling.find(map->GetId()) != disabledScaling.end())
     //    return;
 
+    // Don't scale Naxxramas at all
+    if (map->GetId() == 533)
+        return;
+
     uint32 playerCount = map->GetPlayersCountExceptGMs();
     uint32 maxCount = map->GetMaxPlayers();
 
@@ -59,10 +63,6 @@ void AutoScaler::Scale(DungeonMap* map)
         playerCount = 12;
     else if (maxCount == 40 && playerCount < 20)
         playerCount = 20;
-
-    // Naxxramas specific scaling
-    if (map->GetId() == 533 && playerCount < 35)
-        playerCount = 35;
 
     auto& lock = map->GetObjectLock();
     Read_Mutex_Guard guard{ lock };
