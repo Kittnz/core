4effdb2075ee81296a00d7177abd5b0209a64e1f
Torta <slowtorta@gmail.com>
2021-05-25 07:35:45 +0300

    New quest: Ms. Fix-it!

---
+++ sql/database_structures/WORLD_autobroadcast.sql
@@ -0,0 +1,16 @@
+delete from mangos_string where entry between 50000 and 50005;
+delete from autobroadcast where string_id between 50000 and 50005;
+
+replace into mangos_string (entry, content_default) values 
+
+('50000', 'Delete your WDB folder regularly. This is your game cache, and deleting it can often fix minor game glitches. Always try this before submitting a ticket.'),
+
+('50001', 'If you want to help the server grow, check our shop for these donation rewards: exotic mounts, large bags, mysterious engineering devices, and much more: turtle-wow.org/donate'),
+
+('50002', 'Join /world to connect with the community around you!'),
+
+('50003', 'Read more about our special features here: turtle-wow.org/features'),
+
+('50004', 'Keep up to date with the news and join our Discord channel: discord.turtle-wow.org');
+
+replace into autobroadcast (string_Id) values ('50000'),('50001'),('50002'),('50003'),('50004');
