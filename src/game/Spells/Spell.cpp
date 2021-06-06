b4ecc74d61a8a4d7d8f236b685ef6ff287da8e1c
놀린 <nolin.nolin.nolin.nolin@gmail.com>
2021-06-06 06:20:31 +0200

    Update dependencies (#16)
    * Update dependencies
    
    * update mpq_libmpq
    
    * follow up
    
    * Add support for MinGW and ARM
    
    * update path
    
    * Compile fix

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -8406,7 +8406,7 @@ public:
         }
     }
 
-#ifdef WIN32
+#ifdef _MSC_VER
     template<> void Visit(CorpseMapType &) {}
     template<> void Visit(GameObjectMapType &) {}
     template<> void Visit(DynamicObjectMapType &) {}
@@ -8414,7 +8414,7 @@ public:
 #endif
 };
 
-#ifndef WIN32
+#ifndef _MSC_VER
 template<> inline void SpellNotifierCreatureAndPlayer::Visit(CorpseMapType&) {}
 template<> inline void SpellNotifierCreatureAndPlayer::Visit(GameObjectMapType&) {}
 template<> inline void SpellNotifierCreatureAndPlayer::Visit(DynamicObjectMapType&) {}
