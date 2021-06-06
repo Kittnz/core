299eeaaf91a6f569821ca05ea99717e5d31e874a
놀린 <nolin.nolin.nolin.nolin@gmail.com>
2021-06-06 07:16:40 +0200

    Refactor areatrigger

--- sql/world_020_areatrigger_correction.sql
+++ sql/world_020_areatrigger_correction.sql
@@ -122,3 +122,61 @@ UPDATE `areatrigger_teleport` SET `name`='Ruins Of Ahn\'Qiraj - Exit', `target_p
 UPDATE `areatrigger_teleport` SET `name`='Ruins Of Ahn\'Qiraj - Entrance', `target_position_x`=-8436.53, `target_position_y`=1519.17, `target_position_z`=31.907, `target_orientation`=2.61799 WHERE `id`=4008;
 UPDATE `areatrigger_teleport` SET `name`='Temple of Ahn\'Qiraj - Entrance', `target_position_x`=-8221.35, `target_position_y`=2014.34, `target_position_z`=129.071, `target_orientation`=0.872665 WHERE `id`=4010;
 UPDATE `areatrigger_teleport` SET `name`='Temple of Ahn\'Qiraj - Exit', `target_position_x`=-8239.01, `target_position_y`=1993.25, `target_position_z`=129.071, `target_orientation`=4.01426 WHERE `id`=4012;
+
+-- Fix portal in Maexxna room. It's partly inside the wall, and thus not in LOS of users.
+DELETE FROM `spell_mod` WHERE `ID` = 28444 AND `AttributesEx2` = 4;
+INSERT INTO `spell_mod` (`Id`, `AttributesEx2`, `Comment`) VALUES (28444, 4, 'Portal Effect: Deathknight Wing - Ignore LOS');
+
+ALTER TABLE `areatrigger_teleport`
+	CHANGE COLUMN `name` `name` VARCHAR(64) NOT NULL DEFAULT '' AFTER `patch`,
+	ADD COLUMN `message` VARCHAR(128) NOT NULL DEFAULT '' AFTER `name`;
+
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 20 to enter.' WHERE `id`=45;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 10 to enter.' WHERE `id`=78;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 15 to enter.' WHERE `id`=101;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 10 to enter.' WHERE `id`=145;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 10 to enter.' WHERE `id`=228;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 15 to enter.' WHERE `id`=244;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 10 to enter.', `required_level`=10 WHERE `id`=257;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 30 to enter.' WHERE `id`=286;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 15 to enter.' WHERE `id`=324;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 25 to enter.' WHERE `id`=442;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 35 to enter.' WHERE `id`=446;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 15 to enter.' WHERE `id`=523;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 20 to enter.' WHERE `id`=610;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 20 to enter.' WHERE `id`=612;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 20 to enter.' WHERE `id`=614;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 30 to enter.' WHERE `id`=902;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 35 to enter.' WHERE `id`=924;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 40 to enter.' WHERE `id`=1466;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 45 to enter.' WHERE `id`=1468;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 45 to enter.' WHERE `id`=2214;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 45 to enter.' WHERE `id`=2216;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 45 to enter.' WHERE `id`=2217;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 8 to enter.' WHERE `id`=2230;
+UPDATE `areatrigger_teleport` SET `message`='You must be a Stone Guard or higher rank in order to enter the Hall of Legends.' WHERE `id`=2527;
+UPDATE `areatrigger_teleport` SET `message`='You must be a Knight or higher rank in order to enter the Champions\' Hall.' WHERE `id`=2532;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 45 to enter.' WHERE `id`=2567;
+UPDATE `areatrigger_teleport` SET `message`='Only the Horde may use this portal.' WHERE `id`=2606;
+UPDATE `areatrigger_teleport` SET `message`='Only the Alliance may use this portal.' WHERE `id`=2608;
+UPDATE `areatrigger_teleport` SET `message`='You must be part of a Raid group, at least level 50 and have the Drakefire Amulet in your inventory to enter.' WHERE `id`=2848;
+UPDATE `areatrigger_teleport` SET `message`='You must be part of a Raid group and at least level 50 to enter.' WHERE `id`=2886;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 30 to enter.' WHERE `id`=3133;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 30 to enter.' WHERE `id`=3134;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 45 to enter.' WHERE `id`=3183;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 45 to enter.' WHERE `id`=3184;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 45 to enter.' WHERE `id`=3185;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 45 to enter.' WHERE `id`=3186;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 45 to enter.' WHERE `id`=3187;
+UPDATE `areatrigger_teleport` SET `message`='You must be at least level 45 to enter.' WHERE `id`=3189;
+UPDATE `areatrigger_teleport` SET `message`='You must be attuned and in a raid group to enter.' WHERE `id`=3528;
+UPDATE `areatrigger_teleport` SET `message`='Only the Horde may use this portal.' WHERE `id`=3669;
+UPDATE `areatrigger_teleport` SET `message`='Only the Alliance may use this portal.' WHERE `id`=3671;
+UPDATE `areatrigger_teleport` SET `message`='You must be part of a raid group and at least 50th level to enter.', `required_level`=50 WHERE `id`=3726;
+UPDATE `areatrigger_teleport` SET `message`='You must be part of a Raid group and at least level 50 to enter.' WHERE `id`=3928;
+UPDATE `areatrigger_teleport` SET `message`='Only the Alliance may use this portal.' WHERE `id`=3948;
+UPDATE `areatrigger_teleport` SET `message`='Only the Horde may use this portal.' WHERE `id`=3949;
+UPDATE `areatrigger_teleport` SET `message`='You cannot enter here unless the gates of Ahn\'Qiraj have been opened, you are level 50+, and in a raid group.' WHERE `id`=4008;
+UPDATE `areatrigger_teleport` SET `message`='You cannot enter here unless the gates of Ahn\'Qiraj have been opened, you are level 50+, and in a raid group.' WHERE `id`=4010;
+UPDATE `areatrigger_teleport` SET `message`='You must be level 51+, in a Raid group, and be attuned to Naxxramas to enter.' WHERE `id`=4055;
+UPDATE `areatrigger_teleport` SET `required_level`=0 WHERE `id`=2890;
