b38817c2ab9207284514776584e1d46cb9c6a658
Cosmin POP <mtx.rake@gmail.com>
2021-06-09 21:30:48 +0300

    Update world_027_miscellaneous_db_fixes.sql
    make mage/lock new quest items master lootable

--- sql/world_027_miscellaneous_db_fixes.sql
+++ sql/world_027_miscellaneous_db_fixes.sql
@@ -1,3 +1,7 @@
 -- Fix Open House rewards bonding:
 
 update item_template set bonding = 6 where entry in (51889, 51894);
+
+-- make mage/lock new quest items master lootable
+
+update item_template set quality = 2 where entry = 83005 or entry = 83006;
