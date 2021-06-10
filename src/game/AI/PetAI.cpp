3d3b314e41c863c968bd27be9444d996521ae813
놀린 <nolin.nolin.nolin.nolin@gmail.com>
2021-06-10 23:35:48 +0200

    Fix crash with Infernal pet

--- src/game/AI/PetAI.cpp
+++ src/game/AI/PetAI.cpp
@@ -565,9 +565,7 @@ void PetAI::HandleReturnMovement()
             ClearCharmInfoFlags();
             m_creature->GetCharmInfo()->SetIsReturning(true);
             m_creature->GetMotionMaster()->Clear(false);
-            m_creature->GetMotionMaster()->MoveFollow(m_creature->GetCharmerOrOwner(), PET_FOLLOW_DIST, m_creature->IsPet()
-                && m_creature->ToPet() && m_creature->ToPet()->getPetType() && m_creature->ToPet()->getPetType() == MINI_PET
-                ? 270.0f * (M_PI_F / 180.0f): PET_FOLLOW_ANGLE);
+            m_creature->GetMotionMaster()->MoveFollow(m_creature->GetCharmerOrOwner(), PET_FOLLOW_DIST, m_creature->IsPet() && static_cast<Pet*>(m_creature)->getPetType() == MINI_PET ? MINI_PET_FOLLOW_ANGLE : PET_FOLLOW_ANGLE);
         }
     }
 }
