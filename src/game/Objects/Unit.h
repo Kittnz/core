c55a228360b5471b614730a72ad714f1daac0081
놀린 <nolin.nolin.nolin.nolin@gmail.com>
2021-06-11 18:14:00 +0200

    Improve teleport handling

--- src/game/Objects/Unit.h
+++ src/game/Objects/Unit.h
@@ -1409,6 +1409,7 @@ class Unit : public WorldObject
         void ResolvePendingMovementChange(PlayerMovementPendingChange& change);
         bool FindPendingMovementFlagChange(uint32 movementCounter, bool applyReceived, MovementChangeType changeTypeReceived);
         bool FindPendingMovementRootChange(uint32 movementCounter, bool applyReceived);
+        bool FindPendingMovementTeleportChange(uint32 movementCounter);
         bool FindPendingMovementKnockbackChange(MovementInfo& movementInfo, uint32 movementCounter);
         bool FindPendingMovementSpeedChange(float speedReceived, uint32 movementCounter, UnitMoveType moveType);
         void CheckPendingMovementChanges();
