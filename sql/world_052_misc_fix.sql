f0209c272826e62e8b74007da594894695df6e3d
놀린 <nolin.nolin.nolin.nolin@gmail.com>
2021-06-11 18:02:05 +0200

    Add target for Test Ribbon Pole Channel spell

--- sql/world_052_misc_fix.sql
+++ sql/world_052_misc_fix.sql
@@ -1,3 +1,7 @@
 
 -- Not captured AB banners should be friendly faction according to TDB.
 UPDATE `gameobject_template` SET `faction`=35 WHERE `entry` IN (180087, 180088, 180089, 180090, 180091);
+
+-- Add missing target for Test Ribbon Pole Channel.
+INSERT INTO `spell_script_target` (`entry`, `type`, `targetEntry`) VALUES (29705, 1, 17066);
+
