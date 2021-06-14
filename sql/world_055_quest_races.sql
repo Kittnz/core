e0a44cd284d7907433fcd250dd8e1f9ea52f039f
Torta <slowtorta@gmail.com>
2021-06-14 07:52:12 +0300

    Quest 'You Reap What You Saw' will now grant 20 Ungoro Soil.

--- sql/world_055_quest_races.sql
+++ sql/world_055_quest_races.sql
@@ -1,2 +1,5 @@
 
 update quest_template set requiredraces = 589 where entry = 6402;
+
+update quest_template set rewitemid3 = 11018, rewitemcount3 = 20 where entry = 60060;
+update quest_template set rewitemid3 = 11018, rewitemcount3 = 20 where entry = 60061;
