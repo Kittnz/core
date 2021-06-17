01108667f8ef09c3d9bb9b393e38eaa014aca250
놀린 <nolin.nolin.nolin.nolin@gmail.com>
2021-06-18 00:25:56 +0200

    some brackets lol

--- src/game/Mail/Mail.cpp
+++ src/game/Mail/Mail.cpp
@@ -309,7 +309,7 @@ void MailDraft::SendMailTo(MailReceiver const& receiver, MailSender const& sende
             expire_delay = HOUR;
         // default case: expire time if COD 3 days, if no COD 30 days
         else
-            expire_delay = (m_COD > 0) ? 3 * DAY : 30 * DAY;
+            expire_delay = (m_COD > 0) ? (3 * DAY) : (30 * DAY);
     }
 
     time_t expire_time = deliver_time + expire_delay;
