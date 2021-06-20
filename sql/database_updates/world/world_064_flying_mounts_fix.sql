e77f8aadf4381036422c4b18901e89ddcb95ed3c
Torta <slowtorta@gmail.com>
2021-06-20 18:50:44 +0300

    Make Goldshire Gryphon and Darkshore Wyvern fun again.

---
+++ sql/database_updates/world/world_064_flying_mounts_fix.sql
@@ -0,0 +1,5 @@
+
+-- Riding Gryphon
+
+update creature_template set script_name = 'npc_flying_mount' where entry = 51562;
+update creature_template set script_name = 'npc_flying_mount' where entry = 51685;
