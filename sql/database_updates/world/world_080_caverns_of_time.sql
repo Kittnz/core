9c1fe4b7d71b7498562a960a8072eb6f9960055d
Torta <slowtorta@gmail.com>
2021-07-04 23:24:39 +0300

    New quest: The First Opening of The Dark Portal (CoT).

--- sql/database_updates/world/world_080_caverns_of_time.sql
+++ sql/database_updates/world/world_080_caverns_of_time.sql
@@ -1,3 +1,10 @@
+-- Misc. DB fixes:
+
+update creature_template set scale = 2.5, level_min = 63, level_max = 63 where entry = 65105;
+update creature_template set display_id1 = 18544 where entry = 65004; 
+
+-- Quests:
+
 replace into creature_template values 
 (65019, 0, 10008, 0, 0, 0, 'Chromie', 'Keepers of Time', 2911, 63, 63, 4496, 4496, 0, 0, 4641, 35, 6, 1, 1.14286, 1.15, 20, 5, 0, 0, 1, 178, 229, 0, 290, 1, 2000, 2000, 1, 33536, 0, 0, 0, 0, 0, 0, 220.81, 105.197, 100, 2, 0, 0, 0, 0, 0, 15, 15, 15, 15, 15, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 10667, 0, 0, 0, 0, 2, '');
 
@@ -5,4 +12,12 @@ replace into quest_template (prevquestid, entry, zoneorsort, questlevel, minleve
 
 replace into creature_questrelation (id, quest) values (10667, 80604);
 replace into creature_involvedrelation (id, quest) values (65019, 80604);
+
+replace into quest_template (prevquestid, entry, zoneorsort, questlevel, minlevel, questflags, specialflags, title, details, objectives, requestitemstext, offerrewardtext, reqitemid1, reqitemcount1, reqitemid2, reqitemcount2, reqitemid3, reqitemcount3, reqitemid4, reqitemcount4, reqcreatureorgoid1, reqcreatureorgocount1, reqcreatureorgoid2, reqcreatureorgocount2, reqcreatureorgoid3, reqcreatureorgocount3, reqcreatureorgoid4, reqcreatureorgocount4, srcitemid, srcitemcount, reworreqmoney, rewmoneymaxlevel, rewrepfaction1, rewrepvalue1,  rewrepfaction2, rewrepvalue2, rewrepfaction3, rewrepvalue3, rewrepfaction4, rewrepvalue4, rewspell, rewspellcast, completeemote, rewitemid1, rewitemcount1, rewitemid2, rewitemcount2, rewitemid3, rewitemcount3, rewitemid4, rewitemcount4, rewchoiceitemid1, rewchoiceitemcount1, rewchoiceitemid2, rewchoiceitemcount2, rewchoiceitemid3, rewchoiceitemcount3, rewchoiceitemid4, rewchoiceitemcount4) values (80604,80605,2300,60,60,0,0,'The First Opening of The Dark Portal','Roughly seventeen years ago, Medivh the last guardian under the control of Sargeras opened the Dark Portal, and let the Orcish Horde into this world.\n\nWe have detected a temporal anomaly in the area consistent with the same energy that Kheyna and Antnormi had.\n\nKheyna is all but certain that Antnormi is behind this, please take the timeway to the right and stop Antnormi before she damages history, I will oversee the situation from here.\n\nBefore you doubt whether this is a bad thing, the Orcs were instrumental in defeating the Legion, I assure you, the outcome if they didn\'t arrive is catastrophic!','Enter the Timeways into Black Morass\'s past and slay Antnormi. Bring her head to Kheyna.','Please... You cannot let her win!','<Kheyna stares somberly at the head, her ocular implants contracts for a moment>\n\nAntnormi was like a sister to me once.\n\nI wish this could have ended differently, but alas... We did what we had to.\n\nFor the time being I will remain here until Chromie figures out what to do with me. I was once a Bronze dragon after all, maybe I can be one again if we find a cure.\n\nRemember when I had you collect brains for me? That work paid off, and I\'ve made a version of my implant that can connect to your brain without needing to drill into your skull.\n\nIt\'s your reward for what you\'ve done. Take it, it\'ll help you.\n\nI also wish to introduce myself to you properly. My real name is Keridormi. I\'ve never had many friends, so thank you for everything you\'ve done for me.\n\nPerhaps we will meet again in time. Stay safe!',51044,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,30000,0,910,500,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+
+replace into creature_questrelation (id, quest) values (65004, 65004);
+replace into creature_involvedrelation (id, quest) values (65004, 65004);
+
+replace into item_template values (51044, 0, 12, 0, 'Antnormi\'s Head', '', 31434, 1, 0, 1, 0, 0, 0, -1, -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 4, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, null);
+update item_template set flags = 2048 where entry = 51044;
 
