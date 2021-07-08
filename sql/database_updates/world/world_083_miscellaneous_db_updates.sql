6e5e58e455c3a91cc84ea65e92fddca8d97b2abb
Torta <slowtorta@gmail.com>
2021-07-08 11:30:42 +0300

    Added some gossips for Caverns of Time NPCs.

---
+++ sql/database_updates/world/world_083_miscellaneous_db_updates.sql
@@ -0,0 +1,8 @@
+replace into broadcast_text (entry, male_text) values 
+(51673, 'Welcome to the Caverns of Time, $N.\n\nYou have been granted permission to enter due to what you have done or what you will do.\n\nThe Caverns of Time are the home of my people, the Bronze Dragonflight. We are the guardians of time, and thus our home is connected to the time stream.\n\nMake no mistake Mortal, this place is not for a vacation. If you go rogue expect to never leave. In the Caverns you follow our rules and directions to the letter.\n\nIf you understand then I will teleport inside, but remember, we can see what you intend to do before you do it.'); 
+replace into npc_text (ID, BroadcastTextID0) values (51673, 51673);
+
+replace into broadcast_text (entry, male_text) values 
+(51674, 'The path to the caverns is long arduous. Mayhap I can ease your burdens and take you deeper in the Cavern.'); 
+replace into npc_text (ID, BroadcastTextID0) values (51674, 51674);
+
