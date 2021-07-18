6da40c2664e91ce548ec880d968bae2bd613657e
Torta <slowtorta@gmail.com>
2021-07-19 02:48:12 +0300

    Revert "Debuff test v2."
    This reverts commit 8358cde85ccc39299d994035bb8ddd688f2c3656.

--- src/game/Spells/SpellAuraDefines.h
+++ src/game/Spells/SpellAuraDefines.h
@@ -22,7 +22,7 @@
 #ifndef MANGOS_SPELLAURADEFINES_H
 #define MANGOS_SPELLAURADEFINES_H
 
-#define MAX_AURAS 56                                        // 12*4 (byte per aura) slots in UNIT_FIELD_AURA*
+#define MAX_AURAS 48                                        // 12*4 (byte per aura) slots in UNIT_FIELD_AURA*
 #define MAX_POSITIVE_AURAS 32
 
 enum AuraFlags
