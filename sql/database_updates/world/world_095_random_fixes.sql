617bfdeb41c61ddb403e591400f72cbed200a984
Torta <slowtorta@gmail.com>
2021-08-02 08:59:31 +0300

    Remove 40 minute durations from the Lost Farm Sheeps.

--- sql/database_updates/world/world_095_random_fixes.sql
+++ sql/database_updates/world/world_095_random_fixes.sql
@@ -1,1 +1,4 @@
 update quest_template set requiredraces = 434 where entry = 70052;
+
+UPDATE item_template SET description='' WHERE entry=51220;
+UPDATE item_template SET duration='0' WHERE entry=51220;
