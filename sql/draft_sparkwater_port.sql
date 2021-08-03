cbd381586532f2a8011a21de9ddc2013f57d6891
Torta <slowtorta@gmail.com>
2021-08-03 16:57:35 +0300

    New quest: `Empty Handed` (Sparkwater Port).

--- sql/draft_sparkwater_port.sql
+++ sql/draft_sparkwater_port.sql
@@ -648,3 +648,9 @@ replace into quest_template (prevquestid, entry, zoneorsort, questlevel, minleve
 replace into creature_questrelation (id, quest) values (91234, 55007);
 replace into creature_involvedrelation (id, quest) values (91254, 55007);
 
+-- Frix's Folly PH 55008
+
+replace into quest_template (prevquestid, entry, zoneorsort, questlevel, minlevel, questflags, specialflags, title, details, objectives, requestitemstext, offerrewardtext, reqitemid1, reqitemcount1, reqitemid2, reqitemcount2, reqitemid3, reqitemcount3, reqitemid4, reqitemcount4, reqcreatureorgoid1, reqcreatureorgocount1, reqcreatureorgoid2, reqcreatureorgocount2, reqcreatureorgoid3, reqcreatureorgocount3, reqcreatureorgoid4, reqcreatureorgocount4, srcitemid, srcitemcount, reworreqmoney, rewxp, rewrepfaction1, rewrepvalue1,  rewrepfaction2, rewrepvalue2, rewrepfaction3, rewrepvalue3, rewrepfaction4, rewrepvalue4, rewspell, rewspellcast, completeemote, rewitemid1, rewitemcount1, rewitemid2, rewitemcount2, rewitemid3, rewitemcount3, rewitemid4, rewitemcount4, rewchoiceitemid1, rewchoiceitemcount1, rewchoiceitemid2, rewchoiceitemcount2, rewchoiceitemid3, rewchoiceitemcount3, rewchoiceitemid4, rewchoiceitemcount4) values (55008,55009,16,47,45,0,0,'Empty Handed','Upon searching the corpse of Frix Tallycog and the surrounding area there is no sign of the Surveying Gear that you were meant to retrieve, you should report your findings to Gazztoggle Krewpipe!','Return to Gazztoggle Krewpipe in Valormok with the bad news.','Hmm?','Ahh, just as I suspected, dead... Wait, he lost it?!\n\nDamn you Frix, you blasted, good for nothing, worthless...',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,750,1001,150,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+
+replace into creature_questrelation (id, quest) values (91255, 55009);
+replace into creature_involvedrelation (id, quest) values (91254, 55009);
