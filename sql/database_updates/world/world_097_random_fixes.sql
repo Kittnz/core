ab58b99674291bb394a5b69ff16831fc965042bf
Torta <slowtorta@gmail.com>
2021-08-04 14:50:46 +0300

    New companion: `Lil' Tinyfin`.

--- sql/database_updates/world/world_097_random_fixes.sql
+++ sql/database_updates/world/world_097_random_fixes.sql
@@ -1,3 +1,7 @@
 update creature_involvedrelation set id = 80932 where quest = 80390;
 delete from creature_template where entry = 80134;
 delete from creature where id = 80134;
+
+replace into item_template values (83010, 0, 15, 0, 'Tinyfin', 'He will become a paladin someday!',  18050, 1, 0, 1, 6000, 1500, 0, -1, -1, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 28505, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, null);
+replace into `creature_template` values (90989, 0, 15369, 0, 0, 0, 'Lil\' Tinyfin', '', 0, 1, 1, 64, 64, 0, 0, 0, 35, 0, 1, 1.14286, 1, 20, 5, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, '');
+replace into custom_pet_entry_relation values (83010, 90989);
