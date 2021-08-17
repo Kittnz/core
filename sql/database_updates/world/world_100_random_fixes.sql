e9a58fccedc20f30274fbb8ac9b0db3770a04759
Torta <slowtorta@gmail.com>
2021-08-17 21:19:44 +0300

    Misc. DB fixes.

---
+++ sql/database_updates/world/world_100_random_fixes.sql
@@ -0,0 +1,3 @@
+update item_template set subclass = 5, dmg_min1 = 120, dmg_max1 = 175, delay = 3800 where entry = 81199;
+update item_template set display_id = 61052 where entry = 61060;
+update item_template set sheath = 0 where entry in (80546, 80646);
