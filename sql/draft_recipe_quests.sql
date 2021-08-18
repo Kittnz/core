f66a17f06189b224ddb65ea00e4604c9b097cbad
Torta <slowtorta@gmail.com>
2021-08-18 11:15:41 +0300

    New quest: `Imperial Plate Gauntlets`.

---
+++ sql/draft_recipe_quests.sql
@@ -0,0 +1,6 @@
+replace into quest_template values (55300, 2, 0, -121, 50, 0, 60, 0, 0, 0, 164, 265, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7652, 0, 0, 0, 0, 0, 0, 'Imperial Plate Gauntlets', '', 'For the gauntlets plans, I\'ll be needin\' 20 thorium bars.', 'Not only those be comfy, mind you, it\'s also a style. Of course, they do provide protection too. ', 'For the gauntlets plans, I\'ll be needin\' 10 thorium bars.$B$BAre you alright, sonny? Yer getting\' all red.', '', '', '', '', '', 12359, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 12690, 0, 0, 0, 1, 0, 0, 0, 59, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+-- TODO: Replace item ID 12690 with [Plans: Imperial Plate Gauntlets]
+
+replace into creature_questrelation (id, quest) values (14567, 55300);
+replace into creature_involvedrelation (id, quest) values (14567, 55300);
