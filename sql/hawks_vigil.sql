a5fe82dd029a89f9a61ad23f3450fc7131676b5c
Torta <slowtorta@gmail.com>
2021-08-24 14:25:36 +0300

    New script for Harlus.

--- sql/hawks_vigil.sql
+++ sql/hawks_vigil.sql
@@ -260,4 +260,7 @@ update quest_template set reqcreatureorgoid1 = 52119, reqcreatureorgocount1 = 1 
 replace into creature_questrelation (id, quest) values (52024, 55225);
 replace into creature_involvedrelation (id, quest) values (52024, 55225);
 
+update creature_template set health_min = 3981, health_max = 3981, shadow_res = 25, script_name = 'npc_harlus', npc_flags = 1, faction = 35 where entry = 52119;
 
+set @gossip_menu_id = 60045; set @magic_number = 52119; 
+replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); replace into broadcast_text (entry, Male_Text) values (@magic_number, 'What do you want?'); replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_number); update creature_template set gossip_menu_id = @gossip_menu_id where entry = @magic_number;
--- sql/sparkwater_port.sql
+++ sql/sparkwater_port.sql
@@ -269,6 +269,8 @@ replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id,
 set @gossip_menu_id = 60044; set @magic_number = 91245; 
 replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); replace into broadcast_text (entry, Male_Text) values (@magic_number, 'The Goblin\'s are an interesting folk, but could use more spiritualism.'); replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_number); update creature_template set gossip_menu_id = @gossip_menu_id where entry = @magic_number;
 
+-- 60045 reserved.
+
 replace into npc_trainer values 
 (91202, 2275, 10, 0, 0, 5, 0, 5875),
 (91202, 2339, 100, 171, 25, 0, 0, 5875),
