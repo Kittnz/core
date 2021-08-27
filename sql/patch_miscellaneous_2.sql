75b5d9c716a46b6dd8f41283e1718cc8feef8006
Torta <slowtorta@gmail.com>
2021-08-27 15:14:42 +0300

    New Tirisfal NPCs.

---
+++ sql/patch_miscellaneous_2.sql
@@ -0,0 +1,59 @@
+replace into creature_template values 
+(91750, 0, 4108, 0, 0, 0, 'Apothecary Villa', '', 0, 20, 20, 942, 942, 0, 0, 1373, 68, 2, 1, 1.14286, 0, 20, 5, 0, 0, 1, 51, 65, 0, 138, 1, 2000, 2000, 1, 37376, 0, 0, 0, 0, 0, 0, 51.128, 70.301, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 2216, 0, 0, 0, 0, 524298, 0, ''),
+(91751, 0, 1590, 0, 0, 0, 'Deathguard Jalen', NULL, 0, 55, 55, 4667, 4667, 0, 0, 4292, 71, 0, 1, 1.42857, 0, 20, 5, 0, 0, 1, 240, 260, 0, 248, 1, 2000, 2000, 1, 36864, 0, 0, 0, 0, 0, 0, 301.34, 422.17, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 1, 714, '', 0, 3, 0, 0, 3, 1739, 0, 0, 0, 0, 524288, 0, ''),
+(91752, 0, 1496, 0, 0, 0, 'Deathguard Mike', NULL, 0, 55, 55, 4667, 4667, 0, 0, 4292, 71, 0, 1, 1.42857, 0, 20, 5, 0, 0, 1, 240, 260, 0, 248, 1, 2000, 2000, 1, 36864, 0, 0, 0, 0, 0, 0, 301.34, 422.17, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 1, 714, '', 0, 3, 0, 0, 3, 1739, 0, 0, 0, 0, 524288, 0, ''),
+(91753, 0, 4039, 0, 0, 0, 'Vathras Millson', NULL, 0, 55, 55, 4667, 4667, 0, 0, 4292, 68, 2, 1, 1.14286, 0, 20, 5, 0, 0, 1, 240, 260, 0, 106, 1, 2000, 2000, 1, 37376, 0, 0, 0, 0, 0, 0, 38.72, 53.24, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 6389, 0, 0, 0, 0, 524298, 0, ''),
+(91754, 0, 3517, 0, 0, 0, 'Nina Millson', 'Cooking Trainer', 9257, 35, 35, 1342, 1342, 0, 0, 1373, 68, 19, 1, 1.14286, 0, 20, 5, 0, 0, 1, 51, 65, 0, 138, 1, 2000, 2000, 1, 37376, 0, 0, 2, 0, 0, 0, 51.128, 70.301, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 4552, 0, 0, 0, 0, 524298, 0, ''),
+(91755, 0, 1669, 0, 0, 0, 'Dockmaster Ormen', NULL, 9419, 15, 15, 328, 328, 0, 0, 20, 68, 2, 1, 1.14286, 0, 20, 5, 0, 0, 1, 17, 22, 0, 74, 1, 2000, 2000, 1, 37376, 0, 0, 0, 0, 0, 0, 24.552, 33.759, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 5693, 0, 0, 0, 0, 524298, 0, ''),
+(91756, 0, 10979, 0, 0, 0, 'Wretched Farmhand', NULL, 0, 14, 15, 300, 328, 0, 0, 677, 21, 0, 1.01, 1.14286, 1, 20, 5, 0, 0, 1, 38, 43, 0, 74, 1, 1830, 2013, 1, 0, 0, 0, 0, 0, 0, 0, 25.7664, 35.4288, 100, 6, 0, 846, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 19, 'EventAI', 1, 1, 0, 0, 3, 0, 0, 0, 8388624, 0, 0, 0, ''),
+(91757, 0, 569, 0, 0, 0, 'Wretched Guard', NULL, 0, 15, 16, 400, 428, 0, 0, 677, 21, 0, 1.01, 1.14286, 1, 20, 5, 0, 0, 1, 38, 43, 0, 74, 1, 1830, 2013, 1, 0, 0, 0, 0, 0, 0, 0, 25.7664, 35.4288, 100, 6, 0, 846, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 19, 'EventAI', 1, 1, 0, 0, 3, 0, 0, 0, 8388624, 0, 0, 0, ''),
+(91758, 0, 569, 0, 0, 0, 'Wretched Bodyguard', NULL, 0, 18, 18, 600, 628, 0, 0, 677, 21, 0, 1.01, 1.14286, 1, 20, 5, 0, 0, 1, 38, 43, 0, 74, 1, 1830, 2013, 1, 0, 0, 0, 0, 0, 0, 0, 25.7664, 35.4288, 100, 6, 0, 846, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 19, 'EventAI', 1, 1, 0, 0, 3, 0, 0, 0, 8388624, 0, 0, 0, ''),
+(91759, 0, 1435, 0, 0, 0, 'Necromancer Reginald', '', 0, 20, 20, 700, 787, 0, 0, 761, 21, 0, 0.96, 1.14286, 0, 20, 5, 0, 0, 1, 39, 48, 0, 110, 1, 1700, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 42.1344, 57.9348, 100, 6, 0, 511, 511, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5110, 0, 36, 52, 'EventAI', 0, 1, 0, 0, 3, 511, 0, 0, 8388624, 0, 0, 0, ''),
+(91760, 0, 11180, 0, 0, 0, 'Tirisclaw Ambusher', NULL, 0, 16, 16, 519, 600, 0, 0, 642, 16, 0, 1, 1.14286, 0, 20, 5, 0, 1, 1, 57, 73, 0, 70, 1, 1541, 1695, 1, 0, 0, 0, 0, 0, 0, 0, 18.984, 26.103, 100, 7, 0, 1892, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 50, 'EventAI', 1, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
+(91761, 0, 4329, 4327, 0, 0, 'Rogue Enforcer ', '', 0, 15, 16, 328, 356, 0, 0, 695, 17, 0, 0.95, 1.14286, 0, 20, 5, 0, 0, 1, 23, 30, 0, 76, 1, 2000, 2000, 1, 32768, 0, 0, 0, 0, 0, 0, 32.8944, 45.2298, 100, 7, 0, 824, 824, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 19, 29, 'EventAI', 0, 3, 0, 0, 3, 824, 0, 0, 0, 0, 0, 0, ''),
+(91762, 0, 3467, 2181, 4328, 0, 'Rogue Conjurer', '', 0, 16, 16, 303, 325, 710, 790, 528, 17, 0, 0.95, 1.14286, 0, 20, 5, 0, 0, 1, 15, 21, 0, 74, 1, 2000, 2000, 8, 32768, 0, 0, 0, 0, 0, 0, 21.2784, 29.2578, 100, 7, 0, 589, 589, 0, 0, 0, 0, 0, 0, 0, 20793, 12544, 0, 0, 5890, 0, 16, 26, 'EventAI', 1, 3, 0, 0, 3, 589, 0, 0, 0, 0, 0, 0, ''),
+(91763, 0, 3960, 0, 0, 0, 'Rogue Arcanist', '', 0, 17, 17, 855, 921, 1580, 1712, 373, 17, 0, 0.95, 1.14286, 0, 20, 5, 0, 1, 1, 74, 96, 0, 76, 1, 2000, 2000, 8, 0, 0, 0, 0, 0, 0, 0, 23.8896, 32.8482, 100, 7, 0, 619, 619, 0, 0, 10, 10, 10, 10, 10, 5108, 9053, 12544, 0, 6190, 0, 62, 86, 'EventAI', 1, 3, 0, 0, 3, 619, 0, 0, 0, 0, 0, 0, ''),
+(91763, 0, 3960, 0, 0, 0, 'Rogue Arcanist', '', 0, 17, 17, 855, 921, 1580, 1712, 373, 17, 0, 0.95, 1.14286, 0, 20, 5, 0, 1, 1, 74, 96, 0, 76, 1, 2000, 2000, 8, 0, 0, 0, 0, 0, 0, 0, 23.8896, 32.8482, 100, 7, 0, 619, 619, 0, 0, 10, 10, 10, 10, 10, 5108, 9053, 12544, 0, 6190, 0, 62, 86, 'EventAI', 1, 3, 0, 0, 3, 619, 0, 0, 0, 0, 0, 0, '');
+
+update creature_template set npc_flags = 3 where entry = 91753;
+update creature_template set npc_flags = 3 where entry = 91755;
+
+set @equip_template = 55060; set @weapon_1 = 1905; set @weapon_2 = 0; set @creature = 91751;
+replace into creature_equip_template values (@equip_template, 0, @weapon_1, @weapon_2, 0); 
+update creature_template set equipment_id = @equip_template where entry = @creature;
+
+set @equip_template = 55061; set @weapon_1 = 1905; set @weapon_2 = 0; set @creature = 91752;
+replace into creature_equip_template values (@equip_template, 0, @weapon_1, @weapon_2, 0); 
+update creature_template set equipment_id = @equip_template where entry = @creature;
+
+set @equip_template = 55062; set @weapon_1 = 15782; set @weapon_2 = 0; set @creature = 91758;
+replace into creature_equip_template values (@equip_template, 0, @weapon_1, @weapon_2, 0); 
+update creature_template set equipment_id = @equip_template where entry = @creature;
+
+set @equip_template = 55063; set @weapon_1 = 15275; set @weapon_2 = 0; set @creature = 91759;
+replace into creature_equip_template values (@equip_template, 0, @weapon_1, @weapon_2, 0); 
+update creature_template set equipment_id = @equip_template where entry = @creature;
+
+set @gossip_menu_id = 60060; set @magic_number = 91750; 
+replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); 
+replace into broadcast_text (entry, Male_Text) values (@magic_number, 'Are you here to help the Apothecaries?'); 
+replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_number); 
+update creature_template set gossip_menu_id = @gossip_menu_id where entry = @magic_number;
+
+set @gossip_menu_id = 60061; set @magic_number = 91753; 
+replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); 
+replace into broadcast_text (entry, Male_Text) values (@magic_number, 'Is there something you can do for me, $R?'); 
+replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_number); 
+update creature_template set gossip_menu_id = @gossip_menu_id where entry = @magic_number;
+
+set @gossip_menu_id = 60062; set @magic_number = 91754; 
+replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); 
+replace into broadcast_text (entry, Male_Text) values (@magic_number, 'These lands are a wasteland, it is best to learn how to cook what little you do find.'); 
+replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_number); 
+update creature_template set gossip_menu_id = @gossip_menu_id where entry = @magic_number;
+
+set @gossip_menu_id = 60063; set @magic_number = 91755; 
+replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); 
+replace into broadcast_text (entry, Male_Text) values (@magic_number, 'There is much to do, can you help?'); 
+replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_number); 
+update creature_template set gossip_menu_id = @gossip_menu_id where entry = @magic_number;
