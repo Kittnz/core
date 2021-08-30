abd77928f995ea91dc725b051a8478f856b186dc
Dormilon <79369417+Dormilon@users.noreply.github.com>
2021-08-30 17:46:38 +0300

    New quest: `Discovering the Source`.

--- sql/Dalarans_quests.sql
+++ sql/Dalarans_quests.sql
@@ -17,9 +17,6 @@ replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel,
 replace into creature_questrelation (id, quest) values (91350, 40123);
 replace into creature_involvedrelation (id, quest) values (91350, 40123);
 
-delete from gameobject_loot_template where entry = 1000251;
-replace into gameobject_loot_template (entry, item, chanceorquestchance, mincountorref, maxcount, condition_id, patch_max) values (1000251,81324,-100,1,1,0,10);
-
 delete from quest_template where entry = 40124;
 replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel, minlevel, questflags, specialflags, title, details, objectives, requestitemstext, offerrewardtext, reqitemid1, reqitemcount1, reqitemid2, reqitemcount2, reqitemid3, reqitemcount3, reqitemid4, reqitemcount4, reqcreatureorgoid1, reqcreatureorgocount1, reqcreatureorgoid2, reqcreatureorgocount2, reqcreatureorgoid3, reqcreatureorgocount3, reqcreatureorgoid4, reqcreatureorgocount4, srcitemid, srcitemcount, reworreqmoney, rewxp, rewrepfaction1, rewrepvalue1,  rewrepfaction2, rewrepvalue2, rewrepfaction3, rewrepvalue3, rewrepfaction4, rewrepvalue4, rewspell, rewspellcast, completeemote, rewitemid1, rewitemcount1, rewitemid2, rewitemcount2, rewitemid3, rewitemcount3, rewitemid4, rewitemcount4, rewchoiceitemid1, rewchoiceitemcount1, rewchoiceitemid2, rewchoiceitemcount2, rewchoiceitemid3, rewchoiceitemcount3, rewchoiceitemid4, rewchoiceitemcount4, objectivetext2) values (40123,40124,2,16,52,40,0,0,'Interfering Naga','Even the Ley-Shard\'s of the city of Eldarath have their energies compromised. This could be the work of the Naga that have encroached and invaded from the ocean depths. I would not put it past the beasts, as savage as they are, for attempting to claim dominion over the landscape.\n\nWe must look into this matter further before claiming we have gotten this solved. I have prepared a scroll of dispelling, use it upon the Spitelash Shrine in their clutches. If they are behind this dampening, this should solve the problem. While you\'re there, slay their sirens as well, they are the ones who wield a crude, and foul magic.','Slay 15 Spitelash Siren\'s and dispell the magic at the Spitelash Shrine.','Have the Naga been dealt with?','It has been done? You have dispelled the shrine? Let us see just how much of an effect this has had on our magics, let us hope this is done, once and for all.',0,0,0,0,0,0,0,0,6195,15,60312,1,0,0,0,0,60113,1,0,6000,61,200,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'Use Bromley\'s Dispelling Scroll near Spitelash Shrine');
 
@@ -31,6 +28,12 @@ replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel,
 
 replace into creature_questrelation (id, quest) values (91350,40125);
 replace into creature_involvedrelation (id, quest) values (2543,40125);
+
+delete from quest_template where entry = 40126;
+replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel, minlevel, questflags, specialflags, title, details, objectives, requestitemstext, offerrewardtext, reqitemid1, reqitemcount1, reqitemid2, reqitemcount2, reqitemid3, reqitemcount3, reqitemid4, reqitemcount4, reqcreatureorgoid1, reqcreatureorgocount1, reqcreatureorgoid2, reqcreatureorgocount2, reqcreatureorgoid3, reqcreatureorgocount3, reqcreatureorgoid4, reqcreatureorgocount4, srcitemid, srcitemcount, reworreqmoney, rewxp, rewrepfaction1, rewrepvalue1,  rewrepfaction2, rewrepvalue2, rewrepfaction3, rewrepvalue3, rewrepfaction4, rewrepvalue4, rewspell, rewspellcast, completeemote, rewitemid1, rewitemcount1, rewitemid2, rewitemcount2, rewitemid3, rewitemcount3, rewitemid4, rewitemcount4, rewchoiceitemid1, rewchoiceitemcount1, rewchoiceitemid2, rewchoiceitemcount2, rewchoiceitemid3, rewchoiceitemcount3, rewchoiceitemid4, rewchoiceitemcount4) values (40125,40126,2,16,52,40,0,0,'Discovering the Source','<The Archmage would take his time in studying the magic>.\n\nHmm, I can hardly believe this, why would they... <Ansirem mumbles quietly to himself>.\n\n$N, what we are dealing with is much more serious then I had originally thought. The magic remnants upon the Ley-Shard\'s are that of a draconic origin. What the Dragonflights would want, or need from dampening magic in Azshara is a great mystery but at the least we now have a source.\n\nI have prepared this letter for Bromley, take it to him, and let him know that the Blue Dragonflight is responsible.','Take Archmage Ansirem\'s Letter to Magus Bromley in Azshara.','So, you return, what did Ansirem say about the Ley-Shard\'s?','<Magus Bromley begins to read the letter>.\n\nOh... Well, things have gotten a whole lot more complicated if this is the case.',60115,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,60115,1,0,2150,61,100,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+
+replace into creature_questrelation (id, quest) values (2543,40126);
+replace into creature_involvedrelation (id, quest) values (91350,40126);
 
 delete from creature_template where entry = 60312;
 replace into creature_template (entry, name, display_id1) values (60312 , 'quest_40124_dummy_triger', 328);
@@ -45,5 +48,17 @@ replace into item_template (entry, display_id, name, class, quality, flags, buy_
 delete from item_template where entry = 60114;
 replace into item_template (entry, display_id, name, class, quality, flags, buy_count, allowable_class, allowable_race, item_level, stackable, spellcooldown_1, spellcategorycooldown_1, spellcooldown_2, spellcategorycooldown_2, bonding, description) values (60114,7913,'Crate of Ley-Shard\'s',12,1,1,1,-1,-1,1,1,-1,-1,-1,-1,4,'');
 
+delete from item_template where entry = 60115;
+replace into item_template (entry, display_id, name, class, quality, flags, buy_count, allowable_class, allowable_race, item_level, stackable, spellcooldown_1, spellcategorycooldown_1, spellcooldown_2, spellcategorycooldown_2, bonding, page_text, page_material) values (60115,3033,'Archmage Ansirem\'s Letter',12,1,1,1,-1,-1,1,1,-1,-1,-1,-1,4,50500,1);
+
 delete from gameobject where id = 2010801;
 replace into gameobject (id, map, position_x, position_y, position_z, orientation) values (2010801, 1, 3471.790, -5168.149, 85.351, 4.68);
+
+delete from gameobject_loot_template where entry = 1000251;
+replace into gameobject_loot_template (entry, item, chanceorquestchance, mincountorref, maxcount, condition_id, patch_max) values (1000251,81324,-100,1,1,0,10);
+
+delete from page_text where entry = 50500;
+replace into page_text (entry, text, next_page) value (50500,'To Magus Bromley\n\nThe Ley-Shard\'s you sent me were magnificent to behold. The way in which the Highborne have channeled magic has always been interesting to me, and shall advance the research of Dalaran well.\n\nThough, that is not why you have sent them. After some studying of the Ley-Shard\'s I have found traces of a foreign magic lingering upon the crystals and exhausting their magical potency. This all but confirms there is tampering going on within the region of Azshara as you have thought.',50501);
+
+delete from page_text where entry = 50501;
+replace into page_text (entry, text, next_page) value (50501,'Whilst, I have given you good news, I must also deliver bad. The foreign magic is that of a Draconic nature, we have many examples and studies done, so I am completely certain. The only group operating in that region would be the Blue Dragonflight. The reason they would have to do so I cannot give you, but it is their magic lingering upon the Shard\'s. I wish you luck in resolving this, and advise you look for outside help, rather then tackling the Dragonflight on your own.\n\nA friend, Archmage Ansirem Runeweaver of Dalaran.',0);
