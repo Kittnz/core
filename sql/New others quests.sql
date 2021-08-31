53c2fa20a61224d63292836ad924e57dc65e396e
Dormilon <79369417+Dormilon@users.noreply.github.com>
2021-08-31 16:47:46 +0300

    New quest: `Preparing for War`.

--- sql/New others quests.sql
+++ sql/New others quests.sql
@@ -45,3 +45,22 @@ replace into creature_loot_template (entry, item, chanceorquestchance, mincounto
 
 delete from creature_loot_template where entry = 2564 and item = 60108;
 replace into creature_loot_template (entry, item, chanceorquestchance, mincountorref, maxcount, condition_id, patch_max) values (2564,60108,-12,1,1,0,10);
+
+
+delete from quest_template where entry = 40017;
+replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel, minlevel, questflags, specialflags, title, details, objectives, requestitemstext, offerrewardtext, reqitemid1, reqitemcount1, reqitemid2, reqitemcount2, reqitemid3, reqitemcount3, reqitemid4, reqitemcount4, reqcreatureorgoid1, reqcreatureorgocount1, reqcreatureorgoid2, reqcreatureorgocount2, reqcreatureorgoid3, reqcreatureorgocount3, reqcreatureorgoid4, reqcreatureorgocount4, srcitemid, srcitemcount, reworreqmoney, rewxp, rewrepfaction1, rewrepvalue1,  rewrepfaction2, rewrepvalue2, rewrepfaction3, rewrepvalue3, rewrepfaction4, rewrepvalue4, rewspell, rewspellcast, completeemote, rewitemid1, rewitemcount1, rewitemid2, rewitemcount2, rewitemid3, rewitemcount3, rewitemid4, rewitemcount4, rewchoiceitemid1, rewchoiceitemcount1, rewchoiceitemid2, rewchoiceitemcount2, rewchoiceitemid3, rewchoiceitemcount3, rewchoiceitemid4, rewchoiceitemcount4) values (0,40017,2,45,35,25,0,0,'Preparing for War','Dere be a war $r , between us and the Witherbark and we must prepare for it. Yin\'do be telling me to created as much armor as I can for the upcoming battle, but my supplies are limited. If we are to survive against a stronger foe, then we need all the help we can get, especially from sturdy armor.\n\nI need you to gather me Arathi Raptor Hide from the beasts down south from the mountains. It be the strongest hide from the creatures in the region.','Collect 10 Arathi Raptor Hide from Raptor\'s in Arathi.','Have you gathered the war materials?','You\'ve done Wildtusk Village a favor, and me as well, take this silver, as a thanks for helping.',60119,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2500,2000,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+
+replace into creature_questrelation (id, quest) values (91400, 40017);
+replace into creature_involvedrelation (id, quest) values (91400, 40017);
+
+delete from item_template where entry = 60119;
+replace into item_template (entry, display_id, name, class, quality, flags, buy_count, allowable_class, allowable_race, item_level, stackable, spellcooldown_1, spellcategorycooldown_1, spellcooldown_2, spellcategorycooldown_2, bonding, description) values (60119,7594,'Arathi Raptor Hide',12,1,2048,1,-1,-1,1,10,-1,-1,-1,-1,4,'');
+
+delete from creature_loot_template where entry = 2559 and item = 60119;
+replace into creature_loot_template (entry, item, chanceorquestchance, mincountorref, maxcount, condition_id, patch_max) values (2559,60119,-20,1,1,0,10);
+
+delete from creature_loot_template where entry = 2561 and item = 60119;
+replace into creature_loot_template (entry, item, chanceorquestchance, mincountorref, maxcount, condition_id, patch_max) values (2561,60119,-20,1,1,0,10);
+
+delete from creature_loot_template where entry = 2560 and item = 60119;
+replace into creature_loot_template (entry, item, chanceorquestchance, mincountorref, maxcount, condition_id, patch_max) values (2560,60119,-20,1,1,0,10);
