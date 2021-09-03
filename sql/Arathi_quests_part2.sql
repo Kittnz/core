f08bdca198c1c6921f0224b880470b22a7d0e411
Dormilon <79369417+Dormilon@users.noreply.github.com>
2021-09-03 15:37:45 +0300

    New quest: `Spoils Left Behind`.

--- sql/Arathi_quests_part2.sql
+++ sql/Arathi_quests_part2.sql
@@ -130,3 +130,20 @@ replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel,
 
 replace into creature_questrelation (id, quest) values (91779, 40034);
 replace into creature_involvedrelation (id, quest) values (91776, 40034);
+
+
+delete from quest_template where entry = 40035;
+replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel, minlevel, questflags, specialflags, title, details, objectives, requestitemstext, offerrewardtext, reqitemid1, reqitemcount1, reqitemid2, reqitemcount2, reqitemid3, reqitemcount3, reqitemid4, reqitemcount4, reqcreatureorgoid1, reqcreatureorgocount1, reqcreatureorgoid2, reqcreatureorgocount2, reqcreatureorgoid3, reqcreatureorgocount3, reqcreatureorgoid4, reqcreatureorgocount4, srcitemid, srcitemcount, reworreqmoney, rewxp, rewrepfaction1, rewrepvalue1,  rewrepfaction2, rewrepvalue2, rewrepfaction3, rewrepvalue3, rewrepfaction4, rewrepvalue4, rewspell, rewspellcast, completeemote, rewitemid1, rewitemcount1, rewitemid2, rewitemcount2, rewitemid3, rewitemcount3, rewitemid4, rewitemcount4, rewchoiceitemid1, rewchoiceitemcount1, rewchoiceitemid2, rewchoiceitemcount2, rewchoiceitemid3, rewchoiceitemcount3, rewchoiceitemid4, rewchoiceitemcount4) values (40034,40035,2,16,54,45,0,0,'Spoils Left Behind','Before we left for the great depths the Rethress spent some time within Stranglethorn. We butted heads with other Naga there but otherwise kept low and to ourselves. Tide Mistress Rashal had something of a personal mission far in the east and we made the island of Jaguero into a temporary base.\n\nIt wasn\'t long before we returned to the great depths in order to begin our mission here, in our quickness we left behind a small trunk that contains spoils of great importance to us and for our future. Go there, and gather our Spoils!','Obtain the Rethress Spoils for Hashaz.','Have you got what I asked?','Many battles were fought for this, it is good to hear lives were not wasted for nothing. You are proving to be a good ally interloper.',60136,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,6000,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+
+replace into creature_questrelation (id, quest) values (91779, 40035);
+replace into creature_involvedrelation (id, quest) values (91779, 40035);
+
+delete from item_template where entry = 60136;
+replace into item_template (entry, display_id, name, class, quality, flags, buy_count, allowable_class, allowable_race, item_level, stackable, spellcooldown_1, spellcategorycooldown_1, spellcooldown_2, spellcategorycooldown_2, bonding, description) values (60136,7260,'Rethress Spoils',12,1,2048,1,-1,-1,1,1,-1,-1,-1,-1,4,'A large collection of wealth and trinkets');
+
+delete from gameobject_template where entry = 2010807;
+replace into gameobject_template values
+(2010807, 0, 3, 23430, 'Sealed Naga Trunk', 0, 4, 1, 43, 2010807, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');
+
+delete from gameobject_loot_template where entry = 2010807;
+replace into gameobject_loot_template (entry, item, chanceorquestchance, mincountorref, maxcount, condition_id, patch_max) values (2010807,60136,-100,1,1,0,10);
