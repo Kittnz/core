87872dfd81a107d08cd3f32ca3963a3899bf739e
Tanatos <tanatos@gmail.com>
2021-09-04 02:50:38 +0700

    update bosses sql part 1
    Signed-off-by: Tanatos <tanatos@gmail.com>

---
+++ sql/database_updates/world/worldrares_part1.sql
@@ -0,0 +1,5 @@
+UPDATE `creature_template` SET `script_name` = 'boss_tarangos' WHERE `entry` = '49001';
+UPDATE `creature_template` SET `script_name` = 'boss_blademaster_kargron' WHERE `entry` = '49002'; 
+UPDATE `creature_template` SET `script_name` = 'boss_xalvic_blackclaw' WHERE `entry` = '49003'; 
+UPDATE `creature_template` SET `script_name` = 'boss_mallon_the_moontouched' WHERE `entry` = '49004'; 
+UPDATE `creature_template` SET `script_name` = 'boss_grugthok_the_seer' WHERE `entry` = '49005'; 
