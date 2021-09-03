a48f593778cfd900bd673a7ed494dcb42e09f292
Torta <slowtorta@gmail.com>
2021-09-03 11:26:48 +0300

    Bunch of new Hazzuri trolls.

---
+++ sql/patch_miscellaneous_5.sql
@@ -0,0 +1,86 @@
+replace into creature_template values 
+(91809, 0, 4625, 0, 0, 0, 'Hazzuri Troll', NULL, 0, 47, 49, 2487, 2787, 0, 0, 2835, 28, 0, 1, 1.14286, 0, 20, 5, 0, 0, 1, 120, 144, 0, 212, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 62.8672, 86.4424, 100, 7, 0, 7809, 0, 0, 0, 70, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 43, 213, 'EventAI', 0, 3, 0, 0, 3, 7809, 0, 0, 0, 0, 0, 0, ''),
+(91810, 0, 4622, 0, 0, 0, 'Hazzuri Warrior', NULL, 0, 51, 53, 2887, 3100, 0, 0, 3035, 28, 0, 1, 1.14286, 0, 20, 5, 0, 0, 1, 200, 234, 0, 212, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 62.8672, 86.4424, 100, 7, 0, 7809, 0, 0, 0, 70, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 43, 213, 'EventAI', 0, 3, 0, 0, 3, 7809, 0, 0, 0, 0, 0, 0, ''),
+(91811, 0, 4621, 0, 0, 0, 'Hazzuri Hexer', NULL, 0, 50, 51, 2016, 2085, 4393, 4506, 1532, 28, 0, 0.666668, 0.992063, 0, 20, 5, 0, 0, 1, 54, 57, 0, 230, 1, 2000, 2000, 8, 0, 0, 0, 0, 0, 0, 0, 63.7824, 87.7008, 100, 7, 0, 6188, 6188, 0, 0, 0, 0, 0, 0, 0, 20295, 0, 0, 0, 61880, 0, 86, 118, 'EventAI', 0, 3, 0, 0, 3, 6188, 0, 0, 0, 0, 0, 0, ''),
+(91812, 0, 11158, 0, 0, 0, 'Hazzuri Darkcaller', NULL, 0, 52, 54, 8020, 8020, 9012, 9012, 1546, 28, 0, 1.1, 1.14286, 0, 20, 5, 0, 1, 1, 344, 444, 0, 230, 1, 1183, 2000, 8, 32768, 0, 0, 0, 0, 0, 0, 59.3256, 81.5727, 100, 7, 0, 7995, 7995, 0, 0, 0, 0, 0, 0, 0, 11641, 11642, 11639, 0, 79950, 0, 432, 569, 'EventAI', 1, 3, 0, 0, 3, 7995, 0, 0, 0, 0, 0, 0, ''),
+(91813, 0, 4633, 0, 0, 0, 'Darkmaster Haza\'gi ', NULL, 0, 54, 54, 16020, 16020, 9012, 9012, 1546, 28, 0, 1.1, 1.14286, 0, 20, 5, 0, 1, 1, 344, 444, 0, 230, 1, 1183, 2000, 8, 32768, 0, 0, 0, 0, 0, 0, 59.3256, 81.5727, 100, 7, 0, 7995, 7995, 0, 0, 0, 0, 0, 0, 0, 11641, 11642, 11639, 0, 79950, 0, 432, 569, 'EventAI', 1, 3, 0, 0, 3, 7995, 0, 0, 0, 0, 0, 0, ''),
+(91814, 0, 11167, 0, 0, 0, 'Hazzuri Defender', NULL, 0, 53, 54, 7359, 7599, 0, 0, 3216, 28, 0, 1.6, 1.14286, 0, 20, 5, 0, 1, 1, 344, 444, 0, 244, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 59.3256, 81.5727, 100, 7, 0, 11440, 11440, 0, 0, 54, 54, 54, 54, 54, 0, 0, 0, 0, 114400, 0, 414, 546, 'EventAI', 0, 3, 0, 0, 3, 11440, 0, 0, 0, 0, 0, 0, ''),
+(91815, 0, 11189, 0, 0, 0, 'Hazzuri Stalker', NULL, 0, 52, 55, 7186, 7414, 0, 0, 2916, 28, 0, 1, 1.14286, 0, 20, 5, 0, 1, 1, 335, 432, 0, 220, 1, 2000, 2000, 1, 32768, 0, 0, 0, 0, 0, 0, 58.688, 80.696, 100, 7, 0, 2644, 2644, 0, 0, 0, 0, 0, 0, 0, 7159, 0, 0, 0, 0, 0, 351, 463, 'EventAI', 0, 3, 0, 0, 3, 2644, 0, 0, 0, 0, 0, 0, ''),
+(91816, 0, 11075, 0, 0, 0, 'Hazzuri Headhunter', NULL, 0, 51, 52, 2645, 3376, 0, 0, 3026, 28, 0, 1.1, 1.14286, 0, 20, 5, 0, 0, 1, 344, 444, 0, 230, 1, 1183, 2000, 1, 32768, 0, 0, 0, 0, 0, 0, 59.3256, 81.5727, 100, 7, 0, 2648, 2648, 0, 0, 0, 0, 0, 0, 0, 8242, 0, 0, 0, 0, 0, 375, 495, 'EventAI', 0, 3, 0, 0, 3, 2648, 0, 0, 0, 0, 0, 0, ''),
+(91816, 0, 11075, 0, 0, 0, 'Hazzuri Headhunter', NULL, 0, 51, 52, 2645, 3376, 0, 0, 3026, 28, 0, 1.1, 1.14286, 0, 20, 5, 0, 0, 1, 344, 444, 0, 230, 1, 1183, 2000, 1, 32768, 0, 0, 0, 0, 0, 0, 59.3256, 81.5727, 100, 7, 0, 2648, 2648, 0, 0, 0, 0, 0, 0, 0, 8242, 0, 0, 0, 0, 0, 375, 495, 'EventAI', 0, 3, 0, 0, 3, 2648, 0, 0, 0, 0, 0, 0, ''),
+(91817, 0, 4582, 0, 0, 0, 'Champion Raggazi', NULL, 0, 54, 54, 17359, 17599, 0, 0, 4751, 28, 0, 1, 1.14286, 0, 20, 5, 0, 1, 1, 344, 444, 0, 244, 1, 2000, 2000, 1, 32768, 0, 0, 0, 0, 0, 0, 59.3256, 81.5727, 100, 7, 0, 9043, 9043, 0, 0, 0, 0, 0, 0, 0, 11976, 14895, 0, 0, 0, 0, 410, 540, 'EventAI', 1, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
+(91818, 0, 12671, 0, 0, 0, 'Chieftain Woh\'zo', NULL, 0, 55, 55, 20273, 20477, 6351, 6489, 2677, 28, 0, 1, 1.14286, 0, 20, 5, 0, 1, 1, 372, 479, 0, 254, 1, 2000, 2000, 2, 32832, 0, 0, 0, 0, 0, 0, 61.152, 84.084, 100, 7, 0, 11346, 11346, 0, 0, 0, 0, 0, 0, 0, 16006, 15501, 15982, 0, 0, 0, 371, 489, '', 1, 3, 0, 0, 3, 11346, 0, 0, 0, 0, 0, 0, 'generic_spell_ai'),
+(91819, 0, 320, 0, 0, 0, 'Hazzuri Tiger', NULL, 0, 50, 50, 2768, 3768, 0, 0, 2101, 28, 0, 1, 1.14286, 0, 20, 5, 0, 0, 1, 60, 74, 0, 226, 1, 2000, 2000, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2522, 0, 2522, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5833, 0, 0, '', 1, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 'generic_spell_ai'),
+(91820, 0, 4625, 0, 0, 0, 'Hazzuri Skinner', NULL, 0, 50, 50, 2487, 2787, 0, 0, 2835, 28, 0, 1, 1.14286, 0, 20, 5, 0, 0, 1, 150, 194, 0, 212, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 62.8672, 86.4424, 100, 7, 0, 7809, 0, 0, 0, 70, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 43, 213, 'EventAI', 0, 3, 0, 0, 3, 7809, 0, 0, 0, 0, 0, 0, ''),
+(91821, 0, 4580, 0, 0, 0, 'Hazzuri Beastkeeper', NULL, 0, 50, 52, 2487, 2987, 0, 0, 2835, 28, 0, 1, 1.14286, 0, 20, 5, 0, 0, 1, 140, 184, 0, 212, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 62.8672, 86.4424, 100, 7, 0, 7809, 0, 0, 0, 70, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 43, 213, 'EventAI', 0, 3, 0, 0, 3, 7809, 0, 0, 0, 0, 0, 0, ''),
+(91822, 0, 4609, 0, 0, 0, 'Hazzuri Fisherman', NULL, 0, 50, 52, 2487, 2987, 0, 0, 2835, 28, 0, 1, 1.14286, 0, 20, 5, 0, 0, 1, 110, 184, 0, 212, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 62.8672, 86.4424, 100, 7, 0, 7809, 0, 0, 0, 70, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 43, 213, 'EventAI', 0, 3, 0, 0, 3, 7809, 0, 0, 0, 0, 0, 0, ''),
+(91823, 0, 4071, 0, 0, 0, 'Hazzuri Darkpriest', NULL, 0, 51, 53, 2550, 2631, 5206, 5340, 2327, 28, 0, 1, 1.14286, 0, 20, 5, 0, 0, 1, 113, 146, 0, 258, 1, 2000, 2000, 8, 0, 0, 0, 0, 0, 0, 0, 72.688, 99.946, 100, 6, 0, 0, 0, 0, 0, 0, 0, 0, 100, 0, 17435, 0, 0, 0, 85260, 0, 102, 138, '', 1, 3, 0, 0, 3, 0, 0, 0, 8602131, 0, 0, 0, ''),
+(91824, 0, 4571, 0, 0, 0, 'Hazzuri Scavenger', NULL, 0, 50, 51, 2872, 2968, 0, 0, 2999, 28, 0, 1, 1.42857, 0, 20, 5, 0, 0, 1, 85, 105, 0, 226, 1, 2000, 2000, 1, 0, 0, 1, 0, 0, 0, 0, 66.44, 91.355, 100, 1, 1, 8960, 0, 8960, 0, 0, 0, 0, 0, 0, 3604, 17230, 0, 0, 0, 0, 0, 0, 'EventAI', 1, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, '');
+
+update creature_template set loot_id = 0 where entry between 91809 and 91824;
+
+set @equip_template = 55088; set @weapon_1 = 2092; set @weapon_2 = 0; set @creature = 91809;
+replace into creature_equip_template values (@equip_template, 0, @weapon_1, @weapon_2, 0); 
+update creature_template set equipment_id = @equip_template where entry = @creature;
+
+set @equip_template = 55089; set @weapon_1 = 37; set @weapon_2 = 0; set @creature = 91810;
+replace into creature_equip_template values (@equip_template, 0, @weapon_1, @weapon_2, 0); 
+update creature_template set equipment_id = @equip_template where entry = @creature;
+
+set @equip_template = 55090; set @weapon_1 = 1317; set @weapon_2 = 0; set @creature = 91811;
+replace into creature_equip_template values (@equip_template, 0, @weapon_1, @weapon_2, 0); 
+update creature_template set equipment_id = @equip_template where entry = @creature;
+
+set @equip_template = 55091; set @weapon_1 = 2503; set @weapon_2 = 0; set @creature = 91812;
+replace into creature_equip_template values (@equip_template, 0, @weapon_1, @weapon_2, 0); 
+update creature_template set equipment_id = @equip_template where entry = @creature;
+
+set @equip_template = 55092; set @weapon_1 = 13339; set @weapon_2 = 0; set @creature = 91813;
+replace into creature_equip_template values (@equip_template, 0, @weapon_1, @weapon_2, 0); 
+update creature_template set equipment_id = @equip_template where entry = @creature;
+
+set @equip_template = 55093; set @weapon_1 = 3491; set @weapon_2 = 13319; set @creature = 91814;
+replace into creature_equip_template values (@equip_template, 0, @weapon_1, @weapon_2, 0); 
+update creature_template set equipment_id = @equip_template where entry = @creature;
+
+set @equip_template = 55094; set @weapon_1 = 3494; set @weapon_2 = 3494; set @creature = 91815;
+replace into creature_equip_template values (@equip_template, 0, @weapon_1, @weapon_2, 0); 
+update creature_template set equipment_id = @equip_template where entry = @creature;
+
+set @equip_template = 55095; set @weapon_1 = 12449; set @weapon_2 = 0; set @creature = 91816;
+replace into creature_equip_template values (@equip_template, 0, @weapon_1, @weapon_2, 0); 
+update creature_template set equipment_id = @equip_template where entry = @creature;
+
+set @equip_template = 55096; set @weapon_1 = 9486; set @weapon_2 = 0; set @creature = 91817;
+replace into creature_equip_template values (@equip_template, 0, @weapon_1, @weapon_2, 0); 
+update creature_template set equipment_id = @equip_template where entry = @creature;
+
+set @equip_template = 55097; set @weapon_1 = 4022; set @weapon_2 = 0; set @creature = 91818;
+replace into creature_equip_template values (@equip_template, 0, @weapon_1, @weapon_2, 0); 
+update creature_template set equipment_id = @equip_template where entry = @creature;
+
+set @equip_template = 55098; set @weapon_1 = 4571; set @weapon_2 = 0; set @creature = 91820;
+replace into creature_equip_template values (@equip_template, 0, @weapon_1, @weapon_2, 0); 
+update creature_template set equipment_id = @equip_template where entry = @creature;
+
+set @equip_template = 55099; set @weapon_1 = 15231; set @weapon_2 = 0; set @creature = 91821;
+replace into creature_equip_template values (@equip_template, 0, @weapon_1, @weapon_2, 0); 
+update creature_template set equipment_id = @equip_template where entry = @creature;
+
+set @equip_template = 55100; set @weapon_1 = 6256; set @weapon_2 = 0; set @creature = 91822;
+replace into creature_equip_template values (@equip_template, 0, @weapon_1, @weapon_2, 0); 
+update creature_template set equipment_id = @equip_template where entry = @creature;
+
+set @equip_template = 55101; set @weapon_1 = 3414; set @weapon_2 = 0; set @creature = 91823;
+replace into creature_equip_template values (@equip_template, 0, @weapon_1, @weapon_2, 0); 
+update creature_template set equipment_id = @equip_template where entry = @creature;
+
+set @equip_template = 55102; set @weapon_1 = 2092; set @weapon_2 = 0; set @creature = 91824;
+replace into creature_equip_template values (@equip_template, 0, @weapon_1, @weapon_2, 0); 
+update creature_template set equipment_id = @equip_template where entry = @creature;
+
+set @gossip_menu_id = 60115; set @magic_number = 2543; 
+replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); 
+replace into broadcast_text (entry, Male_Text) values (@magic_number, 'I am Ansirem Runeweaver, the one in charge of our external affairs.\n\nThe Kirin Tor has survived the onslaught of the demons and are currently rebuilding Dalaran.\n\nI am afraid I cannot let you within our shield of protection, nor offer you shelter aside from the ruins nearby.\n\n Perhaps you have come for a different reason?'); 
+replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_number); 
+update creature_template set gossip_menu_id = @gossip_menu_id where entry = @magic_number;
