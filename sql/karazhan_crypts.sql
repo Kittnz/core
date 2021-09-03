93e79802ecd3d4d978b745256f16fb777a98a1ee
Torta <slowtorta@gmail.com>
2021-09-03 23:32:17 +0300

    Fix Karazhan Crypt's portal destination.

--- sql/karazhan_crypts.sql
+++ sql/karazhan_crypts.sql
@@ -1,7 +1,7 @@
 replace into map_template (entry, map_name) values ('800', 'Karazhan Crypt');
 
-replace into gameobject_template values (181580, 0, 5, 23442, 'Karazhan Crypt (Entrance)', 0, 0, 1.6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'karazhan_crypts_portal');
-replace into gameobject_template values (181581, 0, 5, 23442, 'Karazhan Crypt (Exit)', 0, 0, 1.6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'karazhan_crypts_portal');
+replace into gameobject_template values (181580, 0, 5, 23442, 'Karazhan Crypt (Entrance)', 0, 0, 1.6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'karazhan_crypt_portal');
+replace into gameobject_template values (181581, 0, 5, 23442, 'Karazhan Crypt (Exit)', 0, 0, 1.6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'karazhan_crypt_portal');
 replace into gameobject_template values (181582, 0, 5, 23443, 'Karazhan Crypt (Collision)', 0, 0, 1.6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');
 
 update gameobject_template set type = 0, flags = 32, data0 = 0, name = 'Karazhan Crypt', script_name = 'karazhan_crypts_gate' where entry = 177203;
