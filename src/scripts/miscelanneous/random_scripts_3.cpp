e0d7c5303aa16fca2bf1630b9b0fce497403e508
Torta <slowtorta@gmail.com>
2021-09-04 20:40:09 +0300

    Challenge The Cow King and die 🐮

--- src/scripts/miscelanneous/random_scripts_3.cpp
+++ src/scripts/miscelanneous/random_scripts_3.cpp
@@ -95,9 +95,133 @@ bool GossipSelect_analyzor_53(Player* pPlayer, Creature* pCreature, uint32 uiSen
     return true;
 }
 
+bool GossipHello_the_cow_king(Player* pPlayer, Creature* pCreature)
+{
+    pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TALK, "King Moo, I wish to challenge you.", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+    pPlayer->SEND_GOSSIP_MENU(91799, pCreature->GetGUID());
+    return true;
+}
+
+bool GossipSelect_the_cow_king(Player* pPlayer, Creature* pCreature, uint32 uiSender, uint32 uiAction)
+{
+    if (uiAction == GOSSIP_ACTION_INFO_DEF + 1)
+    {
+        DoAfterTime(pPlayer, 3 * IN_MILLISECONDS, [player = pPlayer, creature = pCreature]() {
+            creature->MonsterSayToPlayer("Very well, young one...", player);
+            creature->SetFactionTemporary(14, TEMPFACTION_RESTORE_COMBAT_STOP);
+            creature->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
+            creature->HandleEmote(EMOTE_ONESHOT_ATTACK1H);
+            });
+    }
+    pPlayer->CLOSE_GOSSIP_MENU();
+    return true;
+}
+
+enum cow_king_spells
+{
+    SPELL_CLEAVE = 15284,
+    SPELL_MORTAL_STRIKE = 16856,
+    SPELL_THUNDERCLAP = 23931,
+    SPELL_UPPERCUT = 22916,
+    SPELL_CHARGE = 22911,
+    SPELL_WARSTOMP = 16727,
+};
+
+struct the_cow_kingAI : public ScriptedAI
+{
+public:
+    the_cow_kingAI(Creature* pCreature) : ScriptedAI(pCreature) { Reset(); }
+
+    void Reset() override
+    {
+        m_uiCleaveTimer = 10000;
+        m_uiMortalStrikeTimer = 15000;
+        m_uiUppercutTimer = 5000;
+        m_uiChargeTimer = 20000;
+        m_uiWarstompTimer = 0;
+    }
+
+    void SpellHitTarget(Unit* /*pTarget*/, const SpellEntry* pSpell) override
+    {
+        if (pSpell->Id == SPELL_CHARGE)
+            m_uiWarstompTimer = 500;
+    }
+
+    void UpdateAI(const uint32 uiDiff) override
+    {
+        if (!m_creature->SelectHostileTarget() || !m_creature->GetVictim())
+            return;
+
+        if (m_uiCleaveTimer <= uiDiff)
+        {
+            if (DoCastSpellIfCan(m_creature->GetVictim(), SPELL_CLEAVE) == CAST_OK)
+                m_uiCleaveTimer = 10000;
+        }
+        else
+            m_uiCleaveTimer -= uiDiff;
+
+        if (m_uiMortalStrikeTimer <= uiDiff)
+        {
+            if (DoCastSpellIfCan(m_creature->GetVictim(), SPELL_MORTAL_STRIKE) == CAST_OK)
+                m_uiMortalStrikeTimer = 15000;
+        }
+        else
+            m_uiMortalStrikeTimer -= uiDiff;
+
+        if (m_uiUppercutTimer <= uiDiff)
+        {
+            if (DoCastSpellIfCan(m_creature->GetVictim(), SPELL_UPPERCUT) == CAST_OK)
+                m_uiUppercutTimer = 20000;
+        }
+        else
+            m_uiUppercutTimer -= uiDiff;
+
+        if (m_uiChargeTimer <= uiDiff)
+        {
+            if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 1))
+            {
+                if (DoCastSpellIfCan(pTarget, SPELL_CHARGE) == CAST_OK)
+                    m_uiChargeTimer = urand(15000, 25000);
+            }
+        }
+        else
+            m_uiChargeTimer -= uiDiff;
+
+        if (m_uiWarstompTimer)
+        {
+            if (m_uiWarstompTimer <= uiDiff)
+            {
+                if (DoCastSpellIfCan(m_creature, SPELL_WARSTOMP) == CAST_OK)
+                    m_uiWarstompTimer = 0;
+            }
+            else
+                m_uiWarstompTimer -= uiDiff;
+        }
+
+        DoMeleeAttackIfReady();
+        EnterEvadeIfOutOfCombatArea(uiDiff);
+    }
+
+private:
+    uint32 m_uiCleaveTimer;
+    uint32 m_uiMortalStrikeTimer;
+    uint32 m_uiUppercutTimer;
+    uint32 m_uiChargeTimer;
+    uint32 m_uiWarstompTimer;
+};
+
+CreatureAI* GetAI_the_cow_king(Creature* pCreature){ return new the_cow_kingAI(pCreature); }
+
 void AddSC_random_scripts_3()
 {
     Script* newscript;
+
+    newscript = new Script;
+    newscript->Name = "the_cow_king";
+    newscript->pGossipHello = &GossipHello_the_cow_king;
+    newscript->pGossipSelect = &GossipSelect_the_cow_king;
+    newscript->GetAI = &GetAI_the_cow_king;
+    newscript->RegisterSelf();
 
     newscript = new Script;
     newscript->Name = "analyzor_53";
