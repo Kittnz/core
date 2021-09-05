9bef7df0a846df34f281aedc6c9e3f326e7371c5
Torta <slowtorta@gmail.com>
2021-09-05 10:25:04 +0300

    Misc. DB updates.

--- sql/patch_miscellaneous_6.sql
+++ sql/patch_miscellaneous_6.sql
@@ -5,21 +5,34 @@ replace into creature_template values
 (91902, 0, 18145, 0, 0, 0, 'Molasses', '', 0, 35, 35, 1342, 1342, 0, 0, 1357, 35, 2, 1.11, 1.14286, 1.8, 20, 5, 0, 0, 1, 51, 65, 0, 138, 1, 2000, 2000, 1, 768, 0, 0, 0, 0, 0, 0, 51.128, 70.301, 100, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 2, 0, ''),
 (91903, 0, 18145, 0, 0, 0, 'Duke', '', 0, 35, 35, 1342, 1342, 0, 0, 1357, 35, 2, 1.11, 1.14286, 1.8, 20, 5, 0, 0, 1, 51, 65, 0, 138, 1, 2000, 2000, 1, 768, 0, 0, 0, 0, 0, 0, 51.128, 70.301, 100, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 2, 0, ''),
 (91904, 0, 18144, 0, 0, 0, 'Larry', '', 0, 35, 35, 1342, 1342, 0, 0, 1357, 35, 2, 1.11, 1.14286, 1.8, 20, 5, 0, 0, 1, 51, 65, 0, 138, 1, 2000, 2000, 1, 768, 0, 0, 0, 0, 0, 0, 51.128, 70.301, 100, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 2, 0, ''),
-(91905, 0, 12610, 0, 0, 0, 'Sailor Darkbay', 'Kul Tiras', 0, 45, 53, 1981, 4281, 0, 0, 2246, 15, 0, 1.11111, 1.14286, 0, 20, 5, 0, 0, 1, 300, 345, 0, 172, 1, 2000, 2000, 1, 768, 0, 0, 0, 0, 0, 0, 58.7664, 80.8038, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 1, 1, 0, 0, 3, 0, 0, 0, 0, 0, 524290, 0, '');
+(91905, 0, 12610, 0, 0, 0, 'Sailor Darkbay', 'Kul Tiras', 0, 45, 53, 1981, 4281, 0, 0, 2246, 15, 0, 1.11111, 1.14286, 0, 20, 5, 0, 0, 1, 300, 345, 0, 172, 1, 2000, 2000, 1, 768, 0, 0, 0, 0, 0, 0, 58.7664, 80.8038, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 1, 1, 0, 0, 3, 0, 0, 0, 0, 0, 524290, 0, ''),
+(91906, 0, 2423, 0, 0, 0, 'Deepwater Hydra', '', 0, 53, 53, 4554, 4945, 0, 0, 2808, 16, 0, 1, 1.14286, 0, 20, 5, 0, 1, 1, 243, 313, 0, 212, 1, 2000, 2000, 1, 32832, 0, 0, 0, 0, 0, 0, 50.796, 69.8445, 100, 10, 8, 12207, 0, 12207, 0, 0, 0, 0, 0, 0, 3417, 21790, 0, 0, 122070, 0, 0, 0, 'EventAI', 1, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, '');
 
 update creature_template set npc_flags = 1, faction = 189, unit_flags = 2, scale = 2.5 where entry = 91799;
 update creature_template set display_id1 = 18149 where entry = 91800;
 update creature_template set display_id1 = 3264 where entry = 91886;
+update creature_template set display_id1 = 3264 where entry = 91773;
 update creature_template set rank = 1 where entry = 70891;
 update creature_template set scale = 1.2 where entry = 91797;
 update creature_template set scale = 1.3 where entry = 91798;
+update creature_template set scale = 1.4 where entry = 91831;
 update quest_template set suggestedplayers = 3 where entry = 80291;
+update creature_template set name = 'Wallowfin Murloc' where entry = 91857;
+update creature_template set name = 'Primalist Jongi' where entry = 91867;
+update creature_template set name = 'Cannoneer Darlug' where entry = 91886;
+update creature_template set equipment_id = 0, level_min = 55, level_max = 55 where entry = 91891;
+update creature_template set equipment_id = 0 where entry = 91865;
+update creature_template set inhabit_type = 2 where entry = 91837;
 
 replace into creature_template_addon values 
 (91881, 0, 0, 0, 0, 0, 234, 0, NULL),
 (91885, 0, 0, 0, 0, 0, 234, 0, NULL),
 (91880, 0, 0, 0, 0, 0, 234, 0, NULL),
 (91884, 0, 0, 0, 0, 0, 234, 0, NULL);
+
+set @equip_template = 55140; set @weapon_1 = 14879; set @weapon_2 = 0; set @creature = 91305;
+replace into creature_equip_template values (@equip_template, 0, @weapon_1, @weapon_2, 0); 
+update creature_template set equipment_id = @equip_template where entry = @creature;
 
 set @gossip_menu_id = 60117; set @magic_number = 91799; 
 replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); 
