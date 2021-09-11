3c10977f0b0839f9d846f9d037d29bae287bcf50
Dormilon <79369417+Dormilon@users.noreply.github.com>
2021-09-11 17:34:05 +0300

    New quest: `Mysterious Cow Scroll V`.

--- sql/cow_scrolls.sql
+++ sql/cow_scrolls.sql
@@ -48,3 +48,14 @@ replace into gameobject_involvedrelation (id, quest) values (2010813, 40054);
 delete from gameobject_template where entry = 2010813;
 replace into gameobject_template values
 (2010813, 0, 2, 22656, 'Mysterious Sunken Chest', 0, 4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');
+
+
+delete from quest_template where entry = 40055;
+replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel, minlevel, questflags, specialflags, title, details, objectives, requestitemstext, offerrewardtext, reqitemid1, reqitemcount1, reqitemid2, reqitemcount2, reqitemid3, reqitemcount3, reqitemid4, reqitemcount4, reqcreatureorgoid1, reqcreatureorgocount1, reqcreatureorgoid2, reqcreatureorgocount2, reqcreatureorgoid3, reqcreatureorgocount3, reqcreatureorgoid4, reqcreatureorgocount4, srcitemid, srcitemcount, reworreqmoney, rewxp, rewrepfaction1, rewrepvalue1,  rewrepfaction2, rewrepvalue2, rewrepfaction3, rewrepvalue3, rewrepfaction4, rewrepvalue4, rewspell, rewspellcast, completeemote, rewitemid1, rewitemcount1, rewitemid2, rewitemcount2, rewitemid3, rewitemcount3, rewitemid4, rewitemcount4, rewchoiceitemid1, rewchoiceitemcount1, rewchoiceitemid2, rewchoiceitemcount2, rewchoiceitemid3, rewchoiceitemcount3, rewchoiceitemid4, rewchoiceitemcount4,requiredminrepfaction,requiredminrepvalue,objectivetext1) values (40054,40055,2,876,60,5,0,0,'Mysterious Cow Scroll V','The hunt for the final clue. It will be incredibly dangerous, handle yourself with care little moo. It is a place where those with green skin came to our world.','Find the Final Clue.','','<Hidden within the Mysterious Charred Locker you find a secretive scroll>.',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,500,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'');
+
+replace into gameobject_questrelation (id, quest) values (2010813, 40055);
+replace into gameobject_involvedrelation (id, quest) values (2010814, 40055);
+
+delete from gameobject_template where entry = 2010814;
+replace into gameobject_template values
+(2010814, 0, 2, 23430, 'Mysterious Charred Locker', 0, 4, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');
