aa18cb19beb13267f2b3e7a49abf0e1c823e4bf1
Torta <slowtorta@gmail.com>
2021-09-22 14:01:42 +0200

    Vilethorn Grove: mode creatures and boss templates.

--- sql/general_fixes.sql
+++ sql/general_fixes.sql
@@ -1,1 +1,9 @@
 update creature_template set speed_walk = 1 where entry = 80506;
+update creature_template set equipment_id = 0, name = '\'Sly\' Duncan' where entry = 92022;
+
+replace into npc_vendor values (92022, 4604, 0, 0, 0, 0);
+replace into npc_vendor values (92022, 4605, 0, 0, 0, 0);
+replace into npc_vendor values (92022, 4606, 0, 0, 0, 0);
+replace into npc_vendor values (92022, 4607, 0, 0, 0, 0);
+replace into npc_vendor values (92022, 4608, 0, 0, 0, 0);
+replace into npc_vendor values (92022, 8948, 0, 0, 0, 0);
