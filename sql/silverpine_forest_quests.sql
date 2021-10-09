e47bbee4a57e22d2de73722c31346bf51839b810
Dormilon <79369417+Dormilon@users.noreply.github.com>
2021-10-09 13:58:18 +0300

    New quest: `Divide and Conquer`.

--- sql/silverpine_forest_quests.sql
+++ sql/silverpine_forest_quests.sql
@@ -93,3 +93,19 @@ replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel,
 
 replace into creature_questrelation (id, quest) values (91712, 40096);
 replace into creature_involvedrelation (id, quest) values (91712, 40096);
+
+-- Divide and Conquer --
+delete from quest_template where entry = 40097;
+replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel, minlevel, questflags, specialflags, title, details, objectives, requestitemstext, offerrewardtext, reqitemid1, reqitemcount1, reqitemid2, reqitemcount2, reqitemid3, reqitemcount3, reqitemid4, reqitemcount4, reqcreatureorgoid1, reqcreatureorgocount1, reqcreatureorgoid2, reqcreatureorgocount2, reqcreatureorgoid3, reqcreatureorgocount3, reqcreatureorgoid4, reqcreatureorgocount4, srcitemid, srcitemcount, reworreqmoney, rewxp, rewrepfaction1, rewrepvalue1,  rewrepfaction2, rewrepvalue2, rewrepfaction3, rewrepvalue3, rewrepfaction4, rewrepvalue4, rewspell, rewspellcast, completeemote, rewitemid1, rewitemcount1, rewitemid2, rewitemcount2, rewitemid3, rewitemcount3, rewitemid4, rewitemcount4, rewchoiceitemid1, rewchoiceitemcount1, rewchoiceitemid2, rewchoiceitemcount2, rewchoiceitemid3, rewchoiceitemcount3, rewchoiceitemid4, rewchoiceitemcount4,requiredminrepfaction,requiredminrepvalue,objectivetext4) values (40096,40097,2,85,19,15,0,0,'Divide and Conquer','With their Shaman dead, it is time for the final part of the plan. You are to return to Gnolls camp and kill their Chief Rnarl as well as those around him. Make it look like there was a large-scale battle, and after you are done plant this in their base.\n\n<The Duke hands you a Lordaeron Banner>.\n\nHopefully, this will be enough to convince those still alive, that the assault on their camp was orchestrated by the Humans and not us.','Duke Nargelas wants me to kill Chief Rnarl and his guards and pin the assault on the Humans by planting their Banner in the Gnoll camp.','Yes?','Hahaha... Good to see you back here in one piece! My scouts tell me that the Gnolls launched an assault on the Human base. Soon we will be able to retake this land to its rightful rulers.',0,0,0,0,0,0,0,0,91736,6,91737,6,91716,1,60323,1,60189,1,1700,1900,68,200,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'The Banner of Lordaeron is established');
+
+replace into creature_questrelation (id, quest) values (91712, 40097);
+replace into creature_involvedrelation (id, quest) values (91712, 40097);
+
+delete from creature_template where entry = 60323;
+replace into creature_template (entry, name, display_id1) values (60323, 'quest_40097_dummy_triger', 328);
+
+delete from item_template where entry = 60189;
+replace into item_template (entry, display_id, name, class, quality, flags, buy_count, allowable_class, allowable_race, item_level, stackable, spellcooldown_1, spellcategorycooldown_1, spellcooldown_2, spellcategorycooldown_2, bonding, description, page_text) values (60189,932,'Lordaeron Banner',12,1,2048,1,-1,-1,1,1,-1,-1,-1,-1,4,'A Banner used by the Remnants of Lordaeron',0);
+
+delete from gameobject_template where entry = 2010824;
+replace into gameobject_template (entry, type, displayid, size, name, flags, script_name) values (2010824, 2, 49, 0.8, 'Pile of Dirt', 32, 'go_pile_of_dirt');
