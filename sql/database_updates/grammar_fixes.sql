e8662f16c310a05968f65f699c8a33a56da3d0a6
Torta <slowtorta@gmail.com>
2021-10-11 12:45:15 +0300

    Added some missing item_templates.

---
+++ sql/database_updates/grammar_fixes.sql
@@ -0,0 +1,4 @@
+
+update item_template set description = 'Be sure to save up on some acorns and to share your undivided love and attention with Mr. Tails, and he\'ll become your best companion on your long journey throughout Azeroth.' where entry = 83272;
+update item_template description = 'Happy Birthday, Turtle WoW!' where entry = 83271;
+update item_template description = 'Parties are full of joy and presents. But what joy is there without any schnapps?' where entry = 51020;
