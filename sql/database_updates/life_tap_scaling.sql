0d6f28c9a9e313e922163a84300ae5e1667e6b07
Torta <slowtorta@gmail.com>
2021-10-12 17:33:57 +0300

    Update Tonal Stone: Kamio to be a trinket item.

--- sql/database_updates/life_tap_scaling.sql
+++
@@ -1,4 +0,0 @@
-
--- Fix Life Tap scaling
-UPDATE `spell_template` SET `effectBonusCoefficient1` = 0.80 WHERE `entry` IN
-(1454, 1455, 1456, 11687, 11688, 11689);
--- sql/database_updates/petrification_flask.sql
+++
@@ -1,3 +0,0 @@
-
--- Flask of Petrification should not stack with other flasks.
-UPDATE `spell_elixir` SET `mask`=3 WHERE `entry` = 17624;
