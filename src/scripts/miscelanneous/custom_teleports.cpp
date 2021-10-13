b8df0183be3b092e07be7e91d7201b0f88ef48cf
Torta <slowtorta@gmail.com>
2021-10-13 18:11:00 +0300

    Update for custom portals.

--- src/scripts/miscelanneous/custom_teleports.cpp
+++ src/scripts/miscelanneous/custom_teleports.cpp
@@ -21,27 +21,33 @@ struct custom_dungeon_portal : public GameObjectAI
             PlayerListSearcher<AnyPlayerInObjectRangeCheck> searcher(players, check);
             Cell::VisitWorldObjects(me, searcher, 2.0f);
 
-            for (Player* pPlayer : players)
+            for (Player* player : players)
             {
-                if (!pPlayer->IsInCombat())
+                if (!player->IsInCombat())
                 {
-                    if (!pPlayer->IsAlive())
+                    if (!player->IsAlive())
                     {
-                        pPlayer->ResurrectPlayer(0.5f);
-                        pPlayer->SpawnCorpseBones();
+                        player->ResurrectPlayer(0.5f);
+                        player->SpawnCorpseBones();
                     }
 
-                    switch (me->GetEntry())
+                    array<pair<uint32, WorldLocation>, 8> portals_and_locations =
+                    {{
+                        { 181580, WorldLocation{800, -11068.1F, -1806.4F, 52.7F, 1.5F} },
+                        { 181581, WorldLocation{0, -11068.9F, -1828.6F, 60.26F, 3.1F} },
+                        { 112911, WorldLocation{802, 579.13F, 90.70F, 276.11F, 3.4F} },
+                        { 112912, WorldLocation{1, 1722.0F, -1272.6F, 163.26F, 5.8F} },
+                        { 112915, WorldLocation{269, -2002.5F, 6575.3F, -154.9F, 5.7F} },
+                        { 112916, WorldLocation{1, -8756.8F, -4191.3F, -209.4F, 5.5F} },
+                        { 112917, WorldLocation{35, -1.15F, 44.4F, -25.58F, 1.6F} },
+                        { 112918, WorldLocation{0, -8677.6F, 637.0F, 96.9F, 5.3F} },
+                    }};
+
+                    for (auto const& teleport : portals_and_locations)
                     {
-                    case 181580: pPlayer->TeleportTo(800, -11068.1F, -1806.4F, 52.7F, 1.5F); break; // Karazhan Crypt (Entrance)
-                    case 181581: pPlayer->TeleportTo(0, -11068.9F, -1828.6F, 60.26F, 3.1F);  break; // Karazhan Crypt (Exit)
-                    case 112911: pPlayer->TeleportTo(802, 579.13F, 90.70F, 276.11F, 3.4F);   break; // The Crescent Grove (Entrance)
-                    case 112912: pPlayer->TeleportTo(1, 1722.0F, -1272.6F, 163.26F, 5.8F);   break; // The Crescent Grove (Exit)
-                    case 112915: pPlayer->TeleportTo(269, -2002.5F, 6575.3F, -154.9F, 5.7F); break; // Black Morass (Entrance)
-                    case 112916: pPlayer->TeleportTo(1, -8756.8F, -4191.3F, -209.4F, 5.5F);  break; // Black Morass (Exit)
-                    case 112917: pPlayer->TeleportTo(35, -1.15F, 44.4F, -25.58F, 1.6F);      break; // Stromwind Vault (Entrance)
-                    case 112918: pPlayer->TeleportTo(0, -8677.6F, 637.0F, 96.9F, 5.3F);      break; // Stromwind Vault (Exit)
-                    }                     
+                        if (me->GetEntry() == teleport.first)
+                            player->TeleportTo(teleport.second);
+                    }                  
                 }
             }
             m_uiUpdateTimer = 1000;
@@ -52,7 +58,6 @@ struct custom_dungeon_portal : public GameObjectAI
 };
 
 GameObjectAI* GetAI_custom_dungeon_portal(GameObject* gameobject) { return new custom_dungeon_portal(gameobject); }
-
 
 void AddSC_custom_teleports()
 {
