bd63c989870e361392d8db5e1b6ed850360e4c51
Dormilon <79369417+Dormilon@users.noreply.github.com>
2021-10-14 16:55:57 +0300

    New quest: `An Extravagent Book`.

--- sql/glenshire_and_other_quests.sql
+++ sql/glenshire_and_other_quests.sql
@@ -918,3 +918,20 @@ REPLACE INTO npc_vendor VALUES
 (60460, 2686, 0, 0, 0, 0),
 (60460, 17402, 0, 0, 0, 0);
 -- Vil'do One-Tusk <Pirate Booty> sells junk will make later.
+
+-- An Extravagent Book --
+delete from quest_template where entry = 40153;
+replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel, minlevel, questflags, specialflags, title, details, objectives, requestitemstext, offerrewardtext, reqitemid1, reqitemcount1, reqitemid2, reqitemcount2, reqitemid3, reqitemcount3, reqitemid4, reqitemcount4, reqcreatureorgoid1, reqcreatureorgocount1, reqcreatureorgoid2, reqcreatureorgocount2, reqcreatureorgoid3, reqcreatureorgocount3, reqcreatureorgoid4, reqcreatureorgocount4, srcitemid, srcitemcount, reworreqmoney, rewxp, rewrepfaction1, rewrepvalue1,  rewrepfaction2, rewrepvalue2, rewrepfaction3, rewrepvalue3, rewrepfaction4, rewrepvalue4, rewspell, rewspellcast, completeemote, rewitemid1, rewitemcount1, rewitemid2, rewitemcount2, rewitemid3, rewitemcount3, rewitemid4, rewitemcount4, rewchoiceitemid1, rewchoiceitemcount1, rewchoiceitemid2, rewchoiceitemcount2, rewchoiceitemid3, rewchoiceitemcount3, rewchoiceitemid4, rewchoiceitemcount4,requiredminrepfaction,requiredminrepvalue,objectivetext1) values (0,40153,2,409,52,45,0,0,'An Extravagent Book','<The barrel appears to still be held together quite well and looks to be quite sturdy. Upon searching further you find an extravagent book, it is certainly old, but still has golden thread on the spine. It has the label \'Great Virtue - Father Ben.\' written upon the front, perhaps this belongs to someone>.','Find the owner of the book.','Yes child? Are you here to learn about the light?','Oh... I had left this book upon an elvish vessel quite some time ago, you managed to recover it? I must say, I had considered this lost after the news of what happened, thank you so much my child, for returning this book.',60225,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,60225,1,0,4650,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'');
+
+replace into gameobject_questrelation (id, quest) values (2010830, 40153);
+replace into creature_involvedrelation (id, quest) values (91953, 40153);
+
+update quest_template set requiredraces = 1101 where entry = 40153;
+update creature_template set npc_flags = 3 where entry = 91953;
+
+delete from item_template where entry = 60225;
+replace into item_template (entry, display_id, name, class, quality, flags, buy_count, allowable_class, allowable_race, item_level, stackable, spellcooldown_1, spellcategorycooldown_1, spellcooldown_2, spellcategorycooldown_2, bonding, description, page_text) values
+(60225,1134,'Great Virtue',12,1,2048,1,-1,-1,1,1,-1,-1,-1,-1,4,'',0);
+
+delete from gameobject_template where entry = 2010830;
+replace into gameobject_template (entry, type, displayid, size, name, flags, script_name) values (2010830, 2, 23262, 1, 'Half Buried Barrel', 32, '');
