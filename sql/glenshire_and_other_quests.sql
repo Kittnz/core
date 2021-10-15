bf7fc503a2034309f28e1d3a5bf473aabbb931da
Dormilon <79369417+Dormilon@users.noreply.github.com>
2021-10-15 12:55:09 +0300

    New quest: `The Tower of Lapidis IX`.

--- sql/glenshire_and_other_quests.sql
+++ sql/glenshire_and_other_quests.sql
@@ -1245,3 +1245,19 @@ replace into item_template (entry, display_id, name, class, quality, flags, buy_
 
 replace into creature_loot_template (entry, item, chanceorquestchance, mincountorref, maxcount, condition_id, patch_max) values
 (60461,60243,-100,1,1,0,10);
+
+-- The Tower of Lapidis IX --
+delete from quest_template where entry = 40171;
+replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel, minlevel, questflags, specialflags, title, details, objectives, requestitemstext, offerrewardtext, reqitemid1, reqitemcount1, reqitemid2, reqitemcount2, reqitemid3, reqitemcount3, reqitemid4, reqitemcount4, reqcreatureorgoid1, reqcreatureorgocount1, reqcreatureorgoid2, reqcreatureorgocount2, reqcreatureorgoid3, reqcreatureorgocount3, reqcreatureorgoid4, reqcreatureorgocount4, srcitemid, srcitemcount, reworreqmoney, rewxp, rewrepfaction1, rewrepvalue1,  rewrepfaction2, rewrepvalue2, rewrepfaction3, rewrepvalue3, rewrepfaction4, rewrepvalue4, rewspell, rewspellcast, completeemote, rewitemid1, rewitemcount1, rewitemid2, rewitemcount2, rewitemid3, rewitemcount3, rewitemid4, rewitemcount4, rewchoiceitemid1, rewchoiceitemcount1, rewchoiceitemid2, rewchoiceitemcount2, rewchoiceitemid3, rewchoiceitemcount3, rewchoiceitemid4, rewchoiceitemcount4,requiredminrepfaction,requiredminrepvalue,objectivetext1) values (40170,40171,2,409,55,45,0,0,'The Tower of Lapidis IX','Now, I will do what I can to restore the attunement to this key. It shouldn\'t be that difficult, the magi who left the magic did a poor job with leaving magical traces upon it.\n\nStep back, I will do as you asked, it shouldn\'t take long. When I am done, you may return to the one who sent you.','Wait for Insom\'ni to attune the Enchanted Brass Key, once the key is attuned, return it to Hydromancer Flakereef in Caelan\'s Rest.','How have you fared? Have you gotten any answers about the key?','Now that they key is attuned, and enchanted, we can begin our planning to destroy Hydromancer Lapidis.',60244,1,0,0,0,0,0,0,60328,1,0,0,0,0,0,0,0,0,0,450,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'Insom\'ni finished enchanting');
+
+replace into creature_questrelation (id, quest) values (60446, 40171);
+replace into creature_involvedrelation (id, quest) values (92001, 40171);
+
+delete from creature_template where entry = 60328;
+replace into creature_template (entry, name, display_id1) values (60328, 'quest_40170_dummy_triger', 328);
+
+delete from item_template where entry = 60244;
+replace into item_template (entry, display_id, name, class, quality, flags, buy_count, allowable_class, allowable_race, item_level, stackable, spellcooldown_1, spellcategorycooldown_1, spellcooldown_2, spellcategorycooldown_2, bonding, description, page_text) values
+(60244,9153,'Attuned Brass Key',12,1,2048,1,-1,-1,1,1,-1,-1,-1,-1,4,'Enchanted and Attuned with potent, powerful magic',0);
+
+update creature_template set script_name = 'npc_insomni' where entry = 60446;
