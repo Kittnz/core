c7fa35a2b85d2111ab7c152df8b6bd062b8d9751
Torta <slowtorta@gmail.com>
2021-10-17 23:40:44 +0300

    Add spell effects for `Debug: Previous / Next DisplayID` spells.

--- src/scripts/miscelanneous/random_scripts_1.cpp
+++ src/scripts/miscelanneous/random_scripts_1.cpp
@@ -5306,12 +5306,14 @@ bool GOHello_go_shagu_shisha(Player* pPlayer, GameObject* pGo)
 
 bool GossipHello_npc_custodian_of_time(Player* pPlayer, Creature* pCreature)
 {
-    switch (pCreature->GetEntry())
+    if (sWorld.getConfig(CONFIG_BOOL_BETA))
     {
-    case 65000: pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "I wish to enter Caverns of Time.", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1); break;
-    case 65015: pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "I am leaving.", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2); break;
-    }
-    
+        switch (pCreature->GetEntry())
+        {
+        case 65000: pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "I wish to enter Caverns of Time.", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1); break;
+        case 65015: pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "I am leaving.", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2); break;
+        }
+    }    
     pPlayer->PrepareQuestMenu(pCreature->GetGUID());
     pPlayer->SEND_GOSSIP_MENU(51673, pCreature->GetGUID());
     return true;
