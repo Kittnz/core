ce57ba7ea9145936874ac23fa22267a9181f52e2
Torta <slowtorta@gmail.com>
2021-10-22 20:00:19 +0300

    Update Commands.cpp

--- src/game/Commands/Commands.cpp
+++ src/game/Commands/Commands.cpp
@@ -5412,7 +5412,7 @@ bool ChatHandler::HandleGPSCommand(char* args)
         zone_x, zone_y, ground_z, floor_z, have_map, have_vmap);
 
     // Need this temporary.
-    sLog.outString("XY: %f, %f", obj->GetPositionX(), obj->GetPositionY());
+    sLog.outString("XYZ: %f %f %f", obj->GetPositionX(), obj->GetPositionY(), obj->GetPositionZ());
 
     GridMapLiquidData liquid_status;
     GridMapLiquidStatus res = terrain->getLiquidStatus(obj->GetPositionX(), obj->GetPositionY(), obj->GetPositionZ(), MAP_ALL_LIQUIDS, &liquid_status);
