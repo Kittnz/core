cce4664f9d6dd23595d97feab0c0f06da1cc463f
Torta <slowtorta@gmail.com>
2021-11-03 15:37:27 +0300

    Quest `Mastering the Arcane` is now available for all Horde races.

--- sql/database_updates/fixes_november.sql
+++ sql/database_updates/fixes_november.sql
@@ -1,2 +1,4 @@
 -- Gnome hunters are missing the gnome racial passive Arcane Resistance.
 replace into playercreateinfo_spell values (7, 3, 20592, 0, 5875, 'Arcane Resistance');
+-- Quest Mastering the Arcane not available for trolls.
+update quest_template set requiredraces = 434 where entry in (80311, 80312);
