5a4efc807f1626eba40342edc399483f0173f980
Jameyboor <jamey_D@hotmail.com>
2021-11-06 16:15:19 +0100

    Trigger delay upon death of all invasion bosses to not despawn them immediately

---
+++ sql/fix_invasion_bosses.sql
@@ -0,0 +1,6 @@
+INSERT INTO `creature_ai_scripts` VALUES (1446107, 0, 54, 30017, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Baron Charr - Set Variable 30017 to 5');
+INSERT INTO `creature_ai_scripts` VALUES (1445709, 0, 54, 30018, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Princess Tempestria - Set Variable 30018 to 5');
+INSERT INTO `creature_ai_scripts` VALUES (1446408, 0, 54, 30019, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Avalanchion - Set Variable 30019 to 5');
+INSERT INTO `creature_ai_scripts` VALUES (1445409, 0, 54, 30020, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'The Windreaver - Set Variable 30011 to 5');
+
+
