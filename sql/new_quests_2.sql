da997efe0641742b3a08cf047d36833908c37df1
Dormilon <79369417+Dormilon@users.noreply.github.com>
2021-11-08 13:55:47 +0300

    New quest: `Returning to Janathos`.

--- sql/new_quests_2.sql
+++ sql/new_quests_2.sql
@@ -66,3 +66,14 @@ replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel,
 
 replace into creature_questrelation (id, quest) values (92017, 40231);
 replace into creature_involvedrelation (id, quest) values (1284, 40231);
+
+-- Returning to Janathos --
+delete from quest_template where entry = 40232;
+replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel, minlevel, questflags, specialflags, title, details, objectives, requestitemstext, offerrewardtext, reqitemid1, reqitemcount1, reqitemid2, reqitemcount2, reqitemid3, reqitemcount3, reqitemid4, reqitemcount4, reqcreatureorgoid1, reqcreatureorgocount1, reqcreatureorgoid2, reqcreatureorgocount2, reqcreatureorgoid3, reqcreatureorgocount3, reqcreatureorgoid4, reqcreatureorgocount4, srcitemid, srcitemcount, reworreqmoney, rewxp, rewrepfaction1, rewrepvalue1,  rewrepfaction2, rewrepvalue2, rewrepfaction3, rewrepvalue3, rewrepfaction4, rewrepvalue4, rewspell, rewspellcast, completeemote, rewitemid1, rewitemcount1, rewitemid2, rewitemcount2, rewitemid3, rewitemcount3, rewitemid4, rewitemcount4, rewchoiceitemid1, rewchoiceitemcount1, rewchoiceitemid2, rewchoiceitemcount2, rewchoiceitemid3, rewchoiceitemcount3, rewchoiceitemid4, rewchoiceitemcount4,requiredminrepfaction,requiredminrepvalue,objectivetext1) values (40231,40232,2,8,38,30,0,0,'Returning to Janathos','With the tome in my charge I would ask of you to bring a scroll to Watch Paladin Janathos in Sorrowguard Keep. From this scroll I hope to bless him and those underneath his command, for the light to watch over them safely, and deliver them from evil.\n\n When you see him, be sure to mention I wish him well.','Deliver the Scroll of Blessing to Watch Paladin Janathos at Sorrowguard Keep in Swamp of Sorrows.','Have you spoken with Benedictus?','So, then it is done? The Archbishop has always been a friend of mine, his convictions and word in the light has always been true.\n\nThis scroll will continue to guide our actions here, and keep us safe within Sorrowguard.',60314,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,60314,1,0,450,72,25,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'');
+
+replace into creature_questrelation (id, quest) values (1284, 40232);
+replace into creature_involvedrelation (id, quest) values (92017, 40232);
+
+delete from item_template where entry = 60314;
+replace into item_template (entry, display_id, name, class, quality, flags, buy_count, allowable_class, allowable_race, item_level, stackable, spellcooldown_1, spellcategorycooldown_1, spellcooldown_2, spellcategorycooldown_2, bonding, description, page_text) values
+(60314,1037,'Scroll of Blessing',12,1,2048,1,-1,-1,1,1,-1,-1,-1,-1,4,'Contains written scriptures in bright golden ink',0);
