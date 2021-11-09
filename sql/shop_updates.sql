f2148109ff09d2ada448eb797dd20954e334bbdf
Torta <slowtorta@gmail.com>
2021-11-09 05:17:29 +0300

    Update spell data & DBC for 1.15.2.

--- sql/shop_updates.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `shop_categories` ADD `icon` VARCHAR(32) NOT NULL DEFAULT 'default' AFTER `name`;
-ALTER TABLE `shop_items` ADD `descriptionLong` VARCHAR(1024) NOT NULL DEFAULT '' AFTER `price`;
