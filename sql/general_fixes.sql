f4c500769357a03c0c6b8377d027fc156c62c018
Torta <slowtorta@gmail.com>
2021-11-11 21:09:52 +0300

    Fix skin index for Blood Elf skin token.

---
+++ sql/general_fixes.sql
@@ -0,0 +1,6 @@
+update creature_template set display_id1 = 3845 where entry = 92201;
+update creature_template set display_id1 = 4010 where entry = 60488;
+update creature_template set display_id1 = 2613 where entry = 91982;
+update creature_template set subname = '' where entry = 60497;
+update creature_template set npc_flags = 16391 + 2 where entry = 4556;
+update creature_template set npc_flags = 135 + 2 where entry = 6739;
