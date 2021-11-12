87fb5de72596a9fe9c55ea0097eff6e621a6a006
Nolin7777 <nolin.nolin.nolin.nolin@gmail.com>
2021-11-12 09:52:08 +0100

    Correct bounding radius and combat reach for players

--- sql/database_updates/misc_fixes.sql
+++ sql/database_updates/misc_fixes.sql
@@ -250,3 +250,51 @@ INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalo
 
 -- Prevent Zum'rah from assisting zombies.
 UPDATE `creature_template` SET `flags_extra` = `flags_extra` | 65536 WHERE `entry`=7271;
+
+-- Human Male
+UPDATE `creature_display_info_addon` SET `bounding_radius`=0.306, `combat_reach`=1.5 WHERE `display_id`=49;
+
+-- Human Female
+UPDATE `creature_display_info_addon` SET `bounding_radius`=0.208, `combat_reach`=1.5 WHERE `display_id`=50;
+
+-- Orc Male
+UPDATE `creature_display_info_addon` SET `bounding_radius`=0.372, `combat_reach`=1.5 WHERE `display_id`=51;
+
+-- Orc Female
+UPDATE `creature_display_info_addon` SET `bounding_radius`=0.236, `combat_reach`=1.5 WHERE `display_id`=52;
+
+-- Dwarf Male
+UPDATE `creature_display_info_addon` SET `bounding_radius`=0.347, `combat_reach`=1.5 WHERE `display_id`=53;
+
+-- Dwarf Female
+UPDATE `creature_display_info_addon` SET `bounding_radius`=0.347, `combat_reach`=1.5 WHERE `display_id`=54;
+
+-- Night Elf Male
+UPDATE `creature_display_info_addon` SET `bounding_radius`=0.389, `combat_reach`=1.5 WHERE `display_id`=55;
+
+-- Night Elf Female
+UPDATE `creature_display_info_addon` SET `bounding_radius`=0.306, `combat_reach`=1.5 WHERE `display_id`=56;
+
+-- Undead Male
+UPDATE `creature_display_info_addon` SET `bounding_radius`=0.383, `combat_reach`=1.5 WHERE `display_id`=57;
+
+-- Undead Female
+UPDATE `creature_display_info_addon` SET `bounding_radius`=0.383, `combat_reach`=1.5 WHERE `display_id`=58;
+
+-- Tauren Male
+UPDATE `creature_display_info_addon` SET `bounding_radius`=0.9747, `combat_reach`=4.05 WHERE `display_id`=59;
+
+-- Tauren Female
+UPDATE `creature_display_info_addon` SET `bounding_radius`=0.8725, `combat_reach`=3.75 WHERE `display_id`=60;
+
+-- Gnome Male
+UPDATE `creature_display_info_addon` SET `bounding_radius`=0.3519, `combat_reach`=1.725 WHERE `display_id`=1563;
+
+-- Gnome Female
+UPDATE `creature_display_info_addon` SET `bounding_radius`=0.3519, `combat_reach`=1.725 WHERE `display_id`=1564;
+
+-- Troll Male
+UPDATE `creature_display_info_addon` SET `bounding_radius`=0.306, `combat_reach`=1.5 WHERE `display_id`=1478;
+
+-- Troll Female
+UPDATE `creature_display_info_addon` SET `bounding_radius`=0.306, `combat_reach`=1.5 WHERE `display_id`=1479;
