ee746800b253855e2a22c361e0fa8c8515aea194
Nolin7777 <nolin.nolin.nolin.nolin@gmail.com>
2021-11-14 19:46:39 +0100

    Check only Ghost aura in IsInvisibleForAlive

--- sql/database_updates/misc_fixes.sql
+++ sql/database_updates/misc_fixes.sql
@@ -307,3 +307,17 @@ INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `Spell
 
 -- Prevent Archmage Arugal from evading while teleported to platform.
 UPDATE `creature_template` SET `flags_extra`=`flags_extra`|67108864 WHERE `entry`=4275;
+
+-- Mouse
+UPDATE `creature_template` SET `auras`='' WHERE `entry`=6271;
+-- Spirit Healer
+UPDATE `creature_template` SET `auras`='10848 9036' WHERE `entry`=6491;
+-- Franclorn Forgewright
+UPDATE `creature_template` SET `auras`='10848 9617 9036' WHERE `entry`=8888;
+-- Gaeriyan
+UPDATE `creature` SET `position_x`=-6946.937, `position_y`=-3944.356, `position_z`=32.399, `movement_type`=1, `wander_distance`=8 WHERE `guid`=51781;
+DELETE FROM `creature_addon` WHERE `guid`=51781;
+UPDATE `creature_template` SET `auras`='10848 9617 9036' WHERE `entry`=9299;
+INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (3909, 8, 3909, 0, 0, 0, 0);
+INSERT INTO `gossip_menu` (`entry`, `text_id`) VALUES (1914, 2567);
+UPDATE `gossip_menu` SET `condition_id`=3909 WHERE `entry`=1914 && `text_id`=2568;
