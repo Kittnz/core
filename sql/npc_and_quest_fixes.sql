994fd9789b18d446f4e1ff2960b2b5f04be9160b
Dormilon <79369417+Dormilon@users.noreply.github.com>
2021-11-15 15:59:13 +0300

    New quest: `The Hunter’s Path`.

--- sql/npc_and_quest_fixes.sql
+++ sql/npc_and_quest_fixes.sql
@@ -250,3 +250,12 @@ replace into creature_questrelation (id, quest) values (60483, 40260);
 replace into creature_involvedrelation (id, quest) values (81050, 40260);
 
 update creature_template set npc_flags = 19 where entry = 81050;
+
+-- The Hunter’s Path --
+delete from quest_template where entry = 40261;
+replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel, minlevel, questflags, specialflags, title, details, objectives, requestitemstext, offerrewardtext, reqitemid1, reqitemcount1, reqitemid2, reqitemcount2, reqitemid3, reqitemcount3, reqitemid4, reqitemcount4, reqcreatureorgoid1, reqcreatureorgocount1, reqcreatureorgoid2, reqcreatureorgocount2, reqcreatureorgoid3, reqcreatureorgocount3, reqcreatureorgoid4, reqcreatureorgocount4, srcitemid, srcitemcount, reworreqmoney, rewxp, rewrepfaction1, rewrepvalue1,  rewrepfaction2, rewrepvalue2, rewrepfaction3, rewrepvalue3, rewrepfaction4, rewrepvalue4, rewspell, rewspellcast, completeemote, rewitemid1, rewitemcount1, rewitemid2, rewitemcount2, rewitemid3, rewitemcount3, rewitemid4, rewitemcount4, rewchoiceitemid1, rewchoiceitemcount1, rewchoiceitemid2, rewchoiceitemcount2, rewchoiceitemid3, rewchoiceitemcount3, rewchoiceitemid4, rewchoiceitemcount4,requiredminrepfaction,requiredminrepvalue,objectivetext1) values (40260,40261,2,85,10,10,0,0,'The Hunter\’s Path','Hail aspiring hunter. By now you should be able to train a beast and claim it as your own, I am afraid I will not be the one to aid you with that although I have played a role in your training.\n\nYou must speak to Liott Maneskin, it seems others such as you have found a way to enthrall a creature as your own.','Speak with Liott Maneskin in Brill.','And who are you?','Ah, both Morpheus and Dark Ranger Lanissa mentioned you, I guess it\’s time to offer you a lesson.',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,100,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'');
+
+replace into creature_questrelation (id, quest) values (81050, 40261);
+replace into creature_involvedrelation (id, quest) values (60484, 40261);
+
+update quest_template set requiredraces = 16 where entry = 40261;
