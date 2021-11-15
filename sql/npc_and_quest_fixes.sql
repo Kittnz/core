135764af7192e1d83a7cf80729196b63ab0ca811
Dormilon <79369417+Dormilon@users.noreply.github.com>
2021-11-15 19:17:13 +0300

    New quest: ``.

--- sql/npc_and_quest_fixes.sql
+++ sql/npc_and_quest_fixes.sql
@@ -259,3 +259,14 @@ replace into creature_questrelation (id, quest) values (81050, 40261);
 replace into creature_involvedrelation (id, quest) values (60484, 40261);
 
 update quest_template set requiredraces = 16 where entry = 40261;
+
+-- Taming the Beast I --
+delete from quest_template where entry = 40248;
+replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel, minlevel, questflags, specialflags, title, details, objectives, requestitemstext, offerrewardtext, reqitemid1, reqitemcount1, reqitemid2, reqitemcount2, reqitemid3, reqitemcount3, reqitemid4, reqitemcount4, reqcreatureorgoid1, reqcreatureorgocount1, reqcreatureorgoid2, reqcreatureorgocount2, reqcreatureorgoid3, reqcreatureorgocount3, reqcreatureorgoid4, reqcreatureorgocount4, srcitemid, srcitemcount, reworreqmoney, rewxp, rewrepfaction1, rewrepvalue1,  rewrepfaction2, rewrepvalue2, rewrepfaction3, rewrepvalue3, rewrepfaction4, rewrepvalue4, rewspell, rewspellcast, completeemote, rewitemid1, rewitemcount1, rewitemid2, rewitemcount2, rewitemid3, rewitemcount3, rewitemid4, rewitemcount4, rewchoiceitemid1, rewchoiceitemcount1, rewchoiceitemid2, rewchoiceitemcount2, rewchoiceitemid3, rewchoiceitemcount3, rewchoiceitemid4, rewchoiceitemcount4,requiredminrepfaction,requiredminrepvalue,objectivetext1) values (40261,40248,2,-261,10,10,0,0,'Taming the Beast I','As once people of Lordaeron, we have ventured into the forests before, I suppose you are no stranger to the wolves we once had to fear in the midst of the night.\n\nEven these wolves were plagued and corrupted by the Scourge, as you may see, I carry one at my side currently. Blackpaw here is a darkhound, one I\’ve been able to tame without the need of any special skill.\n\nRegarding our condition, some beasts will still be approachable as they wouldn\’t mind the actual smell of death, but others will probably attempt to have a bite out of you, for that, I have something I will bestow upon you at the end of this training since you must be able to tame a beast without it as well.\n\nNow go, and return with a Darkhound.','Use the Taming Rod to tame a Decrepit Darkhound. Practice your skills, then return the Taming Rod to Liott Maneskin in Brill.','Trust in your skills. Tame that Darkhound.','I can see it\’s very eager to gnaw on some Scourge bones, you\’ve done well.',60329,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,60329,1,0,850,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'');
+
+replace into creature_questrelation (id, quest) values (60484, 40248);
+replace into creature_involvedrelation (id, quest) values (60484, 40248);
+
+update quest_template set questflags = 2 where entry = 40248;
+update quest_template set specialflags = 2 where entry = 40248;
+update quest_template set completeemote = 1 where entry = 40248;
