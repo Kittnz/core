7c9e1de1fea5885406bee2d7b005ed36282e098a
Torta <slowtorta@gmail.com>
2021-11-16 18:35:07 +0300

    Fix SQL errors.

--- sql/black_morass_update_nov.sql
+++ sql/black_morass_update_nov.sql
@@ -2,4 +2,4 @@ UPDATE creature_template SET display_id1 = 18040 WHERE entry = 65105;
 
 UPDATE creature SET spawntimesecsmin = 14400 WHERE id = 65100;
 
-REPLACE INTO `gameobject` VALUES (4013209, 2010845, 269, -1430, 7010, -14.36, 0.966082, 0, 0, 0.464474, 0.885587, 300, 300, 100, 1, 0, 0, 0, 10);
+REPLACE INTO `gameobject` VALUES (4013209, 2010845, 269, -1430, 7010, -14.36, 0.966082, 0, 0, 0.464474, 0.885587, 300, 300, 100, 1, 0, 0);
