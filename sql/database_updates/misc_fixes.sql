699d3a70dce158c79b1331b9ce7cf56305ea5134
Nolin7777 <nolin.nolin.nolin.nolin@gmail.com>
2021-11-18 19:50:59 +0100

    Don't use bounding radius for aggro distance checks

--- sql/database_updates/misc_fixes.sql
+++ sql/database_updates/misc_fixes.sql
@@ -29,3 +29,9 @@ UPDATE `event_scripts` SET `dataint4`=2 WHERE `id`=1653 && `command`=10;
 
 -- Fix wrong spells used by the warlock's Doomguard.
 UPDATE `creature_template` SET `spell_id1`=19474, `spell_id2`=19482, `spell_id3`=89 WHERE `entry`=11859;
+
+-- Aggro range for mobs in starting zones is lower than normal.
+UPDATE `creature_template` SET `detection_range`=10 WHERE `entry` IN (6, 38, 69, 80, 103, 257, 299, 704, 705, 706, 707, 708, 724, 808, 946, 1501, 1502, 1504, 1505, 1506, 1507, 1508, 1509, 1512, 1513, 1667, 1688, 1890, 1916, 1917, 1918, 1919, 1984, 1985, 1986, 1988, 1989, 1994, 2031, 2032, 2952, 2953, 2954, 2955, 2961, 2966, 3098, 3101, 3102, 3124, 3183, 3229, 3281, 3300, 8554);
+
+-- Aggro range for most mobs in classic is around 3-4 yards lower than our current default.
+UPDATE `creature_template` SET `detection_range`=18 WHERE `detection_range`=20 && `rank`=0;
