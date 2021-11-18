219d802a0ccc07ce4b6ea0b1c979d5a316ce76aa
Nolin7777 <nolin.nolin.nolin.nolin@gmail.com>
2021-11-18 19:19:34 +0100

    Pause wandering npcs when player interacts with them

--- src/game/Handlers/NPCHandler.cpp
+++ src/game/Handlers/NPCHandler.cpp
@@ -375,8 +375,8 @@ void WorldSession::HandleGossipHelloOpcode(WorldPacket & recv_data)
 
     GetPlayer()->RemoveAurasWithInterruptFlags(AURA_INTERRUPT_FLAG_TALK); // Removes stealth, feign death ...
 
-    if (!pCreature->IsStopped() && !(pCreature->GetCreatureInfo()->flags_extra & CREATURE_FLAG_EXTRA_NO_MOVEMENT_PAUSE))
-        pCreature->StopMoving();
+    if (!pCreature->HasExtraFlag(CREATURE_FLAG_EXTRA_NO_MOVEMENT_PAUSE))
+        pCreature->PauseOutOfCombatMovement();
 
     if (pCreature->IsSpiritGuide())
         pCreature->SendAreaSpiritHealerQueryOpcode(_player);
@@ -422,14 +422,8 @@ void WorldSession::HandleGossipSelectOptionOpcode(WorldPacket & recv_data)
         // Turtle WoW Specific: required to make sure that companions with menu options will continue to follow players after option selection.
         if (pCreature->GetCharmInfo() == nullptr || !pCreature->GetCharmInfo()->IsFollowing())
         {
-            if (!(pCreature->GetCreatureInfo()->flags_extra & CREATURE_FLAG_EXTRA_NO_MOVEMENT_PAUSE))
-            {
-                // Clear possible StopMoving motion
-                if (pCreature->IsStopped())
-                    pCreature->GetMotionMaster()->Clear();
-
-                pCreature->StopMoving();
-            }
+            if (!pCreature->HasExtraFlag(CREATURE_FLAG_EXTRA_NO_MOVEMENT_PAUSE))
+                pCreature->PauseOutOfCombatMovement();
         }
 
         if (!sScriptMgr.OnGossipSelect(_player, pCreature, sender, action, code.empty() ? nullptr : code.c_str()))
