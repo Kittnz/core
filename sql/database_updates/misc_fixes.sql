1e477fb5ae59e2c880fca9d85585acbdde1968da
Nolin7777 <nolin.nolin.nolin.nolin@gmail.com>
2021-11-18 19:02:38 +0100

    Misc DB fixes
    - Change despawn type of Mana Surges
    
    - Correct Rain of Fire spell used by Doomguard

--- sql/database_updates/misc_fixes.sql
+++ sql/database_updates/misc_fixes.sql
@@ -15,11 +15,17 @@ UPDATE `creature_template` SET `flags_extra`= `flags_extra` - 524288 WHERE (`fla
 UPDATE `creature_template` SET `flags_extra`= `flags_extra` - 8 WHERE (`flags_extra` & 8) && `entry` IN (3024, 3454, 3455, 10618, 10619, 10919, 11196, 11198, 11806, 11835, 14622, 15177, 16241, 16285, 16781, 17635, 17647);
 
 -- Blasted Lands buffs from turning in quests shouldn't stack, just like the versions on the items.
-REPLACE INTO `spell_group` (`group_id`, `group_spell_id`, `spell_id`, `build_min`, `build_max`) VALUES
-(1004, 3, 10671, 4695, 5875),
-(1004, 4, 10691, 4695, 5875),
-(1008, 5, 10670, 0, 5875),
-(1008, 6, 10671, 0, 5875),
-(1008, 7, 10672, 0, 5875),
-(1008, 8, 10690, 0, 5875),
-(1008, 9, 10691, 0, 5875);
+REPLACE INTO `spell_group` (`group_id`, `group_spell_id`, `spell_id`) VALUES
+(1004, 3, 10671),
+(1004, 4, 10691),
+(1008, 5, 10670),
+(1008, 6, 10671),
+(1008, 7, 10672),
+(1008, 8, 10690),
+(1008, 9, 10691);
+
+-- Change despawn type of Mana Surges.
+UPDATE `event_scripts` SET `dataint4`=2 WHERE `id`=1653 && `command`=10;
+
+-- Fix wrong spells used by the warlock's Doomguard.
+UPDATE `creature_template` SET `spell_id1`=19474, `spell_id2`=19482, `spell_id3`=89 WHERE `entry`=11859;
