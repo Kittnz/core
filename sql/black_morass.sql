e31656951aef864a3f8d3c911125cc7d3598dc74
Torta <slowtorta@gmail.com>
2021-11-22 11:52:00 +0300

    Deleted some obsolete quests.

--- sql/black_morass.sql
+++ sql/black_morass.sql
@@ -1,3 +1,9 @@
+UPDATE creature_template SET display_id1 = 18040 WHERE entry = 65105;
+
+UPDATE creature SET spawntimesecsmin = 14400 WHERE id = 65100;
+
+REPLACE INTO `gameobject` VALUES (4013209, 2010845, 269, -1430, 7010, -14.36, 0.966082, 0, 0, 0.464474, 0.885587, 300, 300, 100, 1, 0, 0);
+
 UPDATE creature_template SET scale = 1.5 WHERE entry = 65102;
 UPDATE creature_template SET scale = 2 WHERE entry = 65114;
 UPDATE creature_template SET display_id1 = 18592 WHERE entry = 65105;
--- sql/black_morass_update_nov.sql
+++
@@ -1,5 +0,0 @@
-UPDATE creature_template SET display_id1 = 18040 WHERE entry = 65105;
-
-UPDATE creature SET spawntimesecsmin = 14400 WHERE id = 65100;
-
-REPLACE INTO `gameobject` VALUES (4013209, 2010845, 269, -1430, 7010, -14.36, 0.966082, 0, 0, 0.464474, 0.885587, 300, 300, 100, 1, 0, 0);
