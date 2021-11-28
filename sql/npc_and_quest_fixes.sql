e47764ff75450c00d22e855b39b49fbb4ad9aeca
Dormilon <79369417+Dormilon@users.noreply.github.com>
2021-11-28 13:04:26 +0300

    New quest: `The Search for the Blade`.

--- sql/npc_and_quest_fixes.sql
+++ sql/npc_and_quest_fixes.sql
@@ -4545,3 +4545,22 @@ replace into creature_involvedrelation (id, quest) values (92024, 40285);
 
 delete from creature_template where entry = 60340;
 replace into creature_template (entry, name, display_id1) values (60340, 'quest_40285_dummy_triger', 328);
+
+-- The Search for the Blade --
+delete from quest_template where entry = 40286;
+replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel, minlevel, questflags, specialflags, title, details, objectives, requestitemstext, offerrewardtext, reqitemid1, reqitemcount1, reqitemid2, reqitemcount2, reqitemid3, reqitemcount3, reqitemid4, reqitemcount4, reqcreatureorgoid1, reqcreatureorgocount1, reqcreatureorgoid2, reqcreatureorgocount2, reqcreatureorgoid3, reqcreatureorgocount3, reqcreatureorgoid4, reqcreatureorgocount4, srcitemid, srcitemcount, reworreqmoney, rewxp, rewrepfaction1, rewrepvalue1,  rewrepfaction2, rewrepvalue2, rewrepfaction3, rewrepvalue3, rewrepfaction4, rewrepvalue4, rewspell, rewspellcast, completeemote, rewitemid1, rewitemcount1, rewitemid2, rewitemcount2, rewitemid3, rewitemcount3, rewitemid4, rewitemcount4, rewchoiceitemid1, rewchoiceitemcount1, rewchoiceitemid2, rewchoiceitemcount2, rewchoiceitemid3, rewchoiceitemcount3, rewchoiceitemid4, rewchoiceitemcount4,requiredminrepfaction,requiredminrepvalue,objectivetext1) values (40285,40286,2,16,52,45,0,0,'The Search for the Blade','The Temple of Arrkoran...\n\n<Katokar would seem almost distant for a moment as his mind thought>.\n\n This must be the place $N, it must be. I have no other leads, venture deep within the Temple, and seek out his blade, if it is anywhere, then it must be there. You can find this temple far to the south east, it is massive, and cannot be missed. Be careful in your searching, I am sure a terrible evil lurks inside.','Search for the sword that could have belonged to a fallen Blademaster inside the Temple of Arrkoran.','I will find his Blade.','It’s broken...',60197,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,12000,5150,76,100,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'');
+
+replace into creature_questrelation (id, quest) values (92024, 40286);
+replace into creature_involvedrelation (id, quest) values (92024, 40286);
+
+delete from item_template where entry = 60197;
+replace into item_template (entry, display_id, name, class, quality, flags, buy_count, allowable_class, allowable_race, item_level, stackable, spellcooldown_1, spellcategorycooldown_1, spellcooldown_2, spellcategorycooldown_2, bonding, description, page_text) values
+(60197,40029,'Broken Blademaster’s Sword',12,1,2048,1,-1,-1,1,1,-1,-1,-1,-1,4,'Broken, but still sharp.',0);
+
+delete from gameobject_template where entry = 2010857;
+replace into gameobject_template values
+(2010857,3,22683,'Distinct Sword',0,4,1,43,2010857,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'');
+
+delete from gameobject_loot_template where entry = 2010857;
+replace into gameobject_loot_template values 
+(2010857,60197,-100,0,1,1,0);
