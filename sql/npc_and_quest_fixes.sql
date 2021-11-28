197ec74e669ecb8f308bc429abe1f3fab21d0df9
Dormilon <79369417+Dormilon@users.noreply.github.com>
2021-11-28 17:08:34 +0300

    New quest: `Mystery of Lake Mennar`.

--- sql/npc_and_quest_fixes.sql
+++ sql/npc_and_quest_fixes.sql
@@ -4704,3 +4704,16 @@ replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel,
 
 replace into creature_questrelation (id, quest) values (92197, 40294);
 replace into creature_involvedrelation (id, quest) values (92199, 40294);
+
+-- Mystery of Lake Mennar --
+delete from quest_template where entry = 40295;
+replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel, minlevel, questflags, specialflags, title, details, objectives, requestitemstext, offerrewardtext, reqitemid1, reqitemcount1, reqitemid2, reqitemcount2, reqitemid3, reqitemcount3, reqitemid4, reqitemcount4, reqcreatureorgoid1, reqcreatureorgocount1, reqcreatureorgoid2, reqcreatureorgocount2, reqcreatureorgoid3, reqcreatureorgocount3, reqcreatureorgoid4, reqcreatureorgocount4, srcitemid, srcitemcount, reworreqmoney, rewxp, rewrepfaction1, rewrepvalue1,  rewrepfaction2, rewrepvalue2, rewrepfaction3, rewrepvalue3, rewrepfaction4, rewrepvalue4, rewspell, rewspellcast, completeemote, rewitemid1, rewitemcount1, rewitemid2, rewitemcount2, rewitemid3, rewitemcount3, rewitemid4, rewitemcount4, rewchoiceitemid1, rewchoiceitemcount1, rewchoiceitemid2, rewchoiceitemcount2, rewchoiceitemid3, rewchoiceitemcount3, rewchoiceitemid4, rewchoiceitemcount4,requiredminrepfaction,requiredminrepvalue,objectivetext1) values (40294,40295,2,16,50,45,0,0,'Mystery of Lake Mennar','We came to Azshara to heal these lands from remnants of the Legion’s corruption. What we found here however, was different than expected. You see, young $r, demonic corruption is not the biggest problem here.\n\nThe land cries out for help. Some kind of evil forces have settled near Lake Mennar. They drain the very essence of the Earth, consuming magic and nature alike. Nagas or satyrs are not that powerful, they could not do that. Blue Dragonfilght, on the other hand.n\n\Please, go and see what is happening at Lake Mennar, the future of Azshara may be in your hands.','Scout Lake Mennar.','Have you been to Lake Mennar yet? It’s in the southern part of Azshara.','How many? This is worse than we expected.',0,0,0,0,0,0,0,0,60343,1,0,0,0,0,0,0,0,0,15000,5100,76,150,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'Lake Mennar has been explored');
+
+replace into creature_questrelation (id, quest) values (92199, 40295);
+replace into creature_involvedrelation (id, quest) values (92199, 40295);
+
+update quest_template set type = 1 where entry = 40295
+
+delete from creature_template where entry = 60343;
+replace into creature_template (entry, name, display_id1) values (60343, 'quest_40295_dummy_triger', 328);
+
