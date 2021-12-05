91e4020458eebb624d936c6b17bf796ca2580417
Dormilon <79369417+Dormilon@users.noreply.github.com>
2021-12-05 18:49:03 +0300

    NPS and item fixes. (Correct).

--- sql/npc_and_quest_fixes_2.sql
+++ sql/npc_and_quest_fixes_2.sql
@@ -1324,6 +1324,7 @@ values (@spell_list_id, @description,
 update creature_template set unit_flags = 0, npc_flags = 3 where entry = 91954;
 update creature_template set unit_flags = 0, npc_flags = 3 where entry = 91955;
 update npc_vendor set maxcount = 1, incrtime = 86400 where entry = 91256;
+update item_template set ammo_type = 3, range_mod = 100 where entry = 83257;
 
 set @gossip_menu_id = 41051; set @magic_number = 91798;
 replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); 
