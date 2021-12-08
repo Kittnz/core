13be4e755e38e759909a8d052d8d52f28fd748c4
Torta <slowtorta@gmail.com>
2021-12-08 20:58:24 +0100

    Crescent Grove: added aggro & death VA for bosses.

--- src/scripts/dungeons/crescent_grove/instance_crescent_grove.cpp
+++ src/scripts/dungeons/crescent_grove/instance_crescent_grove.cpp
@@ -6,6 +6,14 @@ void DoAfterTime(Player* player, uint32 p_time, Functor&& function)
     player->m_Events.AddEvent(new LambdaBasicEvent<Functor>(std::move(function)), player->m_Events.CalculateTime(p_time));
 }
 
+enum boss_entries
+{
+    grovetender_engryss = 92107,
+    keeper_ranatos = 92109,
+    fenektis_the_deceiver = 92111,
+    master_raxxieth = 92110
+};
+
 struct instance_crescent_grove : public ScriptedInstance
 {
     explicit instance_crescent_grove(Map* p_Map) : ScriptedInstance(p_Map)
@@ -17,6 +25,49 @@ struct instance_crescent_grove : public ScriptedInstance
     {
         if (pCreature->IsAlive() && !pCreature->IsInCombat())
             pCreature->SetInCombatWithZone();
+
+        switch (pCreature->GetEntry())
+        {
+        case grovetender_engryss:
+            pCreature->PlayDirectMusic(30268);
+            pCreature->MonsterYell("I am the leader of the tribes, me! The Groveweald shall destroy everyone that gets in the way!");
+            break;
+        case keeper_ranatos:
+            pCreature->PlayDirectMusic(30256);
+            pCreature->MonsterYell("What? Who..Who are you? They wouldn't send you! Ancients, to my side!");
+            break;
+        case fenektis_the_deceiver:
+            pCreature->PlayDirectMusic(30252);
+            pCreature->MonsterYell("We will serve at the Master's will!");
+            break;
+        case master_raxxieth:
+            pCreature->PlayDirectMusic(30271);
+            pCreature->MonsterYell("Do you think you can withstand the might of the Burning Legion?");
+            break;
+        }
+    }
+
+    void OnCreatureDeath(Creature* pCreature) override
+    {
+        switch (pCreature->GetEntry())
+        {
+        case grovetender_engryss:
+            pCreature->PlayDirectMusic(30270);
+            pCreature->MonsterYell("I.. I can see clearly now.. The madness... It's... Over...");
+            break;
+        case keeper_ranatos:
+            pCreature->PlayDirectMusic(30258);
+            pCreature->MonsterYell("We must.. Stop.. The shadow...");
+            break;
+        case fenektis_the_deceiver:
+            pCreature->PlayDirectMusic(30253);
+            pCreature->MonsterYell("You think.. This.... Is the end?");
+            break;
+        case master_raxxieth:
+            pCreature->PlayDirectMusic(30273);
+            pCreature->MonsterYell("My death.. Means little in the grand scheme, mortals... Drink down your victory... It will mean nothing... In the end...");
+            break;
+        }
     }
 };
 
@@ -24,5 +75,10 @@ InstanceData* GetInstanceData_instance_crescent_grove(Map* p_Map) { return new i
 
 void AddSC_instance_crescent_grove()
 {
-   // Script* newscript;
+    Script* newscript;
+
+    newscript = new Script;
+    newscript->Name = "instance_crescent_grove";
+    newscript->GetInstanceData = &GetInstanceData_instance_crescent_grove;
+    newscript->RegisterSelf();
 }
