1384befa86d6632ab9c44dca869e4f54e14b71a4
Giperion <dereline@hotmail.com>
2021-12-14 23:34:22 +0200

    [Stormwind Vault] Updated Arctiras, update mobs health and damage

---
+++ sql/database_updates/World_Migration_14_december_2021.sql
@@ -0,0 +1,18 @@
+UPDATE `creature_template` SET `subname` = 'The Glacier of Madness', `health_min` = '45851', `health_max` = '45851' WHERE (`entry` = '93107');
+UPDATE `creature_template` SET `type_flags` = '0' WHERE (`entry` = '93107');
+
+
+UPDATE `creature_template` SET `dmg_min` = '280', `dmg_max` = '305' WHERE (`entry` = '93106');
+UPDATE `creature_template` SET `health_min` = '41064', `health_max` = '41064' WHERE (`entry` = '80854');
+UPDATE `creature_template` SET `health_min` = '39116', `health_max` = '39116' WHERE (`entry` = '80850');
+UPDATE `creature_template` SET `health_min` = '40119', `health_max` = '40119' WHERE (`entry` = '80853');
+UPDATE `creature_template` SET `health_min` = '37069', `health_max` = '37069' WHERE (`entry` = '80852');
+
+UPDATE `creature_template` SET `health_min` = '14523', `health_max` = '14863' WHERE (`entry` = '60597');
+UPDATE `creature_template` SET `health_min` = '14423', `health_max` = '14793' WHERE (`entry` = '60603');
+UPDATE `creature_template` SET `health_min` = '14451', `health_max` = '14798' WHERE (`entry` = '60601');
+UPDATE `creature_template` SET `health_min` = '14461', `health_max` = '14801' WHERE (`entry` = '60602');
+UPDATE `creature_template` SET `health_min` = '14561', `health_max` = '14912' WHERE (`entry` = '60598');
+UPDATE `creature_template` SET `health_min` = '14277', `health_max` = '14533' WHERE (`entry` = '60599');
+UPDATE `creature_template` SET `health_min` = '14625', `health_max` = '14852' WHERE (`entry` = '60604');
+UPDATE `creature_template` SET `health_min` = '14589', `health_max` = '14816' WHERE (`entry` = '93105');
