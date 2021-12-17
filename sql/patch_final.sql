68b8b5554686b2a564cb1cf540aa5ef58035e004
Torta <slowtorta@gmail.com>
2021-12-17 12:49:14 +0100

    Update Sunnyglade Valley strings.

---
+++ sql/patch_final.sql
@@ -0,0 +1,16 @@
+
+replace into `mangos_string` values('10051','The battle for Sunnyglade Valley begins in 1 minute.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+replace into `mangos_string` values('10052','The battle for Sunnyglade Valley begins in 30 seconds. Prepare yourselves!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+replace into `mangos_string` values('10053','Let the battle for Sunnyglade Valley begin!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+replace into `mangos_string` values('10054','The %s has taken the %s',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+replace into `mangos_string` values('10055','$n has defended the %s',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+replace into `mangos_string` values('10056','$n has assaulted the %s',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+replace into `mangos_string` values('10057','$n claims the %s! If left unchallenged, the %s will control it in 1 minute!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+replace into `mangos_string` values('10058','Past grudges led you to your failure, what a waste...',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+replace into `mangos_string` values('10059','The timeway been changed and we were unable to find which events shifted it, and now it will take unpredictable effect.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+replace into `mangos_string` values('10060','Great work, heroes! For these bitter few moments, lay down your weapons away from each other. And strike together at your common foe!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+replace into `mangos_string` values('10061','Lion\'s Pride Tower.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+replace into `mangos_string` values('10062','Krokvel Tower.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+replace into `mangos_string` values('10063','Fort Wrynn.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+
+update creature_template set subname = '' where entry = 13760;
