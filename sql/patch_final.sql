240e02dd9a7943d4ccd08a00803e8fa2d935c990
Torta <slowtorta@gmail.com>
2021-12-24 01:08:23 +0100

    Update patch_final.sql

--- sql/patch_final.sql
+++ sql/patch_final.sql
@@ -2328,7 +2328,7 @@ update creature_template set dmg_min = 252, dmg_max = 278 where entry = 93106;
 
 update creature set spawntimesecsmin = 604800, spawntimesecsmax = 604800 where guid in (2569245, 2568627, 2568817, 2568630, 2569245);
 
-update item_template set spelltrigger_1 = 1, spell_id2 = 0 where entry = 60410;
+update item_template set spelltrigger_1 = 1, spellid_2 = 0 where entry = 60410;
 
 replace into gameobject_template (`entry`, `type`, `displayId`, `name`, `faction`, `flags`, `size`, `data0`, `data1`, `data2`, `data3`, `data4`, `data5`, `data6`, `data7`, `data8`, `data9`, `data10`, `data11`, `data12`, `data13`, `data14`, `data15`, `data16`, `data17`, `data18`, `data19`, `data20`, `data21`, `data22`, `data23`, `mingold`, `maxgold`, `phase_quest_id`, `script_name`) VALUES (4000509, 0, 23910, 'Vault Door', 114, 54, 1, 0, 85, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');
 
