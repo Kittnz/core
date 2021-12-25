cc506bf35c67fcbd8150a7ac4bd0b6ffe2353542
Torta <slowtorta@gmail.com>
2021-12-25 21:49:17 +0100

    Dummy creatures for Tree of Life.

--- sql/patch_final.sql
+++ sql/patch_final.sql
@@ -2407,3 +2407,10 @@ replace into `map_template` (`entry`, `parent`, `map_type`, `linked_zone`, `play
 update creature_template set faction = 1682 where entry = 60443;
 update gameobject set spawntimesecsmin = 30, spawntimesecsmax = 30 where id = 2010827;
 update item_template set class = 4, subclass = 0 where entry = 83239;
+
+replace into `creature_template` (`entry`, `display_id1`, `display_id2`, `display_id3`, `display_id4`, `name`, `subname`, `gossip_menu_id`, `level_min`, `level_max`, `health_min`, `health_max`, `mana_min`, `mana_max`, `armor`, `faction`, `npc_flags`, `speed_walk`, `speed_run`, `scale`, `detection_range`, `call_for_help_range`, `leash_range`, `rank`, `xp_multiplier`, `dmg_min`, `dmg_max`, `dmg_school`, `attack_power`, `dmg_multiplier`, `base_attack_time`, `ranged_attack_time`, `unit_class`, `unit_flags`, `dynamic_flags`, `beast_family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `ranged_dmg_min`, `ranged_dmg_max`, `ranged_attack_power`, `type`, `type_flags`, `loot_id`, `pickpocket_loot_id`, `skinning_loot_id`, `holy_res`, `fire_res`, `nature_res`, `frost_res`, `shadow_res`, `arcane_res`, `spell_id1`, `spell_id2`, `spell_id3`, `spell_id4`, `spell_list_id`, `pet_spell_list_id`, `gold_min`, `gold_max`, `ai_name`, `movement_type`, `inhabit_type`, `civilian`, `racial_leader`, `regeneration`, `equipment_id`, `trainer_id`, `vendor_id`, `mechanic_immune_mask`, `school_immune_mask`, `flags_extra`, `phase_quest_id`, `script_name`) VALUES 
+
+(40050, 18031, 0, 0, 0, 'Tree Form (Night Elf Druid)', '', 0, 1, 1, 42, 42, 0, 0, 7, 35, 0, 1.2, 1.14286, 1, 18, 5, 0, 0, 1, 2, 2, 0, 44, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 2, 0, ''),
+(40051, 18030, 0, 0, 0, 'Tree Form (Tauren Druid)', '', 0, 1, 1, 42, 42, 0, 0, 7, 35, 0, 1.2, 1.14286, 1, 18, 5, 0, 0, 1, 2, 2, 0, 44, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 2, 0, '');
+
+
