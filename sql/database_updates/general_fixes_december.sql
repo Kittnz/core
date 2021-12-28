9ce582d2cd30f536ba0384eec932535d684adfcf
Torta <slowtorta@gmail.com>
2021-12-28 14:42:11 +0100

    Delete buggy PvP quests.

--- sql/database_updates/general_fixes_december.sql
+++ sql/database_updates/general_fixes_december.sql
@@ -17,3 +17,7 @@ update creature set spawntimesecsmin = 7200, spawntimesecsmax = 7200 where guid 
 -- Karazhan Crypt spawn timers:
 update creature set spawntimesecsmin = 10800, spawntimesecsmax = 10800 where map = 800 and spawntimesecsmin = 300;
 update creature set spawntimesecsmin = 10800, spawntimesecsmax = 10800 where map = 800 and spawntimesecsmin = 25;
+-- Delete buggy PvP quests:
+delete from quest_template where entry in (80255, 80302);
+delete from creature_questrelation where quest in (80255, 80302);
+delete from creature_involvedrelation where quest in (80255, 80302);
