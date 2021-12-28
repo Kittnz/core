253703872a6a747ff0fe1a7ec6d874664182d84b
Torta <slowtorta@gmail.com>
2021-12-28 14:15:00 +0100

    Make War Mode available from any level.

---
+++ sql/database_updates/general_fixes_december.sql
@@ -0,0 +1,10 @@
+-- Make War Mode available from any level:
+delete from npc_vendor where item = 80060;
+replace into npc_vendor values (51540, 80060, 0, 0, 0, 0);
+replace into npc_vendor values (51541, 80060, 0, 0, 0, 0);
+replace into npc_vendor values (51542, 80060, 0, 0, 0, 0);
+replace into npc_vendor values (51543, 80060, 0, 0, 0, 0);
+replace into npc_vendor values (51544, 80060, 0, 0, 0, 0);
+replace into npc_vendor values (51545, 80060, 0, 0, 0, 0);
+replace into npc_vendor values (51546, 80060, 0, 0, 0, 0);
+replace into npc_vendor values (51547, 80060, 0, 0, 0, 0);
