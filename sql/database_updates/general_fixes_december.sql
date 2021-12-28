f702ba226806f055893faa52f47fe6a09c683200
Torta <slowtorta@gmail.com>
2021-12-28 14:38:51 +0100

    Fixed Karazhan Crypt spawn timers.

--- sql/database_updates/general_fixes_december.sql
+++ sql/database_updates/general_fixes_december.sql
@@ -8,3 +8,12 @@ replace into npc_vendor values (51544, 80060, 0, 0, 0, 0);
 replace into npc_vendor values (51545, 80060, 0, 0, 0, 0);
 replace into npc_vendor values (51546, 80060, 0, 0, 0, 0);
 replace into npc_vendor values (51547, 80060, 0, 0, 0, 0);
+-- Icepoint Crawler:
+update creature_template set display_id1 = 981 where entry = 60545;
+-- Greaves of the Elite Guard
+update item_template set armor = 603 where entry = 60434;
+-- Spawn timers (random):
+update creature set spawntimesecsmin = 7200, spawntimesecsmax = 7200 where guid = 2567945 
+-- Karazhan Crypt spawn timers:
+update creature set spawntimesecsmin = 10800, spawntimesecsmax = 10800 where map = 800 and spawntimesecsmin = 300;
+update creature set spawntimesecsmin = 10800, spawntimesecsmax = 10800 where map = 800 and spawntimesecsmin = 25;
