a47a3a224ae1583193bf258176d8bd9d89f9a059
Tanatos <tanatos@gmail.com>
2021-12-29 22:38:45 +0700

    typo
    Signed-off-by: Tanatos <tanatos@gmail.com>

--- src/game/Chat/Chat.h
+++ src/game/Chat/Chat.h
@@ -528,10 +528,6 @@ class ChatHandler
         bool HandleCreaturePathAddPoint(char*);
         bool HandleCreaturePathLaunch(char*);
 
-        // temp new helpers
-        bool HandleMoveLogsCommand(char*);
-        bool HandleSkirmishAddCommand(char*);
-
         Player*   GetSelectedPlayer();
         Creature* GetSelectedCreature();
         Unit*     GetSelectedUnit();
