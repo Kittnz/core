6b7ce9313f1bd1c529f91ca4c9b9d9c30cba3ca4
Neto <netowowclassic@gmail.com>
2021-12-29 00:42:08 -0500

    Vestia Moonspear Update
    Added dialogue in case player does not have quest

--- src/scripts/miscelanneous/random_scripts_3.cpp
+++ src/scripts/miscelanneous/random_scripts_3.cpp
@@ -401,6 +401,9 @@ bool GossipHello_npc_vestia_moonspear(Player* pPlayer, Creature* pCreature)
         pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Ashylah Starcaller has sent me with this missive.", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
         pPlayer->SEND_GOSSIP_MENU(7878, pCreature->GetGUID());
     }
+    else // if no quest - still show a dialogue
+        pPlayer->SEND_GOSSIP_MENU(7878, pCreature->GetGUID());
+
     return true;
 }
 
