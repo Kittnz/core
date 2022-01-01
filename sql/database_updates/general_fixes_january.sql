21c7d51d544a0e15d5a247cc19a27359fea5e23d
Neto <netowowclassic@gmail.com>
2022-01-01 14:47:05 -0500

    Bugfix #567: Duplicate Mailbox

--- sql/database_updates/general_fixes_january.sql
+++ sql/database_updates/general_fixes_january.sql
@@ -299,3 +299,5 @@ UPDATE gameobject SET position_z = 150.00 WHERE guid = 5637;
 UPDATE creature SET spawntimesecsmin = 300, spawntimesecsmax = 300 WHERE id = 3810;
 -- Bug # 560 DR ref: https://classic.wowhead.com/item=3397/young-crocolisk-skin
 UPDATE creature_loot_template SET Chanceorquestchance = -29 WHERE item = 3397;
+-- Bug # 567 Duplicate Mailbox @ Blue Recluse
+delete from gameobject where guid = 73246;
