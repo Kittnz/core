a1718f499febb12df03e436ddda33c8cbf699154
Dormilon <79369417+Dormilon@users.noreply.github.com>
2022-01-03 14:03:29 +0300

    New quest: `The Way of Spiritwalking I`.

--- sql/shamans_quests.sql
+++ sql/shamans_quests.sql
@@ -19,3 +19,13 @@ replace into creature_involvedrelation (id, quest) values (3032, 40343);
 update quest_template set requiredclasses = 64 where entry = 40343;
 update quest_template set requiredraces = 32 where entry = 40343;
 
+-- The Way of Spiritwalking I --
+delete from quest_template where entry = 40344;
+replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel, minlevel, questflags, specialflags, title, details, objectives, requestitemstext, offerrewardtext, reqitemid1, reqitemcount1, reqitemid2, reqitemcount2, reqitemid3, reqitemcount3, reqitemid4, reqitemcount4, reqcreatureorgoid1, reqcreatureorgocount1, reqcreatureorgoid2, reqcreatureorgocount2, reqcreatureorgoid3, reqcreatureorgocount3, reqcreatureorgoid4, reqcreatureorgocount4, srcitemid, srcitemcount, reworreqmoney, rewxp, rewrepfaction1, rewrepvalue1,  rewrepfaction2, rewrepvalue2, rewrepfaction3, rewrepvalue3, rewrepfaction4, rewrepvalue4, rewspell, rewspellcast, completeemote, rewitemid1, rewitemcount1, rewitemid2, rewitemcount2, rewitemid3, rewitemcount3, rewitemid4, rewitemcount4, rewchoiceitemid1, rewchoiceitemcount1, rewchoiceitemid2, rewchoiceitemcount2, rewchoiceitemid3, rewchoiceitemcount3, rewchoiceitemid4, rewchoiceitemcount4,requiredminrepfaction,requiredminrepvalue,objectivetext1) values (0,40344,2,1638,40,40,0,0,'The Way of Spiritwalking I','Long as it been that we tauren shaman have upheld our ancient traditions. Spiritwalking is apart of our culture and around longer then you could even imagine young one. It is how we connect with those of the after life in the arms of the Earthmother.\n\nIf you wish to begin on this path, then special regeants will be required for you to first make your connection with the spirit world.\n\nTo the southwest is a land called Feralas, at Camp Mojache you will find Sage Palerunner, speak with him, and tell him I was the one who sent you.','Travel to Camp Mojache and speak with Sage Palerunner.','Yes?','So, you have been sent here by Beram Skychaser you say. You must be another who is looking to learn the ways of old, come, sit around, there is much to learn.',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,250,81,150,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'');
+
+replace into creature_questrelation (id, quest) values (3032, 40344);
+replace into creature_involvedrelation (id, quest) values (5390, 40344);
+
+update quest_template set requiredclasses = 64 where entry = 40344;
+update quest_template set requiredraces = 32 where entry = 40344;
+
