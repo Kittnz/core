11087dd7559ebf96f867ca58eb1827125b0a6c41
Cosmin POP <mtx.rake@gmail.com>
2022-01-12 12:37:47 +0200

    adjust Risen Lackeys hp

--- sql/database_updates/general_fixes_january.sql
+++ sql/database_updates/general_fixes_january.sql
@@ -781,3 +781,6 @@ REPLACE INTO creature_loot_template VALUES
 (91989, 30022, 0.5, 0, -30022, 1, 0),
 (91989, 30024, 0.5, 0, -30024, 1, 0),
 (91989, 30038, 0.5, 0, -30038, 1, 0);
+
+# hp update for https://github.com/slowtorta/turtlewow-bug-tracker/issues/862
+update creature_template set health_min = 1000, health_max = 1000 where entry = 10482;
