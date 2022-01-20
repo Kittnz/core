a353c9a08842b740f768b3ac6139eb9bdf364373
Neto <netowowclassic@gmail.com>
2022-01-20 00:51:01 -0500

    Quest Update: In Da Banshee’s Favor

--- sql/database_updates/general_fixes_january.sql
+++ sql/database_updates/general_fixes_january.sql
@@ -1743,4 +1743,8 @@ REPLACE INTO `creature` VALUES (2569339, 65138, 0, 0, 0, 0, 0, 14730, 9.33784, 1
 
 REPLACE INTO `gossip_menu` VALUES (65536, 65100, 0);
 
+-- In Da Banshee’s Favor text change and add whistle
+REPLACE INTO `quest_template` VALUES (65007, 2, 36, 55, 60, 55, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 65006, 0, 0, 0, 65020, 1, 0, 'In Da Banshee’s Favor', 'Zul\'jin be countin\' on us to kill da mages at Dalaran. I be gatherin\' information on \'em and it points to dis High Elven Magister.$B$B\nLets be makin a dent in they ranks n\' bring our findins\' to Zul\'Jin. Use dis whistle by da broken down wagon by Dalaran. I be ready.', 'Cull the Dalaran forces with Zo\'hjik.', 'Aye, it be done.', 'Da Banshee will see da might of da Amani.', '', 'Kill 10 Dalaran Magi', '', '', '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 65139, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3000, 4000, 1800, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
+-- Whistle for quest
+REPLACE INTO `item_template` VALUES (65020, 0, 0, 'Amani Whistle', 'A gift from Zo’hijk.', 15798, 1, 0, 1, 0, 0, 0, -1, -1, 1, 55, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 31726, 0, 0, 0, 30000, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 'item_zhojik_whistle');
