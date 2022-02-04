a6b6282f2054e87dc987351c5cf2b83036585bf4
Dormilon <79369417+Dormilon@users.noreply.github.com>
2022-02-04 17:46:32 +0300

    https://github.com/slowtorta/turtlewow-bug-tracker/issues/750 (Kruxis).

--- sql/database_updates/general_fixes_february.sql
+++ sql/database_updates/general_fixes_february.sql
@@ -1860,3 +1860,5 @@ REPLACE INTO creature_loot_template VALUES (91794, 8959, 4.2672, 0, 1, 3, 0);
 REPLACE INTO creature_loot_template VALUES (91794, 7973, 40.4453, 0, 1, 1, 0);
 -- Correct resp time for NPC "The Cow King" setted:
 update creature set spawntimesecsmin = 345600, spawntimesecsmax = 604800 where id = 91799;
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/750(Kruxis):
+UPDATE quest_template SET Objectives = 'Collect 10 Raptor Eggs and bring them to Grovug Mithrilmane stationed in Dun Agarth.' WHERE entry = 55101;
