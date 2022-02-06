22dc925f0dc7ec0a31a1b3909cffdf75b2cd353f
Jameyboor <jamey_D@hotmail.com>
2022-02-06 21:22:29 +0100

    Fix invasion bosses once and for all

---
+++ sql/database_updates/fix_invasion_bosses.sql
@@ -0,0 +1,5 @@
+REPLACE INTO `creature_ai_events` VALUES (1446408, 14464, 0, 6, 0, 100, 0, 0, 0, 0, 0, 1446408, 0, 0, 'Avalanchion - Set Variable on Death');
+REPLACE INTO `creature_ai_events` VALUES (1445709, 14457, 0, 6, 0, 100, 0, 0, 0, 0, 0, 1445709, 0, 0, 'Princess Tempestria - variable on Death');
+REPLACE INTO `creature_ai_events` VALUES (1446107, 14461, 0, 6, 0, 100, 0, 0, 0, 0, 0, 1446107, 0, 0, 'Baron Charr - variable on Death');
+REPLACE INTO `creature_ai_events` VALUES (1445409, 14454, 0, 6, 0, 100, 0, 0, 0, 0, 0, 1445409, 0, 0, 'The Windreaver - variable on Death');
+
