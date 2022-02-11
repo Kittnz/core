26c04f1839686f85323b0a63487ce34197275c8f
Torta <slowtorta@gmail.com>
2022-02-11 07:24:29 +0100

    Fixed Kul Tiran Expeditionary Fleet Repuation.

---
+++ sql/database_updates/fix_kul_tiras_reputation.sql
@@ -0,0 +1,22 @@
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/884
+update quest_template set rewrepfaction1 = 1011, rewrepvalue1 = 25 where entry = 40171;	 -- The Tower of Lapidis IX
+update quest_template set rewrepfaction1 = 1011, rewrepvalue1 = 125 where entry = 40170; -- The Tower of Lapidis VIII
+update quest_template set rewrepfaction1 = 1011, rewrepvalue1 = 50 where entry = 40169;	 -- The Tower of Lapidis VII
+update quest_template set rewrepfaction1 = 1011, rewrepvalue1 = 25 where entry = 40168;	 -- The Tower of Lapidis VI
+update quest_template set rewrepfaction1 = 1011, rewrepvalue1 = 50 where entry = 40166;	 -- The Tower of Lapidis IV
+update quest_template set rewrepfaction1 = 1011, rewrepvalue1 = 25 where entry = 40167;	 -- The Tower of Lapidis V
+update quest_template set rewrepfaction1 = 1011, rewrepvalue1 = 125 where entry = 40165; -- The Tower of Lapidis III
+update quest_template set rewrepfaction1 = 1011, rewrepvalue1 = 50 where entry = 40164;	 -- The Tower of Lapidis II
+update quest_template set rewrepfaction1 = 1011, rewrepvalue1 = 10 where entry = 40163;	 -- The Tower of Lapidis
+update quest_template set rewrepfaction1 = 1011, rewrepvalue1 = 200 where entry = 40156; -- The Tyrant King
+update quest_template set rewrepfaction1 = 1011, rewrepvalue1 = 150 where entry = 40155; -- The Gor'dosh Ogres
+update quest_template set rewrepfaction1 = 1011, rewrepvalue1 = 125 where entry = 40152; -- Faelon's Folly
+update quest_template set rewrepfaction1 = 1011, rewrepvalue1 = 150 where entry = 40151; -- The Aqua Stone
+update quest_template set rewrepfaction1 = 1011, rewrepvalue1 = 150 where entry = 40150; -- Drowning Deeptide
+update quest_template set rewrepfaction1 = 1011, rewrepvalue1 = 199 where entry = 40148; -- Southsea Reserve
+update quest_template set rewrepfaction1 = 1011, rewrepvalue1 = 125 where entry = 40144; -- Southsea Interference
+update quest_template set rewrepfaction1 = 1011, rewrepvalue1 = 75 where entry = 40142;	 -- Sailors Innovation
+update quest_template set rewrepfaction1 = 1011, rewrepvalue1 = 100 where entry = 40141; -- The Boran Family
+update quest_template set rewrepfaction1 = 1011, rewrepvalue1 = 150 where entry = 40094; -- The Water Rat
+update quest_template set rewrepfaction1 = 1011, rewrepvalue1 = 100 where entry = 40093; -- The Tidemaster
+update quest_template set rewrepfaction1 = 1011, rewrepvalue1 = 500 where entry = 40303; -- The Tower of Lapidis X
