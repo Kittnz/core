38568a08041e89572f42db172a9301fcfcea1290
Dormilon <79369417+Dormilon@users.noreply.github.com>
2022-02-12 09:05:21 +0300

    Spell "Curse of the Plague Rat" deleted from NPC "Groveweald Warrior".

---
+++ sql/database_updates/general_fixes_february_2.sql
@@ -0,0 +1,3 @@
+-- Spell "Curse of the Plague Rat" deleted from NPC "Groveweald Warrior":
+REPLACE INTO creature_spells VALUES (180019, 'The Crescent Grove: Groveweald Warrior', 16431, 100, 0, 0, 0, 4, 6, 6, 12, 12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 15232, 100, 1, 0, 0, 12, 1, 1, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+update creature_template set spell_list_id = 180019 where entry = 92100;
