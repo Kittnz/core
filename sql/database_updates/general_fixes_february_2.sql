7fbef6ca4d45d75b110c805f3f6c9d1f354f8f61
Dormilon <79369417+Dormilon@users.noreply.github.com>
2022-02-12 16:21:06 +0300

    Frostly's memorial.

--- sql/database_updates/general_fixes_february_2.sql
+++ sql/database_updates/general_fixes_february_2.sql
@@ -5,3 +5,6 @@ update creature_template set spell_list_id = 180019 where entry = 92100;
 update gameobject set position_x = -334.396, position_y = 0.519818, position_z = 55.9874 where guid = 20881;
 -- Quest "The Completed Orb of Dar'Orahil" have truely prequest alredy:
 update quest_template set prevquestid = 4976 where entry = 4964;
+-- Frostly's memorial:
+REPLACE INTO gameobject_template VALUES (2010797, 9, 25251, 'Frostly', 0, 0, 1, 50525, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, '');
+REPLACE INTO page_text VALUES (50525, 'In good memory of Frostly, a champion of the Horde, a raider, and a friend. He will forever be remembered within our hearts.', 0);
