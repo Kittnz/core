d12964828be4ba275014532114f09d839f71c5ed
Torta <slowtorta@gmail.com>
2022-02-14 14:28:14 +0100

    Guardians of Icecrown shouldn't be immune to Turn Undead, Frost Trap, Goblin Rocket Helm, Stun nor Taunt.

--- sql/database_updates/general_fixes_february_4.sql
+++ sql/database_updates/general_fixes_february_4.sql
@@ -1,2 +1,5 @@
 -- https://github.com/slowtorta/turtlewow-bug-tracker/issues/866
 update gameobject_template set faction = 0, data0 = 259 where entry = 164958;
+
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/485
+update creature_template set mechanic_immune_mask = 1019427442-4194304-2048-1024-8192 where entry = 16441; -- Guardian of Icecrown
