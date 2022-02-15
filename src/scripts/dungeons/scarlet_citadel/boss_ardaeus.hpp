e7012ac87702abb2e3da06c2f0d375168419e6f0
Nolin7777 <nolin.nolin.nolin.nolin@gmail.com>
2022-02-15 21:51:42 +0100

    [SC] Boss Ardaeus: Implement gossip menu

--- src/scripts/dungeons/scarlet_citadel/boss_ardaeus.hpp
+++ src/scripts/dungeons/scarlet_citadel/boss_ardaeus.hpp
@@ -12,6 +12,12 @@
 
 namespace nsArdaeus
 {
+    template <typename Functor>
+    void DoAfterTime(Creature* pCreature, const uint32& uiTime, Functor&& function)
+    {
+        pCreature->m_Events.AddEvent(new LambdaBasicEvent<Functor>(std::move(function)), pCreature->m_Events.CalculateTime(uiTime));
+    }
+
     struct Location
     {
         float m_fX{}, m_fY{}, m_fZ{}, m_fO{}, m_fR0{}, m_fR1{}, m_fR2{}, m_fR3{};
@@ -24,6 +30,74 @@ namespace nsArdaeus
         { 255.f, -100.f, 18.5f, 0.f, 0.f, 0.f, 0.f, 0.f }  // End location
     };
 
+    // Gossip Menu
+    static constexpr uint32 GOSSIP_TEXT{ 1000001 };
+    static constexpr auto GOSSIP_ANSWER{ "GOSSIP_ANSWER" };
+
     // Misc
     static constexpr float ROOM_DIAGONAL{ (54.0f / 2) }; // Room size
+    static constexpr uint32 FACTION_SCARLET{ 67 };       // Scarlet Citadel Faction // TODO: Find proper ID
+    static constexpr uint32 FACTION_NEUTRAL{ 189 };      // Neutral Faction
+
+
+    enum class CombatNotifications
+    {
+        ABOUT_TO_START,
+        START,
+        RAIDWIPE,
+        BOSSDIED,
+        ACHIEVEMENT_FAILED
+    };
+
+    std::string [[nodiscard]] CombatNotification(const CombatNotifications& combatNotifications)
+    {
+        std::string strNotification{};
+        switch (combatNotifications)
+        {
+            case CombatNotifications::ABOUT_TO_START:
+            {
+                return (strNotification = "ABOUT_TO_START");
+            }
+            case CombatNotifications::START:
+            {
+                return (strNotification = "START");
+            }
+            case CombatNotifications::RAIDWIPE:
+            {
+                return (strNotification = "RAIDWIPE");
+            }
+            case CombatNotifications::BOSSDIED:
+            {
+                return (strNotification = "BOSSDIED");
+            }
+            case CombatNotifications::ACHIEVEMENT_FAILED:
+            {
+                return (strNotification = "ACHIEVEMENT_FAILED");
+            }
+        }
+    }
+
+    std::string [[nodiscard]] SayOnPlayersDeath(const uint8& uiRandomNumber)
+    {
+        std::string strNotification{};
+        switch (uiRandomNumber)
+        {
+            case 0:
+            {
+                return (strNotification = "PLACEHOLDER0");
+            }
+            case 1:
+            {
+                return (strNotification = "PLACEHOLDER1");
+            }
+            case 2:
+            {
+                return (strNotification = "PLACEHOLDER2");
+            }
+            case 3:
+            {
+                return (strNotification = "PLACEHOLDER3");
+            }
+        }
+    }
 }
