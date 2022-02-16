cb1c329e7b613fac926bd24b5a424c8dbccb5bf2
Dormilon <79369417+Dormilon@users.noreply.github.com>
2022-02-17 00:38:15 +0300

    Fix name for GO Tree of Lashes.

---
+++ sql/database_updates/dormilon_fixes_february.sql
@@ -0,0 +1,4 @@
+-- Fox name for GO Tree of Lashes:
+delete from gameobject_template where entry = 2010796;
+REPLACE INTO gameobject_template VALUES (2010796, 9, 25251, 'Tree of Lashes', 0, 0, 1, 50526, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, '');
+REPLACE INTO page_text VALUES (50526, 'The Tree of Lashes is covered in countless wounds of varied age. Some still drip with sap, while others are faint, ghostly scars. One name is torn into the tree\'s flesh, and bleeds still: "Ariele."', 0);
