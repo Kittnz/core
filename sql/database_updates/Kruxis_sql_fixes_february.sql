986f3fb711e6e9f6cca5ff5ee0cf8eee195cb8c9
Kruxis <32237664+Kruxis@users.noreply.github.com>
2022-02-16 13:37:08 +0100

    Fixed NPC scaling for Coun

--- sql/database_updates/Kruxis_sql_fixes_february.sql
+++ sql/database_updates/Kruxis_sql_fixes_february.sql
@@ -1,2 +1,5 @@
 -- https://github.com/slowtorta/turtlewow-bug-tracker/issues/1120
 UPDATE creature_template SET beast_family = 7 WHERE entry = 91824;
+
+-- Coun request
+UPDATE creature_template SET scale = 1, level = 15 WHERE entry = 50140;
