c5fdb59823c8394a0b8e9e9c3c1cfbbbc02f4cf7
Kruxis <32237664+Kruxis@users.noreply.github.com>
2022-02-16 13:40:17 +0100

    Update Kruxis_sql_fixes_february.sql

--- sql/database_updates/Kruxis_sql_fixes_february.sql
+++ sql/database_updates/Kruxis_sql_fixes_february.sql
@@ -3,3 +3,4 @@ UPDATE creature_template SET beast_family = 7 WHERE entry = 91824;
 
 -- Coun request
 UPDATE creature_template SET scale = 1, level = 15 WHERE entry = 50140;
+UPDATE creature_template SET display_id1 = 4999 WHERE entry = 60663;
