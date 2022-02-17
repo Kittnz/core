788090692a87f1ea25ff2bcd834b48c9ccd66dd7
Kruxis <32237664+Kruxis@users.noreply.github.com>
2022-02-17 22:48:11 +0100

    Quest ZoneOrSort fix
    Fixed ZoneOrSort of the Quest "Haze of Evil"

--- sql/database_updates/Kruxis_sql_fixes_february.sql
+++ sql/database_updates/Kruxis_sql_fixes_february.sql
@@ -4,3 +4,6 @@ UPDATE creature_template SET beast_family = 7 WHERE entry = 91824;
 -- Coun request
 UPDATE creature_template SET scale = 1, level = 15 WHERE entry = 50140;
 UPDATE creature_template SET display_id1 = 4999 WHERE entry = 60663;
+
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/1125
+UPDATE quest_template SET ZoneOrSort = 1477 WHERE entry = 4143;
