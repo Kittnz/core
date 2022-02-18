0add7f4e7663e75b91bd921910aca1981e3eb667
Dormilon <79369417+Dormilon@users.noreply.github.com>
2022-02-18 16:06:13 +0300

    When pulling one scarab it pull the other one around him (8 yards).

--- sql/database_updates/dormilon_fixes_february.sql
+++ sql/database_updates/dormilon_fixes_february.sql
@@ -2,3 +2,5 @@
 delete from gameobject_template where entry = 2010796;
 REPLACE INTO gameobject_template VALUES (2010796, 9, 25251, 'Tree of Lashes', 0, 0, 1, 50526, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, '');
 REPLACE INTO page_text VALUES (50526, 'The Tree of Lashes is covered in countless wounds of varied age. Some still drip with sap, while others are faint, ghostly scars. One name is torn into the tree\'s flesh, and bleeds still: "Ariele."', 0);
+-- When pulling one scarab it pull the other one around him (8 yards):
+REPLACE INTO `creature_linking_template` (`entry`, `map`, `master_entry`, `flag`, `search_range`) VALUES (7269, 209, 7269, 3, 8);
