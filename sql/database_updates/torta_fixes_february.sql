022338535f2a50d5f2e835d7fcf1690417a287d5
Torta <slowtorta@gmail.com>
2022-02-21 13:39:28 +0100

    Deeptide Sanctum and The Blazno Touch typos.

--- sql/database_updates/torta_fixes_february.sql
+++ sql/database_updates/torta_fixes_february.sql
@@ -1,2 +1,6 @@
 -- https://github.com/slowtorta/turtlewow-bug-tracker/issues/1143
 update item_template set flags = 2048 where entry = 9234;
+
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/1138
+update quest_template set objectives = 'Gather 10 Deeptide Bracelets from Deeptide Naga for Explorer Fangosh in Maul\'ogg Refuge.' where entry = 40275;
+update quest_template set objectives = 'Gather a Gold Bar for Blazno Blastpipe on Gillijim\'s Isle.' where entry = 40190;
