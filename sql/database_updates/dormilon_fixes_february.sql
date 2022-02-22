8b5e519d295836f54e107e4860a01591a8389443
Dormilon <79369417+Dormilon@users.noreply.github.com>
2022-02-22 14:53:42 +0300

    New NPC 'Quorthon' added.

--- sql/database_updates/dormilon_fixes_february.sql
+++ sql/database_updates/dormilon_fixes_february.sql
@@ -8,3 +8,11 @@ REPLACE INTO `creature_linking_template` (`entry`, `map`, `master_entry`, `flag`
 update quest_template set prevquestid = 4976 where entry = 4964;
 -- Phasing for NPC Vereesa removed:
 update creature_template set phase_quest_id = 0 where entry = 80877;
+-- New NPC 'Quorthon' added:
+REPLACE INTO creature_template VALUES
+(60668, 14494, 0, 0, 0, 'Quorthon', 'Nephilim', 0, 55, 55, 3398, 3398, 0, 0, 4500, 290, 16388, 1, 1.14286, 1.3, 18, 5, 0, 0, 1, 96, 125, 0, 248, 1, 2000, 2000, 1, 4608, 0, 0, 0, 0, 0, 0, 70.664, 97.163, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 3038, 0, 0, 0, 0, 0, 0, 1, '');
+set @gossip_menu_id = 41093; set @magic_number = 60668;
+replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); 
+replace into broadcast_text (entry, Male_Text) values (@magic_number, 'Lok\'tar ogar!\n\nIn this camp we do not draw blood from each other, no matter what clan you come from. Here we heal our wounds and rest.\n\nTo prepare for the next battle ahead, against the pinkskins.\n\nThru blood, fire and death!');
+replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_number);
+update creature_template set gossip_menu_id = @gossip_menu_id where entry = @magic_number;
