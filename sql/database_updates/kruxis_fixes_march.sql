424d8fe6cc14eb8f2ec5fd1038dd59f33f4c79c9
Kruxis <32237664+Kruxis@users.noreply.github.com>
2022-03-03 12:51:00 +0100

    Evermoon Staff repair fix

--- sql/database_updates/kruxis_fixes_march.sql
+++ sql/database_updates/kruxis_fixes_march.sql
@@ -1,2 +1,5 @@
 -- Tinyfin thing from discord
 REPLACE INTO creature_involvedrelation VALUES (3978, 40115);
+
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/1179
+UPDATE item_template SET item_level = 5 WHERE entry = 60112;
