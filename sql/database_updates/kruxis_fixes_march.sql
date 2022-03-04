abda61322ddd6c0bcb5664180398b5c57ce9149d
Kruxis <32237664+Kruxis@users.noreply.github.com>
2022-03-04 18:54:03 +0100

    added "name" to NPC

--- sql/database_updates/kruxis_fixes_march.sql
+++ sql/database_updates/kruxis_fixes_march.sql
@@ -3,3 +3,6 @@ REPLACE INTO creature_involvedrelation VALUES (3978, 40115);
 
 -- https://github.com/slowtorta/turtlewow-bug-tracker/issues/1179
 UPDATE item_template SET item_level = 5 WHERE entry = 60112;
+
+-- Fix for Coun
+UPDATE creature_template SET name = '*' WHERE entry = 7;
