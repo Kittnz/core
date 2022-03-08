68b69f3103e840b16d28088016b36b848bd923c4
Torta <slowtorta@gmail.com>
2022-03-09 00:26:57 +0300

    Sinrek's Childern text update.

--- sql/database_updates/torta_fixes_march.sql
+++ sql/database_updates/torta_fixes_march.sql
@@ -4,4 +4,9 @@ update item_template set spellid_2 = 18384, spelltrigger_2 = 1 where entry = 151
 update item_template set spelltrigger_2 = 1 where entry = 21481;
 
 -- Hazzuri Scavenger on Lapidis Isle is tracked as a beast mob:
-update creature_template set beast_family = 0, type = 7, skinning_loot_id = 0 where entry = 91824;
+update creature_tem
+
+-- Sinrek's Childern text update:
+replace into broadcast_text values (9656, '', 'There are costs with war, $c... ones that are often overlooked.\n\nThose costs are the orphans—children who have lost their parents to the far-too-numerous conflicts that rage across Azeroth.\n\nThose children have no one else to turn to come here often to live until they are old enough to fend for themselves.\n\nOne week out of the month, the Horde takes time to give something back to these innocents. This time is called Children\'s Week.', 0, 0, 0, 1, 1, 0, 0, 0, 0);
+replace into broadcast_text values (9659, '', 'There are costs with war, $c... ones that are often overlooked.\n\nThose costs are the orphans—children who have lost their parents to the far-too-numerous conflicts that rage across Azeroth.\n\nThose children have no one else to turn to come here often to live until they are old enough to fend for themselves.\n\nOne week out of the month, the Alliance takes time to give something back to these innocents. This time is called Children\'s Week.', 0, 0, 0, 1, 1, 0, 0, 0, 0);
+
