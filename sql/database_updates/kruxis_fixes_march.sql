b009474c83fe31f6b0b5848a00c37052499691a8
Kruxis <32237664+Kruxis@users.noreply.github.com>
2022-03-09 21:05:24 +0100

    Fixed Jin'dos shade type ~Xerrron

--- sql/database_updates/kruxis_fixes_march.sql
+++ sql/database_updates/kruxis_fixes_march.sql
@@ -6,3 +6,6 @@ UPDATE item_template SET item_level = 5 WHERE entry = 60112;
 
 -- Fix for Coun
 UPDATE creature_template SET name = '*' WHERE entry = 7;
+
+-- make Shades of Jin'do undead instead of beasts / Xerron
+update creature_template set type = 6 where entry = 14986;
