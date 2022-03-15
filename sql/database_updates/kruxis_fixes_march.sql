787c5ccb1270d0ac807fb4407c0211c3326f4d88
Kruxis <32237664+Kruxis@users.noreply.github.com>
2022-03-15 19:12:37 +0100

    Updated immune mask / Fix for Coun

--- sql/database_updates/kruxis_fixes_march.sql
+++ sql/database_updates/kruxis_fixes_march.sql
@@ -15,3 +15,10 @@ UPDATE creature SET spawntimesecsmin = 300, spawntimesecsmax = 300 WHERE id = 93
 
 -- https://github.com/slowtorta/turtlewow-bug-tracker/issues/1193
 UPDATE quest_template SET Details = 'Lapidis was always an interesting, if strange man, at the time we hadn\'t considered his refusal to our curiosity as something potentially bad. But hearing of what he has done upon Lapidis Isle has certainly shifted my opinion.\n\n<Ansirem Runeweaver studies the Shimmering Brass Key>. It would appear that this key still has lingering of magic upon it. It is possible to reconstruct the enchantment, but may take considerable power and regeants to do so. If the key is to work upon the lock as suggested, it will need to be both attuned, and enchanted to the magic that once powered it.\n\nIt is within my power to enchant this key with the same magic that once powered it. The coast of Feralas is covered in water elementals. Bring me a Pure Aqua Orb from one of them, it should be sufficient and powerful enough to mimic the magic of the key.' WHERE entry = 40165;
+
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/1203
+UPDATE creature_template SET mechanic_immune_mask = 16401 WHERE entry = 8907;
+
+-- Fix NPCS for Coun
+UPDATE creature_template SET ai_name = "" WHERE entry IN (60664, 60665);
+UPDATE creature SET spawntimesecsmin = 300, spawntimesecsmax = 300 WHERE id IN (60664, 60665); -- Not spawned on prod yet
