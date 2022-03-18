0cc913736d3cc9d80e910a0910394a09d4a2a9b5
Torta <slowtorta@gmail.com>
2022-03-18 18:48:03 +0300

    New mini-pet for Duroater Labor Union quartermaster.

--- sql/database_updates/companion_world_drop.sql
+++ sql/database_updates/companion_world_drop.sql
@@ -28,4 +28,10 @@ replace into creature_loot_template values (13599, 18967, 2, 6, 1, 1, 0); -- Mud
 replace into creature_loot_template values (4400, 18967, 2, 6, 1, 1, 0);  -- Aku'mai Snapjaw
 replace into creature_loot_template values (4825, 18967, 2, 6, 1, 1, 0);  -- Stolid Snapjaw
 replace into creature_loot_template values (2408, 18967, 2, 6, 1, 1, 0);  -- Snapjaw
-
+-- Water Snake
+update item_template set name = 'Green Water Snake', description = 'Brought to Sparkwater Port aboard merchant ships, these short-lived reptiles are considered pests by the port\'s residents.', buy_price = 6000, sell_price = 1500, required_reputation_faction = 1001, required_reputation_rank = 4 where entry = 50068;
+replace into npc_vendor (entry, item) values (80961, 50068); -- Vanessa Clang <Durotar Labor Union Quartermaster>
+-- Scarlet Snake
+replace into creature_loot_template values (3807, 50069, 2, 6, 1, 1, 0); -- Forsaken Assassin
+replace into creature_loot_template values (2246, 50069, 2, 6, 1, 1, 0); -- Syndicate Assassin
+replace into creature_loot_template values (5623, 50069, 2, 6, 1, 1, 0); -- Wastewander Assassin
