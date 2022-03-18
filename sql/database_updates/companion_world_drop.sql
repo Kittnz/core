0ed333514fb51ea2025ea81e785cffc7932bad03
Torta <slowtorta@gmail.com>
2022-03-18 18:27:45 +0300

    Added various snapjaw companion drops to the turtles all over the world.

---
+++ sql/database_updates/companion_world_drop.sql
@@ -0,0 +1,31 @@
+-- Albino Snapjaw
+replace into creature_loot_template values (3461, 18963, 2, 6, 1, 1, 0);  -- Oasis Snapjaw
+replace into creature_loot_template values (13599, 18963, 2, 6, 1, 1, 0); -- Mudrock Snapjaw
+replace into creature_loot_template values (4400, 18963, 2, 6, 1, 1, 0);  -- Aku'mai Snapjaw
+replace into creature_loot_template values (4825, 18963, 2, 6, 1, 1, 0);  -- Stolid Snapjaw
+replace into creature_loot_template values (2408, 18963, 2, 6, 1, 1, 0);  -- Snapjaw
+-- Hawksbill Snapjaw
+replace into creature_loot_template values (3461, 18965, 2, 6, 1, 1, 0);  -- Oasis Snapjaw
+replace into creature_loot_template values (13599, 18965, 2, 6, 1, 1, 0); -- Mudrock Snapjaw
+replace into creature_loot_template values (4400, 18965, 2, 6, 1, 1, 0);  -- Aku'mai Snapjaw
+replace into creature_loot_template values (4825, 18965, 2, 6, 1, 1, 0);  -- Stolid Snapjaw
+replace into creature_loot_template values (2408, 18965, 2, 6, 1, 1, 0);  -- Snapjaw
+-- Leatherback Snapjaw
+replace into creature_loot_template values (3461, 18966, 2, 6, 1, 1, 0);  -- Oasis Snapjaw
+replace into creature_loot_template values (13599, 18966, 2, 6, 1, 1, 0); -- Mudrock Snapjaw
+replace into creature_loot_template values (4400, 18966, 2, 6, 1, 1, 0);  -- Aku'mai Snapjaw
+replace into creature_loot_template values (4825, 18966, 2, 6, 1, 1, 0);  -- Stolid Snapjaw
+replace into creature_loot_template values (2408, 18966, 2, 6, 1, 1, 0);  -- Snapjaw
+-- Loggerhead Snapjaw
+replace into creature_loot_template values (3461, 18964, 2, 6, 1, 1, 0);  -- Oasis Snapjaw
+replace into creature_loot_template values (13599, 18964, 2, 6, 1, 1, 0); -- Mudrock Snapjaw
+replace into creature_loot_template values (4400, 18964, 2, 6, 1, 1, 0);  -- Aku'mai Snapjaw
+replace into creature_loot_template values (4825, 18964, 2, 6, 1, 1, 0);  -- Stolid Snapjaw
+replace into creature_loot_template values (2408, 18964, 2, 6, 1, 1, 0);  -- Snapjaw
+-- Loggerhead Snapjaw
+replace into creature_loot_template values (3461, 18967, 2, 6, 1, 1, 0);  -- Oasis Snapjaw
+replace into creature_loot_template values (13599, 18967, 2, 6, 1, 1, 0); -- Mudrock Snapjaw
+replace into creature_loot_template values (4400, 18967, 2, 6, 1, 1, 0);  -- Aku'mai Snapjaw
+replace into creature_loot_template values (4825, 18967, 2, 6, 1, 1, 0);  -- Stolid Snapjaw
+replace into creature_loot_template values (2408, 18967, 2, 6, 1, 1, 0);  -- Snapjaw
+
