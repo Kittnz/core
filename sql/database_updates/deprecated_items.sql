1ae39f2144a0fbd76f1bca84be6e7eb5337c5fa9
Torta <slowtorta@gmail.com>
2022-03-18 19:22:21 +0300

    Delete Tinyfin :skull:

--- sql/database_updates/deprecated_items.sql
+++ sql/database_updates/deprecated_items.sql
@@ -1,4 +1,5 @@
 delete from item_template where entry = 69005;
 delete from item_template where entry = 20651;
+delete from item_template where entry = 83010;
 -- Players keep asking about those, but they're not coming back!
 delete from item_template where name like '%Dressing Box%';
