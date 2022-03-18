8d557caa8072c3b949ef6b34996c0b3278644d60
Torta <slowtorta@gmail.com>
2022-03-18 19:21:30 +0300

    Added `Tirisfal Bat` drops.

--- sql/database_updates/deprecated_items.sql
+++ sql/database_updates/deprecated_items.sql
@@ -1,3 +1,4 @@
 delete from item_template where entry = 69005;
+delete from item_template where entry = 20651;
 -- Players keep asking about those, but they're not coming back!
 delete from item_template where name like '%Dressing Box%';
