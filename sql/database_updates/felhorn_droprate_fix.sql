78bda228c3205a3eb8f4caf6a287a7f6cfead53a
Kruxis <32237664+Kruxis@users.noreply.github.com>
2022-03-18 20:11:42 +0100

    fixed braindamage

--- sql/database_updates/felhorn_droprate_fix.sql
+++ sql/database_updates/felhorn_droprate_fix.sql
@@ -3,8 +3,8 @@ REPLACE INTO creature_loot_template VALUES
 (7110, 60295, -14, 0, 1, 1, 0),
 (60461, 60295, -21, 0, 1, 1, 0);
 
-UPDATE creature_loot_template SET ChanceOrQuestChance = -11 WHERE entry = 7105 AND item = 60295;
-UPDATE creature_loot_template SET ChanceOrQuestChance = -15 WHERE entry = 7107 AND item = 60295;
-UPDATE creature_loot_template SET ChanceOrQuestChance = -15 WHERE entry = 7108 AND item = 60295;
-UPDATE creature_loot_template SET ChanceOrQuestChance = -13 WHERE entry = 7109 AND item = 60295;
-UPDATE creature_loot_template SET ChanceOrQuestChance = -16 WHERE entry = 7111 AND item = 60295;
+UPDATE creature_loot_template SET ChanceOrQuestChance = -14 WHERE entry = 7105 AND item = 60295;
+UPDATE creature_loot_template SET ChanceOrQuestChance = -18 WHERE entry = 7107 AND item = 60295;
+UPDATE creature_loot_template SET ChanceOrQuestChance = -18 WHERE entry = 7108 AND item = 60295;
+UPDATE creature_loot_template SET ChanceOrQuestChance = -16 WHERE entry = 7109 AND item = 60295;
+UPDATE creature_loot_template SET ChanceOrQuestChance = -19 WHERE entry = 7111 AND item = 60295;
