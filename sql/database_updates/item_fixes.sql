3edd0ec6848b8e6b2be7d181378598d90146eadd
Torta <slowtorta@gmail.com>
2022-03-28 23:45:24 +0300

    `Cryptwatcher's Call`: fixed item level.

--- sql/database_updates/item_fixes.sql
+++ sql/database_updates/item_fixes.sql
@@ -7,3 +7,5 @@ delete from item_template where name like '%Dressing Box%';
 update item_template set name = 'Steel Helm' where entry = 7922;
 -- Farmer's Guidebook: Surviving on the Land!
 update item_template set sell_price = 0, buy_price = 0 where entry = 51709;
+-- Cryptwatcher's Call can't be repaired:
+update item_template set item_level = 65, required_level = 60 where entry = 83467;
