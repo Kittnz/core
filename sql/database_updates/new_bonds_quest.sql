777aa4ece62d1fb00d18cc3cd3655eed5f4bf9b6
Torta <slowtorta@gmail.com>
2022-03-29 00:09:00 +0300

    Fixed race mask for Melyndella's quest chain in Mulgore.

---
+++ sql/database_updates/new_bonds_quest.sql
@@ -0,0 +1,4 @@
+update quest_template set requiredraces = 434 where entry = 40363;	
+update quest_template set requiredraces = 434 where entry = 40364;	
+update quest_template set requiredraces = 434 where entry = 40365;	
+update quest_template set requiredraces = 434 where entry = 40366;	
