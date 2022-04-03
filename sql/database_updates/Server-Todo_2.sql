f7d93691fc6b87e8832e1cbc52638d06335a03f1
Torta <slowtorta@gmail.com>
2022-04-04 00:02:12 +0200

    Quest `Lucky Coin`: grammar improvements.

--- sql/database_updates/Server-Todo_2.sql
+++
@@ -1,7 +0,0 @@
--- Dragu Formular fix
-UPDATE item_template SET spellid_1 = 46604, name = "Formula: Enchant Gloves - Arcane Power", display_id = 7798 WHERE entry = 70001;
-UPDATE item_template SET spellid_1 = 46605, name = "Formula: Enchant Gloves - Nature Power", display_id = 7798 WHERE entry = 70000;
-UPDATE item_template SET spellid_1 = 46606, name = "Formula: Enchant Gloves - Holy Power", display_id = 7798 WHERE entry = 70002;
-UPDATE item_template SET required_spell = 9787 WHERE entry = 83535;
-UPDATE item_template SET display_id = 32903 WHERE entry = 60405;
-
--- sql/database_updates/quest_fix_40366.sql
+++
@@ -1,2 +0,0 @@
--- https://github.com/slowtorta/turtlewow-bug-tracker/issues/1260
-UPDATE creature_involvedrelation SET id = 60652 WHERE quest = 40366;
