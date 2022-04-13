38816980aba1cb534cb4241b60df578d60b8e57a
Dormilon <79369417+Dormilon@users.noreply.github.com>
2022-04-13 16:09:34 +0300

    New quest: `Captain Grayson's Revenge`.

---
+++ sql/database_updates/captain_graysons_revenge.sql
@@ -0,0 +1,30 @@
+-- Captain Grayson's Revenge
+delete from quest_template where entry = 40396;
+replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel, minlevel, questflags, specialflags, title, details, objectives, requestitemstext, offerrewardtext, reqitemid1, reqitemcount1, reqitemid2, reqitemcount2, reqitemid3, reqitemcount3, reqitemid4, reqitemcount4, reqcreatureorgoid1, reqcreatureorgocount1, reqcreatureorgoid2, reqcreatureorgocount2, reqcreatureorgoid3, reqcreatureorgocount3, reqcreatureorgoid4, reqcreatureorgocount4, srcitemid, srcitemcount, reworreqmoney, rewxp, rewrepfaction1, rewrepvalue1,  rewrepfaction2, rewrepvalue2, rewrepfaction3, rewrepvalue3, rewrepfaction4, rewrepvalue4, rewspell, rewspellcast, completeemote, rewitemid1, rewitemcount1, rewitemid2, rewitemcount2, rewitemid3, rewitemcount3, rewitemid4, rewitemcount4, rewchoiceitemid1, rewchoiceitemcount1, rewchoiceitemid2, rewchoiceitemcount2, rewchoiceitemid3, rewchoiceitemcount3, rewchoiceitemid4, rewchoiceitemcount4,requiredminrepfaction,requiredminrepvalue,objectivetext1) values (40395,40396,2,1581,22,15,0,0,'Captain Grayson\'s Revenge','This is Cookie\'s cookbook, it\'s how I taught him to understand our tongue and how to make a somewhat decent meal. I tell you, I am not sure if it’s because he\'s a Murloc or not but he barely has a taste for flavor.\n\nI am getting off the sails here.\n\nThe night the ship sank, Cookie fled, leaving all of us to perish. I almost forgot! I guess my mind gets foggy nowadays, not that I still have a working brain or a consciousness, mind you.\n\nThat motherless Murloc must pay for this bloody mutiny. Bring me proof of his death and I will reward you handsomely! Perhaps I will even be able to move on.','End Cookie.','Is he done with?','I can\'t believe he didn\'t toss this away.\n\nIn my anger I might\'ve mistaken Cookie’s actions, he was always a scared catfish. He must\'ve panicked.\n\n<Grayson sighs for a moment>.\n\nDrying the book, keeping this necklace...\n\nOh $N, what have I done.',60526,1,0,0,0,0,0,0,645,1,0,0,0,0,0,0,0,0,600,1950,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'');
+
+replace into creature_questrelation (id, quest) values (392, 40396);
+replace into creature_involvedrelation (id, quest) values (392, 40396);
+
+REPLACE INTO item_template VALUES
+(60526, 12, 0, 'Grayson\'s Pendant', '', 9854, 1, 2048, 1, 0, 0, 0, -1, -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, NULL);
+
+delete from creature_loot_template where item = 60526;
+replace into creature_loot_template values
+(645,60526,-100,1,1,1,0);
+
+REPLACE INTO creature_template VALUES
+(60709, 1305, 0, 0, 0, 'Cookie', 'The Ship\'s Cook', 0, 20, 20, 2904, 2904, 0, 0, 852, 35, 0, 1, 1.14286, 0, 20, 5, 0, 1, 1, 85, 110, 0, 90, 1, 2000, 2000, 1, 32832, 0, 0, 0, 0, 0, 0, 26.24, 36.08, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 645, 0, 0, 0, 0, 0, 0, '');
+REPLACE INTO creature_template_addon VALUES
+(60709, 0, 0, 0, 0, 0, 0, '9617');
+
+update creature_template set script_name = 'npc_captain_grayson' where entry = 392;
+
+set @gossip_menu_id = 41095; set @magic_number = 392;
+replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); 
+replace into broadcast_text (entry, Male_Text) values (@magic_number, 'Do not be alarmed, $r. I have long since passed from this land but I intend no harm to your kind. I have witnessed too much death in my time. My only wish now is for peace. Perhaps you can help my cause.');
+replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_number);
+update creature_template set gossip_menu_id = @gossip_menu_id where entry = @magic_number;
+
+set @magic_number = 30019;
+replace into broadcast_text (entry, Male_Text) values (@magic_number, 'I took Cookie on the ship when he was very young, he was hostile and bitter at first but through that book, you\'ve found we had developed a bond. In a way Cookie was able to understand anyone without actually knowing a word they were saying.\n\nNot sure what to tell you matey, Cookie was my friend and I\'ve sent you to end him, which may be my greatest regret yet.');
+replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_number);
--- src/scripts/miscelanneous/random_scripts_3.cpp
+++ src/scripts/miscelanneous/random_scripts_3.cpp
@@ -3364,9 +3364,62 @@ bool GOSelect_mournful_apparition_atack(Player* pPlayer, GameObject* pGo, uint32
     return false;
 }
 
+bool QuestRewarded_npc_captain_grayson(Player* pPlayer, Creature* pQuestGiver, Quest const* pQuest)
+{
+    if (!pQuestGiver || !pPlayer) return false;
+
+    if (pQuest->GetQuestId() == 40396 && !pQuestGiver->FindNearestCreature(60709, 30.0F)) // Captain Grayson's Revenge
+    {
+        Creature* npc_cookie = pQuestGiver->SummonCreature(60709, -11410.70F, 1966.56F, 10.60F, 6.12F, TEMPSUMMON_TIMED_DESPAWN, 0.125 * MINUTE * IN_MILLISECONDS);
+
+        DoAfterTime(pPlayer, 5 * IN_MILLISECONDS, [player = pPlayer, npc = pQuestGiver]() {
+            Creature* npc_cookie = npc->FindNearestCreature(60709, 30.0F);
+            npc_cookie->PMonsterEmote("Cookie looks at Grayson with sadness in his eyes and waves him off.");
+            npc_cookie->MonsterSay("Mrrgl?");
+            });
+        DoAfterTime(pPlayer, 9 * IN_MILLISECONDS, [player = pPlayer, npc = pQuestGiver]() {
+            Creature* npc_captain_grayson = npc->FindNearestCreature(392, 30.0F);
+            npc_captain_grayson->MonsterSay("Cookie, I am sorry! I swear I will make it right. Farewell, my friend.");
+            });
+    }
+
+    return false;
+}
+
+bool GossipHello_npc_captain_grayson(Player* pPlayer, Creature* pCreature)
+{
+    if (pPlayer->GetQuestRewardStatus(40396))
+    {
+        pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TALK, "What was Cookie to you, Grayson?", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+    }
+
+    if (pCreature->IsQuestGiver())
+        pPlayer->PrepareQuestMenu(pCreature->GetGUID());
+
+    pPlayer->SEND_GOSSIP_MENU(392, pCreature->GetGUID());
+    return true;
+}
+
+bool GossipSelect_npc_captain_grayson(Player* pPlayer, Creature* pCreature, uint32 uiSender, uint32 uiAction)
+{
+    if (uiAction == GOSSIP_ACTION_INFO_DEF + 1)
+    {
+        pPlayer->SEND_GOSSIP_MENU(30019, pCreature->GetGUID());
+    }
+
+    return true;
+}
+
 void AddSC_random_scripts_3()
 {
     Script* newscript;
+
+    newscript = new Script;
+    newscript->Name = "npc_captain_grayson";
+    newscript->pQuestRewardedNPC = &QuestRewarded_npc_captain_grayson;
+    newscript->pGossipHello = &GossipHello_npc_captain_grayson;
+    newscript->pGossipSelect = &GossipSelect_npc_captain_grayson;
+    newscript->RegisterSelf();
 
     newscript = new Script;
     newscript->Name = "mournful_apparition_atack";
