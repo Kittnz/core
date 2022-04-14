cca24a667fbb90a28ddb5fbe082c6c85ec4f7b69
Torta <slowtorta@gmail.com>
2022-04-14 22:47:29 +0200

    Update lucky_coin_grammar.sql

--- sql/database_updates/lucky_coin_grammar.sql
+++ sql/database_updates/lucky_coin_grammar.sql
@@ -1,6 +1,7 @@
 update quest_template set 
 
-details = 'Hey... uhm, excuse me, I have a request for you!\n\nHah, I\'ve said it! It makes me so nervous to approach people leaving the abbey, but it\'s the only way I can possibly get my wish fulfilled.\n\nOh, the request, of course! You see, I love visiting Stormwind when I go to trade at Gallina Winery and I always walk past the trade district fountain, which is right next to the bank.\n\nCould you toss a coin of mine into the fountain for good luck? I\'ve heard that if you drop a coin into the fountain, your wish might get granted, and my wish is to visit Stormwind again as soon as possible!',
-objectives = 'Drop a coin in the Trade District Fountain in the Stormwind City for Teresa.'
+details = 'Hey... uhm, excuse me, I have a request for you!\n\nHah, I\'ve said it! It makes me so nervous to approach people leaving the abbey, but it\'s the only way I can possibly get my wish fulfilled.\n\nOh, the request, of course! You see, I love visiting Stormwind when I go to trade at Gallina Winery and I always walk past the trade district fountain, which is right next to the bank.\n\nCould you toss a coin of mine into the fountain for good luck? I\'ve heard that if you drop a coin into the fountain, your wish might get granted, and my wish is to visit Stormwind again as soon as possible!\n\nOne more thing, please send my greetings to Suzetta Gallina!',
+objectives = 'Drop a coin in the Trade District Fountain in the Stormwind City for Teresa.',
+requestitemstext = 'Greetings! Are you new here?'
 
 where entry = 60050;
