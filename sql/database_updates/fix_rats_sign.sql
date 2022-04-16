a9b3454a5ab0628c8b388ae30e42ee89389a9e8a
Kruxis <32237664+Kruxis@users.noreply.github.com>
2022-04-16 14:00:13 +0200

    adjustment

--- sql/database_updates/fix_rats_sign.sql
+++ sql/database_updates/fix_rats_sign.sql
@@ -1,2 +1,2 @@
 -- Fix Rats Sign
-UPDATE gameobject_template SET name = '<Rats>', data0 = 1, data1 = 1 WHERE entry = 2004012;
+UPDATE gameobject_template SET name = '<Rats>', data0 = 1, data1 = 1 WHERE entry = 6000003;
