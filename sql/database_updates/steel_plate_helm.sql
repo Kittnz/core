d6207c38c923dd47f2edda6883a36119e8c600bf
Torta <slowtorta@gmail.com>
2022-04-20 02:01:28 +0200

    Fixed Steel Plate Helm.

--- sql/database_updates/steel_plate_helm.sql
+++ sql/database_updates/steel_plate_helm.sql
@@ -1,1 +1,1 @@
-update item_template set spellid_1 = 0, required_skill = 163, required_skill_rank = 230 where entry = 83415;
+update item_template set spellid_1 = 0, required_skill = 164, required_skill_rank = 230 where entry = 83415;
