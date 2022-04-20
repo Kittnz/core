4f7437868e9e4b323b33c690bf67acc324af13e9
Torta <slowtorta@gmail.com>
2022-04-20 19:07:29 +0200

    Fixed duplicate of High Elf Ranger.

---
+++ sql/database_updates/alahthalas_questrelations.sql
@@ -0,0 +1,4 @@
+delete from creature_template where entry = 60429;
+
+replace into creature_questrelation (id, quest) values (80230, 40392);
+replace into creature_questrelation (id, quest) values (80230, 40393);
