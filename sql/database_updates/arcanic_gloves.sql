5d86b0f6cb6d730de883c495d4990e14fe75be25
Torta <slowtorta@gmail.com>
2022-04-20 12:25:13 +0200

    Update arcanic_gloves.sql

--- sql/database_updates/arcanic_gloves.sql
+++ sql/database_updates/arcanic_gloves.sql
@@ -1,1 +1,3 @@
-update item_template set stat_value2 = 6, stat_type2 = 6 where entry = 61057;
+update item_template set stat_value1 = 16, stat_type1 = 7 where entry = 61057;
+update item_template set stat_value2 = 0, stat_type2 = 0 where entry = 61057;
+update item_template set stat_value3 = 0, stat_type3 = 0 where entry = 61057;
