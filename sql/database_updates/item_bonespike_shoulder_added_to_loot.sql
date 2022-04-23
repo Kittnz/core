b84ba85c37547025981f1de02de75508f2426937
Dormilon <79369417+Dormilon@users.noreply.github.com>
2022-04-23 11:34:58 +0300

    Item 'Bonespike Shoulder' added to drop loot.

--- sql/database_updates/item_bonespike_shoulder_added_to_loot.sql
+++ sql/database_updates/item_bonespike_shoulder_added_to_loot.sql
@@ -1,3 +1,4 @@
 -- Item 'Bonespike Shoulder' added to drop loot.
-REPLACE INTO creature_loot_template VALUES
-(10429, 12588, 1, 0, 1, 1, 0);
+INSERT INTO `vmangos`.`creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (10429, 10429, 100, 0, -10429, 1, 0, 0, 10);
+
+INSERT INTO `vmangos`.`reference_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (10429, 12588, 0, 1, 1, 1, 0, 0, 7);
