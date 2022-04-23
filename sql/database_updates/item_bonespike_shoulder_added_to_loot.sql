dac8db649b272b384b4000abf3e6c54e3f4cd4b0
Torta <slowtorta@gmail.com>
2022-04-23 14:29:13 +0200

    Fixed copied query.

--- sql/database_updates/item_bonespike_shoulder_added_to_loot.sql
+++ sql/database_updates/item_bonespike_shoulder_added_to_loot.sql
@@ -1,4 +1,4 @@
 -- Item 'Bonespike Shoulder' added to drop loot.
-INSERT INTO `vmangos`.`creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (10429, 10429, 100, 0, -10429, 1, 0, 0, 10);
+REPLACE INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (10429, 10429, 100, 0, -10429, 1, 0);
 
-INSERT INTO `vmangos`.`reference_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (10429, 12588, 0, 1, 1, 1, 0, 0, 7);
+REPLACE INTO `reference_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (10429, 12588, 0, 1, 1, 1, 0);
