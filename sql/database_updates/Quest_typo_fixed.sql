e03cf8b3eca8a04099426d9dad637e44032cc7a8
Kruxis <32237664+Kruxis@users.noreply.github.com>
2022-04-24 12:11:48 +0200

    Fixed typo in "Out for Delivery"

---
+++ sql/database_updates/Quest_typo_fixed.sql
@@ -0,0 +1,1 @@
+UPDATE quest_template SET Details = 'I am not going anywhere near that Tauren!\n\nI\'m unemployed, remember? You want to see the deliver through, you take it to Kaja yourself.' WHERE entry = 60122;
