e69b5ea3a3c0ca375d9cf9c8f95dcc75edd3853e
Torta <slowtorta@gmail.com>
2022-05-16 02:06:36 +0200

    Update hateforge_mining_animations.sql

--- sql/database_updates/hateforge_mining_animations.sql
+++ sql/database_updates/hateforge_mining_animations.sql
@@ -14,12 +14,12 @@ replace into creature_addon (guid, mount, bytes1, b2_0_sheath, b2_1_flags, emote
 replace into creature_addon (guid, mount, bytes1, b2_0_sheath, b2_1_flags, emote, moveflags, auras) VALUES (2570594, 0, 0, 0, 0, 0, 0, 0);
 replace into creature_addon (guid, mount, bytes1, b2_0_sheath, b2_1_flags, emote, moveflags, auras) VALUES (2570595, 0, 0, 0, 0, 0, 0, 0);
 -- GUIDS FOR WORK/CRAFT EMOTE
-replace into creature_addon (guid, mount, bytes1, b2_0_sheath, b2_1_flags, emote, moveflags, auras) VALUES (2570460, 0, 0, 0, 0, 173, 0, 0);
-replace into creature_addon (guid, mount, bytes1, b2_0_sheath, b2_1_flags, emote, moveflags, auras) VALUES (2570452, 0, 0, 0, 0, 173, 0, 0);
-replace into creature_addon (guid, mount, bytes1, b2_0_sheath, b2_1_flags, emote, moveflags, auras) VALUES (2570487, 0, 0, 0, 0, 173, 0, 0);
-replace into creature_addon (guid, mount, bytes1, b2_0_sheath, b2_1_flags, emote, moveflags, auras) VALUES (2570697, 0, 0, 0, 0, 173, 0, 0);
-replace into creature_addon (guid, mount, bytes1, b2_0_sheath, b2_1_flags, emote, moveflags, auras) VALUES (2570606, 0, 0, 0, 0, 173, 0, 0);
-replace into creature_addon (guid, mount, bytes1, b2_0_sheath, b2_1_flags, emote, moveflags, auras) VALUES (2570646, 0, 0, 0, 0, 173, 0, 0);
+replace into creature_addon (guid, mount, bytes1, b2_0_sheath, b2_1_flags, emote, moveflags, auras) VALUES (2570460, 0, 0, 0, 0, 69, 0, 0);
+replace into creature_addon (guid, mount, bytes1, b2_0_sheath, b2_1_flags, emote, moveflags, auras) VALUES (2570452, 0, 0, 0, 0, 69, 0, 0);
+replace into creature_addon (guid, mount, bytes1, b2_0_sheath, b2_1_flags, emote, moveflags, auras) VALUES (2570487, 0, 0, 0, 0, 69, 0, 0);
+replace into creature_addon (guid, mount, bytes1, b2_0_sheath, b2_1_flags, emote, moveflags, auras) VALUES (2570697, 0, 0, 0, 0, 69, 0, 0);
+replace into creature_addon (guid, mount, bytes1, b2_0_sheath, b2_1_flags, emote, moveflags, auras) VALUES (2570606, 0, 0, 0, 0, 69, 0, 0);
+replace into creature_addon (guid, mount, bytes1, b2_0_sheath, b2_1_flags, emote, moveflags, auras) VALUES (2570646, 0, 0, 0, 0, 69, 0, 0);
 -- GUIDS FOR MINE EMOTE
 replace into creature_addon (guid, mount, bytes1, b2_0_sheath, b2_1_flags, emote, moveflags, auras) VALUES (2570597, 0, 0, 0, 0, 233, 0, 0);
 replace into creature_addon (guid, mount, bytes1, b2_0_sheath, b2_1_flags, emote, moveflags, auras) VALUES (2570645, 0, 0, 0, 0, 233, 0, 0);
