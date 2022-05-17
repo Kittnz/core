af94181d1369f4275f65dd7cb3e8c77a4f593d4a
Torta <slowtorta@gmail.com>
2022-05-17 16:04:22 +0200

    Update verog_the_dervish.sql

--- sql/database_updates/verog_the_dervish.sql
+++ sql/database_updates/verog_the_dervish.sql
@@ -1,2 +1,2 @@
 -- Increase spawn chance of Verog the Dervish:
-update creature_ai_events set event_chance = 20 where id in (327402, 327504, 339703); 
+update creature_ai_events set event_chance = 10 where id in (327402, 327504, 339703); 
