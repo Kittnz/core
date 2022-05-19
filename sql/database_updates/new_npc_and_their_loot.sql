a0ed4bdfa4b13b5d9a2501ea54ba8c9000a9972b
Dormilon <79369417+Dormilon@users.noreply.github.com>
2022-05-19 19:38:53 +0300

    NPC `Karfang` gossip fix.

--- sql/database_updates/new_npc_and_their_loot.sql
+++ sql/database_updates/new_npc_and_their_loot.sql
@@ -427,7 +427,7 @@ update creature_template set gossip_menu_id = @gossip_menu_id where entry = @mag
 
 set @gossip_menu_id = 41119; set @magic_number = 60770;
 replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); 
-replace into broadcast_text (entry, Male_Text) values (@magic_number, 'My name is Karfang, I am leader here, my people should cause you no harm, lest you insult or bother them to much. There is many things to discuss, and many things to do for us to secure our place in the world.\n\nThe Blackrock find themselves now within the mountain, manipulated by greater forces, I would not have my people follow that same madness that drives the Old Horde now.');
+replace into broadcast_text (entry, Male_Text) values (@magic_number, '"My name is Karfang, I am leader here.\n\nMy people should cause you no harm, lest you insult, or bother them too much. There are many things to discuss, and many things to do for us to secure our place in this world.\n\nThe Blackrock Clan find themselves manipulated by greater forces. The Old Horde is a shell of what it once was, and I will not allow my people to follow them into madness.');
 replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_number);
 update creature_template set gossip_menu_id = @gossip_menu_id where entry = @magic_number;
 
