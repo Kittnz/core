3eb59f11cbf6311a6d47d395640558639da6958d
Torta <slowtorta@gmail.com>
2022-05-19 12:40:07 +0200

    Fix migrations.

--- sql/database_updates/gm_ranks.sql
+++ sql/database_updates/gm_ranks.sql
@@ -1,23 +1,23 @@
-CREATE TABLE `staff_ranks` (
+CREATE TABLE IF NOT EXISTS `staff_ranks` (
   `bit_index` INT UNSIGNED NOT NULL,
   `name` VARCHAR(20) NOT NULL,
   PRIMARY KEY (`bit_index`),
   UNIQUE INDEX `bit_index_UNIQUE` (`bit_index` ASC),
   UNIQUE INDEX `name_UNIQUE` (`name` ASC));
 
-ALTER TABLE `command` 
-DROP COLUMN `flags`,
-DROP COLUMN `help`,
-CHANGE COLUMN `security` `rank` VARCHAR(20) NOT NULL DEFAULT '0' ;
+DROP TABLE IF EXISTS `command`;
 
-CREATE TABLE `staff_ranks` (
+CREATE TABLE IF NOT EXISTS `command` (
+  `name` VARCHAR(50) NOT NULL,
+  `rank` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`name`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC));   
+
+CREATE TABLE IF NOT EXISTS `staff_ranks` (
   `bit_index` INT UNSIGNED NOT NULL,
   `name` VARCHAR(20) NOT NULL,
   PRIMARY KEY (`bit_index`),
   UNIQUE INDEX `bit_index_UNIQUE` (`bit_index` ASC),
   UNIQUE INDEX `name_UNIQUE` (`name` ASC));
   
-ALTER TABLE `realmlist` 
-CHANGE COLUMN `allowedSecurityLevel` `allowedSecurityLevel` INT UNSIGNED NOT NULL DEFAULT '0' ;
-
 UPDATE `mangos_string` SET `content_default` = 'Your account level is: %s', `content_loc2` = 'Votre niveau de compte est: %s' WHERE (`entry` = '11');
