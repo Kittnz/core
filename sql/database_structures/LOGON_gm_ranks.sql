99e2ba20a51208e2d0b5da05e63b4455bcac3659
Torta <slowtorta@gmail.com>
2022-05-20 22:54:10 +0200

    Update LOGON_gm_ranks.sql

--- sql/database_structures/LOGON_gm_ranks.sql
+++ sql/database_structures/LOGON_gm_ranks.sql
@@ -1,11 +1,3 @@
-UPDATE account acc
-INNER JOIN account_access acs ON acc.id = acs.id
-SET acc.gmlevel = acs.gmlevel;
-
-ALTER TABLE `account` 
-CHANGE COLUMN `gmlevel` `rank` INT UNSIGNED NOT NULL DEFAULT '0' ;
+ALTER TABLE `account` CHANGE COLUMN `gmlevel` `rank` INT UNSIGNED NOT NULL DEFAULT '0' ;
 
 DROP TABLE `account_access`;
-
-ALTER TABLE `realmlist` 
-CHANGE COLUMN `allowedSecurityLevel` `allowedSecurityLevel` INT UNSIGNED NOT NULL DEFAULT '0' ;
