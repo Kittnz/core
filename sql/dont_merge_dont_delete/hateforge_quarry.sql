943a8c9242ce26c7b158e1ba714940e3169e1d1d
Nolin7777 <nolin.nolin.nolin.nolin@gmail.com>
2022-05-22 10:27:32 +0200

    [HQ] Implement script for Hargesh Doomcaller

---
+++ sql/dont_merge_dont_delete/hateforge_quarry.sql
@@ -0,0 +1,3 @@
+
+UPDATE `creature_template` SET `script_name` = 'boss_hargesh_doomcaller' WHERE `entry` = 60737; -- Assign C++ Script
+UPDATE `creature_template` SET `unit_class` = '4' WHERE `entry` = 60737; -- Hargesh should have mana since he is casting shadow spells
---
+++ src/scripts/dungeons/hateforge_quarry/hateforge_quarry_trash.cpp
@@ -0,0 +1,15 @@
+
+#include "hateforge_quarry.h"
+
+
+
+
+
+
+
+
+
+void AddSC_trash_mobs_hateforge_quarry()
+{
+
+}
