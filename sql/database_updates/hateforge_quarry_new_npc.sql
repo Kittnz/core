c294bd96621bcecdbacdaeda935f55b7510045a5
Dormilon <79369417+Dormilon@users.noreply.github.com>
2022-05-22 11:49:45 +0300

    `HATEFORGE QUARRY` NPCs damage and spell fixes.

--- sql/database_updates/hateforge_quarry_new_npc.sql
+++ sql/database_updates/hateforge_quarry_new_npc.sql
@@ -9,19 +9,18 @@
 -- Hateforge Warden, display ID 18849, level 53 elite, weapon1 : 14880, weapon2 : 15991 , casts 12169 every 10 seconds, casts 871 at 25% hp (Has 50% more armor) ,humanoid , faction 54
 -- Twilight Fireblade, display ID 18851, 18852, 18853, level 54 elite, weapon1 : 10823, weapon2: 15892, casts 56524 on 1 nearby friendly at start of combat (Has 50% more armor) ,humanoid , faction 54
 -- Twilight Shadowcaller, display ID 18854, 18855, 18856, level 54 elite, weapon1: 12250, casts 56519 every 40 seconds ,humanoid , faction 54
-
 REPLACE INTO creature_template VALUES
-(60716, 18834, 18835, 0, 0, 'Hateforge Watcher', NULL, 0, 48, 49, 4179, 4179, 0, 0, 2432, 54, 0, 1, 1.14286, 0, 20, 5, 0, 1, 1, 210, 260, 0, 226, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 54.6056, 75.0827, 100, 7, 0, 60716, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 127, 155, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
-(60717, 18836, 18837, 0, 0, 'Hateforge Chemist', NULL, 0, 52, 53, 6876, 6876, 0, 0, 2432, 54, 0, 1, 1.14286, 0, 20, 5, 0, 1, 1, 301, 334, 0, 226, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 54.6056, 75.0827, 100, 7, 0, 60717, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 127, 155, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
-(60718, 18838, 18839, 0, 0, 'Hateforge Cleric', NULL, 0, 51, 52, 5151, 5151, 2100, 2100, 2232, 54, 0, 1, 1.14286, 0, 20, 5, 0, 1, 1, 290, 301, 0, 226, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 54.6056, 75.0827, 100, 7, 0, 60718, 0, 0, 0, 0, 0, 0, 0, 0, 25314, 15654, 0, 0, 180023, 0, 127, 155, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
-(60719, 18840, 18841, 0, 0,'Hateforge Craftsman', NULL, 0, 50, 52, 5000, 5151, 0, 0, 2232, 54, 0, 1, 1.14286, 0, 20, 5, 0, 1, 1, 280, 301, 0, 226, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 54.6056, 75.0827, 100, 7, 0, 60719, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 127, 155, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
-(60720, 18842, 0, 0, 0, 'Hateforge Engineer', NULL, 0, 51, 52, 3800, 3800, 0, 0, 2840, 54, 0, 1, 1.14286, 0, 20, 5, 40, 0, 1, 87, 107, 0, 230, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 67.32, 92.565, 100, 7, 0, 60720, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 101, 137, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
-(60721, 18843, 18844, 0, 0, 'Hateforge Miner', NULL, 0, 49, 50, 4179, 4400, 0, 0, 2432, 54, 0, 1, 1.14286, 0, 20, 5, 0, 1, 1, 260, 280, 0, 226, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 54.6056, 75.0827, 100, 7, 0, 60721, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 127, 155, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
-(60722, 18845, 18846, 0, 0, 'Hateforge Overseer', NULL, 0, 53, 53, 8100, 8100, 0, 0, 3400, 54, 0, 1.1, 1.14286, 0, 20, 5, 0, 1, 1, 344, 444, 0, 244, 1, 1300, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 69.696, 95.832, 100, 7, 0, 60722, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 116, 157, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
-(60723, 18847, 18848, 0, 0, 'Hateforge Taskmaster', NULL, 0, 52, 53, 7043, 7043, 0, 0, 2432, 54, 0, 1, 1.14286, 0, 20, 5, 0, 1, 1, 260, 280, 0, 226, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 54.6056, 75.0827, 100, 7, 0, 60723, 0, 0, 0, 0, 0, 0, 0, 0, 56522, 0, 0, 0, 180027, 0, 127, 155, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
-(60724, 18849, 0, 0, 0, 'Hateforge Warden', NULL, 0, 53, 53, 7043, 7124, 0, 0, 4000, 54, 0, 1.1, 1.14286, 0, 20, 5, 0, 1, 1, 260, 280, 0, 226, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 69.696, 95.832, 100, 7, 0, 60724, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 116, 157, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
-(60725, 18851, 18852, 18853, 0, 'Hateforge Fireblade', NULL, 0, 54, 54, 6273, 6273, 0, 0, 4000, 54, 0, 1.1, 1.14286, 0, 20, 5, 0, 1, 1, 260, 280, 0, 226, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 69.696, 95.832, 100, 7, 0, 60725, 0, 0, 0, 0, 0, 0, 0, 0, 56524, 0, 0, 0, 180029, 0, 116, 157, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
-(60726, 18854, 18855, 18856, 0, 'Hateforge Shadowcaller', NULL, 0, 54, 54, 6273, 6273, 0, 0, 2500, 54, 0, 1.1, 1.14286, 0, 20, 5, 0, 1, 1, 260, 280, 0, 226, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 69.696, 95.832, 100, 7, 0, 60726, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 116, 157, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, '');
+(60716, 18834, 18835, 0, 0, 'Hateforge Watcher', NULL, 0, 48, 49, 4179, 4179, 0, 0, 2432, 54, 0, 1, 1.14286, 0, 20, 5, 0, 1, 1, 273, 338, 0, 226, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 54.6056, 75.0827, 100, 7, 0, 60716, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 127, 155, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
+(60717, 18836, 18837, 0, 0, 'Hateforge Chemist', NULL, 0, 52, 53, 6876, 6876, 0, 0, 2432, 54, 0, 1, 1.14286, 0, 20, 5, 0, 1, 1, 451, 501, 0, 226, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 54.6056, 75.0827, 100, 7, 0, 60717, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 127, 155, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
+(60718, 18838, 18839, 0, 0, 'Hateforge Cleric', NULL, 0, 51, 52, 5151, 5151, 2100, 2100, 2232, 54, 0, 1, 1.14286, 0, 20, 5, 0, 1, 1, 290, 301, 0, 226, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 54.6056, 75.0827, 100, 7, 0, 60718, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 127, 155, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
+(60719, 18840, 18841, 0, 0,'Hateforge Craftsman', NULL, 0, 50, 52, 5000, 5151, 0, 0, 2232, 54, 0, 1, 1.14286, 0, 20, 5, 0, 1, 1, 350, 376, 0, 226, 1, 1670, 1670, 1, 0, 0, 0, 0, 0, 0, 0, 54.6056, 75.0827, 100, 7, 0, 60719, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 127, 155, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
+(60720, 18842, 0, 0, 0, 'Hateforge Engineer', NULL, 0, 51, 52, 3800, 3800, 0, 0, 2840, 54, 0, 1, 1.14286, 0, 20, 5, 40, 0, 1, 130, 160, 0, 230, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 67.32, 92.565, 100, 7, 0, 60720, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 101, 137, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
+(60721, 18843, 18844, 0, 0, 'Hateforge Miner', NULL, 0, 49, 50, 4179, 4400, 0, 0, 2432, 54, 0, 1, 1.14286, 0, 20, 5, 0, 1, 1, 364, 392, 0, 226, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 54.6056, 75.0827, 100, 7, 0, 60721, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 127, 155, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
+(60722, 18845, 18846, 0, 0, 'Hateforge Overseer', NULL, 0, 53, 53, 8100, 8100, 0, 0, 3400, 54, 0, 1.1, 1.14286, 0, 20, 5, 0, 1, 1, 412, 532, 0, 244, 1, 1300, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 69.696, 95.832, 100, 7, 0, 60722, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 116, 157, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
+(60723, 18847, 18848, 0, 0, 'Hateforge Taskmaster', NULL, 0, 52, 53, 7043, 7043, 0, 0, 2432, 54, 0, 1, 1.14286, 0, 20, 5, 0, 1, 1, 325, 350, 0, 226, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 54.6056, 75.0827, 100, 7, 0, 60723, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 127, 155, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
+(60724, 18849, 0, 0, 0, 'Hateforge Warden', NULL, 0, 53, 53, 7043, 7124, 0, 0, 4000, 54, 0, 1.1, 1.14286, 0, 20, 5, 0, 1, 1, 325, 350, 0, 226, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 69.696, 95.832, 100, 7, 0, 60724, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 116, 157, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
+(60725, 18851, 18852, 18853, 0, 'Twilight Fireblade', NULL, 0, 54, 54, 6273, 6273, 0, 0, 4000, 54, 0, 1.1, 1.14286, 0, 20, 5, 0, 1, 1, 286, 308, 2, 226, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 69.696, 95.832, 100, 7, 0, 60725, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 116, 157, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
+(60726, 18854, 18855, 18856, 0, 'Twilight Shadowcaller', NULL, 0, 54, 54, 6273, 6273, 0, 0, 2500, 54, 0, 1.1, 1.14286, 0, 20, 5, 0, 1, 1, 338, 364, 0, 226, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 69.696, 95.832, 100, 7, 0, 60726, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 116, 157, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, '');
 
 set @equip_template = 20085; set @weapon_1 = 12250; set @weapon_2 = 0; set @weapon_3 = 0; set @creature = 60726;
 replace into creature_equip_template values (@equip_template, @weapon_1, @weapon_2, @weapon_3);
@@ -67,10 +66,106 @@ set @equip_template = 20075; set @weapon_1 = 12403; set @weapon_2 = 0; set @crea
 replace into creature_equip_template values (@equip_template, @weapon_1, @weapon_2, 0);
 update creature_template set equipment_id = @equip_template where entry = @creature;
 
-REPLACE INTO creature_spells VALUES
-(180023, 'HATEFORGE QUARRY: Hateforge Cleric', 25314, 100, 15, 0, 0, 0, 0, 0, 18, 21, 0, 15654, 100, 1, 0, 0, 8, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(180027, 'HATEFORGE QUARRY: Hateforge Taskmaster', 56522, 100, 15, 0, 0, 0, 0, 0, 300, 300, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(180029, 'HATEFORGE QUARRY: Hateforge Fireblade', 56524, 100, 15, 0, 0, 0, 0, 0, 300, 300, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+delete from creature_spells where entry in (180027,180029,180023);
+
+-- Hateforge Miner
+set @creature_entry = 60721;
+set @description = 'HATEFORGE QUARRY: Hateforge Miner';
+set @spell_list_id = 180034;
+ 
+set @spellid_1 = 19644; -- Heroic Strike
+set @probability_1 = 100; 
+set @casttarget_1 = 1; 
+set @castflags_1 = 4;
+set @delayinitialmin_1 = 1; 
+set @delayinitialmax_1 = 1; 
+set @delayrepeatmin_1 = 35; 
+set @delayrepeatmax_1 = 35;
+
+set @spellid_2 = 0;
+set @probability_2 = 0;
+set @casttarget_2 = 0; 
+set @castflags_2 = 0;
+set @delayinitialmin_2 = 0; 
+set @delayinitialmax_2 = 0; 
+set @delayrepeatmin_2 = 0; 
+set @delayrepeatmax_2 = 0;
+
+set @spellid_3 = 0;
+set @probability_3 = 0; 
+set @casttarget_3 = 0; 
+set @castflags_3 = 0;
+set @delayinitialmin_3 = 0; 
+set @delayinitialmax_3 = 0; 
+set @delayrepeatmin_3 = 0; 
+set @delayrepeatmax_3 = 0;
+
+set @spellid_4 = 0;
+set @probability_4 = 0; 
+set @casttarget_4 = 0; 
+set @castflags_4 = 0;
+set @delayinitialmin_4 = 0; 
+set @delayinitialmax_4 = 0; 
+set @delayrepeatmin_4 = 0; 
+set @delayrepeatmax_4 = 0;
+
+set @spellid_5 = 0; 
+set @probability_5 = 0; 
+set @casttarget_5 = 0; 
+set @castflags_5 = 0;
+set @delayinitialmin_5 = 0; 
+set @delayinitialmax_5 = 0; 
+set @delayrepeatmin_5 = 0; 
+set @delayrepeatmax_5 = 0;
+
+set @spellid_6 = 0; 
+set @probability_6 = 0; 
+set @casttarget_6 = 0; 
+set @castflags_6 = 0;
+set @delayinitialmin_6 = 0; 
+set @delayinitialmax_6 = 0; 
+set @delayrepeatmin_6 = 0; 
+set @delayrepeatmax_6 = 0;
+
+set @spellid_7 = 0; 
+set @probability_7 = 0; 
+set @casttarget_7 = 0; 
+set @castflags_7 = 0;
+set @delayinitialmin_7 = 0; 
+set @delayinitialmax_7 = 0; 
+set @delayrepeatmin_7 = 0; 
+set @delayrepeatmax_7 = 0;
+
+set @spellid_8 = 0; 
+set @probability_8 = 0; 
+set @casttarget_8 = 0; 
+set @castflags_8 = 0;
+set @delayinitialmin_8 = 0; 
+set @delayinitialmax_8 = 0; 
+set @delayrepeatmin_8 = 0; 
+set @delayrepeatmax_8 = 0;
+
+-- Do not touch this part:
+update creature_template set spell_list_id = @spell_list_id, ai_name = 'EventAI', script_name = '', spell_id1 = 19644, spell_id2 = 0, spell_id3 = 0 
+where entry = @creature_entry;
+replace into creature_spells (entry, name, 
+spellid_1, probability_1, casttarget_1, castflags_1, delayinitialmin_1, delayinitialmax_1, delayrepeatmin_1, delayrepeatmax_1, 
+spellid_2, probability_2, casttarget_2, castflags_2, delayinitialmin_2, delayinitialmax_2, delayrepeatmin_2, delayrepeatmax_2, 
+spellid_3, probability_3, casttarget_3, castflags_3, delayinitialmin_3, delayinitialmax_3, delayrepeatmin_3, delayrepeatmax_3, 
+spellid_4, probability_4, casttarget_4, castflags_4, delayinitialmin_4, delayinitialmax_4, delayrepeatmin_4, delayrepeatmax_4, 
+spellid_5, probability_5, casttarget_5, castflags_5, delayinitialmin_5, delayinitialmax_5, delayrepeatmin_5, delayrepeatmax_5, 
+spellid_6, probability_6, casttarget_6, castflags_6, delayinitialmin_6, delayinitialmax_6, delayrepeatmin_6, delayrepeatmax_6, 
+spellid_7, probability_7, casttarget_7, castflags_7, delayinitialmin_7, delayinitialmax_7, delayrepeatmin_7, delayrepeatmax_7, 
+spellid_8, probability_8, casttarget_8, castflags_8, delayinitialmin_8, delayinitialmax_8, delayrepeatmin_8, delayrepeatmax_8) 
+values (@spell_list_id, @description,
+@spellid_1, @probability_1, @casttarget_1, @castflags_1, @delayinitialmin_1, @delayinitialmax_1, @delayrepeatmin_1, @delayrepeatmax_1,
+@spellid_2, @probability_2, @casttarget_2, @castflags_2, @delayinitialmin_2, @delayinitialmax_2, @delayrepeatmin_2, @delayrepeatmax_2,
+@spellid_3, @probability_3, @casttarget_3, @castflags_3, @delayinitialmin_3, @delayinitialmax_3, @delayrepeatmin_3, @delayrepeatmax_3,
+@spellid_4, @probability_4, @casttarget_4, @castflags_4, @delayinitialmin_4, @delayinitialmax_4, @delayrepeatmin_4, @delayrepeatmax_4,
+@spellid_5, @probability_5, @casttarget_5, @castflags_5, @delayinitialmin_5, @delayinitialmax_5, @delayrepeatmin_5, @delayrepeatmax_5,
+@spellid_6, @probability_6, @casttarget_6, @castflags_6, @delayinitialmin_6, @delayinitialmax_6, @delayrepeatmin_6, @delayrepeatmax_6,
+@spellid_7, @probability_7, @casttarget_7, @castflags_7, @delayinitialmin_7, @delayinitialmax_7, @delayrepeatmin_7, @delayrepeatmax_7,
+@spellid_8, @probability_8, @casttarget_8, @castflags_8, @delayinitialmin_8, @delayinitialmax_8, @delayrepeatmin_8, @delayrepeatmax_8);
 
 -- Hateforge Shadowcaller
 set @creature_entry = 60726;
@@ -83,8 +178,8 @@ set @casttarget_1 = 1;
 set @castflags_1 = 4;
 set @delayinitialmin_1 = 1; 
 set @delayinitialmax_1 = 1; 
-set @delayrepeatmin_1 = 40; 
-set @delayrepeatmax_1 = 40;
+set @delayrepeatmin_1 = 20; 
+set @delayrepeatmax_1 = 20;
 
 set @spellid_2 = 0;
 set @probability_2 = 0;
@@ -281,8 +376,8 @@ set @casttarget_1 = 1;
 set @castflags_1 = 4;
 set @delayinitialmin_1 = 5; 
 set @delayinitialmax_1 = 5; 
-set @delayrepeatmin_1 = 19; 
-set @delayrepeatmax_1 = 21;
+set @delayrepeatmin_1 = 10; 
+set @delayrepeatmax_1 = 10;
 
 set @spellid_2 = 6660; -- Shoot
 set @probability_2 = 100;
@@ -479,8 +574,8 @@ set @casttarget_1 = 1;
 set @castflags_1 = 4;
 set @delayinitialmin_1 = 0; 
 set @delayinitialmax_1 = 0; 
-set @delayrepeatmin_1 = 300; 
-set @delayrepeatmax_1 = 300;
+set @delayrepeatmin_1 = 35; 
+set @delayrepeatmax_1 = 35;
 
 set @spellid_2 = 0;
 set @probability_2 = 0;
