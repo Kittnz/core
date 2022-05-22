e159e9f2702225a2116c17ef80b1db78ec3768ed
Torta <slowtorta@gmail.com>
2022-05-22 21:42:52 +0200

    Update hateforge_quarry_bosses.sql

--- sql/database_updates/hateforge_quarry_bosses.sql
+++ sql/database_updates/hateforge_quarry_bosses.sql
@@ -325,3 +325,8 @@ REPLACE INTO creature_display_info_addon VALUES
 (18833, 0, 0, 0, 0),
 (18236, 0, 0, 0, 0),
 (18850, 0, 0, 0, 0);
+
+UPDATE `creature_template` SET `script_name` = 'boss_hargesh_doomcaller' WHERE `entry` = 60737; -- Assign C++ Script
+UPDATE `creature_template` SET `unit_class` = '4' WHERE `entry` = 60737; -- Hargesh should have mana since he is casting shadow spells
+
+UPDATE `creature_template` SET `script_name` = 'mob_hateforge_cleric' WHERE `entry` = 60718; -- Assign C++ Script
