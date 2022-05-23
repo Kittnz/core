17c16f546ed2d6ef6b10d6fd28e8f6f03ed32a9d
Torta <slowtorta@gmail.com>
2022-05-24 00:39:48 +0200

    Added new guild teleports.

--- src/game/Spells/SpellEffects.cpp
+++ src/game/Spells/SpellEffects.cpp
@@ -1921,7 +1921,10 @@ void Spell::EffectDummy(SpellEffectIndex eff_idx)
                         case 351: m_caster->ToPlayer()->TeleportTo(0, -10377.1F, -3374.04F, 21.82F, 1.8F);  break; // Blacktooth Grin
                         case 295: m_caster->ToPlayer()->TeleportTo(0, 843.65F, -5069.647F, 8.589F, 0.53F);  break; // Nephilim
                         case 376: m_caster->ToPlayer()->TeleportTo(0, -10598.617F, -40.95F, 35.9F, 5.89F);  break; // The Lions Pride Legion
-                        case 76:  m_caster->ToPlayer()->TeleportTo(1, -11523.83F, -4660.29F, 3.005F, 2.2F);  break; // Schmetterlingsbrigade
+                        case 76:  m_caster->ToPlayer()->TeleportTo(1, -11523.83F, -4660.29F, 3.005F, 2.2F); break; // Schmetterlingsbrigade
+                        case 466: m_caster->ToPlayer()->TeleportTo(1, -6904.55F, -4238.45F, 11.81F, 0.6F);  break; // Hard Knocks Society
+                        case 444: m_caster->ToPlayer()->TeleportTo(0, -8809.33F, 1093.16F, 95.53F, 3.4F);   break; // Thalassian Alliance
+
                         default: break;
                         }
                     }
