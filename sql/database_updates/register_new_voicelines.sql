0ff67d0bea5f96da0c950776677c39a774089d04
Nolin7777 <nolin.nolin.nolin.nolin@gmail.com>
2022-05-30 20:04:00 +0200

    [HQ] Assign new sound file IDs

--- sql/database_updates/register_new_voicelines.sql
+++ sql/database_updates/register_new_voicelines.sql
@@ -22,3 +22,18 @@ REPLACE INTO `sound_entries` VALUES
 (9219, 'Scescent Grove - KEEPER RANATHOS'),
 (9220, 'Scescent Grove - KEEPER RANATHOS'),
 (9221, 'Scescent Grove - KEEPER RANATHOS');
+
+REPLACE INTO `script_texts` (`entry`, `content_default`, `sound`, `type`, `comment`) VALUES
+-- Boss Bargul Blackhammer
+('-1999950', "We must maintain our production! WORK HARDER!", '9201', '6', "BARGUL_BLACKHAMMER_OOC_0"),
+('-1999951', "Shadowforge shall reward us all for our work here in the Quarry!", '9202', '6', "BARGUL_BLACKHAMMER_OOC_1"),
+('-1999952', "I don't see enough of you busy out there, We don't have all month!", '9203', '6', "BARGUL_BLACKHAMMER_OOC_2"),
+('-1999953', "You think you contend with the High Foreman? Feel the fury of the Dark Iron!", '9200', '0', "BARGUL_BLACKHAMMER_AGGRO"),
+('-1999954', "Curse you.. Damn you... The work, must go on.", '9204', '0', "BARGUL_BLACKHAMMER_DEAD"),
+-- Boss Engineer Figgles
+('-1999955', "You foolish outsiders! You're not supposed to be here interrupting my great work. Broody, get them, destroy them!", '9205', '0', "Engineer_Figgles_AGGRO"),
+('-1999956', "I.. Was going to build an army... I would've never been stopped!", '9206', '0', "Engineer_Figgles_DEAD"),
+-- Boss Hatereaver Annihilator
+('-1999957', "Unidentified intruders, defensive measures, engaged.", '9207', '0', "Hatereaver_Annihilator_AGGRO"),
+('-1999958', "Activate emergency power... Emergency power activation failed... Commence... Shut... Do..wn...", '9208', '0', "Hatereaver_Annihilator_DEAD");
+-- Nolin End
