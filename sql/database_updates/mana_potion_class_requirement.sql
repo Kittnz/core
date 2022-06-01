eb5694b38b55f6c88fef348a7f3083a302b8b24b
Torta <slowtorta@gmail.com>
2022-06-01 15:45:24 +0200

    Stop this nonsense.

--- sql/database_updates/mana_potion_class_requirement.sql
+++ sql/database_updates/mana_potion_class_requirement.sql
@@ -1,2 +1,2 @@
 -- Reverted class restriction for Mana potions.
-UPDATE item_template SET allowable_class = 0 WHERE entry IN (2455, 3385, 3827, 6149, 13443, 18841, 13444);
+UPDATE item_template SET allowable_class = -1 WHERE entry IN (2455, 3385, 3827, 6149, 13443, 18841, 13444);
