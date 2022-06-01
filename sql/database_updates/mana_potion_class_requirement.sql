27d2c1727c8eb4db46e9f9f07b7f29d376db9476
Kruxis <32237664+Kruxis@users.noreply.github.com>
2022-06-01 11:11:43 +0200

    Update mana_potion_class_requirement.sql

--- sql/database_updates/mana_potion_class_requirement.sql
+++ sql/database_updates/mana_potion_class_requirement.sql
@@ -1,2 +1,2 @@
 -- https://github.com/slowtorta/turtlewow-bug-tracker/issues/1433
-UPDATE item_template SET allowable_class = 1490 WHERE entry IN (2455, 3385, 3827, 6149, 13443, 18841, 13444);
+UPDATE item_template SET allowable_class = 1494 WHERE entry IN (2455, 3385, 3827, 6149, 13443, 18841, 13444);
