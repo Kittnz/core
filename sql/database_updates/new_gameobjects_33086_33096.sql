020392094152e326b67e38a969e199b18a5061c8
Torta <slowtorta@gmail.com>
2022-06-25 19:48:02 +0200

    Correct quest reward for Lykourgos.

---
+++ sql/database_updates/new_gameobjects_33086_33096.sql
@@ -0,0 +1,11 @@
+replace into gameobject_template values 
+
+(300510, 14, 33080,"World\wmo\Dungeon\OL_OgreHuts\Ogre_GuardTower.wmo", 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(300511, 14, 33081,"World\wmo\Dungeon\OL_OgreHuts\Ogre_Hut.wmo", 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(300512, 14, 33082,"World\wmo\Dungeon\OL_OgreHuts\Ogre_Hut_Huge.wmo", 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(300513, 14, 33083,"World\wmo\Dungeon\OL_OgreHuts\Ogre_Hut2Story.wmo", 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(300514, 14, 33084,"World\wmo\Dungeon\OL_OgreHuts\Ogre_HutBig.wmo", 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(300515, 14, 33085,"world\wmo\dungeon\test\gnome_shrink\gnome_shrink.wmo", 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(300516, 5, 33086,"World\Expansion03\Doodads\Generic\Alliance_Submarine_Door.mdx", 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(300517, 14, 33087,"NEWSTYLE\VLAD\WMO\FEUDALSHIP\feudalship.wmo", 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(300518, 5, 33088,"NEWSTYLE\VLAD\LAMPSTREET.mdx", 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');
