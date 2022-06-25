9ecc6b5f0caacb16584154bb1610239c47a44cdb
Torta <slowtorta@gmail.com>
2022-06-25 15:55:58 +0200

    Fixed Stormwind Vault meeting stone zone link.

---
+++ sql/database_updates/arena_set_bonuses.sql
@@ -0,0 +1,36 @@
+-- Cloth Set
+update item_template set set_id = 608 where entry = 83428;
+update item_template set set_id = 608 where entry = 83427;
+update item_template set set_id = 608 where entry = 83426;
+update item_template set set_id = 608 where entry = 83425;
+update item_template set set_id = 608 where entry = 83424;
+update item_template set set_id = 608 where entry = 83423;
+update item_template set set_id = 608 where entry = 83421;
+update item_template set set_id = 608 where entry = 83420;
+-- Leather Set
+update item_template set set_id = 609 where entry = 83429;
+update item_template set set_id = 609 where entry = 83430;
+update item_template set set_id = 609 where entry = 83431;
+update item_template set set_id = 609 where entry = 83432;
+update item_template set set_id = 609 where entry = 83433;
+update item_template set set_id = 609 where entry = 83434;
+update item_template set set_id = 609 where entry = 83435;
+update item_template set set_id = 609 where entry = 83436;
+-- Mail Set
+update item_template set set_id = 610 where entry = 60350;
+update item_template set set_id = 610 where entry = 60351;
+update item_template set set_id = 610 where entry = 60352;
+update item_template set set_id = 610 where entry = 60353;
+update item_template set set_id = 610 where entry = 60354;
+update item_template set set_id = 610 where entry = 60355;
+update item_template set set_id = 610 where entry = 60356;
+update item_template set set_id = 610 where entry = 60357;
+-- Plate Set
+update item_template set set_id = 611 where entry = 60358;
+update item_template set set_id = 611 where entry = 60359;
+update item_template set set_id = 611 where entry = 60360;
+update item_template set set_id = 611 where entry = 60361;
+update item_template set set_id = 611 where entry = 60362;
+update item_template set set_id = 611 where entry = 60363;
+update item_template set set_id = 611 where entry = 60364;
+update item_template set set_id = 611 where entry = 60365;
