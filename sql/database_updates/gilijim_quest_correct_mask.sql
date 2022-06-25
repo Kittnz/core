d0d8afbd7642676f3bee40f4ad6b0905bd46f851
Torta <slowtorta@gmail.com>
2022-06-25 13:55:37 +0200

    Fixed race mask for `The Rotten Renown`.

---
+++ sql/database_updates/gilijim_quest_correct_mask.sql
@@ -0,0 +1,3 @@
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/1512	
+update quest_template set requiredraces = 589 where entry = 40149;
+update quest_template set requiredraces = 589 where entry = 40153;
