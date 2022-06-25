2fe5c44c7038bad0bd9fe19386c184ea2c72053d
Torta <slowtorta@gmail.com>
2022-06-25 14:20:15 +0200

    Fixed Rejold's New Brew previous quest ID.

--- sql/database_updates/cloak_of_denatharion.sql
+++ sql/database_updates/cloak_of_denatharion.sql
@@ -1,2 +1,2 @@
 -- https://github.com/slowtorta/turtlewow-bug-tracker/issues
-update item_templte set subclass = 1 where entry = 60181;
+update item_template set subclass = 1 where entry = 60181;
---
+++ sql/database_updates/rejolds_new_brew.sql
@@ -0,0 +1,2 @@
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/1490
+update quest_template set prevquestid = 315 where entry = 415;
