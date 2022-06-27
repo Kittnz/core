646b934be60419443e7786be2a953d406d0d1c24
Torta <slowtorta@gmail.com>
2022-06-27 18:22:12 +0200

    Misc. DB fixes.

--- sql/patch_general.sql
+++ sql/patch_general.sql
@@ -952,3 +952,57 @@ replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VA
 replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (10333, 10332, 0, 8, 10332); -- Holy Strike Rank 8
 
 replace into creature_template (entry, display_id1, display_id2, display_id3, display_id4, name, subname, gossip_menu_id, level_min, level_max, health_min, health_max, mana_min, mana_max, armor, faction, npc_flags, speed_walk, speed_run, scale, detection_range, call_for_help_range, leash_range, rank, xp_multiplier, dmg_min, dmg_max, dmg_school, attack_power, dmg_multiplier, base_attack_time, ranged_attack_time, unit_class, unit_flags, dynamic_flags, beast_family, trainer_type, trainer_spell, trainer_class, trainer_race, ranged_dmg_min, ranged_dmg_max, ranged_attack_power, type, type_flags, loot_id, pickpocket_loot_id, skinning_loot_id, holy_res, fire_res, nature_res, frost_res, shadow_res, arcane_res, spell_id1, spell_id2, spell_id3, spell_id4, spell_list_id, pet_spell_list_id, gold_min, gold_max, ai_name, movement_type, inhabit_type, civilian, racial_leader, regeneration, equipment_id, trainer_id, vendor_id, mechanic_immune_mask, school_immune_mask, flags_extra, phase_quest_id, script_name) VALUES (8620, 18788, 0, 0, 0, 'Incubus', '', 0, 20, 20, 919, 919, 436, 436, 0, 14, 0, 1, 1.14286, 0, 18, 5, 0, 0, 1, 24, 31, 0, 90, 1, 2000, 2000, 2, 0, 0, 17, 0, 0, 0, 0, 31.856, 43.802, 100, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, '');
+
+-- Apply Custom Effects to Crafted Items
+update item_template set spelltrigger_3 = 1, spellid_3 = 48009 where entry = 65006; -- Stormscale Leggings
+update item_template set spelltrigger_1 = 1, spellid_1 = 48003 where entry = 65004; -- Ornate Bloodstone Dagger
+update item_template set spelltrigger_1 = 2, spellid_1 = 48004, spellppmRate_1 = 0.85 where entry = 65008; -- Dream's Herald
+update item_template set spelltrigger_1 = 2, spellid_1 = 48008, spellppmRate_1 = 1 where entry = 65005; -- Bloodletter Razor
+update item_template set spelltrigger_2 = 1, spellid_2 = 48007 where entry = 65014; -- Pauldron of Deflectino
+update item_template set spelltrigger_3 = 1, spellid_3 = 48002, spellcooldown_3 = 900000 where entry = 65003; -- Robe of Sacrifice
+
+-- SHAMAN SPELLCHAIN
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (45521, 17364, 17364, 2, 0); -- Bloodlust Rank 2
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (45511, 45509, 45509, 2, 0); -- Stormstrike Rank 2
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (45514, 45505, 45505, 2, 0); -- Feral Spirit Rank 2
+-- PRIEST SPELLCHAIN
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (45553, 15473, 15473, 2, 0); -- Improved Shadowform (Spellchain to require Shadowform talent)
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (45556, 15473, 15473, 3, 0); -- Pain Spike (Spellchain to require Shadowform talent)
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (45554, 15473, 15473, 4, 0); -- Shadow Mend (Spellchain to require Shadowform talent)
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (45564, 45562, 45562, 2, 0); -- Champion's Bond (Spellchain to require Proclaim Champion)
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (45565, 45562, 45562, 3, 0); -- Empower Champion (Spellchain to require Proclaim Champion)
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (45567, 45562, 45562, 4, 0); -- Revive Champion (Spellchain to require Proclaim Champion)
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (45569, 45562, 45562, 5, 0); -- Champion's Resolve (Spellchain to require Proclaim Champion)
+-- WARRIOR SPELLCHAIN
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (45599, 11605, 1464, 6, 0); -- Decisive Strike (Rank 1) (Requires Slam Rank 4)
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (45560, 45599, 1464, 7, 45961); -- Decisive Strike (Rank 2) (Requires both Decisive Strike Rank 1 and Slam Rank 5)
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (45961, 11605, 1464, 5, 0); -- Slam (Rank 5) (Requires Slam Rank 5)
+-- HUNTER SPELLCHAIN
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (3036, 3035, 3035, 2, 0); -- Trueshot Rank 2
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (3037, 3036, 3035, 3, 0); -- Trueshot Rank 3
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (3038, 3037, 3035, 4, 0); -- Trueshot Rank 4
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (3668, 3038, 3035, 5, 0); -- Trueshot Rank 5
+-- DRUID SPELLCHAIN
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (24858, 45734, 45734, 2, 0); -- Owlkin Frenzy (Requires Owlkin Form)
+-- WARLOCK SPELLCHAIN
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (45911, 45910, 45910, 2, 0); -- Mana Funnel Rank 2 (Require Mana Funnel Rank 1)
+-- PALADIN SPELLCHAIN
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (8823, 2537, 2537, 2, 0); -- Crusader Strike Rank 2 
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (8824, 8823, 2537, 3, 0); -- Crusader Strike Rank 3
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (10336, 8824, 2537, 4, 0); -- Crusader Strike Rank 4
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (10337, 10336, 2537, 5, 0); -- Crusader Strike Rank 5
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (678, 679, 679, 2, 0); -- Holy Strike Rank 2
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (1866, 678, 679, 3, 0); -- Holy Strike Rank 3
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (680, 1866, 679, 4, 0); -- Holy Strike Rank 4
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (2495, 680, 679, 5, 0); -- Holy Strike Rank 5
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (5569, 2495, 679, 6, 0); -- Holy Strike Rank 6
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (10332, 5569, 679, 7, 0); -- Holy Strike Rank 7
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (10333, 10332, 679, 8, 0); -- Holy Strike Rank 8
+
+update item_template set spellid_2 = 56525, spellcooldown_2 = 600000, spellid_1 = 13679, spellcooldown = -1 where entry = 60577;
+
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (45555, 0, 0, 0 0);
+
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (45556, 15473, 15473, 3, 0);
+
+replace into spell_chain (spell_id, prev_spell, first_spell, rank, req_spell) VALUES (45553, 15473, 15473, 2, 0);
