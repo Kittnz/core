28da40bba833352fd81e0360fa1fab5b40fb1194
Torta <slowtorta@gmail.com>
2022-07-02 12:34:21 +0200

    Fix Kruxis's migration file name.

--- sql/database_updates/20220702103328_world.sql
+++ sql/database_updates/20220702103328_world.sql
@@ -4,3 +4,5 @@ UPDATE creature_template SET name = 'Warleader Zol\'majin' WHERE entry = 80802;
 UPDATE item_template SET stat_type3 = 7 WHERE entry = 16953;
 
 UPDATE creature_template SET subname = 'Chieftain of the Darkspear Tribe' WHERE entry = 10540;
+
+
