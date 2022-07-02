1dab9e54c7f4ae297b684b0902258e63653f48ff
Kruxis <32237664+Kruxis@users.noreply.github.com>
2022-07-02 12:41:00 +0200

    servertodo fixes

---
+++ sql/database_updates/new_sign_new_nodeposition.sql
@@ -0,0 +1,5 @@
+-- Added new sign (Shang)
+REPLACE INTO gameobject_template VALUES (6000004, 5, 24305, 'THE OLD CHURCH OF WESTFALL IS CLOSED', 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');
+
+-- Changed Node position (dragunovi)
+REPLACE INTO gameobject VALUES (3998576, 165658, 0, -7589.07, -2941.46, 144.305, 6.10282, 0, 0, 0.090061, -0.995936, 2700, 2700, 100, 1, 12, 0);
