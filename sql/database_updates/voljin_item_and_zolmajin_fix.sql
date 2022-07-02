1a68b52388e74385d4a2437984c7c01fea4ac1b3
Kruxis <32237664+Kruxis@users.noreply.github.com>
2022-07-02 09:46:57 +0200

    several Server Todo Fixes

---
+++ sql/database_updates/voljin_item_and_zolmajin_fix.sql
@@ -0,0 +1,6 @@
+-- ServerToDo Cleanup 
+UPDATE creature_template SET name = 'Warleader Zol\'majin' WHERE entry = 80802;
+
+UPDATE item_template SET stat_type3 = 7 WHERE entry = 16953;
+
+UPDATE creature_template SET subname = 'Chieftain of the Darkspear Tribe' WHERE entry = 10540;
