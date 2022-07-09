103cdec7096243f3b9485da313a3e4c562af5b0a
Torta <66763404+slowtorta@users.noreply.github.com>
2022-07-09 16:50:13 +0200

    Updated Schematic: Ice Deflector's restock time.

--- sql/database_updates/20220709112653_world.sql
+++ sql/database_updates/20220709112653_world.sql
@@ -1,3 +1,6 @@
-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/1534
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/1534
 
 update page_text set text = 'To Duke Ramon III,\n\nThanks again for everything you\'ve done. For my own safety, I had to keep my head low since I left Stormwind.\n\nIt appears that SI:7 has been keeping tabs on me, but I assure you that I have kept myself unseen so far and well-hidden within my room.' where entry = 80327;
+
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/1555
+update npc_vendor set incrtime = 7200, maxcount = 1 where item = 13308;
