95c259452f5823f93ff2844a861ad44a8cc3330d
Torta <66763404+slowtorta@users.noreply.github.com>
2022-07-09 19:20:43 +0200

    Vendor list for Thorvald Deepforge.

--- sql/database_updates/20220709112653_world.sql
+++ sql/database_updates/20220709112653_world.sql
@@ -4,3 +4,13 @@ update page_text set text = 'To Duke Ramon III,\n\nThanks again for everything y
 
 -- https://github.com/slowtorta/turtlewow-bug-tracker/issues/1555
 update npc_vendor set incrtime = 7200, maxcount = 1 where item = 13308;
+
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/1561
+replace into npc_vendor values (6030, 853, 0, 0, 0, 0);
+replace into npc_vendor values (6030, 854, 0, 0, 0, 0);
+replace into npc_vendor values (6030, 2207, 0, 0, 0, 0);
+replace into npc_vendor values (6030, 851, 0, 0, 0, 0);
+replace into npc_vendor values (6030, 1197, 0, 0, 0, 0);
+replace into npc_vendor values (6030, 1198, 0, 0, 0, 0);
+replace into npc_vendor values (6030, 852, 0, 0, 0, 0);
+replace into npc_vendor values (6030, 1196, 0, 0, 0, 0);
