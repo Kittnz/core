90dccf19c1e22798352ec68677074a2482137b0a
Kruxis <32237664+Kruxis@users.noreply.github.com>
2022-07-12 15:50:14 +0200

    Added more Loot to 3 Gilijim Isle mobs
    * Added Loot for Ice Crawler
    * Added Loot for Icepoint Whiteclaw
    * Added Loot for Tarqsiku

---
+++ sql/database_updates/20220712154944_world.sql
@@ -0,0 +1,7 @@
+REPLACE INTO creature_loot_template (entry, item, chanceorquestchance, groupid) VALUES
+(60605, 12206, 30, 2), -- Crab
+(60605, 2675, 30, 3), -- Crab
+(60545, 4234, 30, 2), -- Bear
+(60545, 11410, 30, 3), -- Bear
+(60544, 2838, 30, 2), -- Elemental
+(60544, 12361, 15, 2); -- Elemental
