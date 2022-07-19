ee34f6b06392d0475ad3eb8f6116f32ae72ac449
ratkosrb <ratkomladic2@abv.bg>
2022-07-19 19:06:49 +0300

    Add ability to switch gossip menu of npcs.
    https://github.com/vmangos/core/commit/f6d3b0de7a12ec526c4f47f51860007345792628

--- src/game/ScriptMgr.h
+++ src/game/ScriptMgr.h
@@ -339,6 +339,8 @@ enum eScriptCommand
                                                             // datalong = db_guid
     SCRIPT_COMMAND_QUEST_CREDIT             = 83,           // source = Player (from provided source or target)
                                                             // target = WorldObject (from provided source or target)
+    SCRIPT_COMMAND_SET_GOSSIP_MENU          = 84,           // source = Creature
+                                                            // datalong = gossip_menu_id
     SCRIPT_COMMAND_MAX,
 
     SCRIPT_COMMAND_DISABLED                 = 9999          // Script action was disabled during loading.
@@ -1016,6 +1018,13 @@ struct ScriptInfo
         {
             uint32 goGuid;                                  // datalong
         } loadGo;
+
+                                                            // SCRIPT_COMMAND_QUEST_CREDIT (83)
+
+        struct                                              // SCRIPT_COMMAND_SET_GOSSIP_MENU (84)
+        {
+            uint32 gossipMenuId;                            // datalong
+        } setGossipMenu;
 
         struct
         {
