2e84a566e57611ee0361a596eceeba932ffecc11
Torta <66763404+slowtorta@users.noreply.github.com>
2022-07-20 11:18:41 +0200

    Updated spell & talent data.

---
+++ sql/database_updates/20220720090900_world.sql
@@ -0,0 +1,9 @@
+-- Thalassian language for High Elves:
+replace into playercreateinfo_spell values (10, 8, 813, 'Thalassian Language');
+replace into playercreateinfo_spell values (10, 5, 813, 'Thalassian Language');
+replace into playercreateinfo_spell values (10, 4, 813, 'Thalassian Language');
+replace into playercreateinfo_spell values (10, 3, 813, 'Thalassian Language');
+replace into playercreateinfo_spell values (10, 2, 813, 'Thalassian Language');
+replace into playercreateinfo_spell values (10, 1, 813, 'Thalassian Language');
+
+
