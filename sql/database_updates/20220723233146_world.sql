f42deff8ee1d7b0778444ee0a7237d925355fbea
Torta <66763404+slowtorta@users.noreply.github.com>
2022-07-24 01:50:07 +0200

    Added Grimoire of Avoidance to the Demon Trainers.

--- sql/database_updates/20220723233146_world.sql
+++ sql/database_updates/20220723233146_world.sql
@@ -7,3 +7,18 @@ replace into playercreateinfo_spell values (8, 7, 199, 'Two-Handed Maces');
 replace into playercreateinfo_spell values (2, 7, 197, 'Two-Handed Axes');
 replace into playercreateinfo_spell values (6, 7, 197, 'Two-Handed Axes');
 replace into playercreateinfo_spell values (8, 7, 197, 'Two-Handed Axes');
+
+-- Grimoire of Avoidance needs to be added to all Demon Trainer book vendors.
+
+replace into npc_vendor values (5520,  83584, 0, 0, 0, 0);
+replace into npc_vendor values (5749,  83584, 0, 0, 0, 0);
+replace into npc_vendor values (5750,  83584, 0, 0, 0, 0);
+replace into npc_vendor values (5753,  83584, 0, 0, 0, 0);
+replace into npc_vendor values (5815,  83584, 0, 0, 0, 0);
+replace into npc_vendor values (6027,  83584, 0, 0, 0, 0);
+replace into npc_vendor values (6328,  83584, 0, 0, 0, 0);
+replace into npc_vendor values (6373,  83584, 0, 0, 0, 0);
+replace into npc_vendor values (6374,  83584, 0, 0, 0, 0);
+replace into npc_vendor values (6376,  83584, 0, 0, 0, 0);
+replace into npc_vendor values (6382,  83584, 0, 0, 0, 0);
+replace into npc_vendor values (12776, 83584, 0, 0, 0, 0);
