e5622d1537b96d70d1d58a1d1c313a21f493f217
Torta <66763404+slowtorta@users.noreply.github.com>
2022-07-24 01:45:31 +0200

    Added Two-Handed Maces & Axes for Shamans by default.

---
+++ sql/database_updates/20220723233146_world.sql
@@ -0,0 +1,9 @@
+-- Shaman Two-Handed Maces and Axes should be learned on character creation, didn’t have time to fully figure it out, they won’t mind getting it earlier than level 5. Make sure existing shamans also get them. 
+
+replace into playercreateinfo_spell values (2, 7, 199, 'Two-Handed Maces');
+replace into playercreateinfo_spell values (6, 7, 199, 'Two-Handed Maces');
+replace into playercreateinfo_spell values (8, 7, 199, 'Two-Handed Maces');
+
+replace into playercreateinfo_spell values (2, 7, 197, 'Two-Handed Axes');
+replace into playercreateinfo_spell values (6, 7, 197, 'Two-Handed Axes');
+replace into playercreateinfo_spell values (8, 7, 197, 'Two-Handed Axes');
