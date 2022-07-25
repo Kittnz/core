810c3a3a7d033301a69bd04a04f9e204bc40d5d2
Torta <66763404+slowtorta@users.noreply.github.com>
2022-07-25 10:57:25 +0200

    Misc. DB fixes.

--- sql/database_updates/20220723233146_world.sql
+++ sql/database_updates/20220723233146_world.sql
@@ -43,3 +43,21 @@ update creature_template set scale = 1.5 where entry = 51621;
 update creature_template set faction = 189 where entry = 7;
 update gameobject_template set faction = 35 where name like '%Mailbox%';
 update item_template set display_id = 4738 where name = 'Fishing Boat';
+
+UPDATE skill_line_ability SET learn_on_get_skill = 0 WHERE id = 36284;
+
+-- Embrace of the Fang Rebalance
+update item_template set stat_value2 = 4 where entry = 10412; -- Belt of the Fang
+update item_template set stat_value2 = 3, required_level = 16, item_level = 21, armor = 49 where entry = 10412; -- Gloves of the Fang
+update item_template set stat_type2 = 7 where entry = 10412; -- Armor of the Fang
+update item_template set set_id = 162 where entry = 81006; -- Cowl of the Fang
+-- Defias Leather Rebalance
+update item_template set stat_value2 = 4, stat_value1 = 3, required_level = 16, item_level = 21, armor = 54 where entry = 10402; -- Blackened Defias Boots
+update item_template set stat_value2 = 3, stat_value1 = 3, required_level = 16, item_level = 21, armor = 49 where entry = 10401; -- Blackened Defias Gloves
+update item_template set stat_value2 = 5, stat_value1 = 4, required_level = 16, item_level = 21, armor = 69 where entry = 10400; -- Blackened Defias Leggings
+update item_template set stat_type1 = 7, stat_value1 = 4, allowable_race = -1, set_id = 161, bonding = 1, name = 'Blackened Defias Mask' where entry = 81007; -- Enchanted Defias Mask
+
+-- Hateforge Quarry meeting stone:
+
+replace into gameobject_template (entry, type, displayId, name, faction, flags, size, data0, data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12, data13, data14, data15, data16, data17, data18, data19, data20, data21, data22, data23, mingold, maxgold, phase_quest_id, script_name) values (1772027, 23, 5495, 'Meeting Stone', 35, 0, 1, 50, 60, 5103, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');
+
