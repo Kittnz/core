dba3073bc377e74316c94ac0d366f11c7357ba4e
Torta <66763404+slowtorta@users.noreply.github.com>
2022-07-25 16:49:34 +0200

    Update 20220723233146_world.sql

--- sql/database_updates/20220723233146_world.sql
+++ sql/database_updates/20220723233146_world.sql
@@ -61,3 +61,6 @@ update item_template set stat_type1 = 7, stat_value1 = 4, allowable_race = -1, s
 
 replace into gameobject_template (entry, type, displayId, name, faction, flags, size, data0, data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12, data13, data14, data15, data16, data17, data18, data19, data20, data21, data22, data23, mingold, maxgold, phase_quest_id, script_name) values (1772027, 23, 5495, 'Meeting Stone', 35, 0, 1, 50, 60, 5103, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');
 
+-- Disable this temporary:
+
+update gameobject_template set script_name = '' where script_name = 'go_scarlet_attack_trigger';
