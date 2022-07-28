82f0a09fa27f5040bef04b43d2c84337aacd0d9e
ratkosrb <ratkomladic2@abv.bg>
2022-07-29 02:38:14 +0300

    Fix paladin mana gain from Shield Specialization.

--- sql/database_updates/world_spell_fixes.sql
+++ sql/database_updates/world_spell_fixes.sql
@@ -20362,3 +20362,5 @@ update spell_template set effectbonuscoefficient1 = 0, effectbonuscoefficient2 =
 UPDATE `spell_template` SET `effectBonusCoefficient1`=0.033 WHERE `name` = "Retribution Aura" && `effectApplyAuraName1`=15;
 -- Change damage class of Holy Strike to Melee so it procs seals and does 2x damage on crit. Add no resistance attribute.
 UPDATE `spell_template` SET `dmgClass`=2, `attributesEx4`= (`attributesEx4` | 1) WHERE `name` = 'Holy Strike' && `effect1`=58
+-- Lasting Defense change effect type to Energize
+UPDATE `spell_template` SET `effect1`=30, `effectApplyAuraName1`=0 WHERE `entry`=45813;
