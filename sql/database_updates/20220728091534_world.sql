7e289a0da105d813924270e4d1efe15cfec9320d
Torta <66763404+slowtorta@users.noreply.github.com>
2022-07-28 17:52:56 +0300

    Fixed Electro-Lantern recipe.

--- sql/database_updates/20220728091534_world.sql
+++ sql/database_updates/20220728091534_world.sql
@@ -2,3 +2,9 @@
 replace into spell_target_position (id, target_map, target_position_x, target_position_y, target_position_z, target_orientation) VALUES (52005, 1, -8170.67, -4758.11, 33.33, 4.8);
 
 update spell_template set effect1 = 5 where entry = 52005;
+
+-- Outline: Repaired Electro-Lantern
+
+update item_template set spellid_1 = 46078, required_skill = 142, required_skill_rank = 90, buy_price = 100000, sell_price = 5000 where entry = 65031;
+update item_template set quality = 1 where entry = 1630;
+
