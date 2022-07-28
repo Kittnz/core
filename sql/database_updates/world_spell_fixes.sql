8c8cb35334cc91442ab6213bcb63ef2e0b4c1253
ratkosrb <ratkomladic2@abv.bg>
2022-07-28 23:48:01 +0300

    Fix more Holy Strike bugs.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/1659
    
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/506

--- sql/database_updates/world_spell_fixes.sql
+++ sql/database_updates/world_spell_fixes.sql
@@ -20360,3 +20360,5 @@ update spell_template set effectbonuscoefficient1 = 0, effectbonuscoefficient2 =
 
 -- Add spell power scaling to Retribution Aura.
 UPDATE `spell_template` SET `effectBonusCoefficient1`=0.033 WHERE `name` = "Retribution Aura" && `effectApplyAuraName1`=15;
+-- Change damage class of Holy Strike to Melee so it procs seals and does 2x damage on crit. Add no resistance attribute.
+UPDATE `spell_template` SET `dmgClass`=2, `attributesEx4`= (`attributesEx4` | 1) WHERE `name` = 'Holy Strike' && `effect1`=58
