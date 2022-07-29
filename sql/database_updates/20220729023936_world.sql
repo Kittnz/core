7798ac0ca5d8001bb3d279fedfae69fa46093a00
ratkosrb <ratkomladic2@abv.bg>
2022-07-29 05:44:04 +0300

    Fix mistake in last commit.

--- sql/database_updates/20220729023936_world.sql
+++ sql/database_updates/20220729023936_world.sql
@@ -1,6 +1,3 @@
 -- Fix Crusader Strike being considered a Judgement and removing other Judgements.
-UPDATE `spell_template` SET `SpellFamilyMask`=34359738368 WHERE `name`='Crusader Strike' && `spellFamilyFlags`=536870912;
+UPDATE `spell_template` SET `spellFamilyFlags`=34359738368 WHERE `name`='Crusader Strike' && `spellFamilyFlags`=536870912;
 UPDATE `spell_affect` SET `SpellFamilyMask` = (`SpellFamilyMask` | 34359738368) WHERE `entry` IN (23300, 28852);
-
-
-
