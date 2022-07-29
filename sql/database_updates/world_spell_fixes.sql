291edaaab0feba8ae0e08ce132e3b7380a81c539
ratkosrb <ratkomladic2@abv.bg>
2022-07-29 05:40:30 +0300

    Fix Crusader Strike removing Judgements.

--- sql/database_updates/world_spell_fixes.sql
+++ sql/database_updates/world_spell_fixes.sql
@@ -20364,3 +20364,5 @@ UPDATE `spell_template` SET `effectBonusCoefficient1`=0.033 WHERE `name` = "Retr
 UPDATE `spell_template` SET `dmgClass`=2, `attributesEx4`= (`attributesEx4` | 1) WHERE `name` = 'Holy Strike' && `effect1`=58
 -- Lasting Defense change effect type to Energize
 UPDATE `spell_template` SET `effect1`=30, `effectApplyAuraName1`=0 WHERE `entry`=45813;
+-- Fix Crusader Strike being considered a Judgement and removing other Judgements.
+UPDATE `spell_template` SET `SpellFamilyMask`=34359738368 WHERE `name`='Crusader Strike' && `spellFamilyFlags`=536870912;
