ab0fe7e1968a1277f70a3eb04e49a7ca716e4ff2
ratkosrb <ratkomladic2@abv.bg>
2022-07-29 05:44:04 +0300

    Fix mistake in last commit.

--- sql/database_updates/world_spell_fixes.sql
+++ sql/database_updates/world_spell_fixes.sql
@@ -20365,4 +20365,4 @@ UPDATE `spell_template` SET `dmgClass`=2, `attributesEx4`= (`attributesEx4` | 1)
 -- Lasting Defense change effect type to Energize
 UPDATE `spell_template` SET `effect1`=30, `effectApplyAuraName1`=0 WHERE `entry`=45813;
 -- Fix Crusader Strike being considered a Judgement and removing other Judgements.
-UPDATE `spell_template` SET `SpellFamilyMask`=34359738368 WHERE `name`='Crusader Strike' && `spellFamilyFlags`=536870912;
+UPDATE `spell_template` SET `spellFamilyFlags`=34359738368 WHERE `name`='Crusader Strike' && `spellFamilyFlags`=536870912;
