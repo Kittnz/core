4c011e9a4035ad3d084b26062f49172d58139b8e
ratkosrb <ratkomladic2@abv.bg>
2022-07-30 18:54:17 +0300

    Missing semicolon.

--- sql/database_updates/20220730151858_world.sql
+++ sql/database_updates/20220730151858_world.sql
@@ -11,7 +11,7 @@ UPDATE `spell_template` SET `effect1` = 5 where `entry` = 52005;
 UPDATE `spell_template` SET `effectBonusCoefficient1`=0.033 WHERE `name` = "Retribution Aura" && `effectApplyAuraName1`=15;
 
 -- Change damage class of Holy Strike to Melee so it procs seals and does 2x damage on crit. Add no resistance attribute.
-UPDATE `spell_template` SET `dmgClass`=2, `attributesEx4`= (`attributesEx4` | 1) WHERE `name` = 'Holy Strike' && `effect1`=58
+UPDATE `spell_template` SET `dmgClass`=2, `attributesEx4`= (`attributesEx4` | 1) WHERE `name` = 'Holy Strike' && `effect1`=58;
 
 -- Lasting Defense change effect type to Energize
 UPDATE `spell_template` SET `effect1`=30, `effectApplyAuraName1`=0 WHERE `entry`=45813;
