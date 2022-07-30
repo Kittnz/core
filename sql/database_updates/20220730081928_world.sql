20c5dee43f44eb936b8a6c49ecb244e8b8d1682a
Torta <66763404+slowtorta@users.noreply.github.com>
2022-07-30 12:14:45 +0300

    Misc. DB fixes.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/1733

---
+++ sql/database_updates/20220730081928_world.sql
@@ -0,0 +1,12 @@
+-- Remove XP from Tomb Bats:
+update creature_template set flags_extra = 64 where entry = 91921;
+-- Doomcaller Staff needs a sell value added to it, make it 6 gold 74 silver, 8 copper:
+update item_template set buy_price = 269920, sell_price = 67480 where entry = 60546;
+-- Remove casttimeindex from Illusions:
+update spell_template set castingtimeindex = 0 where entry = 46003;
+-- Remove cooldown from functional companions:
+update spell_template set recoverytime = 0 where entry in (46460, 46472, 46457);
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/1733
+update item_template set allowable_race = -1 where entry = 5415;
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/1726
+update creature set position_x = -10035.79, position_y = 1980.40, position_z = 0 where guid = 89490;
