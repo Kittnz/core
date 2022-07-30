1e74e6330442004ad19fe7210a72ea4f233869c1
Torta <66763404+slowtorta@users.noreply.github.com>
2022-07-30 16:47:50 +0300

    Fixed Infinite Dragonspawn respawn timers.

--- sql/database_updates/20220730081928_world.sql
+++ sql/database_updates/20220730081928_world.sql
@@ -10,3 +10,5 @@ update spell_template set recoverytime = 0 where entry in (46460, 46472, 46457);
 update item_template set allowable_race = -1 where entry = 5415;
 -- https://github.com/slowtorta/turtlewow-bug-tracker/issues/1726
 update creature set position_x = -10035.79, position_y = 1980.40, position_z = 0 where guid = 89490;
+-- Fix Infinite Dragonspawns in Black Morass randomly respawn in seconds:
+update creature set spawntimesecsmin = 14400, spawntimesecsmax = 14400 where id in (65100, 65123, 65127);
