8ee45d1d5f7c5458f902128c14dc5dac78c2f319
ratkosrb <ratkomladic2@abv.bg>
2022-07-30 18:19:58 +0300

    Move a bunch spell fixes to new migration so they don't get overwritten.

--- sql/database_updates/20220727232102_world.sql
+++
@@ -1,2 +0,0 @@
--- Use mana regen aura type for Brilliance Aura.
-UPDATE `spell_template` SET `effectApplyAuraName1`=85 WHERE `entry`=45408;
--- sql/database_updates/20220728203138_world.sql
+++
@@ -1,2 +0,0 @@
--- Change damage class of Holy Strike to Melee so it procs seals and does 2x damage on crit. Add no resistance attribute.
-UPDATE `spell_template` SET `dmgClass`=2, `attributesEx4`= (`attributesEx4` | 1) WHERE `name` = 'Holy Strike' && `effect1`=58
