68c613c324d02792fe3d71281da76c634977340f
Torta <66763404+slowtorta@users.noreply.github.com>
2022-07-30 16:55:37 +0300

    Removed charge from Tempest's Rage.

--- sql/database_updates/20220730081928_world.sql
+++ sql/database_updates/20220730081928_world.sql
@@ -12,3 +12,5 @@ update item_template set allowable_race = -1 where entry = 5415;
 update creature set position_x = -10035.79, position_y = 1980.40, position_z = 0 where guid = 89490;
 -- Fix Infinite Dragonspawns in Black Morass randomly respawn in seconds:
 update creature set spawntimesecsmin = 14400, spawntimesecsmax = 14400 where id in (65100, 65123, 65127);
+-- Remove charge from Tempest's Rage:
+update item_template set spellcharges_1 = 0 where entry = 83564;
