d9d226c35188cc19beb291116b14a7b1988f21d7
Torta <66763404+slowtorta@users.noreply.github.com>
2022-07-31 17:48:37 +0300

    Fixed Lapidis Isle's graveyard.

--- sql/database_updates/20220730081928_world.sql
+++ sql/database_updates/20220730081928_world.sql
@@ -21,3 +21,11 @@ update item_template set spellid_1 = 9394 where entry = 60690;
 update item_template set script_name = '' where entry = 83002;
 -- Set the graveyard in Crescent Grove to be in Ashenvale:
 replace into custom_graveyards (id, name, map_id, zone_id, area_id, max_level, map_gy_alliance, gy_x_alliance, gy_y_alliance, gy_z_alliance, orientation_alliance, map_gy_horde, gy_x_horde, gy_y_horde, gy_z_horde, orientation_horde) values (32, 'Crescent Grove', 802, 5077, 5077, 60, 1,2633.4, -629.7, 107.58, 1.8, 1, 2421.7, -2953.6, 123.4, 0.05);
+-- Fix Lapidis Isle's graveyard:
+replace into custom_graveyards (id, name, map_id, zone_id, area_id, max_level, map_gy_alliance, gy_x_alliance, gy_y_alliance, gy_z_alliance, orientation_alliance, map_gy_horde, gy_x_horde, gy_y_horde, gy_z_horde, orientation_horde) values 
+(13, 'Lapidis Isle', 0, 409, 5047, 60, 0, -11505.1, 3509.7, 66.1, 2.04, 0, -11505.1, 3509.7, 66.1, 2.04),
+(33, 'Lapidis Isle', 0, 409, 409, 60, 0, -11505.1, 3509.7, 66.1, 2.04, 0, -11505.1, 3509.7, 66.1, 2.04),
+(34, 'Lapidis Isle', 0, 409, 5050, 60, 0, -11505.1, 3509.7, 66.1, 2.04, 0, -11505.1, 3509.7, 66.1, 2.04),
+(35, 'Lapidis Isle', 0, 409, 5051, 60, 0, -11505.1, 3509.7, 66.1, 2.04, 0, -11505.1, 3509.7, 66.1, 2.04),
+(36, 'Lapidis Isle', 0, 409, 5048, 60, 0, -11505.1, 3509.7, 66.1, 2.04, 0, -11505.1, 3509.7, 66.1, 2.04),
+(37, 'Lapidis Isle', 0, 409, 5046, 60, 0, -11505.1, 3509.7, 66.1, 2.04, 0, -11505.1, 3509.7, 66.1, 2.04);
