148b7b732af44186b2b332ae357264754f06202a
Torta <66763404+slowtorta@users.noreply.github.com>
2022-08-05 11:01:39 +0300

    Misc. spell fixes from Moon.

--- sql/database_updates/20220804152404_world.sql
+++ sql/database_updates/20220804152404_world.sql
@@ -13,3 +13,39 @@ replace into creature_display_info_addon (display_id) values (18815);
 replace into spell_template (entry, school, category, castUI, dispel, mechanic, attributes, attributesEx, attributesEx2, attributesEx3, attributesEx4, stances, stancesNot, targets, targetCreatureType, requiresSpellFocus, casterAuraState, targetAuraState, castingTimeIndex, recoveryTime, categoryRecoveryTime, interruptFlags, auraInterruptFlags, channelInterruptFlags, procFlags, procChance, procCharges, maxLevel, baseLevel, spellLevel, durationIndex, powerType, manaCost, manCostPerLevel, manaPerSecond, manaPerSecondPerLevel, rangeIndex, speed, modelNextSpell, stackAmount, totem1, totem2, reagent1, reagent2, reagent3, reagent4, reagent5, reagent6, reagent7, reagent8, reagentCount1, reagentCount2, reagentCount3, reagentCount4, reagentCount5, reagentCount6, reagentCount7, reagentCount8, equippedItemClass, equippedItemSubClassMask, equippedItemInventoryTypeMask, effect1, effect2, effect3, effectDieSides1, effectDieSides2, effectDieSides3, effectBaseDice1, effectBaseDice2, effectBaseDice3, effectDicePerLevel1, effectDicePerLevel2, effectDicePerLevel3, effectRealPointsPerLevel1, effectRealPointsPerLevel2, effectRealPointsPerLevel3, effectBasePoints1, effectBasePoints2, effectBasePoints3, effectBonusCoefficient1, effectBonusCoefficient2, effectBonusCoefficient3, effectMechanic1, effectMechanic2, effectMechanic3, effectImplicitTargetA1, effectImplicitTargetA2, effectImplicitTargetA3, effectImplicitTargetB1, effectImplicitTargetB2, effectImplicitTargetB3, effectRadiusIndex1, effectRadiusIndex2, effectRadiusIndex3, effectApplyAuraName1, effectApplyAuraName2, effectApplyAuraName3, effectAmplitude1, effectAmplitude2, effectAmplitude3, effectMultipleValue1, effectMultipleValue2, effectMultipleValue3, effectChainTarget1, effectChainTarget2, effectChainTarget3, effectItemType1, effectItemType2, effectItemType3, effectMiscValue1, effectMiscValue2, effectMiscValue3, effectTriggerSpell1, effectTriggerSpell2, effectTriggerSpell3, effectPointsPerComboPoint1, effectPointsPerComboPoint2, effectPointsPerComboPoint3, spellVisual1, spellVisual2, spellIconId, activeIconId, spellPriority, name, nameFlags, nameSubtext, nameSubtextFlags, description, descriptionFlags, auraDescription, auraDescriptionFlags, manaCostPercentage, startRecoveryCategory, startRecoveryTime, minTargetLevel, maxTargetLevel, spellFamilyName, spellFamilyFlags, maxAffectedTargets, dmgClass, preventionType, stanceBarOrder, dmgMultiplier1, dmgMultiplier2, dmgMultiplier3, minFactionId, minReputation, requiredAuraVision, customFlags) VALUES (46539, 0, 0, 0, 0, 21, 269582608, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 14, 0, 0, 31, 128, 0, 0, 101, 0, 0, 0, 1, 21, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 6, 6, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 99, 0, -1, -1, -1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 78, 32, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 50595, 0, 0, 0, 0, 0, 0, 0, 0, 1703, 0, 115, 0, 0, 'Lovely Pink Talbuk', 16712190, '', 16712190, 'Summons and dismisses a rideable talbuk. This mount changes based on your Riding skill.', 16712190, 'Increases speed based on your Riding skill.', 16712190, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 1, 1, 1, 0, 0, 0, 0);
 
 update skill_line_ability set spell_id = 46539 where id = 36294;
+
+-- Hardcore mount reward:
+
+-- update item_template set name = 'Mysterious Warp Stalker' where entry = 80692;
+-- update creature_template set display_id1 = 18567, name = 'Mysterious Warp Stalker' where entry = 80160;
+-- update spell_template set name = 'Mysterious Warp Stalker', description = 'Summons and dismisses a rideable Warp Stalker. This mount changes depending on your Riding skill.', spellvisual1 = 1705, spelliconid = 214 where entry = 46216;
+
+-- Misc. DB fixes:
+
+update item_template set spelltrigger_1 = 1, spelltrigger_3 = 0 where entry = 65003;
+
+-- Misc spell fixes from Moon:
+
+UPDATE spell_template SET `castingTimeIndex` = 6 WHERE `entry` = 47330;
+UPDATE spell_template SET `castingTimeIndex` = 6 WHERE `entry` = 47331;
+UPDATE spell_template SET `castingTimeIndex` = 6 WHERE `entry` = 47332;
+UPDATE spell_template SET `castingTimeIndex` = 6 WHERE `entry` = 47333;
+UPDATE spell_template SET `castingTimeIndex` = 6 WHERE `entry` = 47334;
+UPDATE spell_template SET `castingTimeIndex` = 6 WHERE `entry` = 47335;
+UPDATE spell_template SET `castingTimeIndex` = 6 WHERE `entry` = 47336;
+UPDATE spell_template SET `effectBasePoints1` = 86 WHERE `entry` = 45599;
+UPDATE spell_template SET `effectBasePoints1` = 104 WHERE `entry` = 45960;
+UPDATE spell_template SET `effect1` = 17 WHERE `entry` = 45599;
+UPDATE spell_template SET `effect1` = 17 WHERE `entry` = 45960;
+UPDATE spell_template SET `spellIconId` = 2264 WHERE `entry` = 49509;
+UPDATE spell_template SET `spellIconId` = 2266 WHERE `entry` = 46216;
+UPDATE spell_template SET `spellIconId` = 2240 WHERE `entry` = 45652;
+UPDATE spell_template SET `spellIconId` = 2275 WHERE `entry` = 47262;
+UPDATE spell_template SET `spellIconId` = 2240 WHERE `entry` = 45664;
+UPDATE spell_template SET `spellIconId` = 2277 WHERE `entry` = 45513;
+UPDATE spell_template SET `spellIconId` = 2157 WHERE `entry` = 47340;
+UPDATE spell_template SET `spellIconId` = 2273 WHERE `entry` = 45514;
+UPDATE spell_template SET `spellIconId` = 2273 WHERE `entry` = 45519;
+UPDATE spell_template SET `spellIconId` = 2273 WHERE `entry` = 45520;
+UPDATE spell_template SET `spellIconId` = 2240 WHERE `entry` = 47337;
+UPDATE spell_template SET `spellIconId` = 2253 WHERE `entry` = 46203;
