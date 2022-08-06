1d095251d6bd102bb41e634f33e0be8967f3a53d
Torta <66763404+slowtorta@users.noreply.github.com>
2022-08-06 12:58:09 +0300

    Fixed female trolls custom character skins.

--- sql/database_updates/20220806093121_world.sql
+++ sql/database_updates/20220806093121_world.sql
@@ -6,3 +6,8 @@ delete from creature_loot_template where entry = 80253;
 update mount_spells set spellid = 46525 where itemid = 81153; -- Purple
 update mount_spells set spellid = 46524 where itemid = 81154; -- Black
 update mount_spells set spellid = 46526 where itemid = 81155; -- Red
+
+-- Fixed female trolls custom character skins:
+update custom_character_skins set skin_female = 13 where token_id = 51011; 
+update custom_character_skins set skin_female = 19 where token_id = 50210; 
+update custom_character_skins set skin_female = 14 where token_id = 50225; 
