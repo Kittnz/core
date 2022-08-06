ce45d968d511ec0665f147d09eef6e04e571294f
Torta <66763404+slowtorta@users.noreply.github.com>
2022-08-06 14:41:15 +0300

    Added rest-bonus to the Karfang Hold.

--- sql/database_updates/20220806093121_world.sql
+++ sql/database_updates/20220806093121_world.sql
@@ -11,3 +11,10 @@ update mount_spells set spellid = 46526 where itemid = 81155; -- Red
 update custom_character_skins set skin_female = 13 where token_id = 51011; 
 update custom_character_skins set skin_female = 19 where token_id = 50210; 
 update custom_character_skins set skin_female = 14 where token_id = 50225; 
+
+-- 2022-08-06 12:58:08 ERROR:Script not found: npc_foreman_tanoth.
+update creature_template set script_name = '' where script_name = 'npc_foreman_tanoth';
+
+-- Resting area for Karfang Hold:
+replace into areatrigger_tavern values (6, 'Burning Steppes - Karfang Hold');
+replace into areatrigger_template values (6, 0, -7471.82, -2893.55, 230.06, 30, 0, 0, 0, 0); 
