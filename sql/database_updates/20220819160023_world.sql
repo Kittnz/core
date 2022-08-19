c1755ad02635ba38a02093fd3cb5884a489a0f47
Kruxis <32237664+Kruxis@users.noreply.github.com>
2022-08-19 16:04:38 +0200

    Made items BoE ~Dragunovi

--- sql/database_updates/20220819160023_world.sql
+++ sql/database_updates/20220819160023_world.sql
@@ -3,3 +3,6 @@ UPDATE item_template SET spellid_1 = 56530 WHERE entry in (83550, 83554);
 UPDATE item_template SET spellid_1 = 56531 WHERE entry in (83551, 83555);
 UPDATE item_template SET spellid_1 = 56532 WHERE entry in (83552, 83556);
 UPDATE item_template SET spellid_1 = 56533 WHERE entry in (83553, 83557);
+
+-- Make all items BoE
+UPDATE item_template SET bonding = 2 WHERE entry in (65024, 65025);
