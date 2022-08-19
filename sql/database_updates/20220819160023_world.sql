05b574408d40a9332b4b0ecc42f2eb397bd7102a
Kruxis <32237664+Kruxis@users.noreply.github.com>
2022-08-19 16:03:55 +0200

    Added Spells to Items ~Dragunovi

---
+++ sql/database_updates/20220819160023_world.sql
@@ -0,0 +1,5 @@
+-- Add converge spells to items
+UPDATE item_template SET spellid_1 = 56530 WHERE entry in (83550, 83554);
+UPDATE item_template SET spellid_1 = 56531 WHERE entry in (83551, 83555);
+UPDATE item_template SET spellid_1 = 56532 WHERE entry in (83552, 83556);
+UPDATE item_template SET spellid_1 = 56533 WHERE entry in (83553, 83557);
