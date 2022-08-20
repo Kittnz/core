204e220d2a38f0b583aaace3a0ec5694ec619d34
Torta <66763404+slowtorta@users.noreply.github.com>
2022-08-20 13:07:10 +0300

    Misc. DB fixes from Dragu and server-todo.

--- sql/database_updates/20220820090057_world.sql
+++ sql/database_updates/20220820090057_world.sql
@@ -1,4 +1,127 @@
--- The mithril node at X=0.518, Y=0.273 in STV is unreachable:
+-- Change disenchant id of grayson's hat to 1:
+update item_template set disenchant_id = 1 where entry = 70070;
 
+-- Undercity bucket hat does not have a mode or in game icon
+update item_template set display_id = 34495 where entry = 84037;
 
+-- golded plated crossbbow animation is broken
+update item_template set display_id = 31239 where entry = 60624;
 
+-- karavan kodo still has the 10 minutes cooldown
+replace into spell_mod (id, recoverytime) values (46483, 0);
+update spell_template set recoverytime = 0 where entry = 46483;
+
+-- Cube:
+delete from item_template where entry = 0;
+
+-- shadowcaller axe can't be repaired
+update item_template set item_level = 54 where entry = 60616;
+
+-- Embrace of the Fang Rebalance
+update item_template set stat_value2 = 3 where entry = 10412; -- Belt of the Fang
+update item_template set stat_value2 = 3, required_level = 16, item_level = 21, armor = 49 where entry = 10412; -- Gloves of the Fang
+update item_template set stat_type2 = 7 where entry = 10412; -- Armor of the Fang
+update item_template set set_id = 162 where entry = 81006; -- Cowl of the Fang
+-- Defias Leather Rebalance
+update item_template set stat_value2 = 4, stat_value1 = 3, required_level = 16, item_level = 21, armor = 54 where entry = 10402; -- Blackened Defias Boots
+update item_template set stat_value2 = 3, stat_value1 = 3, required_level = 16, item_level = 21, armor = 49 where entry = 10401; -- Blackened Defias Gloves
+update item_template set stat_value2 = 5, stat_value1 = 4, required_level = 16, item_level = 21, armor = 69 where entry = 10400; -- Blackened Defias Leggings
+update item_template set stat_type1 = 7, stat_value1 = 4, allowable_race = -1, set_id = 161, bonding = 1, name = 'Blackened Defias Mask' where entry = 81007; -- Enchanted Defias Mask
+
+-- Quest 'The Ramolus Gem' change the first sentence 'Ironforge' to 'Stormwind' in main description to be the following : "My trip to Stormwind has been a failure, and truth be told ... 
+
+update quest_template set details = 'My trip to Stormwind has been a failure, and truth be told, I don\'t really have the courage to return just yet.\n\nI was ambushed by a group of troggs heading to Ironforge, and I just so happened to drop the most important item on me, The Ramolus Gem.\n\nThe Trogg that happened to take it was enfused with the latent arcane power of the gem, and now has some ability with magic... Or so I heard.\n\n<Ansela pouts.>\n\nCan you recover it for me by chance? The Trogg named Grash can be found deep in the Gol\'Bolar Quarry.' where entry = 40518;
+
+/* 
+
+Change subname of Soalara Dawnstar to <Silvermoon Remnant Quartermaster>
+Change subname of Alinerenah Sunsparrow to <Alliance Cloth Quartermaster>
+
+Change subname of Tan'Pogo to <Revantusk Trolls Quartermaster>
+Change subname of Reolis Riptusk to <Horde Cloth Quartermaster>
+
+Remove Vendor Flags from Reolis Riptusk, Alinerenah Sunsparrow.
+
+*/
+
+update creature_template set subname = 'Silvermoon Remnant Quartermaster' where entry = 80266;
+update creature_template set subname = 'Alliance Cloth Quartermaster' where entry = 80459;
+
+update creature_template set subname = 'Revantusk Trolls Quartermaster' where entry = 80915;
+update creature_template set subname = 'Horde Cloth Quartermaster' where entry = 80807;
+
+update creature_template set npc_flags = 3 where entry in (80459, 80807);
+
+/*
+REMOVE ALL ITEMS from Soalara Dawnstar, Alinerenah, Tan'Pogo, and Reolis Riptusk vendor tables
+
+Add items to Soalara Dawnstar Vendor Table
+
+https://database.turtle-wow.org/?item=80524
+https://database.turtle-wow.org/?item=80523
+https://database.turtle-wow.org/?item=80525
+https://database.turtle-wow.org/?item=80522
+https://database.turtle-wow.org/?item=80526
+https://database.turtle-wow.org/?item=80317
+https://database.turtle-wow.org/?item=80537
+https://database.turtle-wow.org/?item=80546
+https://database.turtle-wow.org/?item=80502
+https://database.turtle-wow.org/?item=80504
+https://database.turtle-wow.org/?item=80501
+https://database.turtle-wow.org/?item=80500
+https://database.turtle-wow.org/?item=80503
+https://database.turtle-wow.org/?item=80536
+
+Add items to Tan'Pogo Vendor Table
+
+https://database.turtle-wow.org/?item=80612
+https://database.turtle-wow.org/?item=80616
+https://database.turtle-wow.org/?item=80615
+https://database.turtle-wow.org/?item=80613
+https://database.turtle-wow.org/?item=80614
+https://database.turtle-wow.org/?item=80605
+https://database.turtle-wow.org/?item=80606
+https://database.turtle-wow.org/?item=81098
+https://database.turtle-wow.org/?item=80646
+https://database.turtle-wow.org/?item=80638
+https://database.turtle-wow.org/?item=80602
+https://database.turtle-wow.org/?item=80600
+https://database.turtle-wow.org/?item=80603
+https://database.turtle-wow.org/?item=80604
+https://database.turtle-wow.org/?item=80636
+*/
+
+delete from npc_vendor where entry in (80266, 80915);
+
+replace into npc_vendor (entry, item) values 
+
+(80266, 80524),
+(80266, 80523),
+(80266, 80525),
+(80266, 80522),
+(80266, 80526),
+(80266, 80317),
+(80266, 80537),
+(80266, 80546),
+(80266, 80502),
+(80266, 80504),
+(80266, 80501),
+(80266, 80500),
+(80266, 80503),
+(80266, 80536),
+
+(80915, 80612),
+(80915, 80616),
+(80915, 80615),
+(80915, 80613),
+(80915, 80614),
+(80915, 80605),
+(80915, 80606),
+(80915, 81098),
+(80915, 80646),
+(80915, 80638),
+(80915, 80602),
+(80915, 80600),
+(80915, 80603),
+(80915, 80604),
+(80915, 80636);
--- src/scripts/miscelanneous/custom_teleports.cpp
+++ src/scripts/miscelanneous/custom_teleports.cpp
@@ -38,43 +38,38 @@ struct custom_dungeon_portal : public GameObjectAI
                     available = false;
                     break;
                 }
-
-                if (!player->IsInCombat())
+                if (!player->IsAlive())
                 {
-                    if (!player->IsAlive())
+                    player->ResurrectPlayer(0.5f);
+                    player->SpawnCorpseBones();
+                }
+                array<tuple<uint32, WorldLocation, int32>, 10> portals_and_locations =
+                { {
+                    { 181580, WorldLocation{800, -11068.1F, -1806.4F, 52.7F, 1.5F}, 55 },  // Karazhan Crypt (Entrance)
+                    { 181581, WorldLocation{0, -11068.9F, -1828.6F, 60.26F, 3.1F},  1 },   // Karazhan Crypt (Exit)
+                    { 112911, WorldLocation{802, 579.13F, 90.70F, 276.11F, 3.4F},   32 },  // The Crescent Grove (Entrance)
+                    { 112912, WorldLocation{1, 1722.0F, -1272.6F, 163.26F, 5.8F},   1 },   // The Crescent Grove (Exit)
+                    { 112915, WorldLocation{269, -2002.5F, 6575.3F, -154.9F, 5.7F}, 58 },  // Black Morass (Entrance)
+                    { 112916, WorldLocation{1, -8756.8F, -4191.3F, -209.4F, 5.5F},  1 },   // Black Morass (Exit)
+                    { 112917, WorldLocation{35, -1.15F, 44.4F, -25.58F, 1.6F},      58 },  // Stromwind Vault (Entrance)
+                    { 112918, WorldLocation{0, -8702.01F, 646.16F, 99.97F, 0.04F},  1 },   // Stromwind Vault (Exit)      
+                    { 112940, WorldLocation{808, -8173.9F, -3120.6F, 199.8F, 4.7F}, 48 },  // Hateforge Quarry (Entrance)
+                    { 112941, WorldLocation{0, -8169.2F, -3106.71F, 200.4F, 1.1F},  1 },   // Hateforge Quarry (Exit)   
+                } };
+                for (auto const& teleport : portals_and_locations)
+                {
+                    const auto& [portal_id, destination, min_level] = teleport;
+                    if (me->GetEntry() == portal_id)
                     {
-                        player->ResurrectPlayer(0.5f);
-                        player->SpawnCorpseBones();
-                    }
-                    array<tuple<uint32, WorldLocation, int32>, 10> portals_and_locations =
-                    { {
-                        { 181580, WorldLocation{800, -11068.1F, -1806.4F, 52.7F, 1.5F}, 55 },  // Karazhan Crypt (Entrance)
-                        { 181581, WorldLocation{0, -11068.9F, -1828.6F, 60.26F, 3.1F},  1 },   // Karazhan Crypt (Exit)
-                        { 112911, WorldLocation{802, 579.13F, 90.70F, 276.11F, 3.4F},   32 },  // The Crescent Grove (Entrance)
-                        { 112912, WorldLocation{1, 1722.0F, -1272.6F, 163.26F, 5.8F},   1 },   // The Crescent Grove (Exit)
-                        { 112915, WorldLocation{269, -2002.5F, 6575.3F, -154.9F, 5.7F}, 58 },  // Black Morass (Entrance)
-                        { 112916, WorldLocation{1, -8756.8F, -4191.3F, -209.4F, 5.5F},  1 },   // Black Morass (Exit)
-                        { 112917, WorldLocation{35, -1.15F, 44.4F, -25.58F, 1.6F},      58 },  // Stromwind Vault (Entrance)
-                        { 112918, WorldLocation{0, -8702.01F, 646.16F, 99.97F, 0.04F},  1 },   // Stromwind Vault (Exit)      
-                        { 112940, WorldLocation{808, -8173.9F, -3120.6F, 199.8F, 4.7F}, 48 },  // Hateforge Quarry (Entrance)
-                        { 112941, WorldLocation{0, -8169.2F, -3106.71F, 200.4F, 1.1F},  1 },   // Hateforge Quarry (Exit)   
-                    } };
-                    for (auto const& teleport : portals_and_locations)
-                    {
-                        const auto& [portal_id, destination, min_level ] = teleport;
-                        if (me->GetEntry() == portal_id)
-                        {
-                            
-                            if (player->GetLevel() >= min_level)
-                            {
-                                if (available)
-                                    player->TeleportTo(destination);
-                            }
-                            else 
-                                player->GetSession()->SendNotification("Your level is too low.");
-                        }
-                    }    
 
+                        if (player->GetLevel() >= min_level)
+                        {
+                            if (available)
+                                player->TeleportTo(destination);
+                        }
+                        else
+                            player->GetSession()->SendNotification("Your level is too low.");
+                    }
                 }
             }
             m_uiUpdateTimer = 300;
