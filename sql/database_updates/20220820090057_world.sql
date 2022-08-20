5ead54c517b0f7bb256f22c813689a4c6b000d09
Torta <66763404+slowtorta@users.noreply.github.com>
2022-08-20 14:32:37 +0300

    Misc. DB fixes from tracker and server-todo.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/1971
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/1969

--- sql/database_updates/20220820090057_world.sql
+++ sql/database_updates/20220820090057_world.sql
@@ -125,3 +125,34 @@ replace into npc_vendor (entry, item) values
 (80915, 80603),
 (80915, 80604),
 (80915, 80636);
+
+/*
+Map: 1 (Kalimdor) Zone: 17 (The Barrens) Area: 1697 (Raptor Grounds)
+X: -2050.023193 Y: -3218.093994 Z: 91.681976 Orientation: 2.725144
+grid[25,28]cell[15,2] InstanceID: 0
+ ZoneX: 57.641556 ZoneY: 54.209404
+GroundZ: 91.680283 FloorZ: 91.680283 Have height data (Map: 1 VMap: 1)
+there is a batter chest stuck inside a hut that can't be opened 
+*/
+
+update gameobject set position_x = -2049.6, position_y = -3218.08, position_z = 91.6, orientation = 5.8 where guid = 4007546;
+
+-- Please make a stateless monster item with displayID: 50499 it should be a fist weapon; then assign this weapon to this NPC: https://database.turtle-wow.org/?npc=80867 | make it always unsheath
+
+REPLACE INTO item_template (entry, class, subclass, name, description, display_id, quality, flags, buy_count, buy_price, sell_price, inventory_type, allowable_class, allowable_race, item_level, required_level, required_skill, required_skill_rank, required_spell, required_honor_rank, required_city_rank, required_reputation_faction, required_reputation_rank, max_count, stackable, container_slots, stat_type1, stat_value1, stat_type2, stat_value2, stat_type3, stat_value3, stat_type4, stat_value4, stat_type5, stat_value5, stat_type6, stat_value6, stat_type7, stat_value7, stat_type8, stat_value8, stat_type9, stat_value9, stat_type10, stat_value10, delay, range_mod, ammo_type, dmg_min1, dmg_max1, dmg_type1, dmg_min2, dmg_max2, dmg_type2, dmg_min3, dmg_max3, dmg_type3, dmg_min4, dmg_max4, dmg_type4, dmg_min5, dmg_max5, dmg_type5, block, armor, holy_res, fire_res, nature_res, frost_res, shadow_res, arcane_res, spellid_1, spelltrigger_1, spellcharges_1, spellppmrate_1, spellcooldown_1, spellcategory_1, spellcategorycooldown_1, spellid_2, spelltrigger_2, spellcharges_2, spellppmrate_2, spellcooldown_2, spellcategory_2, spellcategorycooldown_2, spellid_3, spelltrigger_3, spellcharges_3, spellppmrate_3, spellcooldown_3, spellcategory_3, spellcategorycooldown_3, spellid_4, spelltrigger_4, spellcharges_4, spellppmrate_4, spellcooldown_4, spellcategory_4, spellcategorycooldown_4, spellid_5, spelltrigger_5, spellcharges_5, spellppmrate_5, spellcooldown_5, spellcategory_5, spellcategorycooldown_5, bonding, page_text, page_language, page_material, start_quest, lock_id, material, sheath, random_property, set_id, max_durability, area_bound, map_bound, duration, bag_family, disenchant_id, food_type, min_money_loot, max_money_loot, extra_flags, other_team_entry, script_name) VALUES (84017, 2, 5, 'Monster - Fist Weapon, Zul\'jin ', '', 50499, 0, 0, 1, 14, 2, 17, -1, -1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2000, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, 0, 0, 2, 1, 0, 0, 20, 0, 0, 0, 0, 0, 0, 0, 0, 4, 1, NULL);
+
+update creature_template set equipment_id = 80867 where entry = 80867;
+replace into creature_equip_template (entry, equipentry1) values (80867, 84017);
+
+-- Remove https://database.turtle-wow.org/?item=50084 from Turtle WoW Shop, give it Revered requiremenemt with Dalaran and place it on the vendor table of https://database.turtle-wow.org/?npc=80953 Add 2 gold buy price and 50 silver sell price.
+-- Kirin To Familiar, ENABLE LATER:
+-- update item_template set required_reputation_faction = 61, required_reputation_rank = 7, buy_price = 25000, sell_price = 6250 where entry = 50084;
+-- replace into npc_vendor (entry, item) value (80953, 50084);
+
+-- Oil-Stained Gold XP reward and quest item spawn rates. #1971
+update gameobject set spawntimesecsmin = 60, spawntimesecsmax = 80 where id = 1772001;
+update quest_template set rewxp = 630 where entry = 70051; 
+
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/1969
+
+update gameobject set spawntimesecsmin = 7200, spawntimesecsmax = 7200 where guid in (5008116, 5008117);
