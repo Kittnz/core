df1520b3d99406f5bf0c332fd9771f8f4fd6960b
slowtorta <slowtorta@gmail.com>
2022-08-22 03:27:01 +0100

    Live changes.

--- sql/database_updates/20220821211309_world.sql
+++ sql/database_updates/20220821211309_world.sql
@@ -6,3 +6,11 @@ DELETE FROM creature_movement WHERE id=2561215;
 DELETE FROM game_event_creature WHERE guid=2561215;
 DELETE FROM game_event_creature_data WHERE guid=2561215;
 DELETE FROM creature_battleground WHERE guid=2561215;
+
+-- Changes by SHANG
+DELETE FROM creature WHERE guid=73954;
+DELETE FROM creature_addon WHERE guid=73954;
+DELETE FROM creature_movement WHERE id=73954;
+DELETE FROM game_event_creature WHERE guid=73954;
+DELETE FROM game_event_creature_data WHERE guid=73954;
+DELETE FROM creature_battleground WHERE guid=73954;
