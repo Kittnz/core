7f53e1b91697ab3bc79599f2347c4825561903b6
Nolin7777 <nolin.nolin.nolin.nolin@gmail.com>
2022-08-27 01:00:02 +0200

    [BM] Push Boss Scripts

--- src/scripts/dungeons/black_morass/boss_chromie.hpp
+++ src/scripts/dungeons/black_morass/boss_chromie.hpp
@@ -6,3 +6,18 @@
  */
 
 #pragma once
+
+static constexpr std::uint32_t SPELL_PURPLE_PARTICLES_VISUAL{ 28126 };
+static constexpr std::uint32_t SPELL_AOE_SLEEP{ 20669 };
+static constexpr std::uint32_t SPELL_LIGHTNING_CLOUD{ 23105 };
+static constexpr std::uint32_t SPELL_KNOCKBACK{ 10689 };
+static constexpr std::uint32_t SPELL_ARCANE_EXPLOSION{ 30096 };
+static constexpr std::uint32_t SPELL_TWIN_TELEPORT_VISUAL{ 26638 };
+static constexpr std::uint32_t SPELL_GREEN_CHANNELING{ 13540 };
+static constexpr std::uint32_t SPELL_PARTICLES_GREEN{ 18951 };
+
+static constexpr std::uint32_t CREATURE_CHRONORMU{ 50117 };
+
+static constexpr std::uint32_t SOUND_BODY_OF_KATHUNE{ 8674 };
+
+static constexpr std::uint32_t FACTION_FRIENDLY{ 35 };
