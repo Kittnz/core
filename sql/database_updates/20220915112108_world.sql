cbe421c9d46e6a57b74c732b95540dc47d9c9f99
slowtorta <slowtorta@gmail.com>
2022-09-15 18:19:34 +0100

    Direct world update.

--- sql/database_updates/20220915112108_world.sql
+++ sql/database_updates/20220915112108_world.sql
@@ -7,3 +7,6 @@ DELETE FROM creature_movement WHERE id=2572379;
 DELETE FROM game_event_creature WHERE guid=2572379;
 DELETE FROM game_event_creature_data WHERE guid=2572379;
 DELETE FROM creature_battleground WHERE guid=2572379;
+
+-- Changes by COUN
+REPLACE INTO `gameobject` VALUES ( 5010089, 2002232, 1, -1621.52, -4891.53, 16.0884, 2.33416, 0, 0, 0.919607, 0.392839, 300, 300, 100, 1, 0, 0);
