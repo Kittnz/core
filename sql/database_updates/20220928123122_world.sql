98ac689ed536781bad0ac44ad4d1d1e43363d8e0
slowtorta <slowtorta@gmail.com>
2022-09-30 10:27:12 +0100

    Direct world update.

--- sql/database_updates/20220928123122_world.sql
+++ sql/database_updates/20220928123122_world.sql
@@ -1,3 +1,5 @@
 
 -- Changes by COUN
 REPLACE INTO `gameobject` VALUES ( 5010650, 1000373, 1, -7562.36, -2366.75, -179.645, 6.18877, 0, 0, 0.0471924, -0.998886, 300, 300, 100, 1, 1, 200);
+REPLACE INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (855,-7594.107910,-2351.300293,-188.580887,2.125116,1,'hardknocks');
+REPLACE INTO `gameobject` VALUES ( 5010651, 1000373, 1, -54.2158, -3544.66, 27.3126, 2.91694, 0, 0, 0.993698, 0.112089, 300, 300, 100, 1, 1, 200);
