003dd0088e06cac5fc93426e5b10d904941a2137
ratkosrb <ratkomladic2@abv.bg>
2022-10-13 08:40:27 +0300

    Add missing return.

--- src/game/Spells/SpellEffects.cpp
+++ src/game/Spells/SpellEffects.cpp
@@ -543,18 +543,19 @@ void Spell::EffectDummy(SpellEffectIndex eff_idx)
         {
             switch (m_spellInfo->Id)
             {
-            case 18955: // Ranshalla's Torch Trap
-            case 18993: // Ranshalla's Altar Trap
-            {
-                if (unitTarget)
-                    unitTarget->RemoveAurasDueToSpellByCancel(18953);
-                return;
-            }
-            case 18954: // Ranshalla Despawn
-            {
-                if (Creature* pRanshalla = ToCreature(unitTarget))
-                    pRanshalla->ForcedDespawn();
-            }
+                case 18955: // Ranshalla's Torch Trap
+                case 18993: // Ranshalla's Altar Trap
+                {
+                    if (unitTarget)
+                        unitTarget->RemoveAurasDueToSpellByCancel(18953);
+                    return;
+                }
+                case 18954: // Ranshalla Despawn
+                {
+                    if (Creature* pRanshalla = ToCreature(unitTarget))
+                        pRanshalla->ForcedDespawn();
+                    return;
+                }
                 case 23383: // Alliance Flag Click
                 case 23384: // Horde Flag Click
                 {
