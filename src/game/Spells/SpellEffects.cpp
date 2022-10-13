d1e9fc9f021d220a1f6c70f3ee1e3c72f278c4b2
ratkosrb <ratkomladic2@abv.bg>
2022-10-13 07:29:02 +0300

    Follow up to last commit.

--- src/game/Spells/SpellEffects.cpp
+++ src/game/Spells/SpellEffects.cpp
@@ -2406,13 +2406,16 @@ void Spell::EffectDummy(SpellEffectIndex eff_idx)
                             m_casterUnit->CastCustomSpell(m_casterUnit, 45709, &healthModSpellBasePoints0, nullptr, nullptr, true, nullptr);
                         }
                     }
-                }break;
-
+                    break;
+                }
                 case 5229:                                  // Enrage
                 {
+                    if (!unitTarget)
+                        return;
+
                     // Reduce base armor by 27% in Bear Form and 16% in Dire Bear Form
-                    if (Player* pPlayer = ToPlayer(unitTarget))
-                        pPlayer->CastSpell(pPlayer, 25503, true);
+                    int32 reductionMod = unitTarget->HasAura(9634) ? -16 : -27;
+                    unitTarget->CastCustomSpell(unitTarget, 25503, &reductionMod, nullptr, nullptr, true);
                     break;
                 }
                 case 29201: // Loatheb Corrupted Mind triggered sub spells
