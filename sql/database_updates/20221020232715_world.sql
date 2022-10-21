64c1268ba9637076370292b3bf2a6642a3dbc1f4
slowtorta <slowtorta@gmail.com>
2022-10-21 16:50:22 +0100

    Direct world update.

---
+++ sql/database_updates/20221020232715_world.sql
@@ -0,0 +1,3 @@
+
+-- Changes by SHANG
+REPLACE INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (858,-1993.319458,2605.962646,62.311344,1.126615,1,'gelkis');
