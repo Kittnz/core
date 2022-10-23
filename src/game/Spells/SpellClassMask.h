cba8191f120d17fe05ea7c98edefebb922b6f537
ratkosrb <ratkomladic2@abv.bg>
2022-10-23 17:21:59 +0300

    Make Pain Spike benefit from same bonuses as Mind Flay.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2333

--- src/game/Spells/SpellClassMask.h
+++ src/game/Spells/SpellClassMask.h
@@ -155,7 +155,7 @@ enum ClassFlag
     CF_PRIEST_HOLY_FIRE                     = 20,   // CM0 00100000
     CF_PRIEST_STARSHARDS                    = 21,   // CM0 00200000
     CF_PRIEST_HOLY_NOVA1                    = 22,   // CM0 00400000
-    CF_PRIEST_MIND_FLAY                     = 23,   // CM0 00800000
+    CF_PRIEST_MIND_FLAY                     = 23,   // CM0 00800000 Also used for Mind Spike (Custom)
     CF_PRIEST_DESPERATE_PRAYER              = 24,   // CM0 01000000
     CF_PRIEST_DEVOURING_PLAGUE              = 25,   // CM0 02000000
     CF_PRIEST_MIND_VISION                   = 26,   // CM0 04000000
