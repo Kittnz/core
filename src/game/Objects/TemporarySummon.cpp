33821a51916cde69b2f093b372210e82abc77a9a
ratkosrb <ratkomladic2@abv.bg>
2022-10-27 20:37:57 +0300

    Do not count dead creatures for unique count.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2367

--- src/game/Objects/TemporarySummon.cpp
+++ src/game/Objects/TemporarySummon.cpp
@@ -211,6 +211,9 @@ void TemporarySummon::Summon(TempSummonType type, uint32 lifetime, CreatureAiSet
     m_timer = lifetime;
     m_lifetime = lifetime;
 
+    if (lifetime && IsRespawnableTempSummonType(type))
+        m_respawnDelay = lifetime / IN_MILLISECONDS;
+
     if (pFuncAiSetter)
         pFuncAiSetter(this);
     else
