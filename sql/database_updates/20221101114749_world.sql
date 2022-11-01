4af9d5d75eaf6b8697bad3ef3aeff31aaf619086
Dormilon <79369417+Dormilon@users.noreply.github.com>
2022-11-01 15:17:13 +0300

    Quest 4297: Grammar fix.

--- sql/database_updates/20221101114749_world.sql
+++ sql/database_updates/20221101114749_world.sql
@@ -8,3 +8,5 @@ update creature_template set display_id1 = 19100 where entry = 61064;
 REPLACE INTO creature_display_info_addon VALUES (19100, 0, 0, 0, 0);
 -- Change that quote from Orgnil. Instead of "Food can get…" should be: What?? The Burning Blade is Spreading! We must investigate!
 update broadcast_text set male_text = 'What?? The Burning Blade is Spreading! We must investigate!' where entry = 982;
+-- Quest "Food for Baby" grammar fix.
+update quest_template set details = 'Sprite darters are omnivores--that means they eat both meat and vegetation. If your egg here\'s gonna hatch, we best be prepared. Some good eats for these little guys are flanks of meat, raw and juicy, believe it or not. I was never able to find out why, but it has somethin\' to do with Elune\'s plan for the elves... maybe they were to be protectors or somethin\', I don\'t know.\n\nI\'ll take care of the egg for now, why don\'t you get me some good Silvermane stalker flanks for \'em to eat when he\'s hatched.' where entry = 4297;
