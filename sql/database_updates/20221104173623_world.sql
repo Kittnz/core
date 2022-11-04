585372b4368529d3b69887d30e7135526f5528f2
Torta <66763404+slowtorta@users.noreply.github.com>
2022-11-04 18:45:14 +0100

    Update Repair Bot's model to something less loud.

---
+++ sql/database_updates/20221104173623_world.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_template` SET `display_id1` = 18618, `scale` = 0.8 WHERE `entry` = 50041;
+REPLACE INTO `creature_display_info_addon` (`display_id`, `bounding_radius`, `combat_reach`) VALUES (18618, 0.3, 1);
