42c28d41206b5830a38ea82667237c3a905d3909
Torta <66763404+slowtorta@users.noreply.github.com>
2022-11-04 19:13:59 +0100

    Assign correct sound to Great Chieftan's Kodo.

--- sql/database_updates/20221104173623_world.sql
+++ sql/database_updates/20221104173623_world.sql
@@ -1,2 +1,3 @@
 UPDATE `creature_template` SET `display_id1` = 18618, `scale` = 0.8 WHERE `entry` = 50041;
 REPLACE INTO `creature_display_info_addon` (`display_id`, `bounding_radius`, `combat_reach`) VALUES (18618, 0.3, 1);
+UPDATE `spell_template` SET `spellVisual1` = 5160 WHERE `entry` = 18363;
