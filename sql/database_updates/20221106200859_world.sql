1a3ceb3948b1cbe653ab6289e449a768ebdaada9
Torta <66763404+slowtorta@users.noreply.github.com>
2022-11-06 21:11:06 +0100

    Fixed Fifth Mosh'aru Tablet object respawn timer.

---
+++ sql/database_updates/20221106200859_world.sql
@@ -0,0 +1,6 @@
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/2161
+
+UPDATE `gameobject` SET `spawntimesecsmin` = 120, `spawntimesecsmax` = 170 WHERE `guid` = 30277;
+UPDATE `gameobject` SET `spawntimesecsmin` = 120, `spawntimesecsmax` = 170 WHERE `guid` = 350470;
+
+
