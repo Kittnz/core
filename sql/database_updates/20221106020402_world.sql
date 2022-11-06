69e36eed9d911fd7ebc2701fadd9242f31e8a410
Torta <66763404+slowtorta@users.noreply.github.com>
2022-11-06 13:20:37 +0100

    Make Plans: Rune-Etched Breastplate consumed on use.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2403

--- sql/database_updates/20221106020402_world.sql
+++ sql/database_updates/20221106020402_world.sql
@@ -1,2 +1,5 @@
 DELETE FROM `spell_chain` WHERE `spell_id` = 45553;
 
+-- Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2403
+
+UPDATE `item_template` SET `spellcharges_1` = -1 WHERE `entry` = 83503;
