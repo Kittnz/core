e0096b99138b60d4d9af5fc1509cf88f7f8778e3
Torta <66763404+slowtorta@users.noreply.github.com>
2022-11-09 15:16:58 +0100

    Fixed ZoneID for Chen's quests.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2425

--- sql/database_updates/20221109141046_world.sql
+++ sql/database_updates/20221109141046_world.sql
@@ -2,4 +2,7 @@
 
 UPDATE `quest_template` SET `RewRepFaction2` = 0, `RewRepFaction3` = 0, `RewRepValue2` = 0, `RewRepValue3` = 0 WHERE `entry` = 80396;
 
+-- Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2425
 
+UPDATE `quest_template` SET `ZoneOrSort` = 17 WHERE `entry` = 40684;
+UPDATE `quest_template` SET `ZoneOrSort` = 17 WHERE `entry` = 40685;
