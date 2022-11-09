8c6b7e061515a203ac59de4c6917e63dc9d7ce2c
Torta <66763404+slowtorta@users.noreply.github.com>
2022-11-09 15:21:10 +0100

    Fixed Cocroach's weird loot tables.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2423

--- sql/database_updates/20221109141046_world.sql
+++ sql/database_updates/20221109141046_world.sql
@@ -6,3 +6,10 @@ UPDATE `quest_template` SET `RewRepFaction2` = 0, `RewRepFaction3` = 0, `RewRepV
 
 UPDATE `quest_template` SET `ZoneOrSort` = 17 WHERE `entry` = 40684;
 UPDATE `quest_template` SET `ZoneOrSort` = 17 WHERE `entry` = 40685;
+
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/2423
+
+DELETE FROM `creature_loot_template` WHERE `entry` = 7395;
+UPDATE `creature_template` SET `loot_id` = 0 WHERE `entry` = 7395;
+UPDATE `creature_template` SET `skinning_loot_id` = 0 WHERE `entry` = 7395;
+UPDATE `creature_template` SET `type` = 8 WHERE `entry` = 7395;
