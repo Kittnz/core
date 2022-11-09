ad1ccda9e8cb2c679c92197cfe057d575f7743f6
Torta <66763404+slowtorta@users.noreply.github.com>
2022-11-09 15:43:57 +0100

    Added skinning loot to Lar'korwi.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2413

--- sql/database_updates/20221109141046_world.sql
+++ sql/database_updates/20221109141046_world.sql
@@ -27,3 +27,15 @@ REPLACE INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `
 (7848, 18947, 80, 2, 1, 2, 0);
 
 UPDATE `creature_template` SET `skinning_loot_id` = 7848 WHERE `entry` = 7848;
+
+-- Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2413
+
+UPDATE `creature_template` SET `skinning_loot_id` = 9683 WHERE `entry` = 9683;
+
+REPLACE INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES 
+(9683, 4304, 77.2672, 1, 1, 1, 0),
+(9683, 8169, 3.2648,  1, 1, 1, 0),
+(9683, 8170, 19.468,  1, 1, 1, 0),
+(9684, 4304, 12.9032, 2, 1, 1, 0),
+(9684, 8170, 83.2845, 2, 1, 1, 0),
+(9684, 8171, 3.8123,  2, 1, 1, 0);
