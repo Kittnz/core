0592aabf06d34fdafd3d4d24af1a99631e180cc3
-Shiken- <den13501@yahoo.com.tw>
2022-11-14 06:26:03 +0800

    add missing column of characters table

--- sql/worldbot/worldbot_character_update.sql
+++ sql/worldbot/worldbot_character_update.sql
@@ -2,3 +2,8 @@ ALTER TABLE `characters`
 	ADD COLUMN `is_worldbot` TINYINT(11) NOT NULL DEFAULT '0' AFTER `world_phase_mask`;
 
 /*UPDATE `characters` SET `is_worldbot`=1*/
+
+ALTER TABLE `characters`
+	ADD COLUMN `virtual_player_realm` INT(11) NOT NULL DEFAULT '0' AFTER `is_worldbot`;
+	
+/*UPDATE `characters` SET `virtual_player_realm`=721682444*/
