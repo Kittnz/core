b2521d16dd73485c0186e36ffd3acae5a235a5c6
slowtorta <slowtorta@gmail.com>
2022-11-17 23:11:23 +0000

    Direct world update.

--- sql/database_updates/20221117215625_world.sql
+++ sql/database_updates/20221117215625_world.sql
@@ -1,3 +1,4 @@
 
 -- Changes by SHANG
 REPLACE INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (865,-2649.426758,1122.853516,55.723938,0.113815,813,'winterveilvale');
+REPLACE INTO `gameobject` VALUES ( 5011888, 177514, 813, -2645.67, 1123.4, 54.9014, 2.94203, 0, 0, 0.995026, 0.099614, 300, 300, 100, 1, 0, 0);
