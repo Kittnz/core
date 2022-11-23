b8c94c9311202d550cced410a368b4cf44171f0a
slowtorta <slowtorta@gmail.com>
2022-11-23 18:59:52 +0000

    Direct world update.

--- sql/database_updates/20221123111609_world.sql
+++ sql/database_updates/20221123111609_world.sql
@@ -5,3 +5,9 @@ UPDATE creature SET position_x = '-8907.752930', position_y = '-6539.927734', po
 
 -- Changes by TAMMO
 REPLACE INTO `creature` VALUES (2573747,103,0,0,0,813,-2696.97,1143.9,52.1155,5.71896,120,120,0,100,100,0,0,0);
+DELETE FROM creature WHERE guid=2573747;
+DELETE FROM creature_addon WHERE guid=2573747;
+DELETE FROM creature_movement WHERE id=2573747;
+DELETE FROM game_event_creature WHERE guid=2573747;
+DELETE FROM game_event_creature_data WHERE guid=2573747;
+DELETE FROM creature_battleground WHERE guid=2573747;
