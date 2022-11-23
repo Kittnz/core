3c31cadeb86f8d50c51a4539f1698cd028f66a34
slowtorta <slowtorta@gmail.com>
2022-11-23 18:38:53 +0000

    Direct world update.

--- sql/database_updates/20221123111609_world.sql
+++ sql/database_updates/20221123111609_world.sql
@@ -1,3 +1,4 @@
 
 -- Changes by SHANG
 REPLACE INTO `creature` VALUES (2573746,61133,0,0,0,1,-4569.98,-3221.23,34.9228,5.55084,120,120,0,100,100,0,0,0);
+UPDATE creature SET position_x = '-8907.752930', position_y = '-6539.927734', position_z = '12.283902', orientation = '2.231135' WHERE guid = '2573046';
