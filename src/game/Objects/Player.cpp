c6a2b675df1429e0ca4f5407a3422f32fa4873ef
ratkosrb <ratkomladic2@abv.bg>
2022-11-26 09:27:58 +0200

    Redesign Judgement of the Crusader to trigger weapon procs.

--- src/game/Objects/Player.cpp
+++ src/game/Objects/Player.cpp
@@ -7723,7 +7723,7 @@ void Player::UpdateEquipSpellsAtFormChange()
     }
 }
 
-void Player::CastItemCombatSpell(Unit* Target, WeaponAttackType attType)
+void Player::CastItemCombatSpell(Unit* Target, WeaponAttackType attType, float chanceMultiplier)
 {
     Item *item = GetWeaponForAttack(attType, true, true);
     if (!item)
@@ -7768,7 +7768,7 @@ void Player::CastItemCombatSpell(Unit* Target, WeaponAttackType attType)
         else if (chance > 100.0f)
             chance = GetPPMProcChance(WeaponSpeed, 1.0f);   // default to 1 PPM for unknown proc rates
 
-        if (roll_chance_f(chance))
+        if (roll_chance_f(chance * chanceMultiplier))
             CastSpell(Target, spellInfo->Id, true, item);
     }
 
@@ -7803,7 +7803,7 @@ void Player::CastItemCombatSpell(Unit* Target, WeaponAttackType attType)
 
             ApplySpellMod(spellInfo->Id, SPELLMOD_CHANCE_OF_SUCCESS, chance);
 
-            if (roll_chance_f(chance) || HasOption(PLAYER_CHEAT_ALWAYS_PROC))
+            if (roll_chance_f(chance * chanceMultiplier) || HasOption(PLAYER_CHEAT_ALWAYS_PROC))
             {
                 uint32 charges = item->GetEnchantmentCharges(EnchantmentSlot(e_slot));
 
