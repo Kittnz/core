c153c24021a227962e53055577f9fade7110aaf6
slowtorta <slowtorta@gmail.com>
2022-11-27 13:16:44 +0000

    Direct world update.

--- sql/database_updates/20221127113555_world.sql
+++ sql/database_updates/20221127113555_world.sql
@@ -14,3 +14,8 @@ UPDATE creature SET spawntimesecsmin=5 WHERE guid=2573767;
 REPLACE INTO `creature` VALUES (2573769,61096,0,0,0,1,-7623.22,-6715.6,6.44536,4.36918,120,120,0,100,100,0,0,0);
 REPLACE INTO `creature` VALUES (2573770,61096,0,0,0,1,-7583.09,-6773.91,24.7895,2.42924,120,120,0,100,100,0,0,0);
 UPDATE creature SET position_x = '-8898.800781', position_y = '-6498.705078', position_z = '10.656863', orientation = '0.000325' WHERE guid = '2573765';
+UPDATE creature SET position_x = '-8898.745117', position_y = '-6498.552246', position_z = '10.731578', orientation = '0.071796' WHERE guid = '2573765';
+UPDATE creature SET position_x = '-2696.239746', position_y = '1107.256104', position_z = '55.199841', orientation = '1.525953' WHERE guid = '2573543';
+UPDATE creature SET position_x = '-2696.604736', position_y = '1106.720703', position_z = '55.388016', orientation = '1.443485' WHERE guid = '2573543';
+UPDATE creature SET position_x = '-2696.268799', position_y = '1106.670532', position_z = '55.437447', orientation = '1.950069' WHERE guid = '2573543';
+UPDATE creature SET position_x = '-2695.802734', position_y = '1106.812012', position_z = '55.477787', orientation = '2.385504' WHERE guid = '2573543';
