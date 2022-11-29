3e357c953155ebdc72d8fc646b774bef265824d8
slowtorta <slowtorta@gmail.com>
2022-11-29 18:55:16 +0000

    Direct world update.

---
+++ sql/database_updates/20221129184445_world.sql
@@ -0,0 +1,8 @@
+
+-- Changes by WILLBERG
+DELETE FROM creature WHERE guid=2573794;
+DELETE FROM creature_addon WHERE guid=2573794;
+DELETE FROM creature_movement WHERE id=2573794;
+DELETE FROM game_event_creature WHERE guid=2573794;
+DELETE FROM game_event_creature_data WHERE guid=2573794;
+DELETE FROM creature_battleground WHERE guid=2573794;
