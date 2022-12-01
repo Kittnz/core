5c44a6e5d28033cdb5e46ed924e8809dc7b1605c
Torta <66763404+slowtorta@users.noreply.github.com>
2022-12-01 23:12:25 +0300

    Made it snow in the Winter Veil Vale.

--- sql/database_updates/20221201192155_world.sql
+++ sql/database_updates/20221201192155_world.sql
@@ -3,3 +3,6 @@ UPDATE `game_weather` SET `fall_snow_chance` = 100 WHERE `zone` = 5131;
 -- Winter Veil Vale
 REPLACE INTO `game_weather` (`zone`, `copy_weather_from`, `spring_rain_chance`, `spring_snow_chance`, `spring_storm_chance`, `summer_rain_chance`, `summer_snow_chance`, `summer_storm_chance`, `fall_rain_chance`, `fall_snow_chance`, `fall_storm_chance`, `winter_rain_chance`, `winter_snow_chance`, `winter_storm_chance`) VALUES (5130, NULL, 0, 100, 0, 0, 100, 0, 0, 100, 0, 0, 100, 0);
 REPLACE INTO `game_weather` (`zone`, `copy_weather_from`, `spring_rain_chance`, `spring_snow_chance`, `spring_storm_chance`, `summer_rain_chance`, `summer_snow_chance`, `summer_storm_chance`, `fall_rain_chance`, `fall_snow_chance`, `fall_storm_chance`, `winter_rain_chance`, `winter_snow_chance`, `winter_storm_chance`) VALUES (5024, NULL, 0, 100, 0, 0, 100, 0, 0, 100, 0, 0, 100, 0);
+
+-- Figure out why Winter Veil Vale doesn't return zone ID:
+UPDATE `map_template` SET `linked_zone` = 5130 WHERE `entry` = 813;
