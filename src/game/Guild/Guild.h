aa9b337334292ccb3223c976d1c4b8f4abc71375
ratkosrb <ratkomladic2@abv.bg>
2022-12-05 13:38:26 +0200

    Fix possible guild exploit and add notification on decline.
    https://github.com/TrinityCore/TrinityCore/commit/036c72327f50af5f231c5ec8cc69af7db7f4aaa8

--- src/game/Guild/Guild.h
+++ src/game/Guild/Guild.h
@@ -339,7 +339,7 @@ class Guild
         void   LoadGuildEventLogFromDB();
         void   DisplayGuildEventLog(WorldSession *session);
         void   LogGuildEvent(uint8 EventType, ObjectGuid playerGuid1, ObjectGuid playerGuid2 = ObjectGuid(), uint8 newRank = 0);
-
+        ObjectGuid GetGuildInviter(ObjectGuid playerGuid) const;
 
     protected:
         void AddRank(std::string const& name,uint32 rights);
