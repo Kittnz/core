e5224d6231ad03367a7e2fa7ea975072e6ba6537
slowtorta <slowtorta@gmail.com>
2022-12-06 21:23:16 +0000

    Direct world update.

--- sql/database_updates/20221206135450_world.sql
+++ sql/database_updates/20221206135450_world.sql
@@ -646,3 +646,4 @@ DELETE FROM game_event_creature WHERE guid=2573833;
 DELETE FROM game_event_creature_data WHERE guid=2573833;
 DELETE FROM creature_battleground WHERE guid=2573833;
 UPDATE creature SET position_x = '-7568.697266', position_y = '-6819.969727', position_z = '32.632454', orientation = '4.776791' WHERE guid = '2573799';
+REPLACE INTO `creature` VALUES (2573799,61134,0,0,0,1,-7568.7,-6819.97,32.6325,4.77679,120,120,5,100,100,1,0,0);
