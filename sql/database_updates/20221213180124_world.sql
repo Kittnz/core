07f2c360d1e6388e7b816bb4a4592fed8f8f87de
Torta <66763404+slowtorta@users.noreply.github.com>
2022-12-13 21:21:06 +0300

    Gorishi Sting should only be dropped by Gorishi Wasp, Gorishi Stinger, and Gorishi Hive Queen.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2501

--- sql/database_updates/20221213180124_world.sql
+++ sql/database_updates/20221213180124_world.sql
@@ -6,3 +6,11 @@ REPLACE INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ite
 REPLACE INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `itemflags`, `condition_id`) VALUES (60543, 59, 6533, 2, 7200, 0, 0);
 
 UPDATE `creature_template` SET `npc_flags` = 7 WHERE `entry` = 60543;
+
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/2501
+
+DELETE FROM `creature_loot_template` WHERE `item` = 22435;
+
+REPLACE INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (6551, 22435, -15, 0, 1, 1, 0);
+REPLACE INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (6554, 22435, -15, 0, 1, 1, 0);
+REPLACE INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (10041, 22435, -100, 0, 2, 3, 0);
