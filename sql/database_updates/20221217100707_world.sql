20e580d32c72fd311b2ac16d4b784ac2c1e7e715
slowtorta <slowtorta@gmail.com>
2022-12-17 13:40:14 +0000

    Direct world update.

--- sql/database_updates/20221217100707_world.sql
+++ sql/database_updates/20221217100707_world.sql
@@ -1,3 +1,6 @@
 
 -- Changes by TORTA
 UPDATE `creature_template` set `scale` = 1.000000 where entry = 61180;
+
+-- Changes by TAMMO
+REPLACE INTO `creature` VALUES (2573931,61175,0,0,0,813,-2647.04,1117.79,54.9182,1.97757,120,120,0,100,100,0,0,0);
