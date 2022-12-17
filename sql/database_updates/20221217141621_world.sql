493e866a43d869b2d811ddce7db9a57d2be7846e
slowtorta <slowtorta@gmail.com>
2022-12-17 19:11:35 +0000

    Direct world update.

--- sql/database_updates/20221217141621_world.sql
+++ sql/database_updates/20221217141621_world.sql
@@ -5,3 +5,6 @@ UPDATE creature SET position_x = '1797.228271', position_y = '1398.203125', posi
 UPDATE creature SET position_x = '1820.286133', position_y = '1400.409424', position_z = '21.097971', orientation = '5.854315' WHERE guid = '39933';
 UPDATE creature SET position_x = '1797.110596', position_y = '1399.226196', position_z = '23.512384', orientation = '4.717882' WHERE guid = '39858';
 UPDATE creature SET position_x = '1797.376343', position_y = '1398.092285', position_z = '23.513115', orientation = '4.698254' WHERE guid = '39858';
+
+-- Changes by GHEOR
+REPLACE INTO `gameobject` VALUES ( 5012296, 2004698, 451, 16298.6, -16204.3, 43.3935, 0.421174, 0, 0, 0.209034, 0.977908, 300, 300, 100, 1, 0, 0);
