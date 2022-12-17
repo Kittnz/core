e638c979cf05be4ac3a36165ef4d38926f8aabc1
slowtorta <slowtorta@gmail.com>
2022-12-17 14:00:16 +0000

    Direct world update.

--- sql/database_updates/20221217100707_world.sql
+++ sql/database_updates/20221217100707_world.sql
@@ -10,3 +10,11 @@ UPDATE creature SET position_x = '-2683.995361', position_y = '1157.793823', pos
 REPLACE INTO `creature` VALUES (2573934,61183,0,0,0,813,-2684.08,1156,51.1154,1.58721,120,120,0,100,100,0,0,0);
 REPLACE INTO `creature` VALUES (2573935,61176,0,0,0,813,-2642.18,1140.92,54.4651,6.031,120,120,0,100,100,0,0,0);
 UPDATE creature SET position_x = '-2657.460449', position_y = '1115.522827', position_z = '54.950851', orientation = '4.275641' WHERE guid = '2573931';
+REPLACE INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (867,-4099.364746,101.069916,76.281075,5.216433,1,'larisspavillon');
+REPLACE INTO `creature` VALUES (2573936,14355,0,0,0,1,-4098.64,106.229,76.2811,4.9219,120,120,0,100,100,0,0,0);
+DELETE FROM creature WHERE guid=2573936;
+DELETE FROM creature_addon WHERE guid=2573936;
+DELETE FROM creature_movement WHERE id=2573936;
+DELETE FROM game_event_creature WHERE guid=2573936;
+DELETE FROM game_event_creature_data WHERE guid=2573936;
+DELETE FROM creature_battleground WHERE guid=2573936;
