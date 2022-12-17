b2369a83a0e2723f36ba490e49108cbe01c490bd
slowtorta <slowtorta@gmail.com>
2022-12-17 16:28:43 +0000

    Direct world update.

--- sql/database_updates/20221217141621_world.sql
+++ sql/database_updates/20221217141621_world.sql
@@ -1,3 +1,5 @@
 
 -- Changes by JOE
 UPDATE creature SET position_x = '1797.723267', position_y = '1398.446777', position_z = '23.512671', orientation = '4.632236' WHERE guid = '39864';
+UPDATE creature SET position_x = '1797.228271', position_y = '1398.203125', position_z = '23.513243', orientation = '4.707639' WHERE guid = '39929';
+UPDATE creature SET position_x = '1820.286133', position_y = '1400.409424', position_z = '21.097971', orientation = '5.854315' WHERE guid = '39933';
