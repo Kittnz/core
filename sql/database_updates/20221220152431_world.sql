e218bb2cfa655f0366598001f7f6b49a4d4a3b98
slowtorta <slowtorta@gmail.com>
2022-12-20 15:35:01 +0000

    Direct world update.

---
+++ sql/database_updates/20221220152431_world.sql
@@ -0,0 +1,7 @@
+
+-- Changes by TAMMO
+DELETE FROM gameobject WHERE guid = '5012234';
+DELETE FROM game_event_gameobject WHERE guid = '5012234';
+DELETE FROM gameobject_battleground WHERE guid = '5012234';
+REPLACE INTO `gameobject` VALUES ( 5012318, 2000001, 813, -2679.68, 1146.27, 52.99, 2.06552, 0, 0, 0.858715, 0.512453, 300, 300, 100, 1, 0, 0);
+REPLACE INTO `gameobject` VALUES ( 5012318, 2000001, 813, -2679.68, 1146.27, 52.99, 2.06552, 0, 0, 0.858715, 0.512453, 300, 300, 100, 1, 0, 0);
