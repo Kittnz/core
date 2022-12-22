fa3d81907cbc83c6d022b0eebec2a00e94c777af
Torta <66763404+slowtorta@users.noreply.github.com>
2022-12-22 20:52:30 +0300

    Change respawn timer of Oilmaster Higgle Wirefuse to 5 mins.

--- sql/database_updates/20221222174858_world.sql
+++ sql/database_updates/20221222174858_world.sql
@@ -1,2 +1,4 @@
 -- Snowball respawn timer:
 UPDATE `creature` SET `spawntimesecsmin` = 50400, `spawntimesecsmax` = 72000 WHERE `id` = 50112;
+-- Change respawn timer of Oilmaster Higgle Wirefuse to 5 mins:
+UPDATE `creature` SET `spawntimesecsmin` = 300, `spawntimesecsmax` = 300 WHERE `id` = 61036;
