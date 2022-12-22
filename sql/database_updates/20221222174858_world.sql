5bc70322acfa47d4c03473349798222bb3335fe5
Torta <66763404+slowtorta@users.noreply.github.com>
2022-12-22 21:43:12 +0300

    Fixed SIlvermane Pelt drop from SIlvermane wolves.

--- sql/database_updates/20221222174858_world.sql
+++ sql/database_updates/20221222174858_world.sql
@@ -5,4 +5,9 @@ UPDATE `creature` SET `spawntimesecsmin` = 300, `spawntimesecsmax` = 300 WHERE `
 -- NPC Warleader Temukar make unkillable and unaggroable:
 UPDATE `creature_template` SET `unit_flags` = 2, `flags_extra` = 2 WHERE `entry` = 61045;
 -- NPC Vengeful Spirit needs ghost effects:
-UPDATE `turtle_world`.`creature_template` SET `auras` = '9617' WHERE `entry` = 61118;
+UPDATE `creature_template` SET `auras` = '9617' WHERE `entry` = 61118;
+-- Wolfves don't drop the Silvermane Pelt for Protecting Aerie Peak:
+REPLACE INTO `creature_loot_template` VALUES
+(2924, 60898, -72, 4, 1, 1, 0),
+(2925, 60898, -72, 4, 1, 1, 0),
+(2926, 60898, -72, 4, 1, 1, 0);
