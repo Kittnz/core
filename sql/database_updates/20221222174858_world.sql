bf64e71f3a6d105ffca1606656c4addf5abc1823
Torta <66763404+slowtorta@users.noreply.github.com>
2022-12-22 21:05:23 +0300

    NPC Vengeful Spirit needs ghost effects.

--- sql/database_updates/20221222174858_world.sql
+++ sql/database_updates/20221222174858_world.sql
@@ -4,3 +4,5 @@ UPDATE `creature` SET `spawntimesecsmin` = 50400, `spawntimesecsmax` = 72000 WHE
 UPDATE `creature` SET `spawntimesecsmin` = 300, `spawntimesecsmax` = 300 WHERE `id` = 61036;
 -- NPC Warleader Temukar make unkillable and unaggroable:
 UPDATE `creature_template` SET `unit_flags` = 2, `flags_extra` = 2 WHERE `entry` = 61045;
+-- NPC Vengeful Spirit needs ghost effects:
+UPDATE `turtle_world`.`creature_template` SET `auras` = '9617' WHERE `entry` = 61118;
