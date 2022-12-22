d02ab17dd0fea0d4a5efef5ecfc03051cdc7728c
Torta <66763404+slowtorta@users.noreply.github.com>
2022-12-22 20:54:04 +0300

    NPC Warleader Temukar make unkillable and unaggroable.

--- sql/database_updates/20221222174858_world.sql
+++ sql/database_updates/20221222174858_world.sql
@@ -2,3 +2,5 @@
 UPDATE `creature` SET `spawntimesecsmin` = 50400, `spawntimesecsmax` = 72000 WHERE `id` = 50112;
 -- Change respawn timer of Oilmaster Higgle Wirefuse to 5 mins:
 UPDATE `creature` SET `spawntimesecsmin` = 300, `spawntimesecsmax` = 300 WHERE `id` = 61036;
+-- NPC Warleader Temukar make unkillable and unaggroable:
+UPDATE `creature_template` SET `unit_flags` = 2, `flags_extra` = 2 WHERE `entry` = 61045;
