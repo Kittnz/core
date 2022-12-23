57f5dd74c4853944f3c75c670cb48fc1733bc8c8
Torta <66763404+slowtorta@users.noreply.github.com>
2022-12-24 02:08:36 +0300

    Fixed typo in Deckmaster's Commendation flair.

--- sql/database_updates/20221223230521_world.sql
+++ sql/database_updates/20221223230521_world.sql
@@ -1,4 +1,4 @@
 -- Change Taskmaster objective to 0/4 instead of 0/10:
 UPDATE `quest_template` SET `ReqCreatureOrGOCount1` = 4, `ReqCreatureOrGOCount2` = 4, `ReqCreatureOrGOCount3` = 4, `ReqCreatureOrGOCount4` = 4 WHERE `entry` = 70058;
-
-
+-- Typo in Deckmaster's Commendation flair:
+UPDATE `item_template` SET `description` = 'In service to the Kul Tiran navy.' WHERE `entry` = 61100;
