f995aae157e4756ef3c9d193b530d759297fe7fd
slowtorta <slowtorta@gmail.com>
2022-12-25 16:47:47 +0000

    Direct world update.

--- sql/database_updates/20221221204219_world.sql
+++ sql/database_updates/20221221204219_world.sql
@@ -1,3 +1,4 @@
 
 -- Changes by GHEOR
 REPLACE INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (869,15827.593750,-16326.744141,32.969059,4.684318,451,'gheorbuilding');
+REPLACE INTO `gameobject` VALUES ( 5012325, 1000074, 451, 16861.5, 15668.1, 68.2791, 2.8964, 0, 0, 0.992494, 0.122291, 300, 300, 100, 1, 0, 0);
