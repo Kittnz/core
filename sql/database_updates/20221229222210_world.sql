f69e58f8de10cea6ae34c15b1b477ab100ea9e4a
Torta <66763404+slowtorta@users.noreply.github.com>
2022-12-30 02:09:06 +0300

    Text fixes for Undoing the Curse.

--- sql/database_updates/20221229222210_world.sql
+++ sql/database_updates/20221229222210_world.sql
@@ -10,3 +10,5 @@ UPDATE `quest_template` SET `MinLevel` = 32 WHERE `ZoneOrSort` = 5077;
 UPDATE `quest_template` SET `RewRepFaction1` = 108 WHERE `entry` IN (40554, 40555, 40556, 40557, 40558);
 
 UPDATE `item_template` SET `stackable` = 60 WHERE `entry` = 81301;
+
+UPDATE `quest_template` SET `Details` = 'I feel pride. The fire that had settled before has once again been fueled. It\'s time for my last order.\n\n$N, I command you to find Zetharia and end this nightmare. As a last gift to my people, the Lord of the Living Flame has granted me the way to find her.\n\nFar to the south west in these lands and atop the rocks, she lingers, forever searching for new souls to torture. Make sure you succeed, if not she will probably torment your soul for generations, as she did to me and my people.' WHERE `entry` = 40717;
