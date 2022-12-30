d7569980ff32285f3df1e1c373f052b0dcd334ef
slowtorta <slowtorta@gmail.com>
2022-12-30 03:19:51 +0000

    Direct world update.

--- sql/database_updates/20221229223110_world.sql
+++ sql/database_updates/20221229223110_world.sql
@@ -2,3 +2,4 @@
 -- Changes by JOE
 UPDATE creature SET position_x = '-3579.982910', position_y = '-4084.886963', position_z = '-4.601640', orientation = '1.971700' WHERE guid = '18643';
 REPLACE INTO `gameobject` VALUES ( 5012326, 144112, 1, -8872.5, -6537.74, 13.4056, 1.67519, 0, 0, 0.743034, 0.669253, 300, 300, 100, 1, 0, 0);
+UPDATE creature SET position_x = '1284.117310', position_y = '-631.099731', position_z = '42.537022', orientation = '3.401522' WHERE guid = '29856';
