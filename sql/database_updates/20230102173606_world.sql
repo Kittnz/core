34de1a1233f3d5876b764875fa81c706fc1cc697
slowtorta <slowtorta@gmail.com>
2023-01-03 06:19:57 +0000

    Direct world update.

--- sql/database_updates/20230102173606_world.sql
+++ sql/database_updates/20230102173606_world.sql
@@ -1,3 +1,4 @@
 
 -- Changes by JOE
 UPDATE creature SET position_x = '1763.729614', position_y = '1330.931396', position_z = '154.791428', orientation = '0.400429' WHERE guid = '2559966';
+REPLACE INTO `creature` VALUES (2573966,51299,0,0,0,0,-9376.06,-1342.96,54.6909,2.99065,120,120,0,100,100,0,0,0);
