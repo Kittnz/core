fa95465505e64cc6378e315040c5321d21306775
Torta <66763404+slowtorta@users.noreply.github.com>
2023-01-09 21:58:36 +0300

    Various fixes from task list and the bug tracker.
    -- Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2681

---
+++ sql/database_updates/20230109183910_world.sql
@@ -0,0 +1,5 @@
+UPDATE `quest_template` SET `Details` = 'Look, I have a favor I need to ask, and one that is really important to me. Do you know the meaning of sunk cost fallacy? Well, in my world it\'s completely true. I have spent years, and buckets of gold working on a machine that can sort bananas on being worthy to collect or not. It is able to judge ripeness, age, and all sorts of factors to determine if it is worth our time to collect.\n\nLet\'s just say, the buffoons that I sent it out with, got lost, and left it at the camp to the west of here.\n\nI don\'t have a bunch of people to waste getting ripped apart by Venomflayers to go get it themselves, so why don\'t you do it? Recover the Screwfuse 1000 and be sure to bring it back gently, it should be down the road to the west, at a small abandoned camp.' WHERE `entry` = 40755;
+	   
+UPDATE `creature_template` SET `faction` = 69 WHERE `entry` = 61057;
+UPDATE `creature_template` SET `faction` = 69 WHERE `entry` = 61060;
+
