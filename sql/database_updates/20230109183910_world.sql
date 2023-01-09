1fb47188066588f4d3c5cd96231c2cf153612805
Torta <66763404+slowtorta@users.noreply.github.com>
2023-01-09 22:37:11 +0300

    Fixed Bolvar's Decree letter page text.
    -- https://github.com/slowtorta/turtlewow-bug-tracker/issues/2676

--- sql/database_updates/20230109183910_world.sql
+++ sql/database_updates/20230109183910_world.sql
@@ -69,3 +69,7 @@ REPLACE INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `
 -- Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2682
 
 UPDATE `quest_template` SET `RewItemId1` = 0, `RewItemCount1` = 0 WHERE `entry` = 40701;
+
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/2676
+
+UPDATE `page_text` SET `text` = 'Solomon,$B$BThe carrier of this decree has been granted official status as an acting deputy of Stormwind. You may use $g him:her; to find proof of the black dragonflight\'s involvement with the Blackrock orcs. Should such proof be found, this deputy shall return said proof to me in Stormwind, at which time I shall release the order to dispense sufficient military force to aid Lakeshire.$B$BRegards,$B$B$B$BHighlord Bolvar Fordragon\n', `next_page` = 0 WHERE `entry` = 1471;
