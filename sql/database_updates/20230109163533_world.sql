a34320e21c07b1050dc144dd3eda464b414fbef4
ratkosrb <ratkomladic2@abv.bg>
2023-01-09 19:02:01 +0200

    Fix accidental uppercase.

--- sql/database_updates/20230109163533_world.sql
+++ sql/database_updates/20230109163533_world.sql
@@ -1,2 +1,2 @@
 -- Assign spells and loot from Greater Fleshripper to Plateau Fleshripper.
-UPDATE `creature_Template` SET `spell_list_id`=1540, `pet_spell_list_id`=8834, `ai_name`='', `loot_id`=154 WHERE `entry`=61018;
+UPDATE `creature_template` SET `spell_list_id`=1540, `pet_spell_list_id`=8834, `ai_name`='', `loot_id`=154 WHERE `entry`=61018;
