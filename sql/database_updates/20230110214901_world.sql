c4e0e4cf6d9ec0adcd7d9ba612deb1efdea85f2f
Torta <66763404+slowtorta@users.noreply.github.com>
2023-01-11 01:40:12 +0300

    DB stuff from RMJ.

--- sql/database_updates/20230110214901_world.sql
+++ sql/database_updates/20230110214901_world.sql
@@ -8,3 +8,14 @@ WHERE `entry` = 40470;
 UPDATE `quest_template` SET `Details` = REPLACE(`Details`, '\n\n', '$B$B');
 UPDATE `quest_template` SET `RequestItemsText` = REPLACE(`RequestItemsText`, '\n\n', '$B$B');
 UPDATE `quest_template` SET `OfferRewardText` = REPLACE(`OfferRewardText`, '\n\n', '$B$B');
+
+UPDATE `quest_template` SET
+`Details` = 'Whoa, whoa, easy there, champ! I can see you\'re chomping at the bit to tame a wild beast, but there\'s a catch. Us goblins don\'t exactly get along with nature, know what I mean?$B$B<Viz gives grins wryly.>$B$BLet me show you how it\'s done, the goblin way. See this rod? It comes with a special collar that I call the "Friend Request". You put this around any wild animal\'s neck and that beastie becomes your bestie, get me?$B$BProbably oughta start you on something easy. A Dire Mottled Boar should be simple. You\'ll find them wandering around just outside of Razor Hill.$B$BEven a kid like you should be able to do that much. So don\'t waste my time by coming back here without one!',
+`OfferRewardText` = 'So you\'re not completely incompetent. Alright then, let\'s really test your beast-wranglin\' skills!'
+WHERE `entry` = 80331;
+
+UPDATE `quest_template` SET `RewXP` = 85,`RewMoneyMaxLevel` = 60 WHERE `entry` = 80339;
+UPDATE `quest_template` SET `RewXP` = 85,`RewMoneyMaxLevel` = 60 WHERE `entry` = 80366;
+
+UPDATE `quest_template` SET `RewXP` = 840 WHERE `entry` BETWEEN 80331 AND 80333;
+UPDATE `quest_template` SET `RewXP` = 420 WHERE `entry` = 80334;
