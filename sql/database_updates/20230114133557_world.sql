82bb2f633688e0fb3465d86536c704650ed83b11
ratkosrb <ratkomladic2@abv.bg>
2023-01-14 15:43:27 +0200

    Fix comment.

--- sql/database_updates/20230114133557_world.sql
+++ sql/database_updates/20230114133557_world.sql
@@ -11,7 +11,7 @@ INSERT INTO `gossip_scripts` (`id`, `delay`, `priority`, `command`, `datalong`, 
 DELETE FROM `gossip_scripts` WHERE `id`=21214;
 INSERT INTO `gossip_scripts` (`id`, `delay`, `priority`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
 (21214, 0, 0, 4, 147, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Deathstalker Adamant - Remove Gossip Flag'),
-(21214, 0, 1, 4, 46, 768, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Deathstalker Adamant - Add Passive Flag'),
+(21214, 0, 1, 4, 46, 768, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Deathstalker Adamant - Add Immune Flag'),
 (21214, 0, 2, 60, 3, 0, 4000, 0, 0, 0, 0, 0, 1, 3849, 0, 0, 0, 0, 0, 0, 0, 'Deathstalker Adamant - Start Waypoints'),
 (21214, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1320, 0, 0, 0, 0, 0, 0, 0, 0, 'Deathstalker Adamant - Say Text'),
 (21214, 0, 4, 4, 147, 1, 2, 0, 3850, 50, 8, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Sorcerer Ashcrombe - Remove Gossip Flag ');
