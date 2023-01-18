4ca811086d5b15d78abf74de99c3b741b7767f3d
Torta <66763404+slowtorta@users.noreply.github.com>
2023-01-19 02:01:23 +0300

    Adjust reputation pairs for the faction change.

---
+++ sql/database_updates/20230118223632_world.sql
@@ -0,0 +1,30 @@
+TRUNCATE TABLE `player_factionchange_reputations`;
+
+REPLACE INTO `player_factionchange_reputations` (`alliance_id`, `horde_id`) VALUES 
+(47, 530), -- Ironforge <> Darkspear Trolls
+(54, 81), -- Gnomeregan <> Thunder Bluff
+(69, 68), -- Darnassus <> Undercity
+(72, 76), -- Stormwind <> Orgrimmar
+(269, 893), -- Silvermoon Remnant <> Revantusk Trolls
+(509, 510), -- League of Arathor <> The Defilers
+(730, 729), -- Stormpike Guard <> Frostwolf Clan
+(890, 889); -- Silverwing Sentinels <> Warsong Outriders
+
+
+-- 47	Ironforge
+-- 54	Gnomeregan Exiles
+-- 68	Undercity
+-- 69	Darnassus
+-- 72	Stormwind
+-- 76	Orgrimmar
+-- 81	Thunder Bluff
+-- 269	Silvermoon Remnant
+-- 509	The League of Arathor
+-- 510	The Defilers
+-- 530	Darkspear Trolls
+-- 729	Frostwolf Clan
+-- 730	Stormpike Guard
+-- 889	Warsong Outriders
+-- 890	Silverwing Sentinels
+-- 1001	Durotar Labor Union
+-- 893	Revantusk Trolls
