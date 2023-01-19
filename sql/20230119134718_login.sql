422378229aae1e10379f55b345d00d632ab16c03
ratkosrb <ratkomladic2@abv.bg>
2023-01-19 15:48:35 +0200

    Fix comment.

--- sql/20230119134718_login.sql
+++ sql/20230119134718_login.sql
@@ -1,7 +1,7 @@
 -- Add table for banned fingerprints.
 DROP TABLE IF EXISTS `fingerprint_banned`;
 CREATE TABLE IF NOT EXISTS `fingerprint_banned` (
-  `fingerprint` bigint(20) NOT NULL DEFAULT '0' COMMENT 'Account id',
+  `fingerprint` bigint(20) NOT NULL DEFAULT '0' COMMENT 'Fingerprint',
   `bandate` bigint(40) NOT NULL DEFAULT '0',
   `unbandate` bigint(40) NOT NULL DEFAULT '0',
   `bannedby` varchar(50) NOT NULL,
