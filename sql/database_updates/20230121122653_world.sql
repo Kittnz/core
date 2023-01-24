e391291ada27ce1ecda00a7b4917ae21f00aa9fa
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-01-24 17:18:42 +0300

    HP decrease on lvl up #2708.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2708

--- sql/database_updates/20230121122653_world.sql
+++ sql/database_updates/20230121122653_world.sql
@@ -124,3 +124,5 @@ update quest_template set RewOrReqMoney = 325 where entry = 39001;
 update creature_template set display_id1 = 4492 where entry = 80918;
 -- Stonard Grunts mobs are not triggering World Defense channels (Stonard Scouts mobs do though) #2711.
 update creature_template set flags_extra = 524296 where entry = 866;
+-- HP decrease on lvl up #2708.
+update player_classlevelstats set basehp = 52 where class = 5 and level = 3;
