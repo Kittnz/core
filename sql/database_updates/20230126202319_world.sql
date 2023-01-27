0c8560fdf120700806058146ebb83dfac1673add
slowtorta <slowtorta@gmail.com>
2023-01-27 21:45:13 +0000

    Direct world update.

--- sql/database_updates/20230126202319_world.sql
+++ sql/database_updates/20230126202319_world.sql
@@ -3,3 +3,6 @@
 DELETE FROM gameobject WHERE guid = '1270223';
 DELETE FROM game_event_gameobject WHERE guid = '1270223';
 DELETE FROM gameobject_battleground WHERE guid = '1270223';
+
+-- Changes by DRAGUNOVI
+REPLACE INTO `creature` VALUES (2574439,61193,0,0,0,0,-8119.08,-1553.18,133.52,4.6653,120,120,0,100,100,0,0,0);
