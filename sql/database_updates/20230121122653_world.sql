bb08a9df312a9aba7a138197580bc80c911e9f2f
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-01-30 15:26:24 +0300

    Revert "Quest updates: 55035, 40681, 40679, 40680."
    This reverts commit 1034fac478563ac032a65f1b92e9dff397aef460.

--- sql/database_updates/20230121122653_world.sql
+++ sql/database_updates/20230121122653_world.sql
@@ -356,11 +356,3 @@ replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_numbe
 update creature_template set npc_flags = 3, gossip_menu_id = @gossip_menu_id where entry = @magic_number;
 -- Waters of Vision have no sell price #2749.
 update item_template set buy_price = 3500, sell_price = 700 where entry = 51861;
--- Quest Hidden-Hidden Reward change zoneid to wetlands.
-update quest_template set zoneorsort = 11 where entry = 55035;
--- Quest Cursed Sagepaw! change level to 24.
-update quest_template set questlevel = 24 where entry = 40681;
--- Quest Reunite Tribe-Tribe! change level to 23.
-update quest_template set questlevel = 23 = 40679;
--- Quest The One Alpha change level to 27.
-update quest_template set questlevel = 27 = 40680;
