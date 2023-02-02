444a7cff6fedee2b328ea5f01de8bd9ba9934914
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-02-02 17:10:28 +0300

    Greeting edit for 80200: Thank the Sunwell you made it here in one piece, $N. We have much to do.

--- sql/database_updates/20230202133141_world.sql
+++ sql/database_updates/20230202133141_world.sql
@@ -76,3 +76,9 @@ REPLACE INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `itemflags`,
 REPLACE INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `itemflags`, `condition_id`) VALUES (61191, 14341, 0, 0, 0, 0);
 REPLACE INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `itemflags`, `condition_id`) VALUES (61191, 18256, 0, 0, 0, 0);
 REPLACE INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `itemflags`, `condition_id`) VALUES (61191, 50231, 0, 0, 0, 0);
+-- Greeting edit for https://database.turtle-wow.org/?npc=80200: Thank the Sunwell you made it here in one piece, $N. We have much to do.
+set @gossip_menu_id = 41352; set @magic_number = 80200;
+replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); 
+replace into broadcast_text (entry, Male_Text) values (@magic_number, 'Thank the Sunwell you made it here in one piece, $N. We have much to do.');
+replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_number);
+update creature_template set gossip_menu_id = @gossip_menu_id where entry = @magic_number;
