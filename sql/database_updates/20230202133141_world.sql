d64ca3f0c7278a52961210ffd4bbb2412b1e4627
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-02-04 18:06:25 +0300

    Duty to the light quest text error #2774.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2774

--- sql/database_updates/20230202133141_world.sql
+++ sql/database_updates/20230202133141_world.sql
@@ -135,3 +135,5 @@ UPDATE quest_template SET Details = 'Have you ever stood at the edge of Teldrass
 -- Slight edit to strange naming for Mountain Berry assets (RMJ).
 UPDATE item_template SET Name = 'Mountain Berry Bush Seeds' WHERE entry = 51707;
 UPDATE gameobject_template SET Name = 'Mountain Berry Bush' WHERE entry = 1000371;
+-- Duty to the light quest text error #2774.
+update quest_template set OfferRewardText = '<You explain why Brother WIlhelm sent you. Eldrin\'s angry, defensive posture softens as you explain, and eventually you see the shine of tears in his eyes.>$B$BMiles away and as busy serving the Light as he is, yet he still finds time to help us. It pains me to burden him so yet again. He\'s already done so much for us, you see.$B$BThings\'ve been hard this year, so I\'ve been goin\' without so Sara and Eric don\'t have to, and... Well, thank you. Bless Brother Wilhelm, and bless you, $N.$B$BHere. I know it\'s not much, but Sara makes them for the locals. Maybe you can find some use in it. Take it! It\'s the least we can do in return for your selfless deed.' where entry = 60141;
