dc16e09e840651d1096a09c4c20ff77871f81547
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-02-13 17:51:29 +0300

    Rew money added for quests 40608,40694,40695,40696,40700,40701,40702.

--- sql/database_updates/20230202133141_world.sql
+++ sql/database_updates/20230202133141_world.sql
@@ -362,8 +362,12 @@ WHERE entry = 40205;
 -- NPC GUID 2567483, 2567482 set respawn time to 10 hrs.
 update creature set spawntimesecsmin = 36000, spawntimesecsmax = 36000 where guid in (2567483,2567482);
 -- Rew money and rew reputation added for quests 40704 and 40705.
-update quest_template set reworreqmoney = 630, rewrepfaction1 = 470, rewrepvalue1 = 150 where entry in (40704,40705);
+update quest_template set rewmoneymaxlevel = 630, rewrepfaction1 = 470, rewrepvalue1 = 150 where entry in (40704,40705);
 -- Fixes for new Alliance Stonetalon quests (RMJ).
 UPDATE quest_template SET NextQuestInChain = 40695 WHERE entry = 40694;
 UPDATE quest_template SET NextQuestInChain = 40696 WHERE entry = 40695;
 UPDATE quest_template SET NextQuestInChain = 40701 WHERE entry = 40700;
+-- Rew money added for quests 40608,40694,40695,40696,40700,40701,40702.
+update quest_template set rewmoneymaxlevel = 650 where entry in (40608,40694,40695,40696);
+update quest_template set rewmoneymaxlevel = 500 where entry in (40700,40701);
+update quest_template set rewmoneymaxlevel = 780 where entry = 40702;
