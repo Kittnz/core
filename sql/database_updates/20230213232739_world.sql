88ffebf43462b5b414ca086a6c4dda14ac8396f3
slowtorta <slowtorta@gmail.com>
2023-02-14 19:57:02 +0000

    Direct world update.

--- sql/database_updates/20230213232739_world.sql
+++ sql/database_updates/20230213232739_world.sql
@@ -10,3 +10,25 @@ DELETE FROM creature_battleground WHERE guid=2574460;
 
 -- Changes by DRAGUNOVI
 REPLACE INTO `creature_addon` (`guid`, `stand_state`) VALUES (2574072, 1);
+REPLACE INTO `creature_addon` (`guid`, `stand_state`) VALUES (2574072, 0);
+DELETE FROM creature WHERE guid=2574447;
+DELETE FROM creature_addon WHERE guid=2574447;
+DELETE FROM creature_movement WHERE id=2574447;
+DELETE FROM game_event_creature WHERE guid=2574447;
+DELETE FROM game_event_creature_data WHERE guid=2574447;
+DELETE FROM creature_battleground WHERE guid=2574447;
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574214, 233);
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574082, 233);
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574213, 233);
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574078, 1);
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574079, 1);
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574105, 69);
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574100, 1);
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574103, 1);
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574115, 1);
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574114, 1);
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574093, 69);
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574094, 69);
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574095, 1);
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574096, 1);
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574212, 313);
