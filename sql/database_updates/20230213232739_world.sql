a6a82829deb128f4eddcae3ecbcdb61c54b3be5a
slowtorta <slowtorta@gmail.com>
2023-02-14 20:07:03 +0000

    Direct world update.

--- sql/database_updates/20230213232739_world.sql
+++ sql/database_updates/20230213232739_world.sql
@@ -32,3 +32,28 @@ REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574094, 69);
 REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574095, 1);
 REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574096, 1);
 REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574212, 313);
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574122, 69);
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574119, 69);
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574223, 69);
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574223, 234);
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574223, 69);
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574224, 69);
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574233, 1);
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574232, 1);
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574234, 1);
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574235, 1);
+
+-- Changes by SHANG
+REPLACE INTO `creature` VALUES (2574461,11382,0,0,0,0,-7967.04,-1643.59,133.203,4.87214,120,120,5,100,100,1,1,200);
+DELETE FROM creature_movement WHERE id=2574239;
+
+-- Changes by DRAGUNOVI
+UPDATE `creature` SET `movement_type`=0 WHERE `guid`=2574239;
+REPLACE INTO `creature_addon` (`guid`, `emote_state`) VALUES (2574239, 69);
+
+-- Changes by GHEOR
+DELETE FROM `game_tele` WHERE `name` = 'gheoremeralddream';
+REPLACE INTO `gameobject` VALUES ( 5012981, 2004752, 1, -4665.82, -1697.45, 93.1768, 6.02025, 0, 0, 0.131092, -0.99137, 300, 300, 100, 1, 0, 0);
+REPLACE INTO `gameobject` VALUES ( 5012982, 2004751, 1, -4668.85, -1690.65, 94.0517, 0.395221, 0, 0, 0.196327, 0.980539, 300, 300, 100, 1, 0, 0);
+REPLACE INTO `gameobject` VALUES ( 5012982, 2004751, 1, -4668.85, -1690.65, 94.0517, 3.80385, 0, 0, 0.945676, -0.32511, 300, 300, 100, 1, 0, 0);
+REPLACE INTO `gameobject` VALUES ( 5012982, 2004751, 1, -4668.85, -1690.65, 94.0517, 3.4355, 0, 0, 0.989222, -0.146425, 300, 300, 100, 1, 0, 0);
