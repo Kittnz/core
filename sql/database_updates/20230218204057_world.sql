525a5e7300e1ad0eaae73d7707e6115b95816067
slowtorta <slowtorta@gmail.com>
2023-02-19 04:32:44 +0000

    Direct world update.

--- sql/database_updates/20230218204057_world.sql
+++ sql/database_updates/20230218204057_world.sql
@@ -6,3 +6,6 @@ DELETE FROM game_event_gameobject WHERE guid = '5013375';
 DELETE FROM gameobject_battleground WHERE guid = '5013375';
 REPLACE INTO `gameobject` VALUES ( 5013376, 2010997, 0, 2511.85, 2439.43, 1.2622, 5.19352, 0, 0, 0.518274, -0.855215, 300, 300, 100, 1, 0, 0);
 REPLACE INTO `gameobject` VALUES ( 5013376, 2010997, 0, 2511.85, 2439.43, 1.2622, 1.98517, 0, 0, 0.837442, 0.546526, 300, 300, 100, 1, 0, 0);
+
+-- Changes by JOE
+UPDATE `creature` SET `position_x` = -652.160706, `position_y` = 695.482971, `position_z` = -329.113892, `orientation` = 2.859456 WHERE `guid` = 33491;
