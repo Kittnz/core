090ee0f544f5079d7d1e6b7697860a45a217e651
Torta <66763404+slowtorta@users.noreply.github.com>
2023-02-19 23:31:39 +0300

    Shovel things.

--- sql/database_updates/20230219202713_world.sql
+++ sql/database_updates/20230219202713_world.sql
@@ -5,7 +5,8 @@ update item_template set required_skill = 0, required_skill_rank = 0 where entry
 update item_template set required_skill = 0, required_skill_rank = 0 where entry = 14153; -- Robe of the Void
 update item_template set required_skill = 0, required_skill_rank = 0 where entry = 14152; -- Robe of the Archmage
 
+-- Shovel stuff.
 
-
+UPDATE `quest_template` SET `OfferRewardText` = 'What do you have there, $C?$B$BOh, where did you find this?$B$BTThis was the shovel my father had given me back when we worked the farmstead years ago. It served me well, and reminds me of him.$B$BMemories of a better time.$B$B<Sven stares at the shovel and hands it back to you.>$B$BIt was a nice gesture of you to bring me this, $N, but I have no use for it anymore.$B$BDo with it what you will.' WHERE `entry` = 70057;
 
 
