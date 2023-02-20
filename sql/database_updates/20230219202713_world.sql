dd80b0d03d719662bdd18145ebf1ff9d76ca8351
Torta <66763404+slowtorta@users.noreply.github.com>
2023-02-20 12:46:50 +0300

    Restored Uldum Pedestal original GUID.

--- sql/database_updates/20230219202713_world.sql
+++ sql/database_updates/20230219202713_world.sql
@@ -14,3 +14,7 @@ UPDATE `creature_template` SET `npc_flags` = 2 WHERE `entry` = 60821;
 -- Fix stupid HC drake:
 
 UPDATE `creature_template` SET `display_id1` = 18286 WHERE `entry` = 80155;
+
+-- Restored Uldum Pedestal original GUID.
+
+REPLACE INTO `gameobject` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecsmin`, `spawntimesecsmax`, `animprogress`, `state`, `spawn_flags`, `visibility_mod`) VALUES (17230, 142343, 1, -9619.27, -2815.05, 8.21132, -3.13287, 0, 0, 0.99999, -0.004363, 2, 2, 100, 1, 0, 0);
