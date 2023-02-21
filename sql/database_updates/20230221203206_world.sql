75020a7b8f2657b7a3b4e9c332813ba6865f546f
Torta <66763404+slowtorta@users.noreply.github.com>
2023-02-21 23:35:08 +0300

    Fixed Sputtervalve Conductor respawn timer.

--- sql/database_updates/20230221203206_world.sql
+++ sql/database_updates/20230221203206_world.sql
@@ -2,4 +2,5 @@
 -- Gravelsnout Digger should respawn in 5 minutes:
 UPDATE `creature` SET `spawntimesecsmin` = 300, `spawntimesecsmax` = 300 WHERE `id` = 4113;
 
-
+-- Sputtervalve Conductor should respawn faster:
+UPDATE `gameobject` SET `spawntimesecsmin` = 5, `spawntimesecsmax` = 5 WHERE `guid` = 5011101;
