4afc6e19a9b90404b10ed9f05f37908b52f1306f
Torta <66763404+slowtorta@users.noreply.github.com>
2023-02-22 00:03:53 +0300

    More fixes for DoN items.

--- sql/database_updates/20230221203206_world.sql
+++ sql/database_updates/20230221203206_world.sql
@@ -7,3 +7,8 @@ UPDATE `gameobject` SET `spawntimesecsmin` = 5, `spawntimesecsmax` = 5 WHERE `gu
 
 -- Smuggled Gunpowder:
 UPDATE `gameobject` SET `spawntimesecsmin` = 5, `spawntimesecsmax` = 5 WHERE `id` = 2010978;
+
+-- Item Fix Vol 2
+update item_template set stat_value4 = 0 where entry = 20628; -- Deviate Growth Cap
+update item_template set spellid_1 = 13679, spelltrigger_1 = 1, stat_value1 = 12, stat_type4 = 3, stat_type5 = 3, stat_value4 = 12, item_level = 76, armor = 332 where entry = 20629; -- Malignant Footguards
+update item_template set stat_value2 = 38 where entry = 20623; -- Circlet of Restless Dreams;
