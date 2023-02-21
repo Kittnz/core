6c7fd02583cbc51b04fbd21200b1267846d3f122
Torta <66763404+slowtorta@users.noreply.github.com>
2023-02-22 00:15:28 +0300

    Move ship up.

--- sql/database_updates/20230221203206_world.sql
+++ sql/database_updates/20230221203206_world.sql
@@ -12,3 +12,6 @@ UPDATE `gameobject` SET `spawntimesecsmin` = 5, `spawntimesecsmax` = 5 WHERE `id
 update item_template set stat_value4 = 0 where entry = 20628; -- Deviate Growth Cap
 update item_template set spellid_1 = 13679, spelltrigger_1 = 1, stat_value1 = 12, stat_type4 = 3, stat_type5 = 3, stat_value4 = 12, item_level = 76, armor = 332 where entry = 20629; -- Malignant Footguards
 update item_template set stat_value2 = 38 where entry = 20623; -- Circlet of Restless Dreams;
+
+-- Changes by GHEOR
+REPLACE INTO `gameobject` VALUES ( 5005463, 2010882, 1, -3947.74, -4626.38, 5.52041, 0.747484, 0, 0, 0.365102, 0.930968, 300, 300, 100, 1, 0, 0);
