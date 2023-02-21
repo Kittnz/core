0809749465033f5f600b50c010a67a8530fe6adb
Torta <66763404+slowtorta@users.noreply.github.com>
2023-02-21 23:45:03 +0300

    Increase duration of Mark of Nature and Frost to 6 minutes.

--- sql/database_updates/20230217143148_world.sql
+++ sql/database_updates/20230217143148_world.sql
@@ -1,2 +1,2 @@
 -- Reduce duration of Mark of Nature and Frost.
-UPDATE `spell_template` SET `durationIndex`=5 WHERE `entry` IN (25040, 23182);
+UPDATE `spell_template` SET `durationIndex`=41 WHERE `entry` IN (25040, 23182);
