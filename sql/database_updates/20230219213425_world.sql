afbc404c8fb6f668974781390a5e046c8086eb64
slowtorta <slowtorta@gmail.com>
2023-02-21 09:10:22 +0000

    Direct world update.

---
+++ sql/database_updates/20230219213425_world.sql
@@ -0,0 +1,8 @@
+
+-- Changes by JOE
+DELETE FROM creature WHERE guid=2574506;
+DELETE FROM creature_addon WHERE guid=2574506;
+DELETE FROM creature_movement WHERE id=2574506;
+DELETE FROM game_event_creature WHERE guid=2574506;
+DELETE FROM game_event_creature_data WHERE guid=2574506;
+DELETE FROM creature_battleground WHERE guid=2574506;
