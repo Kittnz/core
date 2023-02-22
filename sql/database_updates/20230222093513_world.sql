9f8ea66d33a9d55decb2918dbc387068cc39bf6f
slowtorta <slowtorta@gmail.com>
2023-02-22 13:38:39 +0000

    Direct world update.

---
+++ sql/database_updates/20230222093513_world.sql
@@ -0,0 +1,8 @@
+
+-- Changes by DRAGUNOVI
+DELETE FROM creature WHERE guid=2574069;
+DELETE FROM creature_addon WHERE guid=2574069;
+DELETE FROM creature_movement WHERE id=2574069;
+DELETE FROM game_event_creature WHERE guid=2574069;
+DELETE FROM game_event_creature_data WHERE guid=2574069;
+DELETE FROM creature_battleground WHERE guid=2574069;
