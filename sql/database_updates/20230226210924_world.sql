78852f55ba5f677191e41be8ffdbbf1e54dc1514
Torta <66763404+slowtorta@users.noreply.github.com>
2023-02-27 00:10:29 +0300

    Gnoll Skull Hammer nerf.

--- sql/database_updates/20230226210924_world.sql
+++ sql/database_updates/20230226210924_world.sql
@@ -1,1 +1,3 @@
 update `quest_template` set `RequiredRaces` = 589 where `entry` between 40593 and 40603;
+
+update item_template set stat_value1 = 1, stat_type1 = 4, spellid_1 = 0 where entry = 51810; -- Gnoll Skull Hammer
