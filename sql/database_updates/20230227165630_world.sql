94f8add0153f400ea1e46dd1a8e168ac680b2e49
Torta <66763404+slowtorta@users.noreply.github.com>
2023-02-27 20:05:36 +0300

    Missing Vulture spawns (cmangos), fixed item level for some polearm.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2906
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2910

--- sql/database_updates/20230227165630_world.sql
+++ sql/database_updates/20230227165630_world.sql
@@ -14,4 +14,15 @@ update `quest_template` set `NextQuestInChain` = 40586, `NextQuestId` = 0 where 
 update `quest_template` set `NextQuestInChain` = 40587, `NextQuestId` = 0 where `entry` = 40586;
 update `quest_template` set `NextQuestInChain` = 40588, `NextQuestId` = 0 where `entry` = 40587;
 
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/2910
+UPDATE `item_template` SET `item_level` = 68 WHERE `entry` = 83461;
 
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/2906
+DELETE FROM `creature` WHERE `id` = 4158;
+INSERT INTO `creature` (`guid`, `id`, `id2`, `id3`, `id4`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecsmin`, `spawntimesecsmax`, `wander_distance`, `health_percent`, `mana_percent`, `movement_type`, `spawn_flags`, `visibility_mod`) VALUES (21548, 4158, 0, 0, 0, 1, -6041.31, -4138.35, -58.625, 6.28054, 300, 300, 30, 100, 0, 1, 0, 0);
+INSERT INTO `creature` (`guid`, `id`, `id2`, `id3`, `id4`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecsmin`, `spawntimesecsmax`, `wander_distance`, `health_percent`, `mana_percent`, `movement_type`, `spawn_flags`, `visibility_mod`) VALUES (21547, 4158, 0, 0, 0, 1, -6176.85, -3557.04, -58.625, 4.43404, 300, 300, 30, 100, 0, 1, 0, 0);
+INSERT INTO `creature` (`guid`, `id`, `id2`, `id3`, `id4`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecsmin`, `spawntimesecsmax`, `wander_distance`, `health_percent`, `mana_percent`, `movement_type`, `spawn_flags`, `visibility_mod`) VALUES (21546, 4158, 0, 0, 0, 1, -6602.18, -3865.56, -58.7499, -2.61482, 300, 300, 0, 100, 0, 0, 0, 0);
+
+INSERT INTO `creature` (`guid`, `id`, `id2`, `id3`, `id4`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecsmin`, `spawntimesecsmax`, `wander_distance`, `health_percent`, `mana_percent`, `movement_type`, `spawn_flags`, `visibility_mod`) VALUES (32583, 4158, 0, 0, 0, 1, -6040.3, -4084.69, -58.625, 2.36436, 300, 300, 30, 100, 0, 1, 0, 0);
+INSERT INTO `creature` (`guid`, `id`, `id2`, `id3`, `id4`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecsmin`, `spawntimesecsmax`, `wander_distance`, `health_percent`, `mana_percent`, `movement_type`, `spawn_flags`, `visibility_mod`) VALUES (32584, 4158, 0, 0, 0, 1, -6290.34, -3564.31, -58.625, 0.90427, 300, 300, 30, 100, 0, 1, 0, 0);
+INSERT INTO `creature` (`guid`, `id`, `id2`, `id3`, `id4`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecsmin`, `spawntimesecsmax`, `wander_distance`, `health_percent`, `mana_percent`, `movement_type`, `spawn_flags`, `visibility_mod`) VALUES (32585, 4158, 0, 0, 0, 1, -6582.02, -3923.57, -58.625, 5.82247, 300, 300, 0, 100, 0, 0, 0, 0);
