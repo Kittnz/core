a227c9e554ec857ce0e1cadd6cf8961fe34ea1d5
slowtorta <slowtorta@gmail.com>
2023-03-01 19:44:22 +0000

    Direct world update.

--- sql/database_updates/20230301192356_world.sql
+++ sql/database_updates/20230301192356_world.sql
@@ -1,3 +1,9 @@
 
 -- Changes by TINYFIN
 REPLACE INTO `creature` VALUES (2574579,50514,0,0,0,0,-10617.7,290.595,32.8007,6.1999,120,120,0,100,100,0,0,0);
+DELETE FROM creature WHERE guid=2574579;
+DELETE FROM creature_addon WHERE guid=2574579;
+DELETE FROM creature_movement WHERE id=2574579;
+DELETE FROM game_event_creature WHERE guid=2574579;
+DELETE FROM game_event_creature_data WHERE guid=2574579;
+DELETE FROM creature_battleground WHERE guid=2574579;
