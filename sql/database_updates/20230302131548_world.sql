c885b0a7e474f85ae3e54ec563708cbd84d1bfe4
Torta <66763404+slowtorta@users.noreply.github.com>
2023-03-02 16:31:14 +0300

    Assigned missing sell price to beach event items.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2922

---
+++ sql/database_updates/20230302131548_world.sql
@@ -0,0 +1,6 @@
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/2922
+UPDATE `item_template` SET `sell_price` = 1632, `buy_price` = 8161, `item_level` = 27 WHERE `entry` = 81113;
+UPDATE `item_template` SET `sell_price` = 2632, `buy_price` = 9161, `item_level` = 27 WHERE `entry` = 81110;
+
+
+
