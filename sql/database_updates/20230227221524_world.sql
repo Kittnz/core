5a09c2b7f4ebdcabfd6cfbbead91d0c8b679f128
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-03-02 15:46:23 +0300

    New NPC for PTR: 61254 - 61256.

--- sql/database_updates/20230227221524_world.sql
+++ sql/database_updates/20230227221524_world.sql
@@ -5,6 +5,25 @@ REPLACE INTO creature_template VALUES
 (61194, 729, 0, 0, 0, 0, 'Shadowbane Ragefang', NULL, 0, 62, 62, 25122, 25122, 0, 0, 3912, 24, 0, 1, 1.14286, 1.9, 20, 5, 0, 1, 1, 802, 1045, 0, 284, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 203.77, 289.86, 100, 7, 0, 61194, 0, 0, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
 (61195, 4449, 0, 0, 0, 0, 'Grellkin Shadow Weaver', NULL, 0, 61, 61, 20301, 20301, 5995, 5995, 2144, 16, 0, 1, 1.14286, 1.3, 20, 5, 0, 1, 1, 708, 937, 0, 278, 1, 2000, 2000, 2, 0, 0, 0, 0, 0, 0, 0, 172.1, 240.07, 100, 3, 0, 61195, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
 (61196, 2452, 0, 0, 0, 0, 'Grellkin Primalist', NULL, 0, 61, 61, 19301, 19301, 5890, 5890, 2033, 16, 0, 1, 1.14286, 1.2, 20, 5, 0, 1, 1, 708, 937, 0, 278, 1, 2000, 2000, 2, 0, 0, 0, 0, 0, 0, 0, 172.1, 240.07, 100, 3, 0, 61196, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
-(61197, 7554, 0, 0, 0, 0, 'Grellkin Channeler', NULL, 0, 61, 61, 19007, 19007, 5800, 5800, 1862, 16, 0, 1, 1.14286, 1.1, 20, 5, 0, 1, 1, 708, 937, 0, 278, 1, 2000, 2000, 2, 0, 0, 0, 0, 0, 0, 0, 172.1, 240.07, 100, 3, 0, 61197, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, '');
+(61197, 7554, 0, 0, 0, 0, 'Grellkin Channeler', NULL, 0, 61, 61, 19007, 19007, 5800, 5800, 1862, 16, 0, 1, 1.14286, 1.1, 20, 5, 0, 1, 1, 708, 937, 0, 278, 1, 2000, 2000, 2, 0, 0, 0, 0, 0, 0, 0, 172.1, 240.07, 100, 3, 0, 61197, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
+(61254, 3432, 0, 0, 0, 0, '"Legalbrow"', 'Famous Musician', 0, 60, 60, 19033, 19033, 0, 0, 1788, 35, 3, 1, 1.14286, 0, 20, 5, 0, 1, 1, 915, 981, 0, 278, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 6, 0, 61254, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '9617', 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
+(61255, 3665, 0, 0, 0, 0, 'Lord Ebonlocke', NULL, 0, 55, 55, 20181, 20181, 0, 0, 2899, 35, 3, 1, 1.14286, 0, 20, 5, 0, 1, 1, 491, 601, 0, 262, 1, 1300, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 6, 0, 61255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '9617', 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
+(61256, 1753, 0, 0, 0, 0, 'Magistrate Burton', NULL, 0, 49, 49, 2672, 2672, 0, 0, 2944, 35, 3, 1, 1.14286, 0, 18, 5, 0, 0, 1, 85, 104, 0, 220, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 62.8672, 86.4424, 100, 6, 0, 61256, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '9617', 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, '');
 
+set @gossip_menu_id = 41356; set @magic_number = 61256;
+replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); 
+replace into broadcast_text (entry, Male_Text) values (@magic_number, '<The apparition seems preoccupied, lost in thought, and lingering in time.>');
+replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_number);
+update creature_template set gossip_menu_id = @gossip_menu_id where entry = @magic_number;
 
+set @gossip_menu_id = 41355; set @magic_number = 61255;
+replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); 
+replace into broadcast_text (entry, Male_Text) values (@magic_number, 'Why hello there, and welcome to the festivities. I apologize for the other guests not treating you with proper decency, it would appear they are quite ravenous these days.$B$B I am Lord Ebonlocke of Darkshire. I traveled here on my own accord and have not returned home since. I am quite aware of my current state in the tower, for I have been dead for some time. Most others that linger here are not as keen unfortunately. It would appear that I am stuck to haunt these halls until something most fortunate would happen to free me from this eternal confinement.$B$B Perhaps we can help one another, I have come to learn a few things, and meet others in my own situation during the time I have had in this tower.');
+replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_number);
+update creature_template set gossip_menu_id = @gossip_menu_id where entry = @magic_number;
+
+set @gossip_menu_id = 41354; set @magic_number = 61254;
+replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); 
+replace into broadcast_text (entry, Male_Text) values (@magic_number, 'Why hello there, not often I get visitors in the Opera Hall.$B$BYou want to hear any music? It\'s been a while since I have had the chance to test my skills in this place, with the lack of people and all. I promise you it will be great, I am a famous musician after all.');
+replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_number);
+update creature_template set gossip_menu_id = @gossip_menu_id where entry = @magic_number;
