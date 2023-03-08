5caf15deb5e60e6a02590349686c7bdb5a399b6d
slowtorta <slowtorta@gmail.com>
2023-03-08 20:48:21 +0000

    Direct world update.

--- sql/database_updates/20230308153951_world.sql
+++ sql/database_updates/20230308153951_world.sql
@@ -1,3 +1,23 @@
 
 -- Changes by NICK
 REPLACE INTO `creature` VALUES (2574719,16172,0,0,0,1,-7298.34,-3719.15,10.2394,2.92935,120,120,0,100,100,0,1,200);
+DELETE FROM creature WHERE guid=2574719;
+DELETE FROM creature_addon WHERE guid=2574719;
+DELETE FROM creature_movement WHERE id=2574719;
+DELETE FROM game_event_creature WHERE guid=2574719;
+DELETE FROM game_event_creature_data WHERE guid=2574719;
+DELETE FROM creature_battleground WHERE guid=2574719;
+REPLACE INTO `creature` VALUES (2574720,16172,0,0,0,1,-7299.84,-3715.83,9.93223,4.99494,120,120,0,100,100,0,1,200);
+DELETE FROM creature WHERE guid=2574720;
+DELETE FROM creature_addon WHERE guid=2574720;
+DELETE FROM creature_movement WHERE id=2574720;
+DELETE FROM game_event_creature WHERE guid=2574720;
+DELETE FROM game_event_creature_data WHERE guid=2574720;
+DELETE FROM creature_battleground WHERE guid=2574720;
+REPLACE INTO `creature` VALUES (2574721,352,0,0,0,1,-7300.28,-3713.78,9.72754,4.66272,120,120,0,100,100,0,0,0);
+DELETE FROM creature WHERE guid=2574721;
+DELETE FROM creature_addon WHERE guid=2574721;
+DELETE FROM creature_movement WHERE id=2574721;
+DELETE FROM game_event_creature WHERE guid=2574721;
+DELETE FROM game_event_creature_data WHERE guid=2574721;
+DELETE FROM creature_battleground WHERE guid=2574721;
