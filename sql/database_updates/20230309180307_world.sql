c1bd482a4f282257479947e2024ab1206df1f580
Torta <66763404+slowtorta@users.noreply.github.com>
2023-03-09 21:08:06 +0300

    Holy Protection potions will now stack to 10 in a batch.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2968

--- sql/database_updates/20230309180307_world.sql
+++ sql/database_updates/20230309180307_world.sql
@@ -1,1 +1,6 @@
 UPDATE `quest_template` SET `QuestFlags` = 8 WHERE `entry` = 40091;
+
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/2968
+
+UPDATE `item_template` SET `stackable` = 10 WHERE `entry` = 13460;
+UPDATE `item_template` SET `stackable` = 10 WHERE `entry` = 6051;
