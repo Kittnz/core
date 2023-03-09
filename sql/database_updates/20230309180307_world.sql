40f91493e707db589784ed40a54497fc47d98b2f
Torta <66763404+slowtorta@users.noreply.github.com>
2023-03-09 21:30:35 +0300

    Changed On Use spell on Dark Iron Desecrator item.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2967

--- sql/database_updates/20230309180307_world.sql
+++ sql/database_updates/20230309180307_world.sql
@@ -4,3 +4,7 @@ UPDATE `quest_template` SET `QuestFlags` = 8 WHERE `entry` = 40091;
 
 UPDATE `item_template` SET `stackable` = 10 WHERE `entry` = 13460;
 UPDATE `item_template` SET `stackable` = 10 WHERE `entry` = 6051;
+
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/2967
+
+update item_template set spellid_1 = 13441 where entry = 61068; -- Dark Iron Desecrator
