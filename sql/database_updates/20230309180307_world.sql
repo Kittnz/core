afb35f06d722364222f8d42666e938a3275174ee
Torta <66763404+slowtorta@users.noreply.github.com>
2023-03-10 00:28:20 +0300

    Set respawn time of Forgotten Tome  to 5 seconds.

--- sql/database_updates/20230309180307_world.sql
+++ sql/database_updates/20230309180307_world.sql
@@ -6,5 +6,7 @@ UPDATE `item_template` SET `stackable` = 10 WHERE `entry` = 13460;
 UPDATE `item_template` SET `stackable` = 10 WHERE `entry` = 6051;
 
 -- https://github.com/slowtorta/turtlewow-bug-tracker/issues/2967
-
 update item_template set spellid_1 = 13441 where entry = 61068; -- Dark Iron Desecrator
+
+-- Set respawn time of Forgotten Tome (https://database.turtle-wow.org/?object=2010823) to 5 seconds:
+UPDATE gameobject SET spawntimesecsmin = 5, spawntimesecsmax = 5 WHERE id = 2010823;
