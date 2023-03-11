82cd3bd36133d501c2311dd2acfd3694b34bf6e9
Torta <66763404+slowtorta@users.noreply.github.com>
2023-03-11 20:52:52 +0300

    Added Darnassus reputation reward for Silmaron's quests.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2992

--- sql/database_updates/20230311174203_world.sql
+++ sql/database_updates/20230311174203_world.sql
@@ -1,3 +1,8 @@
 -- Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2981
 
 UPDATE `quest_template` SET `OfferRewardText` = 'Oh, a letter?$B$B<Rugnar begins to read intently.>$B$BWell, this is unexpected news, I must offer my thanks for everything you\'ve done. The last thing I expected was a letter back so quickly. Take this, for all of the troubles.' WHERE `entry` = 55023;
+
+-- Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2992
+
+UPDATE `quest_template` SET `RewRepFaction1` = 69, `RewRepValue1` = 125 WHERE `entry` = 40706;
+UPDATE `quest_template` SET `RewRepFaction1` = 69, `RewRepValue1` = 125 WHERE `entry` = 40707;
