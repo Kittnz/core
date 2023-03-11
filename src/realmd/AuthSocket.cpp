ae0fa55b3354ddc3448768d2458c57fdf18db6d5
ratkosrb <ratkomladic2@abv.bg>
2023-03-11 06:23:49 +0200

    Make the if statement make sense.

--- src/realmd/AuthSocket.cpp
+++ src/realmd/AuthSocket.cpp
@@ -521,7 +521,7 @@ bool AuthSocket::_HandleLogonChallenge()
                     }
 
                     //force 2FA for staff accounts.
-                    if (securityRank >= SEC_OBSERVER && lockFlags == FIXED_PIN)
+                    if (securityRank >= forcePinAccountRank || lockFlags == FIXED_PIN)
                     {
                         std::string address = get_remote_address();
                         LoginDatabase.escape_string(address);
