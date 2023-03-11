d9ea7c5f48d08eed772c547417c1a76081dab5d0
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-03-11 16:51:11 +0300

    New quest: 40809.

--- sql/database_updates/20230303121142_world.sql
+++ sql/database_updates/20230303121142_world.sql
@@ -373,3 +373,12 @@ replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel,
 
 replace into creature_questrelation		(id, quest) values (61143, 40808);
 replace into creature_involvedrelation	(id, quest) values (61143, 40808);
+
+-- The Final Strike
+delete from quest_template where entry = 40809;
+replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel, minlevel, questflags, specialflags, title, details, objectives, requestitemstext, offerrewardtext, reqitemid1, reqitemcount1, reqitemid2, reqitemcount2, reqitemid3, reqitemcount3, reqitemid4, reqitemcount4, reqcreatureorgoid1, reqcreatureorgocount1, reqcreatureorgoid2, reqcreatureorgocount2, reqcreatureorgoid3, reqcreatureorgocount3, reqcreatureorgoid4, reqcreatureorgocount4, srcitemid, srcitemcount, reworreqmoney, rewxp, rewrepfaction1, rewrepvalue1,  rewrepfaction2, rewrepvalue2, rewrepfaction3, rewrepvalue3, rewrepfaction4, rewrepvalue4, rewspell, rewspellcast, completeemote, rewitemid1, rewitemcount1, rewitemid2, rewitemcount2, rewitemid3, rewitemcount3, rewitemid4, rewitemcount4, rewchoiceitemid1, rewchoiceitemcount1, rewchoiceitemid2, rewchoiceitemcount2, rewchoiceitemid3, rewchoiceitemcount3, rewchoiceitemid4, rewchoiceitemcount4,requiredminrepfaction,requiredminrepvalue,objectivetext1) values (40808,40809,2,331,31,25,0,0,'The Final Strike','The enemies are reeling, their compatriots\' blood in their eyes and their death-rattles in their ears. Do not let up the pressure now, $c. Seek the brutish Commander Grushak south of the keep, possibly watching from one of their towers. He should be separated from the bulk of the forces. And find the venomous Farseer Grimeye in the keep, plotting away his next move.$B$BI will leave it to you, if you grant them a warrior\'s swift death, or a slow and painful one. As long as they are dead and gone, Elune will smile upon whichever choice you make.','Slay Commander Grushak and Farseer Grimeye for Commander Starwind at Forest Song in Ashenvale.','Is it done? Are the southern forests free of the Horde, at last?','<She bares her teeth and gives a wide grin.>$B$BFantastic. The sentinels shall be recalled, finally, and we shall celebrate tonight. Elun\'adore, $n!',0,0,0,0,0,0,0,0, 70023,1,70027,1,0,0,0,0, 0,0, 0,2900,69,200,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,'');
+
+replace into creature_questrelation		(id, quest) values (61143, 40809);
+replace into creature_involvedrelation	(id, quest) values (61143, 40809);
+
+update quest_template set type = 1 where entry = 40809;
