3fe81d9a0d5ec31acc85907b21cd96ac603073d9
Torta <66763404+slowtorta@users.noreply.github.com>
2023-03-11 20:46:22 +0300

    Fixed Relief and Reprise completion text .
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2981

---
+++ sql/database_updates/20230311174203_world.sql
@@ -0,0 +1,3 @@
+-- Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2981
+
+UPDATE `quest_template` SET `OfferRewardText` = 'Oh, a letter?$B$B<Rugnar begins to read intently.>$B$BWell, this is unexpected news, I must offer my thanks for everything you\'ve done. The last thing I expected was a letter back so quickly. Take this, for all of the troubles.' WHERE `entry` = 55023;
