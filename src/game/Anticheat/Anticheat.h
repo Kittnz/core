802cb23b4d40179d9d0ff5fa0a3230f16a4c48fe
Jameyboor <jamey_D@hotmail.com>
2023-03-13 19:06:31 +0100

    Fix crash

--- src/game/Anticheat/Anticheat.h
+++ src/game/Anticheat/Anticheat.h
@@ -160,7 +160,7 @@ class NullSessionAnticheat : public SessionAnticheatInterface
         bool ReadAddonInfo(WorldPacket* source, WorldPacket& target) override
         {
 #ifdef USING_DISCORD_BOT
-            sDiscordBot->SendMessageToChannel(1083954369503973416, string_format("[Fingerprint]: Account ID %u, IP %s reading addoninfo has no sessionAnticheat. Fingerprint will be 0.", _session->GetAccountId(), _session->GetRemoteAddress()), 
+            sDiscordBot->SendMessageToChannel(1083954369503973416, string_format("[Fingerprint]: Account ID %u, IP %s reading addoninfo has no sessionAnticheat. Fingerprint will be 0.", _session->GetAccountId(), _session->GetRemoteAddress().c_str()), 
                 DiscordBot::MessagePriority::Requeue);
 #endif
             sAddOnHandler.BuildAddonPacket(source, &target);
