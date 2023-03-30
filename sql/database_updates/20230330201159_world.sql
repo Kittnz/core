60ba9a78b0e7d11ef81770345db5562a414109d6
Torta <66763404+slowtorta@users.noreply.github.com>
2023-03-30 23:17:42 +0300

    Reduced the vendor price of Adventurer's Belt Pouch.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/3122

--- sql/database_updates/20230330201159_world.sql
+++ sql/database_updates/20230330201159_world.sql
@@ -1,4 +1,5 @@
 -- https://github.com/slowtorta/turtlewow-bug-tracker/issues/3130
 UPDATE `item_template` SET `spellcharges_1` = -10 WHERE `entry` = 53003;
 
-
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/3122
+UPDATE `item_template` SET `buy_price` = 40000, `sell_price` = 1000 WHERE `entry` = 4981;
