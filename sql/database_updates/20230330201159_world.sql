9b9612dd962cf161799aff0b506841f38909364d
Torta <66763404+slowtorta@users.noreply.github.com>
2023-03-30 23:19:37 +0300

    Assigned correct creature type to the Teldrassil Owls.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/3120

--- sql/database_updates/20230330201159_world.sql
+++ sql/database_updates/20230330201159_world.sql
@@ -3,3 +3,10 @@ UPDATE `item_template` SET `spellcharges_1` = -10 WHERE `entry` = 53003;
 
 -- https://github.com/slowtorta/turtlewow-bug-tracker/issues/3122
 UPDATE `item_template` SET `buy_price` = 40000, `sell_price` = 1000 WHERE `entry` = 4981;
+
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/3120
+
+UPDATE `creature_template` SET `type` = 1 WHERE `entry` = 7553;
+UPDATE `creature_template` SET `type` = 1 WHERE `entry` = 7554;
+UPDATE `creature_template` SET `type` = 1 WHERE `entry` = 7555;
+UPDATE `creature_template` SET `type` = 1 WHERE `entry` = 7556;
