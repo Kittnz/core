4ff9141e1c9ae534836545731476208c086628f4
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-04-01 15:34:38 +0300

    Item 81315 dropped from Sneed.

--- sql/database_updates/20230401115058_world.sql
+++ sql/database_updates/20230401115058_world.sql
@@ -8,3 +8,7 @@ update creature_template set gossip_menu_id = @gossip_menu_id where entry = @mag
 update quest_template set details = 'It does appear that the Deserter are behind the burning of the inn. If there is ever to be justice, we must stop them from continually dragging more and more of our most loyal into their ranks.$B$BThere shall be no quarter, they are wanted and we shall dole out justice, for all of the lives lost. For the deceit caused.$B$BDirectly to the west of Theramore, to the north eastern edge of the Quagmire is the Deserter\'s Hideout, it is there that they gain their strength.$B$BFind Sellick Voss, the new leader of the camp, slay him, and bring his medallion to me as proof of the deed.' where entry = 40565;
 -- Remove https://database.turtle-wow.org/?item=10605 from the loottables of bosses. Add https://database.turtle-wow.org/?item=16045 in its place. 
 update creature_loot_template set item = 16045 where item = 10605 and entry in (14889,10184,6109,14888,14890,14887,11502,11583,50112,16184,14889,14887,14890,14888,11583,11502,12397);
+-- https://database.turtle-wow.org/?item=81315#dropped-by should be probably dropped by Sneed, not Edwin.
+delete from creature_loot_template where item = 81315 and entry = 639;
+REPLACE INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES
+(643, 81315, -100, 6, 1, 1, 0);
