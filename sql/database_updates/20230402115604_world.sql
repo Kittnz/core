de61d922ed9994e5f07f6fb406b81a7fbd37d65e
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-04-02 15:09:03 +0300

    Head of Bazil Thredd deleted from NPCs on STV.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2998

--- sql/database_updates/20230402115604_world.sql
+++ sql/database_updates/20230402115604_world.sql
@@ -4,3 +4,5 @@ delete from creature_loot_template where entry = 9776 and item = 60250;
 delete from creature_loot_template where entry in (9779,91966) and item = 60712;
 -- Item: Junglepaw Fang #3123
 delete from creature_loot_template where entry = 2522 and item = 60248;
+-- Head of Bazil Thredd shouldn't be dropped from goblins in STV #2998.
+delete from creature_loot_template where entry = 921 and item = 2926;
