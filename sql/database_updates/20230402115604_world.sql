f52a296974195c579bdce9df34d3d85e089c9fd1
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-04-02 16:11:22 +0300

    Item 61153 dropped for alliance only.

--- sql/database_updates/20230402115604_world.sql
+++ sql/database_updates/20230402115604_world.sql
@@ -6,3 +6,5 @@ delete from creature_loot_template where entry in (9779,91966) and item = 60712;
 delete from creature_loot_template where entry = 2522 and item = 60248;
 -- Head of Bazil Thredd shouldn't be dropped from goblins in STV #2998.
 delete from creature_loot_template where entry = 921 and item = 2926;
+-- https://database.turtle-wow.org/?item=61153 should onyl drop for alliance.
+update item_template set allowable_race = 589 where entry = 61153;
