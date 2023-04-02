ed1171c5cccd5db467abff28a626b1f4355f911f
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-04-02 15:06:12 +0300

    Item: Junglepaw Fang deletes from Strangletorn Vale.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/3123

--- sql/database_updates/20230402115604_world.sql
+++ sql/database_updates/20230402115604_world.sql
@@ -2,3 +2,5 @@
 delete from creature_loot_template where entry = 9776 and item = 60250;
 -- Item: Scalding Flamekin Heart #3125
 delete from creature_loot_template where entry in (9779,91966) and item = 60712;
+-- Item: Junglepaw Fang #3123
+delete from creature_loot_template where entry = 2522 and item = 60248;
