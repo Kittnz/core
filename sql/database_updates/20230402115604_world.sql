74d6b3ccfaa4e9eb902ad91e10ce10893add93f4
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-04-02 16:16:03 +0300

    Respawn timer of Carver Molson setted to 5m.

--- sql/database_updates/20230402115604_world.sql
+++ sql/database_updates/20230402115604_world.sql
@@ -8,3 +8,5 @@ delete from creature_loot_template where entry = 2522 and item = 60248;
 delete from creature_loot_template where entry = 921 and item = 2926;
 -- https://database.turtle-wow.org/?item=61153 should onyl drop for alliance.
 update item_template set allowable_race = 589 where entry = 61153;
+-- Set respawn timer of Carver Molson to 5m.
+update creature set spawntimesecsmin = 300, spawntimesecsmax = 300 where ID = 60878;
