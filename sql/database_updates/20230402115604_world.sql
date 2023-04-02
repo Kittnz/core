b07f90f62ef98de89ef1f26b528c0b58747190ff
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-04-02 15:03:35 +0300

    Item: Scalding Flamekin Heart deleted outside of Burning Steps.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/3125

--- sql/database_updates/20230402115604_world.sql
+++ sql/database_updates/20230402115604_world.sql
@@ -1,4 +1,4 @@
 -- Item: Jungle Venom Gland #3124
 delete from creature_loot_template where entry = 9776 and item = 60250;
-
-
+-- Item: Scalding Flamekin Heart #3125
+delete from creature_loot_template where entry in (9779,91966) and item = 60712;
