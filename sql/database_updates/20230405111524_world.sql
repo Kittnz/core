95fefcdea08b6ab4434528a6da5332727aa000a8
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-04-05 14:47:29 +0300

    NPC Odon Shademoore display ID changed to 221.

--- sql/database_updates/20230405111524_world.sql
+++ sql/database_updates/20230405111524_world.sql
@@ -2,3 +2,5 @@
 update creature_template set display_id1 = 18174 where entry in (61251,61252,61253);
 -- NPC Brother Elias (61387) change display ID to 18677
 update creature_template set display_id1 = 18677 where entry = 61387;
+-- NPC Odon Shademoore (61380) change display ID to 221
+update creature_template set display_id1 = 221 where entry = 61380;
