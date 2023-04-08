e95b3505b5ee88c3b9dc4c8cb8581bf8316d2ce5
slowtorta <slowtorta@gmail.com>
2023-04-08 12:23:59 +0100

    Direct world update.

--- sql/database_updates/20230408113618_world.sql
+++ sql/database_updates/20230408113618_world.sql
@@ -1,3 +1,7 @@
 
 -- Changes by JOE
 UPDATE `creature` SET `position_x` = -5207.291016, `position_y` = 1272.153564, `position_z` = 54.024601, `orientation` = 3.797226 WHERE `guid` = 51210;
+
+-- Changes by TAMMO
+REPLACE INTO `gameobject` VALUES ( 5013567, 179945, 0, -1201.44, 2700.28, 32.4026, 5.18521, 0, 0, 0.521825, -0.853053, 300, 300, 100, 1, 0, 0);
+REPLACE INTO `creature` VALUES (2574976,70006,0,0,0,0,-1215.31,2702.14,32.267,1.49148,120,120,0,100,100,0,0,0);
