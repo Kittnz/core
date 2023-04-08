159585db6eb5b8443bd0f077f8cabf0a0f9ac77b
slowtorta <slowtorta@gmail.com>
2023-04-08 17:03:35 +0100

    Direct world update.

---
+++ sql/database_updates/20230408152444_world.sql
@@ -0,0 +1,3 @@
+
+-- Changes by SHANG
+REPLACE INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (880,-1095.437622,2234.752930,182.861801,2.595906,815,'gilneascity');
