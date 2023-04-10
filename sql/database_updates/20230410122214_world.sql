5fed1a3e740e42325f3fae3c5ec9cb2cbf341473
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-04-10 16:21:12 +0300

    Typo in Muckhowl's Dialogue #3182.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/3182

---
+++ sql/database_updates/20230410122214_world.sql
@@ -0,0 +1,2 @@
+-- Typo in Muckhowl's Dialogue #3182.
+update broadcast_text set male_text = 'You there, yes yes, I come from swamp called Wetlands, you heard of yes yes?\n\nBig leader Gowlfang used to stay here, look for help help, but now he is fighting to become leader of Mosshide!\n\nI here to help guide people to him.' where entry = 60976;
