a9fd3597223b7fb9c4d9ca4719d7b99be15eee94
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-04-13 17:41:27 +0300

    Mana points added for NPC Nighthowl Shadowcaster.

--- sql/database_updates/20230413115925_world.sql
+++ sql/database_updates/20230413115925_world.sql
@@ -1,2 +1,4 @@
 -- The following npcs need their scale set to 1. 61398, 61397, 61396, 61395, 61394, 61393, 61391, 61390, 61389, 61388, 61366, 61365, 61364, 61363.
 update  creature_template set scale = 0 where entry in (61398, 61397, 61396, 61395, 61394, 61393, 61391, 61390, 61389, 61388, 61366, 61365, 61364, 61363);
+-- Set mana of npc Nighthowl Shadowcaster (entry 61238) to 1299.
+update creature_template set mana_min = 1299, mana_max = 1299, unit_class = 2 where entry = 61238;
