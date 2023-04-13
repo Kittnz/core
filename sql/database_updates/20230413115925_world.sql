deea8dd5bb8fb7837cd9da4f3f8da90521930825
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-04-13 17:43:13 +0300

    New Go for PTR: Joshua the Redeemer.

--- sql/database_updates/20230413115925_world.sql
+++ sql/database_updates/20230413115925_world.sql
@@ -18,3 +18,8 @@ REPLACE INTO creature_display_info_addon (`display_id`, `bounding_radius`, `comb
 (20294, 0, 0, 0, 0),
 (20295, 0, 0, 0, 0),
 (20296, 0, 0, 0, 0);
+-- Create gameobject titled 'Joshua the Redeemer' which uses the same display ID as the gobject 'Grim Batol Memorial' (entry 1772030) it should be readable with the following text (stone background).
+REPLACE INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `faction`, `flags`, `size`, `data0`, `data1`, `data2`, `data3`, `data4`, `data5`, `data6`, `data7`, `data8`, `data9`, `data10`, `data11`, `data12`, `data13`, `data14`, `data15`, `data16`, `data17`, `data18`, `data19`, `data20`, `data21`, `data22`, `data23`, `mingold`, `maxgold`, `phase_quest_id`, `script_name`) VALUES
+(2011052, 9, 218, 'Joshua the Redeemer', 0, 0, 1, 50630, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');
+REPLACE INTO `page_text` (`entry`, `text`, `next_page`) VALUES
+(50630, 'Hierophant Joshua the Redeemer was the holiest man that ever walked the land of Gilneas: Through his blessed sermons he brought salvation to our country, illuminating our souls in the grace of the Holy Light. May our elder brother rest in sacred peace, and when the darkest of days fall upon our city, may his hallowed memory bring solace to our troubled hearts.', 0);
