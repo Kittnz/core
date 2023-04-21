a92ebf0ef2423876242c36a965f4e027417715fe
slowtorta <slowtorta@gmail.com>
2023-04-21 21:31:39 +0100

    Direct world update.

---
+++ sql/database_updates/20230419211046_world.sql
@@ -0,0 +1,5 @@
+
+-- Changes by TAMMO
+DELETE FROM gameobject WHERE guid = '5014169';
+DELETE FROM game_event_gameobject WHERE guid = '5014169';
+DELETE FROM gameobject_battleground WHERE guid = '5014169';
