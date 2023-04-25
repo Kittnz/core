3ef33da9eb833b85a48ac5c9d09e6d4e467c4deb
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-04-25 15:54:11 +0300

    New NPC for PTR added: 61410 - 61416.

--- sql/database_updates/20230425110127_world.sql
+++ sql/database_updates/20230425110127_world.sql
@@ -69,3 +69,62 @@ replace into item_template values
  '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0',
  '-1', '1', '0', '0', '0', '0', '0', '7', '0', '0', '0', '25', '0', '0', '0', '0', '7', '0', '0', '0',
  '0', '1', NULL);
+-- NEW NPC FOR PTR:
+-- Mikoto Shiina, displayid 18639, level 28, faction 35, humanoid, scale 1, quest/gossip flags, gossip text : Spirits be with you stranger, what can I do for you?
+-- Haruhiko Yoshino, displayid 18646, level 42, faction 35, humanoid, scale 1, quest/gossip flags, gossip text: Keep your distance Outsider.
+-- Dae-woong Kim, displayid 18642, level 38,  faction 35, humanoid, scale 1, quest/gossip flags, gossip text : Salutations Outlander. Keep on your good behaviour.
+-- Yasu Ping, displayid 18644, level 55, faction 35, humanoid, scale 1, quest/gossip flags, gossip text : I am watching you Outsider.
+-- Shizuru Yamada, displayid 18640, level 18, faction 35, humanoid, scale 1, quest/gossip flags, gossip text : Thank you for rescuing me. I am in your debt. ((Required for phasing, this npc should not be visible to players unless they complete the quest 'rescuing Shizuru)).
+-- Shizuru Yamada, displayid 18640, level 18, faction 35, humanoid, scale 1, gossip flags ((Required for quest)).
+-- Kurnen Oathbrace, display ID 3307, faction 120, level 44, humanoid, scale 1, gossip/quest flags, gossip text : "Why hello there, you wouldn't mind helping out a dwarf in need would ye?".
+REPLACE INTO creature_template VALUES
+(61410, 18639, 0, 0, 0, 0, 'Mikoto Shiina', NULL, 0, 28, 28, 890, 890, 0, 0, 1235, 35, 3, 1, 1.14286, 1, 18, 5, 0, 0, 1, 41, 57, 0, 112, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 42.1344, 57.9348, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
+(61411, 18646, 0, 0, 0, 0, 'Haruhiko Yoshino', NULL, 0, 42, 42, 1981, 1981, 0, 0, 2246, 35, 3, 1, 1.14286, 1, 18, 5, 0, 0, 1, 64, 79, 0, 172, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 58.7664, 80.8038, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
+(61412, 18642, 0, 0, 0, 0, 'Dae-woong Kim', NULL, 0, 38, 38, 1599, 1599, 0, 0, 1780, 35, 3, 1, 1.14286, 1, 18, 5, 0, 0, 1, 56, 69, 0, 152, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 53.8384, 74.0278, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
+(61413, 18644, 0, 0, 0, 0, 'Yasu Ping', NULL, 0, 55, 55, 3643, 3643, 0, 0, 3272, 35, 3, 1, 1.14286, 1, 20, 5, 40, 0, 1, 94, 118, 0, 254, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 71.2272, 97.9374, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
+(61414, 18640, 0, 0, 0, 0, 'Shizuru Yamada <FOR PHASE. WILL BE PHASED WHEN QUEST WILL BE ADDED>', NULL, 0, 18, 18, 417, 417, 0, 0, 765, 35, 3, 1, 1.14286, 1, 20, 5, 0, 0, 1, 41, 51, 0, 84, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 28.6704, 39.4218, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 18, 23, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
+(61415, 18640, 0, 0, 0, 0, 'Shizuru Yamada', NULL, 0, 18, 18, 417, 417, 0, 0, 765, 35, 3, 1, 1.14286, 1, 20, 5, 0, 0, 1, 41, 51, 0, 84, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 28.6704, 39.4218, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
+(61416, 3307, 0, 0, 0, 0, 'Kurnen Oathbrace', NULL, 0, 44, 44, 2117, 2117, 0, 0, 2625, 120, 3, 1, 1.14286, 1, 18, 5, 0, 0, 1, 78, 95, 0, 204, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 63.7824, 87.7008, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, '');
+
+set @gossip_menu_id = 41421; set @magic_number = 61416;
+replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); 
+replace into broadcast_text (entry, Male_Text) values (@magic_number, 'Why hello there, you wouldn\'t mind helping out a dwarf in need would ye?');
+replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_number);
+update creature_template set gossip_menu_id = @gossip_menu_id where entry = @magic_number;
+
+set @gossip_menu_id = 41420; set @magic_number = 61414;
+replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); 
+replace into broadcast_text (entry, Male_Text) values (@magic_number, 'Thank you for rescuing me. I am in your debt.');
+replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_number);
+update creature_template set gossip_menu_id = @gossip_menu_id where entry = @magic_number;
+
+set @gossip_menu_id = 41419; set @magic_number = 61413;
+replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); 
+replace into broadcast_text (entry, Male_Text) values (@magic_number, 'I am watching you Outsider.');
+replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_number);
+update creature_template set gossip_menu_id = @gossip_menu_id where entry = @magic_number;
+
+set @gossip_menu_id = 41418; set @magic_number = 61412;
+replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); 
+replace into broadcast_text (entry, Male_Text) values (@magic_number, 'Keep on your good behaviour.');
+replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_number);
+update creature_template set gossip_menu_id = @gossip_menu_id where entry = @magic_number;
+
+set @gossip_menu_id = 41417; set @magic_number = 61411;
+replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); 
+replace into broadcast_text (entry, Male_Text) values (@magic_number, 'Keep your distance Outsider.');
+replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_number);
+update creature_template set gossip_menu_id = @gossip_menu_id where entry = @magic_number;
+
+set @gossip_menu_id = 41416; set @magic_number = 61410;
+replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); 
+replace into broadcast_text (entry, Male_Text) values (@magic_number, 'Spirits be with you stranger, what can I do for you?');
+replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_number);
+update creature_template set gossip_menu_id = @gossip_menu_id where entry = @magic_number;
+
+REPLACE INTO `creature_display_info_addon` (`display_id`, `bounding_radius`, `combat_reach`, `gender`, `display_id_other_gender`) VALUES
+(18639, 0, 0, 0, 0),
+(18646, 0, 0, 0, 0),
+(18642, 0, 0, 0, 0),
+(18644, 0, 0, 0, 0),
+(18640, 0, 0, 0, 0);
