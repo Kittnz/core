8170a49fccf334a172cdfcdeaed8976b8249804c
ratkosrb <ratkomladic2@abv.bg>
2023-04-26 05:45:10 +0300

    Don't allow spamming update ticket opcode.
    No legit reason it should be received multiple times per session update, and it iterates over all sessions to send a notification to gms, so define it as slow to prevent abuse.

--- src/game/WorldSession.cpp
+++ src/game/WorldSession.cpp
@@ -977,6 +977,7 @@ bool WorldSession::AllowPacket(uint16 opcode)
         case MSG_PETITION_RENAME:
         case CMSG_SEND_MAIL:
         case CMSG_PLAYER_LOGIN:
+        case CMSG_GMTICKET_UPDATETEXT:
             _floodPacketsCount[FLOOD_VERY_SLOW_OPCODES]++;
         // no break, since slow packets are also very slow packets.
         case CMSG_LOGOUT_REQUEST:
