4ee1a3a6960274ee75cf913bbfba424ff4eb0b92
Torta <66763404+slowtorta@users.noreply.github.com>
2023-04-28 01:11:39 +0300

    Update 20230427205320_world.sql

--- sql/database_updates/20230427205320_world.sql
+++ sql/database_updates/20230427205320_world.sql
@@ -6,7 +6,7 @@ UPDATE item_template SET flags = 2112 WHERE entry = 11140;
 -- revert nerf on boots
 -- revert BoP due to nerf approach instead
 
-update item_template set spellid_2 = 7597,  spellid_4 = 0, spellid_5 = 0, bonding = 2 where entry = 65026 -- Depthstalker Helm
-update item_template set stat_type1 = 3, stat_type2 = 4, bonding = 2 where entry = 65025 -- Flamewrath Leggings
-update item_template set bonding = 2, stat_value1 = 22, spellid_2 = 13669, spellid_1 = 8815 where entry = 65027 -- Windwalker Boots
-update item_template set bonding = 2 where entry = 65024 -- Earthguard Tunic
+update item_template set spellid_2 = 7597,  spellid_4 = 0, spellid_5 = 0, bonding = 2 where entry = 65026; -- Depthstalker Helm
+update item_template set stat_type1 = 3, stat_type2 = 4, bonding = 2 where entry = 65025; -- Flamewrath Leggings
+update item_template set bonding = 2, stat_value1 = 22, spellid_2 = 13669, spellid_1 = 8815 where entry = 65027; -- Windwalker Boots
+update item_template set bonding = 2 where entry = 65024; -- Earthguard Tunic
