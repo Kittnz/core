ad23644fd0281f656fa60b83559e13459b323f14
Torta <66763404+slowtorta@users.noreply.github.com>
2023-04-28 11:23:40 +0300

    Edit wormhole's description.

--- sql/database_updates/20230428080052_world.sql
+++ sql/database_updates/20230428080052_world.sql
@@ -6,3 +6,7 @@ UPDATE gameobject SET spawntimesecsmin = 60, spawntimesecsmax = 60 WHERE id = 15
 
 -- Increase attack delay on https://database.turtle-wow.org/?npc=52119.  NPC uses a 2-handed staff but attacks as fast as a dual-wielder.  Given it's an elite, this easily tears apart even overleveled players.
 UPDATE `creature_template` SET `base_attack_time` = 2000 WHERE `entry` = 52119;
+
+-- Mention that wormholes require money.
+UPDATE `item_template` SET `description` = 'Technologically advanced energy-matter scrambler. Insert a coin to continue!' WHERE `entry` = 51313;
+UPDATE `item_template` SET `description` = 'Technologically advanced energy-matter scrambler. Insert a coin to continue!' WHERE `entry` = 51312;
