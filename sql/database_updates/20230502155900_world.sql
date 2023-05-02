082e4448762f6d0987eb4bcf5f92deb527ddf943
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-05-02 19:02:24 +0300

    Item fixes by Dragunovi.

--- sql/database_updates/20230502155900_world.sql
+++ sql/database_updates/20230502155900_world.sql
@@ -1,11 +1,18 @@
 -- Gilneas City Reward Fixes
 update item_template set bonding = 2 where entry = 61336; -- Knife Juggler Gloves
 update item_template set display_id = 27096 where entry = 61335; -- Dragonbane Pauldrons
-update item_template set display_id = 31899 where entry = 61312 -- The Black Pendant
-update item_template set display_id = 31616 where entry = 61332 -- Ring of Electrical Binding
+update item_template set display_id = 31899 where entry = 61312;-- The Black Pendant
+update item_template set display_id = 31616 where entry = 61332;-- Ring of Electrical Binding
 -- Kara10 Display
-update item_template set display_id = 66384 where entry = 61332 -- Ring of Electrical Binding
-
-
-
-
+update item_template set display_id = 66384 where entry = 61332;-- Ring of Electrical Binding
+-- Gilneas City Reward Fixes
+update item_template set bonding = 2 where entry = 61336; -- Knife Juggler Gloves
+update item_template set display_id = 24966 where entry = 61335; -- Dragonbane Pauldrons
+update item_template set display_id = 31899 where entry = 61312; -- The Black Pendant
+update item_template set display_id = 31616 where entry = 61332; -- Ring of Electrical Binding
+update item_template set spellid_1 = 45421, spelltrigger_1 = 1, spellid_2 = 13679, spelltrigger_2 = 1 where entry = 61328; -- Wolfblood
+update item_template set spellid_1 = 45420, spelltrigger_1 = 1 where entry = 61313; -- Greymane Helmet
+update item_template set spellid_1 = 45420, spelltrigger_1 = 1 where entry = 61324; -- Greymane Shoulders
+update item_template set spellid_1 = 45420, spelltrigger_1 = 1 where entry = 61302; -- Wolfheart Necklace
+update item_template set spellid_1 = 45420, spelltrigger_1 = 1, stat_value1 = 3, stat_value2 = 0 where entry = 61311; -- Sutherland's Cuffs
+update item_template set spellid_1 = 45420, spelltrigger_1 = 1, stat_type1 = 3 where entry = 61331; -- Blackcowl Sash
