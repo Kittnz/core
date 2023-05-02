f55b0bd221081a133a9469c254df62838582a947
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-05-02 16:15:08 +0300

    New quest for PTR: "Report to Luke Agamand" (40844).

--- sql/database_updates/20230501112037_world.sql
+++ sql/database_updates/20230501112037_world.sql
@@ -220,3 +220,10 @@ replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel,
 
 replace into creature_questrelation		(id, quest) values (61145, 40843);
 replace into creature_involvedrelation	(id, quest) values (61145, 40843);
+
+-- Report to Luke Agamand
+delete from quest_template where entry = 40844;
+replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel, minlevel, questflags, specialflags, title, details, objectives, requestitemstext, offerrewardtext, reqitemid1, reqitemcount1, reqitemid2, reqitemcount2, reqitemid3, reqitemcount3, reqitemid4, reqitemcount4, reqcreatureorgoid1, reqcreatureorgocount1, reqcreatureorgoid2, reqcreatureorgocount2, reqcreatureorgoid3, reqcreatureorgocount3, reqcreatureorgoid4, reqcreatureorgocount4, srcitemid, srcitemcount, reworreqmoney, RewMoneyMaxLevel, rewxp, rewrepfaction1, rewrepvalue1,  rewrepfaction2, rewrepvalue2, rewrepfaction3, rewrepvalue3, rewrepfaction4, rewrepvalue4, rewspell, rewspellcast, completeemote, rewitemid1, rewitemcount1, rewitemid2, rewitemcount2, rewitemid3, rewitemcount3, rewitemid4, rewitemcount4, rewchoiceitemid1, rewchoiceitemcount1, rewchoiceitemid2, rewchoiceitemcount2, rewchoiceitemid3, rewchoiceitemcount3, rewchoiceitemid4, rewchoiceitemcount4,requiredminrepfaction,requiredminrepvalue,objectivetext1) values (0,40844,2,5179,45,38,0,0,'Report to Luke Agamand','Luke Agamand was always the outcast among his family. Disowned and banished, he found his way into my notice and joined our crew.$B$BHis old home was one of his first raids. I have entrusted a task to him personally, and you will ensure that he succeeds.','Speak with Luke Agamand at Blackthorn\'s Camp in Gilneas.','Well, hello there.','The boss wants you to help me? I hope you\'re a capable $c and not some diplomatic noodle.$B$BWhat you\'re about to do will take some guts.',0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0, 0,0,75,68,50,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,'');
+
+replace into creature_questrelation		(id, quest) values (61375, 40844);
+replace into creature_involvedrelation	(id, quest) values (61377, 40844);
