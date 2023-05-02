7f853e54b5399ede9437cc905e152b2fdfb89c3b
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-05-02 19:00:33 +0300

    Item fixes by Dragunovi.

---
+++ sql/database_updates/20230502155900_world.sql
@@ -0,0 +1,11 @@
+-- Gilneas City Reward Fixes
+update item_template set bonding = 2 where entry = 61336; -- Knife Juggler Gloves
+update item_template set display_id = 27096 where entry = 61335; -- Dragonbane Pauldrons
+update item_template set display_id = 31899 where entry = 61312 -- The Black Pendant
+update item_template set display_id = 31616 where entry = 61332 -- Ring of Electrical Binding
+-- Kara10 Display
+update item_template set display_id = 66384 where entry = 61332 -- Ring of Electrical Binding
+
+
+
+
