abb5a4f5f9ffa04b1f623254f8a3a58ed95b3366
Torta <66763404+slowtorta@users.noreply.github.com>
2023-05-05 02:04:49 +0300

    Fixed Tier 3 quest progress and completion textes.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/2773

--- sql/database_updates/20230504172105_world.sql
+++ sql/database_updates/20230504172105_world.sql
@@ -15,3 +15,28 @@ UPDATE `quest_template` SET `Objectives` = 'Travel to the Dead Acre, and kill Ca
 UPDATE `item_template` SET `buy_price` = 1200, `sell_price` = 0 WHERE `entry` = 5332;
 
 UPDATE `broadcast_text` SET `male_text` = 'The ambitions and desires of Kul Tiras are a complicated matter, our goal here is to determine the true value of these southerners, and to see if mainlanders can truly be trusted.\n\nShould the nobility from Stormwind prove themselves loyal to the old ways and old causes, then perhaps there may be a future of cooperation.\n\nWe shall see in time where the heart of Stormwind is rightly at.', `female_text` = NULL, `chat_type` = 0, `sound_id` = 0, `language_id` = 0, `emote_id1` = 0, `emote_id2` = 0, `emote_id3` = 0, `emote_delay1` = 0, `emote_delay2` = 0, `emote_delay3` = 0 WHERE `entry` = 61017;
+
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/2773
+UPDATE `quest_template` SET `RequestItemsText` = 'Do you have the items I asked for, $c?', 
+`OfferRewardText` = 'They will fear you, $N.' WHERE `entry` = 9054;
+
+UPDATE `quest_template` SET `RequestItemsText` = 'Do you have the items I asked for, $c?', 
+`OfferRewardText` = 'As you don more of this armor, you will notice that your persona is changing. You will gain a preternatural ability for killing. Perhaps, even, a lust for blood.\n\nLet it be said that I will not be held responsible for what chaos you may invoke...' WHERE `entry` = 9055;
+
+UPDATE `quest_template` SET `RequestItemsText` = 'Do you have the items I asked for, $c?', 
+`OfferRewardText` = 'You are turning into a fearsome creature, $N.' WHERE `entry` = 9056;
+
+UPDATE `quest_template` SET `RequestItemsText` = 'Do you have the items I asked for, $c?',
+ `OfferRewardText` = 'I have taken special care in making these shoulders as large and pronounced as possible.\n\nTake my word for it; you''re going to love them...' WHERE `entry` = 9057;
+ 
+UPDATE `quest_template` SET `RequestItemsText` = 'Do you have the items I asked for, $c?', 
+`OfferRewardText` = 'The boots are ready, $N! Wear them well.' WHERE `entry` = 9058;
+
+UPDATE `quest_template` SET `RequestItemsText` = 'Do you have the items I asked for, $c?', 
+`OfferRewardText` = 'Let the rage of the crypt fiend guide your hand, $N.' WHERE `entry` = 9059;
+
+UPDATE `quest_template` SET `RequestItemsText` = 'Do you have the items I asked for, $c?',
+ `OfferRewardText` = 'A girdle fit for royalty! It''s ready for you, $N.' WHERE `entry` = 9060;
+ 
+UPDATE `quest_template` SET `RequestItemsText` = 'Do you have the items I asked for, $c?', 
+`OfferRewardText` = 'The wristguards are ready. Enjoy!' WHERE `entry` = 9061;
