caae09ee2888924ebefd061252abcf994657739c
Torta <66763404+slowtorta@users.noreply.github.com>
2023-05-05 00:45:41 +0300

    Fixed typo in The Old Church of Westfall IX-X.

--- sql/database_updates/20230504172105_world.sql
+++ sql/database_updates/20230504172105_world.sql
@@ -5,3 +5,8 @@ UPDATE item_template SET bonding = 1 WHERE entry IN (50524, 50525);
 
 -- Make Dim Torch have 1 copper sell price so deleting 50+ of them isn't a chore for people leveling Survival
 UPDATE item_template SET sell_Price = 1, buy_price = 4 WHERE entry = 6182;
+
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/3324
+UPDATE `quest_template` SET `Details` = 'So, who was this about, Marven Horsen? Larsen Wormen?$B$B<Larry lets a crass smirk rest on his face.>$B$BCarver Molsen, he certainly is a character, hangs around that old farmstead, goes by the name \'The Dead Acre\' these days, used to be called the Easton Fields.$B$BYou got what you needed, now go on, and tell who you need to.' WHERE `entry` = 40601;
+
+UPDATE `quest_template` SET `Objectives` = 'Travel to the Dead Acre, and kill Carver Molsen, recover the Deed to Easton Fields, the Deed to Molsen Farm and the Westfall Church for Gryan Stoutmantle at Sentinel Hill in Westfall.' WHERE `entry` = 40602;
