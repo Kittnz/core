4c630c56accca488c0fb06480d34181b6ec761dd
slowtorta <slowtorta@gmail.com>
2023-05-06 03:46:11 +0100

    Direct world update.

--- sql/database_updates/20230505213700_world.sql
+++ sql/database_updates/20230505213700_world.sql
@@ -6,3 +6,4 @@ DELETE FROM creature_movement WHERE id=6458;
 DELETE FROM game_event_creature WHERE guid=6458;
 DELETE FROM game_event_creature_data WHERE guid=6458;
 DELETE FROM creature_battleground WHERE guid=6458;
+REPLACE INTO `creature` VALUES (2576426,61480,0,0,0,1,2467.4,-632.234,115.055,5.01008,120,120,0,100,100,0,0,0);
