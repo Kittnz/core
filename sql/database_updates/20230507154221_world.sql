ab7b8ecdddfd67e28cd80e8a92022f17dcb4f35c
Torta <66763404+slowtorta@users.noreply.github.com>
2023-05-09 02:39:38 +0300

    Missing semicolon.

--- sql/database_updates/20230507154221_world.sql
+++ sql/database_updates/20230507154221_world.sql
@@ -1,8 +1,8 @@
 -- Link Greymane Set to the invidual pieces
-update item_template set set_id = 622 where entry = 61313 -- Greymane Helmet
-update item_template set set_id = 622 where entry = 61324 -- Greymane Shoulders
-update item_template set set_id = 622 where entry = 61376 -- Greymane Gauntlets
-update item_template set set_id = 622 where entry = 61378 -- Greymane Legplates
-update item_template set set_id = 622 where entry = 61377 -- Greymane Sabatons
-update item_template set set_id = 622 where entry = 61379 -- Greymane Vambraces
+update item_template set set_id = 622 where entry = 61313; -- Greymane Helmet
+update item_template set set_id = 622 where entry = 61324; -- Greymane Shoulders
+update item_template set set_id = 622 where entry = 61376; -- Greymane Gauntlets
+update item_template set set_id = 622 where entry = 61378; -- Greymane Legplates
+update item_template set set_id = 622 where entry = 61377; -- Greymane Sabatons
+update item_template set set_id = 622 where entry = 61379; -- Greymane Vambraces
 
