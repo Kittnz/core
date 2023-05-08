329675bb4fd56cc64743843b608dd9fdd4a7b6d8
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-05-08 16:09:27 +0300

    New quest for PTR: "Mastering the Formula I" (40867).

--- sql/database_updates/20230502155900_world.sql
+++ sql/database_updates/20230502155900_world.sql
@@ -986,3 +986,15 @@ REPLACE INTO gameobject_template VALUES
 replace into gameobject_loot_template values
 (2020022,61400,-100,0,1,1,0);
 
+-- Mastering the Formula I
+delete from quest_template where entry = 40867;
+replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel, minlevel, questflags, specialflags, title, details, objectives, requestitemstext, offerrewardtext, reqitemid1, reqitemcount1, reqitemid2, reqitemcount2, reqitemid3, reqitemcount3, reqitemid4, reqitemcount4, reqcreatureorgoid1, reqcreatureorgocount1, reqcreatureorgoid2, reqcreatureorgocount2, reqcreatureorgoid3, reqcreatureorgocount3, reqcreatureorgoid4, reqcreatureorgocount4, srcitemid, srcitemcount, reworreqmoney, RewMoneyMaxLevel, rewxp, rewrepfaction1, rewrepvalue1,  rewrepfaction2, rewrepvalue2, rewrepfaction3, rewrepvalue3, rewrepfaction4, rewrepvalue4, rewspell, rewspellcast, completeemote, rewitemid1, rewitemcount1, rewitemid2, rewitemcount2, rewitemid3, rewitemcount3, rewitemid4, rewitemcount4, rewchoiceitemid1, rewchoiceitemcount1, rewchoiceitemid2, rewchoiceitemcount2, rewchoiceitemid3, rewchoiceitemcount3, rewchoiceitemid4, rewchoiceitemcount4,requiredminrepfaction,requiredminrepvalue,objectivetext1) values (0,40867,2,1,10,6,0,0,'Mastering the Formula I','Greetings $c, I am the Master Chemist here at the Reclamation Facility, working on a groundbreaking formula to purify the air in our once beloved city of Gnomeregan. However, I am in dire need of materials to test my theories.$B$BI require a Pure Aqua Sample, which can only be obtained from the unbalanced aqua found in Dun Morogh\'s Ice Flow Lake. These elementals are known for their unique and magical properties, making them a perfect specimen for my research.$B$BIf you could gather one Pure Aqua Sample for me, it would be of great assistance. Do approach cautiously, as the elementals can be quite dangerous.','Gather a Pure Aqua Sample from the Unbalanced Aqua located at the Ice Flow Lake in Dun Morogh for Master Chemist Volterwhite.','The sample I have tasked you with gathering is of extreme importance. Do not delay in this matter.','<Master Chemist Volterwhite takes his time studying the sample with utmost care.>$B$BHmm, this may prove to be a vital piece in the complex puzzle I am working on. I need to test a few things first...',61401,1,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0, 0,3900,650,54,100,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,'');
+
+replace into creature_questrelation		(id, quest) values (61436, 40867);
+replace into creature_involvedrelation	(id, quest) values (61436, 40867);
+
+replace into item_template (entry, display_id, name, class, quality, flags, buy_count, allowable_class, allowable_race, item_level, stackable, spellcooldown_1, spellcategorycooldown_1, spellcooldown_2, spellcategorycooldown_2, bonding, description, page_text) values
+(61401,1328,'Pure Aqua Sample',12,1,2048,1,-1,-1,1,1,-1,-1,-1,-1,4,'',0);
+
+REPLACE INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES
+(61463, 61401, -100, 0, 1, 1, 0);
