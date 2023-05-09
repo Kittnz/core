7229a7209b7a8e2818a445151b6f83361f4f3cfc
Torta <66763404+slowtorta@users.noreply.github.com>
2023-05-10 00:37:28 +0300

    Revert "[SpellInterruptFlags] Correcting names."
    This reverts commit 3af31e0f9a6aa6f799bc96b71e639d760878676b.

--- src/game/Spells/Spell.h
+++ src/game/Spells/Spell.h
@@ -436,8 +436,8 @@ class Spell
             return  m_spellInfo->Attributes & SPELL_ATTR_RANGED;
         }
         bool IsChannelActive() const { return m_casterUnit ? m_casterUnit->GetUInt32Value(UNIT_CHANNEL_SPELL) != 0 : false; }
-        bool IsMeleeAttackResetSpell() const { return !m_IsTriggeredSpell && (m_spellInfo->InterruptFlags & SPELL_INTERRUPT_FLAG_COMBAT);  }
-        bool IsRangedAttackResetSpell() const { return !m_IsTriggeredSpell && IsRangedSpell() && (m_spellInfo->InterruptFlags & SPELL_INTERRUPT_FLAG_COMBAT); }
+        bool IsMeleeAttackResetSpell() const { return !m_IsTriggeredSpell && (m_spellInfo->InterruptFlags & SPELL_INTERRUPT_FLAG_INTERRUPT);  }
+        bool IsRangedAttackResetSpell() const { return !m_IsTriggeredSpell && IsRangedSpell() && (m_spellInfo->InterruptFlags & SPELL_INTERRUPT_FLAG_INTERRUPT); }
 
         bool IsDeletable() const { return !m_referencedFromCurrentSpell && !m_executeStack; }
         void SetReferencedFromCurrent(bool yes) { m_referencedFromCurrentSpell = yes; }
