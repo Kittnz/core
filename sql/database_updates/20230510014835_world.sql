199e3d2e2aa56828c1042b471b48e409a199691d
slowtorta <slowtorta@gmail.com>
2023-05-10 03:13:42 +0100

    Direct world update.

--- sql/database_updates/20230510014835_world.sql
+++ sql/database_updates/20230510014835_world.sql
@@ -1,3 +1,6 @@
 
 -- Changes by SHANG
 REPLACE INTO `gameobject` VALUES ( 5015212, 2007370, 1, 5562.22, -3694.58, 1598.89, 0.485775, 0, 0, 0.240507, 0.970648, 300, 300, 100, 1, 0, 0);
+DELETE FROM gameobject WHERE guid = '5015212';
+DELETE FROM game_event_gameobject WHERE guid = '5015212';
+DELETE FROM gameobject_battleground WHERE guid = '5015212';
