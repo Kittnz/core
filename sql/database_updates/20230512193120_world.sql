d94dd09fef0ca47a73835dd64520519203c4e25c
Torta <66763404+slowtorta@users.noreply.github.com>
2023-05-12 22:47:24 +0300

    New taxi node: Nordanaar, Hyjal.

---
+++ sql/database_updates/20230512193120_world.sql
@@ -0,0 +1,1 @@
+REPLACE INTO `taxi_nodes` (`id`, `map_id`, `x`, `y`, `z`, `name`, `mount_creature_id1`, `mount_creature_id2`) VALUES (186, 1, 5497.7, -3690.7, 1594.21, 'Nordanaar, Hyjal', 2224, 541);
