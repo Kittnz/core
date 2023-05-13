0875d7e3b77db6d21ee356dd9f691a03098a354e
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-05-13 15:39:18 +0300

    New quest for PTR: "Dreamhide Belt" (40899).

--- sql/database_updates/20230510101617_world.sql
+++ sql/database_updates/20230510101617_world.sql
@@ -2109,3 +2109,12 @@ replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel,
 
 replace into creature_questrelation		(id, quest) values (61493, 40898);
 replace into creature_involvedrelation	(id, quest) values (61493, 40898);
+
+-- Dreamhide Belt
+delete from quest_template where entry = 40899;
+replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel, minlevel, questflags, specialflags, title, details, objectives, requestitemstext, offerrewardtext, reqitemid1, reqitemcount1, reqitemid2, reqitemcount2, reqitemid3, reqitemcount3, reqitemid4, reqitemcount4, reqcreatureorgoid1, reqcreatureorgocount1, reqcreatureorgoid2, reqcreatureorgocount2, reqcreatureorgoid3, reqcreatureorgocount3, reqcreatureorgoid4, reqcreatureorgocount4, srcitemid, srcitemcount, reworreqmoney, RewMoneyMaxLevel, rewxp, rewrepfaction1, rewrepvalue1,  rewrepfaction2, rewrepvalue2, rewrepfaction3, rewrepvalue3, rewrepfaction4, rewrepvalue4, rewspell, rewspellcast, completeemote, rewitemid1, rewitemcount1, rewitemid2, rewitemcount2, rewitemid3, rewitemcount3, rewitemid4, rewitemcount4, rewchoiceitemid1, rewchoiceitemcount1, rewchoiceitemid2, rewchoiceitemcount2, rewchoiceitemid3, rewchoiceitemcount3, rewchoiceitemid4, rewchoiceitemcount4,requiredminrepfaction,requiredminrepvalue,objectivetext1) values (0,40899,2,616,60,55,0,0,'Dreamhide Belt','I am not looking to give my most precious patterns to the unworthy wretches who call themselves leatherworkers. I require a showing of dedication and willpower to offer apprentices a chance to craft my great work.$B$BIf you desire to learn the Dreamhide Belt pattern, bring me 75 Bright Dream Shards.','Gather 75 Bright Dream Shards for Jancel Stardust at Nordanaar in Hyjal.','Have you brought the Dream Shards adventurer?','I remember the day I first learned to craft and create Dreamhide, it was a most rewarding experience.$B$BMay this recipe serve you well, $N.',61199,75,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0, 0,0,250,0,0,0,0,0,0,0,0,0,0,0, 61431,1,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,'');
+
+replace into creature_questrelation		(id, quest) values (61493, 40899);
+replace into creature_involvedrelation	(id, quest) values (61493, 40899);
+
+
