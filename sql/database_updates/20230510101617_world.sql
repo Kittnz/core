078200bc73239901a43620ca8a918672ebfa68fa
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-05-16 15:56:42 +0300

    New NPC for PTR: 61512 - 61514.

--- sql/database_updates/20230510101617_world.sql
+++ sql/database_updates/20230510101617_world.sql
@@ -2156,3 +2156,127 @@ replace into creature_involvedrelation	(id, quest) values (61492, 40904);
 update broadcast_text set male_text = 'The Barkskin Tribe has been lost to aggression. The corruption of Hyjal has taken a toll on my people, and we are all that is left here. It would appear that our sad and tragic downfall is common amongst the other tribes as well.' where entry = 61506;
 -- Change greeting text of NPC Nargg (Entry 61507) to the following : "Nargg is loyal, Nargg is strong!$B$BYou threaten Elder Barkmaw, Nargg threaten you! You hurt Elder Barkmaw, Nargg hurt you!" 
 update broadcast_text set male_text = 'Nargg is loyal, Nargg is strong!$B$BYou threaten Elder Barkmaw, Nargg threaten you! You hurt Elder Barkmaw, Nargg hurt you!' where entry = 61507;
+
+-- Arch Druid Dreamwind, display ID 13409, faction 35, level 62 elite, humanoid, scale 1, quest/gossip flags, weapon 83471, gossip text : "My duty is to ensure the roots of the World Tree recover from the devastation wrought by the Burning Legion. It has been some years since their leader was defeated by Malfurion Stormrage, and yet their kind still linger.$B$BAs long as you remain an enemy to demons, you will always be welcome in Nordrassil Glade." 
+REPLACE INTO creature_template VALUES
+(61512, 13409, 0, 0, 0, 0, 'Arch Druid Dreamwind', NULL, 53106, 62, 62, 65151, 65151, 0, 0, 3791, 35, 3, 1, 1.14286, 1, 20, 5, 0, 1, 1, 873, 1039, 0, 278, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 61.732, 84.8815, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 503, 2517, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
+-- Aliattan Anderson, display ID 1524, Faction 84, Level 30, humanoid, scale 1, quest/gossip flags,  Gossip: Who...? Who goes there?
+(61513, 1524, 0, 0, 0, 0, 'Aliattan Anderson', NULL, 0, 30, 30, 1002, 1002, 0, 0, 1200, 84, 3, 1, 1.14286, 1, 18, 5, 0, 0, 1, 43, 53, 0, 122, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 42.1344, 57.9348, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 44, 62, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, ''),
+-- [Willow] display ID 10728, faction 21, level 44, undead, scale 1.3 (has 4802 mana, casts 11699 3 seconds into the fight and every 12 seconds after, casts 17937 every 20 seconds).
+(61514, 10728, 0, 0, 0, 0, 'Willow', NULL, 0, 44, 44, 2117, 2117, 4802, 4802, 2625, 21, 0, 1, 1.14714, 1.3, 18, 5, 0, 0, 1, 78, 95, 0, 204, 1, 2000, 2000, 2, 0, 0, 0, 0, 0, 0, 0, 63.7824, 87.7008, 100, 6, 0, 61514, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 84, 115, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, '');
+
+set @equip_template = 20390; set @weapon_1 = 83471; set @weapon_2 = 0; set @weapon_3 = 0; set @creature = 61512;
+replace into creature_equip_template values (@equip_template, @weapon_1, @weapon_2, @weapon_3);
+update creature_template set equipment_id = @equip_template where entry = @creature;
+
+set @gossip_menu_id = 41476; set @magic_number = 61513;
+replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); 
+replace into broadcast_text (entry, Male_Text) values (@magic_number, 'Who...? Who goes there?');
+replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_number);
+update creature_template set gossip_menu_id = @gossip_menu_id where entry = @magic_number;
+
+set @gossip_menu_id = 41475; set @magic_number = 61512;
+replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); 
+replace into broadcast_text (entry, Male_Text) values (@magic_number, 'My duty is to ensure the roots of the World Tree recover from the devastation wrought by the Burning Legion. It has been some years since their leader was defeated by Malfurion Stormrage, and yet their kind still linger.$B$BAs long as you remain an enemy to demons, you will always be welcome in Nordrassil Glade.');
+replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_number);
+update creature_template set gossip_menu_id = @gossip_menu_id where entry = @magic_number;
+
+-- Willow
+set @creature_entry = 61514;
+set @description = ': Willow';
+set @spell_list_id = 180190;
+
+set @spellid_1 = 11699; -- Drain Life
+set @probability_1 = 100;
+set @casttarget_1 = 1;
+set @castflags_1 = 4;
+set @delayinitialmin_1 = 3;
+set @delayinitialmax_1 = 3;
+set @delayrepeatmin_1 = 12;
+set @delayrepeatmax_1 = 12;
+
+set @spellid_2 = 17937; -- Curse of Shadow
+set @probability_2 = 100;
+set @casttarget_2 = 1;
+set @castflags_2 = 4;
+set @delayinitialmin_2 = 0;
+set @delayinitialmax_2 = 0;
+set @delayrepeatmin_2 = 20;
+set @delayrepeatmax_2 = 20;
+
+set @spellid_3 = 0;
+set @probability_3 = 0;
+set @casttarget_3 = 0;
+set @castflags_3 = 0;
+set @delayinitialmin_3 = 0;
+set @delayinitialmax_3 = 0;
+set @delayrepeatmin_3 = 0;
+set @delayrepeatmax_3 = 0;
+
+set @spellid_4 = 0;
+set @probability_4 = 0;
+set @casttarget_4 = 0;
+set @castflags_4 = 0;
+set @delayinitialmin_4 = 0;
+set @delayinitialmax_4 = 0;
+set @delayrepeatmin_4 = 0;
+set @delayrepeatmax_4 = 0;
+
+set @spellid_5 = 0;
+set @probability_5 = 0;
+set @casttarget_5 = 0;
+set @castflags_5 = 0;
+set @delayinitialmin_5 = 0;
+set @delayinitialmax_5 = 0;
+set @delayrepeatmin_5 = 0;
+set @delayrepeatmax_5 = 0;
+
+set @spellid_6 = 0;
+set @probability_6 = 0;
+set @casttarget_6 = 0;
+set @castflags_6 = 0;
+set @delayinitialmin_6 = 0;
+set @delayinitialmax_6 = 0;
+set @delayrepeatmin_6 = 0;
+set @delayrepeatmax_6 = 0;
+
+set @spellid_7 = 0;
+set @probability_7 = 0;
+set @casttarget_7 = 0;
+set @castflags_7 = 0;
+set @delayinitialmin_7 = 0;
+set @delayinitialmax_7 = 0;
+set @delayrepeatmin_7 = 0;
+set @delayrepeatmax_7 = 0;
+
+set @spellid_8 = 0;
+set @probability_8 = 0;
+set @casttarget_8 = 0;
+set @castflags_8 = 0;
+set @delayinitialmin_8 = 0;
+set @delayinitialmax_8 = 0;
+set @delayrepeatmin_8 = 0;
+set @delayrepeatmax_8 = 0;
+
+-- Do not touch this part:
+update creature_template set spell_list_id = @spell_list_id, ai_name = 'EventAI', script_name = '', spell_id1 = 0, spell_id2 = 0, spell_id3 = 0 
+where entry = @creature_entry;
+replace into creature_spells (entry, name, 
+spellid_1, probability_1, casttarget_1, castflags_1, delayinitialmin_1, delayinitialmax_1, delayrepeatmin_1, delayrepeatmax_1, 
+spellid_2, probability_2, casttarget_2, castflags_2, delayinitialmin_2, delayinitialmax_2, delayrepeatmin_2, delayrepeatmax_2, 
+spellid_3, probability_3, casttarget_3, castflags_3, delayinitialmin_3, delayinitialmax_3, delayrepeatmin_3, delayrepeatmax_3, 
+spellid_4, probability_4, casttarget_4, castflags_4, delayinitialmin_4, delayinitialmax_4, delayrepeatmin_4, delayrepeatmax_4, 
+spellid_5, probability_5, casttarget_5, castflags_5, delayinitialmin_5, delayinitialmax_5, delayrepeatmin_5, delayrepeatmax_5, 
+spellid_6, probability_6, casttarget_6, castflags_6, delayinitialmin_6, delayinitialmax_6, delayrepeatmin_6, delayrepeatmax_6, 
+spellid_7, probability_7, casttarget_7, castflags_7, delayinitialmin_7, delayinitialmax_7, delayrepeatmin_7, delayrepeatmax_7, 
+spellid_8, probability_8, casttarget_8, castflags_8, delayinitialmin_8, delayinitialmax_8, delayrepeatmin_8, delayrepeatmax_8) 
+values (@spell_list_id, @description,
+@spellid_1, @probability_1, @casttarget_1, @castflags_1, @delayinitialmin_1, @delayinitialmax_1, @delayrepeatmin_1, @delayrepeatmax_1,
+@spellid_2, @probability_2, @casttarget_2, @castflags_2, @delayinitialmin_2, @delayinitialmax_2, @delayrepeatmin_2, @delayrepeatmax_2,
+@spellid_3, @probability_3, @casttarget_3, @castflags_3, @delayinitialmin_3, @delayinitialmax_3, @delayrepeatmin_3, @delayrepeatmax_3,
+@spellid_4, @probability_4, @casttarget_4, @castflags_4, @delayinitialmin_4, @delayinitialmax_4, @delayrepeatmin_4, @delayrepeatmax_4,
+@spellid_5, @probability_5, @casttarget_5, @castflags_5, @delayinitialmin_5, @delayinitialmax_5, @delayrepeatmin_5, @delayrepeatmax_5,
+@spellid_6, @probability_6, @casttarget_6, @castflags_6, @delayinitialmin_6, @delayinitialmax_6, @delayrepeatmin_6, @delayrepeatmax_6,
+@spellid_7, @probability_7, @casttarget_7, @castflags_7, @delayinitialmin_7, @delayinitialmax_7, @delayrepeatmin_7, @delayrepeatmax_7,
+@spellid_8, @probability_8, @casttarget_8, @castflags_8, @delayinitialmin_8, @delayinitialmax_8, @delayrepeatmin_8, @delayrepeatmax_8);
+
