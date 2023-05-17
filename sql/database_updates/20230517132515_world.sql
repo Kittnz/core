4726e1a1c592372a578281534b6d620bdf9411df
slowtorta <slowtorta@gmail.com>
2023-05-17 15:16:02 +0100

    Direct world update.

--- sql/database_updates/20230517132515_world.sql
+++ sql/database_updates/20230517132515_world.sql
@@ -1,3 +1,37 @@
 
 -- Changes by DRAGUNOVI
 REPLACE INTO `creature` VALUES (2577155,61518,0,0,0,0,-13878.1,3552.42,0.594936,4.97091,120,120,0,100,100,0,0,0);
+REPLACE INTO `creature` VALUES (2577156,92939,0,0,0,0,-13872.6,3551.25,0.736531,2.72546,120,120,0,100,100,0,0,0);
+DELETE FROM creature WHERE guid=2577156;
+DELETE FROM creature_addon WHERE guid=2577156;
+DELETE FROM creature_movement WHERE id=2577156;
+DELETE FROM game_event_creature WHERE guid=2577156;
+DELETE FROM game_event_creature_data WHERE guid=2577156;
+DELETE FROM creature_battleground WHERE guid=2577156;
+UPDATE `creature` SET `position_x` = -13805.695312, `position_y` = 3549.249756, `position_z` = 0.662193, `orientation` = 2.598434 WHERE `guid` = 2577155;
+REPLACE INTO `creature` VALUES (2577155,61518,0,0,0,0,-13805.7,3549.25,0.662193,2.59843,300,300,0,100,100,0,0,0);
+DELETE FROM creature_movement WHERE id=2577155;
+INSERT INTO creature_movement (id,point,position_x,position_y,position_z,orientation) VALUES (2577155,1, -13805.695312,3549.249756,0.662193, 100);
+INSERT INTO creature_movement (id,point,position_x,position_y,position_z,orientation) VALUES (2577155,2, -13862.855469,3566.149658,3.651983, 100);
+INSERT INTO creature_movement (id,point,position_x,position_y,position_z,orientation) VALUES (2577155,3, -13939.987305,3581.805908,1.635642, 100);
+INSERT INTO creature_movement (id,point,position_x,position_y,position_z,orientation) VALUES (2577155,4, -14036.615234,3561.058838,5.750637, 100);
+DELETE FROM creature WHERE guid=2577155;
+DELETE FROM creature_addon WHERE guid=2577155;
+DELETE FROM creature_movement WHERE id=2577155;
+DELETE FROM game_event_creature WHERE guid=2577155;
+DELETE FROM game_event_creature_data WHERE guid=2577155;
+DELETE FROM creature_battleground WHERE guid=2577155;
+REPLACE INTO `creature` VALUES (2577157,61518,0,0,0,0,-13953,3575.21,1.73438,4.70957,120,120,0,100,100,0,0,0);
+UPDATE `creature` SET `position_x` = -13953.011719, `position_y` = 3581.966553, `position_z` = 1.944334, `orientation` = 4.709572 WHERE `guid` = 2577157;
+REPLACE INTO `creature` VALUES (2577157,61518,0,0,0,0,-13953,3581.97,1.94433,4.70957,300,300,0,100,100,0,0,0);
+DELETE FROM creature_movement WHERE id=2577157;
+INSERT INTO creature_movement (id,point,position_x,position_y,position_z,orientation) VALUES (2577157,1, -13953.011719,3581.966553,1.944334, 100);
+INSERT INTO creature_movement (id,point,position_x,position_y,position_z,orientation) VALUES (2577157,2, -13888.590820,3574.653564,1.711782, 100);
+INSERT INTO creature_movement (id,point,position_x,position_y,position_z,orientation) VALUES (2577157,3, -13814.875000,3550.889893,0.538204, 100);
+INSERT INTO creature_movement (id,point,position_x,position_y,position_z,orientation) VALUES (2577157,4, -13866.851562,3571.792725,4.137506, 100);
+INSERT INTO creature_movement (id,point,position_x,position_y,position_z,orientation) VALUES (2577157,5, -13952.673828,3580.958252,1.926329, 100);
+INSERT INTO creature_movement (id,point,position_x,position_y,position_z,orientation) VALUES (2577157,6, -14012.828125,3561.946045,3.918641, 100);
+INSERT INTO creature_movement (id,point,position_x,position_y,position_z,orientation) VALUES (2577157,7, -14049.572266,3560.645020,6.037330, 100);
+INSERT INTO creature_movement (id,point,position_x,position_y,position_z,orientation) VALUES (2577157,8, -14114.654297,3572.844238,1.510013, 100);
+INSERT INTO creature_movement (id,point,position_x,position_y,position_z,orientation) VALUES (2577157,9, -14047.299805,3560.641846,6.159867, 100);
+REPLACE INTO `creature` VALUES (2577157,61518,0,0,0,0,-13953,3581.97,1.94433,4.70957,300,300,0,100,100,2,0,0);
