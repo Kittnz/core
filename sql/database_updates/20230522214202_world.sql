610cff5fc35ef07e258d957d3219d84df6a78f57
Torta <66763404+slowtorta@users.noreply.github.com>
2023-05-23 02:39:55 +0300

    Reduced XP from killing Starving Bonepaw in Desolace.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/3485

--- sql/database_updates/20230522214202_world.sql
+++ sql/database_updates/20230522214202_world.sql
@@ -21,3 +21,8 @@ UPDATE `item_template` SET `material` = 2, `display_id` = 28858, name = 'Felmusk
 -- Fixed a typo in Kelly Vaulstone's gossip.
 
 UPDATE `broadcast_text` SET `male_text` = 'This harbor ain''t too shabby for fishing!', `female_text` = NULL, `chat_type` = 0, `sound_id` = 0, `language_id` = 0, `emote_id1` = 0, `emote_id2` = 0, `emote_id3` = 0, `emote_delay1` = 0, `emote_delay2` = 0, `emote_delay3` = 0 WHERE `entry` = 60928;
+
+-- Reduced XP from killing Starving Bonepaw in Desolace.
+-- In WoW classic, these mobs give reduced experience, since they have reduced hit points (they die in 2-3 spells).
+
+UPDATE `creature_template` SET `xp_multiplier` = 0.3 WHERE `entry` = 4689;
