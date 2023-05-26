76d7b821788a380aa2bf72ae9915c24e6b180901
Torta <66763404+slowtorta@users.noreply.github.com>
2023-05-26 14:25:00 +0300

    Removed mid-flight teleport for custom taxi flights.

--- src/game/Objects/Player.h
+++ src/game/Objects/Player.h
@@ -673,7 +673,6 @@ enum PlayerDelayedOperations
     DELAYED_RESURRECT_PLAYER          = 0x02,
     DELAYED_SPELL_CAST_DESERTER       = 0x04,
     DELAYED_CAST_HONORLESS_TARGET     = 0x08,
-    DELAYED_TAXI_FLIGHT_WITH_TELEPORT = 0x10, // special case: it's not default restoring taxi flight
     DELAYED_END
 };
 
@@ -2887,22 +2886,6 @@ public:
 	public:
 		void SendAddonMessage(std::string prefix, std::string message);
 		void SendAddonMessage(std::string prefix, std::string message, Player* from);
-
-        // For new taxi
-    public:
-        uint32 GetSaveTaxiData(uint8 index) { if (index > 2) return 0; return saveTaxiDataForTeleport[index]; }
-        void SaveTaxiFlightData(uint32 startNodeAfterTeleport)
-        {
-            saveTaxiDataForTeleport[0] = m_taxi.GetTaxiSource();
-            saveTaxiDataForTeleport[1] = m_taxi.GetTaxiDestination();
-            saveTaxiDataForTeleport[2] = startNodeAfterTeleport;
-        }
-        void ClearTaxiFlightData(uint8 i) { saveTaxiDataForTeleport[i] = 0; }
-        void PrepareTaxiFlightWithTeleport() { ScheduleDelayedOperation(DELAYED_TAXI_FLIGHT_WITH_TELEPORT); }
-    private:
-        uint32 saveTaxiDataForTeleport[3]{};
-
-
 };
 
 void AddItemsSetItem(Player*player,Item* item);
