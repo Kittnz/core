9e9813f99b26dff5134b85b01134fe4e081bad5e
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-05-29 16:27:11 +0300

    New quest for PTR: "Allies Against Undeath" (40934).

--- sql/database_updates/20230529112533_world.sql
+++ sql/database_updates/20230529112533_world.sql
@@ -202,3 +202,14 @@ replace into creature_questrelation		(id, quest) values (61384, 40933);
 replace into creature_involvedrelation	(id, quest) values (61381, 40933);
 
 update quest_template set requiredraces = 589 where entry = 40933;
+
+-- Allies Against Undeath
+delete from quest_template where entry = 40934;
+replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel, minlevel, questflags, specialflags, title, details, objectives, requestitemstext, offerrewardtext, reqitemid1, reqitemcount1, reqitemid2, reqitemcount2, reqitemid3, reqitemcount3, reqitemid4, reqitemcount4, reqcreatureorgoid1, reqcreatureorgocount1, reqcreatureorgoid2, reqcreatureorgocount2, reqcreatureorgoid3, reqcreatureorgocount3, reqcreatureorgoid4, reqcreatureorgocount4, srcitemid, srcitemcount, reworreqmoney, RewMoneyMaxLevel, rewxp, rewrepfaction1, rewrepvalue1,  rewrepfaction2, rewrepvalue2, rewrepfaction3, rewrepvalue3, rewrepfaction4, rewrepvalue4, rewspell, rewspellcast, completeemote, rewitemid1, rewitemcount1, rewitemid2, rewitemcount2, rewitemid3, rewitemcount3, rewitemid4, rewitemcount4, rewchoiceitemid1, rewchoiceitemcount1, rewchoiceitemid2, rewchoiceitemcount2, rewchoiceitemid3, rewchoiceitemcount3, rewchoiceitemid4, rewchoiceitemcount4,requiredminrepfaction,requiredminrepvalue,objectivetext1) values (0,40934,2,5179,44,35,8,0,'Allies Against Undeath','The foul taint of undeath is a most vile and terrifying thing. The land of Gilneas some years ago was invaded by a lich who scarred the land with his evil magic.$B$BThis is a tale similar to what befell my own lands. You see, the Scarlet Crusade is not an enemy, for we all serve the same purpose.$B$BI ask you to prove yourself as an agent against necromancy. Travel to Greyshire, or what is left of it. Slay the dead things that still walk there.','Slay 7 Lingering Skeletons and 7 Shambling Dead for Brother Elias at Shademore Tavern in Gilneas.','Have you dealt holy retribution?','So, it would appear that you are indeed not corrupted by the shadow.',0,0,0,0,0,0,0,0, 61234,7,61235,7,0,0,0,0, 0,0, 0,19800,3300,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,'');
+
+replace into creature_questrelation		(id, quest) values (61387, 40934);
+replace into creature_involvedrelation	(id, quest) values (61387, 40934);
+
+update quest_template set requiredraces = 589 where entry = 40934;
+
+
