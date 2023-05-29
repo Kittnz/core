658bf74b47dec3da7652413f8fb5c17387340cc2
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-05-29 17:33:09 +0300

    New quest for PTR: "Nighthowl Nuisance" (40937).

--- sql/database_updates/20230529112533_world.sql
+++ sql/database_updates/20230529112533_world.sql
@@ -252,3 +252,19 @@ replace into gameobject_loot_template values
 
 update gameobject set spawntimesecsmin = 10, spawntimesecsmax = 10 where ID = 2020031;
 
+-- Nighthowl Nuisance
+delete from quest_template where entry = 40937;
+replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel, minlevel, questflags, specialflags, title, details, objectives, requestitemstext, offerrewardtext, reqitemid1, reqitemcount1, reqitemid2, reqitemcount2, reqitemid3, reqitemcount3, reqitemid4, reqitemcount4, reqcreatureorgoid1, reqcreatureorgocount1, reqcreatureorgoid2, reqcreatureorgocount2, reqcreatureorgoid3, reqcreatureorgocount3, reqcreatureorgoid4, reqcreatureorgocount4, srcitemid, srcitemcount, reworreqmoney, RewMoneyMaxLevel, rewxp, rewrepfaction1, rewrepvalue1,  rewrepfaction2, rewrepvalue2, rewrepfaction3, rewrepvalue3, rewrepfaction4, rewrepvalue4, rewspell, rewspellcast, completeemote, rewitemid1, rewitemcount1, rewitemid2, rewitemcount2, rewitemid3, rewitemcount3, rewitemid4, rewitemcount4, rewchoiceitemid1, rewchoiceitemcount1, rewchoiceitemid2, rewchoiceitemcount2, rewchoiceitemid3, rewchoiceitemcount3, rewchoiceitemid4, rewchoiceitemcount4,requiredminrepfaction,requiredminrepvalue,objectivetext1) values (0,40937,2,5179,43,35,8,0,'Nighthowl Nuisance','Night in and night out, we have been subjected to raids from the hostile Nighthowl. They are a ferocious and merciless foe that has tested our defences.$B$BWe ask for your aid in dealing with this threat before they overwhelm us. The Nighthowl can be found lingering all over northern Gilneas, from Oldrock Pass to the Northgate Tower$B$BSlay them, and bring me eleven of the shackles they wear around their wrists.','Slay the Nighthowl Worgen and collect 11 Nighthowl Shackles for Camp Leader Gethenor at the Vagrant Encampment in northern Gilneas.','Each moment wasted is another moment the Nighthowl bide their strength.','These shackles hold some sort of power channeled only by the worgen.$B$BYou have done well in thinning their numbers, but there is still more than we can combat.',61480,11,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0, 1000,19500,3250,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,'');
+
+replace into creature_questrelation		(id, quest) values (61535, 40937);
+replace into creature_involvedrelation	(id, quest) values (61535, 40937);
+
+update quest_template set requiredraces = 589 where entry = 40937;
+
+replace into item_template (entry, display_id, name, class, quality, flags, buy_count, allowable_class, allowable_race, item_level, stackable, spellcooldown_1, spellcategorycooldown_1, spellcooldown_2, spellcategorycooldown_2, bonding, description, page_text, material) values
+(61480,3628,'Nighthowl Shackle',12,1,2048,1,-1,-1,1,11,-1,-1,-1,-1,4,'',0,0);
+
+replace into creature_loot_template values
+(61236,61480,-72,0,1,1,0),
+(61237,61480,-72,0,1,1,0),
+(61238,61480,-72,0,1,1,0);
