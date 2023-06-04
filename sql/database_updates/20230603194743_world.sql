72bbd8e2356bb80c7babe0c953d0e18222e85029
slowtorta <slowtorta@gmail.com>
2023-06-04 08:09:16 +0100

    Direct world update.

--- sql/database_updates/20230603194743_world.sql
+++ sql/database_updates/20230603194743_world.sql
@@ -11,3 +11,8 @@ DELETE FROM creature_battleground WHERE guid=2577388;
 
 -- Changes by SHANG
 REPLACE INTO `creature` VALUES (2577389,61287,0,0,0,0,-1844.77,1855.75,64.6029,0.505751,120,120,0,100,100,0,0,0);
+REPLACE INTO `creature_addon` (`guid`, `stand_state`) VALUES (2577389, 2);
+REPLACE INTO `creature_addon` (`guid`, `stand_state`) VALUES (2577389, 1);
+REPLACE INTO `gameobject` VALUES ( 5015510, 2001789, 0, -1843.75, 1853.51, 64.3471, 0.904735, 0, 0, 0.437096, 0.899415, 300, 300, 100, 1, 0, 0);
+REPLACE INTO `object_scaling` (`fullGuid`, `scale`) VALUES (17370417347219458006, 0.800000);
+REPLACE INTO `gameobject` VALUES ( 5015510, 2001789, 0, -1843.75, 1853.51, 64.3471, 0.904735, 0, 0, 0.437096, 0.899415, 300, 300, 100, 1, 0, 0);
