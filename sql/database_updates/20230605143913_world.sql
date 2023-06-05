6ae43fe02a56aa9f46670ce5b9a3da117e48ad56
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-06-05 19:01:55 +0300

    New GO for PTR added: 2020032.

---
+++ sql/database_updates/20230605143913_world.sql
@@ -0,0 +1,10 @@
+REPLACE INTO gameobject_template VALUES
+(2020032, 9, 25779, 'The Founding of Ronae\'Thalas', 0, 0, 1, 50655, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');
+
+REPLACE INTO `page_text` (`entry`, `text`, `next_page`) VALUES
+(50655, 'Eldre\'Thalas was founded, in part, as a vault for Queen Azshara\'s most prized possessions and secrets. The need for local education quickly became a necessity, but the city lacked a formal ruler capable of establishing this.', 50656),
+(50656, 'Her cousin, Tortheldrin, was tasked with ruling the city in her name, and he had the royal title of Prince bestowed upon him for such a sacred duty.', 50657),
+(50657, 'Prince Tortheldrin was aware of the need for local education, and so the construction of an academy began in the hills near Eldre\'Thalas, dedicated to all manner of subjects deemed important. The Academy itself was heralded as rivalling Nar\'Thalas near Suramar for majesty: large edifices and towers reflecting the splendor of its creators.', 50658),
+(50658, 'The Academy was named Ronae\'Thalas, meaning Land of Tranquility. It came to house a fount of power connected to the Well of Eternity, and a great library containing countless tomes.', 50659),
+(50659, 'As the population of Eldre\'Thalas increased, young nobles were taught the arcane arts within Ronae\'Thalas\' halls. To have been educated here was a renowned mark of prestige and honor.', 50660),
+(50660, 'Ronae\'Thalas has rested in the hills for over a Millenia now, and its prestige is known far and wide. Even the furthest reaches of the empire reflect our Queen\'s greatness with such wondrous places.', 0);
