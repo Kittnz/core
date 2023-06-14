d3cb37b3034c943ec9c2f918c8e9aa593220ad00
slowtorta <slowtorta@gmail.com>
2023-06-14 20:47:58 +0100

    Direct world update.

--- sql/database_updates/20230614035751_world.sql
+++ sql/database_updates/20230614035751_world.sql
@@ -10,3 +10,4 @@ DELETE FROM creature_battleground WHERE guid=2577476;
 
 -- Changes by DRAGUNOVI
 UPDATE `creature` SET `position_x` = -1426.187500, `position_y` = 1575.896484, `position_z` = 142.726318, `orientation` = 2.417884 WHERE `guid` = 2576370;
+REPLACE INTO `gameobject` VALUES ( 5015548, 2002563, 1, 7691.41, -2960.8, 465.855, 3.6313, 0, 0, 0.970173, -0.242414, 300, 300, 100, 1, 0, 0);
