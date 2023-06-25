6a3715ecc645a5a2a371febb8de7a0f1a83c5953
Letic92 <letic92@hotmail.com>
2023-06-26 00:58:54 +0200

    [Gilneas City] More changes.

--- src/scripts/dungeons/gilneas_city/boss_lord_mortimer.cpp
+++ src/scripts/dungeons/gilneas_city/boss_lord_mortimer.cpp
@@ -16,17 +16,10 @@ struct boss_lord_mortimerAI : public ScriptedAI
 
 	void Reset() override
 	{
-		m_uiHolyStrikeTimer = 10 * IN_MILLISECONDS;
-		m_uiConsecrationTimer = 15 * IN_MILLISECONDS;
+		m_uiHolyStrikeTimer = 9 * IN_MILLISECONDS;
+		m_uiConsecrationTimer = 12 * IN_MILLISECONDS;
 		m_uiBlastWaveTimer = 25 * IN_MILLISECONDS;
-		m_pInstance->SetData(PHASE_1, NOT_STARTED);
-		m_pInstance->SetData(PHASE_2, NOT_STARTED);
 		m_creature->SetDisplayId(m_creature->GetNativeDisplayId());
-	}
-
-	void Aggro(Unit* pWho) override
-	{
-		m_pInstance->SetData(PHASE_1, IN_PROGRESS);
 	}
 
 	void UpdateAI(const uint32 uiDiff) override
@@ -34,36 +27,7 @@ struct boss_lord_mortimerAI : public ScriptedAI
 		if (!m_creature->SelectHostileTarget() || !m_creature->GetVictim())
 			return;
 
-		if (m_pInstance->GetData(PHASE_2) != IN_PROGRESS && m_creature->HealthBelowPct(50))
-		{
-			m_pInstance->SetData(PHASE_1, DONE);
-			m_pInstance->SetData(PHASE_2, IN_PROGRESS);
-		}
-
-		if (m_pInstance->GetData(PHASE_1) == IN_PROGRESS)
-		{
-			if (m_uiHolyStrikeTimer <= uiDiff)
-			{
-				Unit* target = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0);
-
-				if (target && DoCastSpellIfCan(target, SPELL_HOLY_STRIKE) == CAST_OK)
-					m_uiHolyStrikeTimer = 10 * IN_MILLISECONDS;
-			}
-			else
-				m_uiHolyStrikeTimer -= uiDiff;
-
-			if (m_uiConsecrationTimer <= uiDiff)
-			{
-				Unit* target = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0);
-
-				if (target && DoCastSpellIfCan(target, SPELL_CONSECRATION) == CAST_OK)
-					m_uiConsecrationTimer = 15 * IN_MILLISECONDS;
-			}
-			else
-				m_uiConsecrationTimer -= uiDiff;
-		}
-
-		if (m_pInstance->GetData(PHASE_2) == IN_PROGRESS)
+		if (m_creature->HealthBelowPct(50))
 		{
 			if (m_creature->GetDisplayId() != MODEL_DRAGONKIN_LORD_MORTIMER)
 			{
@@ -79,6 +43,28 @@ struct boss_lord_mortimerAI : public ScriptedAI
 			}
 			else
 				m_uiBlastWaveTimer -= uiDiff;
+		}
+		else
+		{
+			if (m_uiHolyStrikeTimer <= uiDiff)
+			{
+				Unit* target = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0);
+
+				if (target && DoCastSpellIfCan(target, SPELL_HOLY_STRIKE) == CAST_OK)
+					m_uiHolyStrikeTimer = 9 * IN_MILLISECONDS;
+			}
+			else
+				m_uiHolyStrikeTimer -= uiDiff;
+
+			if (m_uiConsecrationTimer <= uiDiff)
+			{
+				Unit* target = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0);
+
+				if (target && DoCastSpellIfCan(target, SPELL_CONSECRATION) == CAST_OK)
+					m_uiConsecrationTimer = 12 * IN_MILLISECONDS;
+			}
+			else
+				m_uiConsecrationTimer -= uiDiff;
 		}
 
 		DoMeleeAttackIfReady();
