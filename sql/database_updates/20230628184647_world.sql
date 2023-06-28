87b5f8bb1a35f0e16ab357309c3f932cfd8ea0c0
Torta <66763404+slowtorta@users.noreply.github.com>
2023-06-28 20:53:48 +0200

    Fixed Mana Funnel not displaying at trainers.

---
+++ sql/database_updates/20230628184647_world.sql
@@ -0,0 +1,4 @@
+REPLACE INTO `skill_line_ability` (`id`, `skill_id`, `spell_id`, `race_mask`, `class_mask`, `req_skill_value`, `superseded_by_spell`, `learn_on_get_skill`, `max_value`, `min_value`, `req_train_points`) VALUES (36450, 354, 45910, 0, 256, 1, 0, 0, 0, 0, 0);
+
+
+
