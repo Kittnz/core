df1dfa1508f450634ab6061d2821f2385e53b275
Torta <66763404+slowtorta@users.noreply.github.com>
2023-07-01 22:47:15 +0200

    Add item ID 18023 to the following loottables with a 0.02% chance:.

--- sql/database_updates/20230701133848_world.sql
+++ sql/database_updates/20230701133848_world.sql
@@ -15,3 +15,15 @@ UPDATE `quest_template` SET `ZoneOrSort` = 331 WHERE `entry` = 40842;
 -- Add Item ID 18355 to NPC ID 14308 to a new loot group with a 14% chance to drop.
 
 REPLACE INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (14308, 18355, 14, 1, 1, 1, 0);
+
+-- Add Item ID 18023 to the following loottables with a 0.02% chance:
+-- 6006, 6004, 6005, 6009, 6007, 6008
+
+REPLACE INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES 
+
+(6004, 18023, 0.02, 5, 1, 1, 0),
+(6005, 18023, 0.02, 5, 1, 1, 0),
+(6006, 18023, 0.02, 5, 1, 1, 0),
+(6007, 18023, 0.02, 5, 1, 1, 0),
+(6008, 18023, 0.02, 5, 1, 1, 0),
+(6009, 18023, 0.02, 5, 1, 1, 0);
