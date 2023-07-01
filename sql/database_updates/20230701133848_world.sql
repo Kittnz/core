27029a21f50abd289bc1405541f5f2f59724f250
Torta <66763404+slowtorta@users.noreply.github.com>
2023-07-01 15:45:01 +0200

    Changes to NPC Moroes.

--- sql/database_updates/20230701133848_world.sql
+++ sql/database_updates/20230701133848_world.sql
@@ -1,4 +1,9 @@
 -- Increase drop chance of Item ID 81369 to 2%.
 UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = 2 WHERE `entry` = 49002 AND `item` = 81369 AND `groupid` = 2;
 
+-- NPC Moroes (Entry 61225) change faction to 35, add gossip flags
 
+-- NPC Moroes (Entry 61226) change faction to 35, add gossip flags 
+
+UPDATE `creature_template` SET `faction` = 35, `npc_flags` = 1 WHERE `entry` = 61225;
+UPDATE `creature_template` SET `faction` = 35, `npc_flags` = 1 WHERE `entry` = 61226;
