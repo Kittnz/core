2133cbe8f1e25c6d7287f6d5a9d21fdee4e62301
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-07-02 17:27:11 +0300

    NPC "Larannikus" stats fixes added, spell list added. NPC "Ralathius" gossip menu updated.

--- sql/database_updates/20230702110808_world.sql
+++ sql/database_updates/20230702110808_world.sql
@@ -2350,3 +2350,107 @@ REPLACE INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `
 -- Item 61197 add to drop table of 60748, 60747 at a 48% drop chance 
 REPLACE INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (60747, 61197, 48, 10, 1, 1, 0);
 REPLACE INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (60748, 61197, 48, 10, 1, 1, 0);
+
+-- NPC Larannikus set HP to 58319, set mana to 14230, set nature resistance to 50, set shadow resistance to 50, increase damage by 30%.
+update creature_template set health_min = 58319, health_max = 58319, mana_min = 14230, mana_max = 14230, nature_res = 50, dmg_min = 680, dmg_max = 740 where entry = 61556;
+-- NPC Larannikus should cast 26050 every 8 seconds, cast 17928 every 18 seconds, cast 25811 on a random target every 5 seconds, and cast 19463 every 12 seconds.
+-- Larannikus
+set @creature_entry = 61556;
+set @description = ': Larannikus';
+set @spell_list_id = 180208;
+
+set @spellid_1 = 26050; -- Acid Spit
+set @probability_1 = 100;
+set @casttarget_1 = 1;
+set @castflags_1 = 4;
+set @delayinitialmin_1 = 0;
+set @delayinitialmax_1 = 0;
+set @delayrepeatmin_1 = 8;
+set @delayrepeatmax_1 = 8;
+
+set @spellid_2 = 17928; -- Howl of Terror
+set @probability_2 = 100;
+set @casttarget_2 = 1;
+set @castflags_2 = 4;
+set @delayinitialmin_2 = 5;
+set @delayinitialmax_2 = 5;
+set @delayrepeatmin_2 = 18;
+set @delayrepeatmax_2 = 18;
+
+set @spellid_3 = 25811; -- Poison Bolt
+set @probability_3 = 100;
+set @casttarget_3 = 4;
+set @castflags_3 = 4;
+set @delayinitialmin_3 = 3;
+set @delayinitialmax_3 = 3;
+set @delayrepeatmin_3 = 5;
+set @delayrepeatmax_3 = 5;
+
+set @spellid_4 = 19463; -- Corrosive Acid Spit
+set @probability_4 = 100;
+set @casttarget_4 = 1;
+set @castflags_4 = 4;
+set @delayinitialmin_4 = 9;
+set @delayinitialmax_4 = 9;
+set @delayrepeatmin_4 = 12;
+set @delayrepeatmax_4 = 12;
+
+set @spellid_5 = 0;
+set @probability_5 = 0;
+set @casttarget_5 = 0;
+set @castflags_5 = 0;
+set @delayinitialmin_5 = 0;
+set @delayinitialmax_5 = 0;
+set @delayrepeatmin_5 = 0;
+set @delayrepeatmax_5 = 0;
+
+set @spellid_6 = 0;
+set @probability_6 = 0;
+set @casttarget_6 = 0;
+set @castflags_6 = 0;
+set @delayinitialmin_6 = 0;
+set @delayinitialmax_6 = 0;
+set @delayrepeatmin_6 = 0;
+set @delayrepeatmax_6 = 0;
+
+set @spellid_7 = 0;
+set @probability_7 = 0;
+set @casttarget_7 = 0;
+set @castflags_7 = 0;
+set @delayinitialmin_7 = 0;
+set @delayinitialmax_7 = 0;
+set @delayrepeatmin_7 = 0;
+set @delayrepeatmax_7 = 0;
+
+set @spellid_8 = 0;
+set @probability_8 = 0;
+set @casttarget_8 = 0;
+set @castflags_8 = 0;
+set @delayinitialmin_8 = 0;
+set @delayinitialmax_8 = 0;
+set @delayrepeatmin_8 = 0;
+set @delayrepeatmax_8 = 0;
+
+-- Do not touch this part:
+update creature_template set spell_list_id = @spell_list_id, ai_name = '', script_name = '', spell_id1 = 0, spell_id2 = 0, spell_id3 = 0 
+where entry = @creature_entry;
+replace into creature_spells (entry, name, 
+spellid_1, probability_1, casttarget_1, castflags_1, delayinitialmin_1, delayinitialmax_1, delayrepeatmin_1, delayrepeatmax_1, 
+spellid_2, probability_2, casttarget_2, castflags_2, delayinitialmin_2, delayinitialmax_2, delayrepeatmin_2, delayrepeatmax_2, 
+spellid_3, probability_3, casttarget_3, castflags_3, delayinitialmin_3, delayinitialmax_3, delayrepeatmin_3, delayrepeatmax_3, 
+spellid_4, probability_4, casttarget_4, castflags_4, delayinitialmin_4, delayinitialmax_4, delayrepeatmin_4, delayrepeatmax_4, 
+spellid_5, probability_5, casttarget_5, castflags_5, delayinitialmin_5, delayinitialmax_5, delayrepeatmin_5, delayrepeatmax_5, 
+spellid_6, probability_6, casttarget_6, castflags_6, delayinitialmin_6, delayinitialmax_6, delayrepeatmin_6, delayrepeatmax_6, 
+spellid_7, probability_7, casttarget_7, castflags_7, delayinitialmin_7, delayinitialmax_7, delayrepeatmin_7, delayrepeatmax_7, 
+spellid_8, probability_8, casttarget_8, castflags_8, delayinitialmin_8, delayinitialmax_8, delayrepeatmin_8, delayrepeatmax_8) 
+values (@spell_list_id, @description,
+@spellid_1, @probability_1, @casttarget_1, @castflags_1, @delayinitialmin_1, @delayinitialmax_1, @delayrepeatmin_1, @delayrepeatmax_1,
+@spellid_2, @probability_2, @casttarget_2, @castflags_2, @delayinitialmin_2, @delayinitialmax_2, @delayrepeatmin_2, @delayrepeatmax_2,
+@spellid_3, @probability_3, @casttarget_3, @castflags_3, @delayinitialmin_3, @delayinitialmax_3, @delayrepeatmin_3, @delayrepeatmax_3,
+@spellid_4, @probability_4, @casttarget_4, @castflags_4, @delayinitialmin_4, @delayinitialmax_4, @delayrepeatmin_4, @delayrepeatmax_4,
+@spellid_5, @probability_5, @casttarget_5, @castflags_5, @delayinitialmin_5, @delayinitialmax_5, @delayrepeatmin_5, @delayrepeatmax_5,
+@spellid_6, @probability_6, @casttarget_6, @castflags_6, @delayinitialmin_6, @delayinitialmax_6, @delayrepeatmin_6, @delayrepeatmax_6,
+@spellid_7, @probability_7, @casttarget_7, @castflags_7, @delayinitialmin_7, @delayinitialmax_7, @delayrepeatmin_7, @delayrepeatmax_7,
+@spellid_8, @probability_8, @casttarget_8, @castflags_8, @delayinitialmin_8, @delayinitialmax_8, @delayrepeatmin_8, @delayrepeatmax_8);
+-- NPC Ralathius, change greeting text to: "Hidden beneath the veil, a great shadow lurks within the dream. Already many of my kind have been disturbed by the presence.$B$BYou may not know this, but the awakening has begun.$B$BAn ancient ritual of my kin intended to draw us from the dream. I have resisted its temptations already, but not all are willed so strong."
+update broadcast_text set male_text = 'Hidden beneath the veil, a great shadow lurks within the dream. Already many of my kind have been disturbed by the presence.$B$BYou may not know this, but the awakening has begun.$B$BAn ancient ritual of my kin intended to draw us from the dream. I have resisted its temptations already, but not all are willed so strong.' where entry = 61326;
