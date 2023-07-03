52bca7bf9bf1fc6ee95d4b43e26373db7f90426b
Torta <66763404+slowtorta@users.noreply.github.com>
2023-07-03 11:28:45 +0200

    Spell fixes from Moon.

---
+++ sql/database_updates/20230703092744_world.sql
@@ -0,0 +1,14 @@
+UPDATE `spell_template` SET `description` = 'Permanently increase the intellect by 8 and spell power by 6 of an item worn on the chest, legs, hands or feet.  Only usable on items level 60 and above.' WHERE `entry` = 57115;
+UPDATE `spell_template` SET `name` = 'Intellect +8, Spell Power +6' WHERE `entry` = 57136;
+UPDATE `spell_template` SET `equippedItemSubClassMask` = 30 WHERE `entry` = 57127;
+UPDATE `spell_template` SET `equippedItemSubClassMask` = 30 WHERE `entry` = 57142;
+UPDATE `spell_template` SET `equippedItemSubClassMask` = 30 WHERE `entry` = 57148;
+UPDATE `spell_template` SET `equippedItemSubClassMask` = 30 WHERE `entry` = 57036;
+UPDATE `spell_template` SET `equippedItemSubClassMask` = 30 WHERE `entry` = 57034;
+UPDATE `spell_template` SET `equippedItemSubClassMask` = 30 WHERE `entry` = 57032;
+UPDATE `spell_template` SET `equippedItemInventoryTypeMask` = 256 WHERE `entry` = 57127;
+UPDATE `spell_template` SET `equippedItemInventoryTypeMask` = 256 WHERE `entry` = 57142;
+UPDATE `spell_template` SET `equippedItemInventoryTypeMask` = 256 WHERE `entry` = 57148;
+UPDATE `spell_template` SET `name` = 'Enchant Chest - Mighty Mana' WHERE `entry` = 57119;
+
+
