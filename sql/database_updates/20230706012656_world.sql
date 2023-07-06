4c62f19d8be53d528036780c1d5dc148036a624e
slowtorta <slowtorta@gmail.com>
2023-07-06 12:52:16 +0100

    Direct world update.

---
+++ sql/database_updates/20230706012656_world.sql
@@ -0,0 +1,8 @@
+
+-- Changes by VOJI
+DELETE FROM creature WHERE guid=2568918;
+DELETE FROM creature_addon WHERE guid=2568918;
+DELETE FROM creature_movement WHERE id=2568918;
+DELETE FROM game_event_creature WHERE guid=2568918;
+DELETE FROM game_event_creature_data WHERE guid=2568918;
+DELETE FROM creature_battleground WHERE guid=2568918;
