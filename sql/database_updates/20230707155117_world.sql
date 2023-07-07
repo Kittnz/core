6324e29551c8fd6a94b64524e414ddf69904b7b4
slowtorta <slowtorta@gmail.com>
2023-07-07 17:52:40 +0100

    Direct world update.

--- sql/database_updates/20230707155117_world.sql
+++ sql/database_updates/20230707155117_world.sql
@@ -1,3 +1,9 @@
 
 -- Changes by SHANG
 REPLACE INTO `creature` VALUES (2577972,14750,0,0,0,1,-6889.05,-4797.28,9.57414,5.83707,120,120,5,100,100,1,0,0);
+DELETE FROM creature WHERE guid=2577972;
+DELETE FROM creature_addon WHERE guid=2577972;
+DELETE FROM creature_movement WHERE id=2577972;
+DELETE FROM game_event_creature WHERE guid=2577972;
+DELETE FROM game_event_creature_data WHERE guid=2577972;
+DELETE FROM creature_battleground WHERE guid=2577972;
