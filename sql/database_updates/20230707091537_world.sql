d62834d012ac5b978b160a85484511fbafc3cf30
Torta <66763404+slowtorta@users.noreply.github.com>
2023-07-08 08:01:09 +0200

    Jami made me do that

--- sql/database_updates/20230707091537_world.sql
+++ sql/database_updates/20230707091537_world.sql
@@ -1,4 +1,9 @@
 UPDATE`creature_template` SET `gold_min` = 382, `gold_max` = 504 WHERE `entry` = 13956;
 UPDATE`creature_template` SET `gold_min` = 382, `gold_max` = 504 WHERE `entry` = 10983;
 
+UPDATE `creature_template` SET `gold_min` = 152, `gold_max` = 761 WHERE `entry` = 13956;
+UPDATE `creature_template` SET `gold_min` = 162, `gold_max` = 812 WHERE `entry` = 13957;
+UPDATE `creature_template` SET `gold_min` = 157, `gold_max` = 786 WHERE `entry` = 13958;
+
+UPDATE `creature_template` SET `gold_min` = 178, `gold_max` = 891 WHERE `entry` = 12159;
 
