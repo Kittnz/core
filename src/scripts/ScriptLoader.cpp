3616cf99af02c76ba33b5a5d3c5bd24bdd0df1d7
Letic92 <letic92@hotmail.com>
2023-07-08 21:39:22 +0200

    Fixed compile

--- src/scripts/ScriptLoader.cpp
+++ src/scripts/ScriptLoader.cpp
@@ -312,6 +312,7 @@ void AddSC_black_morass_trash();
 void AddSC_instance_black_morass();
 void AddSC_boss_gerastrasz();
 void AddSC_boss_chronormu();
+void AddSC_boss_chromie();
 
 // Misc
 void AddSC_npc_ptr();
@@ -625,6 +626,7 @@ void AddScripts()
     AddSC_instance_black_morass();
     AddSC_boss_gerastrasz();
     AddSC_boss_chronormu();
+    AddSC_boss_chromie();
 
     // Misc
     AddSC_npc_ptr();
