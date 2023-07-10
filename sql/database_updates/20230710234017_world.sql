5280ac69936c4a89b12fe4bacabe566e8ab6beb1
Torta <66763404+slowtorta@users.noreply.github.com>
2023-07-11 01:41:46 +0200

    Shadeflayer Tribal Bracelet (61707) should drop w/o the quest too.

---
+++ sql/database_updates/20230710234017_world.sql
@@ -0,0 +1,8 @@
+-- Shadeflayer Tribal Bracelet (61707) should drop w/o the quest too:
+
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = 10.4 WHERE `entry` = 61356;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = 10.4 WHERE `entry` = 61357;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = 10.4 WHERE `entry` = 61358;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = 10.4 WHERE `entry` = 61359;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = 6.3 WHERE  `entry` = 61367;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = 100 WHERE  `entry` = 61533;
