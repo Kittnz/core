90e2aa51b58363129f5afc47940460c0ff3f46de
slowtorta <slowtorta@gmail.com>
2023-07-12 00:33:19 +0100

    Direct world update.

---
+++ sql/database_updates/20230711235122_world.sql
@@ -0,0 +1,5 @@
+
+-- Changes by SHANG
+DELETE FROM gameobject WHERE guid = '5015548';
+DELETE FROM game_event_gameobject WHERE guid = '5015548';
+DELETE FROM gameobject_battleground WHERE guid = '5015548';
