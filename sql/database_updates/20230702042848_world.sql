0f08b96dfe94ce8b527a8e68512406fca79bbb27
ratkosrb <ratkomladic2@abv.bg>
2023-07-11 20:27:21 +0300

    Nerf Shard of the Fallen Star scaling.
    Requested by Dragunovi.

--- sql/database_updates/20230702042848_world.sql
+++ sql/database_updates/20230702042848_world.sql
@@ -1,3 +1,3 @@
 -- Make Shard of the Fallen Star scale with spell power.
-UPDATE `spell_template` SET `effectBonusCoefficient1`=1 WHERE `entry`=26789;
+UPDATE `spell_template` SET `effectBonusCoefficient1`=0.25 WHERE `entry`=26789;
 
