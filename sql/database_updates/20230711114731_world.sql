a50f6045e68b2ad093bcc43775ebc10e7b68e366
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-07-11 17:13:07 +0300

    Item fixes for ptr.

--- sql/database_updates/20230711114731_world.sql
+++ sql/database_updates/20230711114731_world.sql
@@ -18,3 +18,14 @@ replace into item_template values
  '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0',
  '-1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
  '0', '1', NULL);
+-- Item The Upper Binding of Xanthar, make item BOP.
+update item_template set bonding = 1 where entry = 61696;
+-- Item The Lower Binding of Xanthar, make item into a quest item.
+update item_template set bonding = 4 where entry = 61697;
+-- Item Premium Chocolate (61173) add spell 1129 , make usable with -1 charges so the item is consumed upon use.
+update item_template set spellid_1 = 1129, spellcharges_1 = -1, spellcooldown_1 = 0, spellcategory_1 = 11, spellcategorycooldown_1 = 1000 where entry = 61173;
+-- Item Glyph of the Emerald Bear (Entry 51360), change to blue rarity (rare).
+update item_template set quality = 3 where entry = 51360;
+-- Item Dreamwind Sigil change display ID to 31576.
+update item_template set display_id = 31576 where entry = 61705;
+
