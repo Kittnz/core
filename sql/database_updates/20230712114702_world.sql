548bbdcb9d81636dead15e7a239a66924fbef7fd
Torta <66763404+slowtorta@users.noreply.github.com>
2023-07-12 13:48:38 +0200

    New display IDs for GM stuff.

---
+++ sql/database_updates/20230712114702_world.sql
@@ -0,0 +1,3 @@
+UPDATE `item_template` SET `display_id` =  67804 WHERE `entry` = 12064;
+UPDATE `item_template` SET `display_id` = 67805 WHERE `entry` = 2586;
+UPDATE `item_template` SET `display_id` = 67806 WHERE `entry` = 11508;
