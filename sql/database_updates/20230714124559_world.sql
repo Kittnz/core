177c93779de28b41589fcb251ab2a68a251a735b
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-07-14 16:06:42 +0300

    New quest for PTR: "Stagwood Grasp" (41058).

--- sql/database_updates/20230714124559_world.sql
+++ sql/database_updates/20230714124559_world.sql
@@ -42,3 +42,20 @@ replace into item_template values
  '-1', '1', '0', '0', '0', '0', '0', '2', '0', '0', '0', '0', '0', '0', '0', '0', '65', '0', '0', '0',
  '0', '1', NULL);
 
+-- Stagwood Grasp
+delete from quest_template where entry = 41058;
+replace into quest_template (prevquestid, entry, method, zoneorsort, questlevel, minlevel, questflags, specialflags, title, details, objectives, requestitemstext, offerrewardtext, reqitemid1, reqitemcount1, reqitemid2, reqitemcount2, reqitemid3, reqitemcount3, reqitemid4, reqitemcount4, reqcreatureorgoid1, reqcreatureorgocount1, reqcreatureorgoid2, reqcreatureorgocount2, reqcreatureorgoid3, reqcreatureorgocount3, reqcreatureorgoid4, reqcreatureorgocount4, srcitemid, srcitemcount, reworreqmoney, RewMoneyMaxLevel, rewxp, rewrepfaction1, rewrepvalue1,  rewrepfaction2, rewrepvalue2, rewrepfaction3, rewrepvalue3, rewrepfaction4, rewrepvalue4, rewspell, rewspellcast, completeemote, rewitemid1, rewitemcount1, rewitemid2, rewitemcount2, rewitemid3, rewitemcount3, rewitemid4, rewitemcount4, rewchoiceitemid1, rewchoiceitemcount1, rewchoiceitemid2, rewchoiceitemcount2, rewchoiceitemid3, rewchoiceitemcount3, rewchoiceitemid4, rewchoiceitemcount4,requiredminrepfaction,requiredminrepvalue,objectivetext1) values (0,41058,2,616,60,55,0,0,'Stagwood Grasp','Stored within the armory are many weapons, armor and great relics of old. These items are extremely powerful, and laden with intense enchantments. I have been authorized to offer such items to those who have proved themselves as worthy allies here in Hyjal.$B$BIf you desire the Stagwood Grasp, bring me one hundred and fifty Bright Dream Shards, and it shall be yours.','Gather 150 Bright Dream Shards for Elvanor Heartsong at Nordanaar in Hyjal.','Have you brought the Dream Shards adventurer?','May this relic from our armory serve you well, and assist you in whatever battles may yet come.',61199,150,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0, 0,0,250,0,0,0,0,0,0,0,0,0,0,0, 61755,1,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,'');
+
+replace into creature_questrelation		(id, quest) values (61607, 41058);
+replace into creature_involvedrelation	(id, quest) values (61607, 41058);
+
+replace into item_template values
+ ('61755', '2', '13', 'Stagwood Grasp', '', '67801', '4', '0', '1', '286372', '71593', '22', '-1', '-1', '64',
+ '59', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '4', '8', '0', '0',
+ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1700', '0',
+ '0', '45', '94', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+ '0', '0', '0', '45420', '1', '0', '0', '-1', '0', '-1', '8815', '1', '0', '0', '-1', '0', '-1', '0', '0',
+ '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0',
+ '-1', '1', '0', '0', '0', '0', '0', '2', '0', '0', '0', '75', '0', '0', '0', '0', '65', '0', '0', '0',
+ '0', '1', NULL);
+
