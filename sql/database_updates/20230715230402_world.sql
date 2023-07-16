0900466536370abdc62f2d00eeee3c3b6e40ad85
Torta <66763404+slowtorta@users.noreply.github.com>
2023-07-16 14:20:15 +0300

    Update 20230715230402_world.sql

--- sql/database_updates/20230715230402_world.sql
+++ sql/database_updates/20230715230402_world.sql
@@ -3,4 +3,4 @@ UPDATE `creature_template` SET `ai_name` = '' , `script_name` = 'grellkin_primal
 UPDATE `creature_template` SET `ai_name` = '' , `script_name` = 'grellkin_channeler' WHERE `entry` = '61197';
 UPDATE `creature_template` SET `ai_name` = '' , `script_name` = 'shadowbane_ambusher' WHERE `entry` = '61193';
 UPDATE `creature_template` SET `ai_name` = '' , `script_name` = 'boss_brood_queen_araxxna' WHERE `entry` = '61221';
---UPDATE `creature_template` SET `ai_name` = '' , `script_name` = 'skitterweb_egg' WHERE `entry` = '61205';
+-- UPDATE `creature_template` SET `ai_name` = '' , `script_name` = 'skitterweb_egg' WHERE `entry` = '61205';
