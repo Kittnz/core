dbd37e7de9c702c9dc5107ad2fc602c88d41d90e
slowtorta <slowtorta@gmail.com>
2023-07-19 03:05:50 +0100

    Direct world update.

--- sql/database_updates/20230719020649_world.sql
+++ sql/database_updates/20230719020649_world.sql
@@ -1,3 +1,17 @@
 
 -- Changes by SHANG
 UPDATE `creature` SET `position_x` = 4298.164062, `position_y` = -2822.463135, `position_z` = 1091.191895, `orientation` = 2.156186 WHERE `guid` = 2578189;
+REPLACE INTO `creature` VALUES (2578338,61362,0,0,0,1,5188.9,-3345.04,1639.16,0.852152,120,120,0,100,100,0,0,0);
+DELETE FROM creature WHERE guid=2578338;
+DELETE FROM creature_addon WHERE guid=2578338;
+DELETE FROM creature_movement WHERE id=2578338;
+DELETE FROM game_event_creature WHERE guid=2578338;
+DELETE FROM game_event_creature_data WHERE guid=2578338;
+DELETE FROM creature_battleground WHERE guid=2578338;
+REPLACE INTO `creature` VALUES (2578339,61360,0,0,0,1,5188.9,-3345.04,1639.16,0.852152,120,120,0,100,100,0,0,0);
+DELETE FROM creature WHERE guid=2578339;
+DELETE FROM creature_addon WHERE guid=2578339;
+DELETE FROM creature_movement WHERE id=2578339;
+DELETE FROM game_event_creature WHERE guid=2578339;
+DELETE FROM game_event_creature_data WHERE guid=2578339;
+DELETE FROM creature_battleground WHERE guid=2578339;
