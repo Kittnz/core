1191f91391e68951a41cfb66fb000ae818c8b2fb
Torta <66763404+slowtorta@users.noreply.github.com>
2023-07-24 10:33:41 +0200

    Updated Tel'abim quest objectives as requested by Dragu.

---
+++ sql/database_updates/20230724082250_world.sql
@@ -0,0 +1,5 @@
+-- https://database.turtle-wow.org/?quest=40753 from mountain silversage to Icecap
+-- https://database.turtle-wow.org/?quest=40754 from essence of air to Golden Sansam 
+
+UPDATE `quest_template` SET `ReqItemId2` = 13467 WHERE `entry` = 40753;
+UPDATE `quest_template` SET `ReqItemId2` = 13464 WHERE `entry` = 40754;
