f16bfa3aa77dd2f9bff6cf9216311f0ef67d40ea
Torta <66763404+slowtorta@users.noreply.github.com>
2023-07-24 11:03:56 +0200

    Karazhan Halls must be a raid type map.

--- sql/database_updates/20230724082250_world.sql
+++ sql/database_updates/20230724082250_world.sql
@@ -3,3 +3,7 @@
 
 UPDATE `quest_template` SET `ReqItemId2` = 13467 WHERE `entry` = 40753;
 UPDATE `quest_template` SET `ReqItemId2` = 13464 WHERE `entry` = 40754;
+
+-- Kara, require raid group to enter, limit to 10 people, make sure bosses trigger a lockout with same length as onyxia
+
+UPDATE `map_template` SET `map_type` = 2, `reset_delay` = 7 WHERE `entry` = 532;
