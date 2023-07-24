b26e02ca22ef0f1be24e5abc1df208b36b3f66bf
Torta <66763404+slowtorta@users.noreply.github.com>
2023-07-24 11:19:09 +0200

    Fixed typo in the aura description of Alpha Presence.

--- sql/database_updates/20230724082250_world.sql
+++ sql/database_updates/20230724082250_world.sql
@@ -7,3 +7,9 @@ UPDATE `quest_template` SET `ReqItemId2` = 13464 WHERE `entry` = 40754;
 -- Kara, require raid group to enter, limit to 10 people, make sure bosses trigger a lockout with same length as onyxia
 
 UPDATE `map_template` SET `map_type` = 2, `reset_delay` = 7 WHERE `entry` = 532;
+
+-- Change GUID 5016268, 5016269 to have a respawn timer of 5 seconds.
+
+UPDATE `gameobject` SET `spawntimesecsmin` = 5, `spawntimesecsmax` = 5 WHERE `guid` IN (5016268, 5016269);
+
+UPDATE `spell_template` SET `auraDescription` = 'Friendly units within a $a1 yard radius attack $s1% faster.' WHERE `entry` = 57078;
