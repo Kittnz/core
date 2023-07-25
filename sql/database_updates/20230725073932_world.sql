675cdbb142347ac31bdc3b30b469e180ea9fee3d
slowtorta <slowtorta@gmail.com>
2023-07-25 09:45:36 +0100

    Direct world update.

--- sql/database_updates/20230725073932_world.sql
+++ sql/database_updates/20230725073932_world.sql
@@ -46,3 +46,6 @@ DELETE FROM creature_movement WHERE id=2578371;
 DELETE FROM game_event_creature WHERE guid=2578371;
 DELETE FROM game_event_creature_data WHERE guid=2578371;
 DELETE FROM creature_battleground WHERE guid=2578371;
+
+-- Changes by TAMMO
+REPLACE INTO `creature` VALUES (2578372,70006,0,0,0,0,-9628.49,-314.968,57.9089,0.855445,120,120,0,100,100,0,0,0);
