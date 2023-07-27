2cf3407b51b3e2353a0d7792fa3a525463cf6a16
Torta <66763404+slowtorta@users.noreply.github.com>
2023-07-27 17:50:22 +0200

    New enchants must be trade goods.

--- sql/database_updates/20230727153451_world.sql
+++ sql/database_updates/20230727153451_world.sql
@@ -9,3 +9,23 @@ REPLACE INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`) VALUE
 DELETE FROM `playercreateinfo_spell` WHERE `race` = 9 AND `class` = 3 AND `spell` =  264;
 
 REPLACE INTO `playercreateinfo_spell` (`race`, `class`, `spell`, `note`) VALUES (9, 3, 266, 'Guns');
+
+-- The following items need to be labeled as 'Trade Goods' you can steal this tag from the item Mithril Spurs (entry 7969) if you need a reference.
+
+-- Copper Buckle (61779)
+-- Bronze Buckle (61780)
+-- Iron Buckle (61781)
+-- Mithril Buckle (61782)
+-- Thorium Buckle (61783)
+-- Arcanite Buckle (61784)
+-- Dreamsteel Buckle (61785)
+-- Thorium Spurs (61182) 
+
+UPDATE `item_template` SET `class` = 7 WHERE `entry` = 61779;
+UPDATE `item_template` SET `class` = 7 WHERE `entry` = 61780;
+UPDATE `item_template` SET `class` = 7 WHERE `entry` = 61781;
+UPDATE `item_template` SET `class` = 7 WHERE `entry` = 61782;
+UPDATE `item_template` SET `class` = 7 WHERE `entry` = 61783;
+UPDATE `item_template` SET `class` = 7 WHERE `entry` = 61784;
+UPDATE `item_template` SET `class` = 7 WHERE `entry` = 61785;
+UPDATE `item_template` SET `class` = 7 WHERE `entry` = 61182;
