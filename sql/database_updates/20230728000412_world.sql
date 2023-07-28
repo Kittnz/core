535ab1a6a61d90ab231c801949bac439eeba1a3f
Torta <66763404+slowtorta@users.noreply.github.com>
2023-07-28 02:05:31 +0200

    Spell description fixes.

---
+++ sql/database_updates/20230728000412_world.sql
@@ -0,0 +1,22 @@
+UPDATE `spell_template` SET `description` = 'Targets in a $a1 yard cone in front of the caster take $s2 Frost damage and are slowed by $s1% for $d.' WHERE `entry` = 10160;
+UPDATE `spell_template` SET `description` = 'Targets in a $a1 yard cone in front of the caster take $s2 Frost damage and are slowed by $s1% for $d.' WHERE `entry` = 10161;
+UPDATE `spell_template` SET `description` = 'Targets in a $a1 yard cone in front of the caster take $s2 Frost damage and are slowed by $s1% for $d.' WHERE `entry` = 10159;
+UPDATE `spell_template` SET `description` = 'Targets in a $a1 yard cone in front of the caster take $s2 Frost damage and are slowed by $s1% for $d.' WHERE `entry` = 8492;
+UPDATE `spell_template` SET `description` = 'Targets in a $a1 yard cone in front of the caster take $s2 Frost damage and are slowed by $s1% for $d.' WHERE `entry` = 120;
+
+UPDATE `spell_template` SET `description` = 'Blasts enemies within $a1 yards near the caster for $s1 Frost damage and freezes them in place for up to $d.  Damage caused may interrupt the effect.' WHERE `entry` = 122;
+UPDATE `spell_template` SET `description` = 'Blasts enemies within $a1 yards near the caster for $s1 Frost damage and freezes them in place for up to $d.  Damage caused may interrupt the effect.' WHERE `entry` = 865;
+UPDATE `spell_template` SET `description` = 'Blasts enemies within $a1 yards near the caster for $s1 Frost damage and freezes them in place for up to $d.  Damage caused may interrupt the effect.' WHERE `entry` = 6131;
+UPDATE `spell_template` SET `description` = 'Blasts enemies within $a1 yards near the caster for $s1 Frost damage and freezes them in place for up to $d.  Damage caused may interrupt the effect.' WHERE `entry` = 10230;
+
+UPDATE `spell_template` SET `description` = 'A wave of flame radiates outward from the caster, damaging all enemies caught within the $a1 yard blast for $s1 Fire damage, and dazing them for $d.' WHERE `entry` = 11113;
+UPDATE `spell_template` SET `description` = 'A wave of flame radiates outward from the caster, damaging all enemies caught within the $a1 yard blast for $s1 Fire damage, and dazing them for $d.' WHERE `entry` = 13018;
+UPDATE `spell_template` SET `description` = 'A wave of flame radiates outward from the caster, damaging all enemies caught within the $a1 yard blast for $s1 Fire damage, and dazing them for $d.' WHERE `entry` = 13019;
+UPDATE `spell_template` SET `description` = 'A wave of flame radiates outward from the caster, damaging all enemies caught within the $a1 yard blast for $s1 Fire damage, and dazing them for $d.' WHERE `entry` = 13020;
+UPDATE `spell_template` SET `description` = 'A wave of flame radiates outward from the caster, damaging all enemies caught within the $a1 yard blast for $s1 Fire damage, and dazing them for $d.' WHERE `entry` = 13021;
+
+UPDATE `spell_template` SET `description` = 'Ignites the area surrounding the caster, causing $s2 Fire damage to $ghimself:herself; and $5857s1 Fire damage to all nearby enemies within $s1 yards every $t2 sec.  Lasts $d.' WHERE `entry` = 1949;
+UPDATE `spell_template` SET `description` = 'Ignites the area surrounding the caster, causing $s2 Fire damage to $ghimself:herself; and $5857s1 Fire damage to all nearby enemies within $s1 yards every $t2 sec.  Lasts $d.' WHERE `entry` = 11683;
+UPDATE `spell_template` SET `description` = 'Ignites the area surrounding the caster, causing $s2 Fire damage to $ghimself:herself; and $5857s1 Fire damage to all nearby enemies within $s1 yards every $t2 sec.  Lasts $d.' WHERE `entry` = 11684;
+
+
