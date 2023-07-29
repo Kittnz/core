1668b3dda8c735a3ca1b027083659ff30c0b63d9
Torta <66763404+slowtorta@users.noreply.github.com>
2023-07-29 12:58:41 +0200

    Blood Ring things.
    @ratkosrb Here we go...

--- src/game/Battlegrounds/BattleGround.cpp
+++ src/game/Battlegrounds/BattleGround.cpp
@@ -746,7 +746,7 @@ void BattleGround::EndBattleGround(Team winner)
         BlockMovement(pPlayer);
 
         // Complete Victory on Blood Ring quest:
-        if (bgTypeID == BATTLEGROUND_BR && pPlayer->GetQuestStatus(41110) == QUEST_STATUS_INCOMPLETE)
+        if (bgTypeID == BATTLEGROUND_BR && pPlayer->GetQuestStatus(41110) == QUEST_STATUS_INCOMPLETE && winner)
         {
            if (CreatureInfo const* cInfo = ObjectMgr::GetCreatureTemplate(60051))
                 pPlayer->ToPlayer()->KilledMonster(cInfo, ObjectGuid());
