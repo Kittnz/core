1ef08b0650aef93bc00fcb68fbc1d53d0df395d6
slowtorta <slowtorta@gmail.com>
2023-07-30 10:21:26 +0100

    Direct world update.

--- sql/database_updates/20230729221654_world.sql
+++ sql/database_updates/20230729221654_world.sql
@@ -1,3 +1,12 @@
 
 -- Changes by TAMMO
 UPDATE creature_template SET faction = '104' WHERE entry = '10050';
+UPDATE creature_template SET faction = '104' WHERE entry = '2947';
+UPDATE creature_template SET faction = '104' WHERE entry = '2987';
+UPDATE `creature` SET `position_x` = -1242.390991, `position_y` = 56.022457, `position_z` = 127.017990, `orientation` = 1.562783 WHERE `guid` = 2560613;
+UPDATE creature_template SET faction = '104' WHERE entry = '10054';
+UPDATE creature_template SET faction = '104' WHERE entry = '11500';
+UPDATE creature_template SET faction = '104' WHERE entry = '6410';
+UPDATE creature_template SET faction = '104' WHERE entry = '5770';
+UPDATE creature_template SET faction = '104' WHERE entry = '4875';
+UPDATE creature_template SET faction = '104' WHERE entry = '7777';
