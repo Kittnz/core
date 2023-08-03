bb8bb415671e897b62c04af1d5d9c7905c3d9544
slowtorta <slowtorta@gmail.com>
2023-08-03 03:54:05 +0100

    Direct world update.

--- sql/database_updates/20230802202038_world.sql
+++ sql/database_updates/20230802202038_world.sql
@@ -1,3 +1,4 @@
 
 -- Changes by SHANG
 UPDATE `creature` SET `position_x` = 4297.952637, `position_y` = -2825.840332, `position_z` = 1091.199585, `orientation` = 1.353590 WHERE `guid` = 2578189;
+UPDATE `creature` SET `position_x` = -5108.376953, `position_y` = 406.655151, `position_z` = 400.951630, `orientation` = 4.587507 WHERE `guid` = 2413;
