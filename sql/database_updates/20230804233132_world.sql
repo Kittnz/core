566422368771e2501c521d67c0356dd99f345a3c
slowtorta <slowtorta@gmail.com>
2023-08-05 13:49:25 +0100

    Direct world update.

--- sql/database_updates/20230804233132_world.sql
+++ sql/database_updates/20230804233132_world.sql
@@ -1,3 +1,4 @@
 
 -- Changes by TAMMO
 REPLACE INTO `creature` VALUES (2578574,6015,0,0,0,451,4520.1,-188.166,268.231,5.28538,120,120,0,100,100,0,0,0);
+UPDATE `creature` SET `position_x` = 4519.444824, `position_y` = -188.878021, `position_z` = 268.185394, `orientation` = 5.411837 WHERE `guid` = 2578574;
