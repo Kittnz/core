04bd8501ed5674697603feeb6f59cc75ebf0b83d
slowtorta <slowtorta@gmail.com>
2023-08-06 13:13:25 +0100

    Direct world update.

--- sql/database_updates/20230806112750_world.sql
+++ sql/database_updates/20230806112750_world.sql
@@ -25,3 +25,55 @@ REPLACE INTO `object_scaling` (`fullGuid`, `scale`) VALUES (17370417400604560398
 REPLACE INTO `gameobject` VALUES ( 5016590, 2004971, 1, 6786.62, -4660.41, 725.027, 3.82432, 0, 0, 0.942299, -0.334773, 300, 300, 100, 1, 0, 0);
 REPLACE INTO `object_scaling` (`fullGuid`, `scale`) VALUES (17370417400604560398, 0.640000);
 REPLACE INTO `gameobject` VALUES ( 5016590, 2004971, 1, 6786.62, -4660.41, 725.027, 3.82432, 0, 0, 0.942299, -0.334773, 300, 300, 100, 1, 0, 0);
+
+-- Changes by VOJI
+DELETE FROM gameobject WHERE guid = '5013773';
+DELETE FROM game_event_gameobject WHERE guid = '5013773';
+DELETE FROM gameobject_battleground WHERE guid = '5013773';
+DELETE FROM gameobject WHERE guid = '5015031';
+DELETE FROM game_event_gameobject WHERE guid = '5015031';
+DELETE FROM gameobject_battleground WHERE guid = '5015031';
+DELETE FROM gameobject WHERE guid = '5014918';
+DELETE FROM game_event_gameobject WHERE guid = '5014918';
+DELETE FROM gameobject_battleground WHERE guid = '5014918';
+DELETE FROM gameobject WHERE guid = '5014105';
+DELETE FROM game_event_gameobject WHERE guid = '5014105';
+DELETE FROM gameobject_battleground WHERE guid = '5014105';
+DELETE FROM gameobject WHERE guid = '5013889';
+DELETE FROM game_event_gameobject WHERE guid = '5013889';
+DELETE FROM gameobject_battleground WHERE guid = '5013889';
+
+-- Changes by TAMMO
+REPLACE INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (901,-4339.236328,1619.993896,-41.898792,0.034997,817,'barrowdeeps');
+
+-- Changes by VOJI
+DELETE FROM gameobject WHERE guid = '5014919';
+DELETE FROM game_event_gameobject WHERE guid = '5014919';
+DELETE FROM gameobject_battleground WHERE guid = '5014919';
+DELETE FROM gameobject WHERE guid = '5015212';
+DELETE FROM game_event_gameobject WHERE guid = '5015212';
+DELETE FROM gameobject_battleground WHERE guid = '5015212';
+DELETE FROM gameobject WHERE guid = '5015213';
+DELETE FROM game_event_gameobject WHERE guid = '5015213';
+DELETE FROM gameobject_battleground WHERE guid = '5015213';
+DELETE FROM gameobject WHERE guid = '5015226';
+DELETE FROM game_event_gameobject WHERE guid = '5015226';
+DELETE FROM gameobject_battleground WHERE guid = '5015226';
+DELETE FROM gameobject WHERE guid = '5015225';
+DELETE FROM game_event_gameobject WHERE guid = '5015225';
+DELETE FROM gameobject_battleground WHERE guid = '5015225';
+DELETE FROM gameobject WHERE guid = '5015223';
+DELETE FROM game_event_gameobject WHERE guid = '5015223';
+DELETE FROM gameobject_battleground WHERE guid = '5015223';
+DELETE FROM gameobject WHERE guid = '5015224';
+DELETE FROM game_event_gameobject WHERE guid = '5015224';
+DELETE FROM gameobject_battleground WHERE guid = '5015224';
+DELETE FROM gameobject WHERE guid = '5015222';
+DELETE FROM game_event_gameobject WHERE guid = '5015222';
+DELETE FROM gameobject_battleground WHERE guid = '5015222';
+DELETE FROM gameobject WHERE guid = '5015220';
+DELETE FROM game_event_gameobject WHERE guid = '5015220';
+DELETE FROM gameobject_battleground WHERE guid = '5015220';
+DELETE FROM gameobject WHERE guid = '5015221';
+DELETE FROM game_event_gameobject WHERE guid = '5015221';
+DELETE FROM gameobject_battleground WHERE guid = '5015221';
