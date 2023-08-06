bd69ae7879fc67a2e1760a18f8c51a9e83706d6d
slowtorta <slowtorta@gmail.com>
2023-08-06 12:43:23 +0100

    Direct world update.

--- sql/database_updates/20230806112750_world.sql
+++ sql/database_updates/20230806112750_world.sql
@@ -1,3 +1,16 @@
 
 -- Changes by TAMMO
 REPLACE INTO `gameobject` VALUES ( 5016587, 2020061, 1, 6787.64, -4660.06, 730.375, 3.93663, 0, 0, 0.922025, -0.387131, 300, 300, 100, 1, 0, 0);
+REPLACE INTO `object_scaling` (`fullGuid`, `scale`) VALUES (17370417653772749835, 0.100000);
+REPLACE INTO `gameobject` VALUES ( 5016587, 2020061, 1, 6787.64, -4660.06, 730.375, 3.93663, 0, 0, 0.922025, -0.387131, 300, 300, 100, 1, 0, 0);
+DELETE FROM gameobject WHERE guid = '5016587';
+DELETE FROM game_event_gameobject WHERE guid = '5016587';
+DELETE FROM gameobject_battleground WHERE guid = '5016587';
+REPLACE INTO `gameobject` VALUES ( 5016588, 2020061, 1, 6796.53, -4657.92, 723.383, 4.88068, 0, 0, 0.645175, -0.764035, 300, 300, 100, 1, 0, 0);
+DELETE FROM gameobject WHERE guid = '5016588';
+DELETE FROM game_event_gameobject WHERE guid = '5016588';
+DELETE FROM gameobject_battleground WHERE guid = '5016588';
+REPLACE INTO `gameobject` VALUES ( 5016589, 2020060, 1, 6796.5, -4657.71, 723.277, 4.88068, 0, 0, 0.645175, -0.764035, 300, 300, 100, 1, 0, 0);
+DELETE FROM gameobject WHERE guid = '5016589';
+DELETE FROM game_event_gameobject WHERE guid = '5016589';
+DELETE FROM gameobject_battleground WHERE guid = '5016589';
