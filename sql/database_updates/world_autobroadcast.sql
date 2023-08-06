a4b50aeef80e42ba3a3f4c87033950d1fa8fefde
Torta <66763404+slowtorta@users.noreply.github.com>
2023-08-06 16:50:00 +0200

    Update world_autobroadcast.sql

--- sql/database_updates/world_autobroadcast.sql
+++ sql/database_updates/world_autobroadcast.sql
@@ -1,5 +1,5 @@
-delete from mangos_string where entry between 50000 and 50005;
-delete from autobroadcast where string_id between 50000 and 50005;
+delete from mangos_string where entry between 50000 and 50004;
+delete from autobroadcast where string_id between 50000 and 50004;
 
 replace into mangos_string (entry, content_default) values
 
@@ -7,12 +7,10 @@ replace into mangos_string (entry, content_default) values
 
 ('50001', 'If you want to help our project, check our shop for donation rewards! Visit the donation page on our website for more info.'),
 
-('50002', '/join world to connect with the community around you!'),
+('50002', 'Keep up to date with the news and join our Discord server!'),
 
-('50003', 'Keep up to date with the news and join our Discord server!'),
+('50003', 'We encourage everyone to change their password regularly. If you think you have ever used your Turtle WoW password in different private servers we urge you to change it as soon as possible.'),
 
-('50004', 'We encourage everyone to change their password regularly. If you think you have ever used your Turtle WoW password in different private servers we urge you to change it as soon as possible.'),
+('50004', 'All gold transactions are heavily monitored. Any form of RMT will result in severe actions taken against all parties involved. Please, don''t support individuals who are actively harming our server.');
 
-('50005', 'All gold transactions are heavily monitored. Any form of RMT will result in severe actions taken against all parties involved. Please, don''t support individuals who are actively harming our server.');
-
-replace into autobroadcast (string_Id) values ('50000'),('50001'),('50002'),('50003'),('50004'),('50005');
+replace into autobroadcast (string_Id) values ('50000'),('50001'),('50002'),('50003'),('50004');
