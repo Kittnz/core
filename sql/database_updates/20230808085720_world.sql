7ef7e1ddfd2bea13ee0d5f853f3ee75210d94a56
Torta <66763404+slowtorta@users.noreply.github.com>
2023-08-08 11:03:17 +0200

    Hm-m.

---
+++ sql/database_updates/20230808085720_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM spell_template where name like '%tamamo%';
+UPDATE `creature_template` SET `display_id1` = 100 WHERE `entry` = 41000;
+DELETE FROM `creature_template` WHERE `entry` = 90982;
