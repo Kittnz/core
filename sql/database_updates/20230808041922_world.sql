e97d84e129e9681be0b90411fe5b8b6ab84db034
slowtorta <slowtorta@gmail.com>
2023-08-08 10:06:22 +0100

    Direct world update.

--- sql/database_updates/20230808041922_world.sql
+++ sql/database_updates/20230808041922_world.sql
@@ -17,3 +17,6 @@ DELETE FROM creature_movement WHERE id=2578640;
 DELETE FROM game_event_creature WHERE guid=2578640;
 DELETE FROM game_event_creature_data WHERE guid=2578640;
 DELETE FROM creature_battleground WHERE guid=2578640;
+
+-- Changes by JOE
+UPDATE `creature` SET `position_x` = -649.921814, `position_y` = 1912.587402, `position_z` = -1.153339, `orientation` = 4.586152 WHERE `guid` = 17670;
