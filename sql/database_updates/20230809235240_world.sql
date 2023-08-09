d2f97e450814d32e37286fd3d90344ebd89bea8e
Torta <66763404+slowtorta@users.noreply.github.com>
2023-08-10 01:54:50 +0200

    Misc. DB fixes.

---
+++ sql/database_updates/20230809235240_world.sql
@@ -0,0 +1,3 @@
+UPDATE `item_template` SET `bonding` = 1 WHERE `entry` = 13517;
+
+UPDATE `spell_template` SET `description` = 'All spell criticals grace you with a blessing of nature, reducing the casting time of your next spell by $/1000;16886S1 sec.' WHERE `entry` = 16880;
