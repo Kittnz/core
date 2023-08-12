e09170ab8f4c8e73774aee8a4dbe12e90de3ff4f
Torta <66763404+slowtorta@users.noreply.github.com>
2023-08-13 00:07:08 +0200

    Edited lockout for LKH10.

--- sql/database_updates/20230811170942_world.sql
+++ sql/database_updates/20230811170942_world.sql
@@ -5,3 +5,7 @@ UPDATE `item_template` SET `allowable_race` = 1 WHERE `entry` = 61106;
 
 UPDATE `spell_template` SET `name` = 'Blackwald\'s Boon', `description` = 'Reduces the stats of nearby enemies by $s1%.' WHERE `entry` = 57073;
 UPDATE `spell_template` SET `name` = 'Blackwald\'s Blessing', `description` = 'Increases your attack and casting speed by $s1% and increases size by $s3%. Stacks $u times.' WHERE `entry` = 57074;
+
+-- QA reported issues in Kara 10:
+
+UPDATE `map_template` SET `reset_delay` = 5 WHERE `entry` = 532;
