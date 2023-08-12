e574eb671f7615bb36e929a0a082de4deb217005
Torta <66763404+slowtorta@users.noreply.github.com>
2023-08-13 00:31:43 +0200

    Link groups in LLHK10.

--- sql/database_updates/20230811170942_world.sql
+++ sql/database_updates/20230811170942_world.sql
@@ -13,3 +13,18 @@ UPDATE `map_template` SET `reset_delay` = 5 WHERE `entry` = 532;
 UPDATE `map_template` SET `linked_zone` = 3457 WHERE `entry` = 532;
 REPLACE INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`) VALUES (36, 3457, 0);
 UPDATE `map_template` SET `ghost_entrance_map` = 0, `ghost_entrance_x` = -11104.3, `ghost_entrance_y` = -1999.42 WHERE `entry` = 532;
+
+-- Link the following GUIDS together
+
+REPLACE INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (2574397, 2574395, 3073);
+REPLACE INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (2574396, 2574395, 3073);
+REPLACE INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (2574398, 2574395, 3073);
+REPLACE INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (2574400, 2574395, 3073);
+
+REPLACE INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (2574315, 2574314, 3073);
+REPLACE INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (2574300, 2574301, 3073);
+REPLACE INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (2574357, 2574364, 3073);
+
+REPLACE INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (2574273, 2574276, 3073);
+
+GUID 2574273 needs to be linked with GUID 2574276
