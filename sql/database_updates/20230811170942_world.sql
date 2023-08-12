803058a6817a2731f354f64c34980a2e4efef9dc
Torta <66763404+slowtorta@users.noreply.github.com>
2023-08-12 20:44:43 +0200

    Fixed Necromancer II [Human, male only].

--- sql/database_updates/20230811170942_world.sql
+++ sql/database_updates/20230811170942_world.sql
@@ -1,3 +1,4 @@
 UPDATE `item_template` SET `name` = 'Twilight' WHERE `entry` = 50536;
 UPDATE `spell_template` SET `name` = 'Twilight' WHERE `entry` = 33396;
 
+UPDATE `item_template` SET `allowable_race` = 1 WHERE `entry` = 61106;
