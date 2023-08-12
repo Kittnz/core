b74f2f51f647f01c14a6884ef94b55facdd699c6
slowtorta <slowtorta@gmail.com>
2023-08-12 19:36:24 +0100

    Direct world update.

--- sql/database_updates/20230812164944_world.sql
+++ sql/database_updates/20230812164944_world.sql
@@ -9,3 +9,4 @@ DELETE FROM creature_battleground WHERE guid=2572289;
 
 -- Changes by SHANG
 REPLACE INTO `creature` VALUES (2578665,50041,0,0,0,532,-11110.7,-1962.05,74.3892,5.87804,120,120,0,100,100,0,0,0);
+UPDATE `creature` SET `position_x` = -10932.880859, `position_y` = -1992.220215, `position_z` = 92.182457, `orientation` = 5.953417 WHERE `guid` = 2574312;
