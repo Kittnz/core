a018778c453b333cd64e91700bc77e2d5fca71af
Torta <66763404+slowtorta@users.noreply.github.com>
2023-08-12 20:58:28 +0200

    Fixed Blackwald's spells.

--- sql/database_updates/20230811170942_world.sql
+++ sql/database_updates/20230811170942_world.sql
@@ -2,3 +2,6 @@ UPDATE `item_template` SET `name` = 'Twilight' WHERE `entry` = 50536;
 UPDATE `spell_template` SET `name` = 'Twilight' WHERE `entry` = 33396;
 
 UPDATE `item_template` SET `allowable_race` = 1 WHERE `entry` = 61106;
+
+UPDATE `spell_template` SET `name` = 'Blackwald\'s Boon', `description` = 'Reduces the stats of nearby enemies by $s1%.' WHERE `entry` = 57073;
+UPDATE `spell_template` SET `name` = 'Blackwald\'s Blessing', `description` = 'Increases your attack and casting speed by $s1% and increases size by $s3%. Stacks $u times.' WHERE `entry` = 57074;
