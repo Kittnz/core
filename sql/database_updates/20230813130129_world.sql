0eff02b2575a8bfe7b6f034972265a374e7dab35
Torta <66763404+slowtorta@users.noreply.github.com>
2023-08-13 15:13:13 +0200

    NPC Shattercage Magiskull reduced HP to 39807.

--- sql/database_updates/20230813130129_world.sql
+++ sql/database_updates/20230813130129_world.sql
@@ -13,3 +13,7 @@ UPDATE `quest_template` SET `Type` = 62, `QuestFlags` = 64 WHERE `entry` = 40828
 -- QUEST The Key to Karazhan X (Entry 40829) has the objective "quest_40829_dummy_trigger slain" when it should say 'Upper Karazhan Tower Key Reforged.' 
 
 UPDATE `quest_template` SET `ObjectiveText1` = 'Upper Karazhan Tower Key Reforged' WHERE `entry` = 40829;
+
+-- NPC Shattercage Magiskull reduce HP to 39807
+
+UPDATE `creature_template` SET `health_min` = 39807, `health_max` = 39807 WHERE `entry` = 61199;
