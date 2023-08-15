c6f92d4fc4e6d3395c889c1786d2d6d65b5853c9
Torta <66763404+slowtorta@users.noreply.github.com>
2023-08-15 19:26:41 +0200

    Update 20230815153634_world.sql

--- sql/database_updates/20230815153634_world.sql
+++ sql/database_updates/20230815153634_world.sql
@@ -1,5 +1,4 @@
 /*
-
 -- Note: I reserved 54000-54000 spell entry range for your new recipe spells, for jewecrafting or whatever you want. However for the skillline data, please always check with the recent SkillLibeAbility.dbc in client repository to see what's the last entry in that table, and go with the next one! 
 -- You don't have to include this enum if you're working with the same file:
 
@@ -17,8 +16,8 @@ SET @JEWELCRAFTING            = 755;
 SET @learn_spell_id = 54000; -- Ensure it's unique.
 SET @recipe_spell_id = 54001; -- Ensure it's unique, usually learning spell ID + 1
 
-SET @recipe_learn_spell_name = 'Pattern: Blue Dragonscale Boots'; -- Ensure it's unique, usually learning spell ID + 1
 SET @recipe_spell_name = 'Blue Dragonscale Boots'; -- Ensure it's unique, usually learning spell ID + 1
+SET @recipe_learn_spell_name = CONCAT('Pattern: ', @recipe_spell_name); -- Ensure it's unique, usually learning spell ID + 1
 
 SET @recipe_item = 0000; -- Teaching scroll item. Item must exist in DB. Create it with ANY recipe in the field, it'll be overwritten.
 SET @skill_id = @LEATHERWORKING; 
@@ -62,7 +61,5 @@ SET @recipe_learn_spell_desc = CONCAT('Teaches you how to craft a ', @recipe_spe
 REPLACE INTO `skill_line_ability` VALUES (@skill_line_entry, @skill_id, @recipe_spell_id, 0, 0, 1, 0, 0, @skill_level_max, @skill_level_required , @skill_required_to_train);
 UPDATE `item_template` SET `spellid_1` = @learn_spell_id, `name` = @recipe_learn_spell_name WHERE `entry` = @recipe_item;
 REPLACE INTO `spell_template` VALUES (@learn_spell_id, 0, 0, 0, 0, 0, 262400, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 14, 0, 0, 15, 0, 0, 0, 101, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1, 0, 36, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, @recipe_spell_id, 0, 0, 0, 0, 0, 108, 0, 1, 0, 0, @recipe_learn_spell_name, 4128894, '', 4128876, @recipe_learn_spell_desc, 4128894, '', 4128876, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, -1, 1, 1, 1, 0, 0, 0, 0),
-
 (@recipe_spell_id, 0, 0, 0, 0, 0, 65568, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 39, 0, 0, 15, 0, 0, 0, 101, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, @reagent_item_1, @reagent_item_2, @reagent_item_3, @reagent_item_4, @reagent_item_5, @reagent_item_6, @reagent_item_7, @reagent_item_8, @reagent_item_count_1, @reagent_item_count_2, @reagent_item_count_3, @reagent_item_count_4, @reagent_item_count_5, @reagent_item_count_6, @reagent_item_count_7, @reagent_item_count_8, -1, 0, 0, 24, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, @output_item_id, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4439, 0, 1, 0, 0, @recipe_spell_name, 4128894, '', 4128876, '', 4128876, '', 4128876, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 1, 1, 1, 0, 0, 0, 0);
-
 */
