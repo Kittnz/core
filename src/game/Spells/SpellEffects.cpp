74743d1e7beb3d70f0ed6746ceeb4bb8ab35cdd0
ratkosrb <ratkomladic2@abv.bg>
2023-08-15 20:54:02 +0300

    Auras with this attribute should be skipped by effect dispel mechanic.

--- src/game/Spells/SpellEffects.cpp
+++ src/game/Spells/SpellEffects.cpp
@@ -7479,7 +7479,8 @@ void Spell::EffectDispelMechanic(SpellEffectIndex eff_idx)
         next = iter;
         ++next;
         SpellEntry const *spell = iter->second->GetSpellProto();
-        if (iter->second->HasMechanic(mechanic))
+        if (iter->second->HasMechanic(mechanic) &&
+           !spell->HasAttribute(SPELL_ATTR_CANT_CANCEL))
         {
             unitTarget->RemoveAurasDueToSpell(spell->Id);
             if (Auras.empty())
