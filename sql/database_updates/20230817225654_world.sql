c790dbb4aaa8a8ce9d251d66f6ad1eda3153baa2
slowtorta <slowtorta@gmail.com>
2023-08-19 07:32:30 +0100

    Direct world update.

--- sql/database_updates/20230817225654_world.sql
+++ sql/database_updates/20230817225654_world.sql
@@ -1,3 +1,6 @@
 
 -- Changes by QUARRIONPTR
 REPLACE INTO `creature` VALUES (2578694,10440,0,0,0,0,1590.15,-5730.48,121.68,2.05753,120,120,5,100,100,1,0,0);
+UPDATE creature_template SET faction = '21' WHERE entry = '10440';
+UPDATE creature_template SET faction = '21' WHERE entry = '10440';
+UPDATE creature_template SET faction = '20' WHERE entry = '10440';
