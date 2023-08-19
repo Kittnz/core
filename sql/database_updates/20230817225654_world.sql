b1542675bf089e5dbdc1f4359e10edaeae7033f3
slowtorta <slowtorta@gmail.com>
2023-08-19 21:55:58 +0100

    Direct world update.

--- sql/database_updates/20230817225654_world.sql
+++ sql/database_updates/20230817225654_world.sql
@@ -4,3 +4,11 @@ REPLACE INTO `creature` VALUES (2578694,10440,0,0,0,0,1590.15,-5730.48,121.68,2.
 UPDATE creature_template SET faction = '21' WHERE entry = '10440';
 UPDATE creature_template SET faction = '21' WHERE entry = '10440';
 UPDATE creature_template SET faction = '20' WHERE entry = '10440';
+
+-- Changes by DRAGUNOVI
+DELETE FROM creature WHERE guid=2564143;
+DELETE FROM creature_addon WHERE guid=2564143;
+DELETE FROM creature_movement WHERE id=2564143;
+DELETE FROM game_event_creature WHERE guid=2564143;
+DELETE FROM game_event_creature_data WHERE guid=2564143;
+DELETE FROM creature_battleground WHERE guid=2564143;
