2b66d4ea7cf06f289bfec7c7b58f091988db6d9d
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-08-21 20:44:18 +0300

    Quest 41062 restrict to druid, priest, warlock, mage only.

--- sql/database_updates/20230821144949_world.sql
+++ sql/database_updates/20230821144949_world.sql
@@ -4,3 +4,5 @@ update item_template set name = 'Tome of Expertise: Bows', allowable_class = 13 
 update spell_template SET effectBonusCoefficient1 = 0.05 WHERE entry = 45416; -- Potent Venom
 -- Fix mats of gloves of unwinding mystery.
 update spell_template SET reagentCount3 = 8, reagentCount4 = 4 WHERE entry = 45416; -- Potent Venom
+-- Quest 41062 restrict to druid, priest, warlock, mage only.
+update quest_template set requiredclasses = 1424 where entry = 40962;
