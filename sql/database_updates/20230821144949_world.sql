01d28a7719601d0fb69828bf69b5e79562359fe4
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-08-21 18:01:33 +0300

    Spell fixes: "Potent Venom".

--- sql/database_updates/20230821144949_world.sql
+++ sql/database_updates/20230821144949_world.sql
@@ -1,2 +1,6 @@
 -- Fix expertise: books.
 update item_template set name = 'Tome of Expertise: Bows', allowable_class = 13 where entry = 81160; -- Expertise: Bows
+-- Change scaling to potent venom trinket.
+update spell_template SET effectBonusCoefficient1 = 0.05 WHERE entry = 45416; -- Potent Venom
+-- Fix mats of gloves of unwinding mystery.
+update spell_template SET reagentCount3 = 8, reagentCount4 = 4 WHERE entry = 45416; -- Potent Venom
