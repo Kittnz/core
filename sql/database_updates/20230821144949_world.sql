9c1fac4478df9bdb92c962377fb8392a00fce1a6
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-08-21 20:44:31 +0300

    Item 61184 (The Scythe of Elune) add class restrictions of druid, priest, warlock, mage.

--- sql/database_updates/20230821144949_world.sql
+++ sql/database_updates/20230821144949_world.sql
@@ -6,3 +6,5 @@ update spell_template SET effectBonusCoefficient1 = 0.05 WHERE entry = 45416; --
 update spell_template SET reagentCount3 = 8, reagentCount4 = 4 WHERE entry = 45416; -- Potent Venom
 -- Quest 41062 restrict to druid, priest, warlock, mage only.
 update quest_template set requiredclasses = 1424 where entry = 40962;
+-- Item 61184 (The Scythe of Elune) add class restrictions of druid, priest, warlock, mage.
+update item_template set allowable_class = 1424 where entry = 61184;
