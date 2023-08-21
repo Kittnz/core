691bdd5462cfbd85d2f481c409a9c24034f7b0f8
Torta <66763404+slowtorta@users.noreply.github.com>
2023-08-21 02:54:51 +0200

    Made glyph auras non-dispelable, and persistent through death.

--- sql/database_updates/20230820201918_world.sql
+++ sql/database_updates/20230820201918_world.sql
@@ -7,3 +7,27 @@ REPLACE INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (2578141,
 -- Set the spawn time of the gobjects 'Medivh's Merlot Blue' and 'Medivh's Merlot' to 7d
 
 UPDATE `gameobject` SET `spawntimesecsmin` = 604800, `spawntimesecsmax` = 604800 WHERE `id` in (2011044, 2011043);
+
+-- Glyphs are magic effects, can be dispelled, glyphs fall off when you die
+
+UPDATE `spell_template` SET `dispel` = 0 WHERE `entry` = 53002;
+UPDATE `spell_template` SET `dispel` = 0 WHERE `entry` = 53005;
+UPDATE `spell_template` SET `dispel` = 0 WHERE `entry` = 53008;
+UPDATE `spell_template` SET `dispel` = 0 WHERE `entry` = 53011;
+UPDATE `spell_template` SET `dispel` = 0 WHERE `entry` = 53014;
+UPDATE `spell_template` SET `dispel` = 0 WHERE `entry` = 53017;
+UPDATE `spell_template` SET `dispel` = 0 WHERE `entry` = 53020;
+UPDATE `spell_template` SET `dispel` = 0 WHERE `entry` = 53023;
+UPDATE `spell_template` SET `dispel` = 0 WHERE `entry` = 53026;
+UPDATE `spell_template` SET `dispel` = 0 WHERE `entry` = 53029;
+
+UPDATE `spell_template` SET `attributesEx3` = 1048576 WHERE `entry` = 53002;
+UPDATE `spell_template` SET `attributesEx3` = 1048576 WHERE `entry` = 53005;
+UPDATE `spell_template` SET `attributesEx3` = 1048576 WHERE `entry` = 53008;
+UPDATE `spell_template` SET `attributesEx3` = 1048576 WHERE `entry` = 53011;
+UPDATE `spell_template` SET `attributesEx3` = 1048576 WHERE `entry` = 53014;
+UPDATE `spell_template` SET `attributesEx3` = 1048576 WHERE `entry` = 53017;
+UPDATE `spell_template` SET `attributesEx3` = 1048576 WHERE `entry` = 53020;
+UPDATE `spell_template` SET `attributesEx3` = 1048576 WHERE `entry` = 53023;
+UPDATE `spell_template` SET `attributesEx3` = 1048576 WHERE `entry` = 53026;
+UPDATE `spell_template` SET `attributesEx3` = 1048576 WHERE `entry` = 53029;
