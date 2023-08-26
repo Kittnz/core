1e73796e66d23ce9824120fdbc34b61fd65375b4
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-08-26 16:13:28 +0300

    Quest 40801 and 40807: complete conditions updated.

--- sql/database_updates/20230826110551_world.sql
+++ sql/database_updates/20230826110551_world.sql
@@ -17,3 +17,6 @@ update creature_template set faction = 994 where entry = 13699;
 REPLACE INTO creature_template VALUES
 (61643, 20496, 0, 0, 0, 0, 'Raven', '', 0, 1, 1, 8, 8, 0, 0, 20, 31, 0, 1, 0.857143, 1, 18, 5, 0, 0, 0, 2, 2, 0, 44, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 1.76, 2.42, 100, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, '', 1, 1, 0, 0, 3, 0, 0, 0, 0, 0, 2, 0, '');
 
+-- Quest 40801 should allow you to reads gossips of npc 61143 from quest 40807 and it should require clicking through them to complete the quest in addition to current objective.
+-- Quest 40807 should allow you to reads gossips of npc 61147 from quest 40801 and it should require clicking through them to complete the quest in addition to current objective.
+UPDATE quest_template SET ReqCreatureOrGOId1 = 60032, ReqCreatureOrGOId2 = 60033, ReqCreatureOrGOCount1 = 1, ReqCreatureOrGOCount2 = 1, objectivetext1 = 'Listen to what Brother Ironglade and Commander Starwind have to say', objectivetext2 = 'Listen to what Commander Starwind and Brother Ironglade have to say' WHERE entry in (40801,40807);
