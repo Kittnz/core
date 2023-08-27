74f97460847662136adec156b5da1af15bb8f6af
Torta <66763404+slowtorta@users.noreply.github.com>
2023-08-27 14:37:04 +0200

    Genn should drop Gilnean Raven with 1.5 chance.

--- sql/database_updates/20230827122751_world.sql
+++ sql/database_updates/20230827122751_world.sql
@@ -17,3 +17,11 @@ UPDATE `creature_template` SET `gold_max` = 586 WHERE `entry` = 61339;
 
 UPDATE `creature_template` SET `gold_min` = 445 WHERE `entry` = 61340;
 UPDATE `creature_template` SET `gold_max` = 586 WHERE `entry` = 61340;
+
+-- change display id of 61192 to 20492
+
+UPDATE `creature_template` SET `display_id1` = 20492 WHERE `entry` = 61192;
+
+-- make item 69000 drop from 61418 with 1,5% chance (new loottable)
+
+REPLACE INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (61418, 69000, 1.5, 4, 1, 1, 0);
