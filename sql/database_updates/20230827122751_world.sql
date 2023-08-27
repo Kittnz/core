665cf0eec8df4a49648c2385671edca609a35fd5
Torta <66763404+slowtorta@users.noreply.github.com>
2023-08-27 14:35:16 +0200

    Fixed money drop for Vilemusk NPCs.

--- sql/database_updates/20230827122751_world.sql
+++ sql/database_updates/20230827122751_world.sql
@@ -2,4 +2,18 @@ DELETE FROM quest_template WHERE entry = 40013;
 DELETE FROM quest_template WHERE entry = 40012;
 DELETE FROM quest_template WHERE entry = 40010;
 
+DELETE FROM creature_template WHERE entry = 15;
 
+-- 61341- Vilemusk hellcaller
+-- 61339- Vilemusk Felsworn 
+-- 61340- Vilemusk Shadowstalker
+-- No money drops on any of these satyrs
+
+UPDATE `creature_template` SET `gold_min` = 445 WHERE `entry` = 61341;
+UPDATE `creature_template` SET `gold_max` = 586 WHERE `entry` = 61341;
+
+UPDATE `creature_template` SET `gold_min` = 445 WHERE `entry` = 61339;
+UPDATE `creature_template` SET `gold_max` = 586 WHERE `entry` = 61339;
+
+UPDATE `creature_template` SET `gold_min` = 445 WHERE `entry` = 61340;
+UPDATE `creature_template` SET `gold_max` = 586 WHERE `entry` = 61340;
