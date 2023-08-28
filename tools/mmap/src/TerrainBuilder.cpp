bfcd6ede98e4ec6fa1aa9a7a9e380a234e0ffe16
Jameyboor <jamey_D@hotmail.com>
2023-08-28 20:40:54 +0200

    f

--- tools/mmap/src/TerrainBuilder.cpp
+++ tools/mmap/src/TerrainBuilder.cpp
@@ -753,9 +753,6 @@ namespace MMAP
             /// Check every map vertice
             // x, y * -1
             Vector3 up(0, 0, 1);
-            up.x *= -1.0f;
-            up.y *= -1.0f;
-            up = up * rotation.inverse() / scale;
             for (vector<GroupModel>::iterator it = groupModels.begin(); it != groupModels.end(); ++it)
                 for (int t = 0; t < mapVertsCount / 3; ++t)
                 {
@@ -1020,7 +1017,7 @@ namespace MMAP
                              &p0[0], &p0[1], &p0[2], &p1[0], &p1[1], &p1[2], &size))
                 continue;
 
-            if (mapID == mid, tileX == tx, tileY == ty)
+            if (mapID == mid && tileX == tx && tileY == ty)
             {
                 meshData.offMeshConnections.append(p0[1]);
                 meshData.offMeshConnections.append(p0[2]);
