ff9e55d0af5e3f1063a9165e82040c8e7dab2e23
slowtorta <slowtorta@gmail.com>
2023-08-29 15:13:26 +0100

    Direct world update.

---
+++ sql/database_updates/20230828232410_world.sql
@@ -0,0 +1,3 @@
+
+-- Changes by DRAGUNOVI
+UPDATE `creature` SET `position_x` = 5500.002930, `position_y` = -3703.679199, `position_z` = 1594.327515, `orientation` = 4.801650 WHERE `guid` = 2576952;
