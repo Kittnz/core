e7cc867fb53ef005ec478e0dd94276ebbc94c0ad
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-08-30 21:53:06 +0300

    Creature 61695 loot fix: Drop chance updated.

--- sql/database_updates/20230830182806_world.sql
+++ sql/database_updates/20230830182806_world.sql
@@ -2,3 +2,5 @@
 update quest_template set title = 'The Upper Binding I', type = 62 where entry = 41011;
 -- Change the displayID of Item ID 61695 to 26772.
 update item_template set display_id = 26772 where entry = 61695;
+-- Change the drop chance of Item ID 61695 from NPC 14399 to 40%.
+update creature_loot_template set ChanceOrQuestChance = -40 where entry = 14399 and item = 61695;
