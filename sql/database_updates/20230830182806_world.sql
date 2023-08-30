69586bb409195cef9792b744ee09ddd3d53c39c0
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-08-30 21:52:16 +0300

    Item 61695 fix: display ID.

--- sql/database_updates/20230830182806_world.sql
+++ sql/database_updates/20230830182806_world.sql
@@ -1,2 +1,4 @@
 -- Rename Quest ID 41011 to "The Upper Binding I", quest ID 41011 should be a raid quest.
 update quest_template set title = 'The Upper Binding I', type = 62 where entry = 41011;
+-- Change the displayID of Item ID 61695 to 26772.
+update item_template set display_id = 26772 where entry = 61695;
