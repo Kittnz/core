851877d192b112c952824aa58a9d8af4ed4eda5e
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-08-30 21:54:27 +0300

    Creature 6243, 4819, 4820 and 4818 fixes: dispay ID changed.

--- sql/database_updates/20230830182806_world.sql
+++ sql/database_updates/20230830182806_world.sql
@@ -4,3 +4,15 @@ update quest_template set title = 'The Upper Binding I', type = 62 where entry =
 update item_template set display_id = 26772 where entry = 61695;
 -- Change the drop chance of Item ID 61695 from NPC 14399 to 40%.
 update creature_loot_template set ChanceOrQuestChance = -40 where entry = 14399 and item = 61695;
+-- Change display id of 6243 to 20501 and enable serverside.
+REPLACE INTO `creature_display_info_addon` (`display_id`, `bounding_radius`, `combat_reach`, `gender`, `display_id_other_gender`) VALUES
+(20501, 0, 0, 0, 0);
+update creature_template set display_id1 = 20501 where entry = 6243;
+-- Change display id of 4819, 4820 to 20500 and enable serverside.
+REPLACE INTO `creature_display_info_addon` (`display_id`, `bounding_radius`, `combat_reach`, `gender`, `display_id_other_gender`) VALUES
+(20500, 0, 0, 0, 0);
+update creature_template set display_id1 = 20500 where entry in (4819, 4820);
+-- Change display id of 4818 to 20502 and enable serverside.
+REPLACE INTO `creature_display_info_addon` (`display_id`, `bounding_radius`, `combat_reach`, `gender`, `display_id_other_gender`) VALUES
+(20502, 0, 0, 0, 0);
+update creature_template set display_id1 = 20502 where entry = 4818;
