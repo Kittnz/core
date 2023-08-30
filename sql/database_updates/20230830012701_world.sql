1ca630dde2e8d85d964fe2cb0c221ac52873397f
slowtorta <slowtorta@gmail.com>
2023-08-30 13:11:51 +0100

    Direct world update.

--- sql/database_updates/20230830012701_world.sql
+++ sql/database_updates/20230830012701_world.sql
@@ -7,3 +7,6 @@ REPLACE INTO `creature` VALUES (2578770,61643,0,0,0,0,-1342.07,1183.02,126.912,4
 REPLACE INTO `creature` VALUES (2561853,10814,0,0,0,229,33.5571,-294.378,110.948,6.21954,7200,7200,0,100,100,0,0,0);
 UPDATE `creature` SET `position_x` = 29.022751, `position_y` = -294.337555, `position_z` = 110.943710, `orientation` = 3.047149 WHERE `guid` = 2561853;
 UPDATE `creature` SET `position_x` = 30.198141, `position_y` = -294.283508, `position_z` = 110.944817, `orientation` = 3.212090 WHERE `guid` = 2561853;
+
+-- Changes by VOJI
+UPDATE `creature_addon` SET `stand_state`=1 WHERE `guid`=2578712;
