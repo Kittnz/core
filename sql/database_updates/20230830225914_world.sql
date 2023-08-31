6acb3960fd947ef18c7d41b193778f74a7d00f35
slowtorta <slowtorta@gmail.com>
2023-08-31 16:37:49 +0100

    Direct world update.

--- sql/database_updates/20230830225914_world.sql
+++ sql/database_updates/20230830225914_world.sql
@@ -3,3 +3,6 @@
 DELETE FROM gameobject WHERE guid = '5016657';
 DELETE FROM game_event_gameobject WHERE guid = '5016657';
 DELETE FROM gameobject_battleground WHERE guid = '5016657';
+DELETE FROM gameobject WHERE guid = '5016659';
+DELETE FROM game_event_gameobject WHERE guid = '5016659';
+DELETE FROM gameobject_battleground WHERE guid = '5016659';
