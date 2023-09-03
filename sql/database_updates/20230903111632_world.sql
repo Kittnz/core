2633ca2de26f4d5a03d9b15810c0745bec064c19
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-09-03 15:06:43 +0300

    NPC 61289: Gossip grammar fix.

--- sql/database_updates/20230903111632_world.sql
+++ sql/database_updates/20230903111632_world.sql
@@ -2,3 +2,5 @@
 update quest_template set RewItemId1 = 0, RewItemId2 = 0, RewItemCount1 = 0, RewItemCount2 = 0, RewChoiceItemId1 = 61490, RewChoiceItemId2 = 61619, RewChoiceItemCount1 = 1, RewChoiceItemCount2 = 1 where entry = 40974;
 -- Dark Bishop Mordren's gossip should say bear instead of bare where the point is.
 update broadcast_text set male_text = 'During my life it was I who looked after this church. I was murdered in cold blood by those that bear the emblem of this kingdom. It appears that I have been awoken from my slumber to serve a new purpose, and serve this purpose I shall.$B$BYou will find no hostilities here, I may be Gilnean but I offer a chance to find solace inside the walls of my parish.$B$BShould you need to rest, feel free to ask.' where entry = 61281;
+-- Remove awkward spaces at the start of paragraphs.
+update broadcast_text set male_text = 'I had always wondered what this place looked like beyond the wall... Now that I\'ve been there I must admit I am disappointed. Perhaps once you have seen one kingdom you\'ve seen them all$B$BYou looking to do some work around here, or just admire the scenery?' where entry = 61289;
