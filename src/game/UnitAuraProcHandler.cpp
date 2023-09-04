dc4879ddefc36e154f8a231c330a3157524f5181
ratkosrb <ratkomladic2@abv.bg>
2023-09-04 06:10:37 +0300

    Nerf proc chance for Lightning Speed.

--- src/game/UnitAuraProcHandler.cpp
+++ src/game/UnitAuraProcHandler.cpp
@@ -271,9 +271,9 @@ SpellProcEventTriggerCheck Unit::IsTriggeredAtSpellProcEvent(Unit *pVictim, Spel
         if (spellProto->Id == 45850)
         {
             if (procSpell->IsFitToFamily<SPELLFAMILY_SHAMAN, CF_SHAMAN_LIGHTNING_BOLT>())
-                return roll_chance_u(15) ? SPELL_PROC_TRIGGER_OK : SPELL_PROC_TRIGGER_ROLL_FAILED;
+                return roll_chance_u(5) ? SPELL_PROC_TRIGGER_OK : SPELL_PROC_TRIGGER_ROLL_FAILED;
             if (procSpell->SpellIconID == 2210)
-                return roll_chance_u(30) ? SPELL_PROC_TRIGGER_OK : SPELL_PROC_TRIGGER_ROLL_FAILED;
+                return roll_chance_u(25) ? SPELL_PROC_TRIGGER_OK : SPELL_PROC_TRIGGER_ROLL_FAILED;
             return SPELL_PROC_TRIGGER_FAILED;
         }
         // Bonus Healing
