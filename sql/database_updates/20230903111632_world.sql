061f7d7608390326160a51f61b454bb26a3c3416
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-09-04 16:12:22 +0300

    Spells 18946 and 45864 fixes. (by Dragunovi).

--- sql/database_updates/20230903111632_world.sql
+++ sql/database_updates/20230903111632_world.sql
@@ -31,3 +31,7 @@ update item_template set spellid_1 = 47357 where entry = 61698; -- Totem of the 
 update item_template set max_durability = 120 where entry = 51738; -- Tunnel Fiend Carapace
 -- NPC gossip grammar fix.
 update broadcast_text set male_text = 'I had always wondered what this place looked like beyond the wall... Now that I\'ve been there I must admit I am disappointed. Perhaps once you have seen one kingdom you\'ve seen them all$B$BYou looking to do some work around here, or just admire the scenery?' where entry = 61289;
+-- Lion horn of stormwind.
+update spell_template SET Effect2 = 6, Effect3 = 6, effectDieSides2 = 1, effectDieSides3 = 1, effectBaseDice2 = 1, effectBaseDice3 = 1, EffectBasePoints1 = 4, EffectBasePoints2 = 4, EffectBasePoints3 = 4, effectApplyAuraName1 = 138, effectApplyAuraName2 = 140, effectApplyAuraName3 = 65, effectMiscValue2 = 1, effectMiscValue3 = 1, description = 'Increases attack and casting speed by %$s1 for $d.', auraDescription = 'Increases attack speed by %$s1 for $d.' WHERE entry = 18946; -- The Lion Horn of Stormwind (Proc Buff Effect).
+update spell_template SET procchance = 2, description = 'When struck in combat has a 2% chance of increasing all party member\'s attack and casting speed by %$18946s1 for $18946d.' WHERE entry = 20847; -- The Lion Horn of Stormwind (Equip Spell).
+update spell_template SET description = 'Increases the duration of your Hex by 1.5 secs.' WHERE entry = 45864; -- Hex Mastery.
