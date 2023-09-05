568703f737b1e0d8414460f03c8ebc016cdacc49
Torta <66763404+slowtorta@users.noreply.github.com>
2023-09-05 20:27:50 +0200

    tga to png: convert faster

--- tools/TGAtoPNG/main.cpp
+++ tools/TGAtoPNG/main.cpp
@@ -9,9 +9,60 @@
 #include "lodepng.h"
 #include "tga.h"
 #include "Windows.h"
+#include <queue>
+#include <thread>
+#include <mutex>
 
 using namespace std;
 #define fs std::filesystem
+
+std::queue<std::string> tga_files;
+std::mutex tga_files_guard;
+
+void Convert()
+{
+    while (true)
+    {
+        tga_files_guard.lock();
+
+        if (tga_files.size() == 0)
+        {
+            tga_files_guard.unlock();
+            return;
+        }
+
+        std::string s = tga_files.front();
+        tga_files.pop();
+
+        tga_files_guard.unlock();
+
+        FILE* f = std::fopen(s.c_str(), "rb");
+        tga::StdioFileInterface file(f);
+        tga::Decoder decoder(&file);
+        tga::Header header;
+        if (!decoder.readHeader(header))
+            return;
+
+        tga::Image image;
+        image.bytesPerPixel = header.bytesPerPixel();
+        image.rowstride = header.width * header.bytesPerPixel();
+
+        std::vector<uint8_t> buffer(image.rowstride * header.height);
+        image.pixels = &buffer[0];
+
+        if (!decoder.readImage(header, image, nullptr))
+            return;
+
+        decoder.postProcessImage(header, image);
+
+        size_t srt_pos = s.find_last_of("/\\");
+        std::string s_filename = s.substr(srt_pos + 1);
+        s_filename.erase(s_filename.size() - 4);
+        s_filename.append(".png");
+
+        unsigned saveError = lodepng::encode(s_filename.c_str(), image.pixels, header.width, header.height, LodePNGColorType::LCT_RGBA);
+    }
+}
 
 int main() 
 {
@@ -28,32 +79,28 @@ int main()
             ss << "Screenshots/" << dir_entry.path().filename().string();
             std::string s = ss.str();
 
-            FILE* f = std::fopen(s.c_str(), "rb"); 
- //         int err = GetLastError();
-            tga::StdioFileInterface file(f);
-            tga::Decoder decoder(&file);
-            tga::Header header;
-            if (!decoder.readHeader(header))
-                return 0;
-
-            tga::Image image;
-            image.bytesPerPixel = header.bytesPerPixel();
-            image.rowstride = header.width * header.bytesPerPixel();
-
-            std::vector<uint8_t> buffer(image.rowstride * header.height);
-            image.pixels = &buffer[0];
-
-            if (!decoder.readImage(header, image, nullptr))
-                return 3;
-
-            decoder.postProcessImage(header, image);
-
-            std::stringstream ss_ren;
-            ss_ren << dir_entry.path().stem().string() << ".png";
-            std::string s_ren = ss_ren.str();
-
-            unsigned saveError = lodepng::encode(s_ren.c_str(), image.pixels, header.width, header.height, LodePNGColorType::LCT_RGBA);
+            tga_files.push(s);
         }
+    }
+
+    uint32_t thread_count = std::thread::hardware_concurrency();
+
+    for (uint32_t i = 0; i < thread_count; i++)
+    {
+        std::thread work(Convert);
+        work.detach();
+    }
+
+    while (true)
+    {
+        tga_files_guard.lock();
+        size_t size = tga_files.size();
+        tga_files_guard.unlock();
+
+        if (size == 0)
+            break;
+        else
+            Sleep(1);
     }
 	return 0;
 }
