c8d087df6fbd6c7a8069aafa75782b660b85dcfd
slowtorta <slowtorta@gmail.com>
2023-09-09 11:18:00 +0100

    Direct world update.

--- sql/database_updates/20230908184556_world.sql
+++ sql/database_updates/20230908184556_world.sql
@@ -5,3 +5,4 @@ UPDATE `creature` SET `wander_distance`=0.000000 WHERE `guid`=2578856;
 UPDATE `creature` SET `position_x` = -11267.840820, `position_y` = -1453.603516, `position_z` = 2.654201, `orientation` = 0.131898 WHERE `guid` = 2578856;
 UPDATE `creature` SET `position_x` = -11266.704102, `position_y` = -1453.238525, `position_z` = 2.748662, `orientation` = 0.028223 WHERE `guid` = 2578856;
 UPDATE `creature` SET `spawntimesecsmin`=200000, `spawntimesecsmax`=200000 WHERE `guid`=2578856;
+REPLACE INTO `creature` VALUES (2578857,91910,0,0,0,800,-11032.8,-1528.76,33.5322,4.69424,120,120,5,100,100,1,1,200);
