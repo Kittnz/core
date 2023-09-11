dfb93de93d1ac90c221fb84f74d4801fe57b8c8c
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-09-11 14:31:11 +0300

    Spell 23300 fix.

--- sql/database_updates/20230911110640_world.sql
+++ sql/database_updates/20230911110640_world.sql
@@ -4,3 +4,5 @@ REPLACE INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ite
 update creature_template set faction = 12 where entry = 61611;
 -- NPC Darius Ravenwood, change greeting text to : Gilneas has seen darker days. But with newfound allies, such as you, hope remains. I feared the Alliance would've abandoned us, but here we stand. I will bend no knee to these Alteraci scum.$B$BWe will prevail. 
 update broadcast_text set male_text = 'Gilneas has seen darker days. But with newfound allies, such as you, hope remains. I feared the Alliance would\'ve abandoned us, but here we stand. I will bend no knee to these Alteraci scum.$B$BWe will prevail.' where entry = 61259;
+-- Fix dumb desc.
+update spell_template SET description = 'Increases the Holy damage bonus of your Crusader Strike by $s1 per stack.' WHERE entry = 23300; -- Seal of the Crusader Judgement Increase.
