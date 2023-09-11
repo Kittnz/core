8272f63f92eed8295124816a9f9e2bf80fc1f85f
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-09-11 14:32:17 +0300

    Quest 41015 fix: questgiver, details, questgiver flags, prev quest.

--- sql/database_updates/20230911110640_world.sql
+++ sql/database_updates/20230911110640_world.sql
@@ -6,3 +6,11 @@ update creature_template set faction = 12 where entry = 61611;
 update broadcast_text set male_text = 'Gilneas has seen darker days. But with newfound allies, such as you, hope remains. I feared the Alliance would\'ve abandoned us, but here we stand. I will bend no knee to these Alteraci scum.$B$BWe will prevail.' where entry = 61259;
 -- Fix dumb desc.
 update spell_template SET description = 'Increases the Holy damage bonus of your Crusader Strike by $s1 per stack.' WHERE entry = 23300; -- Seal of the Crusader Judgement Increase.
+-- Quest The Binding of Xanthar (Entry 41015) needs its start npc set to Hanvar the Righteous (Entry 61568).
+delete from creature_questrelation where quest = 41015;
+REPLACE INTO `creature_questrelation` (`id`, `quest`) VALUES (61568, 41015);
+update quest_template set details = 'Some years ago, a rare and powerful item was stored inside of this church. I have been cursed to remain ever since its disappearance years ago. If you could recover this item and return it to Karazhan, I would be more than thankful.$B$BThe Binding was torn apart before it was ever stolen, both halves now rest with separate owners. The first was taken by a hermit mage who held no affiliations, his accent certainly sounded like he came from the north.$B$BThe second half of the binding was taken by a man of faith, of vigilance, wearing red cloth and bearing the symbol of the Light itself. His ravings almost sounded mad!$B$BGather me both the upper and lower half of the binding, and I can make it whole.' where entry = 41015;
+-- NPC Hanvar the Righteous make sure he has Quest flags.
+update creature_template set npc_flags = 3 where entry = 61568;
+-- Quest The Binding of Xanthar, make sure it has no previous required quests.
+update quest_template set prevquestid = 0 where entry = 41015;
