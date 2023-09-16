67ac32ea245318b346c5a838c7102759274ad77d
Torta <66763404+slowtorta@users.noreply.github.com>
2023-09-16 16:14:50 +0200

    Fixed Idol of the Moonfang.

--- sql/database_updates/20230916111117_world.sql
+++ sql/database_updates/20230916111117_world.sql
@@ -9,3 +9,6 @@ UPDATE `creature` SET `spawntimesecsmin`=120, `spawntimesecsmax`=120 WHERE `id2`
 UPDATE `creature` SET `spawntimesecsmin`=120, `spawntimesecsmax`=120 WHERE `id3` IN (7441, 7442, 7440,  7439, 10916, 7438);
 UPDATE `creature` SET `spawntimesecsmin`=120, `spawntimesecsmax`=120 WHERE `id4` IN (7441, 7442, 7440,  7439, 10916, 7438);
 
+-- Fix Idol of the Moonfang
+
+UPDATE `item_template` SET `subclass` = 8 WHERE `entry` = 61293
