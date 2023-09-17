8b0f43546854115b8d0d4eac44caf6a5f5f011b4
Torta <66763404+slowtorta@users.noreply.github.com>
2023-09-17 22:44:15 +0200

    Creature_template IDs 61582 and 61528 need "Rare" status removed.

--- sql/database_updates/20230917200459_world.sql
+++ sql/database_updates/20230917200459_world.sql
@@ -7,3 +7,8 @@ UPDATE `creature_questrelation` SET `id` = '2498' WHERE `id` = 61277 AND `quest`
 
 UPDATE `spell_template` SET `castingTimeIndex` = 14 WHERE `entry` = 47245;
 UPDATE `spell_template` SET `spellVisual1` = 108 WHERE `entry` = 47245;
+
+-- Creature_template IDs 61582 and 61528 need "Rare" status removed
+
+UPDATE `creature_template` SET `rank` = 1 WHERE `entry` = 61582;
+UPDATE `creature_template` SET `rank` = 1 WHERE `entry` = 61582;
