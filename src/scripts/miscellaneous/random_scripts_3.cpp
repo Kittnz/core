752f43a4fc65d40f5ce260275c9afcb178a036ca
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-09-20 16:05:55 +0300

    Correct greeting text for NPC Darius Ravenwood setted.

--- src/scripts/miscellaneous/random_scripts_3.cpp
+++ src/scripts/miscellaneous/random_scripts_3.cpp
@@ -6448,7 +6448,7 @@ bool GossipHello_npc_lord_darius_ravenwood(Player* pPlayer, Creature* pCreature)
         pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TALK, 66812, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
     }
 
-    pPlayer->SEND_GOSSIP_MENU(61326, pCreature->GetGUID());
+    pPlayer->SEND_GOSSIP_MENU(61259, pCreature->GetGUID());
 
     return true;
 }
