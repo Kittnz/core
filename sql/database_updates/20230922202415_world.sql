792dda3ea5f65b0276359d4eb700c18e2cfdaf71
Torta <66763404+slowtorta@users.noreply.github.com>
2023-09-23 19:00:35 +0200

    Disabled War Mode.

--- sql/database_updates/20230922202415_world.sql
+++ sql/database_updates/20230922202415_world.sql
@@ -8,3 +8,8 @@ UPDATE `battleground_template` SET `min_players_per_team` = 8, `max_players_per_
 UPDATE `battleground_template` SET `min_players_per_team` = 10, `max_players_per_team` = 15, `min_level` = 0, `max_level` = 60, `alliance_win_spell` = 24953, `alliance_lose_spell` = 24952, `horde_win_spell` = 24953, `horde_lose_spell` = 24952, `alliance_start_location` = 890, `horde_start_location` = 889, `player_loot_id` = 0 WHERE `id` = 3;
 UPDATE `battleground_template` SET `min_players_per_team` = 3, `max_players_per_team` = 3, `min_level` = 0, `max_level` = 60, `alliance_win_spell` = 29129, `alliance_lose_spell` = 29128, `horde_win_spell` = 29129, `horde_lose_spell` = 29128, `alliance_start_location` = 9000, `horde_start_location` = 9001, `player_loot_id` = 0 WHERE `id` = 4;
 UPDATE `battleground_template` SET `min_players_per_team` = 14, `max_players_per_team` = 20, `min_level` = 51, `max_level` = 60, `alliance_win_spell` = 0, `alliance_lose_spell` = 0, `horde_win_spell` = 0, `horde_lose_spell` = 0, `alliance_start_location` = 700, `horde_start_location` = 701, `player_loot_id` = 0 WHERE `id` = 5;
+
+-- Remove War Mode glyph:
+
+DELETE FROM npc_vendor WHERE item = 80060;
+DELETE FROM npc_vendor_template WHERE item = 80060;
