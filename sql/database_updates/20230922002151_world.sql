580bee316677cf96367cb39cabb09b2e87870e5c
slowtorta <slowtorta@gmail.com>
2023-09-24 16:09:43 +0100

    Direct world update.

--- sql/database_updates/20230922002151_world.sql
+++ sql/database_updates/20230922002151_world.sql
@@ -1,3 +1,6 @@
 
 -- Changes by DRAGUNOVI
 UPDATE `creature` SET `position_x` = -1249.143311, `position_y` = 1509.311401, `position_z` = 81.748268, `orientation` = 6.000592 WHERE `guid` = 2575109;
+
+-- Changes by GHEOR
+REPLACE INTO `gameobject` VALUES ( 5016750, 2000255, 451, 16754.5, 15901, 47.128, 4.77508, 0, 0, 0.6846, -0.728919, 300, 300, 100, 1, 0, 0);
