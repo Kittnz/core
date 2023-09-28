bbfd2906170da0d5eaf2560835d9396efe11ee73
Torta <66763404+slowtorta@users.noreply.github.com>
2023-09-29 01:57:12 +0200

    Correct too short respawn timers for new custom mobs.

---
+++ sql/database_updates/20230928234527_world.sql
@@ -0,0 +1,3 @@
+
+-- Correct too short respawn timers for new custom mobs.
+UPDATE creature SET spawntimesecsmin = 300, spawntimesecsmax = 300 WHERE guid > 2578242 AND spawntimesecsmin = 120 AND spawntimesecsmax = 120;
