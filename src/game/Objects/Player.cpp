17687fd4b8bd5548160ba1740e1d14209e1daf20
Torta <66763404+slowtorta@users.noreply.github.com>
2023-09-29 22:34:48 +0200

    Prettier query :blush:

--- src/game/Objects/Player.cpp
+++ src/game/Objects/Player.cpp
@@ -23047,18 +23047,9 @@ void Player::MailHardcoreModeRewards(uint32 level)
         .AddItem(ToMailItem)
         .SendMailTo(this, MailSender(MAIL_CREATURE, uint32(16547), MAIL_STATIONERY_DEFAULT), MAIL_CHECK_MASK_COPIED, 0, 30 * DAY);
 
-    if (level == 60 && GetSession()->GetSecurity() == SEC_PLAYER)
+    if (level == 60 /*&& GetSession()->GetSecurity() == SEC_PLAYER*/)
     {
-        QueryResult* result = LoginDatabase.PQuery("SELECT `coins` FROM `shop_coins` WHERE `id` = '%u'", GetSession()->GetAccountId());
-
-        if (!result)
-        {
-            LoginDatabase.PExecute("INSERT INTO shop_coins (id, coins) VALUES ('%u', 200)", GetSession()->GetAccountId());
-            delete result;
-        }
-        else
-            LoginDatabase.PExecute("UPDATE `shop_coins` SET `coins`=`coins`+200 WHERE `id`=%u", GetSession()->GetAccountId()); 
-
+        LoginDatabase.PExecute("INSERT INTO `shop_coins` (`id`, `coins`) VALUES (%u, 200) ON DUPLICATE KEY UPDATE `coins` = `coins` + 200", GetSession()->GetAccountId()); 
         ChatHandler(this).SendSysMessage("|cffF58CBASpeedy whispers: Impressive! Your recent achievements on reaching level 60 in Turtle Mode have not gone unnoticed. We've added additional 200 Turtle Tokes to your account balance!|r");
     }
 }
