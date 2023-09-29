71064a41e9f95178ba8c9f2be4f1cc970c447853
Jameyboor <jamey_D@hotmail.com>
2023-09-30 00:14:57 +0200

    Add config option to EU http api too

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -1720,7 +1720,7 @@ void World::SetInitialWorldSettings()
 {
     _server = std::unique_ptr<HttpApi::ApiServer, ApiServerDeleter>(new HttpApi::ApiServer);
     HttpApi::RegisterControllers();
-    _server->Start("127.0.0.1", 1313);
+    _server->Start(sConfig.GetStringDefault("HttpApi.BindIP", "127.0.0.1"), sConfig.GetIntDefault("HttpApi.BindPort", 50000));
     //- Initialize the random number generator
     srand((unsigned int)time(nullptr));
 
