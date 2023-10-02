0eda3d0a40c03a534a44e5a93e7cda4d98c56d21
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-10-02 17:42:32 +0300

    Item "Crocolisk Meat" added to npc 91962 vendor list.

--- sql/database_updates/20231002142121_world.sql
+++ sql/database_updates/20231002142121_world.sql
@@ -1,2 +1,4 @@
 -- Replace Spell ID 25515 with Spell ID 56 on Creature _Template IDs 61074 and 61080.
 update creature_spells set spellid_1 = 56 where entry in (180075,180077);
+-- Add Crocolisk Meat to https://database.turtle-wow.org/?npc=91962 loot table.
+REPLACE INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (91962, 2924, 30.402, 0, 1, 1, 0);
