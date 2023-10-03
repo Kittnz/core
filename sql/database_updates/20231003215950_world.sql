710d2aa15dcff58d37c64b024890c6b7ac6fbb2e
redmagejoe <redmagejoe@yahoo.com>
2023-10-03 18:01:12 -0400

    Spell description QA fixes
    Removed description from a Learn spell and fixed typo in Moonkin Form  spell.

---
+++ sql/database_updates/20231003215950_world.sql
@@ -0,0 +1,5 @@
+UPDATE spell_template SET Description = '' WHERE entry = 47326;
+
+UPDATE spell_template SET
+Description = 'Transforms the Druid into Moonkin Form. While in this form the armor contribution from items is increased by 360%, the Mana cost of your Balance spells is reduced by 20%, and all party members within 30 yards have their spell critical chance increased by 3%. The Moonkin can only cast Balance spells, Innervate, and Remove Curse while shapeshifted. The act of shapeshifting frees the caster of Polymorph and Movement Impairing effects.'
+WHERE entry = 24858;
