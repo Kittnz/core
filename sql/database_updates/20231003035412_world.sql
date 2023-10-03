7ce7af116a50741290562aefd8386d0e556b0970
Torta <66763404+slowtorta@users.noreply.github.com>
2023-10-03 06:00:28 +0200

    Graveyards for Gilneas City.

--- sql/database_updates/20231003035412_world.sql
+++ sql/database_updates/20231003035412_world.sql
@@ -3,3 +3,5 @@
 UPDATE `game_graveyard_zone` SET `ghost_zone` = 5179 WHERE `id` = 936 AND `ghost_zone` = 267;
 UPDATE `map_template` SET `linked_zone` = 5179 WHERE `entry` = 815;
 
+REPLACE INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`) VALUES (936, 5180, 0);
+REPLACE INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`) VALUES (936, 5208, 0);
