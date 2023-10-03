45af0959861e89cc63f24d9ea8ac6121ee92860d
ratkosrb <ratkomladic2@abv.bg>
2023-10-03 21:23:03 +0300

    Fix build.
    Thanks Jamey.

--- src/shared/httplib.h
+++ src/shared/httplib.h
@@ -12,7 +12,9 @@
 
 #define CPPHTTPLIB_OPENSSL_SUPPORT
 
+#ifndef _WIN32
 #define CPPHTTPLIB_USE_POLL
+#endif
 
 /*
  * Configuration
@@ -8641,6 +8643,7 @@ SSLClient::verify_host_with_subject_alt_name(X509 *server_cert) const {
   size_t addr_len = 0;
 
 #ifndef __MINGW32__
+#ifndef _WIN32
   if (inet_pton(AF_INET6, host_.c_str(), &addr6)) {
     type = GEN_IPADD;
     addr_len = sizeof(struct in6_addr);
@@ -8648,6 +8651,7 @@ SSLClient::verify_host_with_subject_alt_name(X509 *server_cert) const {
     type = GEN_IPADD;
     addr_len = sizeof(struct in_addr);
   }
+#endif
 #endif
 
   auto alt_names = static_cast<const struct stack_st_GENERAL_NAME *>(
