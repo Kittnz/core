a33a1803753b01e39eac613b6513954a43172505
redmagejoe <redmagejoe@yahoo.com>
2023-10-03 18:19:16 -0400

    More spell desc fixes
    Corrected description typos of more spells.

---
+++ sql/database_updates/20231003221105_world.sql
@@ -0,0 +1,7 @@
+UPDATE spell_template SET
+Description = 'Opens a portal into the Twisting Nether that summons a Felguard under the command of the caster for 5 min. Once control is lost, the Felguard must be Enslaved to maintain control. Requires the caster and 2 additional party members to complete the ritual. In order to participate, all players must right-click the portal and not move until the ritual is complete.'
+WHERE entry = 45908;
+
+UPDATE spell_template SET
+Description = 'When struck in combat has a 2% chance of increasing all party member''s attack and casting speed by 5% for 30 sec.'
+WHERE entry = 20847;
