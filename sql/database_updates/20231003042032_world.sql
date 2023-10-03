3658ac08f0e877ee32adb05beff1caadd7a3d8fb
Torta <66763404+slowtorta@users.noreply.github.com>
2023-10-03 13:14:16 +0200

    Fixed Genn's loot chances.

--- sql/database_updates/20231003042032_world.sql
+++ sql/database_updates/20231003042032_world.sql
@@ -358,3 +358,8 @@ UPDATE `creature` SET `spawntimesecsmin` = 36000, `spawntimesecsmax` = 36000 WHE
 -- UPDATE `creature` SET `spawntimesecsmin` = 300, `spawntimesecsmax` = 300 WHERE `guid` = 2578850;
 -- UPDATE `creature` SET `spawntimesecsmin` = 300, `spawntimesecsmax` = 300 WHERE `guid` = 2578851;
 -- UPDATE `creature` SET `spawntimesecsmin` = 300, `spawntimesecsmax` = 300 WHERE `guid` = 2578852;
+
+-- Fixed Genn loot chances:
+
+UPDATE `creature_loot_template` SET `groupid` = 7 WHERE `entry` = 61418 AND `item` = 61352 AND `groupid` = 0;
+UPDATE `creature_loot_template` SET `groupid` = 6 WHERE `entry` = 61418 AND `item` = 61496 AND `groupid` = 0;
