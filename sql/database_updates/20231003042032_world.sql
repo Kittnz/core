2d95f22c37beecb2322fa809b2b21a2ca0f2522c
Torta <66763404+slowtorta@users.noreply.github.com>
2023-10-03 06:58:29 +0200

    Some more item stack reverts.

--- sql/database_updates/20231003042032_world.sql
+++ sql/database_updates/20231003042032_world.sql
@@ -1,3 +1,3 @@
 -- Made Zanzas non-unique:
 
-UPDATE `item_template` SET `max_count` = 0 WHERE `entry` IN (20079, 20080, 20081);
+UPDATE `item_template` SET `max_count` = 0 WHERE `entry` IN (20079, 20080, 20081, 8424, 8423, 8411, 8412, 8410);
