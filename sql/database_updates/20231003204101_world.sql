ebb843886fcd4321cad5eda3a10f99524a54d064
redmagejoe <redmagejoe@yahoo.com>
2023-10-03 17:35:40 -0400

    Several quest text QA fixes
    A number of community-contributed typos, grammar fixes, and general language improvements for a number of quests.

---
+++ sql/database_updates/20231003204101_world.sql
@@ -0,0 +1,74 @@
+UPDATE quest_template SET QuestLevel = 25 WHERE entry = 40282;
+
+UPDATE quest_template SET
+Details = 'You the new delivery kid from Razor Hill? Didn''t really think that old orc would get the order here that quick. You better not tell him we''ve just been giving him flak! He might start to think he''s doing too good a job, or something!'
+WHERE entry = 55002;
+
+UPDATE quest_template SET
+Details = 'Hey there, I got a strange request for you if you don''t mind helping. I used to be an Arcanasmith within Shadowforge and worked with all kinds of Golem Cores.$B$BMy loyalty to ol'' Orvak got me tossed out with the rest of the miners. Whenever we get out of this here camp I want to continue my work and I''d like a head start.$B$BUp to the north are the Ruins of Thaurissan. The Dark Irons there have War Reaver patrols, which are powered by Golem Cores. Get me three of them, so we can see what kind of power they contain.'
+WHERE entry = 40465;
+
+UPDATE quest_template SET
+RequestItemsText = 'Battle hard, $N. And do not despair, for despair is evil''s greatest weapon.'
+WHERE entry = 323;
+
+UPDATE quest_template SET
+OfferRewardText = 'The humans have taught us a great deal about the Holy Light and how important their order is to them... and to protecting all of Azeroth. We have a great respect for how steeped in tradition it is, and how just one figure, one sacrifice, can make the difference between pain and salvation.$B$BThe two of us are gonna be working together for a bit. Any time you think you''re ready, come back to me and we''ll see about getting you some training. Can''t have you go out into the world unprepared, now, can we?'
+WHERE entry = 3107;
+
+UPDATE quest_template SET
+Details = 'Some people get all the luck!$B$B<Austil glares at Coleman Farthing across the inn.>$B$B"Oh, I lost my family, the plague took my home from me." On and on and on he goes! We get it! The plague took all of our homes and all of our families. But some of us at least came out of it with a jaw intact!$B$B<Austil cracks a bone back into place.>$B$BIf you want to make yourself useful, bring me some bandages so I can hold what''s left of me together!',
+Objectives = 'Bring 10 Linen Bandages to Austil de Mon, in the Gallows'' End Tavern in Brill. You can make the bandages with the First Aid skill or buy them from someone with the skill.'
+WHERE entry = 60140;
+
+UPDATE quest_template SET
+Details = 'Greetings, $C!$B$BMuch of Orgrimmar''s food supply comes from the sea to the east. Grunts can assist with escorting the fishermen and crabbers through the scorpids and raptors but there''s the issue of sharks that''s getting the better of them.$B$BFishermen have reported aggressive sharks rising in number off the coast and attacking their boats. Go thin their number and be sure to take friends. Rid us of this menace before our food stores dwindle!'
+WHERE entry = 60131;
+
+UPDATE quest_template SET
+Details = 'Psst, buddy. Come here for a second, will you?$B$BHey, how''s it going today? I got myself some lucrative information to share with you, so listen up. You see, there''s these sunken ships, old Alliance boats that supposedly still got fuel left in them and ya know fuel is a valuable commodity for us goblins. They''re just sitting there right off the coast waiting for you!$B$BBring me whatever you manage to salvage from the shipwrecks and I''ll be sure to make it worth your while.'
+WHERE entry = 70051;
+
+UPDATE quest_template SET
+Details = 'What now? Ah, it''s you again. What eggs?$B$BOh, right. Tail lashers. So you want to cook something else besides eggs? I guess I can teach you something.$B$BDo you know where the Southfury River ends its course? This is where you''ll have to go. Why? I''m intending to heighten the stakes here, whelp! Get me those crocolisks'' meat and I''ll teach you how to cook it properly! You''ll find them all over the river so you better get movin'' right away!'
+WHERE entry = 50305;
+
+UPDATE quest_template SET
+OfferRewardText = 'WOW - the dam was WAY bigger than I thought! I could barely hear myself talk from the sound of all the rushing water! Hey, do you think it would ever run out of water? Man, what would happen if that dam ever broke! WOW, that would be scary!$B$BThanks for taking me to see the dam. That was very kind of you. I told you I wouldn''t lean over too far!'
+WHERE entry = 1558;
+
+UPDATE quest_template SET
+Details = 'Well, I guess we figured out why the elves are up here: The whole dang forest up ahead is on fire! And it''s gotta be goblin-made, too.$B$BNo one else uses the kind of oil that burns that long! That''s Undermine stuff, kid. There ain''t no way a gnome could get their hands on it. I don''t know what''s goin'' on in this site, but I don''t like it.$B$BGo out there and bring back some of that oil. Maybe you''ll find some clues about what happened before we got here. If not, at least we''ll have topped off our own oil supplies!'
+WHERE entry = 80103;
+
+UPDATE quest_template SET
+Details = 'Long has it been that we tauren shaman have upheld our ancient traditions. Spirit Walking is a part of our culture, and has been around longer than you could even imagine, young one. It is how we connect with those of the afterlife in the arms of the Earthmother.$B$BIf you wish to begin on this path, then special ingredients will be required for you to first make your connection with the spirit world. To the southwest is a land called Feralas. At Camp Mojache you will find Sage Palerunner. Speak with him, and tell him I was the one who sent you.'
+WHERE entry = 40344;
+
+UPDATE quest_template SET
+RequestItemsText = 'I wonder if my message reached him. I hope I get to see him soon...'
+WHERE entry = 55023;
+
+UPDATE quest_template SET
+OfferRewardText = 'Morpheus sent you from Deathknell?$B$BIt''s been a while since I have seen someone wish to walk this path. You are most welcome, that is if you can keep up with the training.'
+WHERE entry = 40260;
+
+UPDATE quest_template SET
+Objectives = 'Collect 6 Sprigs of Auribloom and bring them back to Denalah Silverpoint at Hawk''s Vigil.'
+WHERE entry = 40368;
+
+UPDATE quest_template SET
+Details = 'Lake Falathim was a place I spent much of my youth, a place of relaxation and fond memories. Now, it is overrun by the Saltspittle murlocs, who attacked with ferocity and without remorse. Some died during the attack, but I was lucky enough to escape.$B$BIn my haste to flee, I left behind an item of extreme value to me: a book left to me by my Uncle who died during the Battle of Hyjal. In it he wrote about his technique in battle and his experiences regarding the war. I''ve found much knowledge and wisdom from that book, and I desire to know everything that he had jotted down before his passing.$B$BTravel to Lake Falathim to the southwest, and find my belongings. While you are there, I ask that you avenge my kin: Slay the Saltspittle and bring me their fins.'
+WHERE entry = 40230;
+
+UPDATE quest_template SET
+Objectives = 'Gather 10 Bloodclaw Pels from the Bloodclaw Worgen for Corporal Ranworth at Ravenshire in Gilneas.'
+WHERE entry = 40926;
+
+UPDATE quest_template SET
+Details = 'Our grunts found one of these Night Elves stalking about. The deformed creature had a letter on its corpse. I will spare you the disgust I had while trying to decipher that abomination of a language.$B$BAbove the canyon you should be able to find a Barrow Den, one of the holes these knife-eared mongrels like to dig. Be mindful not to go to the Dor''Danil Barrow Den: The one you seek is found in the ridge. From what I could gather of this piece of paper, a great menace can be found at its lowest level.$B$BFind out what''s hiding there and return to me as swift as the wind.'
+WHERE entry = 70029;
+
+UPDATE quest_template SET
+Details = 'So it is true. My soldiers, who I thought unwavering in their loyalty, were deserters. I wonder how many more hide in our midst.$B$BFor now, let''s review the information: Caldwell and Hallan were deserters. They killed Paval Reethe when he was questioned by the Horde. Did they do it just to spite the Horde, or... Did he know something...$B$BOnce again, our findings lead us to the deserters in the swamp. Well, I doubt that we could make them talk. They resent Theramore, perhaps more than they hate the Horde. Any mindless attack on their base will not provide us with answers.$B$BWe need to turn to magic. Forbidden magic. If we had something that belonged to Paval Reethe, we could commune with his spirit. It is a dangerous practice that many would consider unethical.$B$BIn this case, I feel the ends justify the means, and fortunately, we have his badge. Since he passed recently with unfinished business, his spirit may still be in Azeroth.$B$BGo to the Mage Tower in Theramore and explain the situation to Magus Halister.'
+WHERE entry = 40560;
