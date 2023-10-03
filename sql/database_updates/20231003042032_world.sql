316183d0685aaf26dfe066f03af69cdb17de4cf4
Torta <66763404+slowtorta@users.noreply.github.com>
2023-10-03 12:48:02 +0200

    Darunovi's spell fixes.

--- sql/database_updates/20231003042032_world.sql
+++ sql/database_updates/20231003042032_world.sql
@@ -6,3 +6,8 @@ update item_template set spellid_1 = 8815 where entry = 17015; -- Dark Iron Reav
 update item_template set spellid_1 = 15874, spellid_2 = 7578, spellid_2 = 7527 where entry = 14551; -- Edgemaster\'s Handguards
 update item_template set name = "Tome of Teleportation: Theramore" where entry = 92001; -- Theramore Tome
 update item_template set name = "Tome of Teleportation: Stonard" where entry = 92002; -- Stonard Tome
+
+-- fix dumb desc
+update spell_template SET description = 'Increases the melee attack power bonus of your Seal of the Crusader by $s1 and the Holy damage increase of your Crusader Strike by $s2 per stack.' WHERE entry = 28852; -- Improved Seal of the Crusader
+update spell_template SET EffectBasePoints1 = 1, EffectBasePoints2 = 2, effectApplyAuraName2 = 71, Effect2 = 6, EffectDieSides2 = 1, effectBaseDice2 = 1, Description = 'Increases your pet\'s melee and spell critical strike chance by 2%.' WHERE entry = 27042; -- Increased Pet Crit +2%
+update spell_template SET description = 'When struck in combat has a 2% chance of increasing all party member\'s attack and casting speed by $18946s1% for $18946d.' WHERE entry = 20847; -- The Lion Horn of Stormwind (Equip Spell)
