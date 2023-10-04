ec7c71d95b53d13a5933f7911a0cbe9aa2594e29
redmagejoe <redmagejoe@yahoo.com>
2023-10-04 17:10:07 -0400

    Dragunovi item fixes
    Bro how do you make a shield and not put block value on it as the first stat?

---
+++ sql/database_updates/20231004210913_world.sql
@@ -0,0 +1,4 @@
+-- troublesome items
+update item_template set spelltrigger_1 = 2, spellppmrate_1 = 1, spellid_1 = 17308 where entry = 61349; -- Dwarven Battle Bludgeon
+update item_template set block = 28 where entry = 61314; -- Marshal's Blocker
+update item_template set armor = 12 where entry = 80818; -- Boarskin Cape
