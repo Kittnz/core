bb361510b1e6a320dacbb0075458b93c61eb0395
ratkosrb <ratkomladic2@abv.bg>
2023-10-05 03:35:56 +0300

    Fix startup errors caused by conditions.

---
+++ sql/database_updates/20231005003440_world.sql
@@ -0,0 +1,4 @@
+-- 152: Current Time Is Between 18:0 And 23:59
+UPDATE `conditions` SET `value3`=23, `value4`=59 WHERE `condition_entry`=152;
+-- 19000: Target Has 1 Stacks Of Item 19470 In Inventory
+UPDATE `conditions` SET `value2`=1 WHERE `condition_entry`=19000;
