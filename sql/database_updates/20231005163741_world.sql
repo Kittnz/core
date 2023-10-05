1ca93c912f4e1af8115bcfbec83eb1a6ad9867da
Torta <66763404+slowtorta@users.noreply.github.com>
2023-10-05 18:40:57 +0200

    Fixed <error> on Maws' death.

---
+++ sql/database_updates/20231005163741_world.sql
@@ -0,0 +1,3 @@
+REPLACE INTO `mangos_string` (`entry`, `content_default`) VALUES (11160, 'The beast returns from whence it came. The wrath of Neptulon has subsided.');
+
+UPDATE `mangos_string` SET `content_loc4` = '這頭野獸回去了，奈普圖隆的憤怒也已平息。' WHERE `entry` = 11160;
