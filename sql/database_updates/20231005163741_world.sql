325f772a6a765dd77b16199365d5333462523ef5
Torta <66763404+slowtorta@users.noreply.github.com>
2023-10-06 01:00:34 +0200

    Dragu's item changes.

--- sql/database_updates/20231005163741_world.sql
+++ sql/database_updates/20231005163741_world.sql
@@ -1,3 +1,8 @@
 REPLACE INTO `mangos_string` (`entry`, `content_default`) VALUES (11160, 'The beast returns from whence it came. The wrath of Neptulon has subsided.');
 
 UPDATE `mangos_string` SET `content_loc4` = '這頭野獸回去了，奈普圖隆的憤怒也已平息。' WHERE `entry` = 11160;
+
+-- fix
+update item_template set spellid_1 = 18107, spellppmrate_1 = 1.5 where entry = 61318; -- Fleshslasher
+update item_template set block = 28 where entry = 61314; -- Marshal's Blocker
+update item_template set stat_type1 = 7 where entry = 16988; -- Fiery Chain Shoulders;
