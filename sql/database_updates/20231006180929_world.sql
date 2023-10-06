35b1c4eb9fe9ad59622ec83fe4f4050c21e0c32c
Torta <66763404+slowtorta@users.noreply.github.com>
2023-10-06 20:09:54 +0200

    Dragu's item fixes.

---
+++ sql/database_updates/20231006180929_world.sql
@@ -0,0 +1,3 @@
+-- pain
+update item_template set stat_value1 = 15, stat_value2 = 18, stat_value3 = 15 where entry = 61756; -- Nordanaar Guardian Spaulders
+update item_template set stat_value1 = 11, stat_type1 = 7, stat_value2 = 0, spellid_1 = 15464, spellid_3 = 7523, spelltrigger_3 = 1, spellid_2 = 9331, nature_res = 7 where entry = 61523; -- Crystal Sword of the Blossom
