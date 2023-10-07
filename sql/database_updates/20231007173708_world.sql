22c6a7daaf907d0bb09a9cb08b443f86ca5ee2be
redmagejoe <redmagejoe@yahoo.com>
2023-10-07 15:53:28 -0400

    Fix armor value on Kara10 plate legs

--- sql/database_updates/20231007173708_world.sql
+++ sql/database_updates/20231007173708_world.sql
@@ -7,6 +7,8 @@ update item_template set max_count = 1 where entry = 61251; -- Kara10 ring
 update item_template set quality = 3 where entry = 8547; -- smelling salts recipe
 update item_template set block = 0 where entry = 61510; -- stonehewn shoulders
 update item_template set stat_type1 = 3 where entry = 61239; -- ancient jade leggings
+update item_template set armor = 618 where entry = 61244; -- kara10 plate legs
+
 
 -- Fix Kara instance binds:
 
