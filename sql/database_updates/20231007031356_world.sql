2000091b01c4c7135bb2db50eb74604f229cfd04
Torta <66763404+slowtorta@users.noreply.github.com>
2023-10-07 19:55:37 +0200

    Fix SQL.

--- sql/database_updates/20231007031356_world.sql
+++ sql/database_updates/20231007031356_world.sql
@@ -3,4 +3,4 @@ UPDATE quest_template SET ZoneOrSort = 5179 WHERE entry = 40974;
 update item_template set stat_value1 = 15, stat_value2 = 18, stat_value3 = 15 where entry = 61756; -- Nordanaar Guardian Spaulders
 update item_template set stat_value1 = 11, stat_type1 = 7, stat_value2 = 0, spellid_1 = 15464, spellid_3 = 7523, spelltrigger_3 = 1, spellid_2 = 9331, nature_res = 7 where entry = 61523; -- Crystal Sword of the Blossom
 update item_template set delay = 1700, dmg_min1 = 54, dmg_max1 = 100 where entry = 61001; -- Claw of the Infinite
-update item_template set max_amount = 1, spelltrigger_4 = 1 where entry = 61205; -- Ring of Nature's Duality
+update item_template set max_count = 1, spelltrigger_4 = 1 where entry = 61205; -- Ring of Nature's Duality
--- sql/database_updates/20231007173708_world.sql
+++ sql/database_updates/20231007173708_world.sql
@@ -1,1 +1,8 @@
 UPDATE `areatrigger_teleport` SET `required_condition` = 30003 WHERE `id` = 5017;
+
+-- fixes
+update item_template set spellppmrate_1 = 2 where entry = 12798; -- Annihilator
+update item_template set max_count = 1 where entry = 61243; -- Kara10 vial
+update item_template set max_count = 1 where entry = 61251; -- Kara10 ring
+update item_template set quality = 3 where entry = 8547; -- smelling salts recipe
+update item_template set block = 0 where entry = 61510; -- stonehewn shoulders
