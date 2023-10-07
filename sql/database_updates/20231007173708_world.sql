2c06fe241d927c7025cc11020a1bb0071f82eace
redmagejoe <redmagejoe@yahoo.com>
2023-10-07 15:07:08 -0400

    Fix stat type on Ancient Jade Leggings

--- sql/database_updates/20231007173708_world.sql
+++ sql/database_updates/20231007173708_world.sql
@@ -6,6 +6,7 @@ update item_template set max_count = 1 where entry = 61243; -- Kara10 vial
 update item_template set max_count = 1 where entry = 61251; -- Kara10 ring
 update item_template set quality = 3 where entry = 8547; -- smelling salts recipe
 update item_template set block = 0 where entry = 61510; -- stonehewn shoulders
+update item_template set stat_type1 = 3 where entry = 61239; -- ancient jade leggings
 
 -- Fix Kara instance binds:
 
