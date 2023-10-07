5ee3c266183293630288dc1ef9af59378b9d4371
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-10-07 15:44:46 +0300

    Quest "Securing Gilneas I" grammar fix.

--- sql/database_updates/20231007103313_world.sql
+++ sql/database_updates/20231007103313_world.sql
@@ -7,4 +7,6 @@ update quest_template set ReqItemId1 = 61817 where entry = 40860;
 REPLACE INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES
 (61399, 61817, -80, 0, 1, 1, 0);
 -- Fix deez nuts.
-update item_template set spellppmrate_1 = 1 where entry = 12798; -- Annihilator
+update item_template set spellppmrate_1 = 1 where entry = 12798; -- Annihilator.
+-- Quest "Securing Gilneas I" grammar fix.
+update quest_template set details = 'The land is tainted with the foul mark of worgen that tread through our lands unopposed. The royalists have seen fit to punish the people by letting their kind spread like a disease.$B$BEven now the worgen reach as far south as the Overgrown Acre just to the northwest of here. If they will not put an end to this threat, then we must. Find the Bloodclaw tribe that stalks within Gilneas, hunt them, and recover ten of their pelts.' where entry = 40926;
