a897c2d0f51851b324cb4c7bd7739dee9a716dcb
Torta <66763404+slowtorta@users.noreply.github.com>
2023-10-07 20:43:18 +0200

    Fix Kara instance boss savses.

--- sql/database_updates/20231007173708_world.sql
+++ sql/database_updates/20231007173708_world.sql
@@ -6,3 +6,7 @@ update item_template set max_count = 1 where entry = 61243; -- Kara10 vial
 update item_template set max_count = 1 where entry = 61251; -- Kara10 ring
 update item_template set quality = 3 where entry = 8547; -- smelling salts recipe
 update item_template set block = 0 where entry = 61510; -- stonehewn shoulders
+
+-- Fix Kara instance binds:
+
+update creature_template set flags_extra = 1  where entry in (61225,61221,61223,61222,61224);
