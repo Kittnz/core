9dccd9a44cef06c23e4406e366e907860fb024ad
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-10-08 16:24:00 +0300

    Item 61198, 20381: Drop chance changed.

--- sql/database_updates/20231008124729_world.sql
+++ sql/database_updates/20231008124729_world.sql
@@ -7,3 +7,6 @@ update item_template set max_count = 1 where entry = 61278; -- vampiric kris
 update item_template set stat_value1 = 25 where entry = 61185; -- Dawnstone Hammer
 update item_template set armor = 618 where entry = 61244; -- kara10 plate legs
 update item_template set stat_value1 = 12, stat_value2 = 12 where entry = 61453; -- anasterian's legacy
+-- For the creatures 60742,60743,60744,60745,60746,61212 set the drop chance of 61198 to 8.1%, set the drop chance of 20381 to 4.5%
+update creature_loot_template set ChanceOrQuestChance = 8.1 where item = 61198 and entry in (60742,60743,60744,60745,60746,61212);
+update creature_loot_template set ChanceOrQuestChance = 4.5 where item = 20381 and entry in (60742,60743,60744,60745,60746,61212);
