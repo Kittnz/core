5087eaa6b49a0672f256990fc6b1c6d37708929d
redmagejoe <redmagejoe@yahoo.com>
2023-10-08 18:55:21 -0400

    Fix Dead Until Dark quest chain prer-eqs
    Quest ID 40879 was improperly requiring quest ID 40877 as a prereq rather than 40878, and quest ID 40880 was improperly requiring 40878 as a prereq rather than 40879.

--- sql/database_updates/20231008211542_world.sql
+++ sql/database_updates/20231008211542_world.sql
@@ -1,2 +1,6 @@
 -- Remove broken engineering recipes from trainer.
 DELETE FROM `npc_trainer` WHERE `spell` IN (19845, 19846, 23088, 49526, 49527, 49528);
+
+-- Fix Dead Until Dark quest chain requirements
+UPDATE quest_template SET PrevQuestId = 40879 WHERE entry = 40880;
+UPDATE quest_template SET PrevQuestId = 40878 WHERE entry = 40879;
