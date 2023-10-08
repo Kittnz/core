ff6d1b8ae6b43f8557884297f944e02b4a52906b
ratkosrb <ratkomladic2@abv.bg>
2023-10-09 00:24:59 +0300

    Item fixes from Dragunovi.

---
+++ sql/database_updates/20231008212445_world.sql
@@ -0,0 +1,9 @@
+-- fix
+update item_template set spellcharges_1 = -1 where entry = 83505;
+update item_template set spellcharges_1 = -1 where entry = 83503;
+update item_template set spellcharges_1 = -1 where entry = 83504;
+update item_template set spellcharges_1 = -1 where entry = 83501;
+update item_template set spellcharges_1 = -1 where entry = 83506;
+update item_template set spellcharges_1 = -1 where entry = 83502;
+update item_template set spellcharges_1 = -1 where entry = 83502;
+update item_template set bonding = 1 where entry = 84504;
