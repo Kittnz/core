08944bc8bd2fe681489e10d62e2b952ae2142434
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-10-08 16:23:05 +0300

    NPC "Dane Lindgren": Recipe "Rough Grinding Stone" added.

---
+++ sql/database_updates/20231008124729_world.sql
@@ -0,0 +1,2 @@
+-- Dane Lindgren <Journeyman Blacksmith> in Stormwind is missing the Rough Grinding Stone Recipe.
+REPLACE INTO `npc_trainer` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillvalue`, `reqlevel`) VALUES (957, 3341, 90, 164, 25, 0);
