fbaad5e5565c8345fdf04b5830748155a86098bc
redmagejoe <redmagejoe@yahoo.com>
2023-10-10 15:10:56 -0400

    QA quest fixes
    A few quests in the Dead Until Dark quest chain for Horde in Gilneas were improperly requiring quests 2 steps back rather than 1 step back.
    Welcome to Amani'Alor quest was not attached to any NPC, preventing further reputation repeatables from being accessible.
    Anchor's End quest was improperly offered right away rather than near the end of the Anchor's Edge quest chain from Razor Hill.

--- sql/database_updates/20231008211542_world.sql
+++ sql/database_updates/20231008211542_world.sql
@@ -4,3 +4,10 @@ DELETE FROM `npc_trainer` WHERE `spell` IN (19845, 19846, 23088, 49526, 49527, 4
 -- Fix Dead Until Dark quest chain requirements
 UPDATE quest_template SET PrevQuestId = 40879 WHERE entry = 40880;
 UPDATE quest_template SET PrevQuestId = 40878 WHERE entry = 40879;
+
+-- Fix missing quest relations on NPC Mystic Guay'Jin
+REPLACE INTO creature_questrelation VALUES (8081, 80313);
+REPLACE INTO creature_involvedrelation VALUES (80801, 80313);
+
+-- Fix missing prereq from quest Anchor's End
+UPDATE quest_template SET PrevQuestId = 40671 WHERE entry = 40672;
