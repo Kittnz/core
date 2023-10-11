662d7770bfc5f329e2645e0df8469a032125a04d
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-10-11 13:37:16 +0300

    Item fixes by Dragunovi.

---
+++ sql/database_updates/20231011103514_world.sql
@@ -0,0 +1,4 @@
+-- whops
+update item_template set stat_value1 = 15, stat_value2 = 20, spellid_2 = 14799, spellid_3 = 23727 where entry = 16928; -- Nemesis Gloves
+update item_template set stat_value3 = 13, spellid_1 = 13679 where entry = 61212; -- Sanctum Bark Wraps
+update item_template set spellid_3 = 23727 where entry = 84504; -- Ring of Nature's Duality
