3fe9034cd03a5bd9fd39d58e8020e806c204b750
redmagejoe <redmagejoe@yahoo.com>
2023-10-12 02:10:30 -0400

    Ornate Bloodstone Dagger desc QA
    Improved the grammar of Burning Hatred spell on Ornate Bloodstone Dagger and brought it into parity with Engulfing Shadows on Eternal Darkness.

--- sql/database_updates/20231012013230_world.sql
+++ sql/database_updates/20231012013230_world.sql
@@ -3,3 +3,6 @@ UPDATE item_template SET Name = 'Tome of Teleportation: Theramore' WHERE entry =
 
 -- Fix missing quest relations on NPC Mystic Guay'Jin
 REPLACE INTO creature_questrelation VALUES (80801, 80313);
+
+-- Improve grammar of Burning Hatred / Parity with Engulfing Shadows
+UPDATE spell_template SET Description = 'Chance on landing a damaging spell to deal 250 Fire damage while sacrificing 250 health.' WHERE entry = 48003;
