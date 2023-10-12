0474ea2a04f2d23f654cd4611a3a8b75ef4e8c01
Torta <info.turtlewow@gmail.com>
2023-10-12 11:05:14 +0200

    Added a way to quit Vagrant's Challenge once.

---
+++ sql/database_updates/20231012083635_world.sql
@@ -0,0 +1,3 @@
+-- A way to end Vagrant's Challenge:
+
+REPLACE INTO `broadcast_text` (`entry`, `male_text`, `female_text`, `chat_type`, `sound_id`, `language_id`, `emote_id1`, `emote_id2`, `emote_id3`, `emote_delay1`, `emote_delay2`, `emote_delay3`) VALUES (67035, 'I would like to end the Vagrant\'s Challenge once and for all.', NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0);
