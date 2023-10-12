2178c43f96efe95cd4b7ea35b8a6da58594596ba
slowtorta <slowtorta@gmail.com>
2023-10-12 11:09:36 +0100

    Direct world update.

---
+++ sql/database_updates/20231012105405_world.sql
@@ -0,0 +1,8 @@
+
+-- Changes by TORTA
+DELETE FROM creature WHERE guid=2561607;
+DELETE FROM creature_addon WHERE guid=2561607;
+DELETE FROM creature_movement WHERE id=2561607;
+DELETE FROM game_event_creature WHERE guid=2561607;
+DELETE FROM game_event_creature_data WHERE guid=2561607;
+DELETE FROM creature_battleground WHERE guid=2561607;
