61a0bcb2090ce9f9b00825c4269ff7c858fdc7cc
redmagejoe <redmagejoe@yahoo.com>
2023-10-11 23:34:18 -0400

    Fix Welcome to Amani'Alor starter NPC
    I'd love for shit I fixed on PTR months ago to stay fixed so I don't have an aneurysm and make hasty commits with typos.

--- sql/database_updates/20231012013230_world.sql
+++ sql/database_updates/20231012013230_world.sql
@@ -1,2 +1,5 @@
 UPDATE item_template SET Name = 'Tome of Teleportation: Stonard' WHERE entry = 92003;
 UPDATE item_template SET Name = 'Tome of Teleportation: Theramore' WHERE entry = 92001;
+
+-- Fix missing quest relations on NPC Mystic Guay'Jin
+REPLACE INTO creature_questrelation VALUES (80801, 80313);
