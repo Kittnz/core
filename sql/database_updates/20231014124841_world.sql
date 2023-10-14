bdaf0ccef33f8c631b0b18f82fd1326c157ebe3c
ratkosrb <ratkomladic2@abv.bg>
2023-10-14 15:49:32 +0300

    Spawn missing creature Haruhiko Yoshino.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/4734

---
+++ sql/database_updates/20231014124841_world.sql
@@ -0,0 +1,2 @@
+-- Spawn missing creature Haruhiko Yoshino.
+INSERT INTO `creature` (`guid`, `id`, `id2`, `id3`, `id4`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecsmin`, `spawntimesecsmax`, `wander_distance`, `health_percent`, `mana_percent`, `movement_type`, `spawn_flags`, `visibility_mod`) VALUES (60004, 61411, 0, 0, 0, 1, -3162.93, -4193.36, 5.66708, 4.75413, 120, 120, 0, 100, 100, 0, 0, 0);
