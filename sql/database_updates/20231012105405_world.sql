bdf2e901949806c283deb4253a27e211bff521d6
Torta <info.turtlewow@gmail.com>
2023-10-15 00:25:53 +0200

    Revert "Direct world update."
    This reverts commit 8cc6a1f17f64c2565c03134d7a81226f200fc687.

--- sql/database_updates/20231012105405_world.sql
+++ sql/database_updates/20231012105405_world.sql
@@ -6,8 +6,3 @@ DELETE FROM creature_movement WHERE id=2561607;
 DELETE FROM game_event_creature WHERE guid=2561607;
 DELETE FROM game_event_creature_data WHERE guid=2561607;
 DELETE FROM creature_battleground WHERE guid=2561607;
-
--- Changes by DRAGU
-DELETE FROM gameobject WHERE guid = '4005021';
-DELETE FROM game_event_gameobject WHERE guid = '4005021';
-DELETE FROM gameobject_battleground WHERE guid = '4005021';
