51be55f271131be41f6e9e454450f5de2c9c4687
Torta <info.turtlewow@gmail.com>
2023-10-15 02:39:31 +0200

    More map changes.

--- sql/database_updates/20231015002043_world.sql
+++ sql/database_updates/20231015002043_world.sql
@@ -1,3 +1,10 @@
 
 -- This edit is required by /who to display the correct name!
+
+-- Change all Zones and Areas on Map 35 from 717 to 5087
+-- Change all Zones and Areas on Map 34 from 5087 to 717
+-- Change all Zones and Areas on Map 800 from 10 to whatever Karazhan Crypt's Zone and Area IDs should be.
+
 UPDATE `map_template` SET `linked_zone` = 5087 WHERE `entry` = 35;
+UPDATE `map_template` SET `linked_zone` = 717 WHERE `entry` = 34;
+UPDATE `map_template` SET `linked_zone` = 5086 WHERE `entry` = 800;
