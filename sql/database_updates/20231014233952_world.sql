6c5ef6bbf15a0bcb44ec352886a90204d8d300c8
slowtorta <slowtorta@gmail.com>
2023-10-16 23:26:29 +0100

    Direct world update.

--- sql/database_updates/20231014233952_world.sql
+++ sql/database_updates/20231014233952_world.sql
@@ -4,3 +4,10 @@ REPLACE INTO `gameobject` VALUES ( 5016904, 1000031, 1, 16226, 16251.1, 12.6866,
 
 -- Changes by GHEOR
 REPLACE INTO `creature` VALUES (2579223,80236,0,0,0,0,3883.1,-2488.3,28.7268,5.07987,120,120,0,100,100,0,0,0);
+UPDATE `creature` SET `position_x` = 3882.559326, `position_y` = -2491.491211, `position_z` = 28.725462, `orientation` = 5.752954 WHERE `guid` = 2579223;
+DELETE FROM creature WHERE guid=2579223;
+DELETE FROM creature_addon WHERE guid=2579223;
+DELETE FROM creature_movement WHERE id=2579223;
+DELETE FROM game_event_creature WHERE guid=2579223;
+DELETE FROM game_event_creature_data WHERE guid=2579223;
+DELETE FROM creature_battleground WHERE guid=2579223;
