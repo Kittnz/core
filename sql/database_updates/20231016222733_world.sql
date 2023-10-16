1d84b6fb204a4c7d6a127743f4dd0b5aa4ceb639
redmagejoe <redmagejoe@yahoo.com>
2023-10-16 18:57:07 -0400

    Fix SQL typo

--- sql/database_updates/20231016222733_world.sql
+++ sql/database_updates/20231016222733_world.sql
@@ -1,2 +1,2 @@
 -- Fix faction locking of Gadgetzan egg quests
-UPDATE quest_template SET RequiredRaces = 0, RewRepValue1 = 0, RewRepValue2 = 0 WHERE entry IN (2741, 2747, 2748, 279, 2750);
+UPDATE quest_template SET RequiredRaces = 0, RewRepValue1 = 0, RewRepValue2 = 0 WHERE entry IN (2741, 2747, 2748, 2749, 2750);
