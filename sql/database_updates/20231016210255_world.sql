5580b6916c86a962664788bc610fa59d3fa989ed
Torta <info.turtlewow@gmail.com>
2023-10-16 23:03:18 +0200

    Removed custom objects from GM Island.

---
+++ sql/database_updates/20231016210255_world.sql
@@ -0,0 +1,4 @@
+delete from gameobject where map = 1 and ( position_x between 16065 and 16418) and (position_y between 16090 and 16550);
+delete from creature where map = 1 and  ( position_x between 16065 and 16418) and (position_y between 16090 and 16550);
+delete from gameobject where guid = 255906;
+delete from gameobject where guid = 255904;
