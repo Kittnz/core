cd50a1b7ffbea64c85ce30fb7d166c9c49551a26
redmagejoe <redmagejoe@yahoo.com>
2023-10-17 02:42:31 -0400

    Fixed item requirements of Bloodstone Dagger quest
    Quest ID 41119 calls for 50 Tokens of Blood, but the actual requirement was Arena Marks of Honor.  Item requirement has been updated.

--- sql/database_updates/20231016222733_world.sql
+++ sql/database_updates/20231016222733_world.sql
@@ -1,2 +1,5 @@
 -- Fix faction locking of Gadgetzan egg quests
 UPDATE quest_template SET RequiredRaces = 0, RewRepValue1 = 0, RewRepValue2 = 0 WHERE entry IN (2741, 2747, 2748, 2749, 2750);
+
+-- Fix incorrect item requirements for Ornate Bloodstone Dagger Plans quest
+UPDATE quest_template SET ReqItemId1 = 61794 WHERE entry = 41119;
