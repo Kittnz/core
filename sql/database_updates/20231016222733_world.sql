18c6e5dd3eb2488101e8316e540bc6a93cd36262
redmagejoe <redmagejoe@yahoo.com>
2023-10-17 07:54:32 -0400

    Chocolate Fish quest fixes
    Added EXP, Level 60 Money Reward, and proper quest chaining to the Chocolate Fish quest chain
    Fixed the incorrect quest Type on all quests in the chain

--- sql/database_updates/20231016222733_world.sql
+++ sql/database_updates/20231016222733_world.sql
@@ -9,3 +9,13 @@ UPDATE item_template SET Name = 'Recipe: Alchemist''s Stone' WHERE entry = 13517
 
 -- Fix missing prerequisite for Azuregos's Magical Ledger
 UPDATE quest_template SET PrevQuestId = 8555 WHERE entry = 8575;
+
+-- Various fixes for Chocolate Fish quest
+UPDATE quest_template SET RewXP = 1550, RewMoneyMaxLevel = 9300, NextQuestInChain = 40999 WHERE entry = 40998;
+UPDATE quest_template SET RewXP = 5150, RewMoneyMaxLevel = 30900, NextQuestInChain = 41000 WHERE entry = 40999;
+UPDATE quest_template SET RewXP = 5320, RewMoneyMaxLevel = 31920, NextQuestInChain = 41001 WHERE entry = 41000;
+UPDATE quest_template SET RewXP = 1550, RewMoneyMaxLevel = 9300, NextQuestInChain = 41002 WHERE entry = 41001;
+UPDATE quest_template SET RewXP = 1550, RewMoneyMaxLevel = 9300, NextQuestInChain = 41003 WHERE entry = 41002;
+UPDATE quest_template SET RewXP = 2680, RewMoneyMaxLevel = 16080, NextQuestInChain = 41004 WHERE entry = 41003;
+UPDATE quest_template SET RewXP = 3150, RewMoneyMaxLevel = 18900 WHERE entry = 41004;
+UPDATE quest_template SET Type = 62 WHERE entry IN (40998, 41001, 41002, 41003, 41004);
