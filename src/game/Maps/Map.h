cdcabe05333790fdca0a95e3500545da948a208e
Torta <info.turtlewow@gmail.com>
2023-10-19 01:17:29 +0200

    Allowed mounting in Emerald Sanctum.

--- src/game/Maps/Map.h
+++ src/game/Maps/Map.h
@@ -103,7 +103,7 @@ struct MapEntry
     bool Instanceable() const { return mapType == MAP_INSTANCE || mapType == MAP_RAID || mapType == MAP_BATTLEGROUND; }
     bool IsRaid() const { return mapType == MAP_RAID; }
     bool IsBattleGround() const { return mapType == MAP_BATTLEGROUND; }
-    bool IsMountAllowed() const { return !IsDungeon() || id == 309 || id == 209 || id == 509 || id == 269 || id == 802; }
+    bool IsMountAllowed() const { return !IsDungeon() || id == 309 || id == 209 || id == 509 || id == 269 || id == 802 || id == 807; }
     bool IsContinent() const { return id == 0 || id == 1; }
 };
 
