9939274ca8f2c0c39cb9423953d9ab42c88b23d4
Torta <info.turtlewow@gmail.com>
2023-10-20 00:42:24 +0200

    Updated all Flurry' descriptions to specity melee crit. strike.

--- sql/database_updates/20231019223902_world.sql
+++ sql/database_updates/20231019223902_world.sql
@@ -1,1 +1,24 @@
 UPDATE `spell_template` SET `description` = 'Teaches Divine Spirit (Rank 1).' WHERE `entry` = 47324;
+
+UPDATE `spell_template` SET `name` = 'Flurry', `description` = 'Increases your attack speed by $12966s1% for your next 3 swings after dealing a melee critical strike.' WHERE `entry` = 12319;
+UPDATE `spell_template` SET `name` = 'Flurry', `description` = '' WHERE `entry` = 12966;
+UPDATE `spell_template` SET `name` = 'Flurry', `description` = '' WHERE `entry` = 12967;
+UPDATE `spell_template` SET `name` = 'Flurry', `description` = '' WHERE `entry` = 12968;
+UPDATE `spell_template` SET `name` = 'Flurry', `description` = '' WHERE `entry` = 12969;
+UPDATE `spell_template` SET `name` = 'Flurry', `description` = '' WHERE `entry` = 12970;
+UPDATE `spell_template` SET `name` = 'Flurry', `description` = 'Increases your attack speed by $12967s1% for your next 3 swings after dealing a melee critical strike.' WHERE `entry` = 12971;
+UPDATE `spell_template` SET `name` = 'Flurry', `description` = 'Increases your attack speed by $12968s1% for your next 3 swings after dealing a melee critical strike.' WHERE `entry` = 12972;
+UPDATE `spell_template` SET `name` = 'Flurry', `description` = 'Increases your attack speed by $12969s1% for your next 3 swings after dealing a melee critical strike.' WHERE `entry` = 12973;
+UPDATE `spell_template` SET `name` = 'Flurry', `description` = 'Increases your attack speed by $12970s1% for your next 3 swings after dealing a melee critical strike.' WHERE `entry` = 12974;
+UPDATE `spell_template` SET `name` = 'Flurry', `description` = 'Increases your attack speed by 30% for your next 3 swings after dealing a melee critical strike.' WHERE `entry` = 15088;
+UPDATE `spell_template` SET `name` = 'Flurry', `description` = 'Increases your attack speed by $16257s1% for your next 3 swings after dealing a melee critical strike.' WHERE `entry` = 16256;
+UPDATE `spell_template` SET `name` = 'Flurry', `description` = '' WHERE `entry` = 16257;
+UPDATE `spell_template` SET `name` = 'Flurry', `description` = '' WHERE `entry` = 16277;
+UPDATE `spell_template` SET `name` = 'Flurry', `description` = '' WHERE `entry` = 16278;
+UPDATE `spell_template` SET `name` = 'Flurry', `description` = '' WHERE `entry` = 16279;
+UPDATE `spell_template` SET `name` = 'Flurry', `description` = '' WHERE `entry` = 16280;
+UPDATE `spell_template` SET `name` = 'Flurry', `description` = 'Increases your attack speed by $16277s1% for your next 3 swings after dealing a melee critical strike.' WHERE `entry` = 16281;
+UPDATE `spell_template` SET `name` = 'Flurry', `description` = 'Increases your attack speed by $16278s1% for your next 3 swings after dealing a melee critical strike.' WHERE `entry` = 16282;
+UPDATE `spell_template` SET `name` = 'Flurry', `description` = 'Increases your attack speed by $16279s1% for your next 3 swings after dealing a melee critical strike.' WHERE `entry` = 16283;
+UPDATE `spell_template` SET `name` = 'Flurry', `description` = 'Increases your attack speed by $16280s1% for your next 3 swings after dealing a melee critical strike.' WHERE `entry` = 16284;
+UPDATE `spell_template` SET `name` = 'Flurry', `description` = '' WHERE `entry` = 17687;
