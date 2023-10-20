ee63c8015ad4196d5d709bcedc52df4c59306d99
redmagejoe <redmagejoe@yahoo.com>
2023-10-20 17:38:46 -0400

    Link Silithus bonfire to Midsummer event

--- sql/database_updates/20231020211630_world.sql
+++ sql/database_updates/20231020211630_world.sql
@@ -1,3 +1,5 @@
 UPDATE gameobject SET spawntimesecsmin = 300, spawntimesecsmax = 300 WHERE id IN (180456, 180518, 180529, 180544, 180549, 180564);
 UPDATE gameobject SET spawntimesecsmin = 900, spawntimesecsmax = 900 WHERE id IN (180461, 180502, 180534, 180554, );
 UPDATE gameobject SET spawntimesecsmin = 10800, spawntimesecsmax = 10800 WHERE id IN (180466, 180539, 180559);
+
+REPLACE INTO game_event_gameobject (guid, event) VALUES (49313, 1);
