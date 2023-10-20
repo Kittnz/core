e6afa17713ffb6bc2f4425d4e9a9c8a6b159a2d2
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-10-20 21:37:33 +0300

    Item fixes by Dragunovi.

---
+++ sql/database_updates/20231020132948_world.sql
@@ -0,0 +1,3 @@
+-- fml
+update item_template set spellid_3 = 23727, spellid_4 = 0 where entry = 61205; -- Ring of Nature's Duality
+update item_template set spellid_3 = 0 where entry = 84504; -- Lasher's Whip
