e158fef1747060716d1f7f197bcc27a53a478407
redmagejoe <redmagejoe@yahoo.com>
2023-10-20 17:22:54 -0400

    Fix / normalize spawn timers of Silithus Wind Stones
    Long-standing report about southern Twilight Camp in Silithus not respawning its Wind Stones. Upon further investigation, it seems that some spawn timers were not set, and several were inconsistent.
    
    Lesser Wind Stones will now respawn every 5 minutes.
    Wind Stones will now respawn every 15 minutes.
    Greater Wind Stones will now respawn every 3 hours.

---
+++ sql/database_updates/20231020211630_world.sql
@@ -0,0 +1,3 @@
+UPDATE gameobject SET spawntimesecsmin = 300, spawntimesecsmax = 300 WHERE id IN (180456, 180518, 180529, 180544, 180549, 180564);
+UPDATE gameobject SET spawntimesecsmin = 900, spawntimesecsmax = 900 WHERE id IN (180461, 180502, 180534, 180554, );
+UPDATE gameobject SET spawntimesecsmin = 10800, spawntimesecsmax = 10800 WHERE id IN (180466, 180539, 180559);
