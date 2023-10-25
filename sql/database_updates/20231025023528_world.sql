2fadfddf9cd03e13391d47e9576a4ca94f3da031
Jameyboor <jamey_D@hotmail.com>
2023-10-25 05:01:46 +0200

    fix signet onequip

--- sql/database_updates/20231025023528_world.sql
+++ sql/database_updates/20231025023528_world.sql
@@ -2,3 +2,5 @@ UPDATE item_template SET spellid_2 = 15896 WHERE entry = 19351; -- fix on equip 
 
 
 UPDATE item_template SET dmg_min1 = 81, dmg_max1 = 151 WHERE entry = 21392;  -- fix damage of sycthe
+
+UPDATE item_template SET spelltrigger_1 = 1 WHERE entry = 21402; -- fix signet of path
