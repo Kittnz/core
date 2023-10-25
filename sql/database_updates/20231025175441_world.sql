2a3d3176daba5961bbeb68550c2d5487d0e0fb24
slowtorta <slowtorta@gmail.com>
2023-10-25 20:43:54 +0100

    Direct world update.

---
+++ sql/database_updates/20231025175441_world.sql
@@ -0,0 +1,3 @@
+
+-- Changes by SHANG
+REPLACE INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (907,-275.597687,-7474.586426,73.191582,2.514761,1,'blackstone');
