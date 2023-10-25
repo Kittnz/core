154fa2f24393933e221c45b165b83b3667c5f7b2
Jameyboor <jamey_D@hotmail.com>
2023-10-25 04:56:20 +0200

    Bugfixes of drag's mid item changes

---
+++ sql/database_updates/20231025023528_world.sql
@@ -0,0 +1,4 @@
+UPDATE item_template SET spellid_2 = 15896 WHERE entry = 19351; -- fix on equip of Maladath
+
+
+UPDATE item_template SET dmg_min1 = 81, dmg_max1 = 151 WHERE entry = 21392;  -- fix damage of sycthe
