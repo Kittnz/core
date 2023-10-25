b132fe5bb38190bf6a7bb1c2146dbcda018c90eb
slowtorta <slowtorta@gmail.com>
2023-10-25 23:07:02 +0100

    Direct world update.

--- sql/database_updates/20231025175441_world.sql
+++ sql/database_updates/20231025175441_world.sql
@@ -1,3 +1,11 @@
 
 -- Changes by SHANG
 REPLACE INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (907,-275.597687,-7474.586426,73.191582,2.514761,1,'blackstone');
+
+-- Changes by TORTA
+DELETE FROM creature WHERE guid=2555783;
+DELETE FROM creature_addon WHERE guid=2555783;
+DELETE FROM creature_movement WHERE id=2555783;
+DELETE FROM game_event_creature WHERE guid=2555783;
+DELETE FROM game_event_creature_data WHERE guid=2555783;
+DELETE FROM creature_battleground WHERE guid=2555783;
