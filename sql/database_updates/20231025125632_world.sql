762b577fcb51ed183eb833926d8aff9fb17511e7
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-10-25 18:24:35 +0300

    Make item Nordanaar Herbal Tea (entry 61675) have 3 charges.

--- sql/database_updates/20231025125632_world.sql
+++ sql/database_updates/20231025125632_world.sql
@@ -1,2 +1,4 @@
 -- https://github.com/slowtorta/turtlewow-bug-tracker/issues/4893.
 update item_template set disenchant_id = 64, armor = 242, item_level = 60, spellid_1 = 15464, spellid_2 = 22015, spelltrigger_2 = 1 where entry = 22015; -- Beastmaster's Gloves
+-- Make item Nordanaar Herbal Tea (entry 61675) have 3 charges.
+update item_template set spellid_1 = 19199, spellcharges_1 = -3, spellcooldown_1 = 0, spellid_2 = 0 where entry = 61675;
