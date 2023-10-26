0ca3a40ae3f6a07710df3c87b2ad3c4755611ea4
Torta <info.turtlewow@gmail.com>
2023-10-26 14:27:25 +0200

    Fix grammar: Dim Light in the Darkness (quest).

--- sql/database_updates/20231026121711_world.sql
+++ sql/database_updates/20231026121711_world.sql
@@ -3,3 +3,8 @@
 -- Proposed fix:  "Demons still remain in Hyjal to this day."
 
 UPDATE `quest_template` SET `Details` = 'If Fel is in the soil, then it means the demons are up to something. Travel up the road until you arrive near a scorched land. It was one of the sites of the battle of Mount Hyjal.$B$B The evil ugly demonlord Archimonde himself arrived and destroyed everything that was once there! Demons still remain in Hyjal to this day.$B$B When I made my way here I ran into a young pretty red Dragon that was investigating the demons. Seek her out!' WHERE `entry` = 41106;
+
+-- Quest Name: Dim Light in the Darkness
+-- Issue: Typo, "Cemetary" should be "Cemetery"
+
+UPDATE `quest_template` SET `Objectives` = 'Find Moranna Rosenberg at the Hollow Web Cemetery in Gilneas.' WHERE `entry` = 40952;
