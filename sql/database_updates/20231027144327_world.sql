1bfe54b6865f2cc4fff8abea3ba84dd27a0cca2d
slowtorta <slowtorta@gmail.com>
2023-10-27 16:17:45 +0100

    Direct world update.

---
+++ sql/database_updates/20231027144327_world.sql
@@ -0,0 +1,5 @@
+
+-- Changes by DRAGU
+DELETE FROM gameobject WHERE guid = '4007929';
+DELETE FROM game_event_gameobject WHERE guid = '4007929';
+DELETE FROM gameobject_battleground WHERE guid = '4007929';
