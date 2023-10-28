ff4982814e1ec7fb30bec28b082ebee1a5e53763
redmagejoe <redmagejoe@yahoo.com>
2023-10-28 10:15:34 -0400

    Remove improper ExclusiveGroup from Gilneas quests
    Somehow parts 2 and 3 of the Orvan Darkeye quest chain had ExclusiveGroup set on them...

--- sql/database_updates/20231028083207_world.sql
+++ sql/database_updates/20231028083207_world.sql
@@ -1,3 +1,5 @@
 UPDATE quest_template SET Objectives = 'Wait until Grimshade performs the ritual of divination.' WHERE entry = 2992;
 
 UPDATE quest_template SET MinLevel = 1 WHERE entry IN (41107, 41108, 41109, 41118, 41119, 41123, 41129, 80738, 80739);
+
+UPDATE quest_template SET ExclusiveGroup = 0 WHERE entry IN (40878, 40879);
