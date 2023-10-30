631694e9cb09dbeb0d9e1e6ffe53d76b540c64b0
redmagejoe <redmagejoe@yahoo.com>
2023-10-30 02:00:42 -0400

    Set Snarlclaw quest to Elite
    Adds Type 1 (Elite) to quest log

--- sql/database_updates/20230802073200_world.sql
+++ sql/database_updates/20230802073200_world.sql
@@ -2,3 +2,5 @@ UPDATE quest_template SET NextQuestInChain = 0 WHERE entry IN (40474, 40476);
 UPDATE quest_template SET MinLevel = 15 WHERE entry = 40477;
 
 UPDATE quest_template SET RewXP = 0, RewMoneyMaxLevel = 0, RewOrReqMoney = 0 WHERE entry = 41021;
+
+UPDATE quest_template SET Type = 1 WHERE entry = 40945;
