d9ea7d3447ab7b2789b27d42246ffe31b620b10c
redmagejoe <redmagejoe@yahoo.com>
2023-10-30 01:28:27 -0400

    Remove EXP and money rewards from repeatable
    As Culling Wildlife has become a repeatable, money and EXP needs to be removed to avoid becoming a gold farm.

--- sql/database_updates/20230802073200_world.sql
+++ sql/database_updates/20230802073200_world.sql
@@ -1,2 +1,4 @@
 UPDATE quest_template SET NextQuestInChain = 0 WHERE entry IN (40474, 40476);
 UPDATE quest_template SET MinLevel = 15 WHERE entry = 40477;
+
+UPDATE quest_template SET RewXP = 0, RewMoneyMaxLevel = 0, RewOrReqMoney = 0 WHERE entry = 41021;
