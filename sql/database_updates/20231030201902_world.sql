679737600792d1d5b81317f815db88bc2adfc253
redmagejoe <redmagejoe@yahoo.com>
2023-10-30 19:47:21 -0400

    Remove disenchant ID from centaur rep vendor goods
    NPC purchased items, even from reputation vendors, should never have disenchant IDs.

--- sql/database_updates/20231030201902_world.sql
+++ sql/database_updates/20231030201902_world.sql
@@ -1,2 +1,4 @@
 UPDATE creature_template SET npc_flags = 3 WHERE entry = 61585;
 UPDATE quest_template SET ReqCreatureORGoCount1 = 8 WHERE entry = 80301;
+
+UPDATE item_template SET disenchant_id = 0 WHERE entry IN (60879, 60880, 60881, 60882, 60883, 60884, 60885, 60899, 60900, 60901, 60902, 60903, 60904, 60905,);
