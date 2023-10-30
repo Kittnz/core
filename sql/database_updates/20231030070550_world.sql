3015c69c81be48cd55287077d45370841af870a6
Jameyboor <jamey_D@hotmail.com>
2023-10-30 08:06:07 +0100

    RMJ changes in new good SQL file

---
+++ sql/database_updates/20231030070550_world.sql
@@ -0,0 +1,8 @@
+UPDATE quest_template SET RewXP = 0, RewMoneyMaxLevel = 0, RewOrReqMoney = 0 WHERE entry = 41021;
+
+UPDATE quest_template SET Type = 1 WHERE entry = 40945;
+
+UPDATE item_template SET Max_Count = 1 WHERE entry = 83016;
+
+
+
