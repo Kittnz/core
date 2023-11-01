ae96c3232e4e44d0ad076e04f4b4d401c3ae180b
Torta <info.turtlewow@gmail.com>
2023-11-02 00:10:38 +0100

    Quest Nordanaar Herbal Tea must reward 5 items now..

--- sql/database_updates/20231101224530_world.sql
+++ sql/database_updates/20231101224530_world.sql
@@ -1,6 +1,2 @@
 UPDATE `item_template` SET `stackable` = 5, `max_count` = 0, `spellcharges_1` = -1 WHERE `entry` = 61675;
-
-
-
-
-
+UPDATE `quest_template` SET `RewItemCount1` = 5 WHERE `entry` = 41005;
