159d593a8af9d0c96e8228946d057d78d38120c9
slowtorta <slowtorta@gmail.com>
2023-11-02 14:20:29 +0000

    Direct world update.

--- sql/database_updates/20231101111411_world.sql
+++ sql/database_updates/20231101111411_world.sql
@@ -1,3 +1,16 @@
 
 -- Changes by WILLBERG
 REPLACE INTO `creature` VALUES (2579287,7559,0,0,0,1,912.582,-5047.36,5.59913,3.59335,120,120,0,100,100,0,0,0);
+REPLACE INTO `creature` VALUES (2579288,7559,0,0,0,1,911.118,-5048.46,5.72337,0.807149,120,120,0,100,100,0,0,0);
+DELETE FROM creature WHERE guid=2579288;
+DELETE FROM creature_addon WHERE guid=2579288;
+DELETE FROM creature_movement WHERE id=2579288;
+DELETE FROM game_event_creature WHERE guid=2579288;
+DELETE FROM game_event_creature_data WHERE guid=2579288;
+DELETE FROM creature_battleground WHERE guid=2579288;
+DELETE FROM creature WHERE guid=2579287;
+DELETE FROM creature_addon WHERE guid=2579287;
+DELETE FROM creature_movement WHERE id=2579287;
+DELETE FROM game_event_creature WHERE guid=2579287;
+DELETE FROM game_event_creature_data WHERE guid=2579287;
+DELETE FROM creature_battleground WHERE guid=2579287;
