07cbd98907622d8f2e261ec8251ee393c1a7e786
redmagejoe <redmagejoe@yahoo.com>
2023-11-05 21:23:52 -0500

    Parity with other Lokhtos repeatables
    Set Method to 0 to make the Dark Iron Bar repeatable an auto-complete like the others, and migrated Details text to Progress text for parity.

---
+++ sql/database_updates/20231106021459_world.sql
@@ -0,0 +1,5 @@
+UPDATE quest_template SET
+Method = 0,
+Details = '',
+RequestItemsText = 'Dark Iron, while plentiful in the Depths, is still a valued commodity amongst the Brotherhood. The sacred arms and armaments of Ragnaros favor Dark Iron. Besides the ore, we also accept Dark Iron in bar form, should you possess the skill to smelt it.$B$BFor every Dark Iron Bar you offer, favor amongst the Brotherhood you will gain.'
+WHERE entry = 41126;
