53cb3351200b2dc73daaa43c578e41c48390ee1b
ratkosrb <ratkomladic2@abv.bg>
2023-11-06 21:10:48 +0200

    Show revision in server info.
    Requested by Bowser.

--- src/game/Commands/Commands.cpp
+++ src/game/Commands/Commands.cpp
@@ -6114,6 +6114,8 @@ bool ChatHandler::HandleUnstuckCommand(char* /*args*/)
 
 bool ChatHandler::HandleServerInfoCommand(char* /*args*/)
 {
+    SendSysMessage("Core revision: " REVISION_HASH " / " REVISION_DATE " / " _ENDIAN_PLATFORM);
+
     uint32 activeClientsNum = sWorld.GetActiveSessionCount();
     uint32 queuedClientsNum = sWorld.GetQueuedSessionCount();
     uint32 maxActiveClientsNum = sWorld.GetMaxActiveSessionCount();
