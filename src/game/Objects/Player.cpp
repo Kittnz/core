608e52bf6ed0bd24209dc521f54da9b6357cabaf
ratkosrb <ratkomladic2@abv.bg>
2023-11-12 09:36:42 +0200

    Allow item set bonuses to trigger procs.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/5171
    
    Look at the stuff we have to do to please ret paladins.

--- src/game/Objects/Player.cpp
+++ src/game/Objects/Player.cpp
@@ -8095,7 +8095,7 @@ void Player::ApplyEquipSpell(SpellEntry const* spellInfo, Item* item, bool apply
 
         DEBUG_LOG("WORLD: cast %s Equip spellId - %i", (item ? "item" : "itemset"), spellInfo->Id);
 
-        CastSpell(this, spellInfo, true, item);
+        CastSpell(this, spellInfo, true, item, nullptr, ObjectGuid(), nullptr, nullptr, false, !item);
     }
     else
     {
