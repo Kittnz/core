04b58ae1bc441e8376c3c82f131e37ca93321e25
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-11-13 15:09:46 +0300

    Display IDs turned on for Morbiena NPC.

---
+++ sql/database_updates/20231113120258_world.sql
@@ -0,0 +1,6 @@
+-- Display IDs turned on for Morbiena NPC.
+REPLACE INTO `creature_display_info_addon` (`display_id`, `bounding_radius`, `combat_reach`, `gender`, `display_id_other_gender`) VALUES (20520, 0, 0, 0, 0);
+REPLACE INTO `creature_display_info_addon` (`display_id`, `bounding_radius`, `combat_reach`, `gender`, `display_id_other_gender`) VALUES (20521, 0, 0, 0, 0);
+REPLACE INTO `creature_display_info_addon` (`display_id`, `bounding_radius`, `combat_reach`, `gender`, `display_id_other_gender`) VALUES (20522, 0, 0, 0, 0);
+
+
