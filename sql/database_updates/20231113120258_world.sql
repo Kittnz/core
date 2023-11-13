3da70a5a8e0f70be40dd91c5383e2775ac722bdb
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-11-13 15:12:39 +0300

    Rename npc Stoneshell Scrabbler Viceclaw to just Stoneshell Scrabbler.

--- sql/database_updates/20231113120258_world.sql
+++ sql/database_updates/20231113120258_world.sql
@@ -2,5 +2,5 @@
 REPLACE INTO `creature_display_info_addon` (`display_id`, `bounding_radius`, `combat_reach`, `gender`, `display_id_other_gender`) VALUES (20520, 0, 0, 0, 0);
 REPLACE INTO `creature_display_info_addon` (`display_id`, `bounding_radius`, `combat_reach`, `gender`, `display_id_other_gender`) VALUES (20521, 0, 0, 0, 0);
 REPLACE INTO `creature_display_info_addon` (`display_id`, `bounding_radius`, `combat_reach`, `gender`, `display_id_other_gender`) VALUES (20522, 0, 0, 0, 0);
-
-
+-- Rename npc Stoneshell Scrabbler Viceclaw to just Stoneshell Scrabbler.
+update creature_template set name = 'Stoneshell Scrabbler' where entry = 61679;
