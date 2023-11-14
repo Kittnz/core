17ead7900118d1ae1b0a5505349630689e314ad5
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-11-14 16:31:54 +0300

    Add flags and greeting text to Kazznik.

--- sql/database_updates/20231114104023_world.sql
+++ sql/database_updates/20231114104023_world.sql
@@ -313,3 +313,11 @@ replace into item_template (entry, display_id, name, class, quality, flags, buy_
 replace into creature_loot_template values
 (61678,41028,-40,0,1,1,0),
 (61679,41028,-40,0,1,1,0);
+
+-- Npc Kazznik give quest/greeting flags, add greeting text : "Can't you see I am busy sweeping around here?".
+update creature_template set npc_flags = 3 where entry = 80131;
+set @gossip_menu_id = 41572; set @magic_number = 80131;
+replace into gossip_menu (entry, text_id, condition_id) VALUES (@gossip_menu_id, @magic_number, '0'); 
+replace into broadcast_text (entry, Male_Text) values (@magic_number, 'Can\'t you see I am busy sweeping around here?');
+replace into npc_text (ID, BroadcastTextID0) values (@magic_number, @magic_number);
+update creature_template set gossip_menu_id = @gossip_menu_id where entry = @magic_number;
