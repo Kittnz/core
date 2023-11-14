4c038ad0b6a37d4c5ec097ea287b4feaedcdbcce
ratkosrb <ratkomladic2@abv.bg>
2023-11-14 12:17:56 +0200

    Add Daisy Hot Pants to Daisy's Gift Box.
    Now that's a cool gift.
    
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/5205

---
+++ sql/database_updates/20231114101700_world.sql
@@ -0,0 +1,2 @@
+-- Add Daisy Hot Pants to Daisy's Gift Box.
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (51009, 51051, 0, 1, 1, 1, 0);
