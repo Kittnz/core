b5d8519730cc993b7dbbb4d92f43b0331375f333
slowtorta <slowtorta@gmail.com>
2023-11-16 21:25:18 +0000

    Direct world update.

--- sql/database_updates/20231115010107_world.sql
+++ sql/database_updates/20231115010107_world.sql
@@ -1,3 +1,4 @@
 
 -- Changes by DRAGU
 UPDATE `creature` SET `position_x` = 5518.486328, `position_y` = -3896.956543, `position_z` = 1625.385132, `orientation` = 1.983391 WHERE `guid` = 2576530;
+UPDATE `creature` SET `position_x` = 4412.991699, `position_y` = -3398.654785, `position_z` = 1014.631042, `orientation` = 1.911980 WHERE `guid` = 2578168;
