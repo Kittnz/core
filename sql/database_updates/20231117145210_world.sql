b635af12ee854a108b56dab4f93263a51824d897
redmagejoe <redmagejoe@yahoo.com>
2023-11-17 12:26:45 -0500

    Lower level of Goblin Brainwashing Device quest
    The minimum level requirement for the in-game quest for purchasing the Goblin Brainwashing Device with gold has been reduced from 50 to 20.

--- sql/database_updates/20231117145210_world.sql
+++ sql/database_updates/20231117145210_world.sql
@@ -3,3 +3,5 @@ update item_template set spelltrigger_2 = 1 where entry = 22067; -- Sorcerer's L
 update item_template set subclass = 1 where entry = 61210; -- Veil of Nightmare
 
 UPDATE item_template set Max_Count = 250 WHERE entry = 50203;
+
+UPDATE quest_template SET Required_Level = 20 WHERE entry = 39000;
