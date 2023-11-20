5900f2c2b6e387c08ec7499610c36129538f4688
slowtorta <slowtorta@gmail.com>
2023-11-20 13:39:19 +0000

    Direct world update.

---
+++ sql/database_updates/20231119034547_world.sql
@@ -0,0 +1,5 @@
+
+-- Changes by WOJI
+DELETE FROM gameobject WHERE guid = '4005973';
+DELETE FROM game_event_gameobject WHERE guid = '4005973';
+DELETE FROM gameobject_battleground WHERE guid = '4005973';
