d863ccb93b2a2b9574e00e64d56411707b16b969
ratkosrb <ratkomladic2@abv.bg>
2023-11-21 12:05:44 +0200

    Notify client when channel name is invalid.
    Also channel names must always begin with a letter.

--- src/game/Handlers/ChannelHandler.cpp
+++ src/game/Handlers/ChannelHandler.cpp
@@ -35,8 +35,15 @@ void WorldSession::HandleJoinChannelOpcode(WorldPacket& recvPacket)
 
     DEBUG_LOG("Opcode CMSG_JOIN_CHANNEL channel \"%s\"", channelname.c_str());
 
-    if (channelname.empty())
+    // Channel name must begin with a letter.
+    if (channelname.empty() || (uint8(channelname[0]) <= 127 && !isalpha(channelname[0])))
+    {
+        WorldPacket data(SMSG_CHANNEL_NOTIFY, 1 + channelname.size() + 1);
+        data << uint8(CHAT_INVALID_NAME_NOTICE);
+        data << channelname;
+        SendPacket(&data);
         return;
+    }
 
     recvPacket >> pass;
 
