329eb34bdd3523c85ae9e7895b876547aec741ac
ratkosrb <ratkomladic2@abv.bg>
2023-11-28 21:35:11 +0200

    Move Barrel of Sweet Nectar in STV to fix los issue.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/5412

---
+++ sql/database_updates/20231128193422_world.sql
@@ -0,0 +1,4 @@
+-- Move Barrel of Sweet Nectar in STV to fix los issue.
+REPLACE INTO `gameobject` VALUES ( 11967, 3706, 0, -12037.9, -552.132, 14.4766, -3.01942, 0, 0, 0.998135, -0.061048, 900, 900, 100, 1, 0, 0);
+
+
