103c32122d1efeb836451e91d5d3f9f6b22ccbfd
ratkosrb <ratkomladic2@abv.bg>
2023-11-28 21:27:48 +0200

    Add Lesser Healing Potion recipe to Kylanna Windwhisper.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/5413

---
+++ sql/database_updates/20231128192655_world.sql
@@ -0,0 +1,4 @@
+-- Add Lesser Healing Potion recipe to Kylanna Windwhisper.
+INSERT INTO `npc_trainer` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillvalue`, `reqlevel`) VALUES (7948, 2341, 250, 171, 55, 0);
+
+
