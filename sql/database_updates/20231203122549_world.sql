aa39096bb61f2a869947c1780f7fa9bb4e454bab
slowtorta <slowtorta@gmail.com>
2023-12-03 12:56:20 +0000

    Direct world update.

--- sql/database_updates/20231203122549_world.sql
+++ sql/database_updates/20231203122549_world.sql
@@ -3,3 +3,7 @@
 REPLACE INTO `gameobject` VALUES ( 5017347, 2002219, 0, 4030.42, -2215.16, 9.71239, 1.89979, 0, 0, 0.813353, 0.58177, 300, 300, 100, 1, 0, 0);
 REPLACE INTO `object_scaling` (`fullGuid`, `scale`) VALUES (17370417354433662723, 2.000000);
 REPLACE INTO `gameobject` VALUES ( 5017347, 2002219, 0, 4030.42, -2215.16, 9.71239, 1.89979, 0, 0, 0.813353, 0.58177, 300, 300, 100, 1, 0, 0);
+
+-- Changes by DRAGU
+UPDATE `creature` SET `position_x` = 3296.694580, `position_y` = -1852.319580, `position_z` = 154.524704, `orientation` = 0.269042 WHERE `guid` = 2581306;
+UPDATE `creature` SET `position_x` = 3306.236816, `position_y` = -1832.974365, `position_z` = 154.582062, `orientation` = 2.465806 WHERE `guid` = 2581082;
