d9be1a874261d92bf76b3a8777f64dfb8720c173
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-12-04 20:58:04 +0300

    Retry fix BL display ids.

---
+++ sql/database_updates/20231204175655_world.sql
@@ -0,0 +1,4 @@
+-- Fix display ids.
+update item_template set display_id = 1416 where entry = 41036; -- Makrura Leg.
+update item_template set display_id = 7602 where entry = 41031; -- Rustgate Tool.
+update item_template set display_id = 9151 where entry = 41032; -- Crate of Rebalanced Frameworks.
