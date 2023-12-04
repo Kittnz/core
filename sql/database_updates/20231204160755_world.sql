904647e13699c27a122f5aa6cb9d42eff999d74b
redmagejoe <redmagejoe@yahoo.com>
2023-12-04 11:43:29 -0500

    Removed Cenarion Circle reputation from Qiraji Scarab kills

--- sql/database_updates/20231204160755_world.sql
+++ sql/database_updates/20231204160755_world.sql
@@ -1,2 +1,5 @@
 -- Make Will of Shahram able to stack 5 times.
 UPDATE `spell_template` SET `stackAmount`=5, `customFlags`=(`customFlags` | 1) WHERE `entry`=16598;
+
+-- Remove rep from Qiraji Scarab
+DELETE FROM creature_onkill_reputation WHERE creature_id = 15316;
