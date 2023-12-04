4727a9a369aa232ff1cda416511756cc459e1538
Torta <info.turtlewow@gmail.com>
2023-12-05 00:30:51 +0100

    Update 20231204231523_world.sql

--- sql/database_updates/20231204231523_world.sql
+++ sql/database_updates/20231204231523_world.sql
@@ -9,3 +9,4 @@ REPLACE INTO `world_safe_locs_facing` (`id`, `orientation`) VALUES (950, 0.03);
 -- Weather:
 
 REPLACE INTO `game_weather` (`zone`, `spring_rain_chance`, `spring_snow_chance`, `spring_storm_chance`, `summer_rain_chance`, `summer_snow_chance`, `summer_storm_chance`, `fall_rain_chance`, `fall_snow_chance`, `fall_storm_chance`, `winter_rain_chance`, `winter_snow_chance`, `winter_storm_chance`, `comment`) VALUES (5536, 10, 0, 0, 10, 0, 0, 20, 0, 0, 15, 0, 0, 'Blackstone Island');
+REPLACE INTO `game_weather` (`zone`, `spring_rain_chance`, `spring_snow_chance`, `spring_storm_chance`, `summer_rain_chance`, `summer_snow_chance`, `summer_storm_chance`, `fall_rain_chance`, `fall_snow_chance`, `fall_storm_chance`, `winter_rain_chance`, `winter_snow_chance`, `winter_storm_chance`, `comment`) VALUES (5225, 10, 0, 0, 10, 0, 0, 20, 0, 0, 15, 0, 0, 'Thalassian Highlands');
