cfd9a76f7af28141859584ddb14d5b561b33b61f
redmagejoe <redmagejoe@yahoo.com>
2023-12-04 22:45:05 -0500

    Make Foul Effigy Unique

--- sql/database_updates/20231204160755_world.sql
+++ sql/database_updates/20231204160755_world.sql
@@ -3,3 +3,6 @@ UPDATE `spell_template` SET `stackAmount`=5, `customFlags`=(`customFlags` | 1) W
 
 -- Remove rep from Qiraji Scarab
 DELETE FROM creature_onkill_reputation WHERE creature_id = 15316;
+
+-- Make Foul Effigy Unique
+UPDATE item_template SET Max_Count = 1 WHERE entry = 61153;
