c5a3f5ab1bb5ecf517ff67ead1502dacb5f0f8ae
Torta <info.turtlewow@gmail.com>
2023-12-05 23:29:34 +0100

    Fix some tabard's prices.

--- sql/database_updates/20231205221357_world.sql
+++ sql/database_updates/20231205221357_world.sql
@@ -2,3 +2,7 @@ delete from npc_vendor where entry = 80958;
 delete from creature where id = 80958;
 delete from creature where guid = 2561158;
 
+-- Fix some tabard's prices:
+
+UPDATE `item_template` SET `buy_price` = 50000, `sell_price` = 15000 WHERE `entry` = 81089;
+UPDATE `item_template` SET `buy_price` = 50000, `sell_price` = 15000 WHERE `entry` = 80316;
