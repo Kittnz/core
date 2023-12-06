985ff8318ab09ebadea804b7c470b0d9328221e8
ratkosrb <ratkomladic2@abv.bg>
2023-12-06 03:21:16 +0200

    Spawn Gaston in Ratchet.

--- sql/database_updates/20231206010613_world.sql
+++ sql/database_updates/20231206010613_world.sql
@@ -1,5 +1,2 @@
--- Readd Gaston the Trash Collector. He is not missing localy for me. Must be only deleted on live server db.
-REPLACE INTO `creature` (`guid`, `id`, `id2`, `id3`, `id4`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecsmin`, `spawntimesecsmax`, `wander_distance`, `health_percent`, `mana_percent`, `movement_type`, `spawn_flags`, `visibility_mod`) VALUES (2573972, 8, 0, 0, 0, 0, -8801.82, 677.644, 101.182, 2.25758, 120, 120, 0, 100, 100, 0, 0, 0);
-REPLACE INTO `creature` (`guid`, `id`, `id2`, `id3`, `id4`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecsmin`, `spawntimesecsmax`, `wander_distance`, `health_percent`, `mana_percent`, `movement_type`, `spawn_flags`, `visibility_mod`) VALUES (2573973, 8, 0, 0, 0, 0, 1647.3, 182.268, -62.1786, 1.86312, 120, 120, 0, 100, 100, 0, 0, 0);
-
-
+-- Spawn Gaston in Ratchet.
+INSERT INTO `creature` (`guid`, `id`, `id2`, `id3`, `id4`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecsmin`, `spawntimesecsmax`, `wander_distance`, `health_percent`, `mana_percent`, `movement_type`, `spawn_flags`, `visibility_mod`) VALUES (400179, 8, 0, 0, 0, 1, -911.062, -3743.12, 9.63662, 4.00079, 120, 120, 0, 100, 100, 0, 0, 0);
