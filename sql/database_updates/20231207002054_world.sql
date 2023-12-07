67d0644c7179905c9549c37e554215df5d7900c4
slowtorta <slowtorta@gmail.com>
2023-12-07 02:17:27 +0000

    Direct world update.

--- sql/database_updates/20231207002054_world.sql
+++ sql/database_updates/20231207002054_world.sql
@@ -4,3 +4,4 @@ UPDATE `creature` SET `position_x` = 4447.822266, `position_y` = -3227.097656, `
 REPLACE INTO `creature_addon` (`guid`, `stand_state`) VALUES (2580924, 5);
 REPLACE INTO `creature_addon` (`guid`, `stand_state`) VALUES (2580924, 6);
 UPDATE `creature` SET `position_x` = 4447.648438, `position_y` = -3226.661377, `position_z` = 16.921209, `orientation` = 1.586795 WHERE `guid` = 2580924;
+REPLACE INTO `creature` VALUES (2582034,61796,0,0,0,451,4574.64,-292.681,268.287,2.73913,120,120,0,100,100,0,0,0);
