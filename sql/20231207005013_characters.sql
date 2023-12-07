b6e51d65c8f97208b2596bfe905ad6ef60bfecef
ratkosrb <ratkomladic2@abv.bg>
2023-12-07 02:51:01 +0200

    Add migration to remove them from table.

---
+++ sql/20231207005013_characters.sql
@@ -0,0 +1,5 @@
+-- Remove Signet Ring of the Bronze Dragonflight from destroyed items list.
+DELETE FROM `character_destroyed_items` WHERE `item_entry` IN (21196, 21197, 21198, 21199, 21200, 21201, 21202, 21203, 21204, 21205, 21206, 21207, 21208, 21209, 21210);
+
+
+
