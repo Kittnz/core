62012fd2438511ee9528199e18caa829e850f64c
slowtorta <slowtorta@gmail.com>
2023-12-07 00:36:32 +0000

    Direct world update.

---
+++ sql/database_updates/20231207002054_world.sql
@@ -0,0 +1,6 @@
+
+-- Changes by WOJI
+UPDATE `creature` SET `position_x` = 4447.822266, `position_y` = -3227.097656, `position_z` = 16.930801, `orientation` = 1.575795 WHERE `guid` = 2580924;
+REPLACE INTO `creature_addon` (`guid`, `stand_state`) VALUES (2580924, 5);
+REPLACE INTO `creature_addon` (`guid`, `stand_state`) VALUES (2580924, 6);
+UPDATE `creature` SET `position_x` = 4447.648438, `position_y` = -3226.661377, `position_z` = 16.921209, `orientation` = 1.586795 WHERE `guid` = 2580924;
