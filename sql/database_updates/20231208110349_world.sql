3bef1d61e4a61b03f98f4c38b03c683bdfe8c48c
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-12-08 15:14:49 +0300

    Friendly NPC for Thalassian Highlands vol 3 (61864 - 61885).

---
+++ sql/database_updates/20231208110349_world.sql
@@ -0,0 +1,81 @@
+REPLACE INTO creature_template VALUES
+(61864, 3167, 0, 0, 0, 0, 'Stormwind Volunteer', NULL, 0, 55, 55, 3398, 3398, 0, 0, 4500, 11, 0, 1, 1.14286, 1, 18, 5, 0, 0, 1, 141, 170, 0, 248, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 70.664, 97.163, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(61865, 3527, 3525, 0, 0, 0, 'Ironforge Volunteer', NULL, 0, 55, 55, 3398, 3398, 0, 0, 4500, 57, 0, 1, 1.14286, 1, 18, 5, 0, 0, 1, 141, 170, 0, 248, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 70.664, 97.163, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(61866, 4844, 4843, 0, 0, 0, 'Darnassus Volunteer', NULL, 0, 55, 55, 3398, 3398, 0, 0, 4500, 79, 0, 1, 1.14286, 1, 18, 5, 0, 0, 1, 141, 170, 0, 248, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 70.664, 97.163, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(61867, 15238, 0, 0, 0, 0, 'Orik Greystone', 'Ironforge Ambassador', 0, 40, 40, 1752, 1752, 0, 0, 1890, 55, 0, 1, 1.14286, 1, 18, 5, 0, 0, 1, 61, 78, 0, 156, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 8, 0, 56.672, 77.924, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(61868, 15540, 0, 0, 0, 0, 'Lord Firian Northshire', 'Stormwind Ambassador', 0, 40, 40, 1752, 1752, 0, 0, 1890, 11, 0, 1, 1.14286, 1, 18, 5, 0, 0, 1, 61, 78, 0, 156, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 8, 0, 56.672, 77.924, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(61869, 15100, 0, 0, 0, 0, 'Denala Moonwind', 'Darnassus Ambassador', 0, 40, 40, 1752, 1752, 0, 0, 1890, 80, 0, 1, 1.14286, 1, 18, 5, 0, 0, 1, 61, 78, 0, 156, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 8, 0, 56.672, 77.924, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(61870, 7861, 0, 0, 0, 0, 'Yagon Grimbeard', 'Wildhammer Ambassador', 0, 40, 40, 1752, 1752, 0, 0, 1890, 694, 0, 1, 1.14286, 1, 18, 5, 0, 0, 1, 61, 78, 0, 156, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 8, 0, 56.672, 77.924, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(61871, 15552, 0, 0, 0, 0, 'Lord Horghita Ironmane', 'Gilneas Ambassador', 0, 40, 40, 1752, 1752, 0, 0, 1890, 84, 0, 1, 1.14286, 1, 18, 5, 0, 0, 1, 61, 78, 0, 156, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 8, 0, 56.672, 77.924, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(61872, 15580, 0, 0, 0, 0, 'Captain Velari Shadelain', NULL, 0, 45, 45, 2217, 2217, 0, 0, 2725, 80, 0, 1, 1.14714, 1, 18, 5, 0, 0, 1, 78, 95, 0, 204, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 63.7824, 87.7008, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(61873, 15581, 0, 0, 0, 0, 'Isilan Bearmane', NULL, 0, 45, 45, 2217, 2217, 0, 0, 2725, 80, 0, 1, 1.14714, 1, 18, 5, 0, 0, 1, 78, 95, 0, 204, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 63.7824, 87.7008, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(61874, 20587, 20586, 0, 0, 0, 'Withering Refugee', NULL, 0, 7, 7, 137, 137, 0, 0, 239, 371, 0, 1, 1.14286, 1, 18, 5, 0, 0, 1, 10, 12, 0, 56, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 13.4464, 18.4888, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(61875, 20588, 0, 0, 0, 0, 'Auctioneer Reilin', NULL, 0, 50, 50, 6645, 6645, 0, 0, 2999, 371, 4096, 1, 1.14286, 1, 18, 5, 0, 0, 1, 72, 93, 0, 226, 1, 2000, 2000, 1, 512, 0, 0, 0, 0, 0, 0, 66.44, 91.355, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 10, 0, ''),
+(61876, 20595, 0, 0, 0, 0, 'Banker Shalanna', NULL, 0, 45, 45, 5544, 5544, 0, 0, 2725, 371, 256, 1, 1.14286, 0, 18, 5, 0, 0, 1, 72, 93, 0, 204, 1, 2000, 2000, 1, 37376, 0, 0, 0, 0, 0, 0, 61.776, 84.942, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 524298, 0, ''),
+(61877, 20584, 0, 0, 0, 0, 'Navanir Thedal', 'Shady Dealer', 0, 45, 45, 2217, 2217, 0, 0, 2725, 371, 4, 1, 1.14714, 1, 18, 5, 0, 0, 1, 78, 95, 0, 204, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 63.7824, 87.7008, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(61878, 20596, 0, 0, 0, 0, 'Solenia Solarcrest', NULL, 0, 10, 10, 594, 594, 0, 0, 20, 371, 2, 1, 1.14286, 1, 20, 5, 0, 0, 1, 27, 35, 0, 62, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 14, 19.25, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(61879, 20597, 0, 0, 0, 0, 'Relaina Whiteshore', NULL, 0, 10, 10, 594, 594, 0, 0, 20, 371, 0, 1, 1.14286, 1, 20, 5, 0, 0, 1, 27, 35, 0, 62, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 14, 19.25, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(61880, 20592, 0, 0, 0, 0, 'Firalius Arrowstrike', NULL, 0, 10, 10, 594, 594, 0, 0, 20, 371, 0, 1, 1.14286, 1, 20, 5, 0, 0, 1, 27, 35, 0, 62, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 14, 19.25, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(61881, 20594, 0, 0, 0, 0, 'Dalicia Sweetsilver', NULL, 0, 10, 10, 594, 594, 0, 0, 20, 371, 2, 1, 1.14286, 1, 20, 5, 0, 0, 1, 27, 35, 0, 62, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 14, 19.25, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(61882, 20591, 0, 0, 0, 0, 'Brindel Morningsun', 'Mining Trainer', 0, 10, 10, 594, 594, 0, 0, 20, 371, 16, 1, 1.14286, 1, 20, 5, 0, 0, 1, 27, 35, 0, 62, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 14, 19.25, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(61883, 20590, 0, 0, 0, 0, 'Verrus Trueshine', 'Cooking Trainer', 0, 10, 10, 594, 594, 0, 0, 20, 371, 16, 1, 1.14286, 1, 20, 5, 0, 0, 1, 27, 35, 0, 62, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 14, 19.25, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(61884, 20593, 0, 0, 0, 0, 'Eirwen Aniron', NULL, 0, 10, 10, 594, 594, 0, 0, 20, 371, 0, 1, 1.14286, 1, 20, 5, 0, 0, 1, 27, 35, 0, 62, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 14, 19.25, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(61885, 20589, 0, 0, 0, 0, 'Wyntherius Skywind', NULL, 0, 10, 10, 594, 594, 0, 0, 20, 371, 0, 1, 1.14286, 1, 20, 5, 0, 0, 1, 27, 35, 0, 62, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 14, 19.25, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, ''),
+(61885, 15518, 15519, 0, 0, 0, 'Alah\'Thalas Researcher', NULL, 0, 10, 10, 594, 594, 0, 0, 20, 371, 0, 1, 1.14286, 1, 20, 5, 0, 0, 1, 27, 35, 0, 62, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 14, 19.25, 100, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 'EventAI', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, '');
+
+set @equip_template = 20502; set @weapon_1 = 60752; set @weapon_2 = 0; set @weapon_3 = 0; set @creature = 61866;
+replace into creature_equip_template values (@equip_template, @weapon_1, @weapon_2, @weapon_3);
+update creature_template set equipment_id = @equip_template where entry = @creature;
+
+set @equip_template = 20501; set @weapon_1 = 61185; set @weapon_2 = 0; set @weapon_3 = 0; set @creature = 61865;
+replace into creature_equip_template values (@equip_template, @weapon_1, @weapon_2, @weapon_3);
+update creature_template set equipment_id = @equip_template where entry = @creature;
+
+set @equip_template = 20500; set @weapon_1 = 935; set @weapon_2 = 0; set @weapon_3 = 0; set @creature = 61864;
+replace into creature_equip_template values (@equip_template, @weapon_1, @weapon_2, @weapon_3);
+update creature_template set equipment_id = @equip_template where entry = @creature;
+
+-- Vendor list: Navanir Thedal.
+REPLACE INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `itemflags`, `condition_id`) VALUES (61877, 6, 2928, 0, 0, 0, 0);
+REPLACE INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `itemflags`, `condition_id`) VALUES (61877, 8, 2930, 0, 0, 0, 0);
+REPLACE INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `itemflags`, `condition_id`) VALUES (61877, 0, 2931, 0, 0, 0, 0);
+REPLACE INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `itemflags`, `condition_id`) VALUES (61877, 2, 3371, 0, 0, 0, 0);
+REPLACE INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `itemflags`, `condition_id`) VALUES (61877, 3, 3372, 0, 0, 0, 0);
+REPLACE INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `itemflags`, `condition_id`) VALUES (61877, 0, 3777, 0, 0, 0, 0);
+REPLACE INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `itemflags`, `condition_id`) VALUES (61877, 1, 5060, 0, 0, 0, 0);
+REPLACE INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `itemflags`, `condition_id`) VALUES (61877, 5, 5140, 0, 0, 0, 0);
+REPLACE INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `itemflags`, `condition_id`) VALUES (61877, 10, 5173, 0, 0, 0, 0);
+REPLACE INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `itemflags`, `condition_id`) VALUES (61877, 9, 8923, 0, 0, 0, 0);
+REPLACE INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `itemflags`, `condition_id`) VALUES (61877, 7, 8924, 0, 0, 0, 0);
+REPLACE INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `itemflags`, `condition_id`) VALUES (61877, 4, 8925, 0, 0, 0, 0);
+
+-- Trainer: Verrus Trueshine.
+REPLACE INTO `npc_trainer` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillvalue`, `reqlevel`) VALUES (61883, 2551, 100, 0, 0, 5);
+REPLACE INTO `npc_trainer` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillvalue`, `reqlevel`) VALUES (61883, 2559, 50, 185, 10, 0);
+REPLACE INTO `npc_trainer` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillvalue`, `reqlevel`) VALUES (61883, 2561, 100, 185, 50, 0);
+REPLACE INTO `npc_trainer` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillvalue`, `reqlevel`) VALUES (61883, 2562, 200, 185, 75, 0);
+REPLACE INTO `npc_trainer` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillvalue`, `reqlevel`) VALUES (61883, 2563, 150, 185, 80, 0);
+REPLACE INTO `npc_trainer` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillvalue`, `reqlevel`) VALUES (61883, 6502, 100, 185, 50, 0);
+REPLACE INTO `npc_trainer` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillvalue`, `reqlevel`) VALUES (61883, 6503, 300, 185, 125, 0);
+-- Trainer: Daemar Swiftstrike.
+REPLACE INTO `npc_trainer` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillvalue`, `reqlevel`) VALUES (61882, 2581, 10, 0, 0, 0);
+
+-- Change name of npc:18193 to Withered Refugee and add display id 20584 20585 to them (in addition to current ones).
+update creature_template set name = 'Withered Refugee', display_id3 = 20584, display_id4 = 20585 where entry = 18193;
+-- Serverside enable the following display ids:
+REPLACE INTO `creature_display_info_addon` (`display_id`, `bounding_radius`, `combat_reach`, `gender`, `display_id_other_gender`) VALUES
+(20587, 0, 0, 0, 0),
+(20586, 0, 0, 0, 0),
+(20584, 0, 0, 0, 0),
+(20585, 0, 0, 0, 0),
+(20588, 0, 0, 0, 0),
+(20595, 0, 0, 0, 0),
+(20596, 0, 0, 0, 0),
+(20597, 0, 0, 0, 0),
+(20592, 0, 0, 0, 0),
+(20592, 0, 0, 0, 0),
+(20594, 0, 0, 0, 0),
+(20591, 0, 0, 0, 0),
+(20590, 0, 0, 0, 0),
+(20593, 0, 0, 0, 0),
+(20589, 0, 0, 0, 0);
