da9c4dffbca34c2a1e0738339d5557270642ed71
Torta <info.turtlewow@gmail.com>
2023-12-09 01:49:39 +0100

    Re-routing Alah'thalas taxi directions.

---
+++ sql/database_updates/20231208235927_world.sql
@@ -0,0 +1,4 @@
+	 	
+UPDATE `taxi_nodes` SET `mount_creature_id2` = 803 WHERE `id` = 184;
+UPDATE `creature_template` SET `entry` = 803 WHERE `entry` = 80144;
+UPDATE `creature` SET `id` = 803 WHERE `id` = 80144;
