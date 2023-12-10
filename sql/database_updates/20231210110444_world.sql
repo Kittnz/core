4867c6621ac5e9816f7312cdac583cdb6e116882
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-12-10 18:33:19 +0300

    New GO "Boat to Alah'Thalas" has been added: 2020076.

--- sql/database_updates/20231210110444_world.sql
+++ sql/database_updates/20231210110444_world.sql
@@ -247,3 +247,7 @@ REPLACE INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `facti
 REPLACE INTO `broadcast_text` (`entry`, `male_text`, `female_text`, `chat_type`, `sound_id`, `language_id`, `emote_id1`, `emote_id2`, `emote_id3`, `emote_delay1`, `emote_delay2`, `emote_delay3`) VALUES
 (30151, 'Wine was a true warlock. Beneath the dark exterior laid the most genuine person we\'ve met. Warlock chat will never be the same without him.', NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0),
 (30152, 'It was an honor to have Gallifrey in guild, to teach her all about the game and regard her as a friend. May you ride your white wolf forever!', NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+-- Make a copy of object 176364 and rename it to "Boat to Alah'Thalas".
+REPLACE INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `faction`, `flags`, `size`, `data0`, `data1`, `data2`, `data3`, `data4`, `data5`, `data6`, `data7`, `data8`, `data9`, `data10`, `data11`, `data12`, `data13`, `data14`, `data15`, `data16`, `data17`, `data18`, `data19`, `data20`, `data21`, `data22`, `data23`, `mingold`, `maxgold`, `phase_quest_id`, `script_name`) VALUES
+(2020076, 5, 388, 'Boat to Alah\'Thalas', 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');
