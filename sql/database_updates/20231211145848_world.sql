3a1d251d2fad05d876133fba176653f59c86277b
slowtorta <slowtorta@gmail.com>
2023-12-11 16:12:09 +0000

    Direct world update.

--- sql/database_updates/20231211145848_world.sql
+++ sql/database_updates/20231211145848_world.sql
@@ -1,3 +1,4 @@
 
 -- Changes by SHANG
 REPLACE INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (909,3321.969971,-2447.813721,107.181198,3.472816,0,'highelf');
+REPLACE INTO `creature` VALUES (2582199,81030,0,0,0,1,-255.141,-7146.49,15.8751,1.89213,120,120,5,100,100,2,0,0);
