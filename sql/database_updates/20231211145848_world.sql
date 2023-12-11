3251f64a7e34e3d6f923a44b7bee4e76c5387980
slowtorta <slowtorta@gmail.com>
2023-12-11 17:31:06 +0000

    Direct world update.

--- sql/database_updates/20231211145848_world.sql
+++ sql/database_updates/20231211145848_world.sql
@@ -5,3 +5,16 @@ REPLACE INTO `creature` VALUES (2582199,81030,0,0,0,1,-255.141,-7146.49,15.8751,
 
 -- Changes by DRAGU
 REPLACE INTO `creature` VALUES (2582200,50514,0,0,0,1,1869.03,-4664.03,34.6025,2.5148,120,120,0,100,100,0,0,0);
+REPLACE INTO `creature` VALUES (2582201,50514,0,0,0,1,1872.11,-4661.72,34.3805,2.16923,120,120,0,100,100,0,0,0);
+DELETE FROM creature WHERE guid=2582201;
+DELETE FROM creature_addon WHERE guid=2582201;
+DELETE FROM creature_movement WHERE id=2582201;
+DELETE FROM game_event_creature WHERE guid=2582201;
+DELETE FROM game_event_creature_data WHERE guid=2582201;
+DELETE FROM creature_battleground WHERE guid=2582201;
+DELETE FROM creature WHERE guid=2582200;
+DELETE FROM creature_addon WHERE guid=2582200;
+DELETE FROM creature_movement WHERE id=2582200;
+DELETE FROM game_event_creature WHERE guid=2582200;
+DELETE FROM game_event_creature_data WHERE guid=2582200;
+DELETE FROM creature_battleground WHERE guid=2582200;
