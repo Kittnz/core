bccf7e5a93d7880c5298ddeed943aaa53c4d719a
slowtorta <slowtorta@gmail.com>
2023-12-11 21:58:46 +0000

    Direct world update.

--- sql/database_updates/20231211145848_world.sql
+++ sql/database_updates/20231211145848_world.sql
@@ -24,3 +24,29 @@ REPLACE INTO `gameobject` VALUES ( 5017491, 2020072, 0, 3337.9, -2480.62, 105.09
 
 -- Changes by REN
 UPDATE `creature` SET `position_x` = 4310.803711, `position_y` = -3040.788574, `position_z` = 22.923267, `orientation` = 4.277101 WHERE `guid` = 2581570;
+UPDATE `creature_addon` SET `stand_state`=1 WHERE `guid`=2581570;
+UPDATE `creature_addon` SET `stand_state`=0 WHERE `guid`=2581570;
+UPDATE `creature` SET `position_x` = 4310.140137, `position_y` = -3040.454102, `position_z` = 23.829506, `orientation` = 5.141034 WHERE `guid` = 2581910;
+UPDATE `creature_addon` SET `stand_state`=1 WHERE `guid`=2581910;
+UPDATE `creature` SET `position_x` = 4310.026855, `position_y` = -3040.573242, `position_z` = 23.829655, `orientation` = 5.423780 WHERE `guid` = 2581910;
+UPDATE `creature` SET `position_x` = 4310.768066, `position_y` = -3040.598145, `position_z` = 22.926102, `orientation` = 4.092525 WHERE `guid` = 2581570;
+UPDATE `creature` SET `position_x` = 4310.079590, `position_y` = -3040.593018, `position_z` = 23.829184, `orientation` = 4.316359 WHERE `guid` = 2581910;
+UPDATE `creature_template` set `scale` = 0.100000 where entry = 7383;
+UPDATE `creature_template` set `scale` = 0.400000 where entry = 7383;
+UPDATE `creature_template` set `scale` = 0.300000 where entry = 7383;
+UPDATE `creature_template` set `scale` = 0.400000 where entry = 7383;
+UPDATE `creature` SET `position_x` = 4310.024902, `position_y` = -3040.473389, `position_z` = 23.829084, `orientation` = 5.600483 WHERE `guid` = 2581910;
+REPLACE INTO `gameobject` VALUES ( 5017492, 2003260, 0, 4310.13, -3040.71, 23.8293, 0.326529, 0, 0, 0.16254, 0.986702, 300, 300, 100, 1, 0, 0);
+DELETE FROM gameobject WHERE guid = '5017492';
+DELETE FROM game_event_gameobject WHERE guid = '5017492';
+DELETE FROM gameobject_battleground WHERE guid = '5017492';
+REPLACE INTO `gameobject` VALUES ( 5017493, 2003261, 0, 4309.9, -3040.66, 23.83, 0.354021, 0, 0, 0.176087, 0.984375, 300, 300, 100, 1, 0, 0);
+REPLACE INTO `object_scaling` (`fullGuid`, `scale`) VALUES (17370417371915521941, 0.800000);
+REPLACE INTO `gameobject` VALUES ( 5017493, 2003261, 0, 4309.9, -3040.66, 23.83, 0.354021, 0, 0, 0.176087, 0.984375, 300, 300, 100, 1, 0, 0);
+REPLACE INTO `object_scaling` (`fullGuid`, `scale`) VALUES (17370417371915521941, 0.600000);
+REPLACE INTO `gameobject` VALUES ( 5017493, 2003261, 0, 4309.9, -3040.66, 23.83, 0.354021, 0, 0, 0.176087, 0.984375, 300, 300, 100, 1, 0, 0);
+REPLACE INTO `gameobject` VALUES ( 5017493, 2003261, 0, 4309.7, -3040.66, 23.83, 0.354021, 0, 0, 0.176087, 0.984375, 300, 300, 100, 1, 0, 0);
+REPLACE INTO `gameobject` VALUES ( 5017493, 2003261, 0, 4309.9, -3040.66, 23.83, 0.354021, 0, 0, 0.176087, 0.984375, 300, 300, 100, 1, 0, 0);
+REPLACE INTO `gameobject` VALUES ( 5017493, 2003261, 0, 4310.1, -3040.66, 23.83, 0.354021, 0, 0, 0.176087, 0.984375, 300, 300, 100, 1, 0, 0);
+REPLACE INTO `object_scaling` (`fullGuid`, `scale`) VALUES (17370417371915521941, 0.400000);
+REPLACE INTO `gameobject` VALUES ( 5017493, 2003261, 0, 4310.1, -3040.66, 23.83, 0.354021, 0, 0, 0.176087, 0.984375, 300, 300, 100, 1, 0, 0);
