b5f6c2cb7eb5d3b68717a200c308fbe574764e27
Torta <info.turtlewow@gmail.com>
2023-12-11 16:11:38 +0100

    Update logon sctructure.

--- sql/structure_logon.sql
+++ sql/structure_logon.sql
@@ -680,6 +680,18 @@ CREATE TABLE IF NOT EXISTS `character_transfers` (
 -- Dumping data for table turtle_server_auth.character_transfers: ~0 rows (approximately)
 DELETE FROM `character_transfers`;
 
+CREATE TABLE `pending_commands` (
+	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing identifier for the row',
+	`realm_id` INT UNSIGNED NULL COMMENT 'id of the realm that should run the command',
+	`command` VARCHAR(250) NOT NULL DEFAULT '' COMMENT 'full comand with parameters',
+	`run_at_time` INT UNSIGNED NOT NULL DEFAULT '0' COMMENT 'unixtime',
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_unicode_ci'
+ENGINE=InnoDB
+;
+
+
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
