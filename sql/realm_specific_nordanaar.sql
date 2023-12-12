49308d2e6632815ca366a102b4ceb8bb0242a678
Torta <info.turtlewow@gmail.com>
2023-12-12 21:01:27 +0100

    Update realm_specific_nordanaar.sql

--- sql/realm_specific_nordanaar.sql
+++ sql/realm_specific_nordanaar.sql
@@ -1,6 +1,6 @@
 UPDATE `quest_cast_objective` SET `spell_id` = 21343, `player_guid` = 688152, `player_class` = 0, `objective_text` = 'Throw snowball at Akalix' WHERE `entry` = 50321 AND `idx` = 0;
-UPDATE `quest_cast_objective` SET `spell_id` = 21343, `player_guid` = 369620, `player_class` = 0, `objective_text` = 'Throw snowball at Lexie' WHERE `entry` = 50321 AND `idx` = 1;
-UPDATE `quest_cast_objective` SET `spell_id` = 21343, `player_guid` = 542109, `player_class` = 0, `objective_text` = 'Throw snowball at Sarenrae' WHERE `entry` = 50321 AND `idx` = 2;
+UPDATE `quest_cast_objective` SET `spell_id` = 21343, `player_guid` = 605121, `player_class` = 0, `objective_text` = 'Throw snowball at Shockwarden' WHERE `entry` = 50321 AND `idx` = 1;
+UPDATE `quest_cast_objective` SET `spell_id` = 21343, `player_guid` = 346800, `player_class` = 0, `objective_text` = 'Throw snowball at Dated' WHERE `entry` = 50321 AND `idx` = 2;
 UPDATE `quest_cast_objective` SET `spell_id` = 21343, `player_guid` = 391543, `player_class` = 0, `objective_text` = 'Throw snowball at Vrograg' WHERE `entry` = 50321 AND `idx` = 3;
 
 -- 688152	Akalix
