30cdc1670ec63563c4ea2152d12fff2bbd9defce
Torta <info.turtlewow@gmail.com>
2023-12-13 15:11:46 +0100

    Skin tokens for Undeads.

---
+++ sql/database_updates/20231213140259_world.sql
@@ -0,0 +1,2 @@
+REPLACE INTO `custom_character_skins` (`token_id`, `skin_male`, `skin_female`) VALUES (51920, 6, 6);
+REPLACE INTO `custom_character_skins` (`token_id`, `skin_male`, `skin_female`) VALUES (51921, 6, 6);
