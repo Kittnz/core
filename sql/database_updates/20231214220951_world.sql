498a414ed180c4ea1187c405e61a863f63b60d57
Torta <info.turtlewow@gmail.com>
2023-12-14 23:39:51 +0100

    Shop: Fix Dark Ranger skins, remove Forest Troll skins.

---
+++ sql/database_updates/20231214220951_world.sql
@@ -0,0 +1,8 @@
+
+-- Custom character skin tokens:
+
+UPDATE `custom_character_skins` SET `skin_male` = 17, `skin_female` = 17 WHERE `token_id` = 81206;
+UPDATE `custom_character_skins` SET `skin_male` = 18, `skin_female` = 16 WHERE `token_id` = 81209;
+
+
+
