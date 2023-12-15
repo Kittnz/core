d705eb391c13826b067b094ebfec0165cd6ba9b0
Torta <info.turtlewow@gmail.com>
2023-12-15 20:04:10 +0100

    Dragu's spell changes.

---
+++ sql/database_updates/20231215190335_world.sql
@@ -0,0 +1,6 @@
+-- fix text for new enchant
+update spell_template SET Description = 'Permanently enchant a two-handed melee weapon to grant +2 Intellect.' WHERE entry = 45071; 
+-- make aspect of seradane proc cooler
+update spell_template SET spellVisual1 = 3860, spellIconId = 263, name = "Wrath Volley", effectBasePoints1 = 129, description = 'Unleashes a wave of energy that blasts up to 3 targets for $s1 Nature damage.' WHERE entry = 45076; -- chain lightning
+
+
