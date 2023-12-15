1aae3d04fd6212b17a9edff72ae34f14b8e73c3a
Torta <info.turtlewow@gmail.com>
2023-12-15 21:50:41 +0100

    Fix new mob's respawn timers.

--- sql/database_updates/20231215190335_world.sql
+++ sql/database_updates/20231215190335_world.sql
@@ -3,4 +3,6 @@ update spell_template SET Description = 'Permanently enchant a two-handed melee 
 -- make aspect of seradane proc cooler
 update spell_template SET spellVisual1 = 3860, spellIconId = 263, name = "Wrath Volley", effectBasePoints1 = 129, description = 'Unleashes a wave of energy that blasts up to 3 targets for $s1 Nature damage.' WHERE entry = 45076; -- chain lightning
 
+-- Fix new mob's respawn timers.
 
+UPDATE `creature` SET `spawntimesecsmin` = 300, `spawntimesecsmax` = 300 WHERE `guid` > 2578957 AND `spawntimesecsmin` = 120;
