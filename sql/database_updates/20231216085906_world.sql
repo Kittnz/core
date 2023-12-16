1c84dd1d9705a1d244f430deb6ada8c570c00412
redmagejoe <redmagejoe@yahoo.com>
2023-12-16 05:47:34 -0500

    Fix item reward behavior of 2 quests
    War on Worgen and Removing Leper Leadership only offer a single reward, yet someone incorrectly set RewChoiceItemId1 and RewChoiceItemCount1 instead of RewItemId1 and RewItemCount1.

--- sql/database_updates/20231216085906_world.sql
+++ sql/database_updates/20231216085906_world.sql
@@ -10,3 +10,7 @@ update item_template set item_level = 27, disenchant_id = 24 where entry = 60112
 -- Fix incorrect Winter Veil questgiver
 DELETE FROM creature_questrelation WHERE id = 50653 AND quest = 40784;
 REPLACE INTO creature_questrelation (id, quest) VALUES (61189, 40784);
+
+-- Fix incorrect "choose reward" behavior
+UPDATE quest_template SET RewItemId1 = 0, RewChoiceItemCount1 = 0, RewItemId1 = 61623, RewItemCount1 = 1 WHERE entry = 40976;
+UPDATE quest_template SET RewItemId1 = 0, RewChoiceItemCount1 = 0, RewItemId1 = 61386, RewItemCount1 = 1 WHERE entry = 40857;
