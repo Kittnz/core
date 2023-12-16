c348affb91ca7675a81123ae7a08f767b5ae0908
Torta <info.turtlewow@gmail.com>
2023-12-16 22:32:09 +0100

    Stuff.

--- sql/database_updates/world_autobroadcast.sql
+++ sql/database_updates/world_autobroadcast.sql
@@ -11,6 +11,8 @@ replace into mangos_string (entry, content_default, content_loc4) values
 
 ('50004', 'We encourage everyone to change their password regularly. If you think you have ever used your Turtle WoW password in different private servers we urge you to change it as soon as possible.', '我们鼓励每个人定期更改密码。如果您认为您曾在其他私人服务器上使用过乌龟魔兽的密码，请尽快更改密码。'),
 
-('50005', 'All gold transactions are heavily monitored. Any form of RMT will result in severe actions taken against all parties involved. Please, don''t support individuals who are actively harming our server.', '所有金币交易都受到严格监控。任何形式的RMT交易将对所有涉及方采取严厉行动。请不要支持那些正在损害我们服务器的个人。');
+('50005', 'All gold transactions are heavily monitored. Any form of RMT will result in severe actions taken against all parties involved. Please, don''t support individuals who are actively harming our server.', '所有金币交易都受到严格监控。任何形式的RMT交易将对所有涉及方采取严厉行动。请不要支持那些正在损害我们服务器的个人。'),
 
-replace into autobroadcast (string_Id) values (50000),(50001),(50002),(50003),(50004),(50005);
+('50006', 'Tune in to Everlook Broadcasting Co. Radio to delve into Azeroth''s rich lore, enjoy its finest melodies, and stay updated on the products you never knew you needed!', '收听永望广播公司收音机，深入了解艾泽拉斯丰富的故事，欣赏最美妙的旋律，并及时了解你以前不知道需要的产品！');
+
+replace into autobroadcast (string_Id) values (50000),(50001),(50002),(50003),(50004),(50005),(50006);
