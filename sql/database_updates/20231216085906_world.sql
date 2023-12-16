3e46a272428355f1e073ff55573cfa937dbb7302
redmagejoe <redmagejoe@yahoo.com>
2023-12-16 04:05:07 -0500

    Remove CC rep from Winter Veil Quest + Dragunovi item fixes

---
+++ sql/database_updates/20231216085906_world.sql
@@ -0,0 +1,8 @@
+-- Remove obscene Cenarion Cricle rep from a repeatable quest, undermining Ahn'Qiraj farming
+UPDATE quest_template SET RewRepFaction1 = 0, RewRepValue1 = 0 WHERE entry = 50316;
+
+-- Several item fixes from Dragunovi
+update item_template set bonding = 1 where entry = 51043; -- void linked satchel
+update item_template set inventory_type = 15 where entry = 83215; -- Blackflame Wand
+update item_template set sheath = 2 where entry = 84503; -- Nature's Gift
+update item_template set item_level = 27, disenchant_id = 24 where entry = 60112; -- Evermoon Staff
