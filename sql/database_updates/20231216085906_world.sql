0a3be371f2c5915f4bd1ceecf08c3cb87293c922
redmagejoe <redmagejoe@yahoo.com>
2023-12-16 04:34:52 -0500

    Fix quest starter NPC for Reindeer quest
    A Voucher Of A Lifetime! was incorrectly starting from Quark rather than Mulin Snowbeard.  Updated creature_questrelation to address this.

--- sql/database_updates/20231216085906_world.sql
+++ sql/database_updates/20231216085906_world.sql
@@ -6,3 +6,7 @@ update item_template set bonding = 1 where entry = 51043; -- void linked satchel
 update item_template set inventory_type = 15 where entry = 83215; -- Blackflame Wand
 update item_template set sheath = 2 where entry = 84503; -- Nature's Gift
 update item_template set item_level = 27, disenchant_id = 24 where entry = 60112; -- Evermoon Staff
+
+-- Fix incorrect Winter Veil questgiver
+DELETE FROM creature_questrelation WHERE id = 50653 AND quest = 40784;
+REPLACE INTO creature_questrelation (id, quest) VALUES (61189, 40784);
