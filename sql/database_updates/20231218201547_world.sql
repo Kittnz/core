5141449847a868b88b1a3ef6b95b1edfa06c53ca
Torta <info.turtlewow@gmail.com>
2023-12-18 22:31:52 +0100

    Fix pantaloons.

--- sql/database_updates/20231218201547_world.sql
+++ sql/database_updates/20231218201547_world.sql
@@ -1,3 +1,7 @@
 DELETE FROM spell_template WHERE entry = 0;
 
 
+update item_template set display_id = 40719 where entry = 41187; -- reefscale pantaloons
+update item_template set sheath = 3 where entry = 51818; -- Agal's Ambusher
+
+
