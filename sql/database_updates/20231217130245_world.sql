a8e9d14163fd6ac515817b8bb29963247f1e23ab
Dormilon <79369417+Dormilon@users.noreply.github.com>
2023-12-18 18:09:41 +0300

    Old High elves quests removed.

--- sql/database_updates/20231217130245_world.sql
+++ sql/database_updates/20231217130245_world.sql
@@ -371,3 +371,37 @@ update quest_template set details = 'Why hello there!$B$BWhat, shocked to see a 
 update creature_template set display_id1 = 18510 where entry in (50597, 50598);
 -- Warrior Trainer - Larane Dawnbringer - 80247: Npc flags fix.
 update creature_template set npc_flags = 19 where entry in (80247);
+-- Quests 80250,80251,80256,80252,80253,80254,80258,80260,80261,80262,80263,80264,80265,40371,40373,40372,40374,40375,40376,40377,40378,40379,40380,40381,40383,40384,40392,40393,80200 should be removed (completely or just from their quest starter npc)
+delete from creature_questrelation where quest in (80250,80251,80256,80252,80253,80254,80258,80260,80261,80262,80263,80264,80265,40371,40373,40372,40374,40375,40376,40377,40378,40379,40380,40381,40383,40384,40392,40393,80200);
+delete from creature_involvedrelation where quest in (80250,80251,80256,80252,80253,80254,80258,80260,80261,80262,80263,80264,80265,40371,40373,40372,40374,40375,40376,40377,40378,40379,40380,40381,40383,40384,40392,40393,80200);
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] A Diplomatic Mission' WHERE `entry` = 40371;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] Appealing to the Alliance: Humans' WHERE `entry` = 40372;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] Appealing to the Alliance: Dwarves' WHERE `entry` = 40373;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] Appealing to the Alliance: Gnomes' WHERE `entry` = 40374;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] Appealing to the Alliance: Night Elves' WHERE `entry` = 40375;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] The Kaldorei Problem' WHERE `entry` = 40376;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] Breaking the Felstar' WHERE `entry` = 40377;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] Thalassian Accession' WHERE `entry` = 40378;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] Darnassian Endorsement' WHERE `entry` = 40379;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] The Magical Ailment' WHERE `entry` = 40380;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] A Quest for the Moonwell' WHERE `entry` = 40381;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] Vestia\'s Missive' WHERE `entry` = 40383;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] A People Restored' WHERE `entry` = 40384;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] Summons by the Ranger-General' WHERE `entry` = 40392;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] Summons by Lady Windrunner' WHERE `entry` = 40393;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] Farstrider Lodge' WHERE `entry` = 80200;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] Assisting the Children of the Sun' WHERE `entry` = 80250;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] To Alah\'Thalas!' WHERE `entry` = 80251;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] A Crystal Clear Task' WHERE `entry` = 80252;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] Relics in Feralas' WHERE `entry` = 80253;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] Smashing Zul\'Mashar' WHERE `entry` = 80254;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] Welcome to Alah\'Thalas' WHERE `entry` = 80256;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] Tears of the Poppy' WHERE `entry` = 80258;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] Help With a Compassionate Matter' WHERE `entry` = 80260;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] Teslinah\'s Search I' WHERE `entry` = 80261;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] Teslinah\'s Search II' WHERE `entry` = 80262;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] Teslinah\'s Search III' WHERE `entry` = 80263;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] Teslinah\'s Search IV' WHERE `entry` = 80264;
+UPDATE `quest_template` SET `Title` = '[DEPRECATED] Teslinah\'s Search V' WHERE `entry` = 80265;
+-- Quest 80289 should not require completion of 80256.
+update quest_template set prevquestid = 0 where entry = 80289;
