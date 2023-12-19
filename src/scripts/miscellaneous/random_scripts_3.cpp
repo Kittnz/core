cbedee51dfc1e33a136e955b6e475268fee6bad1
Torta <info.turtlewow@gmail.com>
2023-12-19 13:19:17 +0100

    Jamey you had one job!

--- src/scripts/miscellaneous/random_scripts_3.cpp
+++ src/scripts/miscellaneous/random_scripts_3.cpp
@@ -7419,9 +7419,52 @@ bool GOSelect_go_council_translocation_orb(Player* pPlayer, GameObject* pGo, uin
     return false;
 }
 
+constexpr std::array<uint32, 2> QuestsCompleted = { 1, 2 };
+
+bool GossipHello_Christmas_Entity(Player* player, Creature* creature)
+{
+    const bool canSeeItems = std::any_of(QuestsCompleted.begin(), QuestsCompleted.end(), [player](uint32 questId)
+        {
+            return player->GetQuestStatus(questId) == QUEST_STATUS_COMPLETE;
+        });
+
+    if (canSeeItems)
+        player->ADD_GOSSIP_ITEM(GOSSIP_ICON_VENDOR, "Show me the goods.", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF);
+    player->SEND_GOSSIP_MENU(907, creature->GetGUID());
+    return true;
+}
+
+bool GossipSelect_Christmas_Entity(Player* player, Creature* creature, uint32 sender, uint32 action)
+{
+    if (action == GOSSIP_ACTION_INFO_DEF)
+    {
+        const bool canSeeItems = std::any_of(QuestsCompleted.begin(), QuestsCompleted.end(), [player](uint32 questId)
+            {
+                return player->GetQuestStatus(questId) == QUEST_STATUS_COMPLETE;
+            });
+
+        if (canSeeItems)
+        {
+            player->canSeeVendorList = true;
+            player->PlayerTalkClass->CloseGossip();
+            player->GetSession()->SendListInventory(creature->GetGUID());
+        }
+    }
+    return true;
+}
+
+
 void AddSC_random_scripts_3()
 {
     Script* newscript;
+
+    Script* pNewScript;
+
+    pNewScript = new Script;
+    pNewScript->Name = "npc_snow_vendor";
+    pNewScript->pGossipHello = &GossipHello_Christmas_Entity;
+    pNewScript->pGossipSelect = &GossipSelect_Christmas_Entity;
+    pNewScript->RegisterSelf();
 
     newscript = new Script;
     newscript->Name = "go_council_translocation_orb";
