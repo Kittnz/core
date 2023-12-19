bf02e73cbeb66cb0065ec6484bc92db7d16c4294
Torta <info.turtlewow@gmail.com>
2023-12-19 13:19:17 +0100

    Jamey you had one job!

--- src/scripts/ScriptLoader.cpp
+++ src/scripts/ScriptLoader.cpp
@@ -319,8 +319,6 @@ void AddSC_boss_chronormu();
 
 // Misc
 void AddSC_npc_ptr();
-void AddSC_Quest_Snow();
-
 void AddScripts()
 {
     //Nostalrius
@@ -335,7 +333,6 @@ void AddScripts()
     AddSC_ubrs_trash();
     AddSC_go_scripts();
     AddSC_event_fireworks();
-    AddSC_Quest_Snow();
 
     // Event
     AddSC_elemental_invasions();
