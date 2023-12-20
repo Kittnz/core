bdc891abe03cb803a6c9d0d84990ecdb2f524a77
torta <torta@torta.com>
2023-12-21 00:11:47 +0100

    Fixed new Hunter quests objectives.

--- sql/database_updates/20231220044429_world.sql
+++ sql/database_updates/20231220044429_world.sql
@@ -16,32 +16,46 @@ UPDATE `custom_character_skins` SET `skin_male` = 6, `skin_female` = 8 WHERE `to
 
 -- Demon Hunter:
 
-UPDATE `tw_world`.`custom_character_skins` SET `skin_male` = 12, `skin_female` = 14 WHERE `token_id` = 61105;
+UPDATE `custom_character_skins` SET `skin_male` = 12, `skin_female` = 14 WHERE `token_id` = 61105;
 
 -- Naturalist II:
 
-UPDATE `tw_world`.`custom_character_skins` SET `skin_male` = 16, `skin_female` = 12 WHERE `token_id` = 83099;
+UPDATE `custom_character_skins` SET `skin_male` = 16, `skin_female` = 12 WHERE `token_id` = 83099;
 
 -- Demon:
 
-UPDATE `tw_world`.`custom_character_skins` SET `skin_male` = 11, `skin_female` = 13 WHERE `token_id` = 61104;
+UPDATE `custom_character_skins` SET `skin_male` = 11, `skin_female` = 13 WHERE `token_id` = 61104;
 
 -- Dalaran Magi:
 
-UPDATE `tw_world`.`custom_character_skins` SET `skin_male` = 16, `skin_female` = 14 WHERE `token_id` = 83090;
+UPDATE `custom_character_skins` SET `skin_male` = 16, `skin_female` = 14 WHERE `token_id` = 83090;
 
 -- Necromancer I:
 
-UPDATE `tw_world`.`custom_character_skins` SET `skin_male` = 11, `skin_female` = 11 WHERE `token_id` = 50106;
+UPDATE `custom_character_skins` SET `skin_male` = 11, `skin_female` = 11 WHERE `token_id` = 50106;
 
 -- DK Dwarf:
 
-UPDATE `tw_world`.`custom_character_skins` SET `skin_male` = 24, `skin_female` = 0 WHERE `token_id` = 81229;
+UPDATE `custom_character_skins` SET `skin_male` = 24, `skin_female` = 0 WHERE `token_id` = 81229;
 
 -- Naturalist III:
 
-UPDATE `tw_world`.`custom_character_skins` SET `skin_male` = 17, `skin_female` = 0 WHERE `token_id` = 83100;
+UPDATE `custom_character_skins` SET `skin_male` = 17, `skin_female` = 0 WHERE `token_id` = 83100;
 
 -- Naturalist I:
 
-UPDATE `tw_world`.`custom_character_skins` SET `skin_male` = 15, `skin_female` = 11 WHERE `token_id` = 83092;
+UPDATE `custom_character_skins` SET `skin_male` = 15, `skin_female` = 11 WHERE `token_id` = 83092;
+
+
+-- quest 41177 Taming the Beast has an objective to tame Elder Crimson Lynx 61700, but the given item can only tame Lavender Mana Wyrm 61776
+-- the rod (41064) given to player should be able to target only Elder Crimson Lynx 61700
+
+DELETE FROM `item_required_target` WHERE `entry` = 41064;
+REPLACE INTO `item_required_target` (`entry`, `type`, `target_entry`) VALUES (41064, 1, 61700);
+
+
+-- quest 41779 Taming the Beast has an objective to tame Lavender Mana Wyrm 61776, but the given item can only target Elder Crimson Lynx 61700
+-- the rod (41066) given to player should be able to target only Lavender Mana Wyrm 61776
+
+DELETE FROM `item_required_target` WHERE `entry` = 41066;
+REPLACE INTO `item_required_target` (`entry`, `type`, `target_entry`) VALUES (41066, 1, 61776);
