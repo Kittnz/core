d70e2a7e706cf9f1ad306be7c39933b77f613c9d
torta <torta@torta.com>
2023-12-20 23:50:11 +0100

    Fixed yet another batch of spells.

--- sql/database_updates/20231220044429_world.sql
+++ sql/database_updates/20231220044429_world.sql
@@ -1,15 +1,47 @@
--- Dark Iron Dwarf fixes:
+-- Dark Iron Dwarf:
 
 UPDATE `custom_character_skins` SET `skin_male` = 14, `skin_female` = 15 WHERE `token_id` = 50205;
 
--- Spirit Walker fixes:
+-- Spirit Walker:
 
 UPDATE `custom_character_skins` SET `skin_male` = 20, `skin_female` = 12 WHERE `token_id` = 81228;
 
--- Gnome Necromancer fixes: 
+-- Gnome Necromancer: 
 
 UPDATE `custom_character_skins` SET `skin_male` = 7, `skin_female` = 6 WHERE `token_id` = 81230;
 
--- Leper Gnome fixes:
+-- Leper Gnom:
 
 UPDATE `custom_character_skins` SET `skin_male` = 6, `skin_female` = 8 WHERE `token_id` = 50212;
+
+-- Demon Hunter:
+
+UPDATE `tw_world`.`custom_character_skins` SET `skin_male` = 12, `skin_female` = 14 WHERE `token_id` = 61105;
+
+-- Naturalist II:
+
+UPDATE `tw_world`.`custom_character_skins` SET `skin_male` = 16, `skin_female` = 12 WHERE `token_id` = 83099;
+
+-- Demon:
+
+UPDATE `tw_world`.`custom_character_skins` SET `skin_male` = 11, `skin_female` = 13 WHERE `token_id` = 61104;
+
+-- Dalaran Magi:
+
+UPDATE `tw_world`.`custom_character_skins` SET `skin_male` = 16, `skin_female` = 14 WHERE `token_id` = 83090;
+
+-- Necromancer I:
+
+UPDATE `tw_world`.`custom_character_skins` SET `skin_male` = 11, `skin_female` = 11 WHERE `token_id` = 50106;
+
+-- DK Dwarf:
+
+UPDATE `tw_world`.`custom_character_skins` SET `skin_male` = 24, `skin_female` = 0 WHERE `token_id` = 81229;
+
+-- Naturalist III:
+
+UPDATE `tw_world`.`custom_character_skins` SET `skin_male` = 17, `skin_female` = 0 WHERE `token_id` = 83100;
+
+-- Naturalist I:
+
+UPDATE `tw_world`.`custom_character_skins` SET `skin_male` = 15, `skin_female` = 11 WHERE `token_id` = 83092;
