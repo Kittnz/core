ebad414309e927ea71d33ae34df059100c1f9ca4
torta <torta@torta.com>
2023-12-20 05:56:40 +0100

    Fixed Gnome Necromancer token.

--- sql/database_updates/20231220044429_world.sql
+++ sql/database_updates/20231220044429_world.sql
@@ -5,3 +5,7 @@ UPDATE `custom_character_skins` SET `skin_male` = 14, `skin_female` = 15 WHERE `
 -- Spirit Walker fixes:
 
 UPDATE `custom_character_skins` SET `skin_male` = 20, `skin_female` = 12 WHERE `token_id` = 81228;
+
+-- Gnome Necromancer fixes: 
+
+UPDATE `custom_character_skins` SET `skin_male` = 7, `skin_female` = 6 WHERE `token_id` = 81230;
