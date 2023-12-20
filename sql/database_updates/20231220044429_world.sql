58e53e18c9c406297daa4f55f0345b682666e0a5
torta <torta@torta.com>
2023-12-20 05:51:58 +0100

    Fixed Spirit Walker tauren token.

--- sql/database_updates/20231220044429_world.sql
+++ sql/database_updates/20231220044429_world.sql
@@ -2,5 +2,6 @@
 
 UPDATE `custom_character_skins` SET `skin_male` = 14, `skin_female` = 15 WHERE `token_id` = 50205;
 
+-- Spirit Walker fixes:
 
-
+UPDATE `custom_character_skins` SET `skin_male` = 20, `skin_female` = 12 WHERE `token_id` = 81228;
