fb8ff26e8ca308a0979029ecd907b8b53d82bca7
torta <torta@torta.com>
2023-12-20 03:38:40 +0100

    Fixed some sell/buy gold exploits.

---
+++ sql/database_updates/20231220023722_world.sql
@@ -0,0 +1,4 @@
+UPDATE `item_template` SET `buy_price` = 6000 WHERE `entry` IN (81417, 81416, 81418);
+
+UPDATE `item_template` SET `name` = 'Runed Mithril Hammer', `buy_price` = 25141, `sell_price` = 12576 WHERE `entry` = 7946;
+UPDATE `item_template` SET `name` = 'Dark Iron Leggings',   `buy_price` = 42165, `sell_price` = 17013 WHERE `entry` = 17013;
