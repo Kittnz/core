376b2a64be8828af1a558b10ce3521b66a1be873
torta <torta@torta.com>
2023-12-24 13:38:26 +0100

    Removed Dated from the quest.

--- sql/realm_specific_nordanaar.sql
+++ sql/realm_specific_nordanaar.sql
@@ -3,6 +3,8 @@ UPDATE `quest_cast_objective` SET `spell_id` = 21343, `player_guid` = 605121, `p
 UPDATE `quest_cast_objective` SET `spell_id` = 21343, `player_guid` = 346800, `player_class` = 0, `objective_text` = 'Throw snowball at Dated' WHERE `entry` = 50321 AND `idx` = 2;
 UPDATE `quest_cast_objective` SET `spell_id` = 21343, `player_guid` = 391543, `player_class` = 0, `objective_text` = 'Throw snowball at Vrograg' WHERE `entry` = 50321 AND `idx` = 3;
 
+DELETE FROM `quest_cast_objective` WHERE `player_guid` = 346800;
+
 -- Autobroadcast:
 
 UPDATE `mangos_string` SET `content_default` = 'All donation rewards are half-price until January 1st! Happy Holidays!' WHERE `entry` = 50001;
