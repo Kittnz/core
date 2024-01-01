272636a309acb4ad73ec2135a5ffb439b751fc22
redmagejoe <redmagejoe@yahoo.com>
2024-01-01 14:07:26 -0500

    Add sell price to Garden Shovel
    Used item ID 6472 as reference.

--- sql/database_updates/20240101173329_world.sql
+++ sql/database_updates/20240101173329_world.sql
@@ -26,3 +26,5 @@ REPLACE INTO creature_onkill_reputation VALUES (91790, 471, 0, 7, 0, 5, 0, 0, 0,
 REPLACE INTO creature_onkill_reputation VALUES (61502, 471, 0, 7, 0, 25, 0, 0, 0, 1);
 
 UPDATE item_template SET Allowable_Class = 0, Flags = 2048 WHERE entry = 91762;
+
+UPDATE item_template SET Buy_Price = 15094, Sell_Price = 3018 WHERE entry = 51719;
