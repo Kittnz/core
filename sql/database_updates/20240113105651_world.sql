3605df7ecc8e953b21de9a73d92d9b00b262623c
redmagejoe <redmagejoe@yahoo.com>
2024-01-13 05:58:59 -0500

    Fix gender reference in completion text
    Added missing $gm:f substitution in Kum'isha's completion text for One Draenei's Junk quest.

---
+++ sql/database_updates/20240113105651_world.sql
@@ -0,0 +1,3 @@
+UPDATE quest_template SET
+OfferRewardText = 'You truly disgust me, $R. A grown $gman:woman drooling over a pile of junk? Pitiful! Take your reward and get out of my sight.$B$BSounds familiar? It should, because I say it a thousand times per day.$B$BOh how I loathe this world.'
+WHERE entry = 3502;
