419e354be3390ca6699e25548f55862abe0baa81
redmagejoe <redmagejoe@yahoo.com>
2024-01-13 18:19:19 -0500

    Add proper chaining to Felwood Stolen Notes quests

--- sql/database_updates/20240113105651_world.sql
+++ sql/database_updates/20240113105651_world.sql
@@ -1,3 +1,8 @@
 UPDATE quest_template SET
 OfferRewardText = 'You truly disgust me, $R. A grown $gman:woman drooling over a pile of junk? Pitiful! Take your reward and get out of my sight.$B$BSounds familiar? It should, because I say it a thousand times per day.$B$BOh how I loathe this world.'
 WHERE entry = 3502;
+
+UPDATE quest_template SET NextQuestInChain = 41073 WHERE entry = 41072;
+UPDATE quest_template SET NextQuestInChain = 41074 WHERE entry = 41073;
+UPDATE quest_template SET NextQuestInChain = 41076 WHERE entry = 41075;
+UPDATE quest_template SET NextQuestInChain = 41077 WHERE entry = 41076;
