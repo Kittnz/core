4163334c196b980bdc56af2a35a64faacdab0d97
torta <torta@torta.com>
2024-01-16 02:02:25 +0100

    Added skinning loot to  Bramblethorn Boar.

--- sql/database_updates/20240116003658_world.sql
+++ sql/database_updates/20240116003658_world.sql
@@ -8,7 +8,7 @@ REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `g
 
 -- https://github.com/slowtorta/turtlewow-bug-tracker/issues/5969
 
-UPDATE `quest_template` SET `OfferRewardText` = '<Wide eyes and an extreme expression of bewilderment can be seen on Ranala\'s face.>$B$BI can\'t believe my eyes! My gold pouch, it\'s... it\'s here! But... it seems to have lost some coins through this hole here. Regardless, most of it is still here. $n, thank you so much. Here, this is for your troubles. When we get back, we\'ll tell the superiors of what you did here.' WHERE `entry` = 41160
+UPDATE `quest_template` SET `OfferRewardText` = '<Wide eyes and an extreme expression of bewilderment can be seen on Ranala\'s face.>$B$BI can\'t believe my eyes! My gold pouch, it\'s... it\'s here! But... it seems to have lost some coins through this hole here. Regardless, most of it is still here. $n, thank you so much. Here, this is for your troubles. When we get back, we\'ll tell the superiors of what you did here.' WHERE `entry` = 41160;
 
 -- https://github.com/slowtorta/turtlewow-bug-tracker/issues/5762
 
@@ -30,3 +30,7 @@ REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `g
 REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (5225, 6291, 12.5606, 1, 1, 1, 0);
 REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (5225, 6289, 8.5945, 1, 1, 1, 0);
 REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (5225, 6292, 0.1381, 1, 1, 1, 0);
+
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/3960
+
+UPDATE `creature_template` SET `skinning_loot_id` = 1191 WHERE `entry` = 60980;
