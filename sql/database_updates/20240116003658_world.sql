73565838ea9aa7b9b65f6a0d35d51ca330847178
torta <torta@torta.com>
2024-01-16 02:41:56 +0100

    Added missing campfire to one of the murloc camps in Dustwallow Marsh.

--- sql/database_updates/20240116003658_world.sql
+++ sql/database_updates/20240116003658_world.sql
@@ -47,3 +47,7 @@ UPDATE `broadcast_text` SET `male_text` = 'Hey, hey, heyyyyy! Wake up Booty Bay,
 
 REPLACE INTO `world_safe_locs_facing` (`id`, `orientation`) VALUES (951, 3.8);
 REPLACE INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`) VALUES (951, 15, 67);
+
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/4338
+
+REPLACE INTO `gameobject` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecsmin`, `spawntimesecsmax`, `animprogress`, `state`, `spawn_flags`, `visibility_mod`) VALUES (5541, 179945, 1, -3018.72, -4345.71, 6.77611, 5.36293, 0, 0, 0.444061, -0.895996, 300, 300, 100, 1, 0, 0);
