2f82fd3ee91001be44eef61b2867f128fc4c051e
torta <torta@torta.com>
2024-01-16 01:55:01 +0100

    Added fishing loot tables to Thalassian Highlands.

--- sql/database_updates/20240116003658_world.sql
+++ sql/database_updates/20240116003658_world.sql
@@ -9,3 +9,24 @@ REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `g
 -- https://github.com/slowtorta/turtlewow-bug-tracker/issues/5969
 
 UPDATE `quest_template` SET `OfferRewardText` = '<Wide eyes and an extreme expression of bewilderment can be seen on Ranala\'s face.>$B$BI can\'t believe my eyes! My gold pouch, it\'s... it\'s here! But... it seems to have lost some coins through this hole here. Regardless, most of it is still here. $n, thank you so much. Here, this is for your troubles. When we get back, we\'ll tell the superiors of what you did here.' WHERE `entry` = 41160
+
+-- https://github.com/slowtorta/turtlewow-bug-tracker/issues/5762
+
+DELETE FROM `fishing_loot_template` WHERE `entry` = 2040;
+DELETE FROM `fishing_loot_template` WHERE `entry` = 5225;
+
+REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (2040, 6361, 0.3212, 1, 1, 1, 0);
+REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (2040, 6356, 1.6914, 1, 1, 1, 0);
+REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (2040, 6308, 0.3671, 1, 1, 1, 0);
+REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (2040, 6303, 0, 1, 1, 1, 0);
+REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (2040, 6291, 12.5606, 1, 1, 1, 0);
+REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (2040, 6289, 8.5945, 1, 1, 1, 0);
+REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (2040, 6292, 0.1381, 1, 1, 1, 0);
+
+REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (5225, 6361, 0.3212, 1, 1, 1, 0);
+REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (5225, 6356, 1.6914, 1, 1, 1, 0);
+REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (5225, 6308, 0.3671, 1, 1, 1, 0);
+REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (5225, 6303, 0, 1, 1, 1, 0);
+REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (5225, 6291, 12.5606, 1, 1, 1, 0);
+REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (5225, 6289, 8.5945, 1, 1, 1, 0);
+REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (5225, 6292, 0.1381, 1, 1, 1, 0);
