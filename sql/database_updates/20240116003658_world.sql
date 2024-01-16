1a0f654c3814d5281b4ef1187e2677ccc9719855
torta <torta@torta.com>
2024-01-16 01:42:37 +0100

    Added fishing loot tables to Blackstone Island.

---
+++ sql/database_updates/20240116003658_world.sql
@@ -0,0 +1,7 @@
+REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (5536, 6361, 0.3212, 1, 1, 1, 0);
+REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (5536, 6356, 1.6914, 1, 1, 1, 0);
+REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (5536, 6308, 0.3671, 1, 1, 1, 0);
+REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (5536, 6303, 0, 1, 1, 1, 0);
+REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (5536, 6291, 12.5606, 1, 1, 1, 0);
+REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (5536, 6289, 8.5945, 1, 1, 1, 0);
+REPLACE INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (5536, 6292, 0.1381, 1, 1, 1, 0);
