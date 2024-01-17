d11bd86b12a63dfdbe50ce579527b5d6b4233ab3
Dormilon <79369417+Dormilon@users.noreply.github.com>
2024-01-17 18:19:43 +0300

    Forsaken's riding for goblins works.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/5933

--- sql/database_updates/20240117122320_world.sql
+++ sql/database_updates/20240117122320_world.sql
@@ -1,3 +1,6 @@
 -- 6-slot vendor bags missing from Blackstone Island #5970.
 delete from npc_vendor where entry = 61743 and item = 4497;
 replace INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `itemflags`, `condition_id`) VALUES (61743, 0, 4496, 0, 0, 0, 0);
+-- Forsaken's riding for goblins works. #5933
+delete from conditions where condition_entry = 4127;
+replace INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (4127, 14, 434, 0, 0, 0, 0);
