b9ef5b803d3ce85a40289db68cede29648963167
Dormilon <79369417+Dormilon@users.noreply.github.com>
2024-01-24 17:48:10 +0300

    Item "Schematic: Unstable Mining Dynamite" drop repaired.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/6062

--- sql/database_updates/20240124123024_world.sql
+++ sql/database_updates/20240124123024_world.sql
@@ -2,3 +2,5 @@
 update item_template set item_level = 1 where entry = 41258;
 -- Unfishable school of fish in strangelthorn #6065
 update gameobject set position_x = -12026.2, position_y = 534.563 where guid = 47407;
+-- Schematic: Unstable Mining Dynamite bugged loot table #6062
+update reference_loot_template set chanceorquestchance = 0 where entry = 36000 and item = 51265;
