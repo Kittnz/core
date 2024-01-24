b2a3f7e00956d6155d82f2b2cd49e110015818c6
Dormilon <79369417+Dormilon@users.noreply.github.com>
2024-01-24 19:10:53 +0300

    Linking Ahn'quiraj NPCs.

--- sql/database_updates/20240124123024_world.sql
+++ sql/database_updates/20240124123024_world.sql
@@ -6,3 +6,9 @@ update gameobject set position_x = -12026.2, position_y = 534.563 where guid = 4
 update reference_loot_template set chanceorquestchance = 0 where entry = 36000 and item = 51265;
 -- Both Anthion's Parting Words quest versions (horde and alliance) can be picked up by me #6043
 update quest_template set requiredraces = 434 where entry = 9018;
+-- Simultaneous aggro when combating one of the NPCs.
+delete from creature_linking where master_guid in (88064,88063);
+REPLACE INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (88025, 88064, 3);
+REPLACE INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (88029, 88064, 3);
+REPLACE INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (88028, 88063, 3);
+REPLACE INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (88026, 88063, 3);
