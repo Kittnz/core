648c4395527636a3edcb7e2f8fbcc0d9edc83aac
Dormilon <79369417+Dormilon@users.noreply.github.com>
2024-01-24 18:15:26 +0300

    Quest 9018 fix: alowable races - horde.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/6043

--- sql/database_updates/20240124123024_world.sql
+++ sql/database_updates/20240124123024_world.sql
@@ -4,3 +4,5 @@ update item_template set item_level = 1 where entry = 41258;
 update gameobject set position_x = -12026.2, position_y = 534.563 where guid = 47407;
 -- Schematic: Unstable Mining Dynamite bugged loot table #6062
 update reference_loot_template set chanceorquestchance = 0 where entry = 36000 and item = 51265;
+-- Both Anthion's Parting Words quest versions (horde and alliance) can be picked up by me #6043
+update quest_template set requiredraces = 434 where entry = 9018;
