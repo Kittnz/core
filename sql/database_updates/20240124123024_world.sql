be94d0a1ecb02c10abc832746e8f9b9ba271ba18
Dormilon <79369417+Dormilon@users.noreply.github.com>
2024-01-24 16:55:14 +0300

    Fishing pool fix.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/6065

--- sql/database_updates/20240124123024_world.sql
+++ sql/database_updates/20240124123024_world.sql
@@ -1,2 +1,4 @@
 -- Black Riding Boots are unable to be repaired #6071
 update item_template set item_level = 1 where entry = 41258;
+-- Unfishable school of fish in strangelthorn #6065
+update gameobject set position_x = -12026.2, position_y = 534.563 where guid = 47407;
