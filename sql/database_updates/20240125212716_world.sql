3c455f61d5a5aea8ad148b40aa96abb16cfd5f5b
torta <torta@torta.com>
2024-01-25 22:27:51 +0100

    Dragunovi's item fixes.

---
+++ sql/database_updates/20240125212716_world.sql
@@ -0,0 +1,33 @@
+update item_template set stat_value2 = 9, spellid_2 = 15464 where entry = 81365; -- Kargron's Leggings
+update item_template set sheath = 1 where entry = 61523; -- Crystal Sword of the Blossom
+update item_template set class = 4, subclass = 0 where entry = 18634; -- Gyrofreeze Ice Reflector
+update item_template set bag_family = 256 where entry = 13873; -- Viewing Room Key
+-- fix meditation for good
+update item_template set spellid_1 = 57158 where entry = 19369; -- Gloves of Rapid Evolution
+update item_template set spellid_2 = 57158 where entry = 12756; -- Leggings of Arcana
+update item_template set spellid_1 = 57158 where entry = 83464; -- Mantle of Twisted Damnation
+update item_template set spellid_1 = 57158 where entry = 60788; -- Sandals of Inner Peace
+update item_template set spellid_2 = 57158 where entry = 65103; -- Shell of the Great Sleeper
+update item_template set spellid_3 = 57158 where entry = 61208; -- Staff of the Dreamer
+update item_template set spellid_2 = 57158 where entry = 65021; -- Verdant Dreamer's Breastplate
+update item_template set spellid_2 = 57158 where entry = 61193; -- Verdant Eye Necklace
+update item_template set spellid_2 = 57158 where entry = 51737; -- Vestments of Eternal Autumn
+update item_template set spellid_2 = 57158 where entry = 83451; -- Cowl of Mivax
+update item_template set spellid_2 = 57158 where entry = 9402; -- Earthborn Kilt
+update item_template set spellid_1 = 57158 where entry = 60564; -- Empowered Power Core
+update item_template set spellid_1 = 57158 where entry = 83207; -- Feymist Robe
+update item_template set spellid_1 = 57158 where entry = 15045; -- Green Dragonscale Breastplate
+update item_template set spellid_1 = 57158 where entry = 20296; -- Green Dragonscale Gauntlets
+update item_template set spellid_1 = 57158 where entry = 15046; -- Green Dragonscale Leggings
+update item_template set spellid_1 = 57158 where entry = 60798; -- Loop of the Forgotten
+update item_template set spellid_1 = 57158 where entry = 60790; -- Memento of the Lost
+update item_template set spellid_1 = 57158 where entry = 60809; -- Remains of the Lost
+update item_template set spellid_1 = 57158 where entry = 60735; -- Obsidian Gem Choker
+update item_template set spellid_1 = 57158 where entry = 2043; -- Ring of Forlorn Spirits
+update item_template set spellid_3 = 57158 where entry = 83465; -- Shroud of Haunted Torment
+update item_template set spellid_1 = 57158 where entry = 60590; -- Isle Watcher's Sash
+update item_template set spellid_1 = 57159 where entry = 60558; -- Cloudweaver Gloves
+update item_template set spellid_1 = 57159 where entry = 84032; -- Kilt of the Devoted
+
+
+
