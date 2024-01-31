fbaf01f571dd75ebfa2eb8403f04672a6278d47f
Dormilon <79369417+Dormilon@users.noreply.github.com>
2024-01-31 17:46:27 +0300

    Quest item of the task "The Seeker's Demise" is now available to the whole group.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/5857

--- sql/database_updates/20240131140959_world.sql
+++ sql/database_updates/20240131140959_world.sql
@@ -2,3 +2,5 @@
 update quest_template set RewChoiceItemId1 = 41151, RewChoiceItemId2 = 41152, RewChoiceItemCount1 = 1, RewChoiceItemCount2 = 1, RewItemId1 = 0, RewItemId2 = 0, RewItemCount1 = 0, RewItemCount2 = 0 where entry = 41215;
 -- Сopper vein in Durotar (60.1, 61.8) was pulled out of the ground.
 update gameobject set position_z = 33.129925 where guid = 12376;
+-- Quest item of the task "The Seeker's Demise" is now available to the whole group.
+update item_template set flags = 2048 where entry = 70026;
