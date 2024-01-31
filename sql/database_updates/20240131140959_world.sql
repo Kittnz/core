8711a93beaf3f6b78a272e4698f04d617c00e2ec
Dormilon <79369417+Dormilon@users.noreply.github.com>
2024-01-31 17:39:44 +0300

    Сopper vein was pulled out of the ground.
    Fixes -- https://github.com/slowtorta/turtlewow-bug-tracker/issues/5859

--- sql/database_updates/20240131140959_world.sql
+++ sql/database_updates/20240131140959_world.sql
@@ -1,1 +1,4 @@
+-- Now in the quest "Whispers of Autumn" you need to choose one of two awards.
 update quest_template set RewChoiceItemId1 = 41151, RewChoiceItemId2 = 41152, RewChoiceItemCount1 = 1, RewChoiceItemCount2 = 1, RewItemId1 = 0, RewItemId2 = 0, RewItemCount1 = 0, RewItemCount2 = 0 where entry = 41215;
+-- Сopper vein in Durotar (60.1, 61.8) was pulled out of the ground.
+update gameobject set position_z = 33.129925 where guid = 12376;
