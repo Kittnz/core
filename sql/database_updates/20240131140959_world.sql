b36efee6e1cfd79f7dcfca8b9ddfddfdbaa02ac2
Dormilon <79369417+Dormilon@users.noreply.github.com>
2024-01-31 21:38:07 +0300

    Alliance Chest in the Silverpine Forest (63.3, 58.2) is now available for looting.
    Fixes https://github.com/slowtorta/turtlewow-bug-tracker/issues/5999

--- sql/database_updates/20240131140959_world.sql
+++ sql/database_updates/20240131140959_world.sql
@@ -8,3 +8,5 @@ update item_template set flags = 2048 where entry = 70026;
 UPDATE `broadcast_text` SET `male_text` = 'I got nothing for you, kid.' WHERE `entry` = 7656;
 -- Solid Chest in the Hillsbrad Foothills (75.0, 41.0) is now available for looting.
 update gameobject set position_x = -485.025, position_y = -1340.7, position_z = 53.6757, orientation = 3.95072 where guid = 62585;
+-- Alliance Chest in the Silverpine Forest (63.3, 58.2) is now available for looting.
+update gameobject set position_x = 36.8191, position_y = 790.141, position_z = 64.9497, orientation = 1.57 where guid = 62555;
