9121e0dee1b9c2caa3734e431f526d1f99cbf279
redmagejoe <redmagejoe@yahoo.com>
2024-02-01 15:44:55 -0500

    Make Smoldering Dream Essence bind on pickup

--- sql/database_updates/20240201124524_world.sql
+++ sql/database_updates/20240201124524_world.sql
@@ -10,3 +10,6 @@ UPDATE gameobject_template SET Name = 'Ancient Treasure Chest' WHERE entry = 201
 -- Add direction to quest objectives for Intact Power Core
 UPDATE quest_template SET Type = 81, Objectives = 'Bring an Intact Power Core from Uldaman''s Ancient Treasures to Kex Blowmaster in the southern Barrens.' WHERE entry = 40131;
 UPDATE quest_template SET Objectives = 'Bring an Intact Power Core from Uldaman''s Ancient Treasures to Torble Sparksprocket in the southern Barrens.' WHERE entry = 40129;
+
+-- Make Smoldering Dream Essence Bind on Pickup
+UPDATE item_template SET Bonding = 4 WHERE entry = 61444;
