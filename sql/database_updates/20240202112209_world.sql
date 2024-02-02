51dffbebbe0df9600907fdb28176ac822be983bd
Dormilon <79369417+Dormilon@users.noreply.github.com>
2024-02-02 15:55:12 +0300

    NPC "Ralo'shan" now has blizzlike gossip menu.

---
+++ sql/database_updates/20240202112209_world.sql
@@ -0,0 +1,4 @@
+-- NPC "Ralo'shan" now has blizzlike gossip menu.
+UPDATE `gossip_menu_option` SET `action_menu_id` = 6585 WHERE `menu_id` = 6586;
+UPDATE `gossip_menu_option` SET `action_menu_id` = 6586 WHERE `menu_id` = 6587;
+UPDATE `gossip_menu_option` SET `action_menu_id` = 6587 WHERE `menu_id` = 6588;
