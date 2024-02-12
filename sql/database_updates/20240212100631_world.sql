9eec5a6355ffbe7d0aae847f7ee05fcb627674a3
brotalnia <brotalnia@brotalnia.com>
2024-02-12 12:13:17 +0200

    Reduce amorous aura timers.

---
+++ sql/database_updates/20240212100631_world.sql
@@ -0,0 +1,3 @@
+-- Reduce amore aura refresh timers.
+UPDATE `spell_template` SET `effectAmplitude1`=60000 WHERE `entry`=26870;
+UPDATE `spell_template` SET `effectAmplitude1`=30000 WHERE `entry`=27742;
