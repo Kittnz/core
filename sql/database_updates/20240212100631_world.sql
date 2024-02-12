b5590aa08ccea882bb125201f6887fa31b752ce2
brotalnia <brotalnia@brotalnia.com>
2024-02-12 13:23:19 +0200

    Do not remove Amorous auras on evade.

--- sql/database_updates/20240212100631_world.sql
+++ sql/database_updates/20240212100631_world.sql
@@ -1,3 +1,2 @@
 -- Reduce amore aura refresh timers.
-UPDATE `spell_template` SET `effectAmplitude1`=60000 WHERE `entry`=26870;
 UPDATE `spell_template` SET `effectAmplitude1`=30000 WHERE `entry`=27742;
