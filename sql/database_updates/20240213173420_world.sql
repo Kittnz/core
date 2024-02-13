a13de299742ed1182ed67bccabd877a9a5d4e083
torta <torta@torta.com>
2024-02-13 18:35:04 +0100

    Fallen Crusader Legguards fix.

---
+++ sql/database_updates/20240213173420_world.sql
@@ -0,0 +1,8 @@
+update item_template set spellid_1 = 15464, stat_value1 = 22, stat_value2 = 10, stat_type2 = 3, stat_value3 = 9, stat_type3 = 6 where entry = 81355; 
+
+-- Fallen Crusader Legguards 
+-- 17 str -> 22 str, added 10 agi, removed 28 dmg to undead, added 1 hit
+-- 28 dmg to undead is a flat increase not like attack power so its impact is too big and not a single item can come close to its dps vs undead
+
+
+
