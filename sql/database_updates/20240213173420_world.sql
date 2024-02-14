a0579e30a04eea67091bfa9ce5cb6dd746d96e38
redmagejoe <redmagejoe@yahoo.com>
2024-02-13 22:56:38 -0500

    Fix chaining and redundancy of Captain Grayson vs Cookie quest
    Removed requirement to kill Cookie. There is no reason to require the player to kill an enemy they absolutely MUST KILL in order to acquire the quest item.

--- sql/database_updates/20240213173420_world.sql
+++ sql/database_updates/20240213173420_world.sql
@@ -4,5 +4,6 @@ update item_template set spellid_1 = 15464, stat_value1 = 22, stat_value2 = 10, 
 -- 17 str -> 22 str, added 10 agi, removed 28 dmg to undead, added 1 hit
 -- 28 dmg to undead is a flat increase not like attack power so its impact is too big and not a single item can come close to its dps vs undead
 
-
-
+-- Fix chaining on Captain Grayson vs Cookie quest, and remove redundant quest objectives
+UPDATE quest_template SET NextQuestInChain = 40396 WHERE entry = 40395;
+UPDATE quest_template SET ReqCreatureOrGOId1 = 0, ReqCreatureOrGOCount = 0 WHERE entry = 40396;
