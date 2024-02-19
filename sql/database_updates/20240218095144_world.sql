88438592fd709f477b364a05403f83d1b2a93e6a
redmagejoe <redmagejoe@yahoo.com>
2024-02-19 07:15:00 -0500

    Fix leaf icon for Alterac Yeti Hide

--- sql/database_updates/20240218095144_world.sql
+++ sql/database_updates/20240218095144_world.sql
@@ -3,3 +3,6 @@ UPDATE `creature_spells` SET `castTarget_2` = 1 WHERE `entry` = 160170
 
 -- Delete Holy Strike Libram sellers
 DELETE FROM creature WHERE id IN (51270, 51271);
+
+-- Fix leaf icon on yeti hide
+UPDATE item_template SET Display_ID = 7112 WHERE entry = 41294;
