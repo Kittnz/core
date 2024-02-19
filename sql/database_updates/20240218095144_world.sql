2f9c295a98334c3cf3cf51da41933f5c6ed52264
Dormilon <79369417+Dormilon@users.noreply.github.com>
2024-02-19 16:09:27 +0300

    Missed ";" on file 20240218095144_world.

--- sql/database_updates/20240218095144_world.sql
+++ sql/database_updates/20240218095144_world.sql
@@ -1,5 +1,5 @@
 -- Set Patchwork Golem's Cleave to cast on the target instead of the caster
-UPDATE `creature_spells` SET `castTarget_2` = 1 WHERE `entry` = 160170
+UPDATE `creature_spells` SET `castTarget_2` = 1 WHERE `entry` = 160170;
 
 -- Delete Holy Strike Libram sellers
 DELETE FROM creature WHERE id IN (51270, 51271);
