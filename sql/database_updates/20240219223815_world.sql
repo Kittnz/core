e3289b434342a06793772be364af96e1fcff1559
redmagejoe <redmagejoe@yahoo.com>
2024-02-20 21:26:54 -0500

    Add quest chaining to Into the Dream

--- sql/database_updates/20240219223815_world.sql
+++ sql/database_updates/20240219223815_world.sql
@@ -2,3 +2,10 @@
 -- that way, it will not be interrupted, if player release corpse
 UPDATE `spell_template` SET `rangeIndex` = '13' WHERE (`entry` = '22027');
 UPDATE `spell_template` SET `attributesEx2` = '4' WHERE (`entry` = '22027');
+
+-- Fix Into the Dream chaining
+UPDATE quest_template SET NextQuestInChain = 40958 WHERE entry = 40957;
+UPDATE quest_template SET NextQuestInChain = 40959 WHERE entry = 40958;
+UPDATE quest_template SET NextQuestInChain = 40960 WHERE entry = 40959;
+UPDATE quest_template SET NextQuestInChain = 40961 WHERE entry = 40960;
+UPDATE quest_template SET NextQuestInChain = 40962 WHERE entry = 40961;
