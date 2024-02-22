a43a1207f460ec8776350d56121f6cf147bbb0d5
torta <torta@torta.com>
2024-02-22 23:07:30 +0100

    Jewelcrafting trade items.

--- sql/database_updates/20240222215514_world.sql
+++ sql/database_updates/20240222215514_world.sql
@@ -8,3 +8,21 @@ UPDATE `creature_template` SET `subname` = 'Journeyman Jewelcrafter' WHERE `entr
 UPDATE `creature_template` SET `subname` = 'Expert Jewelcrafter' WHERE `entry` = 61917;
 -- NPC Ethennar Somberfall, change display ID to 16182
 UPDATE `creature_template` SET `display_id1` = 16182 WHERE `entry` = 61917;
+-- Add item 55155 (jewelers kit) to the following vendors 61909, 61905, 61914, 61915
+REPLACE INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `itemflags`, `condition_id`) VALUES 
+(61909, 0, 55155, 0, 0, 0, 0),
+(61905, 0, 55155, 0, 0, 0, 0),
+(61914, 0, 55155, 0, 0, 0, 0),
+(61915, 0, 55155, 0, 0, 0, 0),
+(61909, 0, 55245, 0, 0, 0, 0),
+(61909, 0, 55246, 0, 0, 0, 0),
+(61909, 0, 55247, 0, 0, 0, 0),  
+(61905, 0, 55245, 0, 0, 0, 0),
+(61905, 0, 55246, 0, 0, 0, 0),
+(61905, 0, 55247, 0, 0, 0, 0),  
+(61914, 0, 55245, 0, 0, 0, 0),
+(61914, 0, 55246, 0, 0, 0, 0),
+(61914, 0, 55247, 0, 0, 0, 0),  
+(61915, 0, 55245, 0, 0, 0, 0),
+(61915, 0, 55246, 0, 0, 0, 0),
+(61915, 0, 55247, 0, 0, 0, 0);
