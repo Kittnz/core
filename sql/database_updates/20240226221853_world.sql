610850cfa6df20294affaeba185bffb183b270aa
slowtorta <slowtorta@gmail.com>
2024-02-27 04:23:38 +0000

    Direct world update.

--- sql/database_updates/20240226221853_world.sql
+++ sql/database_updates/20240226221853_world.sql
@@ -4,3 +4,4 @@ REPLACE INTO `gameobject` VALUES ( 5018385, 179862, 0, -4709.41, -1174.03, 505.2
 DELETE FROM gameobject WHERE guid = '5018385';
 DELETE FROM game_event_gameobject WHERE guid = '5018385';
 DELETE FROM gameobject_battleground WHERE guid = '5018385';
+UPDATE `creature` SET `position_x` = 1432.624756, `position_y` = 123.465935, `position_z` = -62.289360, `orientation` = 2.931251 WHERE `guid` = 32040;
