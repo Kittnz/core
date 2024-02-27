ce35e2ba883aeaf830575a7e2b3f15f5c347f6e1
torta <torta@torta.com>
2024-02-27 20:44:57 +0100

    Deleted duplicate spells.

--- sql/database_updates/20240226212702_world.sql
+++ sql/database_updates/20240226212702_world.sql
@@ -297,6 +297,7 @@ UPDATE `spell_template` SET `requiresSpellFocus` = @if_required_spell_focus, `ca
 UPDATE `spell_template` SET `castingTimeIndex` = @cast_time_index, `targets` = @targets, `effectImplicitTargetA1` = 0, `interruptFlags` = 0, `dmgClass` = 0 WHERE `entry` = @learn_spell_id;
 REPLACE INTO `npc_trainer_template` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillvalue`, `reqlevel`) VALUES (4, @learn_spell_id, @cost, @skill_id, @skill_trainer_required, 0);
 
--- Spell fix:
-
-UPDATE `spell_template` SET `description` = 'Your melee and ranged attacks have a chance to inject poison into your target that deals 130 Nature damage over 12 sec. Stacks up to 2 times.' WHERE `entry` = 45417;
+DELETE FROM `spell_template` WHERE `entry` = 41078;
+DELETE FROM `spell_template` WHERE `entry` = 41079;
+DELETE FROM `skill_line_ability` WHERE `spell_id` = 17039;
+DELETE FROM `npc_trainer_template` WHERE `spell` = 41078;
