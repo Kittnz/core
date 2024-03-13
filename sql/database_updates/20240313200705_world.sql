cf1b7aacaf43b7c98c7fdb8ebe82cd78be051e69
Dormilon <79369417+Dormilon@users.noreply.github.com>
2024-03-13 23:56:30 +0300

    Item 55200 and 55204 fixes: bonding.

---
+++ sql/database_updates/20240313200705_world.sql
@@ -0,0 +1,4 @@
+-- Item Voidheart Charm (55200), make bind on equip.
+update item_template set bonding = 2 where entry = 55200;
+-- Item Runebound Amulet (55204), make bind on equip.
+update item_template set bonding = 2 where entry = 55204;
