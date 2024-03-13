8bc118c7fae398b4d951dfb0fa997e12c6293156
slowtorta <slowtorta@gmail.com>
2024-03-13 06:54:03 +0000

    Direct world update.

---
+++ sql/database_updates/20240313001819_world.sql
@@ -0,0 +1,5 @@
+
+-- Changes by MORIENA
+DELETE FROM gameobject WHERE guid = '5018428';
+DELETE FROM game_event_gameobject WHERE guid = '5018428';
+DELETE FROM gameobject_battleground WHERE guid = '5018428';
