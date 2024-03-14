4a3ba09cbf41236bf7598f7cb98ebb48c991e98e
slowtorta <slowtorta@gmail.com>
2024-03-14 00:18:26 +0000

    Direct world update.

--- sql/database_updates/20240313153655_world.sql
+++ sql/database_updates/20240313153655_world.sql
@@ -1,3 +1,9 @@
 
 -- Changes by WOJI
 REPLACE INTO `creature` VALUES (2582886,14399,0,0,0,532,-11269.5,-1950.17,240.47,2.30231,120,120,5,100,100,1,0,0);
+DELETE FROM creature WHERE guid=2582886;
+DELETE FROM creature_addon WHERE guid=2582886;
+DELETE FROM creature_movement WHERE id=2582886;
+DELETE FROM game_event_creature WHERE guid=2582886;
+DELETE FROM game_event_creature_data WHERE guid=2582886;
+DELETE FROM creature_battleground WHERE guid=2582886;
