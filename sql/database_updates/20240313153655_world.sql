fc3fd65478e38b66de65581b0c6e86e155fca143
slowtorta <slowtorta@gmail.com>
2024-03-15 07:35:15 +0000

    Direct world update.

--- sql/database_updates/20240313153655_world.sql
+++ sql/database_updates/20240313153655_world.sql
@@ -7,3 +7,6 @@ DELETE FROM creature_movement WHERE id=2582886;
 DELETE FROM game_event_creature WHERE guid=2582886;
 DELETE FROM game_event_creature_data WHERE guid=2582886;
 DELETE FROM creature_battleground WHERE guid=2582886;
+
+-- Changes by REN
+REPLACE INTO `gameobject` VALUES ( 5018437, 2005741, 0, 1484.98, 178.099, -62.1909, 3.92556, 0, 0, 0.924153, -0.382023, 300, 300, 100, 1, 0, 0);
