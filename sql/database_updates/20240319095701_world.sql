16da62f9330b2ebf27af93eb5c52881983656ca3
Dormilon <79369417+Dormilon@users.noreply.github.com>
2024-03-19 13:07:16 +0300

    Item 55241 and 55241 fixes: Sheath.

---
+++ sql/database_updates/20240319095701_world.sql
@@ -0,0 +1,4 @@
+-- Item Opaline Illuminator (entry 55242), change sheath type to staff (can copy from an existing staff).
+update item_template set sheath = 2 where entry = 55242;
+-- Item Garnet Guardian Stave (Entry 55241) change sheath type to staff (can copy from an existing staff).
+update item_template set sheath = 2 where entry = 55241;
