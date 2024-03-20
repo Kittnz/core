b8a17534267c0faf61ebb2d5b87987d246057fc5
torta <torta@torta.com>
2024-03-20 19:04:07 +0100

    Fixed too fast respawn timers for Young Goretusks.

--- sql/database_updates/20240320174928_world.sql
+++ sql/database_updates/20240320174928_world.sql
@@ -1,4 +1,5 @@
 -- https://turtle-wow.org/bug-report?id=6678
 UPDATE `item_template` SET `sheath` = 3 WHERE `entry` = 21392;
 
-
+-- https://turtle-wow.org/bug-report?id=6588
+UPDATE `creature` SET `spawntimesecsmin` = 300, `spawntimesecsmax` = 300 WHERE `id` = 454;
