117e0c7748184ba14abd744a4d2fb51ffd25a8d2
Dormilon <79369417+Dormilon@users.noreply.github.com>
2024-03-24 14:42:16 +0300

    Drop chances of items "Recipe: Greater Frost Protection Potion", "Recipe: Greater Nature Protection Potion", "Recipe: Greater Shadow Protection Potion", "Recipe: Greater Arcane Protection Potion" setted to blazzlike.
    Fixes https://turtle-wow.org/bug-report?id=6700

--- sql/database_updates/20240324111207_world.sql
+++ sql/database_updates/20240324111207_world.sql
@@ -1,2 +1,10 @@
 -- Drop count of item "AZURE WHELPLIN" changed to 1. https://turtle-wow.org/bug-report?id=6713
 update creature_loot_template set maxcount = 1 where entry = 60909 and item = 50083;
+-- Drop chances of items "Recipe: Greater Frost Protection Potion", "Recipe: Greater Nature Protection Potion", "Recipe: Greater Shadow Protection Potion", "Recipe: Greater Arcane Protection Potion" setted to blazzlike. https://turtle-wow.org/bug-report?id=6700
+update creature_loot_template set ChanceOrQuestChance = 5 where item = 13495 and entry = 7428;
+update creature_loot_template set ChanceOrQuestChance = 3 where item = 13496 and entry in (1812,1813);
+update creature_loot_template set ChanceOrQuestChance = 9 where item = 13497 and entry in (7437);
+update creature_loot_template set ChanceOrQuestChance = 1.6 where item = 13499 and entry in (8546,8550);
+
+-- https://turtle-wow.org/bug-report?id=6709 cannot reproduce
+
