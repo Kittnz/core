8330fce78fdefeb9f7fdf9fc34ccb69a87d17d41
Dormilon <79369417+Dormilon@users.noreply.github.com>
2024-03-24 15:17:06 +0300

    Pet "Cottontail Rabbit" has been deleted from inkeeper's sell lists.

--- sql/database_updates/20240324111207_world.sql
+++ sql/database_updates/20240324111207_world.sql
@@ -5,6 +5,5 @@ update creature_loot_template set ChanceOrQuestChance = 5 where item = 13495 and
 update creature_loot_template set ChanceOrQuestChance = 3 where item = 13496 and entry in (1812,1813);
 update creature_loot_template set ChanceOrQuestChance = 9 where item = 13497 and entry in (7437);
 update creature_loot_template set ChanceOrQuestChance = 1.6 where item = 13499 and entry in (8546,8550);
-
--- https://turtle-wow.org/bug-report?id=6709 cannot reproduce
-
+-- Pet "Cottontail Rabbit" has been deleted from inkeeper's sell lists. https://turtle-wow.org/bug-report?id=6694
+delete from npc_vendor where item = 50081, entry in (61644,81057);
