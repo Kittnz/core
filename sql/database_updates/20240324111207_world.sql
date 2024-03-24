b7a25d37dcbea479ea82141790711d36cc49fe3a
Dormilon <79369417+Dormilon@users.noreply.github.com>
2024-03-24 17:45:48 +0300

    Quest 39993 required rep fix. NPC 2599 faction fix.
    Fixes https://turtle-wow.org/bug-report?id=6638

--- sql/database_updates/20240324111207_world.sql
+++ sql/database_updates/20240324111207_world.sql
@@ -6,4 +6,7 @@ update creature_loot_template set ChanceOrQuestChance = 3 where item = 13496 and
 update creature_loot_template set ChanceOrQuestChance = 9 where item = 13497 and entry in (7437);
 update creature_loot_template set ChanceOrQuestChance = 1.6 where item = 13499 and entry in (8546,8550);
 -- Pet "Cottontail Rabbit" has been deleted from inkeeper's sell lists. https://turtle-wow.org/bug-report?id=6694
-delete from npc_vendor where item = 50081, entry in (61644,81057);
+delete from npc_vendor where item = 50081 and entry in (61644,81057);
+-- Loed falconcrest: syndicate questchain is now fully available. NPC "Otto" now does not attack the player if player has a reputation with the syndicate. https://turtle-wow.org/bug-report?id=6638
+update quest_template set RequiredMaxRepValue = 0, RequiredMinRepValue = 3000, RequiredMaxRepFaction = 0, RequiredMinRepFaction = 70 where entry = 39993;
+update creature_template set faction = 87 where entry = 2599;
