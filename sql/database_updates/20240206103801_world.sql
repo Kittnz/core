4fe9a8684e1df3b334feae0f46b313551b69fd0e
brotalnia <brotalnia@brotalnia.com>
2024-03-27 20:39:06 +0200

    RMJ must be trolling at this point

--- sql/database_updates/20240206103801_world.sql
+++ sql/database_updates/20240206103801_world.sql
@@ -2,4 +2,4 @@
 UPDATE quest_template SET MinLevel = 38 WHERE entry = 40994;
 
 -- Deprecate removed Tamamo / Ronae'Thalas quest
-UPDATE quest_template SET Name = '[DEPRECATED] No Hope For Tomorrow' WHERE entry = 40184;
+UPDATE quest_template SET Title = '[DEPRECATED] No Hope For Tomorrow' WHERE entry = 40184;
