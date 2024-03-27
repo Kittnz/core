4c8fb2ae2a19ade88bf52cee95477f9ed5521cd1
slowtorta <slowtorta@gmail.com>
2024-03-27 21:01:06 +0000

    Direct world update.

--- sql/database_updates/20240327124052_world.sql
+++ sql/database_updates/20240327124052_world.sql
@@ -6,3 +6,11 @@ DELETE FROM creature_movement WHERE id=2580402;
 DELETE FROM game_event_creature WHERE guid=2580402;
 DELETE FROM game_event_creature_data WHERE guid=2580402;
 DELETE FROM creature_battleground WHERE guid=2580402;
+
+-- Changes by QUARRION
+DELETE FROM creature WHERE guid=2582783;
+DELETE FROM creature_addon WHERE guid=2582783;
+DELETE FROM creature_movement WHERE id=2582783;
+DELETE FROM game_event_creature WHERE guid=2582783;
+DELETE FROM game_event_creature_data WHERE guid=2582783;
+DELETE FROM creature_battleground WHERE guid=2582783;
