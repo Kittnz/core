12a6e48dbc882e7cbb138fb743af81aa4d4abb0d
slowtorta <slowtorta@gmail.com>
2024-03-28 10:54:26 +0000

    Direct world update.

--- sql/database_updates/20240327124052_world.sql
+++ sql/database_updates/20240327124052_world.sql
@@ -41,3 +41,5 @@ DELETE FROM creature_battleground WHERE guid=2582779;
 
 -- Changes by JOE
 UPDATE `creature` SET `position_x` = 2.428304, `position_y` = -47.513714, `position_z` = -64.942413, `orientation` = 4.507781 WHERE `guid` = 85959;
+UPDATE `creature` SET `position_x` = 0.153507, `position_y` = -55.652740, `position_z` = -66.274391, `orientation` = 4.045178 WHERE `guid` = 517;
+UPDATE `creature` SET `position_x` = 0.387770, `position_y` = -58.366314, `position_z` = -66.407410, `orientation` = 1.512267 WHERE `guid` = 85958;
