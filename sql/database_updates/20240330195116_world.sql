e8d3144eac39b3240b6f9d2308c4e638eb78c7e0
Dormilon <79369417+Dormilon@users.noreply.github.com>
2024-03-30 23:46:16 +0300

    NPC "Tamaala Bloodhoof" no longer appears as a male tauren.
    Fixes https://turtle-wow.org/bug-report?id=6776

--- sql/database_updates/20240330195116_world.sql
+++ sql/database_updates/20240330195116_world.sql
@@ -1,2 +1,4 @@
 -- NPCs "Healing Ward IV" no longer drop loot https://turtle-wow.org/bug-report?id=6746
 delete from creature_loot_template where entry = 3844;
+-- NPC "Tamaala Bloodhoof" no longer appears as a male tauren. -- https://turtle-wow.org/bug-report?id=6776
+update creature_template set display_id1 = 2121 where entry = 65137;
