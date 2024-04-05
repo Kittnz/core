d3313bd427ff3c2a7157bf45f333fafdf7514c7d
torta <torta@torta.com>
2024-04-05 23:06:55 +0200

    Fix crab pet to be non-bop.

--- sql/database_updates/20240404192242_world.sql
+++ sql/database_updates/20240404192242_world.sql
@@ -88,7 +88,7 @@ SET @pet_name = 'Spawn of Cla''ckora';
 SET @pet_desc = 'The spawn of the mighty Cla''ckora!';
 SET @pet_scale = 0.3;
 SET @pet_model = 1818;
-SET @pet_item_icon = 1144;
+SET @pet_item_icon = 2627;
 SET @pet_spell_icon = 271;
 
 REPLACE INTO `creature_display_info_addon` (`display_id`) VALUES (@pet_model);
@@ -98,3 +98,4 @@ REPLACE INTO `creature_template` VALUES (@pet_npc_entry, @pet_model, 0, 0, 0, 0,
 REPLACE INTO `spell_template` VALUES (@pet_spell_entry, 0, 0, 0, 0, 0, 272, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 31, 0, 0, 0, 101, 0, 0, 0, 0, 21, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 97, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1, -1, 0, 0, 0, 32, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1000, 1000, 1000, 0, 0, 0, 0, 0, 0, @pet_npc_entry, 0, 0, 0, 0, 0, 0, 0, 0, 353, 0, @pet_spell_icon, 0, 0, @pet_name, 16712190, '', 16712190, @pet_desc, 16712190, '', 16712190, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 1, 1, 1, 0, 0, 0, 0);
 REPLACE INTO `skill_line_ability` VALUES (@pet_skilline_entry, 1005, @pet_spell_entry, 0, 0, 0, 0, 0, 0, 0, 0);
 
+UPDATE `item_template` SET `quality` = 3,`bonding` = 0 WHERE `entry` = @pet_item_entry;
