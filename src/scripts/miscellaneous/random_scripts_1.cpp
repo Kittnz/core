83d3cd48b3f9bf848737708c5bddca99a3587f0f
torta <torta@torta.com>
2024-04-07 23:48:46 +0300

    Out-out for the Boaring Challenge.

--- src/scripts/miscellaneous/random_scripts_1.cpp
+++ src/scripts/miscellaneous/random_scripts_1.cpp
@@ -5126,6 +5126,9 @@ bool GossipHello_glyph_master(Player* pPlayer, Creature* pCreature)
     if (pPlayer->HasSpell(SPELL_VARGANT_MODE) && (pPlayer->GetLevel() < 60))
         pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TALK, 67035, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 9);
 
+    if (pPlayer->HasSpell(SPELL_BOARING_MODE) && (pPlayer->GetLevel() < 60))
+        pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TALK, 67037, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 10);
+
     // info about glyphs
     pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TALK, 66632, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 5);
     pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TALK, 66633, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 6);
@@ -5166,6 +5169,12 @@ bool GossipSelect_glyph_master(Player* pPlayer, Creature* pCreature, uint32 uiSe
     {
         if (pPlayer->HasChallenge(CHALLENGE_VAGRANT_MODE))
             pPlayer->RemoveSpell(SPELL_VARGANT_MODE, false, false);
+    }
+
+    if (uiAction == GOSSIP_ACTION_INFO_DEF + 10)
+    {
+        if (pPlayer->HasChallenge(CHALLENGE_BOARING_MODE))
+            pPlayer->RemoveSpell(SPELL_BOARING_MODE, false, false);
     }
 
     if (uiAction == GOSSIP_ACTION_INFO_DEF + 5)
