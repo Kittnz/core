a11d95e0548795ca8564f0085418732e686180d8
torta <torta@torta.com>
2024-04-09 21:47:33 +0300

    Netherbane Spellbook > Netherbane Rod.

--- sql/database_updates/20240404192242_world.sql
+++ sql/database_updates/20240404192242_world.sql
@@ -1370,3 +1370,6 @@ update item_template set set_id = 636 where entry = 55196;
 
 -- Replace the spell on the item 56095 with 21629.
 update item_template set spellid_1 = 21629 where entry = 56095;
+
+UPDATE `spell_template` SET `name` = 'Netherbane Rod' WHERE `entry` = 41735;
+UPDATE `spell_template` SET `name` = 'Netherbane Rod' WHERE `entry` = 41736;
