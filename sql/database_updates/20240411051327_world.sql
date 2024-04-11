1d3c290feaa5a5cf6180231e7fbb1157f8bf243a
slowtorta <slowtorta@gmail.com>
2024-04-11 08:21:26 +0100

    Direct world update.

---
+++ sql/database_updates/20240411051327_world.sql
@@ -0,0 +1,3 @@
+
+-- Changes by SHANG
+REPLACE INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (921,-11048.212891,-1985.837036,101.574295,2.196399,814,'karaforty');
