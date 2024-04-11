fdeb11f5d01d754999f13958c670d61d93450580
slowtorta <slowtorta@gmail.com>
2024-04-11 19:43:25 +0100

    Direct world update.

--- sql/database_updates/20240411051327_world.sql
+++ sql/database_updates/20240411051327_world.sql
@@ -1,3 +1,11 @@
 
 -- Changes by SHANG
 REPLACE INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (921,-11048.212891,-1985.837036,101.574295,2.196399,814,'karaforty');
+
+-- Changes by DRAGU
+DELETE FROM creature WHERE guid=2582882;
+DELETE FROM creature_addon WHERE guid=2582882;
+DELETE FROM creature_movement WHERE id=2582882;
+DELETE FROM game_event_creature WHERE guid=2582882;
+DELETE FROM game_event_creature_data WHERE guid=2582882;
+DELETE FROM creature_battleground WHERE guid=2582882;
