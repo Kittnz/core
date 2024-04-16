a515c8e8767e65aad3d25849bf9df5dd49cc3d2f
Dormilon <79369417+Dormilon@users.noreply.github.com>
2024-04-16 20:46:21 +0300

    Cancelled: Item Tome of Portals: Theramore drops only for alliance, Tome of Portals: Stonard only for horde.

--- sql/database_updates/20240416144607_world.sql
+++ sql/database_updates/20240416144607_world.sql
@@ -1,7 +1,7 @@
 -- Items Tome of Portals: Stonard and Tome of Portals: Theramore are divided by references. Item Tome of Portals: Theramore drops only for alliance, Tome of Portals: Stonard only for horde.
 delete from reference_loot_template where entry in (30593,30610);
-replace INTO reference_loot_template (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (30593, 92004, 100, 1, 1, 1, 4127);
-replace INTO reference_loot_template (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (30610, 92002, 100, 1, 1, 1, 30001);
+replace INTO reference_loot_template (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (30593, 92004, 100, 1, 1, 1, 0);
+replace INTO reference_loot_template (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (30610, 92002, 100, 1, 1, 1, 0);
 
 replace INTO creature_loot_template (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (61192, 30610, 0.32, 1, -30610, 1, 0);
 replace INTO creature_loot_template (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`) VALUES (61193, 30610, 0.32, 1, -30610, 1, 0);
