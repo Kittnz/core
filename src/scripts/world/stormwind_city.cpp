63cd2d7e6425111a163ec45049e27beaf195c3bc
brotalnia <brotalnia@brotalnia.com>
2024-04-19 18:04:16 +0300

    Do not flag players for PvP in quest The Attack.

--- src/scripts/world/stormwind_city.cpp
+++ src/scripts/world/stormwind_city.cpp
@@ -839,7 +839,11 @@ struct npc_lord_gregor_lescovarAI : public npc_escortAI
                         if (Creature* pTyrion = m_creature->GetMap()->GetCreature(m_guidTyrion))
                             DoScriptText(SAY_TYRION_2, pTyrion);
                         if (Creature* pMarzon = m_creature->GetMap()->GetCreature(m_guidMarzon))
+                        {
+                            pMarzon->SetPvP(false);
                             pMarzon->SetFactionTemplateId(FACTION_ENEMYY);
+                        }
+                        m_creature->SetPvP(false);
                         m_creature->SetFactionTemplateId(FACTION_ENEMYY);
                         m_uiEventTimer = 5000;
                         ++m_uiEventPhase;
