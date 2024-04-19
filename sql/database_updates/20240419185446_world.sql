333fa3f03fa16252f4b3f2c83dc2388091d9de8f
Dormilon <79369417+Dormilon@users.noreply.github.com>
2024-04-19 23:06:20 +0300

    Item Recipe: Gilneas Hot Stew have coocking recipe category.
    Fixes https://turtle-wow.org/bug-report?id=6889

--- sql/database_updates/20240419185446_world.sql
+++ sql/database_updates/20240419185446_world.sql
@@ -1,4 +1,6 @@
--- Durability has been removed from bandage shirts -- https://turtle-wow.org/bug-report?id=6877
+-- Durability has been removed from bandage shirts -- Fixes https://turtle-wow.org/bug-report?id=6877
 update item_template set max_durability = 0 where entry in (69109,69110,69111,69112);
--- Item Nature's Call is no longer displayed behind the back during the shooting -- https://turtle-wow.org/bug-report?id=6815
+-- Item Nature's Call is no longer displayed behind the back during the shooting -- Fixes https://turtle-wow.org/bug-report?id=6815
 update item_template set sheath = 0 where entry in (61525);
+-- Item Recipe: Gilneas Hot Stew have coocking recipe category -- Fixes https://turtle-wow.org/bug-report?id=6889
+update item_template set class = 9, subclass = 5, material = 2 where entry in (61676);
