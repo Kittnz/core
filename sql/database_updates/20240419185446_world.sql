4be095e71150f63534f1ca48ea31b6ce45a23653
Dormilon <79369417+Dormilon@users.noreply.github.com>
2024-04-19 22:57:28 +0300

    Item Nature's Call is no longer displayed behind the back during the shooting
    Fixes https://turtle-wow.org/bug-report?id=6815

--- sql/database_updates/20240419185446_world.sql
+++ sql/database_updates/20240419185446_world.sql
@@ -1,2 +1,4 @@
 -- Durability has been removed from bandage shirts -- https://turtle-wow.org/bug-report?id=6877
 update item_template set max_durability = 0 where entry in (69109,69110,69111,69112);
+-- Item Nature's Call is no longer displayed behind the back during the shooting -- https://turtle-wow.org/bug-report?id=6815
+update item_template set sheath = 0 where entry in (61525);
