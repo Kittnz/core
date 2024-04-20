ab9262f7dee92211d0685be95ecdddd704ea0b93
slowtorta <slowtorta@gmail.com>
2024-04-20 11:58:42 +0100

    Direct world update.

---
+++ sql/database_updates/20240418213737_world.sql
@@ -0,0 +1,3 @@
+
+-- Changes by DRAGU
+REPLACE INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (922,-72.994537,-411.323669,55.249283,4.374668,36,'dm_edit1');
