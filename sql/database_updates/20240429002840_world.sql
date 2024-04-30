569a2894b1d7843b26108379889ccbeea75c25f8
slowtorta <slowtorta@gmail.com>
2024-04-30 05:21:10 +0100

    Direct world update.

--- sql/database_updates/20240429002840_world.sql
+++ sql/database_updates/20240429002840_world.sql
@@ -12,3 +12,6 @@ DELETE FROM gameobject_battleground WHERE guid = '5014334';
 DELETE FROM gameobject WHERE guid = '4006144';
 DELETE FROM game_event_gameobject WHERE guid = '4006144';
 DELETE FROM gameobject_battleground WHERE guid = '4006144';
+
+-- Changes by ZORIZAR
+REPLACE INTO `gameobject` VALUES ( 5018590, 24498, 451, 15688.5, 16555.6, 48.6115, 2.71575, 0, 0, 0.977418, 0.211314, 300, 300, 100, 1, 0, 0);
