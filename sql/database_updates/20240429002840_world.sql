ac1ce2c78219151c0e146810830963500638f3a6
slowtorta <slowtorta@gmail.com>
2024-04-30 02:50:29 +0100

    Direct world update.

---
+++ sql/database_updates/20240429002840_world.sql
@@ -0,0 +1,5 @@
+
+-- Changes by SHANG
+DELETE FROM gameobject WHERE guid = '4007523';
+DELETE FROM game_event_gameobject WHERE guid = '4007523';
+DELETE FROM gameobject_battleground WHERE guid = '4007523';
