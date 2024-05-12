d08655430a867c9d3a1feab54d084282a945ec6f
slowtorta <slowtorta@gmail.com>
2024-05-12 20:05:47 +0100

    Direct world update.

--- sql/database_updates/20240512155252_world.sql
+++ sql/database_updates/20240512155252_world.sql
@@ -3,3 +3,11 @@
 UPDATE `creature_addon` SET `stand_state`=0 WHERE `guid`=2583278;
 REPLACE INTO `creature` VALUES (2583268,61983,0,0,0,189,209.491,-401.61,23.8661,6.25961,604800,604800,0,100,100,0,0,0);
 REPLACE INTO `creature` VALUES (2583268,61983,0,0,0,189,209.491,-401.61,23.8661,6.25961,604800,604800,0,100,100,2,0,0);
+
+-- Changes by GHEOR
+DELETE FROM creature WHERE guid=2576402;
+DELETE FROM creature_addon WHERE guid=2576402;
+DELETE FROM creature_movement WHERE id=2576402;
+DELETE FROM game_event_creature WHERE guid=2576402;
+DELETE FROM game_event_creature_data WHERE guid=2576402;
+DELETE FROM creature_battleground WHERE guid=2576402;
