575b994f2aee5f9bc7910669b1743cc3fa3e946f
torta <torta@torta.com>
2024-05-14 23:54:36 +0200

    Don't mangos me

--- sql/database_updates/20240514101958_world.sql
+++ sql/database_updates/20240514101958_world.sql
@@ -1,5 +1,5 @@
 -- Do not open Requisitioning a Core after turning in A Profitable Acquisition.
 UPDATE `quest_template` SET `NextQuestInChain`=0 WHERE `entry`=40130;
 -- Do not open The Profitable Activation after turning in Requisitioning a Core.
-UPDATE `turtle_mangos`.`quest_template` SET `NextQuestInChain`=0 WHERE  `entry`=40131;
+UPDATE `quest_template` SET `NextQuestInChain`=0 WHERE  `entry`=40131;
 
