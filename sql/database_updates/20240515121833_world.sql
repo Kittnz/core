e490c16056e98a96811831f791ca875f7adfe36b
Dormilon <79369417+Dormilon@users.noreply.github.com>
2024-05-15 15:27:55 +0300

    Item fixes: start quest.

---
+++ sql/database_updates/20240515121833_world.sql
@@ -0,0 +1,8 @@
+-- The following items Advanced Gemology I starts the incorrect quest and should start the quest 41333.
+update item_template set start_quest = 41333 where entry = 56109;
+-- The following items Advanced Gemology II starts the incorrect quest and should start the quest 41334.
+update item_template set start_quest = 41334 where entry = 56108;
+-- The following items Advanced Goldsmithing I starts the incorrect quest and should start the quest 41335.
+update item_template set start_quest = 41335 where entry = 56110;
+-- The following items Advanced Goldsmithing II starts the incorrect quest and should start the quest 41337.
+update item_template set start_quest = 41337 where entry = 56111;
