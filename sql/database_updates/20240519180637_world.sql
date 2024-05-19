058648cdfa8f80df6099823b262bee935490c94c
slowtorta <slowtorta@gmail.com>
2024-05-19 21:27:59 +0100

    Direct world update.

---
+++ sql/database_updates/20240519180637_world.sql
@@ -0,0 +1,8 @@
+
+-- Changes by ZORIZAR
+DELETE FROM creature WHERE guid=2583285;
+DELETE FROM creature_addon WHERE guid=2583285;
+DELETE FROM creature_movement WHERE id=2583285;
+DELETE FROM game_event_creature WHERE guid=2583285;
+DELETE FROM game_event_creature_data WHERE guid=2583285;
+DELETE FROM creature_battleground WHERE guid=2583285;
