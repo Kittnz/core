a07199bd7a704ced317f7229a3105bffe03ae23f
torta <torta@torta.com>
2024-05-20 17:37:36 +0200

    Update 20240520143251_world.sql

--- sql/database_updates/20240520143251_world.sql
+++ sql/database_updates/20240520143251_world.sql
@@ -14,3 +14,43 @@ UPDATE `item_template` SET `display_id` = 68225 WHERE `entry` = 50609; --	Race C
 UPDATE `item_template` SET `display_id` = 68217 WHERE `entry` = 50610; --	Race Change Token: Undead
 UPDATE `item_template` SET `display_id` = 68219 WHERE `entry` = 50613; --	Race Change Token: Goblin
 UPDATE `item_template` SET `display_id` = 68221 WHERE `entry` = 50612; --	Race Change Token: High Elf
+
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 51920;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 51921;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 50105;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 50106;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 61106;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 50292;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 50290;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 50291;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 83090;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 83091;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 81210;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 50204;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 50250;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 50251;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 50252;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 50205;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 50206;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 81229;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 61105;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 61104;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 83092;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 83099;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 83100;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 50212;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 81230;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 81206;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 81209;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 50207;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 50208;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 50209;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 50220;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 50221;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 50223;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 50224;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 81255;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 51010;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 51011;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 81208;
+UPDATE `item_template` SET `display_id` = 68226 WHERE `entry` = 81228;
