ffd00be02e093ecb3e87b754ccf1c834efeaa4ba
torta <torta@torta.com>
2024-05-20 16:38:48 +0200

    New icons.

---
+++ sql/database_updates/20240520143251_world.sql
@@ -0,0 +1,16 @@
+
+UPDATE `item_template` SET `display_id` = 68213 WHERE `entry` = 50000; -- 'Token: Character Name Change', '代币：角色名称变更', 160, 0),
+UPDATE `item_template` SET `display_id` = 68214 WHERE `entry` = 80499; -- 'Token: Guild Name Change', '代币：公会名称变更', 300, 0),
+UPDATE `item_template` SET `display_id` = 68212 WHERE `entry` = 80699; -- 'Token: Appearance Change', '代币：外观变更', 160, 0),
+UPDATE `item_template` SET `display_id` = 68215 WHERE `entry` = 80555; -- 'Token: Pet Rename', '代币:宠物重命名', 100, 0),
+
+UPDATE `item_template` SET `display_id` = 68220 WHERE `entry` = 50603; --	Race Change Token: Human
+UPDATE `item_template` SET `display_id` = 68218 WHERE `entry` = 50604; --	Race Change Token: Gnome
+UPDATE `item_template` SET `display_id` = 68216 WHERE `entry` = 50605; --	Race Change Token: Dwarf
+UPDATE `item_template` SET `display_id` = 68222 WHERE `entry` = 50606; --	Race Change Token: Night Elf
+UPDATE `item_template` SET `display_id` = 68224 WHERE `entry` = 50607; --	Race Change Token: Orc
+UPDATE `item_template` SET `display_id` = 68223 WHERE `entry` = 50608; --	Race Change Token: Troll
+UPDATE `item_template` SET `display_id` = 68225 WHERE `entry` = 50609; --	Race Change Token: Tauren
+UPDATE `item_template` SET `display_id` = 68217 WHERE `entry` = 50610; --	Race Change Token: Undead
+UPDATE `item_template` SET `display_id` = 68219 WHERE `entry` = 50613; --	Race Change Token: Goblin
+UPDATE `item_template` SET `display_id` = 68221 WHERE `entry` = 50612; --	Race Change Token: High Elf
