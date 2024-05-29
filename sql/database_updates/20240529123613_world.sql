575fa7f2b3aabee7c72ab36f8e5c808f1dbb4796
Dormilon <79369417+Dormilon@users.noreply.github.com>
2024-05-29 17:44:48 +0300

    Item Earthclad Brasers have vendor price now.
    Fixes https://turtle-wow.org/bug-report?id=6932

--- sql/database_updates/20240529123613_world.sql
+++ sql/database_updates/20240529123613_world.sql
@@ -1,2 +1,4 @@
 -- https://turtle-wow.org/bug-report?id=7277 Item Mageweave Cloth removed from loot of NPCs Gibbering Ghoul and Scourge Warder.
 delete from creature_loot_template where item = 4338 and entry in (8525,8531);
+-- https://turtle-wow.org/bug-report?id=6932 Item Earthclad Brasers have vendor price now.
+update item_template set buy_price = 52082, sell_price = 10416 where entry = 80851;
