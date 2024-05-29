0c65a258ea850d229722e4fb27088f705b5b4d58
Dormilon <79369417+Dormilon@users.noreply.github.com>
2024-05-29 17:45:21 +0300

    NPC Moonstalker Matriarch stuck in the tree in the Darkshore is free now.
    Fixes https://turtle-wow.org/bug-report?id=7270

--- sql/database_updates/20240529123613_world.sql
+++ sql/database_updates/20240529123613_world.sql
@@ -2,3 +2,5 @@
 delete from creature_loot_template where item = 4338 and entry in (8525,8531);
 -- https://turtle-wow.org/bug-report?id=6932 Item Earthclad Brasers have vendor price now.
 update item_template set buy_price = 52082, sell_price = 10416 where entry = 80851;
+-- https://turtle-wow.org/bug-report?id=7270 NPC Moonstalker Matriarch stuck in the tree in the Darkshore is free now.
+update creature set position_x = 7736.90, position_y = -1063.71, position_z = 38.11 where guid = 36959;
