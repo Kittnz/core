93426192c80473fdf38b76ca4dcb4d5a6cbd5570
Dormilon <79369417+Dormilon@users.noreply.github.com>
2024-05-30 16:31:35 +0300

    NPCs Wavecrest Siren, Wavecrest Tidefinder ,Spitefin Wavecreaper ,Spitefin Tidehunter have money loot now.
    Fixes https://turtle-wow.org/bug-report?id=7341

--- sql/database_updates/20240530114351_world.sql
+++ sql/database_updates/20240530114351_world.sql
@@ -1,2 +1,9 @@
 -- https://turtle-wow.org/bug-report?id=6407 Forlorn summit scourgestones dropping correctly now.
 update creature_loot_template set condition_id = 17 where item = 12840 and entry in (61766,61767,61769);
+-- https://turtle-wow.org/bug-report?id=7341 NPCs Wavecrest Siren, Wavecrest Tidefinder ,Spitefin Wavecreaper ,Spitefin Tidehunter have money loot now.
+update creature_template set gold_min = 126, gold_max = 170 where entry = 61131;
+update creature_template set gold_min = 126, gold_max = 170 where entry = 61130;
+update creature_template set gold_min = 126, gold_max = 170 where entry = 61087;
+update creature_template set gold_min = 126, gold_max = 170 where entry = 61086;
+update creature_template set gold_min = 126, gold_max = 170 where entry = 61085;
+update creature_template set gold_min = 126, gold_max = 170 where entry = 61083;
