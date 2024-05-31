78a5f8c9e67ee0661c1b30af0fecefbdd3e77111
Dormilon <79369417+Dormilon@users.noreply.github.com>
2024-05-31 16:24:10 +0300

    Unique items for Scarlet Monastery have been removed from the Crescent Grove loot table.
    Fixes https://turtle-wow.org/bug-report?id=4299

--- sql/database_updates/20240531113816_world.sql
+++ sql/database_updates/20240531113816_world.sql
@@ -1,2 +1,5 @@
 -- https://turtle-wow.org/bug-report?id=3229 Low-level loot table has been deleted from NPC Rage Talon DRAGON Guard.
 delete from creature_loot_template where item = 30015 and entry = 10366;
+-- https://turtle-wow.org/bug-report?id=4299 Unique items for Scarlet Monastery have been removed from the Crescent Grove loot table.
+delete from creature_loot_template where entry = 92118 and item in (10332,5819,8226,7728,7753,7729,7730,7752,7754,7786,7787,7761,8226,7727);
+delete from creature_loot_template where entry = 92119 and item in (10332,5819,8226,7728,7753,7729,7730,7752,7754,7786,7787,7761,8226,7727);
