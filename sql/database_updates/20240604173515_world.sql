7fc1860d5e16803209a5a3bcb8e1d9ffafa0ff80
slowtorta <slowtorta@gmail.com>
2024-06-04 19:35:48 +0100

    Direct world update.

--- sql/database_updates/20240604173515_world.sql
+++ sql/database_updates/20240604173515_world.sql
@@ -1,3 +1,26 @@
 
 -- Changes by DRAGU
 REPLACE INTO `creature` VALUES (2583546,9987,0,0,0,1,16229.1,16268.4,13.1582,5.00529,120,120,0,100,100,0,0,0);
+
+-- Changes by COCELY
+DELETE FROM gameobject WHERE guid = '5003329';
+DELETE FROM game_event_gameobject WHERE guid = '5003329';
+DELETE FROM gameobject_battleground WHERE guid = '5003329';
+DELETE FROM gameobject WHERE guid = '5001441';
+DELETE FROM game_event_gameobject WHERE guid = '5001441';
+DELETE FROM gameobject_battleground WHERE guid = '5001441';
+DELETE FROM gameobject WHERE guid = '5003330';
+DELETE FROM game_event_gameobject WHERE guid = '5003330';
+DELETE FROM gameobject_battleground WHERE guid = '5003330';
+DELETE FROM gameobject WHERE guid = '5001433';
+DELETE FROM game_event_gameobject WHERE guid = '5001433';
+DELETE FROM gameobject_battleground WHERE guid = '5001433';
+DELETE FROM gameobject WHERE guid = '5000512';
+DELETE FROM game_event_gameobject WHERE guid = '5000512';
+DELETE FROM gameobject_battleground WHERE guid = '5000512';
+DELETE FROM gameobject WHERE guid = '5001437';
+DELETE FROM game_event_gameobject WHERE guid = '5001437';
+DELETE FROM gameobject_battleground WHERE guid = '5001437';
+DELETE FROM gameobject WHERE guid = '5001434';
+DELETE FROM game_event_gameobject WHERE guid = '5001434';
+DELETE FROM gameobject_battleground WHERE guid = '5001434';
