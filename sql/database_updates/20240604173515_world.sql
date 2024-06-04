c65da581b9e435d88e1e5fe8d256dd2a9a1f2238
slowtorta <slowtorta@gmail.com>
2024-06-04 21:39:38 +0100

    Direct world update.

--- sql/database_updates/20240604173515_world.sql
+++ sql/database_updates/20240604173515_world.sql
@@ -27,3 +27,11 @@ DELETE FROM gameobject_battleground WHERE guid = '5001434';
 
 -- Changes by DRAGU
 DELETE FROM `npc_vendor` WHERE `entry`='91959' AND `item`='9260';
+INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `itemflags`) VALUES('91959','4595','0','0','0');
+UPDATE `creature` SET `position_x` = -14005.245117, `position_y` = 2282.107422, `position_z` = 59.547104, `orientation` = 6.035007 WHERE `guid` = 2567326;
+UPDATE `creature` SET `position_x` = -13998.135742, `position_y` = 2272.729004, `position_z` = 60.405087, `orientation` = 6.208764 WHERE `guid` = 2567327;
+UPDATE `creature` SET `position_x` = -13997.411133, `position_y` = 2282.439209, `position_z` = 59.784691, `orientation` = 6.208764 WHERE `guid` = 2567326;
+UPDATE `creature` SET `position_x` = -13997.948242, `position_y` = 2274.141602, `position_z` = 59.914036, `orientation` = 6.229970 WHERE `guid` = 2567327;
+UPDATE `creature` SET `position_x` = -14011.561523, `position_y` = 2273.181152, `position_z` = 59.753796, `orientation` = 6.237825 WHERE `guid` = 2567327;
+UPDATE `creature` SET `position_x` = -14011.022461, `position_y` = 2281.850830, `position_z` = 59.279408, `orientation` = 6.214263 WHERE `guid` = 2567326;
+REPLACE INTO `gameobject` VALUES ( 5019607, 2004031, 0, -14089.1, 2271.81, 66.7103, 6.09724, 0, 0, 0.0928383, -0.995681, 300, 300, 100, 1, 0, 0);
