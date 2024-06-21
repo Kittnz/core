4c0dd8e5d7cf47d781852205ea8880e6c8e1d97e
slowtorta <slowtorta@gmail.com>
2024-06-22 00:43:28 +0100

    Direct world update.

---
+++ sql/database_updates/20240621234255_world.sql
@@ -0,0 +1,5 @@
+
+-- Changes by MEFLOR
+DELETE FROM gameobject WHERE guid = '5019634';
+DELETE FROM game_event_gameobject WHERE guid = '5019634';
+DELETE FROM gameobject_battleground WHERE guid = '5019634';
