ff1e6b5b0f7c59febc74fbcc2537260bb3e31a15
Jamey <jamey@jamey.com>
2024-06-24 04:38:41 +0200

    Remove debuglog

--- src/game/Objects/Unit.cpp
+++ src/game/Objects/Unit.cpp
@@ -8194,11 +8194,6 @@ bool Unit::HandleStatModifier(UnitMods unitMod, UnitModifierType modifierType, f
         return false;
     }
 
-    if (unitMod == UNIT_MOD_ATTACK_POWER && IsCreature() && !sWorld.getConfig(CONFIG_BOOL_SEA_NETWORK) && sWorld.IsPvPRealm())
-    {
-        sLog.outString("[AURADEBUG]:[%u]: HandleStatModifier: apply: %s, CanModifyStats() : %s", GetGUIDLow(), apply ? "yes" : "no", CanModifyStats() ? "yes" : "no");
-    }
-
     float val = 1.0f;
 
     switch (modifierType)
