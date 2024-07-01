75b573135f7b2509cf6b495068e2512a494cf313
slowtorta <slowtorta@gmail.com>
2024-07-01 22:29:27 +0100

    Direct world update.

--- sql/database_updates/20240701202853_world.sql
+++ sql/database_updates/20240701202853_world.sql
@@ -2,3 +2,7 @@
 -- Changes by MORIENA
 REPLACE INTO `creature` VALUES (2583662,62063,0,0,0,1,-7128.97,-3758.18,9.6184,0.946226,120,120,0,100,100,0,0,0);
 REPLACE INTO `creature` VALUES (2583663,62064,0,0,0,0,-11074.9,516.329,25.7713,1.14977,120,120,0,100,100,0,0,0);
+
+-- Changes by WOJI
+UPDATE `creature` SET `position_x` = -6287.586914, `position_y` = -2775.137207, `position_z` = 375.592957, `orientation` = 3.313793 WHERE `guid` = 2582884;
+UPDATE `creature` SET `position_x` = -6286.374512, `position_y` = -2775.062256, `position_z` = 375.709686, `orientation` = 3.408040 WHERE `guid` = 2582884;
