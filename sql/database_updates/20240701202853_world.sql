e4efd6987f20715d619d1a7fa77920d539f2eb08
slowtorta <slowtorta@gmail.com>
2024-07-02 05:41:03 +0100

    Direct world update.

--- sql/database_updates/20240701202853_world.sql
+++ sql/database_updates/20240701202853_world.sql
@@ -108,3 +108,8 @@ DELETE FROM creature_movement WHERE id=2583680;
 DELETE FROM game_event_creature WHERE guid=2583680;
 DELETE FROM game_event_creature_data WHERE guid=2583680;
 DELETE FROM creature_battleground WHERE guid=2583680;
+
+-- Changes by JAMEYPTR
+DELETE FROM gameobject WHERE guid = '5019640';
+DELETE FROM game_event_gameobject WHERE guid = '5019640';
+DELETE FROM gameobject_battleground WHERE guid = '5019640';
