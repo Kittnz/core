73add650d03b5ed5bc26a2601d9b791fbc36fc99
kittnz <frederik156@hotmail.com>
2024-10-20 17:12:01 +0200

    Revert "Add missing relations for quest Mortal Champions." & "Correct stats of Deep Sea Threshadon."
    This reverts commit cea2dd2be61dab074619071608c9ace649c64a0f.

--- sql/database_updates/20241020145605_world.sql
+++
@@ -1,3 +0,0 @@
--- Add missing relations for quest Mortal Champions.
-INSERT INTO `creature_questrelation` (`id`, `quest`, `patch_min`) VALUES (15503, 8579, 7);
-INSERT INTO `creature_involvedrelation` (`id`, `quest`, `patch_min`) VALUES (15503, 8579, 7);
